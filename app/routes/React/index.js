2d23e320ff982b70fc228d5ad120ab61b3b0016c
OneDev0411 <cryptoapple1109@gmail.com>
2015-12-03 14:23:10 -0600

    routes edits

--- app/routes/React/index.js
+++ app/routes/React/index.js
@@ -19,6 +19,8 @@ module.exports = (app, config) => {
       
       res.locals.reactMarkup = reactMarkup
 
+      console.log(reactMarkup)
+
       if (error) {
       
         res.status(500).send(error.message)
