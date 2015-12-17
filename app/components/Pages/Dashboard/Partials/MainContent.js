17075d4cf36a79fe715d6bddf976f328b075d1b5
OneDev0411 <cryptoapple1109@gmail.com>
2015-12-16 21:40:58 -0600

    typing

--- app/components/Pages/Dashboard/Partials/MainContent.js
+++ app/components/Pages/Dashboard/Partials/MainContent.js
@@ -80,7 +80,7 @@ export default class MainContent extends Component {
     )
 
     let is_typing
-    if(data.is_typing && data.is_typing.author_id !== data.user.id){
+    if(data.is_typing && data.is_typing.author_id !== data.user.id && data.is_typing.room_id === data.current_room.id){
       // Get user name
       let author_typing = _.findWhere(data.current_room.users, { id: data.is_typing.author_id })
       if(author_typing)
