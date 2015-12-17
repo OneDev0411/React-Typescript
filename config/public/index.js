2ff94c77fcfa077f82370bf30e0a20d9e3c697ef
OneDev0411 <cryptoapple1109@gmail.com>
2015-12-17 11:52:20 -0600

    config edits

--- config/public/index.js
+++ config/public/index.js
@@ -3,5 +3,4 @@ let env = process.env.NODE_ENV
 let env_file = env
 if(env === 'staging') env_file = 'development'
 const config = require('./' + env_file)
-console.log(config)
 module.exports = config
