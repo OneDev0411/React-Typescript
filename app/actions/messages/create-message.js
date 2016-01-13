afd767f8f46b554ed1ed3f17d3027056d968800b
OneDev0411 <cryptoapple1109@gmail.com>
2016-01-12 20:11:14 -0600

    socket tests

--- app/actions/messages/create-message.js
+++ app/actions/messages/create-message.js
@@ -1,15 +1,14 @@
 // actions/create-message.js
 import config from '../../../config/public'
 
-// Socket.io
-import io from 'socket.io-client'
-
 export default (user, room, comment) => {
   // Socket
-  const socket = io(config.socket.server)
+  const socket = window.socket
   const message = {
     comment,
     message_type: 'TopLevel'
   }
-  socket.emit('Message.Send', room.id, message)
+  socket.emit('Message.Send', room.id, message, (res) => {
+    console.log(res)
+  })
 }
