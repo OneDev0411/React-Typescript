3b89bbec2e82996bfad43a141ae268f7790e2054
OneDev0411 <cryptoapple1109@gmail.com>
2016-02-05 13:45:56 -0600

    better chat room loading

--- app/actions/user/get-rooms.js
+++ app/actions/user/get-rooms.js
@@ -6,21 +6,21 @@ import AppStore from '../../stores/AppStore'
 // Get all messages
 import getAllMessages from '../messages/get-all-messages'
 
-export default (user, room_id) => {
+export default user => {
   const params = {
     access_token: user.access_token
   }
   User.getRooms(params, (err, response) => {
     // Success
     if (response.status === 'success') {
-      const rooms = response.data
+      let rooms = response.data
+      // sort my latest message
+      rooms = _.sortBy(rooms, room => {
+        return -room.latest_message.updated_at
+      })
       AppStore.data.rooms = rooms
+      AppStore.data.current_room = rooms[0]
       // Get messages for current room
-      let current_room = rooms[0]
-      // If current room id
-      if (room_id)
-        current_room = _.findWhere(rooms, { id: room_id })
-      AppStore.data.current_room = current_room
       getAllMessages(user, rooms)
     }
     AppStore.emitChange()
