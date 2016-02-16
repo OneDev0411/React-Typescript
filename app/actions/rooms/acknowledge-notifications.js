d7227ebcb3a5be68d3e947287a7edb211a25d2b3
OneDev0411 <cryptoapple1109@gmail.com>
2016-02-16 13:30:29 +0100

    Update the UI, do the actual work in the background. Makes UI feel instant.

--- app/actions/rooms/acknowledge-notifications.js
+++ app/actions/rooms/acknowledge-notifications.js
@@ -3,20 +3,20 @@ import Room from '../../models/Room'
 import AppStore from '../../stores/AppStore'
 
 export default (user, room) => {
+  const summary = AppStore.data.notifications.summary;
+  summary.room_notification_count--
+  summary.room_notification_summaries = summary.room_notification_summaries.filter(s => {
+    if (s.room_id === room)
+      return false
+
+    return true
+  });
+
+  AppStore.emitChange()
+
   const params = {
     room,
     access_token: user.access_token
   }
-  Room.acknowledgeNotifications(params, () => {
-    const summary = AppStore.data.notifications.summary;
-    summary.room_notification_count--
-    summary.room_notification_summaries = summary.room_notification_summaries.filter(s => {
-      if (s.room_id === room)
-        return false
-
-      return true
-    });
-
-    AppStore.emitChange()
-  })
+  Room.acknowledgeNotifications(params, () => {})
 }
