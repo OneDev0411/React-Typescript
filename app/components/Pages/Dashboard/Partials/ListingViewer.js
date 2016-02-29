a7d93d5f0e10d736ccf7180eb12e4ebbd4eadc15
OneDev0411 <cryptoapple1109@gmail.com>
2016-02-29 17:06:19 -0600

    more listing info

--- app/components/Pages/Dashboard/Partials/ListingViewer.js
+++ app/components/Pages/Dashboard/Partials/ListingViewer.js
@@ -166,7 +166,6 @@ export default class ListingViewer extends Component {
           </span>
         )
       }
-      console.log(listing)
       main_content = (
         <div>
           <div style={ S('p-0 relative') }>
