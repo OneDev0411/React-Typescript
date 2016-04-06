e9d2869f7eca1cd9cebf01f3692340aae1006b0d
OneDev0411 <cryptoapple1109@gmail.com>
2016-04-06 12:16:06 -0500

    verify token fix

--- app/routes/app/verify.js
+++ app/routes/app/verify.js
@@ -1,30 +1,22 @@
 // verify.js
 import Crypto from '../../models/Crypto'
 import helpers from '../../utils/helpers'
-
 module.exports = (app, config) => {
-
   app.get('/verify_email',(req, res) => {
-    
     const token = req.query.token
     return res.redirect('/verify/email?token=' + token)
-
   })
-
   app.get('/verify_email/submitted',(req, res) => {
-    
-    let token = decodeURIComponent(req.query.token).replace(' ', '+')
+    let token = decodeURIComponent(req.query.token).replace(/\s/g, '+')
     const decrypted_token = Crypto.decrypt(token).split(':')
     const email = decrypted_token[0]
     const code = decrypted_token[1]
     const api_url = config.api.url
     const verify_email_url = api_url + '/users/email_confirmed'
-
     const request_object = {
       email: email,
       code: code
     }
-
     fetch(verify_email_url,{
       method: 'patch',
       headers: {  
@@ -46,18 +38,13 @@ module.exports = (app, config) => {
   })
 
   app.get('/verify_phone',(req, res) => {
-    
     const token = req.query.token
     return res.redirect('/verify/phone?token=' + token)
   })
 
-  app.get('/reset_password',(req, res) => {
-    
+  app.get('/reset_password',(req, res) => {    
     const token = req.query.token
-
     // TODO: test token for validity then redirect to forgot password
     return res.redirect('/password/reset/?token=' + token)
-
   })
-
 }
