7912df12baf6812bee1ab987626077bec4f4d3f0
OneDev0411 <cryptoapple1109@gmail.com>
2016-04-07 22:37:22 -0500

    search icon

--- app/components/Pages/Dashboard/Recents/Partials/MainContent.js
+++ app/components/Pages/Dashboard/Recents/Partials/MainContent.js
@@ -229,7 +229,8 @@ export default class MainContent extends Component {
           <div style={ main_style }>
             <div className="dashboard__chat-rooms pull-left" style={ rooms_column_style }>
               <div style={ S('p-10 pt-15 h-60 relative') }>
-                <input ref="search_text" onKeyUp={ this.handleSearchRoomKeyUp.bind(this) } style={ S('w-82p br-100') } type="text" placeholder="Search chats" className="form-control pull-left" />
+                <img style={ S('w-12 h-12 absolute l-20 t-28') } src="/images/dashboard/chats/search.svg" />
+                <input ref="search_text" onKeyUp={ this.handleSearchRoomKeyUp.bind(this) } style={ S('w-82p br-100 pl-30') } type="text" placeholder="Search chats" className="form-control pull-left" />
                 <button onClick={ this.props.showModal.bind(this, 'create-chat') } type="button" className="btn btn-primary" style={ S('w-40 h-40 ml-6 pointer absolute p-0 t-15 r-8 br-100') }>
                   <img src="/images/dashboard/icons/create-chat.svg"/>
                 </button>
