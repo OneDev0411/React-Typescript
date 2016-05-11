0b3906ba2518d1d010438b5bb8a3c1b6638070f2
OneDev0411 <cryptoapple1109@gmail.com>
2016-05-11 10:08:08 -0500

    search sold MLS for agents

--- app/routes/api/listings/search-listings.js
+++ app/routes/api/listings/search-listings.js
@@ -2,7 +2,7 @@
 module.exports = (app, config) => {
   app.get('/api/listings/search',(req, res) => {
     const api_url = config.api.url
-    const endpoint = api_url + '/listings/search?q=' + req.query.q + '&status=' + req.query.status
+    const endpoint = api_url + '/listings/search?q=' + req.query.q
     const access_token = req.query.access_token
     fetch(endpoint, {
       method: 'get',
