7101e334fa526966e413815dfe3e0b8de4d7d28e
OneDev0411 <cryptoapple1109@gmail.com>
2016-05-19 10:46:41 -0500

    rm mailchimp

--- app/components/Pages/Landing.js
+++ app/components/Pages/Landing.js
@@ -130,21 +130,6 @@ export default class Landing extends Component {
       blinking_cursor = ''
 
     // Content from data props
-    // Subheadline
-    let subheadline
-    if (data.content)
-      subheadline = data.content.subheadline
-
-    // Call to action
-    let call_to_action
-    if (data.content)
-      call_to_action = data.content.call_to_action
-
-    // Placeholder text
-    let placeholder_text
-    if (data.content)
-      placeholder_text = data.content.placeholder_text
-
     // Styles
     const page_style = {
       position: 'relative',
@@ -162,11 +147,6 @@ export default class Landing extends Component {
       boxShadow: 'none'
     }
     const headline_style = S('mb-35')
-    const tag_style = S('font-22 mb-40')
-    const form_wrap_style = {
-      ...S('br-4 p-30 pb-20'),
-      'backgroundColor': 'rgba(0, 0, 0, 0.5)'
-    }
     const footer_style = {
       ...S('absolute b-0 w-100p mb-20 pt-20 color-ededed font-13  z-2'),
       borderTop: '1px solid rgba(168,168,168, 0.3)'
@@ -187,13 +167,6 @@ export default class Landing extends Component {
         <source src={'/videos/landing/' + video_src + '.ogv'} type="video/ogg"/>
       </video>
     )
-    let call_to_action_text
-    if (call_to_action) {
-      call_to_action_text = (
-        <p style={ S('pt-0 p-10 font-17') } dangerouslySetInnerHTML={ { __html: emojify.replace(call_to_action) } } >
-        </p>
-      )
-    }
     let login_btn_li_style
     let login_btn_style
     let signup_input_style = {
@@ -277,24 +250,6 @@ export default class Landing extends Component {
               <h1 className="tempo headline" style={ headline_style }>
                 { headline_text }
               </h1>
-              <p style={ tag_style }>
-                { subheadline }
-              </p>
-              <div className="form-wrap center-block" style={ form_wrap_style }>
-                { call_to_action_text }
-                <form onSubmit={ this.showThankYou } action="//rechat.us11.list-manage.com/subscribe/post?u=c21e4aeea43aececadaf53146&amp;id=7f1ad19560" method="post" name="mc-embedded-subscribe-form" target="_blank">
-                  <Col className="form-input--email" sm={8} style={ S('pl-0') }>
-                    <Input style={ S('w-100p') } bsSize="large" type="email" name="EMAIL" placeholder={ placeholder_text }/>
-                    <div style={ S('l-5000n absolute') } aria-hidden="true">
-                      <input type="text" name="b_c21e4aeea43aececadaf53146_7f1ad19560" tabIndex="-1" value="" />
-                    </div>
-                  </Col>
-                  <Col sm={4} style={ S('pl-0 pr-0') }>
-                    <Button style={ S('w-100p') } bsStyle="primary" bsSize="large" type="submit">Notify Me!</Button>
-                  </Col>
-                </form>
-                <div className="clearfix"></div>
-              </div>
             </div>
           </div>
         </main>
