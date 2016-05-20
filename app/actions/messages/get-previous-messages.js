5b04ea934e02556d6b925eb6802d5b80857ffe16
OneDev0411 <cryptoapple1109@gmail.com>
2016-05-20 10:00:11 -0500

    load previous edits

--- app/actions/messages/get-previous-messages.js
+++ app/actions/messages/get-previous-messages.js
@@ -1,6 +1,7 @@
 // actions/get-messages.js
 import Room from '../../models/Room'
 import AppStore from '../../stores/AppStore'
+import _ from 'lodash'
 
 export default (user, room, scroll_height) => {
   if (!room || AppStore.data.current_room.loading_previous)
@@ -24,6 +25,11 @@ export default (user, room, scroll_height) => {
   AppStore.emitChange()
 
   Room.getMessages(params, (err, response) => {
+    // Kill return if user changed rooms
+    if (room.id !== AppStore.data.current_room.id) {
+      delete room.loading_previous
+      return AppStore.emitChange()
+    }
     // Success
     if (response.status === 'success') {
       // Reverse order to latest first...
@@ -31,12 +37,15 @@ export default (user, room, scroll_height) => {
       AppStore.data.current_room = room
     }
 
-    if (new_message_count > AppStore.data.messages.length)
-      AppStore.data.current_room.showing_all = true
-
+    // if (new_message_count > AppStore.data.messages.length)
+    //   AppStore.data.current_room.showing_all = true
     AppStore.data.current_room.message_count = new_message_count
-    AppStore.data.current_room.viewing_previous = true
-    AppStore.data.current_room.scroll_height = scroll_height + 60
+    // AppStore.data.current_room.viewing_previous = true
+    // AppStore.data.current_room.scroll_height = scroll_height + 60
+    // Add to rooms array
+    const room_index = _.findIndex(AppStore.data.rooms, { id: room.id })
+    // console.log(room_index)
+    AppStore.data.rooms[room_index] = AppStore.data.current_room
     delete AppStore.data.current_room.loading_previous
     AppStore.emitChange()
   })
