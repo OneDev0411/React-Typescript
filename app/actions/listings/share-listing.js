f1cd9d12ecc218fac9a65035af8daa3ce61b9b94
OneDev0411 <cryptoapple1109@gmail.com>
2016-06-07 12:49:49 -0500

    get all rooms again after listing share

--- app/actions/listings/share-listing.js
+++ app/actions/listings/share-listing.js
@@ -1,6 +1,9 @@
 // actions/listings/share-listing.js
 import Room from '../../models/Room'
+import User from '../../models/User'
 import AppStore from '../../stores/AppStore'
+import async from 'async'
+import getAllMessages from '../messages/get-all-messages'
 export default (user, mls_number, message, rooms, users, emails, phone_numbers, notification) => {
   const params = {
     access_token: user.access_token,
@@ -12,10 +15,23 @@ export default (user, mls_number, message, rooms, users, emails, phone_numbers, 
     phone_numbers,
     notification
   }
-  Room.createRec(params, () => {
-    // Success
-    delete AppStore.data.share_modal.sending_share
-    delete AppStore.data.show_share_listing_modal
-    AppStore.emitChange()
-  })
+  async.series([
+    callback => {
+      Room.createRec(params, () => {
+        // Success
+        delete AppStore.data.share_modal.sending_share
+        delete AppStore.data.show_share_listing_modal
+        AppStore.emitChange()
+        callback()
+      })
+    },
+    () => {
+      User.getRooms(params, (err, res) => {
+        const updated_rooms = res.data
+        AppStore.data.rooms = updated_rooms
+        getAllMessages(user, updated_rooms)
+        AppStore.emitChange()
+      })
+    }
+  ])
 }
