6c0cd4b2b2c7de586bb877e3016b48475fefb518
OneDev0411 <cryptoapple1109@gmail.com>
2016-06-10 15:05:29 -0500

    new room refresh

--- app/actions/notifications/get-summary.js
+++ app/actions/notifications/get-summary.js
@@ -1,7 +1,8 @@
 // actions/notifications/get.js
 import Notification from '../../models/Notification'
 import AppStore from '../../stores/AppStore'
-
+import AppDispatcher from '../../dispatcher/AppDispatcher'
+import _ from 'lodash'
 export default (user) => {
   AppStore.data.notifications = {}
   AppStore.data.notifications.summary = {}
@@ -11,6 +12,27 @@ export default (user) => {
   Notification.getSummary(params, (err, response) => {
     const summary = response.data
     AppStore.data.notifications.summary = summary
+    // If room notification on new room, refresh room
+    if (summary.room_notification_summaries && summary.room_notification_summaries.length) {
+      let refresh_room = true
+      // Search for room
+      summary.room_notification_summaries.forEach(summary_loop => {
+        if (_.find(AppStore.data.rooms, { id: summary_loop.room_id }))
+          refresh_room = false
+      })
+      if (refresh_room) {
+        // If new room refresh with get-rooms
+        const current_room = AppStore.data.current_room
+        let room_id
+        if (current_room)
+          room_id = current_room.id
+        AppDispatcher.dispatch({
+          action: 'get-rooms',
+          user,
+          room_id
+        })
+      }
+    }
     AppStore.emitChange()
   })
 }
