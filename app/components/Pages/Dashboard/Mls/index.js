eed9b63e4122906e351d641c1a0f42cce1f67c8a
OneDev0411 <cryptoapple1109@gmail.com>
2016-06-15 10:26:34 -0500

    My Activity for clients

--- app/components/Pages/Dashboard/Mls/index.js
+++ app/components/Pages/Dashboard/Mls/index.js
@@ -464,7 +464,7 @@ export default class Mls extends Component {
             { data.show_alerts_map ? underline : '' }
           </li>
           <li style={ S('relative pull-left color-263445 font-28 mr-60') }>
-            <span onClick={ this.handleTabClick.bind(this, 'actives') } style={ S('pointer ' + (data.show_actives_map ? 'color-263445' : 'color-8696a4')) }>{ user && user.user_type === 'Agent' ? 'Activity' : 'My Homes' }</span>
+            <span onClick={ this.handleTabClick.bind(this, 'actives') } style={ S('pointer ' + (data.show_actives_map ? 'color-263445' : 'color-8696a4')) }>Activity</span>
             { data.show_actives_map ? underline : '' }
           </li>
         </ul>
