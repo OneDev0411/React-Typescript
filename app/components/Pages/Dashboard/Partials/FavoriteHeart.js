83258b5133e1be9ddfc25a26afdd4ba65b431c98
OneDev0411 <cryptoapple1109@gmail.com>
2016-07-07 16:56:11 -0500

    caret pointer

--- app/components/Pages/Dashboard/Partials/FavoriteHeart.js
+++ app/components/Pages/Dashboard/Partials/FavoriteHeart.js
@@ -9,7 +9,7 @@ export default class FavoriteHeart extends Component {
   render() {
     const listing = this.props.listing
     const mls_number = listing.mls_number
-    const heart_style = S('pointer absolute r-10 t-15 w-44 h-40 mr-5 z-2')
+    const heart_style = S('pointer absolute r-20 t-20 w-44 h-40 mr-5 z-2')
     return (
       <img
         onClick={ controller.listing_card.handleFavoriteAction.bind(this, listing) } style={ heart_style }
