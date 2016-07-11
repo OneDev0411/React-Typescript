fe160bb6af1299ea34a694bca4b279601aabb260
OneDev0411 <cryptoapple1109@gmail.com>
2016-07-11 10:26:11 -0500

    listing widget mobile improvements

--- app/components/Pages/Widgets/Partials/CheckEmailModal.js
+++ app/components/Pages/Widgets/Partials/CheckEmailModal.js
@@ -11,7 +11,7 @@ export default class CheckEmailModal extends Component {
       )
     }
     return (
-      <Modal dialogClassName={ data.is_mobile ? 'modal-mobile' : '' } show={ !data.show_signup_confirm_modal } onHide={ this.props.hideModal }>
+      <Modal dialogClassName={ data.is_mobile ? 'modal-mobile' : '' } show={ data.show_signup_confirm_modal } onHide={ this.props.hideModal }>
         <Modal.Body className="text-center" style={ !data.is_mobile ? S('w-600 h-480') : S('w-100p') } >
           <div style={ S('mb-20 mt-20 center-block text-center' + (!data.is_mobile ? ' mt-100 w-280' : '')) }>
             <img style={ S('h-68 mr-40 relative') } src={ data.brand ? data.brand.logo_url : '' } />
