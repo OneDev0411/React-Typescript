9ff514ab1d7dd47c485e1f144e35bc49437f1692
OneDev0411 <cryptoapple1109@gmail.com>
2016-07-19 16:18:05 -0500

    CORS rollback

--- app/app-server.js
+++ app/app-server.js
@@ -54,13 +54,6 @@ if(process.env.NODE_ENV === 'development'){
     res.header('Access-Control-Allow-Credentials', 'true')
     next()
   })
-} else {
-  app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    res.header('Access-Control-Allow-Credentials', 'true')
-    next()
-  })
 }
 
 // Routes
