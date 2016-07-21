30db83e1268f4348e37b0e6bc65784df55822e51
OneDev0411 <cryptoapple1109@gmail.com>
2016-07-21 08:25:12 -0500

    alert share delay

--- app/components/App.js
+++ app/components/App.js
@@ -207,30 +207,32 @@ export default class App extends Component {
       AppStore.emitChange()
     })
     socket.on('Room.UserJoined', (user, room) => {
-      // Add users
-      if (AppStore.data.rooms) {
-        if (_.find(AppStore.data.rooms, { id: room.id })) {
-          const index = _.findIndex(AppStore.data.rooms, { id: room.id })
-          AppStore.data.rooms[index].users.push(user)
+      setTimeout(() => {
+        // Add users
+        if (AppStore.data.rooms) {
+          if (_.find(AppStore.data.rooms, { id: room.id })) {
+            const index = _.findIndex(AppStore.data.rooms, { id: room.id })
+            AppStore.data.rooms[index].users.push(user)
+          } else {
+            room.users = [user]
+            AppStore.data.rooms = [
+              room,
+              ...AppStore.data.rooms
+            ]
+          }
         } else {
           room.users = [user]
-          AppStore.data.rooms = [
-            room,
-            ...AppStore.data.rooms
-          ]
+          AppStore.data.rooms = [room]
         }
-      } else {
-        room.users = [user]
-        AppStore.data.rooms = [room]
-      }
-      // Get messages
-      if (user.id === AppStore.data.user.id) {
-        AppDispatcher.dispatch({
-          action: 'get-room-and-messages',
-          user: AppStore.data.user,
-          room
-        })
-      }
+        // Get messages
+        if (user.id === AppStore.data.user.id) {
+          AppDispatcher.dispatch({
+            action: 'get-room-and-messages',
+            user: AppStore.data.user,
+            room
+          })
+        }
+      }, 1000)
     })
     socket.on('Notification', this.getNotifications)
   }
