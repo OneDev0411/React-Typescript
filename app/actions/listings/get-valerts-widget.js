002ebcb1d70e0484edc5c6ec9136a2ddb9d3ddac
OneDev0411 <cryptoapple1109@gmail.com>
2016-07-21 17:40:09 -0500

    shitty listings to the bottom

--- app/actions/listings/get-valerts-widget.js
+++ app/actions/listings/get-valerts-widget.js
@@ -19,6 +19,13 @@ export default (user, options) => {
         AppStore.data.widget.listings = _.slice(response.data, 0, 10)
       AppStore.data.widget.listings_info = response.info
     }
+    console.log(AppStore.data.widget.listings[0].price)
+    if (options.listing_statuses[0] === 'Sold') {
+      AppStore.data.widget.listings.sort((a, b) => {
+        return parseFloat(a.price) - parseFloat(b.price);
+      })
+      AppStore.data.widget.listings.reverse()
+    }
     delete AppStore.data.widget.is_loading
     delete AppStore.data.widget.is_loading_listings
     AppStore.emitChange()
