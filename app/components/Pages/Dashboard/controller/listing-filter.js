7dbae41b497df1a633374d46d28ec66328fda1fe
OneDev0411 <cryptoapple1109@gmail.com>
2016-08-11 10:18:13 -0500

    geo locate by listing location for schools

--- app/components/Pages/Dashboard/controller/listing-filter.js
+++ app/components/Pages/Dashboard/controller/listing-filter.js
@@ -214,7 +214,7 @@ const controller = {
     }
     AppStore.data.listing_map.is_loading = true
     AppStore.emitChange()
-    if (options.mls_areas || options.counties) {
+    if (options.mls_areas || options.counties || options.school_districts) {
       options.points = null
       ListingDispatcher.dispatch({
         action: 'get-valerts-no-geo',
