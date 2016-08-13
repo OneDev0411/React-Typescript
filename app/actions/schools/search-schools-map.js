e442f3ca206ab03cc5974ff15faba2bedf60513f
OneDev0411 <cryptoapple1109@gmail.com>
2016-08-13 12:42:32 -0500

    schools fix

--- app/actions/schools/search-schools-map.js
+++ app/actions/schools/search-schools-map.js
@@ -13,12 +13,11 @@ export default (district) => {
         AppStore.data.listing_map.schools = res.data
       else
         AppStore.data.listing_map.schools.push(...res.data)
-      AppStore.data.listing_map.schools = _.uniqBy(AppStore.data.listing_map.schools, 'name')
-      AppStore.data.listing_map.elementary_schools = _.filter(AppStore.data.listing_map.schools, { school_type: 'elementary_school' })
-      AppStore.data.listing_map.middle_schools = _.filter(AppStore.data.listing_map.schools, { school_type: 'middle_school' })
-      AppStore.data.listing_map.junior_high_schools = _.filter(AppStore.data.listing_map.schools, { school_type: 'junior_high_school' })
-      AppStore.data.listing_map.senior_high_schools = _.filter(AppStore.data.listing_map.schools, { school_type: 'senior_high_school' })
-      AppStore.data.listing_map.intermediate_schools = _.filter(AppStore.data.listing_map.schools, { school_type: 'intermediate_school' })
+      AppStore.data.listing_map.elementary_schools = _.uniqBy(_.filter(AppStore.data.listing_map.schools, { school_type: 'elementary_school' }), 'name')
+      AppStore.data.listing_map.middle_schools = _.uniqBy(_.filter(AppStore.data.listing_map.schools, { school_type: 'middle_school' }), 'name')
+      AppStore.data.listing_map.junior_high_schools = _.uniqBy(_.filter(AppStore.data.listing_map.schools, { school_type: 'junior_high_school' }), 'name')
+      AppStore.data.listing_map.senior_high_schools = _.uniqBy(_.filter(AppStore.data.listing_map.schools, { school_type: 'senior_high_school' }), 'name')
+      AppStore.data.listing_map.intermediate_schools = _.uniqBy(_.filter(AppStore.data.listing_map.schools, { school_type: 'intermediate_school' }), 'name')
       AppStore.emitChange()
     }
   })
