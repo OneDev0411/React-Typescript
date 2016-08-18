8ed0608b205a2cbb495d0c42041a5e337f693d1e
OneDev0411 <cryptoapple1109@gmail.com>
2016-08-18 16:31:53 -0500

    added price drop

--- app/actions/subdivisions/search-subdivisions-map.js
+++ app/actions/subdivisions/search-subdivisions-map.js
@@ -1,6 +1,7 @@
 // actions/subdivisions/search-subdivisions-map.js
 import AppStore from '../../stores/AppStore'
 import Subdivision from '../../models/Subdivision'
+// import _ from 'lodash'
 export default q => {
   const params = {
     q
@@ -8,6 +9,11 @@ export default q => {
   Subdivision.search(params, (err, res) => {
     if (res.status === 'success') {
       AppStore.data.listing_map.subdivisions = res.data
+      // AppStore.data.listing_map.subdivisions.forEach(subdivision => {
+      //   console.log(subdivision)
+      //   subdivision.appearances = Number(subdivision.appearances)
+      // })
+      // AppStore.data.listing_map.subdivisions = _.orderBy(AppStore.data.listing_map.subdivisions, ['appearances'], ['desc']);
       AppStore.emitChange()
     }
   })
