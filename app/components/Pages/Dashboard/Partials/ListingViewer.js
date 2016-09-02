2abfb408ac18edad671f2cf13bb63517ad7c8fc8
OneDev0411 <cryptoapple1109@gmail.com>
2016-09-02 16:58:32 -0500

    dom and cdom to int

--- app/components/Pages/Dashboard/Partials/ListingViewer.js
+++ app/components/Pages/Dashboard/Partials/ListingViewer.js
@@ -144,7 +144,7 @@ export default class ListingViewer extends Component {
       )
       let number_days_indicator
       if (listing.list_date) {
-        const days_on_market = listing_util.getDOM(listing.list_date)
+        const days_on_market = listing_util.getDOM(listing.dom)
         number_days_indicator = (
           <div className="pull-left" style={ S('bg-ebeef1 relative t-7 br-100 pt-11 h-35 pl-15 pr-15 mr-15') }>
             <span style={ S('font-14 relative t-3n') }>
