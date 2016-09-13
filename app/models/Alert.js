0d8403e38bf37a2696ad9ae2464e466af1082d87
OneDev0411 <cryptoapple1109@gmail.com>
2016-09-13 10:38:52 -0500

    remove old endpoints

--- app/models/Alert.js
+++ app/models/Alert.js
@@ -5,39 +5,6 @@ import 'isomorphic-fetch'
 import config from '../../config/public'
 
 export default {
-  create: (params, callback) => {
-    let api_host = params.api_host
-    if (!api_host) api_host = config.app.url
-    const endpoint = api_host + '/api/alerts/create-alert'
-    const request_object = {
-      access_token: params.access_token,
-      alert: params.alert,
-      emails: params.emails,
-      users: params.users,
-      phone_numbers: params.phone_numbers,
-      message: params.message
-    }
-    fetch(endpoint, {
-      method: 'post',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(request_object)
-    })
-    .then(response => {
-      if (response.status >= 400) {
-        const error = {
-          status: 'error',
-          response
-        }
-        return callback(error, false)
-      }
-      return response.json()
-    })
-    .then(response => {
-      return callback(false, response)
-    })
-  },
   get: (params, callback) => {
     let api_host = params.api_host
     if (!api_host) api_host = config.app.url
