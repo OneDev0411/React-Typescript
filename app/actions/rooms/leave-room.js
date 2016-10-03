657858a6bf2e6fc67079b4ac4560226f058508f9
OneDev0411 <cryptoapple1109@gmail.com>
2016-10-03 14:04:57 -0500

    limit add members to group room only

--- app/actions/rooms/leave-room.js
+++ app/actions/rooms/leave-room.js
@@ -10,7 +10,7 @@ export default (user, room) => {
     access_token: user.access_token
   }
   console.log(params)
-  Room.removeUser(params, (err, res) => {
+  Room.removeUser(params, () => {
     delete AppStore.data.deleting_room
     delete AppStore.data.show_delete_room_modal
     delete AppStore.data.show_settings_modal
