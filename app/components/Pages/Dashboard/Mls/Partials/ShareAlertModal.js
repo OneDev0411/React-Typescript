228a094bece294e59861a2088beaad8ce8ff8923
OneDev0411 <cryptoapple1109@gmail.com>
2016-10-04 15:47:36 -0500

    fix missing rooms in share

--- app/components/Pages/Dashboard/Mls/Partials/ShareAlertModal.js
+++ app/components/Pages/Dashboard/Mls/Partials/ShareAlertModal.js
@@ -84,9 +84,16 @@ export default class ShareAlertModal extends Component {
       )
     } else {
       // Room
-      profile_image = (
-        <ProfileImageMultiple users={ item.value.users }/>
-      )
+      if (item.value.users.length > 2) {
+        profile_image = (
+          <ProfileImageMultiple users={ item.value.users }/>
+        )
+      } else {
+        const other_user = _.filter(item.value.users, user => user.id !== data.user.id)[0]
+        profile_image = (
+          <ProfileImage data={ data } user={ other_user }/>
+        )
+      }
     }
     return (
       <div style={ S('relative ' + (item.index < 1 ? 'h-74' : 'h-54')) } className={ item.index < 1 ? 'other-users--first' : '' } >
@@ -122,7 +129,7 @@ export default class ShareAlertModal extends Component {
     // Rooms available
     if (data.rooms) {
       data.rooms.forEach(room => {
-        if (room.users.length > 2) {
+        if (room.users.length > 1) {
           // Test if user in available rooms
           if (users_selected_ids && users_selected_ids.length) {
             const room_user_ids = _.map(room.users, 'id')
