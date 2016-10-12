686da887b7f903d5aaccc30ad72da23c98e081df
OneDev0411 <cryptoapple1109@gmail.com>
2016-10-12 15:55:42 -0500

    room list style

--- app/components/Pages/Dashboard/Recents/Partials/MainContent.js
+++ app/components/Pages/Dashboard/Recents/Partials/MainContent.js
@@ -212,10 +212,10 @@ export default class MainContent extends Component {
         >
           <div style={ main_style }>
             <div className="dashboard__chat-rooms pull-left" style={ rooms_column_style }>
-              <div style={ S('p-10 pt-12 h-60 relative border-bottom-1-solid-eeeff3') }>
+              <div style={ S('p-10 pt-12 h-60 relative bg-303e4d') }>
                 <img style={ S('w-12 h-12 absolute l-20 t-23') } src="/images/dashboard/chats/search.svg" />
                 { clear_search_btn }
-                <input ref="search_rooms_input" onChange={ this.handleSearchRoomChange.bind(this) } style={ S('w-170 br-5 bg-f8fafb pl-30 h-34') } type="text" placeholder="Search" className="form-control pull-left" value={ data.search_rooms_input } />
+                <input ref="search_rooms_input" onChange={ this.handleSearchRoomChange.bind(this) } style={ S('w-170 br-5 bg-4e5c6c color-8696a4 border-none pl-30 h-34') } type="text" placeholder="Search" className="form-control pull-left" value={ data.search_rooms_input } />
                 <button onClick={ this.props.showNewMessageView.bind(this) } type="button" className="btn btn-primary" style={ S('pointer absolute t-12 r-10 br-5 pt-7 h-34') }>
                   <img src="/images/dashboard/chats/pencil.svg" style={ S('mr-10') }/>New Message
                 </button>
