23adf07ca0fb98ecf5ffe3f5dc74ed48bd7e8d02
OneDev0411 <cryptoapple1109@gmail.com>
2016-11-09 15:33:27 -0600

    component edits

---
+++ websites/src/components/Partials/EditBar/Steps/QuickHighlights.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import S from 'shorti'
+import { Button } from 'react-bootstrap'
+class Social extends Component {
+  render() {
+    const data = this.props.data
+    return (
+      <div>
+        <div style={ S('w-100p h-4 bg-34475e mb-10 mt-20n br-3') }>
+          <div style={ S('w-60p bg-6488b5 h-4 br-3') }></div>
+        </div>
+        <div style={ S('font-32 mb-20') }>Quick Highlights</div>
+        <div>
+          <div style={ S('mb-20') }>Facebook</div>
+          <div style={ S('mb-20') }>Twitter</div>
+          <div style={ S('mb-20') }>Instagram</div>
+          <div style={ S('mb-20') }>Blog</div>
+        </div>
+        <div style={ S('absolute b-20 w-340') }>
+          <Button style={ S('bg-f67608 border-none color-fff w-45p h-40') } onClick={ this.props.goToStep.bind(this, data.step - 1) }>
+            Back
+          </Button>
+          <Button style={ S('bg-f67608 border-none color-fff w-45p h-40 pull-right') } onClick={ this.props.goToStep.bind(this, data.step + 1) }>
+            Next <i className="fa fa-chevron-right"></i>
+          </Button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Social
