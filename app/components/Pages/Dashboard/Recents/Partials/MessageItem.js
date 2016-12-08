4c17bf69ec1b03d9664e8adac57c1b8e0490bced
OneDev0411 <cryptoapple1109@gmail.com>
2016-12-08 15:12:12 -0600

    prevent user_info null issue on ack

--- app/components/Pages/Dashboard/Recents/Partials/MessageItem.js
+++ app/components/Pages/Dashboard/Recents/Partials/MessageItem.js
@@ -333,6 +333,8 @@ export default class MessageItem extends Component {
                   _.uniq(message.acked_by)
                   .map(id => {
                     const user_info = _.find(current_room.users, { id })
+                    if (!user_info)
+                      return
                     return (
                       <div className="item">
                         <ProfileImage size={30} font={12} data={ data } user={ user_info } show_online_indicator={ false } />
