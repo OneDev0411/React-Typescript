07d60968eb2fa8e5e0668d43717265ab7ea3508f
OneDev0411 <cryptoapple1109@gmail.com>
2016-12-22 09:36:42 -0600

    rm website link if user doesn't have brand

--- app/components/Pages/Dashboard/Partials/SideBar.js
+++ app/components/Pages/Dashboard/Partials/SideBar.js
@@ -551,6 +551,9 @@ export default class SideBar extends Component {
         </div>
       )
     }
+    let website_link
+    if (user.brand)
+      website_link = <li><Link to="/dashboard/website"><i className="fa fa-globe" style={ S('mr-15') }></i>Website</Link></li>
     return (
       <aside style={ sidebar_style } className="sidebar__nav-list pull-left">
         <Nav bsStyle="pills" stacked style={ S('mt-10') }>
@@ -616,7 +619,7 @@ export default class SideBar extends Component {
             <NavDropdown style={ S('z-1000') } title={ title_area } dropup id="account-dropdown" className="account-dropdown" eventKey={3} noCaret>
               { upgrade_account_button }
               <li><a href="#" style={ S('pointer') } onClick={ this.showSettingsModal }><i className="fa fa-cog" style={ S('mr-15') }></i>Settings</a></li>
-              <li><Link to="/dashboard/website"><i className="fa fa-globe" style={ S('mr-15') }></i>Website</Link></li>
+              { website_link }
               <li role="separator" className="divider"></li>
               <li><a href="/signout"><i className="fa fa-power-off" style={ S('mr-15') }></i>Sign out</a></li>
             </NavDropdown>
