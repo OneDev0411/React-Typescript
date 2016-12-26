ff2fa925666760510d3eb39bde7875f20b9364c8
OneDev0411 <cryptoapple1109@gmail.com>
2016-12-26 16:45:44 -0600

    payments page

--- app/components/Pages/Dashboard/Cards/index.js
+++ app/components/Pages/Dashboard/Cards/index.js
@@ -15,7 +15,7 @@ export default class Cards extends Component {
             data={ data }
           />
           <div style={ main_style }>
-            <iframe style={ S('w-100p h-100p absolute') } src={ `https://rechat.site/cards?access_token=${user.access_token}}`} frameBorder="0"></iframe>
+            <iframe style={ S('w-100p h-100p absolute') } src={ `https://rechat.site/cards?access_token=${user.access_token}`} frameBorder="0"></iframe>
           </div>
         </main>
       </div>
