46c13df19b12c339d593a3fb83b8b26eeac716ee
OneDev0411 <cryptoapple1109@gmail.com>
2017-02-09 21:31:06 +0330

    add app entry point

---
+++ app/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from './app'
+
+const rootEl = document.getElementById('app')
+
+ReactDOM.render(
+  <AppContainer>
+    <App />
+  </AppContainer>,
+  rootEl
+)
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./app', () => {
+    ReactDOM.render(
+      <AppContainer>
+        <App />
+      </AppContainer>,
+      rootEl,
+    )
+    console.clear()
+  })
+}
