3dc8d97608a4627b4c5e28af3d1428ba87acc694
OneDev0411 <cryptoapple1109@gmail.com>
2017-02-09 21:33:43 +0330

    fix for es6 export

--- app/utils/user.js
+++ app/utils/user.js
@@ -1,10 +1,15 @@
 import config from '../../config/public'
-export default {
-  getResizeAvatarUrl(full_size_url) {
-    if (!full_size_url)
-      return ''
-    let image_replace = full_size_url.split('/').slice(-1)[0]
-    const imgix_url = config.images.avatars.imgix_url + '/' + image_replace
-    return imgix_url
+
+export function getResizeAvatarUrl(full_size_url) {
+  if (!full_size_url) {
+    return ''
   }
+
+  let image_replace = full_size_url.split('/').slice(-1)[0]
+  const imgix_url = config.images.avatars.imgix_url + '/' + image_replace
+  return imgix_url
+}
+
+export default {
+  getResizeAvatarUrl
 }
