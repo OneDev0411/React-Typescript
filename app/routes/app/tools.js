429aeec944cdc2e8dcc7673f5346ab7cd0cb631a
OneDev0411 <cryptoapple1109@gmail.com>
2017-02-09 21:27:07 +0330

    change some path structures

--- app/routes/app/tools.js
+++
@@ -1,9 +0,0 @@
-// tools.js
-const request = require('request')
-module.exports = (app, config) => {
-  app.get('/delete-phone',(req, res) => {
-    res.locals.api_host_local = config.api_host_local
-    res.locals.title = 'Delete Phone'
-    return res.render('templates/tool.html')
-  })
-}
