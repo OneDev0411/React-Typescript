7d1ad53ddfedd1504fc1369463bc43ee24fdf92e
OneDev0411 <cryptoapple1109@gmail.com>
2017-02-09 21:34:16 +0330

    make history compatible with react-router v3

--- app/utils/history.js
+++ app/utils/history.js
@@ -1,2 +1,2 @@
-import createBrowserHistory from 'history/lib/createBrowserHistory'
-export default createBrowserHistory()
+import { browserHistory } from 'react-router'
+export default browserHistory
