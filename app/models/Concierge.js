24ae45db31b962fa4e8e3f792a54b25e9a08b092
OneDev0411 <cryptoapple1109@gmail.com>
2017-02-21 01:01:02 +0330

    implement Concierge model

--- app/models/Concierge.js
+++ app/models/Concierge.js
@@ -1,4 +1,3 @@
-// models/Notification.js
 import es6Promise from 'es6-promise'
 es6Promise.polyfill()
 import 'isomorphic-fetch'
@@ -13,6 +12,54 @@ export default {
 
     const endpoint = api_host + '/api/brands/deals?access_token=' +
       params.user + '&brand_id=' + params.brand_id
+
+    fetch(endpoint)
+    .then(response => {
+      if (response.status >= 400) {
+        const error = {
+          status: 'error',
+          response
+        }
+        return callback(error, false)
+      }
+      return response.json()
+    })
+    .then(response => {
+      return callback(false, response)
+    })
+  },
+  getEnvelopes: (params, callback) => {
+    let api_host = params.api_host
+
+    if (!api_host)
+      api_host = config.app.url
+
+    const endpoint = api_host + '/api/deals/envelopes?access_token=' +
+      params.user + '&deal_id=' + params.deal_id
+
+    fetch(endpoint)
+    .then(response => {
+      if (response.status >= 400) {
+        const error = {
+          status: 'error',
+          response
+        }
+        return callback(error, false)
+      }
+      return response.json()
+    })
+    .then(response => {
+      return callback(false, response)
+    })
+  },
+  getSubmissions: (params, callback) => {
+    let api_host = params.api_host
+
+    if (!api_host)
+      api_host = config.app.url
+
+    const endpoint = api_host + '/api/deals/submissions?access_token=' +
+      params.user + '&deal_id=' + params.deal_id
 
     fetch(endpoint)
     .then(response => {
