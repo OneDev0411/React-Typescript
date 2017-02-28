6c352d6af502768134d848c0b722247c8961546e
OneDev0411 <cryptoapple1109@gmail.com>
2017-02-28 17:15:12 -0600

    check if attributes available

--- app/components/Pages/Dashboard/Recents/Partials/NewMessageViewer.js
+++ app/components/Pages/Dashboard/Recents/Partials/NewMessageViewer.js
@@ -117,29 +117,32 @@ export default class NewMessageViewer extends Component {
         })
       } else {
         const sub_contact = contact.sub_contacts[0]
-        contact.email = sub_contact.attributes.emails[0].email
-        // Loop through emails
-        sub_contact.attributes.emails.forEach(email_obj => {
-          const deep_search = `${contact.display_name} ${email_obj.email}`
-          contacts_found.push({
-            value: email_obj.email,
-            label: `${contact.display_name} ${email_obj.email}`,
-            deep_search,
-            type: 'email',
-            index: i
+        if (sub_contact.attributes.emails) {
+          // Loop through emails
+          sub_contact.attributes.emails.forEach(email_obj => {
+            const deep_search = `${contact.display_name} ${email_obj.email}`
+            contacts_found.push({
+              value: email_obj.email,
+              label: `${contact.display_name} ${email_obj.email}`,
+              deep_search,
+              type: 'email',
+              index: i
+            })
           })
-        })
+        }
         // Loop through phone numbers
-        sub_contact.attributes.phone_numbers.forEach(phone_number_obj => {
-          const deep_search = `${contact.display_name} ${phone_number_obj.phone_number}`
-          contacts_found.push({
-            value: phone_number_obj.phone_number,
-            label: `${contact.display_name} ${phone_number_obj.phone_number}`,
-            deep_search,
-            type: 'phone_number',
-            index: i
+        if (sub_contact.attributes.phone_numbers) {
+          sub_contact.attributes.phone_numbers.forEach(phone_number_obj => {
+            const deep_search = `${contact.display_name} ${phone_number_obj.phone_number}`
+            contacts_found.push({
+              value: phone_number_obj.phone_number,
+              label: `${contact.display_name} ${phone_number_obj.phone_number}`,
+              deep_search,
+              type: 'phone_number',
+              index: i
+            })
           })
-        })
+        }
       }
     })
     return contacts_found
