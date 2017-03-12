f2a5535dde9cc5dac4e83f4c6e24dca8fb76f5a6
OneDev0411 <cryptoapple1109@gmail.com>
2017-03-13 01:27:02 +0330

    deals upload file

---
+++ app/actions/deals/upload-file.js
@@ -0,0 +1,27 @@
+// actions/deals/upload-file.js
+import _ from 'underscore'
+import Deals from '../../models/Deal'
+import AppStore from '../../stores/AppStore'
+
+export default (id, user, file) => {
+  const params = { id, user, file }
+
+  // find deal index
+  const index = _.findIndex(AppStore.data.deals, deal => deal.id === id)
+
+  Deals.uploadFile(params, (err, response) => {
+
+    let files = AppStore.data.deals[index].files
+
+    if (!files)
+      files = []
+
+    // Success
+    if (response.status === 200) {
+      files.push(response.body.data)
+      AppStore.data.deals[index].files = files
+    }
+
+    AppStore.emitChange()
+  })
+}
---
+++ app/components/Pages/Dashboard/Deals/Uploads/index.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import { Grid, Container, Row, Col, Tabs, Tab, Button } from 'react-bootstrap'
+import S from 'shorti'
+import _ from 'underscore'
+import Dropzone from 'react-dropzone'
+import Avatar from 'react-avatar'
+import AppDispatcher from '../../../../../dispatcher/AppDispatcher'
+import { getTimeAgo } from '../../../../../utils/helpers'
+
+export default class DealForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      file: null
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  onDrop(acceptedFiles, rejectedFiles){
+
+    if (!acceptedFiles || acceptedFiles.length < 1)
+      return false
+
+    console.log('>1')
+    AppDispatcher.dispatch({
+      action: 'upload-file',
+      user: this.props.user,
+      id: this.props.deal_id,
+      file: acceptedFiles[0]
+    })
+    console.log('>2')
+  }
+
+  display(file) {
+    this.setState({
+      file
+    })
+  }
+
+  render() {
+    const { files } = this.props
+    const { file } = this.state
+
+    return (
+      <div>
+        <Row>
+          <Col xs={6} className="list">
+
+            <div className="dropzone">
+              <Dropzone
+                onDrop={this.onDrop.bind(this)}
+                multiple={false}
+                accept="application/pdf,image/*"
+                style={{ border: 'none' }}
+              >
+                <img src="/static/images/deals/upload.png" />
+                <div className="title">DRAG & DROP</div>
+                <div>your files to upload, or <span>Browse</span></div>
+              </Dropzone>
+            </div>
+
+            {
+              files &&
+              _.chain(files)
+              .sortBy(file => file.created_at * -1)
+              .map(file => {
+                return (
+                  <div
+                    className="item"
+                    key={`file_${file.id}`}
+                    onClick={ this.display.bind(this, file) }
+                  >
+                    <Row>
+                      <Col xs={2}>
+                        <Avatar
+                          round={true}
+                          src={file.preview_url}
+                          size={40}
+                        />
+                      </Col>
+
+                      <Col xs={10}>
+                        <div><b>{ file.name }</b></div>
+                        <div>Uploaded { getTimeAgo(file.created_at) } ago</div>
+                      </Col>
+                    </Row>
+                  </div>
+                )
+              })
+              .value()
+            }
+          </Col>
+
+          <Col xs={6}>
+            {
+              file &&
+              <div className="display">
+                <code>I have problem with setting Amazon AWS cookies for private file, so Emil should look and fix that</code>
+                <img src={file.url} />
+              </div>
+            }
+          </Col>
+        </Row>
+      </div>
+    )
+  }
+}
