ae2bd2e4a1f7274d737196de0361fadc36236ddc
OneDev0411 <cryptoapple1109@gmail.com>
2017-03-14 13:06:46 +0330

    implement signout route

---
+++ server/util/pages/app/signout/index.js
@@ -0,0 +1,13 @@
+import Koa from 'koa'
+const router = require('koa-router')()
+const app = new Koa()
+
+router.get('/signout', async (ctx, next) => {
+
+  ctx.session.destroy()
+
+  const redirect_to = ctx.request.query.redirect_to || '/'
+  ctx.redirect(redirect_to)
+})
+
+module.exports = app.use(router.routes())
