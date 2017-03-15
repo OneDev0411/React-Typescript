ecbce9134ecd602e06b8c46158e7ab684f1fcd38
OneDev0411 <cryptoapple1109@gmail.com>
2017-03-15 14:07:19 +0330

    merge signup routes

--- server/util/pages/app/signup/index.js
+++ server/util/pages/app/signup/index.js
@@ -4,6 +4,13 @@ const router = require('koa-router')()
 const app = new Koa()
 
 router.get('/signup', async (ctx, next) => {
+  const { token } = ctx.request.query
+
+  if (token) {
+    const decoded_token = decodeURIComponent(token)
+    const encoded_token = encodeURIComponent(decoded_token)
+    return ctx.redirect('/activate?token=' + encoded_token)
+  }
 
   if (ctx.session.user) {
     return ctx.redirect('/dashboard/mls')
