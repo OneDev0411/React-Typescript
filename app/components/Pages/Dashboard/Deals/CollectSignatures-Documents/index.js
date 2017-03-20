8652e40725870605901f10c4cc7845fcce9f9916
OneDev0411 <cryptoapple1109@gmail.com>
2017-03-21 02:41:25 +0330

    change select documents file name

--- app/components/Pages/Dashboard/Deals/CollectSignatures-Documents/index.js
+++ app/components/Pages/Dashboard/Deals/CollectSignatures-Documents/index.js
@@ -3,6 +3,7 @@ import { Grid, Container, Row, Col, Tabs, Tab, Button } from 'react-bootstrap'
 import { Link } from 'react-router'
 import S from 'shorti'
 import _ from 'underscore'
+import AppStore from '../../../../../stores/AppStore'
 import AppDispatcher from '../../../../../dispatcher/AppDispatcher'
 
 export default class CollectSignatures extends React.Component {
@@ -10,7 +11,8 @@ export default class CollectSignatures extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      submissions: null
+      submissions: null,
+      selectedDocuments: {}
     }
   }
 
@@ -23,10 +25,19 @@ export default class CollectSignatures extends React.Component {
       return this.getSubmissions()
     }
 
-    console.log(deal.submissions)
     this.setState({
       submissions: deal.submissions
     })
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { submissions } = this.state
+    const { deals } = nextProps
+    const deal_id = this.props.params.id
+    const deal = _.find(deals, d => d.id === deal_id)
+
+    if (!submissions && deals.submissions)
+      this.setState({ submissions: deal.submissions })
   }
 
   getSubmissions() {
@@ -35,6 +46,31 @@ export default class CollectSignatures extends React.Component {
       user: this.props.user,
       id: this.props.params.id
     })
+  }
+
+  onDocumentChange(submission, e) {
+    const { selectedDocuments } = this.state
+    selectedDocuments[submission.id] = e.target.checked
+
+    const docs = []
+    _.each(selectedDocuments, (checked, id) => {
+      if (!checked) return
+      docs.push(id)
+    })
+
+    this.setState({
+      selectedDocuments: docs
+    })
+
+  }
+
+  onSubmit() {
+    const { selectedDocuments } = this.state
+    AppStore.data.deals_signatures = {
+      documents: selectedDocuments
+    }
+
+    AppStore.emitChange()
   }
 
   render() {
@@ -63,7 +99,10 @@ export default class CollectSignatures extends React.Component {
                   className="doc-detail"
                 >
                   <div className="control">
-                    <input type="checkbox" />
+                    <input
+                      type="checkbox"
+                      onChange={this.onDocumentChange.bind(this, subm)}
+                    />
                     <img src="/static/images/deals/file.png" />
                   </div>
 
@@ -74,6 +113,15 @@ export default class CollectSignatures extends React.Component {
             })
             .value()
           }
+        </div>
+
+        <div className="right">
+          <Button
+            bsStyle="primary"
+            onClick={ this.onSubmit.bind(this) }
+          >
+            Next
+          </Button>
         </div>
       </div>
     )
