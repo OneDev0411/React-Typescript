341b6623fffc74d82ae68dbd19eb855815ba5959
OneDev0411 <cryptoapple1109@gmail.com>
2017-03-24 16:05:42 -0500

    added intercom name and email

--- app/components/App.js
+++ app/components/App.js
@@ -82,7 +82,7 @@ export default class App extends Component {
       AppStore.data.brand_merged = true
       AppStore.emitChange()
     }
-    setIntercom()
+    this.setIntercom()
   }
   setIntercom() {
     const data = AppStore.data
