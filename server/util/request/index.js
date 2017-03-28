aaf4b5be82d45da9ef33a331ada347e1eeab3310
OneDev0411 <cryptoapple1109@gmail.com>
2017-03-28 17:13:15 -0500

    logs

--- server/util/request/index.js
+++ server/util/request/index.js
@@ -46,8 +46,8 @@ const requestMiddleware = async function (ctx, next) {
           responseText = JSON.parse(responseText)
         }
         catch(e) {}
-        console.log(ctx)
-        console.log(err)
+        console.log('ctx', ctx)
+        console.log('err', err)
         ctx.status = err.response ? err.response.status : 500
         ctx.body = {
           status: 'error',
