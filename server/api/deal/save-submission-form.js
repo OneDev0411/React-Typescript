799cc3d53dffc81dc511aaf9217909d3e504427c
OneDev0411 <cryptoapple1109@gmail.com>
2017-04-01 18:08:53 +0430

    implement save submission form proxy api

---
+++ server/api/deal/save-submission-form.js
@@ -0,0 +1,38 @@
+import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
+const router = require('koa-router')()
+const app = new Koa()
+
+router.post('/deals/submission/form', bodyParser(), async (ctx, next) => {
+
+  const { state, values, form, deal, type, last_revision } = ctx.request.body
+
+  // setup endpoint according to update or create
+  let endpoint = ''
+
+  // define request method
+  const method = (type === 'update') ? 'PUT' : 'POST'
+
+  // initialize data to send
+  const data = { state, values }
+
+  if (type === 'create') {
+    data.form = form
+    endpoint = `/deals/${deal}/submissions`
+  } else {
+    endpoint = `/forms/submissions/${last_revision}`
+  }
+
+  try {
+    const response = await ctx
+      .fetch(endpoint, method)
+      .send(data)
+
+    ctx.body = response.body
+  }
+  catch(e) {
+    console.log(e)
+  }
+})
+
+module.exports = app.use(router.routes())
