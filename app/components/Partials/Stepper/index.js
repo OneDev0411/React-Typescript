741a34c4af71e7affcf6ded476a7e7e6b05c08b1
OneDev0411 <cryptoapple1109@gmail.com>
2017-04-10 22:34:04 +0430

    implement stepper :ok_hand:

---
+++ app/components/Partials/Stepper/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import './styles.scss'
+
+export default class Stepper extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: this.props.active
+    }
+  }
+
+  gotoStep(step, key) {
+    this.setState({
+      active: step
+    })
+
+    this.props.onChange(step, key)
+  }
+
+  render() {
+    const { steps } = this.props
+    const { active } = this.state
+
+    return (
+      <div className="stepper">
+        <ul className="nav nav-tabs" role="tablist">
+          {
+            steps.map((step, key) => (
+              <li
+                key={`STEP_${step}`}
+                role="presentation"
+                className={ active === step ? 'active' : '' }
+                style={{ width: (100 / steps.length) + '%' }}
+                onClick={() => this.gotoStep(step, key)}
+              >
+                <a
+                  className="persistant-disabled"
+                  data-toggle="tab"
+                  aria-controls={`stepper-step-${step}`}
+                  role="tab"
+                  title={ step }
+                >
+                  <span className="round-tab"></span>
+                </a>
+                <span className="name">{ step }</span>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    )
+  }
+}
