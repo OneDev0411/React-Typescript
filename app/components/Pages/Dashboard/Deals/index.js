e0ad2795f15fd000d94a855e1cfd3fee3868167d
OneDev0411 <cryptoapple1109@gmail.com>
2017-04-10 12:02:16 +0430

    move Deals sidebar to Dashboard layout

--- app/components/Pages/Dashboard/Deals/index.js
+++ app/components/Pages/Dashboard/Deals/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router'
 import S from 'shorti'
 import AppDispatcher from '../../../../dispatcher/AppDispatcher'
-import AppStore from '../../../../stores/AppStore'
-import SideBar from '../Partials/SideBar'
-import MobileNav from '../Partials/MobileNav'
 
 export default class Deals extends React.Component {
 
@@ -12,26 +8,11 @@ export default class Deals extends React.Component {
     const { data } = this.props
     const { user } = data
 
-    if (!user)
-      return
-
-    AppStore.data.user = user
-    AppStore.emitChange()
-
     // get deals
     this.getDeals(user)
 
     // get forms
     this.getForms(user)
-
-    // check for mobile
-    this.checkForMobile()
-  }
-
-  checkForMobile() {
-    AppDispatcher.dispatch({
-      action: 'check-for-mobile'
-    })
   }
 
   getForms(user) {
@@ -54,16 +35,6 @@ export default class Deals extends React.Component {
     const { data } = this.props
     const user = data.user
 
-    let main_style = S('ml-5p h-100p')
-    let nav_area = <SideBar data={data} />
-
-    if (data.is_mobile) {
-      main_style = { ...main_style, ...S('') }
-
-      if (user)
-        nav_area = <MobileNav data={data} />
-    }
-
     const children = React.Children.map(this.props.children, child =>
       React.cloneElement(child, {
         user,
@@ -74,8 +45,7 @@ export default class Deals extends React.Component {
 
     return (
       <div>
-        { nav_area }
-        <div className="deals" style={main_style}>
+        <div className="deals">
           {
             !data.deals &&
             <div className="loading-deals">
