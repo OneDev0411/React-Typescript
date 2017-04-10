5d04b42a162db52621dfbdfa368f954a8cd58757
OneDev0411 <cryptoapple1109@gmail.com>
2017-04-10 15:10:21 +0430

    fix component's name

--- app/components/Pages/Dashboard/Contacts/Profile/index.js
+++ app/components/Pages/Dashboard/Contacts/Profile/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default class Contacts extends React.Component {
+export default class ContactProfile extends React.Component {
   constructor(props) {
     super(props)
   }
