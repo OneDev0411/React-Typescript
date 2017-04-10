05d89215b6c4620c00e85c46d4de7bef43fda307
OneDev0411 <cryptoapple1109@gmail.com>
2017-04-10 15:11:43 +0430

    implement Contacts dispatcher

---
+++ app/dispatcher/ContactDispatcher.js
@@ -0,0 +1,23 @@
+import { Dispatcher } from './flux'
+
+import getContacts from '../actions/contacts/get-contacts'
+
+const ContactDispatcher = new Dispatcher()
+
+// Register callback with ContactDispatcher
+ContactDispatcher.register(async function (payload) {
+  const action = payload.action
+
+  switch (action) {
+
+    case 'get-contacts':
+      getContacts(payload.user)
+      break
+
+    default:
+      return true
+  }
+  return true
+})
+
+export default ContactDispatcher
