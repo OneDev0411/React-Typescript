20b1d2664164e352cb62b52132c36617d40fd1ab
OneDev0411 <cryptoapple1109@gmail.com>
2017-04-13 16:32:38 -0500

    set notif limit

--- server/api/notification/all.js
+++ server/api/notification/all.js
@@ -7,7 +7,7 @@ router.get('/get-notifications', async (ctx, next) => {
   const { id } = ctx.request.query
 
   try {
-    const response = await ctx.fetch('/notifications')
+    const response = await ctx.fetch('/notifications?limit=1000')
     ctx.body = response.body
   }
   catch(e) {}
