80fe466655ce2606021b32c13aafb565b2a48989
OneDev0411 <cryptoapple1109@gmail.com>
2017-04-25 14:12:41 +0430

    implement add contact action

---
+++ app/actions/contacts/add-contact.js
@@ -0,0 +1,42 @@
+import Contact from '../../models/Contact'
+import AppStore from '../../stores/AppStore'
+export default async function (user, emails, phone_numbers, first_name, last_name, stage) {
+
+  const contact = {
+    type: 'contact',
+    emails: emails.map(email => {
+      return { type: 'email', email}
+    }),
+    phone_numbers: phone_numbers.map(phone => {
+      return { type: 'phone_number', phone}
+    }),
+    attributes: {
+      names: [{
+        type: 'name',
+        first_name: first_name,
+        last_name: last_name
+      }],
+      stages: [{
+        type: 'stage',
+        stage
+      }]
+    }
+  }
+
+  const params = {
+    contacts: [contact],
+    access_token: user.access_token
+  }
+
+  try {
+    const response = await Contact.add(params)
+    const newContact = response.body.data[0]
+    if (response.status === 200) {
+      AppStore.data.contacts[newContact.id] = newContact
+      AppStore.emitChange()
+    }
+  }
+  catch(e) {
+    console.log(e)
+  }
+}
