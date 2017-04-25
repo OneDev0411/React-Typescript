36639ac7d716c7fdf8d4655a34416419b92675eb
OneDev0411 <cryptoapple1109@gmail.com>
2017-04-25 14:17:23 +0430

    make MessageModal stateless

--- app/components/Partials/MessageModal.js
+++ app/components/Partials/MessageModal.js
@@ -2,28 +2,18 @@ import React from 'react'
 import { Modal } from 'react-bootstrap'
 import S from 'shorti'
 
-export default class extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-    }
-  }
-
-  render() {
-    return (
-      <Modal
-        dialogClassName="modal-alert-saved"
-        show={this.props.show}
-      >
-        <div className="din" style={S('text-center font-60 color-fff')}>
-          <div style={S('bg-2196f3 w-165 h-165 br-100 center-block pt-35')}>
-            <i className="fa fa-check" style={S('h-70 mt-20')} />
-          </div>
-          <span style={{ textShadow: '0 2px 6px rgba(0, 0, 0, 0.2)' }}>
-            { this.props.text }
-          </span>
-        </div>
-      </Modal>
-    )
-  }
-}
+export default ({ show, text }) => (
+  <Modal
+    dialogClassName="modal-alert-saved"
+    show={show}
+  >
+    <div className="din" style={S('text-center font-60 color-fff')}>
+      <div style={S('bg-2196f3 w-165 h-165 br-100 center-block pt-35')}>
+        <i className="fa fa-check" style={S('h-70 mt-20')} />
+      </div>
+      <span style={{ textShadow: '0 2px 6px rgba(0, 0, 0, 0.2)' }}>
+        { text }
+      </span>
+    </div>
+  </Modal>
+)
