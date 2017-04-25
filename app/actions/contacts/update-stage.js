8bc308989fa6cf819b2d9e33f49ac8ae60e7adef
OneDev0411 <cryptoapple1109@gmail.com>
2017-04-25 17:56:51 +0430

    fix update stage

--- app/actions/contacts/update-stage.js
+++ app/actions/contacts/update-stage.js
@@ -13,7 +13,10 @@ export default async function (user, id, stage_id, stage) {
   try {
     const response = await Contact.updateStage(params)
     if (response.status === 200) {
-      AppStore.data.contacts[id] = response.body.data
+      AppStore.data.contacts[id] = {
+        ...response.body.data,
+        ...{ timeline: AppStore.data.contacts[id].timeline }
+      }
     }
   }
   catch(e) {
