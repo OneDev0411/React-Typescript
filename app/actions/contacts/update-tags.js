54152a09b4b0a1849b51429270b9613444550561
OneDev0411 <cryptoapple1109@gmail.com>
2017-04-25 14:12:28 +0430

    implement update-tags action

---
+++ app/actions/contacts/update-tags.js
@@ -0,0 +1,23 @@
+import _ from 'underscore'
+import Contact from '../../models/Contact'
+import AppStore from '../../stores/AppStore'
+
+export default async function (user, id, newTags, removeTags) {
+  let response
+
+  try {
+    let add_params = {
+      id,
+      access_token: user.access_token,
+      tags: newTags
+    }
+    const response = await Contact.addTags(add_params)
+
+    if (response.status === 200) {
+      AppStore.data.contacts[id] = response.body.data
+    }
+
+  } catch(e) { console.log(e)/* nothing */ }
+
+  AppStore.emitChange()
+}
