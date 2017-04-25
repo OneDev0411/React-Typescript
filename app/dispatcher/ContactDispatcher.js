634b9230288e8285ff780a1a4a0f22a1f11c8e29
OneDev0411 <cryptoapple1109@gmail.com>
2017-04-25 14:15:35 +0430

    update Contact Dispatcher

--- app/dispatcher/ContactDispatcher.js
+++ app/dispatcher/ContactDispatcher.js
@@ -1,5 +1,8 @@
 import { Dispatcher } from './flux'
 
+import addContact from '../actions/contacts/add-contact'
+import updateStage from '../actions/contacts/update-stage'
+import updateTags from '../actions/contacts/update-tags'
 import getContacts from '../actions/contacts/get-contacts'
 import getTimeline from '../actions/contacts/get-timeline'
 import addNote from '../actions/contacts/add-note'
@@ -13,26 +16,42 @@ ContactDispatcher.register(async (payload) => {
 
   switch (action) {
 
+    case 'add-contact':
+      return await addContact(
+        payload.user,
+        payload.emails,
+        payload.phone_numbers,
+        payload.first_name,
+        payload.last_name,
+        payload.stage
+      )
+
+    case 'update-tags':
+      updateTags(payload.user, payload.id, payload.new_tags, payload.remove_tags)
+      break
+
+    case 'update-stage':
+      updateStage(payload.user, payload.id, payload.stage_id, payload.stage)
+      break
+
     case 'get-contacts':
       getContacts(payload.user)
       break
 
     case 'get-timeline':
       return await getTimeline(payload.id, payload.user)
-      break
 
     case 'add-note':
       return await addNote(payload.id, payload.user, payload.note)
-      break
 
     case 'update-user-timeline':
       updateUserTimeline(payload.user, payload.user_action, payload.object_class, payload.object)
       break
 
     default:
-      return true
+      return false
   }
-  return true
+  return false
 })
 
 export default ContactDispatcher
