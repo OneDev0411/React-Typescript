6f4f2aea2db8bf94dbac7c21811864fc7c5b552f
OneDev0411 <cryptoapple1109@gmail.com>
2017-04-27 03:32:24 +0430

    reimplement get-tags action

--- app/actions/contacts/get-tags.js
+++ app/actions/contacts/get-tags.js
@@ -10,8 +10,10 @@ export default async function (user) {
   try {
     const response = await Contact.getTags(params)
 
-    if (response.status === 200)
-      AppStore.data.contacts_tags = response.body.data
+    if (response.status === 200) {
+      AppStore.data.contacts_tags = _.indexBy(response.body.data, 'tag') //index by name
+      AppStore.emitChange()
+    }
   } catch(e) {
     console.log(e)
   }
