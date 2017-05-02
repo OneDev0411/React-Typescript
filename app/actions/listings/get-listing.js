fb764fccd3ff1c98ff35a8840eea7d1014046c5e
OneDev0411 <cryptoapple1109@gmail.com>
2017-05-02 21:08:14 +0430

    fix update-listing bug

--- app/actions/listings/get-listing.js
+++ app/actions/listings/get-listing.js
@@ -3,17 +3,17 @@ import Listing from '../../models/Listing'
 import AppStore from '../../stores/AppStore'
 
 export default (user, id) => {
-  const params = {
-    id
-  }
+  const params = { id }
+
   if (user)
     params.user = user.access_token
+
   Listing.get(params, (err, response) => {
     // Success
     if (response.status === 'success') {
       AppStore.data.current_listing = response.data
       AppStore.data.show_listing_viewer = true
+      AppStore.emitChange()
     }
-    AppStore.emitChange()
   })
 }
