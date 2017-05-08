b091f5e313b2c27f3d1e4eb63d6d560094e5027f
OneDev0411 <cryptoapple1109@gmail.com>
2017-05-08 21:03:52 +0430

    implement editing note

--- app/components/Pages/Dashboard/Contacts/Profile/Notes/index.js
+++ app/components/Pages/Dashboard/Contacts/Profile/Notes/index.js
@@ -1,33 +1,51 @@
 import React from 'react'
 import _ from 'underscore'
 import moment from 'moment'
+import Editable from '../Editable'
 
-export default ({
-  notes
-}) => (
-  <div>
-    {
-      _.size(notes) === 0 &&
-      <div className="no-note">
-        <img src="/static/images/contacts/notepad-edit-231.svg" />
-        <p>There are no notes yet</p>
+export default class extends React.Component {
+
+  nl2br(item) {
+    return item.note.split('\n').map((text, key) => (
+      <div key={`item_${item.id}_line_${key}`}>
+        { text }
       </div>
-    }
-    {
-      notes.map(item => (
-        <div key={`note_${item.id}`} className="item">
-          {
-            item.note.split('\n').map((text, key) => (
-              <div key={`item_${item.id}_line_${key}`}>
-                {text}
+    ))
+  }
+
+  render() {
+    const { notes } = this.props
+
+    return (
+      <div>
+        {
+          _.size(notes) === 0 &&
+          <div className="no-note">
+            <img src="/static/images/contacts/notepad-edit-231.svg" />
+            <p>There are no notes yet</p>
+          </div>
+        }
+        {
+          notes.map(item => {
+            // const text = this.nl2br(item)
+
+            return (
+              <div key={`note_${item.id}`} className="item">
+                <Editable
+                  type="note"
+                  id={item.id}
+                  showEdit={true}
+                  showAdd={false}
+                  text={item.note}
+                />
+                <span className="time">
+                  { moment.unix(item.created_at ).format('MMMM DD, YYYY')}
+                </span>
               </div>
-            ))
-          }
-          <span className="time">
-            { moment.unix(item.created_at ).format('MMMM DD, YYYY')}
-          </span>
-        </div>
-      ))
-    }
-  </div>
-)
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
