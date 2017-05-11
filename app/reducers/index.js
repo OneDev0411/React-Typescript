25b71d97e5ba08494261eacc2966aa70662348e4
OneDev0411 <cryptoapple1109@gmail.com>
2017-05-11 20:35:39 +0430

    add contacts to reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,14 +1,14 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 import data from './data'
+import contact from './contact'
 
 const appReducer = combineReducers({
   data,
+  contact,
   routing: routerReducer
 })
 
-const rootReducer = (state, action) => {
+export default(state, action) => {
   return appReducer(state, action)
 }
-
-export default rootReducer
