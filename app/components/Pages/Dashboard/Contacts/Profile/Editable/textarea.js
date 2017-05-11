5bb5c43a217864c0f2626249649f9daa00fa8df1
OneDev0411 <cryptoapple1109@gmail.com>
2017-05-11 18:54:12 +0430

    fix issue 136

--- app/components/Pages/Dashboard/Contacts/Profile/Editable/textarea.js
+++ app/components/Pages/Dashboard/Contacts/Profile/Editable/textarea.js
@@ -3,15 +3,19 @@ import React from 'react'
 export default ({
   value,
   placeholder,
+  lines,
   inputRef,
   onChange,
   onBlur
 }) => (
-  <textarea
-    value={value}
-    placeholder={placeholder}
-    ref={inputRef}
-    onChange={onChange}
-    onBlur={onBlur}
-  />
+  <div>
+    <textarea
+      value={value}
+      placeholder={placeholder}
+      style={{ height: (lines * 23) + 'px' }}
+      ref={inputRef}
+      onChange={onChange}
+      onBlur={onBlur}
+    />
+  </div>
 )
