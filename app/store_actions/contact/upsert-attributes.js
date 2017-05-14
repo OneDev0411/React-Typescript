b1bf48a5d31c867c9a688be2010fd9579d9f59eb
OneDev0411 <cryptoapple1109@gmail.com>
2017-05-14 14:41:41 +0430

    implement upsert-attributes action

---
+++ app/store_actions/contact/upsert-attributes.js
@@ -0,0 +1,61 @@
+import _ from 'underscore'
+import Contact from '../../models/Contact'
+import types from '../../constants/contact'
+
+function upsertAttributes (id, contact) {
+  return {
+    type: types.UPSERT_ATTRIBUTES,
+    id,
+    contact
+  }
+}
+
+export default function (user, id, type, attributes) {
+  const defaultParams = {
+    id,
+    type,
+    access_token: user.access_token
+  }
+
+  const updates = _.filter(attributes, attr => attr.id)
+  const inserts = _.filter(attributes, attr => !attr.id)
+
+  let response
+  let params
+
+  return async (dispatch) => {
+
+    // insert attributes
+    if (inserts.length > 0) {
+      params = {
+        ...defaultParams,
+        ...{attributes: inserts}
+      }
+
+      try {
+        // send save request
+        response = await Contact.createAttributes(params)
+
+        // dispatch
+        dispatch(upsertAttributes(id, response.body.data))
+      } catch(e) { /* nothing */}
+    }
+
+    // update attributes
+    if (updates.length > 0) {
+      params = {
+        ...defaultParams,
+        ...{attributes: updates}
+      }
+
+      try {
+        // send save request
+        response = await Contact.updateAttributes(params)
+
+        // dispatch
+        dispatch(upsertAttributes(id, response.body.data))
+
+      } catch(e) { /* nothing */}
+    }
+  }
+}
