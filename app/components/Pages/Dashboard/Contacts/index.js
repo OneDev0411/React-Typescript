ccc885a6180f445dc1b39a6fa17c4fd89f27c7a5
OneDev0411 <cryptoapple1109@gmail.com>
2017-05-14 14:40:13 +0430

    implement Contact layout

--- app/components/Pages/Dashboard/Contacts/index.js
+++ app/components/Pages/Dashboard/Contacts/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { getContacts } from '../../../../store_actions/contact'
-import Dispatcher from '../../../../dispatcher/ContactDispatcher'
+import { getContacts, getTags } from '../../../../store_actions/contact'
 
 class Contacts extends React.Component {
   constructor(props) {
@@ -9,57 +8,41 @@ class Contacts extends React.Component {
   }
 
   componentDidMount() {
-    const { data } = this.props
-    const { user } = data
-
-    // get deals
-    this.loadContacts(user)
-
-    // get shared tags
-    this.getTags(user)
+    this.init()
   }
 
-  loadContacts(user) {
-    const { dispatch } = this.props
+  async init() {
+    const { dispatch, user } = this.props
+
+    // get contacts
     dispatch(getContacts(user))
 
-    Dispatcher.dispatch({
-      action: 'get-contacts',
-      user
-    })
-  }
-
-  getTags(user) {
-    Dispatcher.dispatch({
-      action: 'get-tags',
-      user
-    })
+    // get tags
+    dispatch(getTags(user))
   }
 
   render() {
-    const { data } = this.props
-    const user = data.user
+    const { user, contacts, dispatch } = this.props
 
-    const children = React.Children.map(this.props.children, child =>
-      React.cloneElement(child, {
-        user,
-        contacts: data.contacts,
-        tags: data.contacts_tags
-      })
-    )
+    const children = React.cloneElement(this.props.children, {
+      dispatch,
+      user,
+      contacts: contacts.list,
+      tags: contacts.tags
+    })
 
     return (
       <div className="crm">
         <div className="contacts">
           {
-            !data.contacts &&
+            !contacts.list &&
             <div className="loading-list">
               <div><i className="fa fa-spinner fa-spin fa-2x fa-fw" /></div>
               <b>Loading contacts ...</b>
             </div>
           }
 
-          { data.contacts && children }
+          { contacts.list && children }
         </div>
       </div>
     )
@@ -67,5 +50,5 @@ class Contacts extends React.Component {
 }
 
 export default connect(state => ({
-  contacts: state.contacts
+  contacts: state.contact
 }))(Contacts)
