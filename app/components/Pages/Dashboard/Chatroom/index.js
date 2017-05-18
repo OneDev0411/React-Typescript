9075115dcf1ddd0038d4f90acf6e5bc9b8bc84b9
OneDev0411 <cryptoapple1109@gmail.com>
2017-05-18 23:18:56 +0430

    add basic component

---
+++ app/components/Pages/Dashboard/Chatroom/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class Recents extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>--------------------</div>
+    )
+  }
+}
