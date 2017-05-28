8fef3e81bf01307c0afe2a352c636869b36524ba
OneDev0411 <cryptoapple1109@gmail.com>
2017-05-28 22:34:54 +0430

    implement get-messages action

--- app/store_actions/chatroom/get-messages.js
+++ app/store_actions/chatroom/get-messages.js
@@ -2,18 +2,20 @@ import _ from 'underscore'
 import Chatroom from '../../models/Chatroom'
 import types from '../../constants/chatroom'
 
-function getMessages (id, messages) {
+function getMessages (id, messages, { total }) {
   return {
     type: types.GET_MESSAGES,
     id,
-    messages
+    messages,
+    info: { total }
   }
 }
 
 export default function (id, limit, max_value) {
   return async (dispatch) => {
     const response = await Chatroom.getMessages(id, limit, max_value)
-    const messages = _.indexBy(response.body.data.reverse(), 'id')
-    dispatch(getMessages(id, messages))
+    const { info, data } = response.body
+    const messages = _.indexBy(data.reverse(), 'id')
+    dispatch(getMessages(id, messages, info))
   }
 }
