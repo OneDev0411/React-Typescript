7a4bd844b4c2f115d7a1764f14eba1c068b99375
OneDev0411 <cryptoapple1109@gmail.com>
2017-05-28 14:11:12 +0430

    implement create message cmpn

---
+++ app/components/Pages/Dashboard/Chatroom/Messages/create-message.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Rx from 'rxjs/Rx'
+
+export default class CreateMessage extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    const { Observable } = Rx
+
+    // create handler for text keypress
+    const handler = Observable
+      .fromEvent(this.text_message, 'keypress')
+
+    handler
+    .filter(e => e.key !== 'Enter')
+    .throttleTime(1500)
+    .subscribe(() => this.onUserTyping())
+
+    handler
+    .filter(e => e.key === 'Enter')
+    .subscribe(() => this.sendMessage())
+  }
+
+  onUserTyping() {
+    console.log('>> TYPING ...', new Date())
+  }
+
+  sendMessage() {
+    console.log(this.text_message.value)
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          placeholder="Write a message ..."
+          ref={ref => this.text_message = ref}
+        />
+      </div>
+    )
+  }
+}
