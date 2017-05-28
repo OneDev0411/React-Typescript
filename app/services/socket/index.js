0ae444b096757c9e1f90775f9a912e4d3023cff8
OneDev0411 <cryptoapple1109@gmail.com>
2017-05-28 22:35:03 +0430

    implement socket

--- app/services/socket/index.js
+++ app/services/socket/index.js
@@ -1,10 +1,12 @@
 import io from 'socket.io-client'
+import moment from 'moment'
+import store from '../../stores'
+import { createMessage } from '../../store_actions/chatroom'
 import config from '../../../config/public'
 
 export default class Socket {
   constructor(user) {
     this.user = user
-
     const socket = io(config.socket.server, {
       reconnection: true,
       reconnectionDelay: 1000,
@@ -12,10 +14,110 @@ export default class Socket {
       reconnectionAttempts: 99999
     })
 
-    // create authentication
-    if (user)
-      socket.emit('Authenticate', user.access_token)
-
+    // bind socket to window
     window.socket = socket
+
+    // create authentication
+    if (!user)
+      return false
+
+    // authenticate user
+    Socket.authenicate(user.access_token)
+
+    // bind User.Typing
+    socket.on('User.Typing', this.onUserTyping)
+
+    // bind User.TypingEnded
+    socket.on('User.TypingEnded', this.onUserTypingEnded)
+
+    // bind Message.Sent
+    socket.on('Message.Sent', this.onNewMessage)
+  }
+
+  /**
+   * authenticate user
+   */
+  static authenicate(access_token) {
+    socket.emit('Authenticate', access_token)
+  }
+
+  /**
+   * emit User.Typing to server
+   */
+  static userIsTyping(roomId) {
+    window.socket.emit('User.Typing', roomId)
+  }
+
+  /**
+   * emit User.TypingEnded to server
+   */
+  static userTypingEnded(roomId) {
+    window.socket.emit('User.TypingEnded', roomId)
+  }
+
+  /**
+   * send new message
+   */
+  static sendMessage(roomId, message) {
+    return new Promise((resolve, reject) => {
+      const unixtime = moment().unix()
+      const qid = 'queued_' + unixtime
+
+      const tempMessage = {
+        ...message,
+        ...{
+          id: qid,
+          queued: true,
+          created_at: unixtime,
+          updated_at: unixtime,
+        }
+      }
+
+      // create temporary message
+      Socket.createMessage(roomId, tempMessage)
+
+      // resolve
+      resolve(tempMessage)
+
+      window.socket.emit('Message.Send', roomId, message, (err, message) => {
+        if (err) return reject(err)
+        Socket.createMessage(roomId, message, qid)
+      })
+    })
+  }
+
+  /**
+   * on send / receive new message
+   */
+  onNewMessage(room, message) {
+    const { messages } = store.getState().chatroom
+    const list = messages[room.id].list
+
+    // do not dispatch when message is created
+    if (list && list[message.id])
+      return false
+
+    Socket.createMessage(room.id, message)
+  }
+
+  /**
+   * create new message and store
+   */
+  static createMessage(roomId, message, queueId) {
+    store.dispatch(createMessage(roomId, { [message.id]: message }, queueId))
+  }
+
+  /**
+   * income event when a user started typing
+   */
+  onUserTyping({user_id, room_id}) {
+    // console.log('>>>> IS TYPING', user_id, room_id)
+  }
+
+  /**
+   * income socket event when user typing has been ended
+   */
+  onUserTypingEnded({user_id, room_id}) {
+    // console.log('>>>> TYPING END', user_id, room_id)
   }
 }
