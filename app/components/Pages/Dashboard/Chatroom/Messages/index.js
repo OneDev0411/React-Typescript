b8402940b5c34b3f83cdf176aa8d4e41d0e2b4c2
OneDev0411 <cryptoapple1109@gmail.com>
2017-05-28 22:33:15 +0430

    implement Messages

--- app/components/Pages/Dashboard/Chatroom/Messages/index.js
+++ app/components/Pages/Dashboard/Chatroom/Messages/index.js
@@ -3,18 +3,23 @@ import { connect } from 'react-redux'
 import Rx from 'rxjs/Rx'
 import _ from 'underscore'
 import moment from 'moment'
+import cn from 'classnames'
 import { getMessages } from '../../../../../store_actions/chatroom'
 import Toolbar from './toolbar'
 import Message from './message-types'
 import CreateMessage from './create-message'
 
 class Messages extends React.Component {
+
   componentDidMount() {
     const { roomId, messages } = this.props
 
     // bind chat's scroll event
     this.initializeScroller()
 
+    window.socket.on('Message.Sent', this.onNewMessage.bind(this))
+
+    // initialize chatroom with latest room
     if (roomId && !messages[roomId])
       this.loadMessages(roomId)
   }
@@ -23,18 +28,17 @@ class Messages extends React.Component {
     const { roomId, messages } = nextProps
 
     if (roomId && !messages[roomId])
-      this.loadMessages(roomId)
+      return this.loadMessages(roomId)
   }
 
   initializeScroller() {
     Rx
     .Observable
     .fromEvent(this.messagesList, 'scroll')
-    .debounceTime(100)
-    .scan(top => (window.pageYOffset || this.messagesList.scrollTop) -
-      (this.messagesList.clientTop || 0), 10000)
-    .filter(top => top <= 200)
-    .subscribe(() => this.loadPreviousMessages())
+    .debounceTime(400)
+    .scan(top => this.messagesList.scrollTop - this.messagesList.clientTop, 10000)
+    .filter(top => top < 120)
+    .subscribe((top) => this.loadPreviousMessages(top))
   }
 
   async loadMessages(roomId, limit = 20, max_value = null, scroll_to = null) {
@@ -57,18 +61,48 @@ class Messages extends React.Component {
     const { dispatch, roomId } = this.props
     const messages = this.props.messages[roomId]
 
-    if (!messages)
+    // check whether old messages are loaded or not
+    if (!messages || messages.total <= _.size(messages.list))
       return false
 
-    const max_value = messages[_.keys(messages)[0]].created_at
-    const lastItem = this.messagesList.children[0]
+    // get key id of last chat
+    const key = _.keys(messages.list)[0]
 
-    this.loadMessages(roomId, 20, max_value, lastItem)
+    // get max value of last chat
+    const max_value = messages.list[key].created_at
+
+    // get html element of first child
+    const lastChild = this.messagesList.children[0]
+
+    this.loadMessages(roomId, 20, max_value, lastChild)
+  }
+
+  onNewMessage(room, message) {
+    const { user, roomId } = this.props
+    const el = this.messagesList.children[this.messagesList.children.length - 3]
+    console.log(el, this.elementInViewport(el))
+    // scroll end when receive new message and in visible area
+    if (this.elementInViewport(el) && room.id === roomId)
+      this.scrollEnd()
+  }
+
+  elementInViewport(el) {
+    const top = el.offsetTop
+    const height = el.offsetHeight
+    const container = this.messagesList
+
+    return (
+      top >= this.messagesList.scrollTop &&
+      (top + height) <= (container.scrollTop + container.offsetHeight)
+    )
+  }
+
+  scrollEnd() {
+    this.messagesList.scrollTop = this.messagesList.scrollHeight
   }
 
   render() {
     const { roomId, user } = this.props
-    console.log('[ x ] RENDER message cmp.')
 
     // get messages of current room
     const messages = roomId ? this.props.messages[roomId] : null
@@ -94,12 +128,16 @@ class Messages extends React.Component {
 
           {
             messages &&
-            _.map(messages, msg =>
-              <Message
+            _.map(messages.list, msg =>
+              <div
+                className={cn('message-item', { queued: msg.queued })}
                 key={`MESSAGE_${msg.id}`}
-                user={user}
-                message={msg}
-              />
+              >
+                <Message
+                  user={user}
+                  message={msg}
+                />
+              </div>
             )
           }
         </div>
@@ -107,7 +145,11 @@ class Messages extends React.Component {
         <div
           className="message-create"
         >
-          <CreateMessage />
+          <CreateMessage
+            user={user}
+            roomId={roomId}
+            onCreateNewMessage={() => this.scrollEnd()}
+          />
         </div>
       </div>
     )
