f0ee8bb22f58d44c377ac0eb3fa644e2bc016857
OneDev0411 <cryptoapple1109@gmail.com>
2017-05-28 22:34:43 +0430

    fix actions

--- app/store_actions/chatroom/index.js
+++ app/store_actions/chatroom/index.js
@@ -1,7 +1,9 @@
 import getRooms from './get-rooms'
 import getMessages from './get-messages'
+import createMessage from './create-message'
 
 export {
   getRooms,
-  getMessages
+  getMessages,
+  createMessage
 }
