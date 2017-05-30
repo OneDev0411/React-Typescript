8e8cae0e197b0f254156570cadf42b5957616062
OneDev0411 <cryptoapple1109@gmail.com>
2017-05-30 14:54:40 +0430

    toggle chatbar action

--- app/store_actions/chatroom/index.js
+++ app/store_actions/chatroom/index.js
@@ -1,9 +1,11 @@
 import getRooms from './get-rooms'
 import getMessages from './get-messages'
 import createMessage from './create-message'
+import toggleChatbar from './toggle-chatbar'
 
 export {
   getRooms,
   getMessages,
-  createMessage
+  createMessage,
+  toggleChatbar
 }
---
+++ app/store_actions/chatroom/toggle-chatbar.js
@@ -0,0 +1,7 @@
+import types from '../../constants/chatroom'
+
+export default function () {
+  return {
+    type: types.TOGGLE_CHATBAR
+  }
+}
