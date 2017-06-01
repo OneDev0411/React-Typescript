4b3c2427e44d87f601014789ed270696e21edea6
OneDev0411 <cryptoapple1109@gmail.com>
2017-06-02 01:03:35 +0430

    update actions list

--- app/store_actions/chatroom/index.js
+++ app/store_actions/chatroom/index.js
@@ -2,14 +2,26 @@ import getRooms from './get-rooms'
 import getMessages from './get-messages'
 import createMessage from './create-message'
 import toggleChatbar from './toggle-chatbar'
-import { addChatPopup, removeChatPopup, minimizeChatPopup } from './popups'
+import toggleFullScreen from './toggle-fullscreen'
+import {
+  addChatPopup,
+  closeChatPopup,
+  minimizeChatPopup,
+  maximizeChatPopup,
+  changeActivePopup,
+} from './popups'
 
 export {
   getRooms,
   getMessages,
   createMessage,
   toggleChatbar,
+  toggleFullScreen,
+
+  /* actions for popup */
   addChatPopup,
-  removeChatPopup,
-  minimizeChatPopup
+  closeChatPopup,
+  minimizeChatPopup,
+  maximizeChatPopup,
+  changeActivePopup
 }
