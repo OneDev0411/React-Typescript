df5af64ee2ea8239ea914654ce960154d1786e85
OneDev0411 <cryptoapple1109@gmail.com>
2017-06-02 01:02:08 +0430

    implement popups

--- app/components/Pages/Dashboard/Chatroom/Popups/index.js
+++ app/components/Pages/Dashboard/Chatroom/Popups/index.js
@@ -2,9 +2,46 @@ import React from 'react'
 import { connect } from 'react-redux'
 import _ from 'underscore'
 import PopupWindow from './window'
+import {
+  minimizeChatPopup,
+  closeChatPopup,
+  maximizeChatPopup,
+  changeActivePopup
+} from '../../../../../store_actions/chatroom'
+import store from '../../../../../stores'
+
+/*
+ * minimize a chat popup
+ */
+const minimize = function(roomId) {
+  store.dispatch(minimizeChatPopup(roomId))
+}
+
+
+/*
+ * maximize a chat popup
+ */
+const maximize = function(roomId) {
+  store.dispatch(maximizeChatPopup(roomId))
+}
+/*
+ * remove a chat popup
+ */
+const close = function(roomId) {
+  store.dispatch(closeChatPopup(roomId))
+  changeActive(roomId)
+}
+
+/**
+ * change active popup window
+ */
+const changeActive = function(roomId) {
+  store.dispatch(changeActivePopup(roomId))
+}
 
 const ChatPopups = ({
   user,
+  rooms,
   popups
 }) => {
   if (!popups)
@@ -16,12 +53,17 @@ const ChatPopups = ({
   return (
     <div>
       {
-        _.map(popups, (popup, roomId) => {
+        _.map(popups, (settings, roomId) => {
           return <PopupWindow
             key={`CHAT_POPUP_${roomId}`}
             user={user}
-            roomId={roomId}
+            settings={settings}
+            room={rooms[roomId]}
             number={counter++}
+            onMinimize={roomId => minimize(roomId)}
+            onMaximize={roomId => maximize(roomId)}
+            onClose={roomId => close(roomId)}
+            onChangeActive={roomId => changeActive(roomId)}
           />
         })
       }
