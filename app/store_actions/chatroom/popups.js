1ae7eef2c9a30e140955ab530b0050fb7c2fe8ae
OneDev0411 <cryptoapple1109@gmail.com>
2017-06-02 01:03:27 +0430

    implement popup actions

--- app/store_actions/chatroom/popups.js
+++ app/store_actions/chatroom/popups.js
@@ -7,10 +7,30 @@ export function addChatPopup(roomId) {
   }
 }
 
-export function removeChatPopup() {
-
+export function closeChatPopup(roomId) {
+  return {
+    type: types.REMOVE_POPUP,
+    roomId
+  }
 }
 
-export function minimizeChatPopup() {
+export function minimizeChatPopup(roomId) {
+  return {
+    type: types.MINIMIZE_POPUP,
+    roomId
+  }
+}
 
+export function maximizeChatPopup(roomId) {
+  return {
+    type: types.MAXIMIZE_POPUP,
+    roomId
+  }
+}
+
+export function changeActivePopup(roomId) {
+  return {
+    type: types.CHANGE_ACTIVE_POPUP,
+    roomId
+  }
 }
