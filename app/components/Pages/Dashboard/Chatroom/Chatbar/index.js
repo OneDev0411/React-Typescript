1d98a71a32d8c68aa3422f5690441f377e7fe3de
OneDev0411 <cryptoapple1109@gmail.com>
2017-06-02 00:58:36 +0430

    implement chat sidebar

--- app/components/Pages/Dashboard/Chatroom/Chatbar/index.js
+++ app/components/Pages/Dashboard/Chatroom/Chatbar/index.js
@@ -2,21 +2,21 @@ import React from 'react'
 import { browserHistory } from 'react-router'
 import { connect } from 'react-redux'
 import { slide as Menu } from 'react-burger-menu'
+import Chatroom from '..'
 import Rooms from '../Rooms'
-import { toggleChatbar, addChatPopup } from '../../../../../store_actions/chatroom'
+import {
+  toggleChatbar,
+  addChatPopup
+} from '../../../../../store_actions/chatroom'
 
 const Chatbar = ({
+  user,
+  fullscreen,
   showChatbar,
+  activePopup,
   dispatch,
   location
 }) => {
-
-  const onFullScreen = () => {
-    dispatch(toggleChatbar())
-    const url = '/dashboard/recents'
-    if (location.pathname !== url)
-      browserHistory.push(url)
-  }
 
   const onSelectRoom = (roomId) => {
     dispatch(toggleChatbar())
@@ -24,26 +24,40 @@ const Chatbar = ({
   }
 
   return (
-    <Menu
-      isOpen={showChatbar}
-      customBurgerIcon={false}
-      customCrossIcon={false}
-      onStateChange={({ isOpen }) => {
-        if (showChatbar !== isOpen) {
-          dispatch(toggleChatbar())
-        }
-      }}
-    >
-      <Rooms
-        isSidebar={true}
-        onFullScreen={() => onFullScreen()}
-        onSelectRoom={roomId => onSelectRoom(roomId)}
-      />
-    </Menu>
+    <div>
+      <div
+        className="chatroom-fullscreen"
+        style={{ display: fullscreen ? 'block' : 'none' }}
+      >
+        <Chatroom
+          user={user}
+          activePopup={activePopup}
+        />
+      </div>
+
+      <Menu
+        isOpen={showChatbar}
+        customBurgerIcon={false}
+        customCrossIcon={false}
+        onStateChange={({ isOpen }) => {
+          if (showChatbar !== isOpen) {
+            dispatch(toggleChatbar())
+          }
+        }}
+      >
+        <Rooms
+          isSidebar={true}
+          onFullScreen={() => onFullScreen()}
+          onSelectRoom={roomId => onSelectRoom(roomId)}
+        />
+      </Menu>
+    </div>
   )
 }
 
 export default connect(s => ({
+  activePopup: s.chatroom.activePopup,
+  fullscreen: s.chatroom.fullscreen,
   showChatbar: s.chatroom.showChatbar,
   location: s.data.location
 }))(Chatbar)
