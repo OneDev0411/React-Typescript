57950bb3d8f85b1b982c22af8b352ab9ee5c7855
OneDev0411 <cryptoapple1109@gmail.com>
2017-06-02 01:03:51 +0430

    update constants

--- app/constants/chatroom.js
+++ app/constants/chatroom.js
@@ -5,7 +5,12 @@ new Array(
   'GET_MESSAGES',
   'CREATE_MESSAGE',
   'TOGGLE_CHATBAR',
-  'ADD_POPUP'
+  'TOGGLE_FULLSCREEN',
+  'ADD_POPUP',
+  'MINIMIZE_POPUP',
+  'MAXIMIZE_POPUP',
+  'CHANGE_ACTIVE_POPUP',
+  'REMOVE_POPUP'
 )
 .forEach(action => {
   ActionTypes[action] = `CHATROOM___${action}`
