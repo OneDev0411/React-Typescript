bd605248fd9058224e54b2c6325c0fad4f716fd9
OneDev0411 <cryptoapple1109@gmail.com>
2017-06-03 01:55:35 +0430

    reimplement Chatbar

--- app/components/Pages/Dashboard/Chatroom/Chatbar/index.js
+++ app/components/Pages/Dashboard/Chatroom/Chatbar/index.js
@@ -2,62 +2,38 @@ import React from 'react'
 import { browserHistory } from 'react-router'
 import { connect } from 'react-redux'
 import { slide as Menu } from 'react-burger-menu'
-import Chatroom from '..'
 import Rooms from '../Rooms'
-import {
-  toggleChatbar,
-  addChatPopup
-} from '../../../../../store_actions/chatroom'
+import { addChatPopup, toggleChatbar } from '../../../../../store_actions/chatroom'
 
 const Chatbar = ({
-  user,
-  fullscreen,
   showChatbar,
-  activePopup,
-  dispatch,
-  location
+  dispatch
 }) => {
 
   const onSelectRoom = (roomId) => {
-    dispatch(toggleChatbar())
     dispatch(addChatPopup(roomId))
+    dispatch(toggleChatbar())
   }
 
   return (
-    <div>
-      <div
-        className="chatroom-fullscreen"
-        style={{ display: fullscreen ? 'block' : 'none' }}
-      >
-        <Chatroom
-          user={user}
-          activePopup={activePopup}
-        />
-      </div>
 
-      <Menu
-        isOpen={showChatbar}
-        customBurgerIcon={false}
-        customCrossIcon={false}
-        onStateChange={({ isOpen }) => {
-          if (showChatbar !== isOpen) {
-            dispatch(toggleChatbar())
-          }
-        }}
-      >
-        <Rooms
-          isSidebar={true}
-          onFullScreen={() => onFullScreen()}
-          onSelectRoom={roomId => onSelectRoom(roomId)}
-        />
-      </Menu>
-    </div>
+    <Menu
+      isOpen={showChatbar}
+      customBurgerIcon={false}
+      customCrossIcon={false}
+      onStateChange={({ isOpen }) => {
+        if (showChatbar !== isOpen) {
+          dispatch(toggleChatbar())
+        }
+      }}
+    >
+      <Rooms
+        onSelectRoom={roomId => onSelectRoom(roomId)}
+      />
+    </Menu>
   )
 }
 
 export default connect(s => ({
-  activePopup: s.chatroom.activePopup,
-  fullscreen: s.chatroom.fullscreen,
-  showChatbar: s.chatroom.showChatbar,
-  location: s.data.location
+  showChatbar: s.chatroom.showChatbar
 }))(Chatbar)
