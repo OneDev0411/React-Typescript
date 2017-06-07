623f6965a14c5276206fa60a0388c36f668d99a4
OneDev0411 <cryptoapple1109@gmail.com>
2017-06-08 00:43:14 +0430

    enhance Chatroom container

--- app/components/Pages/Dashboard/Chatroom/index.js
+++ app/components/Pages/Dashboard/Chatroom/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Grid, Row, Col } from 'react-bootstrap'
+import { browserHistory } from 'react-router'
+import _ from 'underscore'
 import Rooms from './Rooms'
 import Messages from './Messages'
 import { getRooms, changeActiveRoom } from '../../../../store_actions/chatroom'
+import store from '../../../../stores'
 
 class Chatroom extends React.Component {
   static fetchData(dispatch, params) {
@@ -11,40 +14,45 @@ class Chatroom extends React.Component {
     return dispatch(getRooms(user))
   }
 
+  componentWillMount() {
+    const { activeRoom, params } = this.props
+    const { rooms } = store.getState().chatroom
+
+    if (params && params.roomId)
+      return this.changeRoom(params.roomId)
+
+    if (!activeRoom && _.size(rooms) > 0)
+      return this.changeRoom(rooms[_.keys(rooms)[0]].id)
+  }
+
   shouldComponentUpdate(nextProps, nextState) {
     const { activeRoom } = this.props
     return nextProps.activeRoom !== undefined && activeRoom !== nextProps.activeRoom
   }
 
-  onChangeRoom(id) {
+  changeRoom(id) {
     const { changeActiveRoom, activeRoom, location } = this.props
 
     if (id !== activeRoom)
       changeActiveRoom(id)
 
-    // don't update url hash on fullscreen mode
+    // don't change url on fullscreen mode
     if (this.props.location)
-      this.updateHash(id)
-  }
-
-  updateHash(roomId) {
-    if (history.pushState) {
-      history.pushState(null, null, `#${roomId}`)
-    }
-    else {
-      location.hash = `#${roomId}`
-    }
+      browserHistory.push(`/dashboard/recents/${id}`)
   }
 
   render() {
     const { user, activeRoom } = this.props
+
+    if (!activeRoom)
+      return false
 
     return (
       <Row className="chatroom">
         <Col lg={3} md={3} sm={4} xs={5} className="no-padding">
           <Rooms
             user={user}
-            onSelectRoom={id => this.onChangeRoom(id)}
+            onSelectRoom={id => this.changeRoom(id)}
             activeRoom={activeRoom}
           />
         </Col>
