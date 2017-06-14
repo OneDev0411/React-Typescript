4c5e32e053b0c0648e60f54dc6d26f4780990351
OneDev0411 <cryptoapple1109@gmail.com>
2017-06-14 17:43:48 +0430

    implement SocketStatus component

---
+++ app/components/Pages/Dashboard/Chatroom/SocketStatus/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { connect } from 'react-redux'
+const SocketStatus = ({
+  status
+}) => {
+  if (status === 'connected')
+    return false
+
+  return (
+    <div className="socket-status">
+      { status }
+      <img src="/static/images/loading-states/three-dots.svg" />
+    </div>
+  )
+}
+
+export default connect(({ chatroom }) => ({
+  status: chatroom.status
+}))(SocketStatus)
