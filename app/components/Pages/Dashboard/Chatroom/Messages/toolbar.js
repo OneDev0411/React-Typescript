865e9258613f21ebdcd0bc42df0a5da3f0b5193b
OneDev0411 <cryptoapple1109@gmail.com>
2017-06-15 17:14:10 +0430

    refactor toolbar and add close instance mode ban

--- app/components/Pages/Dashboard/Chatroom/Messages/toolbar.js
+++ app/components/Pages/Dashboard/Chatroom/Messages/toolbar.js
@@ -1,24 +1,38 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Row, Col } from 'react-bootstrap'
+import { toggleInstanceMode } from '../../../../../store_actions/chatroom'
 
 const MessagesToolbar = ({
-  rooms,
-  roomId
-}) => {
-
-  if (!roomId)
-    return false
-
-  // get room info
-  const room = rooms[roomId]
-
-  return (
-    <div className="toolbar">
+  toggleInstanceMode,
+  instanceMode,
+  room
+}) => (
+  <Row className="toolbar">
+    <Col md={11} lg={11} sm={11} xs={11}>
       { room.proposed_title }
-    </div>
-  )
+    </Col>
+
+    <Col md={1} lg={1} sm={1} xs={1}>
+      {
+        instanceMode &&
+        <i
+          className="close-instance fa fa-window-close"
+          onClick={() => toggleInstanceMode()}
+        />
+      }
+    </Col>
+  </Row>
+)
+
+function mapStateToProps({ chatroom }, ownProps) {
+  const { roomId } = ownProps
+  const { rooms } = chatroom
+
+  return {
+    instanceMode: chatroom.instanceMode,
+    room: rooms && roomId ? rooms[roomId] : {}
+  }
 }
 
-export default connect(({ chatroom }) => ({
-  rooms: chatroom.rooms
-}))(MessagesToolbar)
+export default connect(mapStateToProps, { toggleInstanceMode })(MessagesToolbar)
