50395b93f87523f0c0d1253d0a1e7b3cedda666c
OneDev0411 <cryptoapple1109@gmail.com>
2017-06-16 14:28:25 +0430

    reimplement a basic auto size input from scratch

---
+++ app/components/Partials/AutoSizeInput/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default class AutoSizeInput extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    this.calculateWidth()
+  }
+
+  inputRef(el) {
+    this.input = el
+  }
+
+  getInput() {
+    return this.input
+  }
+
+  calculateWidth() {
+    const { placeholder } = this.props
+    const input = this.getInput()
+    const value = input.value
+
+    // get input string. if value is blank use placeholder instead
+    const width = (value.length === 0 && placeholder) ?
+      placeholder.length * 10 :
+      input.scrollWidth + 2
+
+    input.style.width = `${width}px`
+  }
+
+  render() {
+    return (
+      <input
+        {...this.props}
+        type="text"
+        ref={el => this.inputRef(el)}
+        onChange={() => this.calculateWidth()}
+      />
+    )
+  }
+}
