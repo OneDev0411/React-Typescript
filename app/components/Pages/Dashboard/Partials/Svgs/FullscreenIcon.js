3639aaa58950e0ace8be35332943304e7a49a730
OneDev0411 <cryptoapple1109@gmail.com>
2017-06-20 19:11:29 +0430

    add new SVG icon components

---
+++ app/components/Pages/Dashboard/Partials/Svgs/FullscreenIcon.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default ({
+  fill = "#B2B2B2"
+}) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="12" height="11" viewBox="0 0 12 11">
+    <path fill={fill} fillRule="evenodd" d="M7 9H3.414L10 3.414V7h2V0H5v2h3.586L2 7.586V4H0v7h7z"/>
+  </svg>
+
+)
---
+++ app/components/Pages/Dashboard/Partials/Svgs/MembersIcon.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default ({
+  fill = "#B2B2B2",
+  width = 16,
+  height = 16
+}) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 16 16">
+    <g fill={fill} fillRule="evenodd">
+      <path d="M8 8c2.206 0 4-1.794 4-4s-1.794-4-4-4-4 1.794-4 4 1.794 4 4 4M8 9c-4.71 0-8 2.467-8 6v1h16v-1c0-3.533-3.29-6-8-6"/>
+    </g>
+  </svg>
+)
