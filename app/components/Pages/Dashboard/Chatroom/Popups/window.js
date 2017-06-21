ef543288f79ec46ccce44079c6696eff7ac82f19
OneDev0411 <cryptoapple1109@gmail.com>
2017-06-21 16:09:41 +0430

    move popup toolbar into separate file

--- app/components/Pages/Dashboard/Chatroom/Popups/window.js
+++ app/components/Pages/Dashboard/Chatroom/Popups/window.js
@@ -1,19 +1,7 @@
 import React from 'react'
 import cn from 'classnames'
 import Messages from '../Messages'
-import Members from '../Rooms/members'
-import RoomSettings from '../Rooms/settings'
-import CloseIcon from '../../Partials/Svgs/CloseIcon'
-import FullscreenIcon from '../../Partials/Svgs/FullscreenIcon'
-import MinimizeIcon from '../../Partials/Svgs/MinimizeIcon'
-
-const getTitle = function(title) {
-  const len = 14
-  if (title.length < len)
-    return title
-  else
-    return title.substr(0, len) + '...'
-}
+import Toolbar from './toolbar'
 
 export default ({
   room,
@@ -50,46 +38,13 @@ export default ({
         left: `${left}px`
       }}
     >
-      <div className={cn('bar', { isActive })}>
-        <span
-          className="room-title"
-          onClick={() => onMinimize(room.id)}
-        >
-          { getTitle(room.proposed_title) }
-        </span>
-
-        <div className="icons">
-          <Members
-            room={room}
-            iconSize={14}
-          />
-
-          <RoomSettings
-            room={room}
-            iconSize={14}
-          />
-
-          <span
-            className="icon minimize"
-            onClick={() => onMinimize(room.id)}>
-            <MinimizeIcon />
-          </span>
-
-          <span
-            className="icon maximize"
-            onClick={() => onMaximize(room.id)}
-          >
-            <FullscreenIcon />
-          </span>
-
-          <span
-            className="icon times"
-            onClick={() => onClose(room.id)}
-          >
-            <CloseIcon />
-          </span>
-        </div>
-      </div>
+      <Toolbar
+        room={room}
+        isActive={isActive}
+        onMinimize={onMinimize}
+        onMaximize={onMaximize}
+        onClose={onClose}
+      />
 
       <Messages
         user={user}
