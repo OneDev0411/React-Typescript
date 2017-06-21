aa571134935592b16a7534788d7ed6e417b4bf2f
OneDev0411 <cryptoapple1109@gmail.com>
2017-06-21 16:10:28 +0430

    polish styles and fix minimize svg position

--- app/components/Pages/Dashboard/Partials/Svgs/MinimizeIcon.js
+++ app/components/Pages/Dashboard/Partials/Svgs/MinimizeIcon.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default ({
   fill = "#BFBFC0"
 }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" viewBox="0 0 10 2">
+  <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 2">
     <path fill={fill} fillRule="evenodd" d="M0 0h10v2H0z"/>
   </svg>
 )
