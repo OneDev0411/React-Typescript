4853c4f9c74e88aab23eaeb4ba693fd9f395c1f2
OneDev0411 <cryptoapple1109@gmail.com>
2017-06-22 00:26:27 +0430

    reimplement members based on compose wrapper

--- app/components/Pages/Dashboard/Chatroom/Rooms/members.js
+++ app/components/Pages/Dashboard/Chatroom/Rooms/members.js
@@ -1,115 +1,81 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Row, Col, Modal, Button } from 'react-bootstrap'
-import { compose,  withState, lifecycle, pure } from 'recompose'
 import moment from 'moment'
-import Compose from '../../../../Partials/Compose'
-import MembersIcon from '../../Partials/Svgs/MembersIcon'
+import { Row, Col } from 'react-bootstrap'
+import Compose from '../Shared/compose-wrapper'
 import UserAvatar from '../../../../Partials/UserAvatar'
+import MembersIcon from '../../Partials/Svgs/MembersIcon'
 import { addMembers } from '../../../../../store_actions/chatroom'
 
-const enhance = compose(
-  pure,
-  withState('showComposeModal', 'onChangeComposeModal', false),
-  withState('recipients', 'onChangeRecipients', {}),
-  connect(null, { addMembers })
-)
+function getLastSeen(user) {
+  const { last_seen_at } = user
 
-function hasRecipients(recipients) {
-  const recps = []
-    .concat(recipients.users, recipients.emails, recipients.phone_numbers)
-    .filter(recp => recp !== undefined)
+  if (!last_seen_at)
+    return 'Offline'
 
-  return recps.length > 0
+  return moment.unix(last_seen_at).fromNow()
 }
 
 const ManageMembers = ({
-  room,
-  showComposeModal,
-  recipients,
-  onChangeComposeModal,
-  onChangeRecipients,
   addMembers,
-  iconSize = 16
+  iconSize = 16,
+  room
 }) => {
 
-  function getLastSeen(user) {
-    const { last_seen_at } = user
+  const Button = ({
+    clickHandler
+  }) => (
+    <span
+      className="icon members"
+      onClick={() => clickHandler()}
+    >
+      <MembersIcon width={iconSize} height={iconSize} />
 
-    if (!last_seen_at)
-      return 'Offline'
-
-    return moment.unix(last_seen_at).fromNow()
-  }
-
-  return (
-    <span>
-      <span
-        className="icon members"
-        onClick={() => onChangeComposeModal(!showComposeModal)}
-      >
-        <MembersIcon width={iconSize} height={iconSize} />
-
-        <span className="bdg">
-          { room.users.length }
-        </span>
+      <span className="bdg">
+        { room.users.length }
       </span>
-
-      <Modal
-        show={showComposeModal}
-        dialogClassName="chatroom-members"
-        onHide={() => onChangeComposeModal(false)}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Members</Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>
-          <Compose
-            dropDownBox
-            onHide={() => onChangeComposeModal(false)}
-            onChangeRecipients={recipients => onChangeRecipients(recipients)}
-          />
-
-          <div className="members-list">
-            {
-              room.users.map(user =>
-                <Row
-                  key={`MEMBER_${user.id}`}
-                  className="item"
-                >
-                  <Col sm={1} md={1} className="vcenter" style={{ padding: 0 }}>
-                    <UserAvatar
-                      userId={user.id}
-                      name={user.display_name}
-                      image={user.profile_image_url}
-                      size={30}
-                    />
-                  </Col>
-                  <Col sm={8} md={8} className="vcenter" style={{ padding: 0 }}>
-                    <div className="title">{ user.display_name }</div>
-                    <div className="status">{ getLastSeen(user) }</div>
-                  </Col>
-                </Row>
-              )
-            }
-          </div>
-        </Modal.Body>
-
-        <Modal.Footer>
-          <Button
-            bsStyle="primary"
-            disabled={!hasRecipients(recipients)}
-            onClick={() => {
-              addMembers(room.id, recipients)
-              onChangeComposeModal(false)
-            }}
-          >
-            Add Members
-          </Button>
-        </Modal.Footer>
-      </Modal>
     </span>
   )
+
+  const RoomMembers = () => (
+    <div className="chatroom-members">
+      <div className="members-list">
+        {
+          room.users.map(user =>
+            <Row
+              key={`MEMBER_${user.id}`}
+              className="item"
+            >
+              <Col sm={1} md={1} className="vcenter" style={{ padding: 0 }}>
+                <UserAvatar
+                  userId={user.id}
+                  name={user.display_name}
+                  image={user.profile_image_url}
+                  size={30}
+                />
+              </Col>
+              <Col sm={8} md={8} className="vcenter" style={{ padding: 0 }}>
+                <div className="title">{ user.display_name }</div>
+                <div className="status">{ getLastSeen(user) }</div>
+              </Col>
+            </Row>
+          )
+        }
+      </div>
+    </div>
+  )
+
+  return (
+    <Compose
+      TriggerButton={Button}
+      InitialState={RoomMembers}
+      dropDownBox={true}
+      inline={true}
+      title="Members"
+      buttonTitle="Add Members"
+      onButtonClick={async recipients => await addMembers(room.id, recipients)}
+    />
+  )
 }
-export default enhance(ManageMembers)
+
+export default connect(null, { addMembers })(ManageMembers)
