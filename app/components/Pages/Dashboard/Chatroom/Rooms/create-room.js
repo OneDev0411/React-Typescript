0c756bff29269f534cdee028dfeaa971db0ac287
OneDev0411 <cryptoapple1109@gmail.com>
2017-06-21 18:32:00 +0430

    reimplement create-room base on ComposeWrapper

--- app/components/Pages/Dashboard/Chatroom/Rooms/create-room.js
+++ app/components/Pages/Dashboard/Chatroom/Rooms/create-room.js
@@ -1,71 +1,25 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Modal, Button } from 'react-bootstrap'
-import { compose,  withState, lifecycle, pure } from 'recompose'
-import Compose from '../../../../Partials/Compose'
+import Compose from '../Shared/compose-wrapper'
 import { createRoom } from '../../../../../store_actions/chatroom'
 
-const enhance = compose(
-  pure,
-  withState('showComposeModal', 'onChangeComposeModal', false),
-  withState('recipients', 'onChangeRecipients', {}),
-  connect(null, { createRoom })
-)
-
-function hasRecipients(recipients) {
-  const recps = []
-    .concat(recipients.users, recipients.emails, recipients.phone_numbers)
-    .filter(recp => recp !== undefined)
-
-  return recps.length > 0
-}
-
-const AddMember = ({
-  showComposeModal,
-  recipients,
-  onChangeComposeModal,
-  onChangeRecipients,
-  createRoom
+const Button = ({
+  clickHandler
 }) => (
-  <div>
-    <div
-      className="new-room"
-      onClick={() => onChangeComposeModal(!showComposeModal)}
-    >
-      <i className="fa fa-edit"></i> New Message
-    </div>
-
-    <Modal
-      show={showComposeModal}
-      dialogClassName="chatroom-add-member"
-      onHide={() => onChangeComposeModal(false)}
-    >
-      <Modal.Header closeButton>
-        <Modal.Title>New Message</Modal.Title>
-      </Modal.Header>
-
-      <Modal.Body>
-        <Compose
-          onHide={() => onChangeComposeModal(false)}
-          onChangeRecipients={recipients => onChangeRecipients(recipients)}
-        />
-      </Modal.Body>
-
-      <Modal.Footer>
-        <Button
-          bsStyle="primary"
-          disabled={!hasRecipients(recipients)}
-          onClick={async () => {
-            await createRoom(recipients)
-            onChangeComposeModal(false)
-          }}
-        >
-          Create
-        </Button>
-      </Modal.Footer>
-
-    </Modal>
+  <div className="new-room" onClick={() => clickHandler()}>
+    <i className="fa fa-edit"></i> New Message
   </div>
 )
 
-export default enhance(AddMember)
+const CreateRoom = ({
+  createRoom
+}) => (
+  <Compose
+    TriggerButton={Button}
+    title="New Message"
+    buttonTitle="Create"
+    onButtonClick={async recipients => await createRoom(recipients)}
+  />
+)
+
+export default connect(null, { createRoom })(CreateRoom)
