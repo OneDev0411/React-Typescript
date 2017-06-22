3371c05e2fbfa0f936362a91042378c8c8166703
OneDev0411 <cryptoapple1109@gmail.com>
2017-06-22 04:55:26 +0430

    fixed [brands]: comment emil changes in get-branding action

--- app/actions/branding/get-branding.js
+++ app/actions/branding/get-branding.js
@@ -2,14 +2,19 @@
 import Brand from '../../models/Brand'
 import AppStore from '../../stores/AppStore'
 import ListingDispatcher from '../../dispatcher/ListingDispatcher'
-import config from '../../../config/public'
-import URL from 'url'
+// import config from '../../../config/public'
+// import URL from 'url'
 
-const app_hostname = URL.parse(config.app.url).hostname
+// const app_hostname = URL.parse(config.app.url).hostname
 
-export default (hostname) => {
-  if (app_hostname === hostname)
+export default hostname => {
+  // if (app_hostname === hostname)
+  //   return
+  if (hostname === 'rechat.com' || hostname === 'localhost') {
+    AppStore.data.brand_queried = true
+    AppStore.emitChange()
     return
+  }
 
   const params = {
     hostname,
