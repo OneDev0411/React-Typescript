d358015092db66bda6fe462979d7881065fa6342
OneDev0411 <cryptoapple1109@gmail.com>
2017-06-25 18:46:35 +0430

    implement date splitter

---
+++ app/components/Pages/Dashboard/Chatroom/Messages/components/date-splitter.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import moment from 'moment'
+import messageUtil from '../../Util/message'
+
+export default ({
+  previousMessage,
+  message
+}) => {
+
+  const messageDate = messageUtil.getYMD(message)
+  const previousMessageDate = messageUtil.getYMD(message)
+
+  // dont show date-splitter when loading previous messages or when
+  // messages are in the same day
+  if (previousMessage === null || previousMessageDate === messageDate)
+    return false
+
+  return (
+    <div className="date-splitter">
+      <span>
+        { messageUtil.getDate(message, 'dddd, MMMM YYYY')}
+      </span>
+    </div>
+  )
+}
