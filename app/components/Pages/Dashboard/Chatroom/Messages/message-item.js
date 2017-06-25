8038aa784dc5af87713274943835f62f25835393
OneDev0411 <cryptoapple1109@gmail.com>
2017-06-25 18:49:11 +0430

    refactor message item

---
+++ app/components/Pages/Dashboard/Chatroom/Messages/message-item.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import messageUtil from '../Util/message'
+import LeadMessage from './message-kinds/lead'
+import SubMessage from './message-kinds/sub'
+
+export default ({
+  user,
+  message,
+  previousMessage
+}) => {
+
+  // get message author
+  const author = messageUtil.getAuthor(message)
+
+  // check message is alert
+  const isAlert = messageUtil.isAlert(alert)
+
+  /*
+   * check message is a lead message or not
+   * lead message has avatar and date
+   * sub message comes without avatar (grouping concept)
+   */
+  const isLeadMessage = previousMessage === null ||
+    message.recommendation ||
+    isAlert ||
+    messageUtil.getAuthor(previousMessage).id !== author.id ||
+    messageUtil.getYMD(previousMessage) !== messageUtil.getYMD(message)
+
+  const props = { user, author, message, previousMessage }
+  return isLeadMessage ? <LeadMessage {...props} /> : <SubMessage {...props} />
+}
