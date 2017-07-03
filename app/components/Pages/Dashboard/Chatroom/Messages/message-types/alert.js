f2a5f11e92ade1f706bb193d17fee4fd97b8e546
OneDev0411 <cryptoapple1109@gmail.com>
2017-07-03 13:15:22 +0430

    add getFeed to alert model

--- app/components/Pages/Dashboard/Chatroom/Messages/message-types/alert.js
+++ app/components/Pages/Dashboard/Chatroom/Messages/message-types/alert.js
@@ -1,18 +1,76 @@
 import React from 'react'
-import moment from 'moment'
+import AlertModel from '../../../../../../models/Alert'
+import AlertViewerModal from '../../../Mls/Partials/AlertViewerModal'
 
-export default ({
-  alert
-}) => {
-  return (
-    <div className="alert">
-      <strong style={{ color: '#9b9a9b' }}>
-        Shared a saved search:
-      </strong>
+export default class Alert extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: false,
+      feed: null
+    }
+  }
 
-      <div>
-        <img src="/static/images/dashboard/mls/map-tile.jpg" />
+  async loadAlert(alert) {
+    if (this.state.loading)
+      return false
+
+    let feed = null
+    const { room: roomId, id: alertId } = alert
+
+    // set loading
+    this.setState({ loading: true })
+
+    try {
+      feed = await AlertModel.getFeed(alertId, roomId)
+    } catch(e) { /* nothing */ }
+
+    this.setState({
+      feed,
+      loading: false
+    })
+  }
+
+  render() {
+    const { alert } = this.props
+    const { feed, loading } = this.state
+
+    console.log(feed)
+    return (
+      <div className="alert">
+        <strong style={{ color: '#9b9a9b' }}>
+          Shared a saved search:
+        </strong>
+
+        <AlertViewerModal
+          show={!loading && feed !== null}
+          feed={feed}
+          onHide={() => this.setState({ feed: null })}
+        />
+
+        <div
+          className="alert-widget"
+          onClick={() => this.loadAlert(alert)}
+        >
+          <div className="icon">
+            <img
+              src={
+                loading ?
+                '/static/images/loading-states/grid-blue.svg':
+                '/static/images/chatroom/alert.svg'
+              }
+            />
+          </div>
+
+          <div className="heading">
+            <div className="title">{ alert.title }</div>
+            <div className="proposed">
+              { alert.proposed_title }
+              <i className="fa fa-angle-right" />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
