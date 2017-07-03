081da4a36870f8c5108f32ae2a3f31eae7669888
OneDev0411 <cryptoapple1109@gmail.com>
2017-07-03 22:18:24 +0430

    fixed: alert feed read more api model #190

--- app/components/Pages/Dashboard/controller/listing-viewer.js
+++ app/components/Pages/Dashboard/controller/listing-viewer.js
@@ -17,8 +17,7 @@ const controller = {
     AppStore.data.current_listing = listing
     delete AppStore.data.show_alert_modal
     AppStore.emitChange()
-    if (!listing.id)
-      return
+    if (!listing.id) { return }
     ListingDispatcher.dispatch({
       action: 'get-listing',
       user,
@@ -29,27 +28,26 @@ const controller = {
   addToAlreadyViewed(id) {
     const data = AppStore.data
     const listing_map = data.listing_map
-    if (!listing_map)
-      return
+    if (!listing_map) { return }
     if (listing_map.listings_viewed) {
-      if (listing_map.listings_viewed.indexOf(id) === -1)
-        AppStore.data.listing_map.listings_viewed.push(id)
-    } else
-      AppStore.data.listing_map.listings_viewed = [id]
+      if (listing_map.listings_viewed.indexOf(id) === -1) { AppStore.data.listing_map.listings_viewed.push(id) }
+    } else { AppStore.data.listing_map.listings_viewed = [id] }
     AppStore.emitChange()
   },
   hideListingViewer() {
     delete AppStore.data.show_listing_viewer
     delete AppStore.data.current_listing
-    if (!AppStore.data.show_search_map || !AppStore.data.show_alerts_map || !AppStore.data.show_actives_map)
-      AppStore.data.show_search_map = true
+    if (
+      !AppStore.data.show_search_map ||
+      !AppStore.data.show_alerts_map ||
+      !AppStore.data.show_actives_map
+    ) { AppStore.data.show_search_map = true }
 
     browserHistory.push('/dashboard/mls')
     AppStore.emitChange()
   },
   showModalGallery(image_url) {
-    if (!image_url)
-      return
+    if (!image_url) { return }
     const data = AppStore.data
     const gallery_image_urls = data.current_listing.gallery_image_urls
     const image_index = gallery_image_urls.indexOf(image_url)
@@ -64,14 +62,10 @@ const controller = {
     const data = AppStore.data
     const gallery_image_urls = data.current_listing.gallery_image_urls
     const current_index = AppStore.data.modal_gallery.current_index
-    if (selectedDirection === 'prev')
-      AppStore.data.modal_gallery.current_index = current_index - 1
-    if (selectedDirection === 'next')
-      AppStore.data.modal_gallery.current_index = current_index + 1
-    if (AppStore.data.modal_gallery.current_index === -1)
-      AppStore.data.modal_gallery.current_index = gallery_image_urls.length - 1
-    if (AppStore.data.modal_gallery.current_index === gallery_image_urls.length)
-      AppStore.data.modal_gallery.current_index = 0
+    if (selectedDirection === 'prev') { AppStore.data.modal_gallery.current_index = current_index - 1 }
+    if (selectedDirection === 'next') { AppStore.data.modal_gallery.current_index = current_index + 1 }
+    if (AppStore.data.modal_gallery.current_index === -1) { AppStore.data.modal_gallery.current_index = gallery_image_urls.length - 1 }
+    if (AppStore.data.modal_gallery.current_index === gallery_image_urls.length) { AppStore.data.modal_gallery.current_index = 0 }
     AppStore.data.modal_gallery.direction = selectedDirection
     AppStore.emitChange()
   },
