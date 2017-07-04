5691a5611af3311dab6b8748fce43f8c5a1d9098
OneDev0411 <cryptoapple1109@gmail.com>
2017-07-04 19:59:25 +0430

    reimplement Brand model

--- app/models/Brand.js
+++ app/models/Brand.js
@@ -1,24 +1,43 @@
 // models/Brand.js
-import es6Promise from 'es6-promise'
-es6Promise.polyfill()
-import 'isomorphic-fetch'
-import config from '../../config/public'
+import Fetch from '../services/fetch'
+
 export default {
   getByHostname: (params, callback) => {
-    let endpoint = `/api/brands/search?hostname=${params.hostname}`
-    if (params.user) endpoint += `&access_token=${params.user.access_token}`
+    const { hostname, user } = params
 
-    fetch(endpoint)
-      .then(response => {
-        if (response.status >= 400) {
-          const error = {
-            status: 'error',
-            response
-          }
-          return callback(error, false)
-        }
-        return response.json()
-      })
-      .then(response => callback(false, response))
+    let hn = typeof window !== 'undefined' ? window.location.hostname : null
+
+    if (hostname === hn)
+      return false
+
+    let endpoint = `/brands/search?hostname=${hostname}`
+    if (params.user) endpoint += `&access_token=${user.access_token}`
+
+    new Fetch()
+    .get(endpoint)
+    .end((err, res) => {
+      if (err) {
+        return callback(err, false)
+      }
+
+      res.status = 'success'
+      res.data = res.body.data
+      res.info = res.body.info
+
+      return callback(false, res)
+    })
+    // console.log('>>>', endpoint)
+    // fetch(endpoint)
+    //   .then(response => {
+    //     if (response.status >= 400) {
+    //       const error = {
+    //         status: 'error',
+    //         response
+    //       }
+    //       return callback(error, false)
+    //     }
+    //     return response.json()
+    //   })
+    //   .then(response => callback(false, response))
   }
 }
