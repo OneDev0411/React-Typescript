24151ac3374984b302d7a60a8281381eeed2f5d5
OneDev0411 <cryptoapple1109@gmail.com>
2017-07-09 03:21:15 +0430

    reimplement compose input component to support mentioning

--- app/components/Pages/Dashboard/Chatroom/ComposeMessage/input.js
+++ app/components/Pages/Dashboard/Chatroom/ComposeMessage/input.js
@@ -1,18 +1,20 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import Textarea from 'react-textarea-autosize'
+import Mentions from './mention'
 
-export default class MessageInput extends React.Component {
+class MessageInput extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      height: 40
+      height: 40,
     }
   }
 
   onHeightChangeHandler(height) {
     const { onHeightChange } = this.props
 
-    this.setState({ height }, () => {
+    this.setState({ height: height + 5 }, () => {
       onHeightChange(height)
     })
   }
@@ -23,26 +25,68 @@ export default class MessageInput extends React.Component {
     inputRef(ref)
   }
 
+  onKeyPress(e) {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+    }
+  }
+
+  getUsersList() {
+    const { users } = this.props
+
+    const usersList = users.map(user => ({
+      id: user.id,
+      avatar: user.profile_image_url,
+      name: user.display_name,
+      email: user.email,
+      username: user.username || user.abbreviated_display_name
+    }))
+
+    usersList.unshift({
+      id: -1,
+      avatar: '/static/images/dashboard/rebot@2x.png',
+      username: 'Room',
+      name: 'Notify every member in this room'
+    })
+
+    return usersList
+  }
+
   render() {
     const { height } = this.state
 
     return (
       <div
         className="message-create"
-        style={{ height: (height+5) + 'px' }}
+        style={{ height: `${height}px` }}
       >
+        <Mentions
+          handler="compose-message"
+          source={this.getUsersList()}
+          position={height}
+          trigger="@"
+        />
+
         <Textarea
           autoFocus
+          id="compose-message"
           dir="auto"
           placeholder="Write a message ..."
           maxRows={5}
           inputRef={ref => this.setReference(ref)}
           onHeightChange={height => this.onHeightChangeHandler(height)}
-          onKeyPress={e => {
-            if (e.key === 'Enter') e.preventDefault()
-          }}
+          onKeyPress={e => this.onKeyPress(e)}
         />
       </div>
     )
   }
 }
+
+function mapStateToProps({ chatroom }, props) {
+  const room = chatroom.rooms[props.roomId]
+
+  return {
+    users: room ? room.users : null
+  }
+}
+export default connect(mapStateToProps)(MessageInput)
