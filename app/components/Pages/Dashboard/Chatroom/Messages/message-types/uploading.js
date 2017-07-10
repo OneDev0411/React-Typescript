96139b589efcbf8ede3c61f3c323122d015726b3
OneDev0411 <cryptoapple1109@gmail.com>
2017-07-11 02:59:24 +0430

    implement uploading component

---
+++ app/components/Pages/Dashboard/Chatroom/Messages/message-types/uploading.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { ProgressBar } from 'react-bootstrap'
+
+export default ({
+  message
+}) => {
+  const percent = (message.index * 100) / message.count
+
+  return (
+    <div className="uploading">
+      <strong style={{ color: '#9b9a9b' }}>
+        Uploading files - &nbsp;
+        {
+          message.index === message.count ?
+          <span>(Finished)</span> :
+          <span>({ message.index } from { message.count })</span>
+        }
+      </strong>
+
+      {
+        message.current &&
+        <div>
+          Uploading: <b>{ message.current.name }</b>
+        </div>
+      }
+
+      {
+        !message.current &&
+        <div>Sending message ...</div>
+      }
+
+      <ProgressBar
+        active={percent < 100}
+        now={percent}
+      />
+    </div>
+  )
+}
