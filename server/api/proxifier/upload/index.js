6c391e214252ab6b60f27346d1fd9fb224d8bccb
OneDev0411 <cryptoapple1109@gmail.com>
2017-07-11 02:57:37 +0430

    implement uploader proxy

---
+++ server/api/proxifier/upload/index.js
@@ -0,0 +1,43 @@
+import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
+import _ from 'underscore'
+const fileParser = require('async-busboy')
+const router = require('koa-router')()
+
+import config from '../../../../config/private'
+
+const app = new Koa()
+
+router.post('/proxifier/upload', bodyParser(), async ctx => {
+  const headers = ctx.headers
+  const {files, fields} = await fileParser(ctx.req)
+
+  try {
+    // remove base_url because current fetcher middleware add it by itself
+    const endpoint = headers['x-endpoint'].replace(config.api.url, '')
+
+    const request = ctx
+      .fetch(endpoint, 'post', 'multipart/form-data')
+
+    _.each(files, file => {
+      request.attach(file.filename, file.path)
+    })
+
+    if (headers.authorization) {
+      request.set({ Authorization: headers.authorization })
+    }
+
+    const response = await request
+
+    ctx.body = response.body
+  } catch (e) {
+    e.response = e.response || {
+      status: 500,
+      text: e.message
+    }
+    ctx.status = e.response.status
+    ctx.body = e.response.text
+  }
+})
+
+module.exports = app.use(router.routes())
