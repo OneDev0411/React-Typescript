5f667934209d724fe18e506895dfad412aa6eb87
OneDev0411 <cryptoapple1109@gmail.com>
2017-07-17 23:02:53 +0430

    remove brand checking from page mdlwr cuz it moved to render midlwr

--- server/util/pages/index.js
+++ server/util/pages/index.js
@@ -4,24 +4,10 @@ import url from 'url'
 import MobileDetect from 'mobile-detect'
 import config from '../../../config/private'
 import handle490 from './490.js'
-import Brand from '../../../app/models/Brand'
 
 const _ = require('underscore')
 
 const app = new Koa()
-
-async function getBrand(user) {
-  return new Promise((resolve, reject) => {
-    const hostname = url.parse(config.app_url).hostname
-
-    Brand.getByHostname({ hostname, user }, (err, res) => {
-      if (err) {
-        return reject(err)
-      }
-      return resolve(res)
-    })
-  })
-}
 
 const routes = {
   app: [
@@ -80,22 +66,6 @@ app.use(async (ctx, next) => {
         user: ctx.session.user
       }
     }
-  }
-
-  try {
-    if (!AppStore.data.brand_checked) {
-      const response = await getBrand(user)
-      AppStore.data = {
-        ...AppStore.data,
-        ...{
-          brand: response.body.data
-        }
-      }
-    }
-  } catch (e) {
-    /* nothing */
-  } finally {
-    AppStore.data.brand_checked = true
   }
 
   ctx.locals.AppStore = AppStore
