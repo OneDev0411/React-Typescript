72bac6278739d3cd2ea07e081b9d01a1cd6318e0
OneDev0411 <cryptoapple1109@gmail.com>
2017-07-19 18:43:12 +0430

    Added [MLS -> Modals]: ShareAlertModal components

--- app/components/Pages/Dashboard/Listings/components/modals/CreateAlertModal.js
+++ app/components/Pages/Dashboard/Listings/components/modals/CreateAlertModal.js
@@ -7,6 +7,7 @@ import withHandlers from 'recompose/withHandlers'
 import { Modal } from 'react-bootstrap'
 
 import SuccessModal from './SuccessModal'
+import ShareAlertModal from './ShareAlertModal'
 import createAlert from '../../../../../../models/listings/alerts/create-alert'
 
 const CreateAlertModal = ({
@@ -14,72 +15,77 @@ const CreateAlertModal = ({
   isActive,
   // internals
   isSaving,
+  alertTitle,
   setAlertName,
   saveAlertHandler,
-  saveAndShareHandler,
-  successModalIsActive
-}) => {
-  let $alertTilteInput
-  return (
-    <div>
-      <Modal
-        show={isActive}
-        onHide={isSaving ? () => {} : onHide}
-        className="c-create-alert-modal"
-      >
-        <Modal.Body style={{ padding: 0 }}>
-          <div className="c-create-alert-modal__hero">
-            <img
-              className="c-create-alert-modal__hero__logo"
-              src="/static/images/dashboard/mls/alert-bell.svg"
-            />
-            <p style={{ marginBottom: 0 }}>Get new listings faster</p>
-            <p style={{ marginBottom: 0 }}>than your local MLS®</p>
-          </div>
-          <div style={{ padding: '2rem' }}>
-            <label htmlFor="alertName" style={{ display: 'block' }}>
-              Alert Name
-            </label>
-            <input
-              id="alertName"
-              type="text"
-              className="c-create-alert-modal__alert-title-input"
-              placeholder={'Naming your alert...'}
-              ref={node => ($alertTilteInput = node)}
-            />
-          </div>
-        </Modal.Body>
-        <Modal.Footer className="c-create-alert-modal__footer">
-          <button
-            onClick={() => saveAlertHandler($alertTilteInput.value)}
-            className={`c-create-alert-modal__button ${isSaving
-              ? 'isSaving'
-              : ''}`}
-            disabled={isSaving}
-            style={{ float: 'left' }}
-          >
-            {isSaving ? 'Saving...' : 'Save for me'}
-          </button>
-          <button
-            onClick={saveAndShareHandler}
-            className={`c-create-alert-modal__button ${isSaving
-              ? 'isSaving'
-              : ''}`}
-            disabled={isSaving}
-            style={{ float: 'right' }}
-          >
-            Save &amp; Share
-          </button>
-        </Modal.Footer>
-      </Modal>
-      <SuccessModal
-        type="SAVED_ALERT"
-        text="Alert Saved"
-        isActive={successModalIsActive}
-      />
-    </div>
-  )
-}
+  titleInputOnChange,
+  hideShareAlertModal,
+  successModalIsActive,
+  activeShareAlertModal,
+  shareAlertModalIsActive
+}) =>
+  <div>
+    <Modal
+      show={isActive}
+      onHide={isSaving ? () => {} : onHide}
+      className="c-create-alert-modal"
+    >
+      <Modal.Body style={{ padding: 0 }}>
+        <div className="c-create-alert-modal__hero">
+          <img
+            className="c-create-alert-modal__hero__logo"
+            src="/static/images/dashboard/mls/alert-bell.svg"
+          />
+          <p style={{ marginBottom: 0 }}>Get new listings faster</p>
+          <p style={{ marginBottom: 0 }}>than your local MLS®</p>
+        </div>
+        <div style={{ padding: '2rem' }}>
+          <label htmlFor="alertName" style={{ display: 'block' }}>
+            Alert Name
+          </label>
+          <input
+            id="alertName"
+            type="text"
+            className="c-create-alert-modal__alert-title-input"
+            placeholder={'Naming your alert...'}
+            onChange={titleInputOnChange}
+          />
+        </div>
+      </Modal.Body>
+      <Modal.Footer className="c-create-alert-modal__footer">
+        <button
+          onClick={saveAlertHandler}
+          className={`c-create-alert-modal__button ${isSaving
+            ? 'isSaving'
+            : ''}`}
+          disabled={isSaving}
+          style={{ float: 'left' }}
+        >
+          {isSaving ? 'Saving...' : 'Save for me'}
+        </button>
+        <button
+          onClick={activeShareAlertModal}
+          className={`c-create-alert-modal__button ${isSaving
+            ? 'isSaving'
+            : ''}`}
+          disabled={isSaving}
+          style={{ float: 'right' }}
+        >
+          Save &amp; Share
+        </button>
+      </Modal.Footer>
+    </Modal>
+    <SuccessModal
+      type="SAVED_ALERT"
+      text="Alert Saved"
+      isActive={successModalIsActive}
+    />
+    <ShareAlertModal
+      alertTitle={alertTitle}
+      onHide={hideShareAlertModal}
+      isActive={shareAlertModalIsActive}
+    />
+  </div>
 
 export default compose(
   pure,
@@ -88,21 +94,27 @@ export default compose(
     searchOptions: search.options
   })),
   withState('isSaving', 'setIsSaving', false),
+  withState('alertTitle', 'setAlertTitle', ''),
   withState('successModalIsActive', 'setSuccessModalIsActive', false),
+  withState('shareAlertModalIsActive', 'setShareAlertModalIsActive', false),
   withHandlers({
+    titleInputOnChange: ({ setAlertTitle }) => e => {
+      setAlertTitle(e.target.value)
+    },
     saveAlertHandler: ({
       user,
       onHide,
       isSaving,
+      alertTitle,
       setIsSaving,
       searchOptions,
       alertProposedTitle,
       setSuccessModalIsActive
-    }) => title => {
+    }) => () => {
       const alertOptions = {
         ...searchOptions,
         limit: null,
-        title: title || alertProposedTitle,
+        title: alertTitle || alertProposedTitle,
         created_by: user.id,
         room: user.personal_room
       }
@@ -118,8 +130,14 @@ export default compose(
         })
         .catch(({ message }) => {
           setIsSaving(false)
-          console.log(message)
         })
+    },
+    hideShareAlertModal: ({ setShareAlertModalIsActive, onHide }) => () => {
+      setShareAlertModalIsActive(false)
+    },
+    activeShareAlertModal: ({ onHide, setShareAlertModalIsActive }) => () => {
+      onHide()
+      setShareAlertModalIsActive(true)
     }
   })
 )(CreateAlertModal)
---
+++ app/components/Pages/Dashboard/Listings/components/modals/ShareAlertModal.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import pure from 'recompose/pure'
+import { connect } from 'react-redux'
+import compose from 'recompose/compose'
+import withState from 'recompose/withState'
+import withHandlers from 'recompose/withHandlers'
+import { Modal } from 'react-bootstrap'
+
+import SuccessModal from './SuccessModal'
+import Compose from '../../../../../Partials/Compose'
+import { hasRecipients } from '../../../../../../utils/helpers'
+import { createRoom } from '../../../../../../store_actions/chatroom/room'
+import createAlert from '../../../../../../models/listings/alerts/create-alert'
+
+const CreateAlertModal = ({
+  onHide,
+  isActive,
+  // internals
+  isSharing,
+  recipients,
+  shareHandler,
+  setRecipients,
+  onChangeRecipients,
+  successModalIsActive
+}) =>
+  <div>
+    <Modal
+      show={isActive}
+      onHide={isSharing ? () => {} : onHide}
+      className="c-share-alert-modal"
+    >
+      <Modal.Body style={{ padding: 0 }}>
+        <Compose onChangeRecipients={recps => setRecipients(recps)} />
+      </Modal.Body>
+      <Modal.Footer className="c-create-alert-modal__footer">
+        <button
+          onClick={shareHandler}
+          className={`c-create-alert-modal__button ${isSharing
+            ? 'isSaving'
+            : ''}`}
+          disabled={isSharing || !hasRecipients(recipients)}
+          style={{ float: 'right' }}
+        >
+          Save &amp; Share
+        </button>
+      </Modal.Footer>
+    </Modal>
+    <SuccessModal
+      type="SAVED_ALERT"
+      text="Alert Shared"
+      isActive={successModalIsActive}
+    />
+  </div>
+
+export default compose(
+  pure,
+  connect(
+    ({ data, search }) => ({
+      user: data.user,
+      searchOptions: search.options
+    }),
+    { createRoom }
+  ),
+  withState('recipients', 'setRecipients', {}),
+  withState('isSharing', 'setIsSharing', false),
+  withState('successModalIsActive', 'setSuccessModalIsActive', false),
+  withHandlers({
+    shareHandler: ({
+      user,
+      onHide,
+      isSharing,
+      recipients,
+      alertTitle,
+      createRoom,
+      setIsSharing,
+      searchOptions,
+      setSuccessModalIsActive
+    }) => () => {
+      setIsSharing(true)
+
+      createRoom(recipients).then(room => {
+        const alertOptions = {
+          ...searchOptions,
+          room,
+          limit: null,
+          title: alertTitle,
+          created_by: user.id
+        }
+
+        createAlert(alertOptions)
+          .then(alert => {
+            setIsSharing(false)
+            onHide()
+            setSuccessModalIsActive(true)
+            setTimeout(() => setSuccessModalIsActive(false), 2000)
+          })
+          .catch(({ message }) => {
+            setIsSharing(false)
+          })
+      })
+    }
+  })
+)(CreateAlertModal)
