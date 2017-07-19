1a3ba96eeddb6fd98e094bfa2fe0f67668303c6a
OneDev0411 <cryptoapple1109@gmail.com>
2017-07-19 13:05:23 +0430

    Added [model -> alerts] create-alert

--- app/models/listings/alerts/index.js
+++ app/models/listings/alerts/index.js
@@ -1,9 +1,11 @@
 import getAlerts from './get-alerts'
+import createAlert from './create-alert'
 import getAlertFeed from './get-alert-feed'
 import clearAlertNotification from './clear-alert-notification'
 
 export default {
   getAlerts,
+  createAlert,
   getAlertFeed,
   clearAlertNotification
 }
