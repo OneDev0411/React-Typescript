b51841373e7dcc86a2a3f6d80c30d5797cb09284
OneDev0411 <cryptoapple1109@gmail.com>
2017-07-27 19:28:59 +0430

    #234 Remove View Exclusive listings button

--- app/components/Pages/Widgets/Listings/index.js
+++ app/components/Pages/Widgets/Listings/index.js
@@ -2,7 +2,6 @@
 import React, { Component } from 'react'
 import S from 'shorti'
 import ListingSection from './Partials/ListingSection'
-import Brand from '../../../../controllers/Brand'
 import AppStore from '../../../../stores/AppStore'
 
 export default class Listings extends Component {
@@ -30,20 +29,8 @@ export default class Listings extends Component {
   render() {
     // Data
     const data = this.props.data
-    let view_all_button
-    if (!data.location.query.all && Brand.asset('listing_url')) {
-      view_all_button = (
-        <div style={S('text-center mt-20 mb-30')}>
-          <a
-            target="_parent" href={Brand.asset('listing_url')} className="btn btn-default"
-            style={S(`w-280 font-17 p-20 color-fff border-1-solid-${Brand.color('primary')} bg-${Brand.color('primary')}`)}
-          >View Exclusive Listings</a>
-        </div>
-      )
-    }
     let links_area = (
       <div>
-        {view_all_button}
         <div style={S('color-9b9b9b font-15 mb-40 mt-40')} className="text-center">
           Powered by <a href="https://rechat.com" target="_blank" style={S('color-2196f3 fw-500')}>
           Rechat<span style={S('color-2196f3 font-9 relative t-7n fw-500')}>TM</span></a>
