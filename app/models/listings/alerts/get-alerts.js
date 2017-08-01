5fdddbcec3193f44cb51ba79a932d67f81a001c6
OneDev0411 <cryptoapple1109@gmail.com>
2017-08-01 21:30:09 +0430

    added [models -> alerts]: get-alert

--- app/models/listings/alerts/get-alerts.js
+++ app/models/listings/alerts/get-alerts.js
@@ -2,15 +2,22 @@ import Fetch from '../../../services/fetch'
 import { normalize } from 'normalizr'
 import * as schema from '../schema'
 
-const getAlerts = async max_value => {
+const getAlerts = async (params = {}) => {
+  const { limit, since_value, max_value } = params
+
   try {
     const response = await new Fetch()
       .get('/alerts')
-      .query({ limit: 10 })
+      .query({ limit: limit || 500 })
       .query({ max_value })
+      .query({ since_value })
       .query({ sorting_value: 'Update' })
 
-    const { info, data } = response.body
+    let { info, data } = response.body
+
+    if (since_value && !max_value) {
+      data = data.reverse()
+    }
 
     return {
       ...normalize(data, schema.listingsList),
--- app/models/listings/alerts/index.js
+++ app/models/listings/alerts/index.js
@@ -1,3 +1,4 @@
+import getAlert from './get-alert'
 import getAlerts from './get-alerts'
 import deleteAlert from './delete-alert'
 import createAlert from './create-alert'
@@ -5,6 +6,7 @@ import getAlertFeed from './get-alert-feed'
 import clearAlertNotification from './clear-alert-notification'
 
 export default {
+  getAlert,
   getAlerts,
   deleteAlert,
   createAlert,
