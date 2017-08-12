d6b538b10a51f22da6b8ac12931e27cae678d36a
OneDev0411 <cryptoapple1109@gmail.com>
2017-08-12 23:40:42 +0430

    #270 Clean brand model & brand role action

--- app/models/BrandConsole.js
+++ app/models/BrandConsole.js
@@ -1,6 +1,3 @@
-import _ from 'underscore'
-import moment from 'moment'
-import config from '../../config/public'
 import Fetch from '../services/fetch'
 
 const BrandConsole = {}
@@ -15,7 +12,6 @@ BrandConsole.getRoles = async function (user) {
   } catch (e) {
     console.log(e)
   }
-
 }
 
 BrandConsole.getMembers = async function (role) {
@@ -26,142 +22,6 @@ BrandConsole.getMembers = async function (role) {
     return await fetchRoles
   } catch (e) {
     console.log(e)
-  }
-
-}
-/**
- * returns rooms list
- */
-BrandConsole.getRooms = async function (user = {}) {
-  const { access_token } = user
-
-  try {
-    const fetchRooms = new Fetch()
-      .get('/rooms')
-      .query({ 'room_types[]': 'Direct' })
-      .query({ 'room_types[]': 'Group' })
-      .query({ 'associations': 'user.last_seen_by' })
-      .query({ 'limit': '1000' })
-      .query({ 'sorting_value': 'Update' })
-
-    // required on ssr
-    if (access_token)
-      fetchRooms.set({ Authorization: `Bearer ${access_token}` })
-
-    return await fetchRooms
-  } catch (e) {
-  }
-}
-
-/**
- * get room by id
- */
-BrandConsole.getRoomById = async function (roomId) {
-  try {
-    const response = await new Fetch()
-      .get(`/rooms/${roomId}?associations=user.last_seen_by`)
-
-    return response.body.data
-  } catch (e) {
-  }
-}
-
-/**
- * add new room
- */
-BrandConsole.createRoom = async function (recipients) {
-  const members = [].concat(recipients.users, recipients.emails, recipients.phone_numbers)
-  const room_type = members.length > 1 ? 'Group' : 'Direct'
-
-  // search room is created before or not
-  const room = await BrandConsole.searchRoom(recipients)
-
-  if (room)
-    return room
-
-  try {
-    const response = await new Fetch()
-      .post('/rooms')
-      .send({ room_type })
-      .send(recipients)
-
-    return response.body.data
-  } catch (e) {
-    return null
-  }
-}
-
-/**
- * leave or delete a room
- */
-BrandConsole.leaveRoom = async function (userId, room) {
-  const endpoint = room.room_type === 'Direct' ? `/rooms/${room.id}` : `/rooms/${room.id}/users/${userId}`
-
-  try {
-    return await new Fetch().delete(endpoint)
-  } catch (e) {
-    return null
-  }
-}
-
-/**
- * add members to a room
- */
-BrandConsole.addMembers = async function (roomId, recipients) {
-  try {
-    return await new Fetch()
-      .post(`/rooms/${roomId}/users`)
-      .send(recipients)
-
-  } catch (e) {
-    return null
-  }
-}
-
-BrandConsole.getMessages = async function (id, limit = 20, value = null, value_type = 'max') {
-  let endpoint = `/rooms/${id}/messages?limit=${limit}&sorting_value=Creation`
-
-  if (value) {
-    let type = `${value_type}_value`
-    endpoint += `&${type}=${value}`
-  }
-
-  try {
-    return await new Fetch().get(endpoint)
-  } catch (e) {
-  }
-}
-
-BrandConsole.uploadAttachment = async function (roomId, file) {
-  let endpoint = `/rooms/${roomId}/attachments`
-
-  try {
-    return await new Fetch()
-      .upload(endpoint)
-      .attach(file.name, file)
-
-  } catch (e) {
-    console.log(e)
-  }
-}
-
-BrandConsole.searchRoom = async function (recipients) {
-  let qs = []
-  _.each(recipients, (recp, key) => {
-    _.each(recp, item => {
-      qs.push(`${key}[]=${item}`)
-    })
-  })
-
-  // create query string
-  const query = qs.join('&')
-
-  try {
-    const response = await new Fetch().get(`/rooms/search?${query}`)
-    const rooms = response.body.data
-    return rooms.length > 0 ? rooms[0] : null
-  } catch (e) {
-    return null
   }
 }
 
