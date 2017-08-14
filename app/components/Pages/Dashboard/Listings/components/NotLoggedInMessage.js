e2118b575061c3bd62aadbc93f10e5d1a0e0d6ae
OneDev0411 <cryptoapple1109@gmail.com>
2017-08-15 00:35:12 +0430

    refactor [MLS -> search]: replace anchor wirh Link in NotLoggedInMessage component

--- app/components/Pages/Dashboard/Listings/components/NotLoggedInMessage.js
+++ app/components/Pages/Dashboard/Listings/components/NotLoggedInMessage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 import branch from 'recompose/branch'
 import compose from 'recompose/compose'
 import renderNothing from 'recompose/renderNothing'
@@ -9,8 +10,7 @@ const enhance = hideIfNotLoggedIn(props => props.isLoggedIn)
 
 const RegisterOrLoginMessage = enhance(({ isLoggedIn }) =>
   <p className="c-notLoggedIn-message">
-    <a href="/signup">Sign up</a> or{' '}
-    <a href="/signin">log in</a>
+    <Link to="/signup">Sign up</Link> or <Link to="/signin">Sign in</Link>
     <span> to save listings and chat with our agents.</span>
   </p>
 )
