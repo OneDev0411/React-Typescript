af5bb8ed3b04e1c9b70f00c71b84965d7ac197cc
OneDev0411 <cryptoapple1109@gmail.com>
2017-08-24 18:14:34 +0430

    fix task detail path

---
+++ app/components/Pages/Dashboard/Deals/dashboard/task-detail/index.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import Header from './header'
+import Documents from '../documents'
+import UploadFile from '../documents/upload'
+
+import Comments from '../comments'
+import CommentInput from '../comments/input'
+
+function scrollEnd() {
+  const el = document.getElementById('deals-task-scrollable')
+  el.scrollTop = el.scrollHeight
+}
+
+const TaskManager = ({
+  tasks,
+  taskId
+}) => {
+  if (!taskId) {
+    return false
+  }
+
+  const task = tasks[taskId]
+
+  return (
+    <div className="full-width">
+      <UploadFile
+        disableClick={true}
+        task={task}
+      >
+        <div
+          className="scrollable"
+          id="deals-task-scrollable"
+        >
+          <Header
+            task={task}
+          />
+
+          <Documents
+            task={task}
+          />
+
+          <Comments
+            task={task}
+          />
+        </div>
+
+        <CommentInput
+          task={task}
+          onCommentSaved={() => scrollEnd()}
+        />
+      </UploadFile>
+    </div>
+  )
+}
+
+export default connect(({ deals }) => ({
+  tasks: deals.tasks
+}))(TaskManager)
