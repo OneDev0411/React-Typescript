dca781a10cd82d8129478368c7417ff5fc67c54a
OneDev0411 <cryptoapple1109@gmail.com>
2017-08-25 14:54:09 +0430

    added [reducer -> data]: sigin and edit user state

--- app/reducers/data/index.js
+++ app/reducers/data/index.js
@@ -1,4 +1,6 @@
 import { types } from '../../store_actions/data'
+import { EDIT_USER_SUCCESS } from '../../constants/user'
+import { SIGNIN_SUCCESS } from '../../constants/auth/signin'
 const initialState = {
   counter: 1
 }
@@ -9,10 +11,23 @@ export default (state = initialState, action) => {
       const counter = !state.counter ? initialState.counter : state.counter + 1
 
       return {
+        ...state,
         ...action.data,
         counter
       }
-
+    case SIGNIN_SUCCESS:
+      return {
+        ...state,
+        user: action.user
+      }
+    case EDIT_USER_SUCCESS:
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          ...action.user
+        }
+      }
     default:
       return state
   }
