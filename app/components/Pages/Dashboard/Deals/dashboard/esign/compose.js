10f9bf42bff63d5ae67e84ba852936788e478800
OneDev0411 <cryptoapple1109@gmail.com>
2017-08-28 01:32:23 +0430

    add envelope to the list after sending

--- app/components/Pages/Dashboard/Deals/dashboard/esign/compose.js
+++ app/components/Pages/Dashboard/Deals/dashboard/esign/compose.js
@@ -7,7 +7,7 @@ import _ from 'underscore'
 import cn from 'classnames'
 import Recipients from './recipients'
 import DealModel from '../../../../../../models/Deal'
-import { closeEsign, showAttachments } from '../../../../../../store_actions/deals'
+import { closeEsign, showAttachments, setEnvelopes } from '../../../../../../store_actions/deals'
 import ComposeAttachments from './compose-attachments'
 import config from '../../../../../../../config/public'
 
@@ -92,7 +92,7 @@ class SendSignatures extends React.Component {
    */
   async send() {
     const { recipients, isSending } = this.state
-    const { closeEsign, user, deal, esign, tasks } = this.props
+    const { closeEsign, setEnvelopes, user, deal, esign, tasks } = this.props
     const subject = this.subject.value
     const message = this.message.value
     const attachments = esign.attachments.map(id => {
@@ -104,13 +104,17 @@ class SendSignatures extends React.Component {
     })
 
     try {
-      await DealModel.sendEnvelope(
+      const envelope = await DealModel.sendEnvelope(
         deal.id,
         subject,
         message,
         attachments,
         recipients
       )
+
+      // add envelope to list of envelopes
+      setEnvelopes(deal.id, [envelope])
+
       // close esign
       closeEsign()
 
@@ -230,4 +234,4 @@ export default connect(({ deals, data }) => ({
   forms: deals.forms,
   tasks: deals.tasks,
   esign: deals.esign || {}
-}), { showAttachments, closeEsign })(SendSignatures)
+}), { showAttachments, closeEsign, setEnvelopes })(SendSignatures)
