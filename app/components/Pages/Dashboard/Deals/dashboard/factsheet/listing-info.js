188937aae6f0084e4604522e953f28a31351cda1
OneDev0411 <cryptoapple1109@gmail.com>
2017-08-30 18:32:40 +0430

    connect listing info to fact sheet

--- app/components/Pages/Dashboard/Deals/dashboard/factsheet/listing-info.js
+++ app/components/Pages/Dashboard/Deals/dashboard/factsheet/listing-info.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import _ from 'underscore'
+import Deal from '../../../../../../models/Deal'
 
 const table = {
-  a1: 'Property Type',
-  a2: 'MLS',
-  a3: 'File ID',
+  property_type: 'Property Type',
+  mls_number: 'MLS',
+  file_id: 'File ID',
 }
 
 export default ({
@@ -16,7 +17,9 @@ export default ({
         _.map(table, (name, field) => (
           <li key={`LISTING_INFO_FIELD_${field}`}>
             <span className="name no-status">{ name }:</span>
-            <span className="field">-</span>
+            <span className="field">
+              {Deal.get.field(deal, field)}
+            </span>
           </li>
         ))
       }
