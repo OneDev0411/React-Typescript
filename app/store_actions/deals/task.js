bad17c2d2c879a587c77590ca014df7a4b43b70e
OneDev0411 <cryptoapple1109@gmail.com>
2017-08-31 21:03:03 +0430

    reimplement add task based on modal and checklists

--- app/store_actions/deals/task.js
+++ app/store_actions/deals/task.js
@@ -38,6 +38,7 @@ export function createTask(deal_id, form, title, status, task_type, list_id) {
   return async (dispatch) => {
     const task = await Deal.createTask(deal_id, form, title, status, task_type, list_id)
     dispatch(addNewTask(deal_id, list_id, task))
+    return task
   }
 }
 
