9a43afa62f65b1647174e8b9079f3e07c2412873
OneDev0411 <cryptoapple1109@gmail.com>
2017-08-31 23:38:54 +0430

    #180 Edit fullscreen icon

--- app/components/Pages/Dashboard/Partials/Svgs/TwoDirectionArrow.js
+++ app/components/Pages/Dashboard/Partials/Svgs/TwoDirectionArrow.js
@@ -5,7 +5,7 @@ export default ({
                   width = '16',
                   fill = '#B2B2B2'
                 }) => (
-                  <svg className="maximize-icon" xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 12 11">
-                    <path className="maximize-icon path" fill={fill} fillRule="evenodd" d="M7 9H3.414L10 3.414V7h2V0H5v2h3.586L2 7.586V4H0v7h7z" />
+                  <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 16 16">
+                    <path fill={fill} fillRule="evenodd" d="M7 14H3.414L14 3.414V7h2V0H9v2h3.586L2 12.586V9H0v7h7z" />
                   </svg>
 )
