ac2d9e80d10a8bdcb99e715eccfa8512f850f1e2
OneDev0411 <cryptoapple1109@gmail.com>
2017-09-07 17:15:07 +0430

    develop auto scale feature

--- app/components/Partials/Pdf/Page.js
+++ app/components/Partials/Pdf/Page.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactResizeDetector from 'react-resize-detector'
 
 class PdfPage extends React.Component {
 
@@ -17,28 +18,60 @@ class PdfPage extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    const { pageNumber } = nextProps
+    const { pageNumber, scale } = nextProps
+    const currentPageNumber = this.props.pageNumber
+    const currentScale = this.props.scale
 
-    if (nextProps.pageNumber !== this.props.pageNumber)
+    if (pageNumber !== currentPageNumber || scale !== currentScale) {
       this.renderPage(pageNumber)
+    }
   }
 
   async setStateSync(states) {
     this.setState(states, () => true)
   }
 
+  onResize() {
+    const { pageNumber } = this.props
+    this.renderPage(pageNumber)
+  }
+
+  async calculateScale(pageNumber) {
+    const { doc } = this.props
+
+    if (!this.container) {
+      return null
+    }
+
+    const width = this.container.clientWidth
+
+    const page = await doc.getPage(pageNumber)
+    const viewport = page.getViewport(1)
+
+    return width / viewport.width
+  }
+
   /**
    *
    */
   async renderPage(pageNumber) {
-    const { doc, scale } = this.props
+    const { doc } = this.props
+    let { scale } = this.props
+
+    if (scale === 'auto') {
+      scale = await this.calculateScale(pageNumber)
+    }
+
+    if (scale === null) {
+      return false
+    }
 
     // load page
     const page = await doc.getPage(pageNumber)
-
     const viewport = page.getViewport(scale)
+
     const { width, height } = viewport
-    const canvas = this.container
+    const canvas = this.canvas
     const context = canvas.getContext('2d')
     canvas.width = width
     canvas.height = height
@@ -54,10 +87,29 @@ class PdfPage extends React.Component {
 
   render() {
     const { width, height } = this.state
+    const { scale } = this.props
+    const containerStyle = {}
+
+    if (scale !== 'auto') {
+      containerStyle.width = width
+      containerStyle.height = height
+    }
 
     return (
-      <div className="pdf-container" style={{ width, height }}>
-        <canvas className="pdf-page" ref={canv => this.container = canv} />
+      <div
+        className="pdf-container"
+        style={containerStyle}
+        ref={ref => this.container = ref}
+      >
+        <ReactResizeDetector
+          handleWidth
+          onResize={() => this.onResize()}
+        />
+
+        <canvas
+          className="pdf-page"
+          ref={ref => this.canvas = ref}
+        />
       </div>
     )
   }
