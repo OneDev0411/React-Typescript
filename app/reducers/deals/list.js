c00520f8e847ef12c3310ed21aec69c2cacb5e8d
OneDev0411 <cryptoapple1109@gmail.com>
2017-09-10 21:14:02 +0430

    implement append checklist reducer

--- app/reducers/deals/list.js
+++ app/reducers/deals/list.js
@@ -17,6 +17,18 @@ export default (state = null, action) => {
         ...state
       }
 
+    case types.APPEND_CHECKLIST:
+      return {
+        ...state,
+        [action.deal_id]: {
+          ...state[action.deal_id],
+          checklists: [
+            ...state[action.deal_id].checklists,
+            action.checklist_id
+          ]
+        }
+      }
+
     case types.UPDATE_ROLES:
       return {
         ...state,
