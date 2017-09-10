a838841b291d77450433a2d855e48ea4e528aee1
OneDev0411 <cryptoapple1109@gmail.com>
2017-09-10 21:14:29 +0430

    fix deals schema

--- app/store_actions/deals/schema.js
+++ app/store_actions/deals/schema.js
@@ -1,14 +1,18 @@
 import { schema } from 'normalizr'
 
-const tasks = new schema.Entity('tasks')
+// schema for task
+const task = new schema.Entity('tasks')
 
-const checklists = new schema.Entity('checklists', {
-  tasks: [tasks]
+// schema for single checklist
+const checklist = new schema.Entity('checklists', {
+  tasks: [task]
 })
 
+// schema for deal
 export const deal = new schema.Entity('deals', {
-  checklists: [checklists]
+  checklists: [checklist]
 })
 
 export const dealsSchema = [deal]
 export const dealSchema = deal
+export const checklistSchema = checklist
