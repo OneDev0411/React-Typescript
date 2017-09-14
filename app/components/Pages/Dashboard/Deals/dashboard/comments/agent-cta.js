9f65a931f6933c4b48c4e639f9c2b78abdaa05b8
OneDev0411 <cryptoapple1109@gmail.com>
2017-09-14 19:41:22 +0430

    fix comments ui

--- app/components/Pages/Dashboard/Deals/dashboard/comments/agent-cta.js
+++ app/components/Pages/Dashboard/Deals/dashboard/comments/agent-cta.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import cn from 'classnames'
 
 export default ({
   task,
+  hasComment,
   onSendComment
 }) => (
   <div>
     <button
-      className="deal-button add-comment"
+      className={cn('deal-button add-comment', {
+        enabled: hasComment === true
+      })}
       onClick={() => onSendComment()}
     >
       Add Comment
     </button>
 
     <button
-      className="deal-button notify-admin"
+      className={cn('deal-button notify-admin', {
+        enabled: hasComment === true
+      })}
       onClick={() => onSendComment(true)}
     >
       Notify Admin
