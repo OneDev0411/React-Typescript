88bdbc83cfe0564aeae0ade8c040bc11a45474ed
OneDev0411 <cryptoapple1109@gmail.com>
2017-09-14 19:36:30 +0430

    implement delete role action

---
+++ app/store_actions/deals/role.js
@@ -0,0 +1,41 @@
+import types from '../../constants/deals'
+import Deals from '../../models/Deal'
+
+function updateRoles(deal_id, roles) {
+  return {
+    type: types.UPDATE_ROLES,
+    deal_id,
+    roles
+  }
+}
+
+function roleDeleted(deal_id, role_id) {
+  return {
+    type: types.DELETE_ROLE,
+    deal_id,
+    role_id
+  }
+}
+
+
+export function createRole(deal_id, form) {
+  return async (dispatch) => {
+    try {
+      const deal = await Deals.createRole(deal_id, form)
+      dispatch(updateRoles(deal.id, deal.roles))
+    } catch(e) {
+      throw e
+    }
+  }
+}
+
+export function deleteRole(deal_id, role_id) {
+  return async (dispatch) => {
+    try {
+      await Deals.deleteRole(deal_id, role_id)
+      dispatch(roleDeleted(deal_id, role_id))
+    } catch(e) {
+      throw e
+    }
+  }
+}
