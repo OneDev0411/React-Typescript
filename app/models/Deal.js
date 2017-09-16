c8b7dfe97e23ff5589b1264857418b9835963329
OneDev0411 <cryptoapple1109@gmail.com>
2017-09-17 01:12:51 +0430

    implement deal deal endpoint and fix deal address

--- app/models/Deal.js
+++ app/models/Deal.js
@@ -48,10 +48,25 @@ Deal.get.field = function(deal, field) {
 * a helper that extracts address from deal
 */
 Deal.get.address = function(deal) {
-  const street_name = Deal.get.field(deal, 'street_name') || ''
-  const street_address = Deal.get.field(deal, 'street_address') || ''
+  const unitNumber = Deal.get.field(deal, 'unit_number')
+  const city = Deal.get.field(deal, 'city')
 
-  return (street_name + ' ' + street_address).trim()
+  const address = [
+    Deal.get.field(deal, 'street_number') || '',
+    Deal.get.field(deal, 'street_name') || '',
+    Deal.get.field(deal, 'street_suffix') || '',
+    unitNumber ? `, #${unitNumber},` : '',
+    city ? `${city},` : '',
+    Deal.get.field(deal, 'postal_code') || '',
+  ]
+  .join(' ')
+  .trim()
+
+  if (address.slice('-1') === ',') {
+    return address.slice(0, -1)
+  }
+
+  return address
 }
 
 /**
@@ -149,7 +164,7 @@ Deal.addForm = async function (brandId, checklistId, formId) {
  */
 Deal.deleteForm = async function (checklist, formId) {
   if (!checklist.brand) {
-    throw new Error("This user does not belong to any brand")
+    throw new Error('This user does not belong to any brand')
   }
 
   try {
@@ -158,6 +173,17 @@ Deal.deleteForm = async function (checklist, formId) {
 
   } catch (e) {
     return null
+  }
+}
+
+/**
+ * delete a deal
+ */
+Deal.deleteDeal = async function (dealId) {
+  try {
+    await new Fetch().delete(`/deals/${dealId}`)
+  } catch (e) {
+    throw e
   }
 }
 
