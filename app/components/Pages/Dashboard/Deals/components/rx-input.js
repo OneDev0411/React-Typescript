9a36e405eb145c9150008b969bc92471dfb8c868
OneDev0411 <cryptoapple1109@gmail.com>
2017-09-19 01:04:09 +0430

    develop a rx based search input

---
+++ app/components/Pages/Dashboard/Deals/components/rx-input.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { FormControl } from 'react-bootstrap'
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  async componentDidMount() {
+    const Rx = await import('rxjs/Rx' /* webpackChunkName: "rx" */)
+    const { Observable } = Rx
+
+    this.searchHandler = Observable
+      .fromEvent(this.search_input, 'keypress')
+      .debounceTime(1200)
+      .subscribe((e) => this.props.subscribe(e.target.value))
+  }
+
+  componentWillUnmount() {
+    if (this.searchHandler) {
+      this.searchHandler.unsubscribe()
+    }
+  }
+
+  render() {
+    return (
+      <FormControl
+        className="rx-input"
+        value={this.props.value}
+        onChange={(e) => this.props.onChange(e)}
+        inputRef={ref => this.search_input = ref}
+        placeholder={this.props.placeholder}
+      />
+    )
+  }
+}
