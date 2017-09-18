876b31506e868670377e8998c72735dd3fb8f23c
OneDev0411 <cryptoapple1109@gmail.com>
2017-09-18 21:46:35 +0430

    fixed [Brand settings] redirect to oops page when user has not access to page

--- app/components/Pages/Dashboard/Brand/index.js
+++ app/components/Pages/Dashboard/Brand/index.js
@@ -1,30 +1,37 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { browserHistory } from 'react-router'
 import { Tab } from 'react-bootstrap'
 import Roles from './Roles'
 import Checklists from './Checklists'
 import Sidebar from './Sidebar'
 
-const Brand = () => (
-  <div className="brand">
-    <Tab.Container defaultActiveKey="Checklists">
-      <div className="clearfix">
-        <Sidebar />
-        <div className="rightPanel">
-          <Tab.Content animation>
-            <Tab.Pane eventKey="Appearance">
-              Not ready yet....
-            </Tab.Pane>
-            <Tab.Pane eventKey="Roles">
-              <Roles />
-            </Tab.Pane>
-            <Tab.Pane eventKey="Checklists">
-              <Checklists />
-            </Tab.Pane>
-          </Tab.Content>
-        </div>
-      </div>
-    </Tab.Container>
-  </div>
-)
+const Brand = ({ user }) => {
+  if (user && user.brand == null) {
+    browserHistory.push('/oops')
+    return null
+  }
 
-export default Brand
+  return (
+    <div className="brand">
+      <Tab.Container defaultActiveKey="Checklists">
+        <div className="clearfix">
+          <Sidebar />
+          <div className="rightPanel">
+            <Tab.Content animation>
+              <Tab.Pane eventKey="Appearance">Not ready yet....</Tab.Pane>
+              <Tab.Pane eventKey="Roles">
+                <Roles />
+              </Tab.Pane>
+              <Tab.Pane eventKey="Checklists">
+                <Checklists />
+              </Tab.Pane>
+            </Tab.Content>
+          </div>
+        </div>
+      </Tab.Container>
+    </div>
+  )
+}
+
+export default connect(({ user }) => ({ user }))(Brand)
