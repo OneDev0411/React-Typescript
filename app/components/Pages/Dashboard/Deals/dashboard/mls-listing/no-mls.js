601cab7e2a78a6ae4a8b20f677945922daa93468
OneDev0411 <cryptoapple1109@gmail.com>
2017-09-19 01:07:25 +0430

    develop no-mls state

--- app/components/Pages/Dashboard/Deals/dashboard/mls-listing/no-mls.js
+++ app/components/Pages/Dashboard/Deals/dashboard/mls-listing/no-mls.js
@@ -1,18 +1,19 @@
 import React from 'react'
 
-export default class extends React.Component {
-  render() {
-    return (
-      <div className="empty-state">
-        <i className="fa fa-info-circle fa-2x" />
-        <div className="title">
-          Complete Deal
-        </div>
+export default ({
+  addMls
+}) => (
+  <div className="empty-state">
+    <i className="fa fa-info-circle fa-2x" />
+    <div className="title">
+      Complete Deal
+    </div>
 
-        <span className="add-mls">
-          Add a MLS #
-        </span>
-      </div>
-    )
-  }
-}
+    <span
+      className="add-mls"
+      onClick={addMls}
+    >
+      Add a MLS #
+    </span>
+  </div>
+)
