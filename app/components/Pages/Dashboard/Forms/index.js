afebb8ca41fc8accd8fc02d24b6fc42968f7f52e
OneDev0411 <cryptoapple1109@gmail.com>
2017-09-27 19:05:39 +0330

    fix mapper

--- app/components/Pages/Dashboard/Forms/index.js
+++ app/components/Pages/Dashboard/Forms/index.js
@@ -1,7 +1,4 @@
-// Dashboard/Forms/index.js
 import React, { Component } from 'react'
-import SideBar from '../Partials/SideBar'
-import S from 'shorti'
 import config from '../../../../../config/public'
 
 export default class Forms extends Component {
@@ -9,23 +6,18 @@ export default class Forms extends Component {
     // Data
     const data = this.props.data
     const user = data.user
-    const main_style = S('absolute h-100p border-left-70-solid-fff w-100p')
+
     return (
-      <div style={S('minw-1000')}>
-        <main>
-          <SideBar
-            data={data}
-          />
-          <div style={main_style}>
-            <iframe style={S('w-100p h-100p absolute')} src={`${config.forms.url}?access_token=${user.access_token}`} frameBorder="0" />
-          </div>
-        </main>
+      <div>
+        <iframe
+          style={{
+            width: '100%',
+            height: '100vh'
+          }}
+          src={`${config.forms.url}?access_token=${user.access_token}`}
+          frameBorder="0"
+        />
       </div>
     )
   }
-}
-
-// PropTypes
-Forms.propTypes = {
-  data: React.PropTypes.object
 }
