50b422dd1b0fd3d7ed1cab3c23142dd4b800f253
OneDev0411 <cryptoapple1109@gmail.com>
2017-10-03 20:09:38 +0330

    fixed [Auth -> sign up]: user messages

--- app/components/Pages/NoMatch.js
+++ app/components/Pages/NoMatch.js
@@ -2,16 +2,16 @@
 import React from 'react'
 import { Link } from 'react-router'
 
-const NoMatch = () =>
+const NoMatch = () => (
   <div className="noMatch">
     <h1 className="logo">Rechat</h1>
     <div id="main-content" className="container text-center">
       <h1 className="tk-calluna-sans">
-        Oops. The page you were looking for doesn’t exist. Beautiful skyline,
-        though.
+        The page you were looking for doesn’t exist. Beautiful skyline, though.
       </h1>
       <Link to="/">Take me home</Link>
     </div>
   </div>
+)
 
 export default NoMatch
