b326ca6ce93d1fedadc3945f5d9c6ba3ddc8b80e
OneDev0411 <cryptoapple1109@gmail.com>
2017-11-13 17:40:58 +0330

    fix deal close

--- app/components/Pages/Dashboard/Deals/create/nav.js
+++ app/components/Pages/Dashboard/Deals/create/nav.js
@@ -1,12 +1,17 @@
 import React from 'react'
-
+import { browserHistory } from 'react-router'
 export default ({
 
 }) => (
   <div className="nav">
     <div className="title">Create New Deal</div>
     <div className="cta">
-      <button className="deal-button cancel">X</button>
+      <button
+        className="deal-button cancel"
+        onClick={() => browserHistory.push('/dashboard/deals')}
+      >
+        X
+      </button>
     </div>
   </div>
 )
