8e3f8c6f23b2c2f1978604b23b73cf90a1e31070
OneDev0411 <cryptoapple1109@gmail.com>
2017-11-14 10:32:58 +0330

    added: Jest and its tools

--- test/stores.js
+++
@@ -1,9 +0,0 @@
-// test/stores.js
-import { expect } from 'chai'
-import AppStore from '../app/stores/AppStore'
-
-describe('Testing stores', () => {
-  it('AppStore.data.ready defaults to false', () => {
-    expect(AppStore.data.ready).to.not.be.true
-  });
-})
