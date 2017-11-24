6afab1ef60c009b54706516286e80f4047c0f358
OneDev0411 <cryptoapple1109@gmail.com>
2017-11-24 22:56:13 +0330

    develop splitter empty state

---
+++ app/components/Pages/Dashboard/Deals/dashboard/upload/pdf-splitter/workspace/empty-state.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default () => (
+  <div className="empty-state">
+    <img src="/static/images/deals/split-empty.svg" />
+    <div className="title">
+      Drag and drop pages here
+    </div>
+
+    <div className="desc">
+      Drag and drop pages from the left column to split them. <br />
+      You can also reorder the pages by dragging them in this section.
+    </div>
+  </div>
+)
