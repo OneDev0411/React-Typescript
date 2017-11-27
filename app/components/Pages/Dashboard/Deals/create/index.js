900b0c44b8038565b387683eb0b4b29324836568
OneDev0411 <cryptoapple1109@gmail.com>
2017-11-27 20:41:57 +0330

    fix some copies in deal creation

--- app/components/Pages/Dashboard/Deals/create/index.js
+++ app/components/Pages/Dashboard/Deals/create/index.js
@@ -60,18 +60,23 @@ class CreateDeal extends React.Component {
       _.size(clients) > 0
   }
 
-  requestChangeDealSide(dealSide) {
-    const { agents, clients } = this.state
+  requestChangeDealSide(nextDealSide) {
+    const { agents, clients, dealSide } = this.state
+
+    if (dealSide === nextDealSide) {
+      return false
+    }
 
     if (_.size(agents) > 0 || _.size(clients) > 0) {
+      let clientsTitle = (dealSide === 'Buying') ? 'buyers' : 'sellers'
       return this.props.confirmation({
-        message: 'Changing deal side will remove all contacts.',
+        message: `Changing deal side will remove all ${clientsTitle}.`,
         confirmLabel: 'Okay, Continue',
-        onConfirm: () => this.changeDealSide(dealSide)
+        onConfirm: () => this.changeDealSide(nextDealSide)
       })
     }
 
-    this.changeDealSide(dealSide)
+    this.changeDealSide(nextDealSide)
   }
 
   changeDealSide(dealSide) {
