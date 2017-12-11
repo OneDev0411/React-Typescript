b791a3a7786ad24f335a55c313684b165968a39a
OneDev0411 <cryptoapple1109@gmail.com>
2017-12-12 02:36:39 +0330

    Audit [MLS -> listing] fetch error message

--- app/components/Pages/Dashboard/Listings/Listing/components/FetchError.js
+++ app/components/Pages/Dashboard/Listings/Listing/components/FetchError.js
@@ -1,19 +1,26 @@
 import React from 'react'
 
-const FetchError = ({ message, backButtonkHandler }) => (
+const FetchError = ({ message, backButtonHandler }) => (
   <div className="c-listing__loading-error">
-    <h2>404</h2>
-    <h3>{`${message}!`}</h3>
-    {backButtonkHandler && (
-      <button
-        style={{
-          background: `#${Brand.color('primary', '006aff')}`
-        }}
-        onClick={backButtonHandler}
-      >
-        Back
-      </button>
-    )}
+    <div className="deals-error">
+      <h3 className="title">Something went wrong!</h3>
+      <p className="descr">
+        Help us improve your experience by sending an error{' '}
+        <a href="mailto:support@rechat.com">report</a> or{' '}
+        <button
+          onClick={backButtonHandler}
+          style={{
+            padding: 0,
+            border: 'none',
+            color: '#3388ff',
+            background: 'transparent'
+          }}
+        >
+          go back
+        </button>{' '}
+        to search for properties.
+      </p>
+    </div>
   </div>
 )
 
