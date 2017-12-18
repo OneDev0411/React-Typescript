68055be1336239380af7aa37436bb9a2cc8e06ba
OneDev0411 <cryptoapple1109@gmail.com>
2017-12-18 18:11:15 +0330

    Design Audit [App -> side-nav] v3.0

--- app/components/Pages/Dashboard/Listings/Listing/components/ListingDesktopView.js
+++ app/components/Pages/Dashboard/Listings/Listing/components/ListingDesktopView.js
@@ -95,7 +95,7 @@ const ListingDesktopView = ({
     viewer_width = window.innerWidth
 
     if (user && !data.is_widget && container !== 'modal') {
-      viewer_width -= 150
+      viewer_width -= 110
     }
   }
 
@@ -688,7 +688,7 @@ const ListingDesktopView = ({
   }
 
   let viewer_wrap_style = S(
-    `absolute h-100p bg-fff t-0 l-0 z-10 ml-150 w-${viewer_width}`
+    `absolute h-100p bg-fff t-0 l-0 z-10 ml-110 w-${viewer_width}`
   )
 
   if (!user || data.is_widget) {
