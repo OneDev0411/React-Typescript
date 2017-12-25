2eff2354728a4257708e1d8c7ec1373311ccd9b0
OneDev0411 <cryptoapple1109@gmail.com>
2017-12-25 12:05:31 +0330

    #494 Hide forms when task name modal showed

--- app/components/Pages/Dashboard/Deals/dashboard/create-task/form/forms-list.js
+++ app/components/Pages/Dashboard/Deals/dashboard/create-task/form/forms-list.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux'
 import cn from 'classnames'
 import _ from 'underscore'
 import { addNotification as notify } from 'reapop'
-import TaskName from './task-name'
 import {
   createFormTask,
   setSelectedTask,
@@ -16,9 +15,6 @@ class Forms extends React.Component {
     super(props)
     this.state = {
       filter: '',
-      showTaskNameModal: false,
-      taskName: '',
-      isCreatingTask: false,
       creatingForm: null
     }
     this.createNewTask = _.debounce(this.createNewTask, 100)
@@ -33,7 +29,12 @@ class Forms extends React.Component {
       creatingForm: form
     })
 
-    const { deal, createFormTask, setSelectedTask, listId } = this.props
+    const {
+      deal,
+      createFormTask,
+      setSelectedTask,
+      listId
+    } = this.props
 
     // create form
     const task = await createFormTask(deal.id, form.id, form.name, listId)
@@ -45,81 +46,15 @@ class Forms extends React.Component {
     this.displayForm(false)
   }
 
-  getDataTransferItems(event) {
-    let dataTransferItemsList = []
-
-    if (event.dataTransfer) {
-      const dt = event.dataTransfer
-
-      if (dt.files && dt.files.length) {
-        dataTransferItemsList = dt.files
-      } else if (dt.items && dt.items.length) {
-        // During the drag even the dataTransfer.files is null
-        // but Chrome implements some drag store, which is accessible via dataTransfer.items
-        dataTransferItemsList = dt.items
-      }
-    } else if (event.target && event.target.files) {
-      dataTransferItemsList = event.target.files
-    }
-
-    // Convert from DataTransferItemsList to the native Array
-    return Array.prototype.slice.call(dataTransferItemsList)
-  }
-
-  create = async (evt) => {
-    const {
-      createFormTask,
-      setSelectedTask,
-      listId,
-      deal,
-      setUploadFiles,
-      onClose,
-      notify
-    } = this.props
-    const { isCreatingTask } = this.state
-    const { taskName } = this
-    const files = this.getDataTransferItems(evt)
-
-    if (isCreatingTask || taskName.length === 0 || files.length === 0) {
-      return false
-    }
-
-    // change status
-    this.setState({ isCreatingTask: true })
-
-    try {
-      // create task
-      const task = await createFormTask(deal.id, null, taskName, listId)
-
-      // make task active
-      setSelectedTask(task)
-
-      // upload file
-      setUploadFiles(files, deal, task)
-    } catch (e) {
-      notify({
-        message: e.message,
-        status: 'error'
-      })
-    } finally {
-      this.setState({
-        isCreatingTask: false,
-        showTaskNameModal: false
-      }, () => onClose())
-    }
-  }
-
-
   render() {
     const {
       forms,
       show,
-      onClose
+      onClose,
+      displayTaskName
     } = this.props
     const {
       filter,
-      showTaskNameModal,
-      isCreatingTask,
       creatingForm
     } = this.state
 
@@ -168,34 +103,12 @@ class Forms extends React.Component {
                 .value()
             }
           </ul>
-
-          <label>
-            <input
-              type="file"
-              style={{ display: 'none', height: 0, width: 0 }}
-              ref={input => this.formNewTaskFileInputEl = input}
-              onChange={this.create}
-            />
-          </label>
         </Modal.Body>
 
         <Modal.Footer>
           <ul>
             <li className="upload">
-
-              <TaskName
-                show={showTaskNameModal}
-                onClose={() => this.setState({ showTaskNameModal: false })}
-                isCreatingTask={isCreatingTask}
-                addTaskName={(taskName) => {
-                  this.taskName = taskName
-                  this.formNewTaskFileInputEl.click()
-                }}
-              />
-              <div onClick={() => this.setState({
-                showTaskNameModal: true
-              })}
-              >
+              <div onClick={displayTaskName}>
                 <i className="fa fa-plus" /> Other
               </div>
             </li>
@@ -216,7 +129,6 @@ export default connect(mapStateToProps,
   {
     createFormTask,
     setSelectedTask,
-    setUploadFiles,
     notify
   }
 )(Forms)
--- app/components/Pages/Dashboard/Deals/dashboard/create-task/form/index.js
+++ app/components/Pages/Dashboard/Deals/dashboard/create-task/form/index.js
@@ -1,55 +1,169 @@
 import React from 'react'
 import Forms from './forms-list'
+import TaskName from './task-name'
+import {
+  createFormTask,
+  setSelectedTask,
+  setUploadFiles
+} from '../../../../../../../store_actions/deals'
+import { connect } from 'react-redux'
+import { addNotification as notify } from 'reapop'
 
 class CreateForm extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      showFormsModal: false
+      showFormsModal: false,
+      showTaskNameModal: false,
+      isCreatingTask: false,
+      taskName: ''
     }
   }
 
   /**
    *
    */
-  displayForm(status) {
+  displayForm = (status) => {
     this.setState({ showFormsModal: status })
+  }
+
+  getDataTransferItems(event) {
+    let dataTransferItemsList = []
+
+    if (event.dataTransfer) {
+      const dt = event.dataTransfer
+
+      if (dt.files && dt.files.length) {
+        dataTransferItemsList = dt.files
+      } else if (dt.items && dt.items.length) {
+        // During the drag even the dataTransfer.files is null
+        // but Chrome implements some drag store, which is accessible via dataTransfer.items
+        dataTransferItemsList = dt.items
+      }
+    } else if (event.target && event.target.files) {
+      dataTransferItemsList = event.target.files
+    }
+
+    // Convert from DataTransferItemsList to the native Array
+    return Array.prototype.slice.call(dataTransferItemsList)
+  }
+
+  create = async (evt) => {
+    const {
+      createFormTask,
+      setSelectedTask,
+      listId,
+      deal,
+      setUploadFiles,
+      notify
+    } = this.props
+    const {
+      taskName,
+      isCreatingTask
+    } = this.state
+    const files = this.getDataTransferItems(evt)
+
+    if (isCreatingTask || taskName.length === 0 || files.length === 0) {
+      return false
+    }
+
+    // change status
+    this.setState({ isCreatingTask: true })
+
+    try {
+      // create task
+      const task = await createFormTask(deal.id, null, taskName, listId)
+
+      // make task active
+      setSelectedTask(task)
+
+      // upload file
+      setUploadFiles(files, deal, task)
+    } catch (e) {
+      notify({
+        message: e.message,
+        status: 'error'
+      })
+    } finally {
+      this.setState({
+        showFormsModal: false,
+        showTaskNameModal: false,
+        isCreatingTask: false
+      })
+    }
   }
 
   render() {
     const {
-      showFormsModal
+      showFormsModal,
+      showTaskNameModal,
+      isCreatingTask
     } = this.state
     const {
       deal,
       listId
     } = this.props
-
+    console.table({
+      listId,
+      showFormsModal,
+      showTaskNameModal,
+      isCreatingTask
+    })
     return (
       <div
-        className="add-task form-task"
-        onClick={() => this.displayForm(true)}
       >
-        <div className="icon">
-          <img
-            className="img-add"
-            src="/static/images/deals/plus.svg"
-          />
-        </div>
+        <div
+          className="add-task form-task"
+          onClick={() => this.displayForm(true)}
+        >
+          <div className="icon">
+            <img
+              className="img-add"
+              src="/static/images/deals/plus.svg"
+            />
+          </div>
 
-        <div className="title">
-          Add checklist item
+          <div className="title">
+            Add checklist item
+          </div>
         </div>
         <Forms
           show={showFormsModal}
+          displayTaskName={() => this.setState({
+            showTaskNameModal: true,
+            showFormsModal: false
+          })}
           deal={deal}
           listId={listId}
           onNew
           onClose={() => this.displayForm(false)}
+        />
+        <TaskName
+          show={showTaskNameModal}
+          onClose={() => this.setState({ showTaskNameModal: false })}
+          isCreatingTask={isCreatingTask}
+          addTaskName={(taskName) => {
+            this.setState({ taskName })
+            this.formNewTaskFileInputEl.click()
+            console.log('taskName: ', taskName)
+          }}
+        />
+        <input
+          type="file"
+          style={{ display: 'none', height: 0, width: 0 }}
+          ref={input => this.formNewTaskFileInputEl = input}
+          onChange={this.create}
         />
       </div>
     )
   }
 }
 
-export default CreateForm
+export default connect(null,
+  {
+    createFormTask,
+    setSelectedTask,
+    setUploadFiles,
+    notify
+  }
+)(CreateForm)
