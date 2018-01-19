eaf4f21dd71f03e7c88dbc597605515ecbdb25bf
OneDev0411 <cryptoapple1109@gmail.com>
2018-01-19 23:28:48 +0330

    #492 Add webkit scrollbar to task detail

--- app/components/Pages/Dashboard/Deals/dashboard/task-detail/index.js
+++ app/components/Pages/Dashboard/Deals/dashboard/task-detail/index.js
@@ -8,12 +8,14 @@ import Comments from '../comments'
 import CommentInput from '../comments/input'
 
 const TaskManager = ({ deal, task }) => {
+  const isWebkit = 'WebkitAppearance' in document.documentElement.style
+
   if (!task) {
     return false
   }
 
   return (
-    <div className="full-width" data-simplebar>
+    <div className="full-width" data-simplebar={!isWebkit || null}>
       <Upload disableClick deal={deal} task={task}>
         <div className="scrollable" id="deals-task-scrollable">
           <Header deal={deal} key={task.id} task={task} />
