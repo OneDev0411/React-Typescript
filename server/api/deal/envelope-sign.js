ef37e67b68bb46d14df84b8875fe22f749d8910b
OneDev0411 <cryptoapple1109@gmail.com>
2018-01-21 15:36:58 +0330

    change sign envelope url

--- server/api/deal/envelope-sign.js
+++ server/api/deal/envelope-sign.js
@@ -1,23 +1,21 @@
 import Koa from 'koa'
+
 const router = require('koa-router')()
 const app = new Koa()
 
 const urlPattern = /(^|\s)((https?:\/\/)?[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)/gi
 
-router.get('/deals/envelope/:id/sign', async (ctx, next) => {
-
-  const { id } = ctx.params
+router.get('/deals/envelope/:id/sign/:recipientId', async (ctx, next) => {
+  const { id, recipientId } = ctx.params
 
   try {
-    await ctx
-      .fetch(`/envelopes/${id}/sign`)
-      .redirects(0)
+    await ctx.fetch(`/envelopes/${id}/sign/${recipientId}`).redirects(0)
 
     ctx.body = ''
-  }
-  catch(e) {
+  } catch (e) {
     if (e.status === 302) {
       const link = e.response.text.match(urlPattern)
+
       ctx.redirect(link)
     }
   }
