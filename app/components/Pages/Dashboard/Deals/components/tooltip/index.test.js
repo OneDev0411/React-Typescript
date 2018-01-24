48b2a379a9134d7f0d16bf2f7d61053c16488b20
OneDev0411 <cryptoapple1109@gmail.com>
2018-01-24 20:53:23 +0330

    tests: use describe function for all suits

--- app/components/Pages/Dashboard/Deals/components/tooltip/index.test.js
+++ app/components/Pages/Dashboard/Deals/components/tooltip/index.test.js
@@ -5,40 +5,42 @@ import { shallow, mount, render } from 'enzyme'
 import ReactTestUtils from 'react-dom/test-utils'
 import Tooltip from '.'
 
-test('Should contain Tooltip object', () => {
-  const Wrapper = (
-    <Tooltip caption="tooltip works">
-      <div>Hover Me!</div>
-    </Tooltip>
-  )
+describe('Test Tooltip component', () => {
+  test('Should contain Tooltip object', () => {
+    const Wrapper = (
+      <Tooltip caption="tooltip works">
+        <div>Hover Me!</div>
+      </Tooltip>
+    )
 
-  const wrapper = shallow(Wrapper)
+    const wrapper = shallow(Wrapper)
 
-  expect(wrapper.find('OverlayTrigger').length).to.equal(1)
-})
+    expect(wrapper.find('OverlayTrigger').length).to.equal(1)
+  })
 
-test('Should not contain Tooltip object when caption is not defined', () => {
-  const Wrapper = (
-    <Tooltip>
-      <div>Hover Me!</div>
-    </Tooltip>
-  )
+  test('Should not contain Tooltip object when caption is not defined', () => {
+    const Wrapper = (
+      <Tooltip>
+        <div>Hover Me!</div>
+      </Tooltip>
+    )
 
-  const wrapper = shallow(Wrapper)
+    const wrapper = shallow(Wrapper)
 
-  expect(wrapper.find('OverlayTrigger').length).to.equal(0)
-})
+    expect(wrapper.find('OverlayTrigger').length).to.equal(0)
+  })
 
-test('Should render Tooltip', () => {
-  const Wrapper = (
-    <Tooltip caption="tooltip works">
-      <div>Hover Me!</div>
-    </Tooltip>
-  )
+  test('Should render Tooltip', () => {
+    const Wrapper = (
+      <Tooltip caption="tooltip works">
+        <div>Hover Me!</div>
+      </Tooltip>
+    )
 
-  const wrapper = shallow(Wrapper)
+    const wrapper = shallow(Wrapper)
 
-  const tooltip = shallow(wrapper.find('OverlayTrigger').prop('overlay'))
+    const tooltip = shallow(wrapper.find('OverlayTrigger').prop('overlay'))
 
-  expect(tooltip.find('.tooltip-inner').text()).to.equal('tooltip works')
+    expect(tooltip.find('.tooltip-inner').text()).to.equal('tooltip works')
+  })
 })
