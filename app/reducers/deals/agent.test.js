fce6ffc1cb687ef22a898a022abd46023e199085
OneDev0411 <cryptoapple1109@gmail.com>
2018-01-24 20:17:03 +0330

    tests: refactor agent reducer

--- app/reducers/deals/agent.test.js
+++ app/reducers/deals/agent.test.js
@@ -6,18 +6,24 @@ import fakeUser from '../../../tests/helpers/user/create-fake-user'
 import agent from '../../constants/deals/agent'
 
 describe('Test agent reducers in deal components', () => {
+  let agents
+
+  beforeEach(() => {
+    agents = [fakeUser('John', 'Doe'), fakeUser('John', 'Wick')]
+  })
+
   test('should return the initial state', () => {
     expect(reducer(undefined, {})).to.equal(null)
   })
 
   test('should handle GET_AGENTS', () => {
-    const agents = [fakeUser('John', 'Doe'), fakeUser('John', 'Wick')]
-
     expect(reducer(undefined, {
       type: types.GET_AGENTS,
       agents
     })).to.equal(agents)
+  })
 
+  test('should handle default state', () => {
     expect(reducer(agents, {})).to.equal(agents)
   })
 })
