ba33ba4b1f14c392d27e1a0c50059343082a26e3
OneDev0411 <cryptoapple1109@gmail.com>
2018-01-28 17:58:41 +0330

    Edit GL issue number.
    666 Added: SearchInput component

---
+++ app/components/Pages/Dashboard/Deals/components/AddToDealModal/components/SearchInput/index.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import SearchIcon from '../../../../../../../../views/components/SvgIcons/SearchIcon'
+
+const Input = styled.input`
+  width: 100%;
+  height: 42px;
+  padding: 0 16px 0 42px;
+  font-size: 1.5rem;
+  color: #8da2b5;
+  border-radius: 3px;
+  background-color: #f0f4f7;
+  border: solid 1px #dce5eb;
+
+  &:focus {
+    color: #333;
+    background-color: hsla(54, 100%, 62%, 0.15);
+    outline-width: 1px;
+  }
+
+  ::-webkit-input-placeholder {
+    /* Chrome/Opera/Safari */
+    color: #8da2b5;
+  }
+  ::-moz-placeholder {
+    /* Firefox 19+ */
+    color: #8da2b5;
+  }
+  :-ms-input-placeholder {
+    /* IE 10+ */
+    color: #8da2b5;
+  }
+  :-moz-placeholder {
+    /* Firefox 18- */
+    color: #8da2b5;
+  }
+`
+
+const propTypes = {
+  onChange: PropTypes.func.isRequired
+}
+
+function SearchInput({ onChange }) {
+  return (
+    <div style={{ position: 'relative' }}>
+      <Input
+        onChange={onChange}
+        placeholder="Enter a keyword for searching on contacts"
+      />
+      <SearchIcon
+        size={24}
+        color="#8DA2B5"
+        style={{
+          position: 'absolute',
+          top: '13px',
+          left: '13px',
+          width: '24px',
+          height: '24px'
+        }}
+      />
+    </div>
+  )
+}
+
+SearchInput.propTypes = propTypes
+
+export default SearchInput
