27fd999ddd8bd757c337eb0f963ea83218813410
OneDev0411 <cryptoapple1109@gmail.com>
2018-02-01 00:23:11 +0330

    a better fix for manual address

--- app/components/Pages/Dashboard/Deals/create/manual-address.js
+++ app/components/Pages/Dashboard/Deals/create/manual-address.js
@@ -24,10 +24,10 @@ export default class extends React.Component {
     }
 
     if (deal.listing) {
-      return deal.mls_context[field]
+      return deal.mls_context[field] || ''
     }
 
-    return Deal.get.field(deal, field)
+    return Deal.get.field(deal, field) || ''
   }
 
   onAdd() {
@@ -51,11 +51,10 @@ export default class extends React.Component {
       street_name, city, state, postal_code
     } = this.state
 
-    // use (variable || '') for backward compatibilites
     return (
-      (street_name || '').trim().length > 0 &&
-      (city || '').trim().length > 0 &&
-      (state || '').trim().length > 0 &&
+      street_name.trim().length > 0 &&
+      city.trim().length > 0 &&
+      state.trim().length > 0 &&
       /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(postal_code)
     )
   }
