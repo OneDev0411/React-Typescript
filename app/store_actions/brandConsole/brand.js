417b967d63e7242679cd1bdbf0179d76e9a6efdf
OneDev0411 <cryptoapple1109@gmail.com>
2018-02-03 14:09:49 +0330

    Linting brand action of brand console

--- app/store_actions/brandConsole/brand.js
+++ app/store_actions/brandConsole/brand.js
@@ -2,7 +2,6 @@ import types from '../../constants/brandConsole'
 import BrandConsole from '../../models/BrandConsole'
 import { addNotification as notify } from 'reapop'
 
-
 export function toggleBrand(brandId) {
   return {
     type: types.TOGGLE_BRAND,
@@ -27,53 +26,85 @@ function _getChildrenBrands(brandId, brands) {
 }
 
 export function getBrand(brandId) {
-  return async (dispatch) => {
+  return async dispatch => {
     dispatch({ type: types.SHOW_SPINNER })
+
     const response = await BrandConsole.getBrands(brandId)
+
     dispatch({ type: types.HIDE_SPINNER })
+
     if (response && !response.error) {
       const { data } = response.body
+
       dispatch(_getBrand(brandId, data))
     } else {
-      dispatch(notify({ message: `getBrands: ${response.error.message}`, status: 'error' }))
+      dispatch(
+        notify({
+          message: `getBrands: ${response.error.message}`,
+          status: 'error'
+        })
+      )
     }
   }
 }
 
 export function getChildrenBrands(brandId) {
-  return async (dispatch) => {
+  return async dispatch => {
     dispatch({ type: types.SHOW_SPINNER })
+
     const response = await BrandConsole.getChildrenBrands(brandId)
+
     dispatch({ type: types.HIDE_SPINNER })
+
     if (response && !response.error) {
       const { data } = response.body
+
       dispatch(_getChildrenBrands(brandId, data))
     } else {
-      dispatch(notify({ message: `getChildrenBrands: ${response.error.message}`, status: 'error' }))
+      dispatch(
+        notify({
+          message: `getChildrenBrands: ${response.error.message}`,
+          status: 'error'
+        })
+      )
     }
   }
 }
 
 export function addBrand(brand) {
-  return async (dispatch) => {
+  return async dispatch => {
     dispatch({ type: types.SHOW_SPINNER })
+
     const response = await BrandConsole.addBrand(brand)
+
     dispatch({ type: types.HIDE_SPINNER })
+
     if (response && !response.error) {
       const { data } = response.body
-      const responseAddRole = await BrandConsole.addRole(data.id, { role: 'Owner', acl: ['Deals', 'BackOffice', 'Admin'] })
+      const responseAddRole = await BrandConsole.addRole(data.id, {
+        role: 'Owner',
+        acl: ['Deals', 'BackOffice', 'Admin']
+      })
+
       if (responseAddRole) {
         const role = responseAddRole.body.data
-        dispatch(_getChildrenBrands(brand.parent,
-          [{
-            ...data,
-            roles: [role]
-          }]
-        ))
-      }
 
+        dispatch(
+          _getChildrenBrands(brand.parent, [
+            {
+              ...data,
+              roles: [role]
+            }
+          ])
+        )
+      }
     } else {
-      dispatch(notify({ message: `addBrand: ${response.error.message}`, status: 'error' }))
+      dispatch(
+        notify({
+          message: `addBrand: ${response.error.message}`,
+          status: 'error'
+        })
+      )
     }
   }
 }
@@ -86,14 +117,22 @@ function _deleteBrand(brand) {
 }
 
 export function deleteBrand(brand) {
-  return async (dispatch) => {
+  return async dispatch => {
     dispatch({ type: types.SHOW_SPINNER })
+
     const response = await BrandConsole.deleteBrand(brand.id)
+
     dispatch({ type: types.HIDE_SPINNER })
+
     if (response && !response.error && response.body.status === 'success') {
       dispatch(_deleteBrand(brand))
     } else {
-      dispatch(notify({ message: `deleteBrand: ${response.error.message}`, status: 'error' }))
+      dispatch(
+        notify({
+          message: `deleteBrand: ${response.error.message}`,
+          status: 'error'
+        })
+      )
     }
   }
 }
@@ -106,15 +145,24 @@ function _editBrand(brand) {
 }
 
 export function editBrand(brand) {
-  return async (dispatch) => {
+  return async dispatch => {
     dispatch({ type: types.SHOW_SPINNER })
+
     const response = await BrandConsole.editBrand(brand)
+
     dispatch({ type: types.HIDE_SPINNER })
+
     if (response && !response.error) {
       const { data } = response.body
+
       dispatch(_editBrand(data))
     } else {
-      dispatch(notify({ message: `addBrand: ${response.error.message}`, status: 'error' }))
+      dispatch(
+        notify({
+          message: `addBrand: ${response.error.message}`,
+          status: 'error'
+        })
+      )
     }
   }
 }
