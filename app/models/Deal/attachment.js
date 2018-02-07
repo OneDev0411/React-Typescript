3d8fc112938b653b8897b9215500c5ab862adcba
OneDev0411 <cryptoapple1109@gmail.com>
2018-02-07 19:20:27 +0330

    develop multi deletion attachments

--- app/models/Deal/attachment.js
+++ app/models/Deal/attachment.js
@@ -3,9 +3,11 @@ import Fetch from '../../services/fetch'
 /**
  * delete attachment
  */
-export async function deleteAttachment(roomId, fileId) {
+export async function deleteAttachment(roomId, files) {
   try {
-    await new Fetch().delete(`/rooms/${roomId}/attachments/${fileId}`)
+    await new Fetch()
+      .delete(`/rooms/${roomId}/attachments`)
+      .query({ 'id[]': [files] })
   } catch (e) {
     throw e
   }
