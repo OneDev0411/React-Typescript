31a7f90bb789877c443065523bd5673f93b756db
OneDev0411 <cryptoapple1109@gmail.com>
2018-02-07 20:13:41 +0330

    #718 Refactor recents page component

--- app/components/Pages/Dashboard/Chatroom/InstantChat/index.js
+++ app/components/Pages/Dashboard/Chatroom/InstantChat/index.js
@@ -1,34 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
 import ChatPopups from '../Popups'
 import Chatbar from '../Chatbar'
-import Chatroom from '..'
+import Chatroom from '../index'
 
-const InstantChat = ({
-  user,
-  chatroom
-}) => (
-  <div>
-    <Chatbar
-      user={user}
-      showChatbar={chatroom.showChatbar}
-    />
+const InstantChat = ({ user, chatroom }) => (
+  <Fragment>
+    <Chatbar user={user} showChatbar={chatroom.showChatbar} />
     <ChatPopups
       user={user}
       rooms={chatroom.rooms}
       instantMode={chatroom.instantMode}
     />
-    <div
-      className="chatroom-fullscreen"
-      style={!chatroom.instantMode ? { display: 'none' } : {}}
-    >
-      <Chatroom
-        user={user}
-        isInstant
-        activeRoom={chatroom.activeRoom}
-      />
-    </div>
-  </div>
+    <Chatroom user={user} isInstant />
+  </Fragment>
 )
 
 export default connect(({ chatroom }) => ({ chatroom }))(InstantChat)
