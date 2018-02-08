883459b560650c39d0cf32a0a82adec5e52438be
OneDev0411 <cryptoapple1109@gmail.com>
2018-02-08 10:16:52 +0330

    aaplause#705 Fixed [Deals] mls search modal search result title - undefined UNIT

--- app/components/Pages/Dashboard/Deals/components/listings-search.js
+++ app/components/Pages/Dashboard/Deals/components/listings-search.js
@@ -3,70 +3,67 @@ import _ from 'underscore'
 import { Row, Col } from 'react-bootstrap'
 import listingsHelper from '../../../../../utils/listing'
 
-export default ({
-  listings,
-  onSelectListing
-}) => {
+export default ({ listings, onSelectListing }) => {
   if (!listings) {
     return false
   }
 
-  const listview = _.filter(listings, item =>
-    item.status.startsWith('Active') || item.status === 'Pending'
+  const listview = _.filter(
+    listings,
+    item => item.status.startsWith('Active') || item.status === 'Pending'
   )
 
   return (
     <div>
-      {
-        _.size(listview) === 0 &&
+      {_.size(listview) === 0 && (
         <div className="empty-state">
-          <img
-            alt="home"
-            src="/static/images/deals/home-icon.svg"
-          />
+          <img alt="home" src="/static/images/deals/home-icon.svg" />
           No matches
         </div>
-      }
+      )}
 
-      {
-        _.map(listview, (item, key) => {
-          const c = item.address_components
+      {_.map(listview, (item, key) => {
+        const c = item.address_components
 
-          return (
-            <Row
-              key={`PLACE_${key}`}
-              className="item listing"
-              onClick={() => onSelectListing(item)}
-            >
-              <Col md={1} sm={1} xs={1}>
-                <img
-                  src={item.image || '/static/images/deals/home.svg'}
-                  className="listing-image"
-                />
-              </Col>
+        return (
+          <Row
+            key={`PLACE_${key}`}
+            className="item listing"
+            onClick={() => onSelectListing(item)}
+          >
+            <Col md={1} sm={1} xs={1}>
+              <img
+                src={item.image || '/static/images/deals/home.svg'}
+                className="listing-image"
+              />
+            </Col>
 
-              <Col md={7} sm={7} xs={7} className="address-col">
-                <span style={{ color: '#5b6469' }}>
-                  {c.street_number} {c.street_name} {c.street_suffix} Unit {c.unit_number}
-                </span>
+            <Col md={7} sm={7} xs={7} className="address-col">
+              <span style={{ color: '#5b6469' }}>
+                {c.street_number} {c.street_name} {c.street_suffix}
+                {c.unit_number ? ` Unit ${c.unit_number}` : ''}
+              </span>
 
-                <span className="addr">
-                  { c.city }, {c.state}, {c.postal_code}, ${item.price}
-                </span>
-              </Col>
+              <span className="addr">
+                {c.city}, {c.state}, {c.postal_code}, ${item.price}
+              </span>
+            </Col>
 
-              <Col md={4} sm={4} xs={4}>
-                <span
-                  className="status"
-                  style={{ backgroundColor: listingsHelper.getStatusColorClass(item.status) }}
-                >
-                  { item.status }
-                </span>
-              </Col>
-            </Row>
-          )
-        })
-      }
+            <Col md={4} sm={4} xs={4}>
+              <span
+                className="status"
+                style={{
+                  backgroundColor: listingsHelper.getStatusColorClass(
+                    item.status
+                  )
+                }}
+              >
+                {item.status}
+              </span>
+            </Col>
+          </Row>
+        )
+      })}
     </div>
   )
 }
