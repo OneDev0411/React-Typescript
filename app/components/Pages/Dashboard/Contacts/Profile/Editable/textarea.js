ce36f1879ca5a146dc112360f9f23a9b07d02fa0
OneDev0411 <cryptoapple1109@gmail.com>
2018-02-08 03:55:31 +0330

    #709 Added [Contact - Profile] Names section

--- app/components/Pages/Dashboard/Contacts/Profile/Editable/textarea.js
+++ app/components/Pages/Dashboard/Contacts/Profile/Editable/textarea.js
@@ -1,19 +1,12 @@
 import React from 'react'
 
-export default ({
-  value,
-  placeholder,
-  lines,
-  inputRef,
-  onChange,
-  onBlur
-}) => (
+export default ({ value, placeholder, lines, inputRef, onChange, onBlur }) => (
   <textarea
     value={value}
+    ref={inputRef}
+    onBlur={onBlur}
+    onChange={onChange}
     placeholder={placeholder}
     style={{ height: `${lines * 23}px` }}
-    ref={inputRef}
-    onChange={onChange}
-    onBlur={onBlur}
   />
 )
