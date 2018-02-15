9b0bbd1f197ea05b0019aad952bb02738ff08718
OneDev0411 <cryptoapple1109@gmail.com>
2018-02-15 12:47:32 +0330

    #753 Add submitted at column to agent deals dashboard

--- app/components/Pages/Dashboard/Deals/list/agent-table.js
+++ app/components/Pages/Dashboard/Deals/list/agent-table.js
@@ -9,6 +9,7 @@ import {
   closeEsignWizard,
   setSelectedTask
 } from '../../../../../store_actions/deals'
+import moment from 'moment/moment'
 
 class AgentTable extends BaseTable {
   constructor(props) {
@@ -50,20 +51,26 @@ class AgentTable extends BaseTable {
         sortable: true,
         className: 'col-md-2 hidden-xs',
         getText: deal =>
-          Deal.get.formattedPrice(Deal.get.field(deal, 'list_price'), 'currency', 0),
+          Deal.get.formattedPrice(
+            Deal.get.field(deal, 'list_price'),
+            'currency',
+            0
+          ),
         getValue: deal => Deal.get.field(deal, 'list_price')
       },
       side: {
         caption: 'SIDE',
         sortable: true,
-        className: 'col-md-2 hidden-sm hidden-xs',
-        getText: (deal, rowId, rowsCount) => this.getSide(deal, rowId, rowsCount),
+        className: 'col-md-1 hidden-sm hidden-xs',
+        getText: (deal, rowId, rowsCount) =>
+          this.getSide(deal, rowId, rowsCount),
         getValue: deal => deal.deal_type.toString() + this.getRoleNames(deal)
       },
       critical_dates: {
         caption: 'CRITICAL DATES',
         className: 'col-md-2 hidden-sm hidden-xs',
-        getText: (deal, rowId, rowsCount) => this.getNextDate(deal, rowId, rowsCount)
+        getText: (deal, rowId, rowsCount) =>
+          this.getNextDate(deal, rowId, rowsCount)
       },
       notificiation: {
         caption: '',
@@ -74,6 +81,19 @@ class AgentTable extends BaseTable {
         caption: '',
         justFilter: true,
         getValue: deal => deal.searchResult
+      },
+      attention_requested_at: {
+        caption: 'SUBMITTED AT',
+        className: 'col-md-1 hidden-sm hidden-xs',
+        sortable: true,
+        getValue: deal => deal.attention_requested_at,
+        getText: deal =>
+          deal.attention_requested_at
+            ? moment
+                .unix(deal.attention_requested_at)
+                .utc()
+                .format('MMM DD, YYYY')
+            : ''
       }
     }
   }
@@ -108,7 +128,9 @@ class AgentTable extends BaseTable {
                   <div key={`ROLE_${role.id}`} className="item">
                     <div className="avatar">
                       <UserAvatar
-                        name={`${role.legal_first_name} ${role.legal_last_name}`}
+                        name={`${role.legal_first_name} ${
+                          role.legal_last_name
+                        }`}
                         image={role.user ? role.user.profile_image_url : null}
                         size={26}
                         showStateIndicator={false}
@@ -121,7 +143,9 @@ class AgentTable extends BaseTable {
 
                       <span className="role">{roleName(role.role)}</span>
 
-                      {role.user && <span className="email">{role.user.email}</span>}
+                      {role.user && (
+                        <span className="email">{role.user.email}</span>
+                      )}
                     </div>
                   </div>
                 )
