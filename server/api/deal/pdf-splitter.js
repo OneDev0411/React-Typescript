670e181570dd4baba13e939b35dad8f597c2292e
OneDev0411 <cryptoapple1109@gmail.com>
2018-02-19 02:36:34 +0330

    reimplement splitter server to use memory instead of writing file on disk

--- server/api/deal/pdf-splitter.js
+++ server/api/deal/pdf-splitter.js
@@ -1,40 +1,40 @@
 import Koa from 'koa'
+import config from '../../../config/private'
+import Promise from 'bluebird'
+import memoryStream from 'memory-streams'
+import agent from 'superagent'
+import request from 'request'
+import bodyParser from 'koa-bodyparser'
+import _ from 'underscore'
+import scissors from 'scissors'
+import { PassThrough } from 'stream'
+import FakeStream from '../../util/fake-stream'
 
-const FakeStream = require('../../util/fake-stream')
+const promisifiedRequest = Promise.promisifyAll(request)
 const router = require('koa-router')()
-const memoryStream = require('memory-streams')
-const PassThrough = require('stream').PassThrough
-const agent = require('superagent')
-const fileParser = require('async-busboy')
-const _ = require('underscore')
-const fs = require('fs')
-const scissors = require('scissors')
-const uuid = require('../../../app/utils/uuid').default
 const app = new Koa()
 
-function splitFiles(splits, filepath) {
+function splitFiles(splits) {
   return new Promise((resolve, reject) => {
-    const writeStream = fs
-      .createWriteStream(filepath)
-      .on('error', e => reject(e))
+    const stream = new memoryStream.WritableStream()
 
     return scissors
       .join(...splits)
       .pdfStream()
-      .pipe(writeStream)
+      .pipe(stream)
       .on('error', e => {
         console.log(e)
         reject(e)
       })
-      .on('finish', resolve)
+      .on('finish', () => resolve(stream))
   })
 }
 
 async function downloadFiles(files) {
   return Promise.all(
-    _.map(JSON.parse(files), async file => {
+    _.map(files, async file => {
       const stream = new memoryStream.ReadableStream()
-      const { body } = await agent.get(file.object.url).buffer()
+      const { body } = await agent.get(file.url).buffer()
 
       stream.append(body)
 
@@ -46,22 +46,8 @@ async function downloadFiles(files) {
   )
 }
 
-async function getFiles(files) {
-  _.each(files, file => {
-    if (!fs.existsSync(file.path)) {
-      throw new Error(
-        `File ${file.filename} is not uploaded correctly, try again.`
-      )
-    }
-  })
-
-  return files
-}
-
-router.post('/deals/pdf-splitter', async ctx => {
-  const parser = await fileParser(ctx.req)
-  const pages = JSON.parse(parser.fields.pages)
-  const { title, room_id, task_id } = parser.fields
+router.post('/deals/pdf-splitter', bodyParser(), async ctx => {
+  const { pages, files, title, room_id, task_id } = ctx.request.body
   const { user } = ctx.session
 
   if (!user) {
@@ -84,17 +70,9 @@ router.post('/deals/pdf-splitter', async ctx => {
   }
 
   try {
-    const filepath = `/tmp/${uuid()}.pdf`
+    const downloadedFiles = await downloadFiles(files)
 
-    let files
-
-    if (parser.files.length > 0) {
-      files = await getFiles(parser.files)
-    } else {
-      files = await downloadFiles(parser.fields.files)
-    }
-
-    const splits = _.map(files, file => {
+    const splits = _.map(downloadedFiles, file => {
       const selectedPages = _.chain(pages)
         .filter(doc => `${doc.documentId}.pdf` === file.filename)
         .pluck('pageNumber')
@@ -108,14 +86,32 @@ router.post('/deals/pdf-splitter', async ctx => {
     }
 
     // split files
-    await splitFiles(splits, filepath)
+    const splittedFileStream = await splitFiles(splits)
 
-    const response = await ctx
-      .fetch(`/rooms/${room_id}/attachments`, 'POST')
-      .set('Authorization', `Bearer ${user.access_token}`)
-      .attach('attachment', filepath, `${title}.pdf`)
+    const response = await promisifiedRequest.postAsync({
+      url: `${config.api.url}/rooms/${room_id}/attachments`,
+      json: true,
+      headers: {
+        // 'Content-Type': 'application/json',
+        'Content-Type': 'multipart/form-data',
+        Authorization: `Bearer ${user.access_token}`
+      },
+      formData: {
+        file: {
+          value: splittedFileStream.toBuffer(),
+          options: {
+            filename: `${title}.pdf`,
+            contentType: 'application/pdf'
+          }
+        }
+      }
+    })
 
     // response is a file object
+    if (response.statusCode !== 200) {
+      throw new Error('Server Error, Try Again.')
+    }
+
     const file = response.body.data
 
     await ctx
@@ -126,16 +122,6 @@ router.post('/deals/pdf-splitter', async ctx => {
         room: room_id,
         attachments: [file.id]
       })
-
-    // cleanup !
-    // fs.unlink(filepath, () => null)
-    // _.each(files, file => {
-    //   if (typeof file.path === 'object') {
-    //     return true
-    //   }
-
-    //   fs.unlink(file.path, () => null)
-    // })
 
     finishStream({ success: true, file })
   } catch (e) {
