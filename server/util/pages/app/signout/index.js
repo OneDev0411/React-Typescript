e0a633ea8cc13f0dcf17adc009e0832bb8fd8d3e
OneDev0411 <cryptoapple1109@gmail.com>
2018-02-21 02:50:35 +0330

    add more control on session expiration

--- server/util/pages/app/signout/index.js
+++ server/util/pages/app/signout/index.js
@@ -1,12 +1,15 @@
 import Koa from 'koa'
+
 const router = require('koa-router')()
 const app = new Koa()
 
-router.get('/signout', async (ctx, next) => {
+router.get('/signout', async ctx => {
   ctx.session = null
+
   const { querystring } = ctx.request
   const { redirectFromSignout, redirect_to } = ctx.request.query
   let redirect = redirectFromSignout || redirect_to || '/signin'
+
   redirect += redirect === '/signin' && querystring ? `?${querystring}` : ''
   ctx.redirect(redirect)
 })
