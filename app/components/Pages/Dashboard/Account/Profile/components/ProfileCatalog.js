d9994a7903d46c07c634fb080d262d558655d9b9
OneDev0411 <cryptoapple1109@gmail.com>
2018-03-04 00:30:41 +0330

    web#816 fixed [accounts -> profile] null value personal info forms

--- app/components/Pages/Dashboard/Account/Profile/components/ProfileCatalog.js
+++ app/components/Pages/Dashboard/Account/Profile/components/ProfileCatalog.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import FormCard from './FormCard'
 import AvatarUploader from './AvatarUploader'
 
 const ProfileCatalog = ({ user }) => {
   const { first_name, last_name, email, phone_number } = user
-  const full_name = `${first_name} ${last_name}`
+  const full_name = [first_name, last_name].join(' ').trim()
 
   return (
     <div className="c-profile-catalog">
@@ -13,8 +12,12 @@ const ProfileCatalog = ({ user }) => {
         <AvatarUploader user={user} />
         <ul className="c-profile-catalog__list u-unstyled-list">
           <li className="c-profile-catalog__item--big">{full_name}</li>
-          <li className="c-profile-catalog__item--small">{phone_number}</li>
-          <li className="c-profile-catalog__item--small">{email}</li>
+          {full_name !== phone_number && (
+            <li className="c-profile-catalog__item--small">{phone_number}</li>
+          )}
+          {full_name !== email && (
+            <li className="c-profile-catalog__item--small">{email}</li>
+          )}
         </ul>
       </FormCard>
     </div>
--- app/components/Pages/Dashboard/Account/Profile/components/SimpleField.js
+++ app/components/Pages/Dashboard/Account/Profile/components/SimpleField.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
-const simpleField = ({ type, input, label, meta: { dirty, error } }) => {
+const simpleField = ({
+  type,
+  input,
+  label,
+  placeholder,
+  meta: { dirty, error }
+}) => {
   const hasError = dirty && error
 
   return (
@@ -12,6 +18,7 @@ const simpleField = ({ type, input, label, meta: { dirty, error } }) => {
         {...input}
         type={type}
         id={input.name}
+        placeholder={placeholder}
         className={`c-simple-field__input ${hasError ? 'has-error' : ''}`}
       />
       {hasError && <div className="c-auth__field__error-alert">{error}</div>}
