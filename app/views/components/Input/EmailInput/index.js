1411de1e3d36a7b81dc99f0b5ad1f27bde1b1afb
OneDev0411 <cryptoapple1109@gmail.com>
2018-03-05 02:29:41 +0330

    fix EmailInput

--- app/views/components/Input/EmailInput/index.js
+++ app/views/components/Input/EmailInput/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import emailMask from 'text-mask-addons/dist/emailMask'
 import Input from 'react-text-mask'
+import _ from 'underscore'
 
 export default props => (
   <Input
     placeholder="dan@rechat.com"
     mask={emailMask}
-    {...props}
+    {..._.omit(props, 'ErrorMessageHandler', 'data-type')}
     onChange={props.onChange}
   />
 )
