d78731e556c53b2bd65869f54d4e69342363bc14
OneDev0411 <cryptoapple1109@gmail.com>
2018-04-03 22:35:32 +0430

    applause#993 [CRM/TASKS/TASK] fixed: associations container in IE

--- app/views/CRM/Tasks/components/NewTask/components/Associations/index.js
+++ app/views/CRM/Tasks/components/NewTask/components/Associations/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { Field } from 'react-final-form'
 
@@ -31,123 +31,121 @@ function Associations({
       >
         Associated Records
       </div>
-      <div style={{ display: 'flex' }}>
-        <Field
-          name="associations"
-          id="new-task__associations"
-          render={({ input }) => {
-            const addHandler = async (object = {}, handleClose) => {
-              const { type } = object
+      <Field
+        name="associations"
+        id="new-task__associations"
+        render={({ input }) => {
+          const addHandler = async (object = {}, handleClose) => {
+            const { type } = object
 
-              if (!type) {
-                return
-              }
-
-              const isDuplicate = associations.some(
-                association =>
-                  association[type] && association[type].id === object.id
-              )
-
-              if (!isDuplicate) {
-                let nextAssociations
-
-                const newAssociation = await handleCreate({
-                  [type]: object.id,
-                  association_type: type
-                })
-
-                if (newAssociation) {
-                  nextAssociations = [
-                    ...associations,
-                    {
-                      ...newAssociation,
-                      [type]: object
-                    }
-                  ]
-                } else {
-                  nextAssociations = [
-                    ...associations,
-                    {
-                      [type]: object,
-                      association_type: type
-                    }
-                  ]
-                }
-
-                input.onChange(nextAssociations)
-                handleClose()
-              }
+            if (!type) {
+              return
             }
 
-            const removeHandler = async association => {
-              let newAssociations
-
-              if (association.id) {
-                await handleDelete(association.id)
-
-                newAssociations = associations.filter(
-                  a => a.id !== association.id
-                )
-              } else {
-                newAssociations = associations.filter(
-                  a =>
-                    a[a.association_type].id !==
-                    association[association.association_type].id
-                )
-              }
-
-              input.onChange(newAssociations)
-            }
-
-            const removable = id => {
-              if (!defaultAssociation) {
-                return true
-              }
-
-              const { association_type } = defaultAssociation
-
-              if (!association_type) {
-                return true
-              }
-
-              const record = defaultAssociation[association_type]
-
-              if (!record || !record.id || record.id !== id) {
-                return true
-              }
-
-              return false
-            }
-
-            return (
-              <div>
-                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-                  {associations.map(association => {
-                    if (!association || !association.association_type) {
-                      return null
-                    }
-
-                    const record = association[association.association_type]
-
-                    return (
-                      <Association
-                        record={record}
-                        key={record.id || record.title}
-                        removable={removable(record.id)}
-                        handleRemove={() => removeHandler(association)}
-                      />
-                    )
-                  })}
-                </div>
-                <div style={{ display: 'flex', marginBottom: '1em' }}>
-                  <AddContactAssociation handleAdd={addHandler} />
-                  <AddListingAssociation handleAdd={addHandler} />
-                </div>
-              </div>
+            const isDuplicate = associations.some(
+              association =>
+                association[type] && association[type].id === object.id
             )
-          }}
-        />
-      </div>
+
+            if (!isDuplicate) {
+              let nextAssociations
+
+              const newAssociation = await handleCreate({
+                [type]: object.id,
+                association_type: type
+              })
+
+              if (newAssociation) {
+                nextAssociations = [
+                  ...associations,
+                  {
+                    ...newAssociation,
+                    [type]: object
+                  }
+                ]
+              } else {
+                nextAssociations = [
+                  ...associations,
+                  {
+                    [type]: object,
+                    association_type: type
+                  }
+                ]
+              }
+
+              input.onChange(nextAssociations)
+              handleClose()
+            }
+          }
+
+          const removeHandler = async association => {
+            let newAssociations
+
+            if (association.id) {
+              await handleDelete(association.id)
+
+              newAssociations = associations.filter(
+                a => a.id !== association.id
+              )
+            } else {
+              newAssociations = associations.filter(
+                a =>
+                  a[a.association_type].id !==
+                  association[association.association_type].id
+              )
+            }
+
+            input.onChange(newAssociations)
+          }
+
+          const removable = id => {
+            if (!defaultAssociation) {
+              return true
+            }
+
+            const { association_type } = defaultAssociation
+
+            if (!association_type) {
+              return true
+            }
+
+            const record = defaultAssociation[association_type]
+
+            if (!record || !record.id || record.id !== id) {
+              return true
+            }
+
+            return false
+          }
+
+          return (
+            <Fragment>
+              <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                {associations.map(association => {
+                  if (!association || !association.association_type) {
+                    return null
+                  }
+
+                  const record = association[association.association_type]
+
+                  return (
+                    <Association
+                      record={record}
+                      key={record.id || record.title}
+                      removable={removable(record.id)}
+                      handleRemove={() => removeHandler(association)}
+                    />
+                  )
+                })}
+              </div>
+              <div style={{ display: 'flex', marginBottom: '1em' }}>
+                <AddContactAssociation handleAdd={addHandler} />
+                <AddListingAssociation handleAdd={addHandler} />
+              </div>
+            </Fragment>
+          )
+        }}
+      />
     </div>
   )
 }
