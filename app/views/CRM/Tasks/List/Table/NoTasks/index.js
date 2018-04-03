2edd507a35056cb56acec896d63b4d805c0802f3
OneDev0411 <cryptoapple1109@gmail.com>
2018-04-03 23:36:16 +0430

    web#1020 [CRM/TASKS/LIST] Added: NoTasks

---
+++ app/views/CRM/Tasks/List/Table/NoTasks/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { browserHistory } from 'react-router'
+import ActionButton from '../../../../../components/Button/ActionButton'
+import IconTodo from '../../../../../components/SvgIcons/Todo/IconTodo'
+
+export default function NoTasks() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <IconTodo style={{ width: 112, height: 112, fill: '#8DA2B5' }} />
+      <h2 style={{ color: '#62778c', margin: '0 0 0.5em' }}>
+        Looks like you don’t have any tasks.
+      </h2>
+      <ActionButton onClick={() => browserHistory.push('/crm/tasks/new')}>
+        New Task
+      </ActionButton>
+    </div>
+  )
+}
