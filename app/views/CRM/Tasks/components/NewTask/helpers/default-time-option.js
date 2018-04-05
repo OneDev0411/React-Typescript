cc4e4375ab0d83466bc1c9b791380a0dede20ed8
OneDev0411 <cryptoapple1109@gmail.com>
2018-04-05 16:24:28 +0430

    web#1032 [CRM/TASKS/EDIT TASK] fixed: accept custom time from ios

--- app/views/CRM/Tasks/components/NewTask/helpers/default-time-option.js
+++ app/views/CRM/Tasks/components/NewTask/helpers/default-time-option.js
@@ -1,27 +1,28 @@
+import fecha from 'fecha'
+
 import { getTime } from '../../../../../../utils/get-time'
 import { getTimes } from '../../../../../../utils/get-times'
 
 /**
  * Generate Default Time optopn for dropdown
- * @param {Date} timestamp The Original date in timestamp
+ * @param {Date} date The Original date in timestamp
  * @param {string} defaultTime fallback time
  * @returns {object} a time option
  */
-export function defaultTimeOption(timestamp, defaultTime) {
+export function defaultTimeOption(date, defaultTime) {
   const times = {}
 
-  if (
-    timestamp &&
-    typeof timestamp === 'number' &&
-    new Date(timestamp) !== 'Invalid Date'
-  ) {
+  if (date && typeof date === 'number' && new Date(date) !== 'Invalid Date') {
     getTimes().forEach(time => (times[time.value] = time))
 
-    const time = times[getTime(timestamp)]
+    const value = getTime(date)
+    let time = times[value]
 
     if (time) {
       return time
     }
+
+    return { title: fecha.format(new Date(date), 'hh:mm A'), value }
   }
 
   if (defaultTime) {
