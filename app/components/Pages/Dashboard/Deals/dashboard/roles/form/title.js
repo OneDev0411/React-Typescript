e52e73dda30dd93638387f94955a27909663ac4f
OneDev0411 <cryptoapple1109@gmail.com>
2018-04-08 17:23:53 +0430

    Applause#1010: In add role modal, show error for invalid prefix

--- app/components/Pages/Dashboard/Deals/dashboard/roles/form/title.js
+++ app/components/Pages/Dashboard/Deals/dashboard/roles/form/title.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Dropdown, MenuItem } from 'react-bootstrap'
 
-export default ({
-  form,
-  onChange
-}) => (
-  <div className="role-title">
+export default ({ form, onChange, isInvalid }) => (
+  <div className="role-title" style={{ position: 'relative' }}>
+    {isInvalid && (
+      <span
+        data-balloon-visible
+        data-balloon-pos="up"
+        data-balloon-length="large"
+        className="c-field-balloon c-field-balloon--error"
+        data-balloon="Please select the prefix from the drop-down."
+      />
+    )}
     <label>Title</label>
     <div>
       <Dropdown id="deal-add-role-title--drp" bsStyle="default">
@@ -14,21 +20,17 @@ export default ({
         </Dropdown.Toggle>
 
         <Dropdown.Menu className="deal-add-role-title--drpmenu">
-          {
-            ['Mr', 'Mrs', 'Miss', 'Ms', 'Dr']
-            .map((name, key) =>
-              <MenuItem
-                key={`Title_${key}`}
-                style={{ width: '40%' }}
-                onClick={() => onChange(name)}
-              >
-                {name}
-              </MenuItem>
-            )
-          }
+          {['Mr', 'Mrs', 'Miss', 'Ms', 'Dr'].map((name, key) => (
+            <MenuItem
+              key={`Title_${key}`}
+              style={{ width: '40%' }}
+              onClick={() => onChange(name)}
+            >
+              {name}
+            </MenuItem>
+          ))}
         </Dropdown.Menu>
       </Dropdown>
     </div>
   </div>
 )
-
