11f1334244e7879a83ebaf091fad9541f820cd8c
OneDev0411 <cryptoapple1109@gmail.com>
2018-04-14 12:16:40 +0430

    web#1060 [CRM/CONTACTS/ACTIONS] added: contact attribute normaliz helper

---
+++ app/store_actions/contacts/helpers/normalize-contacts.js
@@ -0,0 +1,40 @@
+import _ from 'underscore'
+import { normalize } from 'normalizr'
+import { contactsSchema } from '../../../models/contacts/schema'
+
+function normalizeContactAttribute(response) {
+  const { data, contact_attribute_defs } = response
+  const indexedAttrbuteDefs = _.indexBy(contact_attribute_defs, 'id')
+  const contacts = Array.isArray(data) ? data : [data]
+
+  return contacts.map(item => {
+    const subContacts = item.sub_contacts.map(sub => {
+      const { attributes } = sub
+      const newAttributes = _.chain(attributes)
+        .map(attr => ({
+          ...attr,
+          attribute_def: indexedAttrbuteDefs[attr.attribute_def]
+        }))
+        .groupBy(attribute => attribute.attribute_def.section)
+        .value()
+
+      return {
+        ...sub,
+        attributes: newAttributes
+      }
+    })
+
+    return {
+      ...item,
+      sub_contacts: subContacts
+    }
+  })
+}
+
+export function normalizeContacts(response) {
+  const contacts = {
+    contacts: normalizeContactAttribute(response)
+  }
+
+  return normalize(contacts, contactsSchema)
+}
