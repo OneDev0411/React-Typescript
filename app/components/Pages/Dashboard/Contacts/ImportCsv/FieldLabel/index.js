1a3964e24c64a27366bb2b8dc8f774e2d895422e
OneDev0411 <cryptoapple1109@gmail.com>
2018-05-08 12:53:06 +0000

    #1120 refactor contacts model  import csv

--- app/components/Pages/Dashboard/Contacts/ImportCsv/FieldLabel/index.js
+++ app/components/Pages/Dashboard/Contacts/ImportCsv/FieldLabel/index.js
@@ -1,66 +1,109 @@
 import React, { Fragment } from 'react'
+import { connect } from 'react-redux'
 import Select from 'react-select'
 import cn from 'classnames'
+import _ from 'underscore'
+import { isAddressField } from '../helpers/address'
 
-const AddressLabelOptions = [
-  { label: 'Home Address', value: 'Home' },
-  { label: 'Work Address', value: 'Work' },
-  { label: 'Other Address', value: 'Other' }
-]
-
-const LabelOptions = {
-  email: [
-    { label: 'Personal Email', value: 'Personal' },
-    { label: 'Work Email', value: 'Work' },
-    { label: 'Other Email', value: 'Email' }
-  ],
-  phone_number: [
-    { label: 'Mobile Phone', value: 'Mobile' },
-    { label: 'Home Phone', value: 'Home' },
-    { label: 'Work Phone', value: 'Work' },
-    { label: 'Main Phone', value: 'Main' },
-    { label: 'Other Phone', value: 'Other' }
-  ],
-  website: [
-    { label: 'Website', value: 'Website' },
-    { label: 'Instagram', value: 'Instagram' },
-    { label: 'Facebook', value: 'Facebook' },
-    { label: 'Twitter', value: 'Twitter' },
-    { label: 'Blog', value: 'Blog' }
-  ],
-  street_number: AddressLabelOptions,
-  street_prefix: AddressLabelOptions,
-  street_name: AddressLabelOptions,
-  street_suffix: AddressLabelOptions,
-  unit_number: AddressLabelOptions,
-  city: AddressLabelOptions,
-  state: AddressLabelOptions,
-  country: AddressLabelOptions,
-  postal_code: AddressLabelOptions
+/**
+ * returns all options
+ * @param {Array} labels - all possible options of selected field
+ */
+function getAllOptions(labels) {
+  return labels.map(name => ({
+    value: name,
+    label: name
+  }))
 }
 
-const FieldLabel = ({ fieldValue, contactField, fieldName, onChange }) => {
+/**
+ * returns possible options of this field.
+ * this function avoids selecting * multiple fields with same index
+ * @param {Array} labels - all possible options of selected field
+ * @param {Object} attributes - list of attribute definitions
+ * @param {Object} mappedFields - list of mapped fields
+ * @param {Integer} definitionId, definition id of selected field
+ * @param {Integer} index, index id of selected field
+ */
+function getPossibleOptions(
+  labels,
+  attributes,
+  mappedFields,
+  definitionId,
+  index
+) {
+  if (!isAddressField(attributes, definitionId)) {
+    return getAllOptions(labels)
+  }
+
+  const field = _.find(
+    mappedFields,
+    field =>
+      field.label &&
+      isAddressField(attributes, field.definitionId) &&
+      field.index === index
+  )
+
+  if (field) {
+    return [
+      {
+        value: field.label,
+        label: field.label
+      }
+    ]
+  }
+
+  return getAllOptions(labels)
+}
+
+const FieldLabel = ({
+  columnName,
+  labels,
+  fieldName,
+  onChange,
+  attributes,
+  mappedFields
+}) => {
+  const { definitionId, index, label: selectedLabel } = mappedFields[columnName]
+
   const handleChangeValue = data => {
     const value = data ? data.value : null
 
-    if (value !== fieldValue) {
+    if (value !== selectedLabel) {
       onChange(fieldName, value)
     }
   }
 
+  const options = getPossibleOptions(
+    labels,
+    attributes,
+    mappedFields,
+    definitionId,
+    index
+  )
+
   return (
     <Fragment>
       <Select
-        className={cn({ isInvalid: !fieldValue })}
+        className={cn({ isInvalid: !selectedLabel })}
         name="form-field-label"
         placeholder="Select Label"
-        value={fieldValue}
+        value={selectedLabel}
         onChange={handleChangeValue}
-        options={LabelOptions[contactField]}
+        options={options}
       />
-      {!fieldValue && <span className="required">Required</span>}
+      {!selectedLabel && <span className="required">Required</span>}
     </Fragment>
   )
 }
 
-export default FieldLabel
+function mapStateToProps({ contacts }) {
+  const { importCsv, attributeDefs } = contacts
+
+  return {
+    attributes: attributeDefs,
+    mappedFields: importCsv.mappedFields
+  }
+}
+
+export default connect(mapStateToProps)(FieldLabel)
