fbec63c35674b0aa918f684223d41d848d2ea8b6
OneDev0411 <cryptoapple1109@gmail.com>
2018-05-09 18:35:37 +0430

    develop dynamic labels

--- app/components/Pages/Dashboard/Contacts/Profile/Details/fields/Websites.js
+++ app/components/Pages/Dashboard/Contacts/Profile/Details/fields/Websites.js
@@ -1,52 +1,66 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import MultiFields from '../components/MultiFields'
 
-const DEFAULT_LABELS = {
-  default: {
-    title: 'Website',
-    icon: {
-      name: 'briefcase',
-      color: '#15bd6b'
-    }
-  },
-  instagram: {
-    title: 'Instagram',
-    icon: {
-      name: 'instagram',
-      color: '#EC3661'
-    }
-  },
-  facebook: {
-    title: 'Facebook',
-    icon: {
-      color: '#4367B2',
-      name: 'facebook-square'
-    }
-  },
-  twitter: {
-    title: 'Twitter',
-    icon: {
-      name: 'twitter-square',
-      color: '#1da1f2'
-    }
-  },
-  blog: {
-    title: 'Blog',
-    icon: {
-      name: 'wordpress',
-      color: '#446'
-    }
-  }
-}
+import { getWebsiteLabels } from '../../../../../../../models/contacts/helpers/get-attribute-labels'
+import { selectDefinitionByName } from '../../../../../../../reducers/contacts/attributeDefs'
 
-export default function Websites({ contact }) {
+// const DEFAULT_LABELS = {
+//   default: {
+//     title: 'Website',
+//     icon: {
+//       name: 'briefcase',
+//       color: '#15bd6b'
+//     }
+//   },
+//   instagram: {
+//     title: 'Instagram',
+//     icon: {
+//       name: 'instagram',
+//       color: '#EC3661'
+//     }
+//   },
+//   facebook: {
+//     title: 'Facebook',
+//     icon: {
+//       color: '#4367B2',
+//       name: 'facebook-square'
+//     }
+//   },
+//   twitter: {
+//     title: 'Twitter',
+//     icon: {
+//       name: 'twitter-square',
+//       color: '#1da1f2'
+//     }
+//   },
+//   blog: {
+//     title: 'Blog',
+//     icon: {
+//       name: 'wordpress',
+//       color: '#446'
+//     }
+//   }
+// }
+
+function Websites({ contact, attributeDef }) {
+  getWebsiteLabels(attributeDef)
+
   return (
     <MultiFields
       attributeName="website"
       contact={contact}
-      defaultLabels={DEFAULT_LABELS}
+      defaultLabels={getWebsiteLabels(attributeDef)}
       placeholder="rechat.com"
     />
   )
 }
+
+function mapStateToProps({ contacts }) {
+  return {
+    attributeDef: selectDefinitionByName(contacts.attributeDefs, 'website')
+  }
+}
+
+export default connect(mapStateToProps)(Websites)
