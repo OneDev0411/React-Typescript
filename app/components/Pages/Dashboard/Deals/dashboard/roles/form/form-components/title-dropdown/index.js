d2389d45d85aea04919041f1f4925169c488616c
OneDev0411 <cryptoapple1109@gmail.com>
2018-05-10 00:41:53 +0430

    refactor tile and remove unused codes

--- app/components/Pages/Dashboard/Deals/dashboard/roles/form/form-components/title-dropdown/index.js
+++ app/components/Pages/Dashboard/Deals/dashboard/roles/form/form-components/title-dropdown/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Select from 'react-select'
-import { InputContainer, InputLabel, InputRequired } from '../../styles'
+import { InputContainer, InputLabel } from '../../styles'
 
 const OPTIONS = ['Mr', 'Ms', 'Mrs', 'Miss', 'Dr']
 
-export const TitleDropDown = ({ input, placeholder, isRequired }) => (
+export const TitleDropDown = ({ input, placeholder }) => (
   <InputContainer>
     <InputLabel>{placeholder}</InputLabel>
     <Select
       className="deals__role-form--select"
+      searchable={false}
       clearable={false}
       placeholder="Select a title"
       value={input.value}
