f07acc14956c79a82b68f484d230fdcdbe841266
OneDev0411 <cryptoapple1109@gmail.com>
2018-05-10 21:44:48 +0430

    web#1193 [CONTACTS] removed: job title validation

--- app/components/Pages/Dashboard/Contacts/Profile/Details/fields/JobTitles.js
+++ app/components/Pages/Dashboard/Contacts/Profile/Details/fields/JobTitles.js
@@ -3,23 +3,11 @@ import React from 'react'
 import MultiFields from '../components/MultiFields'
 
 export default function Jobs({ contact }) {
-  const validator = text => {
-    /*
-     * Not Allowing Special Characters
-     * Validate the text that contains special characters.
-    */
-    const regular = /\`|\~|\d|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\+|\=|\[|\{|\]|\}|\||\\|\'|\<|\,|\.|\>|\?|\/|\""|\;|\:/
-
-    return !new RegExp(regular).exec(text)
-  }
-
   return (
     <MultiFields
       attributeName="job_title"
       contact={contact}
       placeholder="Lawyer"
-      validator={validator}
-      validationText="Please include only letters and space. You have added a number or special character."
     />
   )
 }
