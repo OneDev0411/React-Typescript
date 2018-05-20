0d224ab4949f8326cd556baa2c872355860bcac9
OneDev0411 <cryptoapple1109@gmail.com>
2018-05-20 17:01:54 +0430

    web#1234 [MODELS/CONTACTS] fixed: search query params type

--- app/models/contacts/search-contacts/index.js
+++ app/models/contacts/search-contacts/index.js
@@ -2,15 +2,21 @@ import Fetch from '../../../services/fetch'
 import { defaultQuery } from '../helpers/default-query'
 import { normalizeContactAttribute } from '../../../store_actions/contacts/helpers/normalize-contacts'
 
-export async function searchContacts(keyword, query = defaultQuery) {
-  if (!keyword) {
-    throw new Error('Keyword is required for query!')
+export async function searchContacts(filter, query = defaultQuery) {
+  if (!filter || typeof filter !== 'string') {
+    throw new Error(`filter value is ${filter}.`)
   }
+
+  const keywords = query
+    .trim()
+    .split(' ')
+    .map(i => `q[]=${i}`)
+    .join('&')
 
   try {
     const response = await new Fetch()
       .post('/contacts/filter')
-      .query({ 'q[]': keyword })
+      .query(keywords)
       .query(query)
 
     return normalizeContactAttribute(response.body)
