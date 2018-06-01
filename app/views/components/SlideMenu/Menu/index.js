ad7c0a76baad2708685c8cdc7a0d053e0d43fbfa
OneDev0411 <cryptoapple1109@gmail.com>
2018-06-01 17:11:06 +0430

    update SlideMenu to support transition

--- app/views/components/SlideMenu/Menu/index.js
+++ app/views/components/SlideMenu/Menu/index.js
@@ -3,11 +3,12 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   background-color: #f8fafb;
-  box-shadow: -1px 0 0 0 rgba(0, 0, 0, 0.09);
-  width: 180px;
-  display: ${props => (props.isOpen ? 'block' : 'none')};
+  box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.09);
+  width: ${props => (props.isOpen ? 180 : 0)}px;
+  opacity: ${props => (props.isOpen ? 1 : 0)};
   max-height: 100vh;
   overflow: auto;
+  transition: 0.5s all ease-in;
 `
 
 export const Menu = ({ children, isOpen }) => (
