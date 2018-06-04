29cfc8bb20c682ae429c7034c435abadd7d9e37e
OneDev0411 <cryptoapple1109@gmail.com>
2018-06-04 17:58:29 +0430

    update menu box shadow

--- app/views/components/SlideMenu/Menu/index.js
+++ app/views/components/SlideMenu/Menu/index.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   background-color: #f8fafb;
-  box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.09);
+  box-shadow: inset -1px 0 0 rgba(0, 0, 0, 0.09);
   width: ${props => (props.isOpen ? 180 : 0)}px;
   opacity: ${props => (props.isOpen ? 1 : 0)};
   max-height: 100vh;
