27e23872ea163e44b0945502935e2a15d2bb58e5
OneDev0411 <cryptoapple1109@gmail.com>
2018-06-04 14:41:03 +0430

    implement filter segments actions, reducers, models

---
+++ app/reducers/filter-segments/index.js
@@ -0,0 +1,86 @@
+import * as types from '../../constants/filter-segments'
+
+const initialState = {
+  list: [],
+  isFetching: false,
+  isFetched: false,
+  activeSegmentId: 'default',
+  fetchError: null
+}
+
+export const getDefaultList = name => ({
+  id: 'default',
+  editable: false,
+  name: `All ${name}`,
+  filters: []
+})
+
+const filterSegments = (state, action) => {
+  switch (action.type) {
+    case types.CHANGE_ACTIVE_FILTER_SEGMENT:
+      return {
+        ...state,
+        activeSegmentId: action.segmentId
+      }
+
+    case types.FETCH_FILTER_SEGMENTS:
+      return {
+        ...state,
+        isFetching: true
+      }
+
+    case types.FETCH_FILTER_SEGMENTS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        isFetched: true,
+        list: action.list
+      }
+
+    case types.FETCH_FILTER_SEGMENTS_FAILURE:
+      return {
+        ...state,
+        fetchError: action.error,
+        isFetching: false
+      }
+    case types.SAVE_FILTER_SEGMENTS:
+      return {
+        ...state,
+        isSaving: false,
+        list: {
+          ...state.list,
+          [action.list.id]: action.list
+        }
+      }
+
+    default:
+      return state
+  }
+}
+
+function createReducer(id) {
+  const createList = reducer => (state = initialState, action) => {
+    if (action && action.namespace === 'filter-segments' && action.id === id) {
+      return reducer(state, action)
+    }
+
+    return state
+  }
+
+  return createList(filterSegments)
+}
+
+export const contactsFilterSegments = createReducer('contacts')
+export const dealsFilterSegments = createReducer('deals')
+export const mlsFilterSegments = createReducer('mls')
+
+export const isFetchingSavedSegments = state => state.isFetching
+
+export const selectActiveSavedSegment = (state, listName = '') =>
+  (state.list && state.list[state.activeSegmentId]) || getDefaultList(listName)
+
+export const getSavedSegments = (state, listName) =>
+  [].concat([getDefaultList(listName)], Object.values(state.list))
+
+export const selectSavedSegmentById = (state, id) =>
+  state.list && state.list[id]
