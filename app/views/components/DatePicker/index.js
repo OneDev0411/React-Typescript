529ce0cd01106a7ed55a10b0d8a93b6fbb09bb49
OneDev0411 <cryptoapple1109@gmail.com>
2018-06-14 23:41:33 +0430

    develop DatePicker

--- app/views/components/DatePicker/index.js
+++ app/views/components/DatePicker/index.js
@@ -1,8 +1,122 @@
 import React from 'react'
-import DayPicker from 'react-day-picker'
+import PropTypes from 'prop-types'
+import fecha from 'fecha'
+
+import DayPicker, { DateUtils } from 'react-day-picker'
+import Toolbar from './Toolbar'
+
+import { Container, TodayButton } from './styled'
+
+const initialState = {
+  currentDate: new Date()
+}
 
 export default class DatePicker extends React.Component {
+  static propTypes = {
+    onChange: PropTypes.func.isRequired,
+    fixedWeeks: PropTypes.bool,
+    modifiers: PropTypes.object,
+    selectedDate: PropTypes.any
+  }
+
+  static defaultProps = {
+    fixedWeeks: false,
+    modifiers: {},
+    selectedDate: null
+  }
+
+  state = initialState
+
+  componentDidMount() {
+    this.setDate(this.props)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setDate(nextProps)
+  }
+
+  handleDateChange = (name, value) => {
+    const date = {
+      year: this.Date.getFullYear(),
+      month: this.Date.getMonth(),
+      day: 1,
+      [name]: value
+    }
+
+    this.setState(
+      {
+        currentDate: new Date(date.year, date.month, date.day)
+      },
+      this.onChange
+    )
+  }
+
+  setDate({ selectedDate }) {
+    const { currentDate } = this.state
+
+    if (selectedDate && selectedDate.toString() !== currentDate.toString()) {
+      this.setState({
+        currentDate: selectedDate
+      })
+    }
+  }
+
+  get Date() {
+    return this.state.currentDate
+  }
+
+  handleToday = () => this.setState(initialState, this.onChange)
+  handlePreviousMonth = () => this.addMonths(-1)
+  handleNextMonth = () => this.addMonths(1)
+  onChange = () => this.props.onChange(this.Date)
+
+  handleDayClick = currentDate =>
+    this.setState(
+      {
+        currentDate
+      },
+      this.onChange
+    )
+
+  addMonths = count =>
+    this.setState(
+      {
+        currentDate: DateUtils.addMonths(this.Date, count)
+      },
+      this.onChange
+    )
+
   render() {
-    return <DayPicker />
+    return (
+      <Container>
+        <DayPicker
+          month={this.Date}
+          selectedDays={this.Date}
+          onDayClick={this.handleDayClick}
+          fixedWeeks={false}
+          canChangeMonth={false}
+          modifiers={this.props.modifiers}
+          captionElement={({ date, localeUtils }) => (
+            <Toolbar
+              date={date}
+              year={this.Date.getFullYear()}
+              month={this.Date.getMonth()}
+              localeUtils={localeUtils}
+              onDateChange={this.handleDateChange}
+              onPreviousClick={this.handlePreviousMonth}
+              onNextClick={this.handleNextMonth}
+            />
+          )}
+        />
+        <TodayButton
+          inverse
+          onClick={this.handleToday}
+          data-balloon={fecha.format(new Date(), 'dddd, MMMM DD')}
+          data-balloon-pos="down"
+        >
+          Today
+        </TodayButton>
+      </Container>
+    )
   }
 }
