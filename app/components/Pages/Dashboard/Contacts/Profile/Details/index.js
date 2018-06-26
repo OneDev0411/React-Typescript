a9e5f3401359c772f8d121863471228b7f267f09
OneDev0411 <cryptoapple1109@gmail.com>
2018-06-26 19:34:18 +0430

    web#1302 [CONTACTS/PROFILE] refactor: contact info section

--- app/components/Pages/Dashboard/Contacts/Profile/Details/index.js
+++ app/components/Pages/Dashboard/Contacts/Profile/Details/index.js
@@ -1,24 +1,21 @@
 import React from 'react'
 
-import Emails from './fields/Emails'
-import Phones from './fields/Phones'
-import { Source } from './fields/Source'
-import Websites from './fields/Websites'
-import OrginalSource from './fields/OrginalSource'
+import SectionWithFields from '../components/SectionWithFields'
 
-export function ContactInfo({ contact }) {
+const fieldsOrder = [
+  'email',
+  'phone_number',
+  'source',
+  'website',
+  'source_type'
+]
+
+export function ContactInfo(props) {
   return (
-    <div className="c-contact-profile-card">
-      <h3 className="c-contact-profile-card__title">Contact info</h3>
-      <Emails contact={contact} />
-
-      <Phones contact={contact} />
-
-      <Source contact={contact} />
-
-      <Websites contact={contact} />
-
-      <OrginalSource contact={contact} />
-    </div>
+    <SectionWithFields
+      contact={props.contact}
+      fieldsOrder={fieldsOrder}
+      section="Contact Info"
+    />
   )
 }
