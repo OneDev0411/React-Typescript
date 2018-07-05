314c65d4269534935f430bc95587c6d2fa0db5df
OneDev0411 <cryptoapple1109@gmail.com>
2018-07-05 16:04:41 +0430

    web#1302 [CONTACTS/PROFILE] refactor: tags section

---
+++ app/components/Pages/Dashboard/Contacts/Profile/Tags/TagsList/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Flex from 'styled-flex-component'
+
+import { Tag } from './Tag'
+
+TagsList.propTypes = {
+  tags: PropTypes.arrayOf(PropTypes.shape()).isRequired
+}
+
+export function TagsList(props) {
+  return (
+    <Flex wrap>
+      {props.tags.map(tag => <Tag key={`tag_${tag.id}`} text={tag.text} />)}
+    </Flex>
+  )
+}
