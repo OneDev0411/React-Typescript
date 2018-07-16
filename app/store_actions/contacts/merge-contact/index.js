bd700544d039b219124c6ec9bad53d838acff4c7
OneDev0411 <cryptoapple1109@gmail.com>
2018-07-16 11:38:11 +0430

    #1480 Add notify for post merge request

--- app/store_actions/contacts/merge-contact/index.js
+++ app/store_actions/contacts/merge-contact/index.js
@@ -1,8 +1,10 @@
+import { addNotification as notify } from 'reapop'
+import { batchActions } from 'redux-batched-actions'
 import {
   FETCH_MERGE_CONTACTS_REQUEST,
   FETCH_MERGE_CONTACTS_FAILURE
 } from '../../../constants/contacts'
-import { mergeContact as patchContact } from '../../../models/contacts/merge-contact'
+import { mergeContact as postMergeContacts } from '../../../models/contacts/merge-contact'
 import { updateStoreBasedOnRemovedContacts } from '../delete-contact'
 
 export function mergeContact(contactId, sub_contacts, query) {
@@ -26,15 +28,34 @@ export function mergeContact(contactId, sub_contacts, query) {
         type: FETCH_MERGE_CONTACTS_REQUEST
       })
 
-      await patchContact(contactId, sub_contacts, query)
+      await postMergeContacts(contactId, sub_contacts, query)
 
-      updateStoreBasedOnRemovedContacts(dispatch, getState, sub_contacts)
+      await updateStoreBasedOnRemovedContacts(dispatch, getState, sub_contacts)
+      dispatch(
+        notify({
+          message: 'The contacts have been successfully merged!',
+          status: 'success'
+        })
+      )
     } catch (error) {
-      dispatch({
-        error,
-        type: FETCH_MERGE_CONTACTS_FAILURE
-      })
-      throw error
+      console.log(error)
+
+      batchActions([
+        dispatch({
+          error,
+          type: FETCH_MERGE_CONTACTS_FAILURE
+        }),
+        dispatch(
+          notify({
+            title: error.message,
+            message:
+              error.response && error.response.body
+                ? error.response.body.message
+                : null,
+            status: 'error'
+          })
+        )
+      ])
     }
   }
 }
