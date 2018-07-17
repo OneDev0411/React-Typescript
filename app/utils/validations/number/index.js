d2f0ec9aa269dfcb2e535ed8554c255697f68e14
OneDev0411 <cryptoapple1109@gmail.com>
2018-07-17 05:50:23 +0430

    web#1302 [CONTACTS/PROFILE] added: select field for array fields with enum values in edit section drawer

--- app/utils/validations/number/index.js
+++ app/utils/validations/number/index.js
@@ -4,8 +4,6 @@ export const isNumber = number => {
 
     const result = new RegExp(regular).exec(number)
 
-    console.log(typeof number, number);
-
     if (result == null) {
       return 'Just positive decimal numbers are valid. Like 13 and 200.00'
     }
