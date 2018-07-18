55cd3271e3c90eeebac233ec68d3f8673bf5b761
OneDev0411 <cryptoapple1109@gmail.com>
2018-07-18 18:27:30 +0430

    web#1396 [CONTACTS/NEW] addded: isLoadedContactAttrDefs

--- app/components/Pages/Dashboard/Contacts/NewContact/index.js
+++ app/components/Pages/Dashboard/Contacts/NewContact/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
+import { connect } from 'react-redux'
+
+import { isLoadedContactAttrDefs } from '../../../../../reducers/contacts/attributeDefs'
+import Loading from '../../../../Partials/Loading'
 
 import Header from './Header'
 import NewContactForm from './Form'
@@ -9,13 +13,22 @@ export const PageContainer = styled.div`
   background: #f0f4f7;
 `
 
-function NewContact() {
+function NewContact(props) {
   return (
     <PageContainer>
       <Header />
-      <NewContactForm />
+
+      {isLoadedContactAttrDefs(props.attributeDefs) ? (
+        <NewContactForm />
+      ) : (
+        <Loading />
+      )}
     </PageContainer>
   )
 }
 
-export default NewContact
+const mapStateToProps = state => ({
+  attributeDefs: state.contacts.attributeDefs
+})
+
+export default connect(mapStateToProps)(NewContact)
