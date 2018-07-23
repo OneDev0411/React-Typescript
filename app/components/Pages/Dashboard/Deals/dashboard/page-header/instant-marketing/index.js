d7e959cbd8d0dafcdb1e891a48a82befcf8ae233
OneDev0411 <cryptoapple1109@gmail.com>
2018-07-23 17:17:53 +0430

    integrate deal and instant marketing

--- app/components/Pages/Dashboard/Deals/dashboard/page-header/instant-marketing/index.js
+++ app/components/Pages/Dashboard/Deals/dashboard/page-header/instant-marketing/index.js
@@ -1,15 +1,25 @@
 import React, { Fragment } from 'react'
+import { connect } from 'react-redux'
+import { addNotification as notify } from 'reapop'
 
 import InstantMarketing from '../../../../../../../views/components/InstantMarketing'
 
 import ActionButton from '../../../../../../../views/components/Button/ActionButton'
 
-import Listing from '../../../../../../../models/listings/listing'
+import { sendContactsEmail } from '../../../../../../../models/email-compose/send-contacts-email'
 
-export default class DealInstantMarketing extends React.Component {
+import { getTemplateScreenshot } from '../../../../../../../models/instant-marketing'
+
+import Listing from '../../../../../../../models/listings/listing'
+import Compose from '../../../../../../../views/components/EmailCompose'
+
+class DealInstantMarketing extends React.Component {
   state = {
     listing: null,
-    isInstantMarketingBuilderOpen: false
+    isInstantMarketingBuilderOpen: false,
+    isComposeEmailOpen: false,
+    htmlTemplate: '',
+    templateScreenshot: null
   }
 
   componentDidMount() {
@@ -20,6 +30,68 @@ export default class DealInstantMarketing extends React.Component {
     this.setState(state => ({
       isInstantMarketingBuilderOpen: !state.isInstantMarketingBuilderOpen
     }))
+
+  toggleComposeEmail = () =>
+    this.setState(state => ({
+      isComposeEmailOpen: !state.isComposeEmailOpen
+    }))
+
+  handleSaveMarketingCard = async template => {
+    this.toggleInstantMarketingBuilder()
+    this.generatePreviewImage(template)
+
+    this.setState({
+      isComposeEmailOpen: true,
+      isInstantMarketingBuilderOpen: false,
+      htmlTemplate: template.result,
+      templateScreenshot: null
+    })
+  }
+
+  handleSendEmails = async values => {
+    this.setState({
+      isSendingEmail: true
+    })
+
+    const emails = values.recipients.map(recipient => ({
+      to: recipient.email,
+      subject: values.subject,
+      html: this.state.htmlTemplate,
+      contact: recipient.contactId
+    }))
+
+    try {
+      await sendContactsEmail(emails)
+
+      this.props.notify({
+        status: 'success',
+        message: `${emails.length} emails has been sent to your contacts`
+      })
+    } catch (e) {
+      console.log(e)
+      // todo
+    } finally {
+      this.setState({
+        isSendingEmail: false,
+        isComposeEmailOpen: false
+      })
+    }
+  }
+
+  generatePreviewImage = async template => {
+    const imageUrl = await getTemplateScreenshot(
+      template.result,
+      [template.width, template.height],
+      {
+        width: template.width / 2,
+        height: template.height / 2
+      }
+    )
+
+    this.setState({
+      templateScreenshot: `<img style="width: calc(100% - 2em); margin: 1em;" src="${imageUrl}" />`
+    })
+  }
 
   getDealListing = async () => {
     const { deal } = this.props
@@ -55,11 +127,25 @@ export default class DealInstantMarketing extends React.Component {
         <InstantMarketing
           isOpen={this.state.isInstantMarketingBuilderOpen}
           onClose={this.toggleInstantMarketingBuilder}
-          onSave={() => null}
+          handleSave={this.handleSaveMarketingCard}
           templateData={{ listing }}
           assets={listing && listing.gallery_image_urls}
+        />
+
+        <Compose
+          isOpen={this.state.isComposeEmailOpen}
+          onClose={this.toggleComposeEmail}
+          recipients={this.Recipients}
+          html={this.state.templateScreenshot}
+          onClickSend={this.handleSendEmails}
+          isSubmitting={this.state.isSendingEmail}
         />
       </Fragment>
     )
   }
 }
+
+export default connect(
+  null,
+  { notify }
+)(DealInstantMarketing)
