46090f2973ea06b0270c54406e86c9c57e83c355
OneDev0411 <cryptoapple1109@gmail.com>
2018-07-29 22:08:08 +0430

    web#1002 [CONTACTS/PROFILE] refactor touches and tasks list item

--- app/components/Pages/Dashboard/Contacts/Profile/Timeline/CRMTaskItem.js
+++ app/components/Pages/Dashboard/Contacts/Profile/Timeline/CRMTaskItem.js
@@ -4,63 +4,60 @@ import timeago from 'timeago.js'
 import { icons } from '../../../../../../views/CRM/Tasks/List/Table/columns/Type/icons'
 import { goTo } from '../../../../../../utils/go-to'
 
-export function CRMTaskItem(props) {
-  const { task } = props
-  const { task_type } = task
-  const Icon = icons[task_type] ? icons[task_type].icon : icons.Todo
+export class CRMTaskItem extends React.Component {
+  onClick = () =>
+    goTo(
+      `/crm/tasks/${this.props.task.id}`,
+      `Contact - ${this.props.contact.display_name}`
+    )
 
-  const activity = (
-    <React.Fragment>
-      <div className="image">
-        <Icon style={{ width: 32, height: 32, fill: '#8da2b5' }} />
-      </div>
+  render() {
+    const { task } = this.props
+    const { task_type } = task
+    const Icon = icons[task_type] ? icons[task_type].icon : icons.Todo
 
-      <div className="info">
-        <div
-          className="desc"
-          style={{
-            display: 'inline-flex',
-            alignItems: 'center',
-            marginBottom: '.5em'
-          }}
-        >
-          <span
+    return (
+      <a className="c-contact-activities-list__item" onClick={this.onClick}>
+        <div className="image">
+          <Icon style={{ width: 32, height: 32, fill: '#8da2b5' }} />
+        </div>
+
+        <div className="info">
+          <div
+            className="desc"
             style={{
-              fontSize: 'small',
-              marginRight: '0.5em',
-              padding: '0 0.5em',
-              borderRadius: 30,
-              color: '#fff',
-              background: '#8da2b5'
+              display: 'inline-flex',
+              alignItems: 'center',
+              marginBottom: '.5em'
             }}
           >
-            {task_type}
-          </span>
-          <span
-            style={{
-              lineHeight: 1,
-              fontSize: '1.5rem'
-            }}
-            dangerouslySetInnerHTML={{ __html: task.title }}
-          />
-        </div>
+            <span
+              style={{
+                fontSize: 'small',
+                marginRight: '0.5em',
+                padding: '0 0.5em',
+                borderRadius: 30,
+                color: '#fff',
+                background: '#8da2b5'
+              }}
+            >
+              {task_type}
+            </span>
+            <span
+              style={{
+                lineHeight: 1,
+                fontSize: '1.5rem'
+              }}
+              dangerouslySetInnerHTML={{ __html: task.title }}
+            />
+          </div>
 
-        <div className="time">
-          <img src="/static/images/contacts/alert-fill@3x.png" alt="alert" />
-          {timeago().format(task.created_at * 1000)}
+          <div className="time">
+            <img src="/static/images/contacts/alert-fill@3x.png" alt="alert" />
+            {timeago().format(task.created_at * 1000)}
+          </div>
         </div>
-      </div>
-    </React.Fragment>
-  )
-
-  return (
-    <a
-      className="c-contact-activities-list__item"
-      onClick={() =>
-        goTo(`/crm/tasks/${task.id}`, `Contact - ${props.contact.display_name}`)
-      }
-    >
-      {activity}
-    </a>
-  )
+      </a>
+    )
+  }
 }
--- app/components/Pages/Dashboard/Contacts/Profile/Timeline/CRMTouchItem.js
+++ app/components/Pages/Dashboard/Contacts/Profile/Timeline/CRMTouchItem.js
@@ -4,39 +4,33 @@ import timeago from 'timeago.js'
 import Icon from '../../../../../../views/components/SvgIcons/AddAlarm/IconAddAlarm'
 import { goTo } from '../../../../../../utils/go-to'
 
-export function CRMTouchItem(props) {
-  const { touch } = props
+export class CRMTouchItem extends React.Component {
+  onClick = () =>
+    goTo(
+      `/crm/touches/${this.props.touch.id}`,
+      `Contact - ${this.props.contact.display_name}`
+    )
 
-  const activity = (
-    <React.Fragment>
-      <div className="image">
-        <Icon style={{ width: 32, height: 32, fill: '#8da2b5' }} />
-      </div>
+  render() {
+    const { touch } = this.props
 
-      <div className="info">
-        <div className="desc" style={{ marginBottom: '.5em' }}>
-          {touch.activity_type}
+    return (
+      <a className="c-contact-activities-list__item" onClick={this.onClick}>
+        <div className="image">
+          <Icon style={{ width: 32, height: 32, fill: '#8da2b5' }} />
         </div>
 
-        <div className="time">
-          <img src="/static/images/contacts/alert-fill@3x.png" alt="alert" />
-          {timeago().format(touch.timestamp * 1000)}
-        </div>
-      </div>
-    </React.Fragment>
-  )
+        <div className="info">
+          <div className="desc" style={{ marginBottom: '.5em' }}>
+            {touch.activity_type}
+          </div>
 
-  return (
-    <a
-      className="c-contact-activities-list__item"
-      onClick={() =>
-        goTo(
-          `/crm/touches/${touch.id}`,
-          `Contact - ${props.contact.display_name}`
-        )
-      }
-    >
-      {activity}
-    </a>
-  )
+          <div className="time">
+            <img src="/static/images/contacts/alert-fill@3x.png" alt="alert" />
+            {timeago().format(touch.timestamp * 1000)}
+          </div>
+        </div>
+      </a>
+    )
+  }
 }
