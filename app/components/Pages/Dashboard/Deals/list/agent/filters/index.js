1f658161c68e25173710a69aad60b9510b8c32d0
OneDev0411 <cryptoapple1109@gmail.com>
2018-07-30 17:02:09 +0430

    fix Lease status (web#1473)

--- app/components/Pages/Dashboard/Deals/list/agent/filters/index.js
+++ app/components/Pages/Dashboard/Deals/list/agent/filters/index.js
@@ -20,7 +20,7 @@ import {
 } from '../../styles/filters/styled'
 
 const FilterNames = {
-  Active: ['Active'],
+  Active: ['Active', 'Lease'],
   Drafts: ['Drafts'],
   Pending: [
     'Active Contingent',
