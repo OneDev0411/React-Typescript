c543bf7297d8c32a10ad2acae1387b1878138345
OneDev0411 <cryptoapple1109@gmail.com>
2018-08-12 18:40:44 +0430

    web#1599 [VIEWS/BUTTON] refactored: TextIconButton

--- app/views/components/Button/TextIconButton/index.js
+++ app/views/components/Button/TextIconButton/index.js
@@ -4,65 +4,73 @@ import PropTypes from 'prop-types'
 import Button from '../ActionButton'
 import { isOutline, getIconSize, getStatesStyle, iconAims } from './helpers'
 
-const propTypes = {
-  /**
-   * Composes the Button component as the base.
-   */
-  ...Button.propTypes,
-
-  /**
-   * The button text.
-   */
-  text: PropTypes.string,
-
-  /**
-   * Sets an icon before the text. Can be any icon from Rechat SVG icons.
-   */
-  iconLeft: PropTypes.func,
-
-  /**
-   * The aim of the left icon. Not a big use case for this.
-   */
-  iconLeftAim: PropTypes.oneOf(Object.keys(iconAims)),
-
-  /**
-   * Sets an icon after the text. Can be any icon from Rechat SVG icons.
-   */
-  iconRight: PropTypes.func,
-
-  /**
-   * The aim of the right icon. Useful to aim a triangle down.
-   */
-  iconRightAim: PropTypes.oneOf(Object.keys(iconAims))
-}
-
-const defaultProps = {
-  appearance: 'default',
-  disabled: false,
-  inverse: false,
-  size: 'medium',
-  text: '',
-  iconLeftAim: 'none',
-  iconRightAim: 'none'
-}
-
 class TextIconButton extends PureComponent {
+  propTypes = {
+    /**
+     * Composes the Button component as the base.
+     */
+    ...Button.propTypes,
+
+    /**
+     * The button text.
+     */
+    text: PropTypes.string,
+
+    /**
+     * Sets an icon before the text. Can be any icon from Rechat SVG icons.
+     */
+    iconLeft: PropTypes.func,
+
+    /**
+     * The aim of the left icon. Not a big use case for this.
+     */
+    iconLeftAim: PropTypes.oneOf(Object.keys(iconAims)),
+
+    /**
+     * Sets an icon after the text. Can be any icon from Rechat SVG icons.
+     */
+    iconRight: PropTypes.func,
+
+    /**
+     * The aim of the right icon. Useful to aim a triangle down.
+     */
+    iconRightAim: PropTypes.oneOf(Object.keys(iconAims))
+  }
+
+  static defaultProps = {
+    appearance: 'default',
+    disabled: false,
+    inverse: false,
+    size: 'medium',
+    text: '',
+    iconLeftAim: 'none',
+    iconRightAim: 'none'
+  }
+
   render() {
-    const { iconLeft, iconRight, text } = this.props
+    const {
+      iconLeft,
+      iconRight,
+      iconLeftAim,
+      iconRightAim,
+      text,
+      ...rest
+    } = this.props
+
     let IconLeft = () => null
     let IconRight = () => null
 
     if (iconLeft) {
       IconLeft = iconLeft.extend`
         margin-right: ${text ? '8px' : 0};
-        transform: rotate(${iconAims[this.props.iconLeftAim]});
+        transform: rotate(${iconAims[iconLeftAim]});
       `
     }
 
     if (iconRight) {
       IconRight = iconRight.extend`
         margin-left: ${text ? '8px' : 0};
-        transform: rotate(${iconAims[this.props.iconRightAim]});
+        transform: rotate(${iconAims[iconRightAim]});
       `
     }
 
@@ -79,7 +87,7 @@ class TextIconButton extends PureComponent {
     const $text = text ? React.createElement('span', {}, text) : null
 
     return (
-      <ExtendedButton {...this.props}>
+      <ExtendedButton {...rest}>
         <IconLeft />
         {$text}
         <IconRight />
@@ -88,7 +96,4 @@ class TextIconButton extends PureComponent {
   }
 }
 
-export default Object.assign(TextIconButton, {
-  propTypes,
-  defaultProps
-})
+export default TextIconButton
