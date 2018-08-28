4e0c6f2087514402b68bea1e5d6f7f2606eac70a
OneDev0411 <cryptoapple1109@gmail.com>
2018-08-28 21:17:25 +0430

    web#1599 [VIEWS/PAGE HEADER] added: vertical padding

--- app/views/components/PageHeader/index.js
+++ app/views/components/PageHeader/index.js
@@ -11,6 +11,7 @@ const Container = styled.div`
   width: 100%;
   height: 56px;
   display: flex;
+  padding: 0 1em;
   justify-content: space-between;
   background-color: ${props => (props.isFlat ? 'transparent' : '#fff')};
   border-bottom: ${props => (props.isFlat ? 'none' : '1px solid #e2e4e5')};
