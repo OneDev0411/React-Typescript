3b0c2da4f9c9ef7142d261148c96251077a8fe15
OneDev0411 <cryptoapple1109@gmail.com>
2018-08-31 20:09:18 +0430

    web#1615 [MLS/PANEL] refactor: grid view - using rechat grid

--- app/components/Pages/Dashboard/Listings/components/ListingsPanels/TableView/index.js
+++ app/components/Pages/Dashboard/Listings/components/ListingsPanels/TableView/index.js
@@ -1,19 +1,83 @@
 import React from 'react'
-import TableViewRow from './TableViewRow'
+import { connect } from 'react-redux'
 
-const TableView = ({ listings, ...rest }) =>
-  <table className="c-tableview__table">
-    <tbody>
-      {(listings.data.length &&
-        listings.data.map((listing, index) =>
-          <TableViewRow
-            {...rest}
-            listing={listing}
-            key={`${rest.activePanel}_PANEL_LIST_ITEM_${index}`}
-          />
-        )) ||
-        ''}
-    </tbody>
-  </table>
+import Table from '../../../../../../../views/components/Grid/Table'
+import { Address } from './columns/Address'
+import prepareListingViewItemProps from '../prepareListingViewItemProps'
 
-export default TableView
+class TableView extends React.Component {
+  getProp = (listing, prop) => {
+    const props = prepareListingViewItemProps(this.props.user, listing)
+
+    return props[prop] || ''
+  }
+
+  columns = [
+    {
+      header: 'Address',
+      id: 'adress',
+      width: '30%',
+      accessor: null,
+      render: ({ rowData: listing }) => (
+        <Address user={this.props.user} listing={listing} />
+      )
+    },
+    {
+      header: 'Price',
+      id: 'price',
+      accessor: listing => this.getProp(listing, 'price'),
+      render: ({ rowData: listing }) => this.getProp(listing, 'price')
+    },
+    {
+      header: 'Beds',
+      id: 'beds',
+      accessor: listing => this.getProp(listing, 'beds'),
+      render: ({ rowData: listing }) => this.getProp(listing, 'beds')
+    },
+    {
+      header: 'Baths',
+      id: 'baths',
+      accessor: listing => this.getProp(listing, 'baths'),
+      render: ({ rowData: listing }) => this.getProp(listing, 'baths')
+    },
+    {
+      header: 'sqft',
+      id: 'sqft',
+      accessor: listing => this.getProp(listing, 'sqft'),
+      render: ({ rowData: listing }) => this.getProp(listing, 'sqft')
+    },
+    {
+      header: '$/Sqft',
+      id: 'pricePerSquareFoot',
+      accessor: listing => this.getProp(listing, 'pricePerSquareFoot'),
+      render: ({ rowData: listing }) =>
+        this.getProp(listing, 'pricePerSquareFoot')
+    },
+    {
+      header: 'Built Year',
+      id: 'year',
+      accessor: listing => this.getProp(listing, 'builtYear'),
+      render: ({ rowData: listing }) => this.getProp(listing, 'builtYear')
+    },
+    {
+      header: 'Zip Code',
+      id: 'zipcode',
+      accessor: listing => this.getProp(listing, 'zipCode'),
+      render: ({ rowData: listing }) => this.getProp(listing, 'zipCode')
+    }
+  ]
+
+  render() {
+    return (
+      <Table
+        columns={this.columns}
+        data={this.props.listings.data}
+        plugins={{
+          sortable: {}
+        }}
+      />
+    )
+  }
+}
+
+export default connect(({ user }) => ({ user }))(TableView)
