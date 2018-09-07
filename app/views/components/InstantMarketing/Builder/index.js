97392b3a9a5de8935694d7f2151694e0eefbbbdb
OneDev0411 <cryptoapple1109@gmail.com>
2018-09-06 17:42:04 -0700

    phone filter for for numbers based on requests on templates#2

--- app/views/components/InstantMarketing/Builder/index.js
+++ app/views/components/InstantMarketing/Builder/index.js
@@ -9,6 +9,8 @@ import nunjucks from 'nunjucks'
 import './AssetManager'
 import config from './config'
 
+import PhoneNumber from 'google-libphonenumber'
+
 import {
   Container,
   TemplatesContainer,
@@ -67,6 +69,20 @@ class Builder extends React.Component {
         maximumFractionDigits: 0,
       }).format(area_meters * 10.7639)
     )
+
+    const pnu = new PhoneNumber.PhoneNumberUtil()
+
+    this.nunjucks.addFilter('phone', phone => {
+      let pn
+
+      try {
+        pn = pnu.parse(phone)
+      } catch(e) {
+        return phone // Cannot parse it.
+      }
+
+      return pnu.format(pn, PhoneNumber.PhoneNumberFormat.NATIONAL)
+    })
   }
 
   disableAssetManager = () => {
