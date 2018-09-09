5a7391a9cafb5e400bc26ebf2d214c4b91b174a3
OneDev0411 <cryptoapple1109@gmail.com>
2018-09-09 12:34:31 +0430

    web#1711 [CONTACT/PROFILE] added: header menu

--- app/components/Pages/Dashboard/Contacts/Profile/Header/index.js
+++ app/components/Pages/Dashboard/Contacts/Profile/Header/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Flex from 'styled-flex-component'
 
+import { Menu } from './Menu'
 import { Catalog } from './Catalog'
 
 export function Header(props) {
   return (
     <Flex justifyBetween style={{ height: '88px', marginBottom: '2.5em' }}>
       <Catalog contact={props.contact} />
-      {/* <Menu contact={props.contact} /> */}
+      <Menu contact={props.contact} />
     </Flex>
   )
 }
