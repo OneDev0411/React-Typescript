db5204a9ec23a4c1151c95af496d8be74e70bb77
OneDev0411 <cryptoapple1109@gmail.com>
2018-09-14 02:21:38 +0430

    web#1711 [VIEWS/FINAL FORM FIELDS] refactor: date time picker

--- app/views/components/final-form-fields/DateTimePicker/index.js
+++ app/views/components/final-form-fields/DateTimePicker/index.js
@@ -1,67 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import DayPicker from 'react-day-picker'
+import { Field } from 'react-final-form'
 
-import fecha from 'fecha'
-import ClickOutSide from 'react-click-outside'
+import { DateTimePicker } from '../../DateTimePicker'
 
-import ActionButton from '../../Button/ActionButton'
-import { PickerContainer } from './styled'
+DateTimeField.propTypes = {
+  name: PropTypes.string.isRequired,
+  selectedDate: PropTypes.instanceOf(Date)
+}
 
-const formatDate = date => fecha.format(date, 'MMM D, YYYY HH:MM A')
+DateTimeField.defaultProps = {
+  selectedDate: new Date()
+}
 
-export class DateTimePicker extends React.Component {
-  static propTypes = {
-    onChange: PropTypes.func.isRequired,
-    defaultSelectedDate: PropTypes.instanceOf(Date),
-    datePickerModifiers: PropTypes.shape()
-  }
-
-  state = {
-    isOpen: false,
-    selectedDate: this.props.defaultSelectedDate || new Date()
-  }
-
-  handleClose = () => this.setState({ isOpen: false })
-
-  handleOpen = () => this.setState({ isOpen: true })
-
-  handleSelectedDate = selectedDate => {
-    this.setState(
-      () => ({ selectedDate, isOpen: false }),
-      () => this.props.onChange(this.state.selectedDate)
-    )
-  }
-
-  render() {
-    const { selectedDate } = this.state
-
-    return (
-      <div style={this.props.style}>
-        <ActionButton
-          type="button"
-          appearance="link"
-          onClick={this.handleOpen}
-          onFocus={this.handleOpen}
-          style={{ fontWeight: 500 }}
-        >
-          {formatDate(selectedDate)}
-        </ActionButton>
-        <div style={{ position: 'relative' }}>
-          {this.state.isOpen && (
-            <ClickOutSide onClickOutside={this.handleClose}>
-              <PickerContainer depth={3}>
-                <DayPicker
-                  initialMonth={selectedDate}
-                  selectedDays={selectedDate}
-                  onDayClick={this.handleSelectedDate}
-                  modifiers={this.props.datePickerModifiers}
-                />
-              </PickerContainer>
-            </ClickOutSide>
-          )}
-        </div>
-      </div>
-    )
-  }
+export function DateTimeField(props) {
+  return (
+    <Field
+      name={props.name}
+      render={fieldProps => (
+        <DateTimePicker
+          onChange={fieldProps.input.onChange}
+          defaultSelectedDate={props.selectedDate}
+        />
+      )}
+    />
+  )
 }
