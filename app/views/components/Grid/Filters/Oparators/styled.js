2e64d137447e763628f319b1a6bff59517d54d06
OneDev0411 <cryptoapple1109@gmail.com>
2018-09-16 00:24:47 +0430

    #1653 Change filter radio input to SVG icon

--- app/views/components/Grid/Filters/Oparators/styled.js
+++ app/views/components/Grid/Filters/Oparators/styled.js
@@ -8,11 +8,12 @@ export const Operator = styled.div`
   cursor: pointer;
 `
 
-export const Input = styled.input`
-  cursor: pointer;
-`
-
 export const Title = styled.span`
   font-size: 16px;
   margin-left: 0.5em;
+`
+
+export const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
 `
