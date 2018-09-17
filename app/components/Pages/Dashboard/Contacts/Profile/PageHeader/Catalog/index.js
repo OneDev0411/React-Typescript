8efc3a9a0fc239040b6e550cfeab2a6098b1b95d
OneDev0411 <cryptoapple1109@gmail.com>
2018-09-18 01:20:39 +0430

    web#1711 [CONTAACT/PROFILE] added: static last touch temporarily

--- app/components/Pages/Dashboard/Contacts/Profile/PageHeader/Catalog/index.js
+++ app/components/Pages/Dashboard/Contacts/Profile/PageHeader/Catalog/index.js
@@ -4,7 +4,7 @@ import Flex from 'styled-flex-component'
 import { H1 } from '../../../../../../../views/components/Typography/headings'
 
 import Avatar from './Avatar'
-import { LastTouched } from './LastTouched'
+// import { LastTouched } from './LastTouched'
 
 export function Catalog(props) {
   const { contact } = props
@@ -14,7 +14,10 @@ export function Catalog(props) {
       <Avatar contact={contact} />
       <Flex column style={{ padding: '0.5em 1.5em' }}>
         <H1 style={{ lineHeight: 1.5 }}>{contact.display_name}</H1>
-        <LastTouched contact={contact} />
+        {/* <LastTouched contact={contact} /> */}
+        <div>
+          Last Touch was <span style={{ fontWeight: 600 }}>2 days ago</span>.
+        </div>
       </Flex>
     </Flex>
   )
