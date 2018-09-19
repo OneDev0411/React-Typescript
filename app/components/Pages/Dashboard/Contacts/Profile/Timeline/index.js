7a2854cfa236981887ebbec1a461bf4a80da8ed6
OneDev0411 <cryptoapple1109@gmail.com>
2018-09-19 14:16:47 +0430

    web#1711 [CONTACT/PROFILE] added: indexed timeline events by date

--- app/components/Pages/Dashboard/Contacts/Profile/Timeline/index.js
+++ app/components/Pages/Dashboard/Contacts/Profile/Timeline/index.js
@@ -7,8 +7,8 @@ import { EventDrawer } from '../../../../../../views/components/EventDrawer'
 import { Card } from '../styled'
 import { NoteItem } from './NoteItem'
 import CRMTaskItem from './TaskItem'
-import { Title } from '../Timeline/styled'
 import { EmptyState } from './EmptyState'
+import { Container, Title } from './styled'
 
 export class Timeline extends React.Component {
   state = {
@@ -28,71 +28,173 @@ export class Timeline extends React.Component {
   closeEditEventDrawer = () =>
     this.setState({ showEditEventDrawer: false, selectedEvent: null })
 
+  renderItems = month => (
+    <React.Fragment>
+      <Title>
+        <b>{month.title}</b>
+      </Title>
+      <Card>
+        {month.items.map(activity => {
+          const key = `timeline_item_${activity.id}`
+
+          if (activity.type === 'crm_task') {
+            return (
+              <CRMTaskItem
+                contact={this.props.contact}
+                key={key}
+                task={activity}
+                onClick={this.openEditEventDrawer}
+              />
+            )
+          }
+
+          if (
+            activity.type === 'contact_attribute' &&
+            activity.attribute_type === 'note'
+          ) {
+            return (
+              <NoteItem
+                contact={this.props.contact}
+                key={key}
+                note={activity}
+                onClick={this.openEditNoteDrawer}
+              />
+            )
+          }
+        })}
+      </Card>
+    </React.Fragment>
+  )
+
   render() {
     if (this.props.isFetching) {
       return <Loading />
     }
 
-    if (this.props.items.length > 0) {
-      return (
-        <div>
-          <Title>
-            <b>Upcoming Events</b>
-          </Title>
-          <Card>
-            {this.props.items.map(activity => {
-              const key = `timeline_item_${activity.id}`
-
-              if (activity.type === 'crm_task') {
-                return (
-                  <CRMTaskItem
-                    contact={this.props.contact}
-                    key={key}
-                    task={activity}
-                    onClick={this.openEditEventDrawer}
-                  />
-                )
-              }
-
-              if (
-                activity.type === 'contact_attribute' &&
-                activity.attribute_type === 'note'
-              ) {
-                return (
-                  <NoteItem
-                    contact={this.props.contact}
-                    key={key}
-                    note={activity}
-                    onClick={this.openEditNoteDrawer}
-                  />
-                )
-              }
-            })}
-          </Card>
-
-          {this.state.selectedNote && (
-            <EditNoteDrawer
-              isOpen={this.state.showEditNoteDrawer}
-              note={this.state.selectedNote}
-              onClose={this.closeEditNoteDrawer}
-              onSubmit={this.props.editNoteHandler}
-              onDelete={this.props.deleteNoteHandler}
-            />
-          )}
-
-          {this.state.selectedEvent && (
-            <EventDrawer
-              title="Edit Event"
-              isOpen={this.state.showEditEventDrawer}
-              eventId={this.state.selectedEvent.id}
-              onClose={this.closeEditEventDrawer}
-              onSubmit={this.props.editEventHandler}
-            />
-          )}
-        </div>
-      )
+    if (this.props.items.length === 0) {
+      return <EmptyState />
     }
 
-    return <EmptyState />
+    const upcomingEvents = []
+    const pastEventsIndexedInMonths = {}
+
+    this.props.items.forEach(item => {
+      let date
+      let { due_date } = item
+
+      function getDateMonthAndYear(date) {
+        date = new Date(date)
+
+        const months = [
+          'January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December'
+        ]
+
+        const monthNumber = date.getMonth()
+        const year = date.getFullYear()
+
+        const index = `${monthNumber}_${year}`
+
+        const title = `${months[monthNumber]} ${year}`
+
+        return {
+          index,
+          title
+        }
+      }
+
+      if (due_date) {
+        due_date *= 1000
+
+        if (due_date > new Date().getTime()) {
+          return upcomingEvents.push(item)
+        }
+
+        date = new Date(due_date)
+      } else {
+        date = new Date(item.created_at * 1000)
+      }
+
+      const monthAndYear = getDateMonthAndYear(date)
+
+      if (pastEventsIndexedInMonths[monthAndYear.index]) {
+        pastEventsIndexedInMonths[monthAndYear.index].items.push(item)
+      } else {
+        pastEventsIndexedInMonths[monthAndYear.index] = {
+          title: monthAndYear.title,
+          items: [item]
+        }
+      }
+    })
+
+    let sortedPastEvents = []
+
+    if (Object.keys(pastEventsIndexedInMonths).length > 0) {
+      sortedPastEvents = Object.keys(pastEventsIndexedInMonths).sort((a, b) => {
+        const [aMonth, aYear] = a.split('_')
+        const [bMonth, bYear] = b.split('_')
+
+        if (aYear === bYear) {
+          return bMonth - aMonth
+        }
+
+        return bYear - aYear
+      })
+    }
+
+    return (
+      <div>
+        {upcomingEvents.length > 0 && (
+          <Container id="upcoming_events" key="upcoming_events">
+            {this.renderItems({
+              title: 'Upcoming Events',
+              items: upcomingEvents
+            })}
+          </Container>
+        )}
+
+        {sortedPastEvents.length > 0 &&
+          sortedPastEvents.map(monthIndex => {
+            const month = pastEventsIndexedInMonths[monthIndex]
+            const id = month.title.replace(' ', '_')
+
+            return (
+              <Container id={id} key={`past_events_${id}`}>
+                {this.renderItems(month)}
+              </Container>
+            )
+          })}
+
+        {this.state.selectedNote && (
+          <EditNoteDrawer
+            isOpen={this.state.showEditNoteDrawer}
+            note={this.state.selectedNote}
+            onClose={this.closeEditNoteDrawer}
+            onSubmit={this.props.editNoteHandler}
+            onDelete={this.props.deleteNoteHandler}
+          />
+        )}
+
+        {this.state.selectedEvent && (
+          <EventDrawer
+            title="Edit Event"
+            isOpen={this.state.showEditEventDrawer}
+            eventId={this.state.selectedEvent.id}
+            onClose={this.closeEditEventDrawer}
+            onSubmit={this.props.editEventHandler}
+          />
+        )}
+      </div>
+    )
   }
 }
--- app/components/Pages/Dashboard/Contacts/Profile/Timeline/styled.js
+++ app/components/Pages/Dashboard/Contacts/Profile/Timeline/styled.js
@@ -1,4 +1,9 @@
+import styled from 'styled-components'
 import { H3 } from '../../../../../../views/components/Typography/headings'
+
+export const Container = styled.div`
+  margin-bottom: 1.5em;
+`
 
 export const Title = H3.extend`
   padding-left: 0.75em;
