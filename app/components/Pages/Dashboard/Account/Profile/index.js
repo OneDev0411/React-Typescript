2e2a843c97e4d07fcc87c7cbeb88383b12c98970
OneDev0411 <cryptoapple1109@gmail.com>
2018-09-19 21:02:18 +0430

    #1707 Refactor pageHeader

--- app/components/Pages/Dashboard/Account/Profile/index.js
+++ app/components/Pages/Dashboard/Account/Profile/index.js
@@ -10,9 +10,9 @@ import PageHeader from '../../../../../views/components/PageHeader'
 
 const Profile = ({ user, brand }) => (
   <Fragment>
-    <PageHeader isFlat style={{ marginBottom: '1.5em' }}>
+    <PageHeader isFlat style={{ marginBottom: '1.5em', marginTop: '1.5rem' }}>
       <PageHeader.Title showBackButton={false}>
-        <PageHeader.Heading>Profile </PageHeader.Heading>
+        <PageHeader.Heading>Profile</PageHeader.Heading>
       </PageHeader.Title>
     </PageHeader>
     <Fragment>
