65eeb4c4aacc123bc2cff93e9bb1e0a7316b60f5
OneDev0411 <cryptoapple1109@gmail.com>
2018-09-21 01:58:08 +0430

    fix DatePicker drawer width

--- app/components/Pages/Dashboard/Deals/form-edit/editor/integrations/context/date-context/index.js
+++ app/components/Pages/Dashboard/Deals/form-edit/editor/integrations/context/date-context/index.js
@@ -18,7 +18,7 @@ function onDateChange(props, value) {
 
 export default function DateContext(props) {
   return (
-    <OverlayDrawer isOpen={props.isOpen} onClose={props.onClose} width={30}>
+    <OverlayDrawer isOpen={props.isOpen} onClose={props.onClose} width={37}>
       <OverlayDrawer.Header title="Edit Field" />
       <OverlayDrawer.Body>
         <OverlayContainer>
