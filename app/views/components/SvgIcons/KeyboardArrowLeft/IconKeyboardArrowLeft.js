d5c83aa7f64c6768605f7b6fc1002681f6b2516d
OneDev0411 <cryptoapple1109@gmail.com>
2018-09-20 14:15:21 +0430

    #1706 Refactor change month buttons

--- app/views/components/SvgIcons/KeyboardArrowLeft/IconKeyboardArrowLeft.js
+++ app/views/components/SvgIcons/KeyboardArrowLeft/IconKeyboardArrowLeft.js
@@ -21,8 +21,7 @@ const Image = styled.svg`
 
 const defaultProps = {
   children: [
-    <path d="M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z" key="key-0" />,
-    <path fill="none" d="M0-.5h24v24H0z" key="key-1" />
+    <path d="M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z" key="key-0" />
   ],
   viewBox
 }
