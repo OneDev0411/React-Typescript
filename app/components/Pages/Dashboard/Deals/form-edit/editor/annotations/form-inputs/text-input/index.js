86151d39923bd2d210a168cd64e2591e858d833d
OneDev0411 <cryptoapple1109@gmail.com>
2018-09-21 01:21:01 +0430

    implement multiline inputs

--- app/components/Pages/Dashboard/Deals/form-edit/editor/annotations/form-inputs/text-input/index.js
+++ app/components/Pages/Dashboard/Deals/form-edit/editor/annotations/form-inputs/text-input/index.js
@@ -1,33 +1,7 @@
 import React from 'react'
-import styled from 'styled-components'
 import parseAppearanceString from '../../../../utils/appearance'
 
-const TextInput = styled.input`
-  position: absolute;
-  left: ${props => props.rect[0]}px;
-  top: ${props => props.rect[1]}px;
-  width: ${props => Math.floor(props.rect[2] - props.rect[0])}px;
-  height: ${props => Math.floor(props.rect[3] - props.rect[1])}px;
-
-  font-size: ${props => props.fontSize || 16}px;
-  font-family: ${props => props.appearance.font};
-  color: ${props => props.appearance.color};
-  font-weight: ${props => (props.appearance.bold ? 'bold' : 'normal')}
-  min-width: 15px;
-  background-color: #d2e5f2;
-  border: 1px solid #ccc;
-  transition: 0.1s ease-in all;
-  padding: 0 3px;
-
-  :focus {
-    background-color: transparent;
-    outline: none;
-    border: none;
-    font-size: ${props => (props.fontSize || 16) - 2}px;
-    font-weight: 400;
-    color: #262626;
-  }
-`
+import { TextArea, TextInput } from './styled'
 
 class TextAnnotation extends React.Component {
   shouldComponentUpdate(nextProps) {
@@ -41,16 +15,22 @@ class TextAnnotation extends React.Component {
       this.props.annotation.defaultAppearance
     )
 
-    return (
-      <TextInput
-        id={this.props.annotation.fieldName}
-        appearance={appearance}
-        fontSize={this.props.fontSize}
-        rect={this.props.annotation.rect}
-        onInput={this.onChange}
-        defaultValue={this.props.value || ''}
-      />
-    )
+    const isTextArea = this.props.annotation.multiLine === true
+
+    const props = {
+      id: this.props.annotation.fieldName,
+      appearance,
+      fontSize: this.props.fontSize,
+      rect: this.props.annotation.rect,
+      onInput: this.onChange,
+      defaultValue: this.props.value || ''
+    }
+
+    if (isTextArea) {
+      return <TextArea {...props} />
+    }
+
+    return <TextInput {...props} />
   }
 }
 
---
+++ app/components/Pages/Dashboard/Deals/form-edit/editor/annotations/form-inputs/text-input/styled.js
@@ -0,0 +1,37 @@
+import styled, { css } from 'styled-components'
+
+const styles = css`
+position: absolute;
+  left: ${props => props.rect[0]}px;
+  top: ${props => props.rect[1]}px;
+  width: ${props => Math.floor(props.rect[2] - props.rect[0])}px;
+  height: ${props => Math.floor(props.rect[3] - props.rect[1])}px;
+
+  font-size: ${props => props.fontSize || 16}px;
+  font-family: ${props => props.appearance.font};
+  color: ${props => props.appearance.color};
+  font-weight: ${props => (props.appearance.bold ? 'bold' : 'normal')}
+  min-width: 15px;
+  background-color: #d2e5f2;
+  border: 1px solid #ccc;
+  transition: 0.1s ease-in all;
+  padding: 0 3px;
+
+  :focus {
+    background-color: transparent;
+    outline: none;
+    border: none;
+    font-size: ${props => (props.fontSize || 16) - 2}px;
+    font-weight: 400;
+    color: #262626;
+  }
+`
+
+export const TextInput = styled.input`
+  ${styles};
+`
+
+export const TextArea = styled.textarea`
+  ${styles};
+  resize: none;
+`
