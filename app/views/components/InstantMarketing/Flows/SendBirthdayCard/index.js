3a4c0c93907113fe64e443477e06585f58bfc089
OneDev0411 <cryptoapple1109@gmail.com>
2018-09-23 03:01:25 +0330

    fix composing birthday card

--- app/views/components/InstantMarketing/Flows/SendBirthdayCard/index.js
+++ app/views/components/InstantMarketing/Flows/SendBirthdayCard/index.js
@@ -3,10 +3,12 @@ import { connect } from 'react-redux'
 import { addNotification as notify } from 'reapop'
 
 import InstantMarketing from 'components/InstantMarketing'
-
 import ActionButton from 'components/Button/ActionButton'
 
 import { sendContactsEmail } from 'models/email-compose/send-contacts-email'
+
+import { selectDefinitionByName } from '../../../../../reducers/contacts/attributeDefs'
+import { getContactAttribute } from 'models/contacts/helpers/get-contact-attribute'
 
 import { getTemplateScreenshot } from 'models/instant-marketing'
 
@@ -59,11 +61,96 @@ class SendBirthdayCard extends React.Component {
       isComposeEmailOpen: !state.isComposeEmailOpen
     }))
 
+  handleSaveMarketingCard = async template => {
+    this.toggleInstantMarketingBuilder()
+    this.generatePreviewImage(template)
+
+    this.setState({
+      isComposeEmailOpen: true,
+      isInstantMarketingBuilderOpen: false,
+      htmlTemplate: template.result,
+      templateScreenshot: null
+    })
+  }
+
+  generatePreviewImage = async template => {
+    const imageUrl = await getTemplateScreenshot(
+      template.result,
+      [template.width, template.height],
+      {
+        width: template.width / 2,
+        height: template.height / 2
+      }
+    )
+
+    this.setState({
+      templateScreenshot: `<img style="width: calc(100% - 2em); margin: 1em;" src="${imageUrl}" />`
+    })
+  }
+
+  handleSendEmails = async values => {
+    this.setState({
+      isSendingEmail: true
+    })
+
+    const recipient = values.recipients[0]
+
+    const emails = [
+      {
+        to: recipient.email,
+        subject: values.subject,
+        html: this.state.htmlTemplate,
+        contact: recipient.contactId
+      }
+    ]
+
+    try {
+      await sendContactsEmail(emails)
+
+      this.props.notify({
+        status: 'success',
+        message: `${emails.length} emails has been sent to your contacts`
+      })
+    } catch (e) {
+      console.log(e)
+      // todo
+    } finally {
+      this.setState({
+        isSendingEmail: false,
+        isComposeEmailOpen: false
+      })
+    }
+  }
+
   get HasMarketingAccess() {
     const { user } = this.props
     const acl = getActiveTeamACL(user)
 
     return acl.includes('Marketing')
+  }
+
+  get Recipients() {
+    const { contact } = this.state
+
+    if (!contact) {
+      return []
+    }
+
+    const emails = getContactAttribute(
+      contact,
+      selectDefinitionByName(this.props.attributeDefs, 'email')
+    )
+
+    return [
+      {
+        contactId: contact.id,
+        name: contact.summary.display_name,
+        avatar: contact.summary.profile_image_url,
+        email: contact.summary.email,
+        emails: emails.map(email => email.text),
+        readOnly: true
+      }
+    ]
   }
 
   render() {
@@ -89,14 +176,25 @@ class SendBirthdayCard extends React.Component {
           templateData={{ user: this.props.user, contact: this.state.contact }}
           templateTypes={['Contact']}
         />
+
+        <Compose
+          isOpen={this.state.isComposeEmailOpen}
+          onClose={this.toggleComposeEmail}
+          recipients={this.Recipients}
+          html={this.state.templateScreenshot}
+          onClickSend={this.handleSendEmails}
+          isSubmitting={this.state.isSendingEmail}
+          disableAddNewRecipient
+        />
       </Fragment>
     )
   }
 }
 
-function mapStateToProps({ user }) {
+function mapStateToProps({ user, contacts }) {
   return {
-    user
+    user,
+    attributeDefs: contacts.attributeDefs
   }
 }
 
