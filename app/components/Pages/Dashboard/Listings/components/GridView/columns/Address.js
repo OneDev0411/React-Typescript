d7fd50296242cf9350ae0556a4473baea2b59946
OneDev0411 <cryptoapple1109@gmail.com>
2018-09-30 15:44:31 +0330

    web#1762 [MLS] added: grid view

---
+++ app/components/Pages/Dashboard/Listings/components/GridView/columns/Address.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Link } from 'react-router'
+import styled from 'styled-components'
+import Flex from 'styled-flex-component'
+
+import { grey, primary } from '../../../../../../../views/utils/colors'
+
+const Thumbnail = styled.div`
+  position: relative;
+  width: 2.5rem;
+  height: 2.5rem;
+  margin-right: 1rem;
+  border-radius: 3px;
+  background-color: ${grey.A200};
+
+  > img {
+    width: 100%;
+    height: 100%;
+    border-radius: inherit;
+  }
+`
+
+const Title = styled.div`
+  margin-bottom: 0.25em;
+  line-height: 1;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+
+  > a {
+    color: #000;
+
+    :hover {
+      color: ${primary};
+    }
+  }
+`
+
+export const Address = ({ listing }) => (
+  <Flex>
+    <Thumbnail>
+      {listing.cover_image_url && (
+        <img alt="a listing" src={listing.cover_image_url} />
+      )}
+    </Thumbnail>
+    <Flex column style={{ width: 'calc(100% - 3.5rem)' }}>
+      <Title>
+        <Link to={`/dashboard/mls/${listing.id}`}>{listing.address}</Link>
+      </Title>
+      <div style={{ color: `#${listing.statusColor}` }}>{listing.status}</div>
+    </Flex>
+  </Flex>
+)
