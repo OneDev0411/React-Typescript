479ff3f7cfb1ddc538da2e9b76e37a7da09de9c3
OneDev0411 <cryptoapple1109@gmail.com>
2018-10-01 13:57:46 +0330

    #1720 Update base drawer style

--- app/views/components/OverlayDrawer/Footer/styled.js
+++ app/views/components/OverlayDrawer/Footer/styled.js
@@ -1,10 +1,8 @@
 import Flex from 'styled-flex-component'
 
-import { brandBackground, borderColor } from '../../../utils/colors'
+import { borderColor } from '../../../utils/colors'
 
 export const Container = Flex.extend`
-  height: 63px;
-  padding: 0 1em;
-  border: 1px solid ${borderColor};
-  background-color: ${brandBackground};
+  height: 72px;
+  border-top: 1px solid ${borderColor};
 `
