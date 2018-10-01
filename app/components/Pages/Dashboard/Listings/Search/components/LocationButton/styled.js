ee13cb636975ee83ad0d23b780247f677641f20d
OneDev0411 <cryptoapple1109@gmail.com>
2018-10-01 14:13:24 +0330

    web#1762 [MLS/SEARCH] refactored: location button

---
+++ app/components/Pages/Dashboard/Listings/Search/components/LocationButton/styled.js
@@ -0,0 +1,15 @@
+import IconButton from '../../../../../../../views/components/Button/IconButton'
+
+export const Button = IconButton.extend`
+  position: absolute;
+  top: 1.5rem;
+  left: 1.5rem;
+  width: 2rem;
+  height: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #fff;
+  box-shadow: 0 12px 28px 0 rgba(0, 0, 0, 0.15),
+    0 8px 10px 0 rgba(0, 0, 0, 0.16);
+`
