791366299f1ad5f524677713bd42877397282f43
OneDev0411 <cryptoapple1109@gmail.com>
2018-10-01 15:44:16 +0330

    #1720 Update send email drawer

--- app/views/components/Forms/styled/index.js
+++ app/views/components/Forms/styled/index.js
@@ -5,14 +5,14 @@ export const InputContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   border-bottom: 1px solid #dce5eb;
-  padding: 4px 10px;
+  padding: 0.5rem 0;
   text-align: left;
 `
 
 export const InputLabel = styled.label`
   font-weight: 400;
   font-size: 13px;
-  color: ${props => (props.hasError ? 'red' : '#26465e')};
+  color: ${props => (props.hasError ? 'red' : '#000000')};
   font-weight: ${props => (props.hasError ? '500' : 'normal')};
 `
 
