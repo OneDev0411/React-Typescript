dcfdb930f64e304dfaa0f242e32a0ef549bcb0da
OneDev0411 <cryptoapple1109@gmail.com>
2018-10-03 00:33:52 +0330

    web#1762 [MLS] refactored: gallery view breakpoints

--- app/components/Pages/Dashboard/Listings/components/GalleryView/styled.js
+++ app/components/Pages/Dashboard/Listings/components/GalleryView/styled.js
@@ -10,7 +10,7 @@ export const rowStyle = css`
   padding: 0;
   border: none;
 
-  @media screen and (min-width: 48em) {
+  @media screen and (min-width: 64em) {
     width: calc((100% / 2) - 0.5rem);
     margin-right: 1rem;
 
@@ -19,7 +19,7 @@ export const rowStyle = css`
     }
   }
 
-  @media screen and (min-width: 64em) {
+  @media screen and (min-width: 90em) {
     width: calc((100% / 3) - (2rem / 3));
 
     &:nth-child(2n) {
@@ -31,7 +31,7 @@ export const rowStyle = css`
     }
   }
 
-  @media screen and (min-width: 90em) {
+  @media screen and (min-width: 110em) {
     width: calc((100% / 4) - 0.75rem);
 
     &:nth-child(3n) {
@@ -43,7 +43,7 @@ export const rowStyle = css`
     }
   }
 
-  @media screen and (min-width: 120em) {
+  @media screen and (min-width: 140em) {
     width: calc((100% / 5) - 0.8rem);
 
     &:nth-child(4n) {
