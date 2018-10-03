63e058b80173fa5eac1fd332f5b96af533184dca
OneDev0411 <cryptoapple1109@gmail.com>
2018-10-03 18:08:48 +0330

    #1720 Fix drawer dropdown shadow not complete

--- app/views/components/OverlayDrawer/Body/styled.js
+++ app/views/components/OverlayDrawer/Body/styled.js
@@ -5,6 +5,7 @@ const heightWithoutFooter = 'calc(100vh - 59px)'
 
 export const Container = styled.div`
   overflow: auto;
+  padding: 58px 1.5rem 0;
 
   min-height: ${props =>
     props.showFooter ? heightWithFooter : heightWithoutFooter};
