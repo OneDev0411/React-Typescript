8de0d9bd5304fbe2ba36ce540a971d96b2a33870
OneDev0411 <cryptoapple1109@gmail.com>
2018-10-07 17:29:47 +0330

    web#1805 [VIEWS/OH] added: Create Open House

---
+++ app/views/components/open-house/CreateOpenHouse/index.js
@@ -0,0 +1,52 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+
+import ActionButton from '../../Button/ActionButton'
+
+import { OpenHouseDrawer } from '../OpenHouseDrawer'
+
+const propTypes = {
+  deal: PropTypes.shape().isRequired,
+  style: PropTypes.shape(),
+  user: PropTypes.shape().isRequired
+}
+
+const defaultProps = {
+  style: {}
+}
+
+export class CreateOpenHouse extends React.Component {
+  state = {
+    isOpen: false
+  }
+
+  handleOpen = () => this.setState({ isOpen: true })
+  handleClose = () => this.setState({ isOpen: false })
+
+  render() {
+    return (
+      <Fragment>
+        <ActionButton
+          appearance="outline"
+          onClick={this.handleOpen}
+          style={this.props.style}
+        >
+          Create OH Event
+        </ActionButton>
+
+        {this.state.isOpen && (
+          <OpenHouseDrawer
+            isOpen
+            user={this.props.user}
+            deal={this.props.deal}
+            onClose={this.handleClose}
+            submitCallback={this.handleClose}
+          />
+        )}
+      </Fragment>
+    )
+  }
+}
+
+CreateOpenHouse.propTypes = propTypes
+CreateOpenHouse.defaultProps = defaultProps
