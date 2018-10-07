9f77424c49373d6aa8d15ab8d85bd152c56f39b9
OneDev0411 <cryptoapple1109@gmail.com>
2018-10-08 01:31:38 +0330

    web#1805 [CONTACTS/PROFILE] Added: OH Item to timeline

---
+++ app/components/Pages/Dashboard/Contacts/Profile/Timeline/OpenHouseItem/index.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import Flex from 'styled-flex-component'
+
+import getListing from '../../../../../../../models/listings/listing/get-listing'
+import { SingleMarkerMap } from '../../../../../../../views/components/maps/SingleMarkerMap'
+
+import { MetaInfo } from '../components/MetaInfo'
+import { Assignees } from '../components/Assignees'
+import { GeneralInfo } from '../components/GeneralInfo'
+import { Associations } from '../components/Associations'
+
+import { CRMTaskItem } from '../CRMTaskItem'
+import { MapContainer, Image } from './styled'
+
+export class OpenHouseItem extends React.Component {
+  state = {
+    listing: null
+  }
+
+  componentDidMount() {
+    this.fetchListing()
+  }
+
+  fetchListing = async () => {
+    try {
+      const listing = await getListing(this.props.task.listings[0])
+
+      this.setState({ listing })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  render() {
+    const { task } = this.props
+    const { listing } = this.state
+    const listingCoverImage = listing && listing.cover_image_url
+
+    return (
+      <CRMTaskItem
+        {...this.props}
+        render={({ onEdit, handleStatus, disabled }) => (
+          <React.Fragment>
+            <MetaInfo task={task} onEdit={onEdit} />
+            <Assignees task={task} />
+            <GeneralInfo
+              task={task}
+              disabled={disabled}
+              statusHandler={handleStatus}
+              onEdit={onEdit}
+            />
+            {listing && (
+              <Flex style={{ marginBottom: '1rem' }}>
+                <MapContainer hasCoverImage={listingCoverImage}>
+                  <SingleMarkerMap
+                    id={`OH_${task.id.replace(/-/gi, '_')}_map`}
+                    location={listing.property.address.location}
+                    options={{
+                      zoomControl: false,
+                      draggable: false
+                    }}
+                  />
+                </MapContainer>
+                {listingCoverImage && <Image image={listingCoverImage} />}
+              </Flex>
+            )}
+            <Associations
+              task={task}
+              user={this.props.user}
+              contact={this.props.contact}
+            />
+          </React.Fragment>
+        )}
+      />
+    )
+  }
+}
