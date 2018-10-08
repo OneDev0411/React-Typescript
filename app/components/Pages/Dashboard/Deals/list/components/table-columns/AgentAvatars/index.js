c54a9f2f2b5f32957adedd406baef278cf2326eb
OneDev0411 <cryptoapple1109@gmail.com>
2018-10-08 20:10:55 +0330

    #1814 Fix agent avatar tooltip not working

--- app/components/Pages/Dashboard/Deals/list/components/table-columns/AgentAvatars/index.js
+++ app/components/Pages/Dashboard/Deals/list/components/table-columns/AgentAvatars/index.js
@@ -2,11 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 import Avatar from 'react-avatar'
 import Tooltip from '../../../../../../../../views/components/tooltip'
-import { grey } from '../../../../../../../../views/utils/colors'
-
-const NoPrimaryAgent = styled.div`
-  color: ${grey.A550};
-`
 
 const AgentAvatars = ({ agent }) => {
   if (!agent) {
@@ -17,14 +12,16 @@ const AgentAvatars = ({ agent }) => {
 
   return (
     <Tooltip placement="bottom" caption={agent.legal_full_name}>
-      <Avatar
-        className="avatar"
-        color="#000000"
-        round
-        name={agent.legal_full_name}
-        src={image}
-        size={32}
-      />
+      <div style={{ display: 'inline-block' }}>
+        <Avatar
+          className="avatar"
+          color="#000000"
+          round
+          name={agent.legal_full_name}
+          src={image}
+          size={32}
+        />
+      </div>
     </Tooltip>
   )
 }
