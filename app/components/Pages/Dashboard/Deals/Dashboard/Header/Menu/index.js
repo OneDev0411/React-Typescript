8e2440a93073304e4075ab0164e53820d7188f16
OneDev0411 <cryptoapple1109@gmail.com>
2018-10-08 20:08:14 +0330

    add offer button

--- app/components/Pages/Dashboard/Deals/Dashboard/Header/Menu/index.js
+++ app/components/Pages/Dashboard/Deals/Dashboard/Header/Menu/index.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Flex from 'styled-flex-component'
 
+import LinkButton from 'components/Button/LinkButton'
 import { CloseButton } from 'components/Button/CloseButton'
+
 import { Divider } from '../../styled'
 
-export function Menu() {
+export function Menu(props) {
   return (
     <Flex alignCenter style={{ padding: '1.5em 0' }}>
+      {props.deal.deal_type === 'Selling' && (
+        <LinkButton
+          to={`/dashboard/deals/${props.deal.id}/create-offer`}
+          appearance="outline"
+        >
+          Add Offer
+        </LinkButton>
+      )}
+
       <Divider />
       <CloseButton isFit iconSize="large" inverse />
     </Flex>
