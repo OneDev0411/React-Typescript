5cb59706aa03294c36e69e66e90daa4b8bfc29e9
OneDev0411 <cryptoapple1109@gmail.com>
2018-10-22 14:22:49 +0330

    fix status color

--- app/components/Pages/Dashboard/Deals/Dashboard/ListingInfo/Image/index.js
+++ app/components/Pages/Dashboard/Deals/Dashboard/ListingInfo/Image/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import Avatar from 'components/Avatar'
 
 import Deal from 'models/Deal'
+import { getStatusColor } from 'utils/listing'
 
 export function ListingImage({ deal }) {
   const imageUrl = Deal.get.field(deal, 'photo')
+  const status = Deal.get.status(deal)
   const placeholderImageUrl = '/static/images/deals/group-146.svg'
 
   return (
@@ -13,7 +15,7 @@ export function ListingImage({ deal }) {
       image={imageUrl || placeholderImageUrl}
       title={deal.title}
       placeHolderImage=""
-      statusColor="#35b863"
+      statusColor={`#${getStatusColor(status)}`}
       isOnline
       showStatus
     />
