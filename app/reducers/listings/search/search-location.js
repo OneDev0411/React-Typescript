5bf1a5c24a9c3216c47a089e8069f656109e6396
OneDev0411 <cryptoapple1109@gmail.com>
2018-10-24 23:04:10 +0330

    web#1837 [REDUCERS/MLS] fixed: initial state of search location

--- app/reducers/listings/search/search-location.js
+++ app/reducers/listings/search/search-location.js
@@ -1,6 +1,6 @@
 import { SET_SEARCH_LOCATION } from '../../../constants/listings/search'
 
-export function location(state = {}, action) {
+export function location(state = null, action) {
   switch (action.type) {
     case SET_SEARCH_LOCATION:
       return action.center
