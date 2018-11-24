ed0f0f14c9e6315f199331ebeb176aea603a4076
OneDev0411 <cryptoapple1109@gmail.com>
2018-11-24 14:13:41 +0330

    web#1936 [VIEWS/SVG ICONS] fixed: circle spinner color

--- app/views/components/SvgIcons/CircleSpinner/IconCircleSpinner.js
+++ app/views/components/SvgIcons/CircleSpinner/IconCircleSpinner.js
@@ -22,7 +22,6 @@ const Image = styled.svg`
 const defaultProps = {
   children: [
     <path
-      fill="#fff"
       d="M73 50c0-12.7-10.3-23-23-23S27 37.3 27 50m3.9 0c0-10.5 8.5-19.1 19.1-19.1S69.1 39.5 69.1 50"
       key="key-0"
     >
