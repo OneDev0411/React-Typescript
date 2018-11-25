db976c31fbc5db2ae76bd1bafe339f627ead2703
OneDev0411 <cryptoapple1109@gmail.com>
2018-11-26 03:12:23 +0330

    web#1936 [MC/STORE] removed: Shortcuts :confounded:

--- app/components/Pages/Dashboard/Marketing/Store/index.js
+++ app/components/Pages/Dashboard/Marketing/Store/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import { Header } from './Header'
-import Shortcuts from './Shortcuts'
 import Cards from './Cards'
 
 export default class Store extends React.Component {
@@ -12,7 +11,6 @@ export default class Store extends React.Component {
           isSideMenuOpen={this.props.isSideMenuOpen}
           toggleSideMenu={this.props.toggleSideMenu}
         />
-        <Shortcuts />
         <Cards isSideMenuOpen={this.props.isSideMenuOpen} />
       </div>
     )
