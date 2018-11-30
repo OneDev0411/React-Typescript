d4d9a8f5fa83128d9c0d331b564502d00c71a63a
OneDev0411 <cryptoapple1109@gmail.com>
2018-11-30 19:03:18 +0330

    web#1936 [MC/STORE] refactored: business cards card

--- app/components/Pages/Dashboard/Marketing/Store/Cards/BusinessCards.js
+++ app/components/Pages/Dashboard/Marketing/Store/Cards/BusinessCards.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import Flex from 'styled-flex-component'
 
 import { Jumbo } from 'components/Typography/headings'
@@ -7,29 +7,49 @@ import { Jumbo } from 'components/Typography/headings'
 import { getMQWidth } from './helpers'
 import ComingSoon from './components/ComingSoonBadge'
 
-const Container = styled(Flex)`
+const absolute_box_style = css`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`
+
+const Container = styled.div`
+  position: relative;
   height: 25rem;
-  padding: 2rem;
   margin-bottom: 1.5rem;
-  text-align: center;
   border-radius: 3px;
   background: url('/static/images/marketing/store/cards/buisness-cards/buisness-cards.jpg')
     no-repeat;
   background-size: cover;
 
-  @media screen and (min-width: ${props => getMQWidth(75, props)}) {
+  @media (min-width: ${props => getMQWidth(75, props)}) {
     width: 38%;
     margin-right: 1.5rem;
   }
+
+  &:before {
+    content: '';
+    ${absolute_box_style};
+    background-color: rgba(0, 0, 0, 0.25);
+  }
+`
+
+const AbsoluteBox = styled(Flex)`
+  ${absolute_box_style};
+  padding: 2rem;
 `
 
 export default function BusinessCards(props) {
   return (
-    <Container center column isSideMenuOpen={props.isSideMenuOpen}>
-      <Jumbo style={{ marginBottom: '1rem', color: '#fff' }}>
-        Business Cards
-      </Jumbo>
-      <ComingSoon />
+    <Container isSideMenuOpen={props.isSideMenuOpen}>
+      <AbsoluteBox center column>
+        <Jumbo style={{ marginBottom: '1rem', color: '#fff' }}>
+          Business Cards
+        </Jumbo>
+        <ComingSoon />
+      </AbsoluteBox>
     </Container>
   )
 }
