d5709989b4a3bc614160910c775dd34c2177b77b
OneDev0411 <cryptoapple1109@gmail.com>
2018-12-03 19:07:31 +0330

    web#1936 [MC/STORE] added: all and my designs icons

--- app/components/Pages/Dashboard/Marketing/Menu/index.js
+++ app/components/Pages/Dashboard/Marketing/Menu/index.js
@@ -3,13 +3,24 @@ import React from 'react'
 import { items } from './data'
 import { ActiveItem } from './ActiveItem'
 import { ComingSoonItem } from './ComingSoonItem'
+import IconAllDesigns from '../components/IconAllDesigns/IconAllDesigns'
+import IconMyDesigns from '../components/IconMyDesigns/IconMyDesigns'
 
 export function Menu() {
   return (
     <React.Fragment>
       <div style={{ marginBottom: '2.5em' }}>
-        <ActiveItem indexed text="Marketing Center" to="/dashboard/marketing" />
-        <ActiveItem text="All My Designs" to="/dashboard/marketing/history" />
+        <ActiveItem
+          indexed
+          text="All Designs"
+          Icon={IconAllDesigns}
+          to="/dashboard/marketing"
+        />
+        <ActiveItem
+          text="My Designs"
+          Icon={IconMyDesigns}
+          to="/dashboard/marketing/history"
+        />
       </div>
 
       {items.map(({ title, url }, index) =>
