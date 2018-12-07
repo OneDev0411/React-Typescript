647f89e5ebbe94592ff28de26045f8b023fc378d
OneDev0411 <cryptoapple1109@gmail.com>
2018-12-07 18:03:14 +0330

    feat(mc): render image uploader react component in mc assets and set selected image to template (no upload yet)

--- app/views/components/InstantMarketing/Builder/AssetManager/index.js
+++ app/views/components/InstantMarketing/Builder/AssetManager/index.js
@@ -1,8 +1,15 @@
+import EventEmitter from 'events'
+
 import grapesjs from 'grapesjs'
 import Backbone from 'backbone'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { ImageUploader } from 'components/ImageUploader'
 
 export default grapesjs.plugins.add('asset-blocks', editor => {
   let target
+
   const AssetView = Backbone.View.extend({
     events: {
       click: 'onClick'
@@ -43,13 +50,68 @@ export default grapesjs.plugins.add('asset-blocks', editor => {
       return this
     }
   })
+  const ee = new EventEmitter()
+  const AssetUploadButtonView = Backbone.View.extend({
+    render() {
+      ReactDOM.render(
+        <ImageUploader
+          closeHandler={console.log}
+          stateChangEventEmitter={ee}
+          width={300}
+          height={200}
+          scale={1}
+          saveHandler={data => {
+            console.log(data)
+
+            const reader = new FileReader()
+
+            reader.addEventListener(
+              'load',
+              () => {
+                const dataURL = reader.result
+
+                console.log(dataURL)
+
+                const setSrc = () => target.set('src', dataURL)
+
+                const setters = {
+                  image: setSrc
+                }
+
+                const type = target.get('type')
+
+                if (type !== 'image') {
+                  return
+                }
+
+                setters[type]()
+              },
+              false
+            )
+
+            reader.readAsDataURL(data.file)
+          }}
+        />,
+        this.el
+      )
+
+      return this
+    }
+  })
 
   const AssetsView = Backbone.View.extend({
     initialize({ coll }) {
       this.collection = coll
     },
     reset() {
+      ee.emit('resetAndDismiss')
+
       this.$el.empty()
+
+      const uploadButtonView = new AssetUploadButtonView()
+
+      uploadButtonView.render()
+      uploadButtonView.$el.appendTo(this.el)
 
       for (let i = 0; i < this.collection.length; i++) {
         const asset = this.collection.at(i)
