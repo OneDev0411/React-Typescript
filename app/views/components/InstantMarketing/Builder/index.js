be82e190206d40aa6799e2d99e15dca6608bc3d3
OneDev0411 <cryptoapple1109@gmail.com>
2018-12-09 02:05:53 +0330

    web#1936 [VIEWS/MC/BUILDER] Fixed: next button initial render delay

--- app/views/components/InstantMarketing/Builder/index.js
+++ app/views/components/InstantMarketing/Builder/index.js
@@ -35,7 +35,7 @@ class Builder extends React.Component {
 
     this.state = {
       template: null,
-      selectedTemplate: null,
+      selectedTemplate: props.defaultTemplate,
       owner: props.templateData.user
     }
 
@@ -238,10 +238,6 @@ class Builder extends React.Component {
       }
     }))
 
-  get ShowSocialButtons() {
-    return this.props.mediums.includes('Social')
-  }
-
   get IsVideoTemplate() {
     return this.state.template && this.state.template.video
   }
@@ -258,6 +254,8 @@ class Builder extends React.Component {
   )
 
   render() {
+    const isSocialMedium = this.props.mediums !== 'Email'
+
     return (
       <Container className="template-builder">
         <Header>
@@ -274,47 +272,45 @@ class Builder extends React.Component {
               buttonRenderer={this.renderAgentPickerButton}
             />
 
-            {this.state.selectedTemplate &&
-              this.ShowSocialButtons && (
-                <Fragment>
-                  <ActionButton
-                    onClick={() => this.handleSocialSharing('Instagram')}
-                  >
-                    <i
-                      className="fa fa-instagram"
-                      style={{
-                        fontSize: '1.5rem',
-                        marginRight: '0.5rem'
-                      }}
-                    />
-                    Post to Instagram
-                  </ActionButton>
+            {this.state.selectedTemplate && isSocialMedium && (
+              <Fragment>
+                <ActionButton
+                  onClick={() => this.handleSocialSharing('Instagram')}
+                >
+                  <i
+                    className="fa fa-instagram"
+                    style={{
+                      fontSize: '1.5rem',
+                      marginRight: '0.5rem'
+                    }}
+                  />
+                  Post to Instagram
+                </ActionButton>
 
-                  <ActionButton
-                    style={{ marginLeft: '0.5rem' }}
-                    onClick={() => this.handleSocialSharing('Facebook')}
-                  >
-                    <i
-                      className="fa fa-facebook-square"
-                      style={{
-                        fontSize: '1.5rem',
-                        marginRight: '0.5rem'
-                      }}
-                    />
-                    Post to Facebook
-                  </ActionButton>
-                </Fragment>
-              )}
-
-            {this.state.selectedTemplate &&
-              !this.ShowSocialButtons && (
                 <ActionButton
                   style={{ marginLeft: '0.5rem' }}
-                  onClick={this.handleSave}
+                  onClick={() => this.handleSocialSharing('Facebook')}
                 >
-                  {this.props.saveButtonLabel}
+                  <i
+                    className="fa fa-facebook-square"
+                    style={{
+                      fontSize: '1.5rem',
+                      marginRight: '0.5rem'
+                    }}
+                  />
+                  Post to Facebook
                 </ActionButton>
-              )}
+              </Fragment>
+            )}
+
+            {this.state.selectedTemplate && !isSocialMedium && (
+              <ActionButton
+                style={{ marginLeft: '0.5rem' }}
+                onClick={this.handleSave}
+              >
+                {this.props.saveButtonLabel}
+              </ActionButton>
+            )}
 
             <Divider />
             <IconButton
@@ -345,13 +341,12 @@ class Builder extends React.Component {
             ref={ref => (this.grapes = ref)}
             style={{ position: 'relative' }}
           >
-            {this.IsVideoTemplate &&
-              this.IsTemplateLoaded && (
-                <VideoToolbar
-                  onRef={ref => (this.videoToolbar = ref)}
-                  editor={this.editor}
-                />
-              )}
+            {this.IsVideoTemplate && this.IsTemplateLoaded && (
+              <VideoToolbar
+                onRef={ref => (this.videoToolbar = ref)}
+                editor={this.editor}
+              />
+            )}
           </div>
         </BuilderContainer>
       </Container>
