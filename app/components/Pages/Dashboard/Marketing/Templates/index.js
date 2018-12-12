291673231356b8b68d85b90122f533090a8fc1a9
OneDev0411 <cryptoapple1109@gmail.com>
2018-12-12 17:17:09 +0330

    Revert "web#1936 [MC/STORE] Hide instagram"
    This reverts commit 18ba37787e463fb28f0501ac1968f8ce1d86ffa6.

--- app/components/Pages/Dashboard/Marketing/Templates/index.js
+++ app/components/Pages/Dashboard/Marketing/Templates/index.js
@@ -40,8 +40,8 @@ export default class Templates extends Component {
       const templates = await getTemplates(types.split(','), [
         'Email',
         'Social',
-        'FacebookCover'
-        // 'InstagramStory'
+        'FacebookCover',
+        'InstagramStory'
       ])
 
       const tabs = getMediums(templates)
