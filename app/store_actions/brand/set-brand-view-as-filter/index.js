44b9d4e624f9563986ab7e9d813a2553a597e971
OneDev0411 <cryptoapple1109@gmail.com>
2018-12-14 18:03:29 +0330

    web#1981 [ACTIONS/BRAND] added: setBrandViewAsFilter

---
+++ app/store_actions/brand/set-brand-view-as-filter/index.js
@@ -0,0 +1,23 @@
+import { batchActions } from 'redux-batched-actions'
+
+import { SET_ACTIVE_BRAND_VIEW_AS } from '../../../constants/brand'
+import { putUserSetting } from '../../../models/user/put-user-setting'
+import getUserTeams from '../../user/teams'
+
+export function setBrandViewAsFilter(user, value) {
+  return async dispatch => {
+    try {
+      await putUserSetting('view_as', value)
+
+      batchActions([
+        dispatch({
+          type: SET_ACTIVE_BRAND_VIEW_AS
+        }),
+        dispatch(getUserTeams(user, true))
+      ])
+    } catch (e) {
+      console.log(e)
+      throw e
+    }
+  }
+}
