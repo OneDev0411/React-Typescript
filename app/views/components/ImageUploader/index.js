4e70676270edb99cb660b1e5cbfe5cb2f3ba7550
OneDev0411 <cryptoapple1109@gmail.com>
2018-12-16 23:50:49 +0330

    fix(image uploader): add image formats to dropzone and remove some redundat code

--- app/views/components/ImageUploader/index.js
+++ app/views/components/ImageUploader/index.js
@@ -230,70 +230,75 @@ export class ImageUploader extends Component {
             backgroundColor: 'f2f2f2'
           }}
         >
-          {({ getRootProps, getInputProps, open }) => (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              textAlign: 'center',
+              height: '484px'
+            }}
+          >
+            <img
+              style={{
+                marginBottom: '25px'
+              }}
+              src="/static/icons/gif-png-jpg.svg"
+              alt="gif, png or jpg"
+            />
+            <h3
+              style={{
+                fontFamily: 'Barlow',
+                fontSize: '20px',
+                fontWeight: 'normal',
+                margin: '0'
+              }}
+            >
+              Drop your photo to upload
+            </h3>
             <div
               style={{
                 display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
+                flexDirection: 'row',
                 alignItems: 'center',
-                textAlign: 'center',
-                height: '484px'
+                width: '250px',
+                padding: '5px'
               }}
             >
-              <h3
-                style={{
-                  fontFamily: 'Barlow',
-                  fontSize: '20px',
-                  fontWeight: 'normal',
-                  margin: '0'
-                }}
-              >
-                Drop your photo to upload
-              </h3>
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  width: '250px',
-                  padding: '5px'
-                }}
-              >
-                <hr style={{ width: '100px' }} />
-                <span
-                  style={{
-                    opacity: '0.5',
-                    fontFamily: 'Barlow',
-                    fontWeight: 'normal',
-                    width: '20px'
-                  }}
-                >
-                  OR
-                </span>
-                <hr style={{ width: '100px' }} />
-              </div>
+              <hr style={{ width: '100px' }} />
               <span
                 style={{
-                  width: '182px',
-                  height: '40px',
-                  lineHeight: '40px',
+                  opacity: '0.5',
                   fontFamily: 'Barlow',
-                  fontSize: '16px',
-                  fontWeight: '500',
-                  fontStyle: 'normal',
-                  fontStretch: 'normal',
-                  letterSpacing: '0.1px',
-                  textAlign: 'center',
-                  color: '#ffffff',
-                  backgroundColor: '#004ce6',
-                  borderRadius: '5px'
+                  fontWeight: 'normal',
+                  width: '20px'
                 }}
               >
-                Choose from files
+                OR
               </span>
+              <hr style={{ width: '100px' }} />
             </div>
-          )}
+            <span
+              style={{
+                width: '182px',
+                height: '40px',
+                lineHeight: '40px',
+                fontFamily: 'Barlow',
+                fontSize: '16px',
+                fontWeight: '500',
+                fontStyle: 'normal',
+                fontStretch: 'normal',
+                letterSpacing: '0.1px',
+                textAlign: 'center',
+                color: '#ffffff',
+                backgroundColor: '#004ce6',
+                borderRadius: '5px'
+              }}
+            >
+              Choose from files
+            </span>
+          </div>
         </Dropzone>
       </Fragment>
     )
