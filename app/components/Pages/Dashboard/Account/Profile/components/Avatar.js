4c042306e480ef7297428498cc59f1ae9e2d2ba5
OneDev0411 <cryptoapple1109@gmail.com>
2018-12-24 16:57:38 +0330

    fix(user profile avatar): use avatar component instead of avatar uploader for showing user profile image

--- app/components/Pages/Dashboard/Account/Profile/components/Avatar.js
+++ app/components/Pages/Dashboard/Account/Profile/components/Avatar.js
@@ -1,64 +1,21 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import compose from 'recompose/compose'
-import withState from 'recompose/withState'
-import withHandlers from 'recompose/withHandlers'
 
-import editUser from 'actions/user/edit'
-import uploadAvatar from 'actions/user/upload-avatar'
+import { borderColor } from 'views/utils/colors'
 import { getUserInitials } from 'models/user/helpers/get-user-initials'
-
-import { AvatarUploader } from 'components/AvatarUploader'
+import AvatarImage from 'components/Avatar'
 
 const Avatar = props => (
-  <AvatarUploader
+  <AvatarImage
     {...props}
-    avatar={{
-      src: props.avatar,
-      initials: getUserInitials(props.user)
-    }}
+    image={props.user.profile_image_url}
+    initials={getUserInitials(props.user)}
+    backgroundColor={borderColor}
+    showStatus
   />
 )
 
-export default compose(
-  connect(),
-  withState('isUploading', 'setUploading', false),
-  withState(
-    'avatar',
-    'setAvatar',
-    props => props.user.profile_image_url || null
-  ),
-  withHandlers({
-    handleOnChange: ({ dispatch, setAvatar, setUploading }) => async data => {
-      const file = data.target ? data.target.files[0] : data.files.file
+Avatar.defaultProps = {
+  size: 104
+}
 
-      // Create a new FileReader instance
-      // https://developer.mozilla.org/en/docs/Web/API/FileReader
-      let reader = new FileReader()
-
-      // Once a file is successfully readed:
-      reader.addEventListener('load', async () => {
-        try {
-          setAvatar(reader.result)
-          setUploading(true)
-          await dispatch(uploadAvatar(file))
-        } catch (error) {
-          setAvatar(null)
-          console.log(error)
-        } finally {
-          setUploading(false)
-        }
-      })
-
-      reader.readAsDataURL(file)
-    },
-    handleOnDelete: ({ setAvatar, dispatch }) => async () => {
-      try {
-        await dispatch(editUser({ profile_image_url: '' }))
-        setAvatar(null)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-  })
-)(Avatar)
+export default Avatar
