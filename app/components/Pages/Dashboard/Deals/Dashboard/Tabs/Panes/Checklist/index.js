c8ef7d1524dae65770453127e8230a040846f599
OneDev0411 <cryptoapple1109@gmail.com>
2018-12-31 03:47:31 +0330

    move upload manager to checklist container

--- app/components/Pages/Dashboard/Deals/Dashboard/Tabs/Panes/Checklist/index.js
+++ app/components/Pages/Dashboard/Deals/Dashboard/Tabs/Panes/Checklist/index.js
@@ -1,26 +1,31 @@
-import React, { Fragment } from 'react'
+import React from 'react'
+
+import Flex from 'styled-flex-component'
 
 import Spinner from 'components/Spinner'
 
 import Checklists from '../../../Checklists'
 import FactsheetsNav from '../../../FactsheetsNav'
+import UploadManager from '../../../../UploadManager'
 
 import { FactsheetContainer, MainContainer } from '../../styled'
 
 export default function ChecklistPane(props) {
   return (
-    <Fragment>
-      <FactsheetContainer>
-        <FactsheetsNav deal={props.deal} isBackOffice={props.isBackOffice} />
-      </FactsheetContainer>
+    <UploadManager deal={props.deal} preventDropOnDocument={false} disableClick>
+      <Flex>
+        <FactsheetContainer>
+          <FactsheetsNav deal={props.deal} isBackOffice={props.isBackOffice} />
+        </FactsheetContainer>
 
-      <MainContainer>
-        {props.isFetchingChecklists ? (
-          <Spinner />
-        ) : (
-          <Checklists deal={props.deal} isBackOffice={props.isBackOffice} />
-        )}
-      </MainContainer>
-    </Fragment>
+        <MainContainer>
+          {props.isFetchingChecklists ? (
+            <Spinner />
+          ) : (
+            <Checklists deal={props.deal} isBackOffice={props.isBackOffice} />
+          )}
+        </MainContainer>
+      </Flex>
+    </UploadManager>
   )
 }
