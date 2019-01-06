9a93e66bc0477d969ef6d66834aedd10c98d2a08
OneDev0411 <cryptoapple1109@gmail.com>
2019-01-06 18:58:11 +0330

    sort digital-form and attachments based on last update

--- app/components/Pages/Dashboard/Deals/Dashboard/Checklists/TaskFiles/index.js
+++ app/components/Pages/Dashboard/Deals/Dashboard/Checklists/TaskFiles/index.js
@@ -1,25 +1,44 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import _ from 'underscore'
 
 import DigitalForm from './DigitalForm'
-import FileAttachments from './Attachments'
+import FileAttachment from './Attachment'
 
 import { Container } from './styled'
 
 class TaskFiles extends React.Component {
+  get Files() {
+    const { task } = this.props
+
+    const attachments = task.room.attachments || []
+
+    return _.sortBy(attachments.concat(task), item => item.updated_at * -1)
+  }
+
   render() {
     const { deal, task, isBackOffice } = this.props
 
     return (
       <Container isOpen={this.props.isOpen}>
-        <DigitalForm deal={deal} task={task} isBackOffice={isBackOffice} />
-
-        <FileAttachments
-          deal={deal}
-          task={task}
-          isBackOffice={isBackOffice}
-          attachments={task.room.attachments || []}
-        />
+        {this.Files.map((item, key) =>
+          item.type === 'task' ? (
+            <DigitalForm
+              key={key}
+              deal={deal}
+              task={item}
+              isBackOffice={isBackOffice}
+            />
+          ) : (
+            <FileAttachment
+              key={key}
+              deal={deal}
+              task={task}
+              isBackOffice={isBackOffice}
+              file={item}
+            />
+          )
+        )}
       </Container>
     )
   }
