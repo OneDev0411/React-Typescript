6663264cd7e5e8a917f9d460647664edce1d3d6b
OneDev0411 <cryptoapple1109@gmail.com>
2019-01-16 17:13:18 +0330

    fix(contacts): apply user filters after bulk event creation
    (cherry picked from commit 68eabe030e2d8e5a2801e8470a908682e5a24ac3)

--- app/components/Pages/Dashboard/Contacts/List/Table/index.js
+++ app/components/Pages/Dashboard/Contacts/List/Table/index.js
@@ -129,6 +129,7 @@ class ContactsList extends React.Component {
         <CreateEvent
           resetSelectedRows={resetSelectedRows}
           selectedRows={selectedRows}
+          filters={this.props.filters}
         />
       )
     },
