870d6e29a5a1baa192594cff98737eda2d093f91
OneDev0411 <cryptoapple1109@gmail.com>
2019-01-17 02:12:32 -0800

    Unused files.

--- app/models/tasks/schema.js
+++
@@ -1,5 +0,0 @@
-import { schema } from 'normalizr'
-
-const task = new schema.Entity('tasks')
-
-export const tasksSchema = { tasks: [task] }
--- app/utils/parse/index.js
+++
@@ -1,9 +0,0 @@
-import moment from 'moment'
-
-export function parseDate(date) {
-  if (date.length === 0) {
-    return 0
-  }
-
-  return moment.utc(date).unix()
-}
