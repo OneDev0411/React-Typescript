521793bc4b3f5f6ef2b1dd6a12e66c17c55d480d
OneDev0411 <cryptoapple1109@gmail.com>
2019-01-19 01:23:34 +0330

    deals: #2014 develop data map of actions

---
+++ app/components/Pages/Dashboard/Deals/components/ActionsButton/helpers/data-collection/index.js
@@ -0,0 +1,493 @@
+import _ from 'underscore'
+
+/**
+ * https://docs.google.com/spreadsheets/d/1XhJQK5Fq8V4dJLx98CAchOwwZci0yWoTFBSWV1deGY0
+ */
+
+const taskTypes = {
+  GENERIC: 'Generic',
+  FORM: 'Form'
+}
+
+const documentTypes = {
+  FORM: 'form',
+  PDF: 'pdf'
+}
+
+const envelopeStates = {
+  NONE: 'None',
+  DELIVERED: 'Delivered',
+  DECLINED: 'Declined',
+  VOIDED: 'Voided',
+  COMPLETED: 'Completed'
+}
+
+const EDIT_BUTTON = 'edit'
+const DOCUSIGN_BUTTON = 'docusign'
+const VOID_BUTTON = 'void'
+const RESEND_BUTTON = 'resend'
+const EMAIL_BUTTON = 'email'
+const NOTIFY_ADMIN_BUTTON = 'notify-button'
+const PRINT_BUTTON = 'print'
+const UPLOAD_BUTTON = 'upload'
+const RENAME_BUTTON = 'rename'
+
+function normalizeConditions(conditions) {
+  return conditions.map(item => ({
+    ...item,
+    actions: _.map(item.actions, (properties, id) => ({
+      id,
+      ...properties,
+      ...actionsDefaultProperties[id]
+    }))
+  }))
+}
+
+const actionsDefaultProperties = {
+  [EDIT_BUTTON]: {
+    label: 'Edit',
+    type: 'edit-form'
+  },
+  [DOCUSIGN_BUTTON]: {
+    label: 'Docusign',
+    type: 'get-signature'
+  },
+  [VOID_BUTTON]: {
+    label: 'Void',
+    type: 'void-envelope'
+  },
+  [RESEND_BUTTON]: {
+    label: 'Resend',
+    type: 'resend-envelope'
+  },
+  [EMAIL_BUTTON]: {
+    label: 'Email',
+    type: 'send-email'
+  },
+  [NOTIFY_ADMIN_BUTTON]: {
+    label: 'Notify Office',
+    type: 'notify-office'
+  },
+  [PRINT_BUTTON]: {
+    label: 'Print',
+    type: 'print'
+  },
+  [UPLOAD_BUTTON]: {
+    label: 'Upload',
+    type: 'upload'
+  },
+  [RENAME_BUTTON]: {
+    label: 'Rename',
+    type: 'rename'
+  }
+}
+
+export const attachmentsConditions = normalizeConditions([
+  {
+    conditions: ({
+      document_type,
+      file_uploaded,
+      form_saved,
+      envelope_status
+    }) =>
+      document_type === documentTypes.FORM &&
+      file_uploaded === null &&
+      form_saved === false &&
+      envelope_status === null,
+
+    actions: {
+      [EDIT_BUTTON]: {
+        primary: true
+      },
+      [DOCUSIGN_BUTTON]: {},
+      [EMAIL_BUTTON]: {},
+      [PRINT_BUTTON]: {}
+    }
+  },
+  {
+    conditions: ({
+      document_type,
+      file_uploaded,
+      form_saved,
+      envelope_status
+    }) =>
+      document_type === documentTypes.FORM &&
+      file_uploaded === null &&
+      form_saved === true &&
+      envelope_status === envelopeStates.NONE,
+
+    actions: {
+      [DOCUSIGN_BUTTON]: {
+        primary: true
+      },
+      [EDIT_BUTTON]: {},
+      [EMAIL_BUTTON]: {},
+      [PRINT_BUTTON]: {}
+    }
+  },
+  {
+    conditions: ({
+      document_type,
+      file_uploaded,
+      form_saved,
+      envelope_status
+    }) =>
+      document_type === documentTypes.FORM &&
+      file_uploaded === null &&
+      form_saved === true &&
+      envelope_status === envelopeStates.DELIVERED,
+
+    actions: {
+      [RESEND_BUTTON]: {
+        primary: true
+      },
+      [DOCUSIGN_BUTTON]: {
+        enabled: false
+      },
+      [EDIT_BUTTON]: {
+        enabled: false
+      },
+      [VOID_BUTTON]: {},
+      [EMAIL_BUTTON]: {},
+      [PRINT_BUTTON]: {}
+    }
+  },
+  {
+    conditions: ({
+      document_type,
+      file_uploaded,
+      form_saved,
+      envelope_status
+    }) =>
+      document_type === documentTypes.FORM &&
+      file_uploaded === null &&
+      form_saved === true &&
+      [envelopeStates.DECLINED, envelopeStates.VOIDED].includes(
+        envelope_status
+      ),
+
+    actions: {
+      [EDIT_BUTTON]: {
+        primary: true
+      },
+      [DOCUSIGN_BUTTON]: {},
+      [RESEND_BUTTON]: {},
+      [EMAIL_BUTTON]: {},
+      [PRINT_BUTTON]: {}
+    }
+  },
+  {
+    conditions: ({
+      document_type,
+      file_uploaded,
+      form_saved,
+      envelope_status
+    }) =>
+      document_type === documentTypes.FORM &&
+      file_uploaded === null &&
+      form_saved === true &&
+      envelope_status === envelopeStates.COMPLETED,
+
+    actions: {
+      [EMAIL_BUTTON]: {
+        primary: true
+      },
+      [EDIT_BUTTON]: {},
+      [DOCUSIGN_BUTTON]: {},
+      [RESEND_BUTTON]: {},
+      [PRINT_BUTTON]: {}
+    }
+  },
+  {
+    conditions: ({
+      document_type,
+      file_uploaded,
+      form_saved,
+      envelope_status
+    }) =>
+      document_type === documentTypes.PDF &&
+      file_uploaded === true &&
+      form_saved === null &&
+      envelope_status === envelopeStates.NONE,
+    actions: {
+      [DOCUSIGN_BUTTON]: {
+        primary: true
+      },
+      [EMAIL_BUTTON]: {},
+      [PRINT_BUTTON]: {},
+      [RENAME_BUTTON]: {}
+    }
+  },
+  {
+    conditions: ({
+      document_type,
+      file_uploaded,
+      form_saved,
+      envelope_status
+    }) =>
+      document_type === documentTypes.PDF &&
+      file_uploaded === true &&
+      form_saved === null &&
+      envelope_status === envelopeStates.DELIVERED,
+    actions: {
+      [RESEND_BUTTON]: {
+        primary: true
+      },
+      [DOCUSIGN_BUTTON]: {
+        enabled: false
+      },
+      [VOID_BUTTON]: {},
+      [EMAIL_BUTTON]: {},
+      [PRINT_BUTTON]: {},
+      [RENAME_BUTTON]: {}
+    }
+  },
+  {
+    conditions: ({
+      document_type,
+      file_uploaded,
+      form_saved,
+      envelope_status
+    }) =>
+      document_type === documentTypes.PDF &&
+      file_uploaded === true &&
+      form_saved === null &&
+      [envelopeStates.DECLINED, envelopeStates.VOIDED].includes(
+        envelope_status
+      ),
+    actions: {
+      [DOCUSIGN_BUTTON]: {},
+      [RESEND_BUTTON]: {},
+      [EMAIL_BUTTON]: {},
+      [PRINT_BUTTON]: {},
+      [RENAME_BUTTON]: {}
+    }
+  },
+  {
+    conditions: ({
+      document_type,
+      file_uploaded,
+      form_saved,
+      envelope_status
+    }) =>
+      document_type === documentTypes.PDF &&
+      file_uploaded === true &&
+      form_saved === null &&
+      envelope_status === envelopeStates.COMPLETED,
+    actions: {
+      [EMAIL_BUTTON]: {
+        primary: true
+      },
+      [DOCUSIGN_BUTTON]: {},
+      [RESEND_BUTTON]: {},
+      [PRINT_BUTTON]: {},
+      [RENAME_BUTTON]: {}
+    }
+  }
+])
+
+export const tasksConditions = normalizeConditions([
+  {
+    conditions: ({ task_type, file_uploaded, form_saved, envelope_status }) =>
+      task_type === taskTypes.GENERIC &&
+      file_uploaded === false &&
+      form_saved === null &&
+      envelope_status === null,
+    actions: {
+      [UPLOAD_BUTTON]: {
+        primary: true
+      },
+      [NOTIFY_ADMIN_BUTTON]: {}
+    }
+  },
+  {
+    conditions: ({ task_type, file_uploaded, form_saved, envelope_status }) =>
+      task_type === taskTypes.GENERIC &&
+      file_uploaded === true &&
+      form_saved === null &&
+      envelope_status === null,
+    actions: {
+      [DOCUSIGN_BUTTON]: {
+        primary: true
+      },
+      [EMAIL_BUTTON]: {},
+      [NOTIFY_ADMIN_BUTTON]: {},
+      [PRINT_BUTTON]: {},
+      [UPLOAD_BUTTON]: {}
+    }
+  },
+  {
+    conditions: ({ task_type, file_uploaded, form_saved, envelope_status }) =>
+      task_type === taskTypes.FORM &&
+      file_uploaded === false &&
+      form_saved === null &&
+      envelope_status === null,
+    actions: {
+      [EDIT_BUTTON]: {
+        primary: true
+      },
+      [EMAIL_BUTTON]: {},
+      [DOCUSIGN_BUTTON]: {},
+      [NOTIFY_ADMIN_BUTTON]: {},
+      [PRINT_BUTTON]: {},
+      [UPLOAD_BUTTON]: {}
+    }
+  },
+  {
+    conditions: ({ task_type, file_uploaded, form_saved, envelope_status }) =>
+      task_type === taskTypes.FORM &&
+      ((file_uploaded === false && form_saved === true) ||
+        (file_uploaded === true && form_saved === false)) &&
+      envelope_status === envelopeStates.NONE,
+    actions: {
+      [DOCUSIGN_BUTTON]: {
+        primary: true
+      },
+      [EDIT_BUTTON]: {},
+      [EMAIL_BUTTON]: {},
+      [NOTIFY_ADMIN_BUTTON]: {},
+      [PRINT_BUTTON]: {},
+      [UPLOAD_BUTTON]: {}
+    }
+  },
+  {
+    conditions: ({ task_type, file_uploaded, form_saved, envelope_status }) =>
+      task_type === taskTypes.FORM &&
+      ((file_uploaded === false && form_saved === true) ||
+        (file_uploaded === true && form_saved === false)) &&
+      envelope_status === envelopeStates.DELIVERED,
+    actions: {
+      [RESEND_BUTTON]: {
+        primary: true
+      },
+      [EDIT_BUTTON]: {
+        enabled: false,
+        tooltip:
+          'You cannot edit while you have sent this out for signature. You can either void or wait for the signature to get completed.'
+      },
+      [DOCUSIGN_BUTTON]: {},
+      [VOID_BUTTON]: {},
+      [EMAIL_BUTTON]: {},
+      [PRINT_BUTTON]: {},
+      [UPLOAD_BUTTON]: {}
+    }
+  },
+  {
+    conditions: ({ task_type, file_uploaded, form_saved, envelope_status }) =>
+      task_type === taskTypes.FORM &&
+      ((file_uploaded === false && form_saved === true) ||
+        (file_uploaded === true && form_saved === false)) &&
+      [envelopeStates.DECLINED, envelopeStates.VOIDED].includes(
+        envelope_status
+      ),
+    actions: {
+      [EDIT_BUTTON]: {
+        primary: true
+      },
+      [DOCUSIGN_BUTTON]: {},
+      [RESEND_BUTTON]: {},
+      [EMAIL_BUTTON]: {},
+      [NOTIFY_ADMIN_BUTTON]: {},
+      [PRINT_BUTTON]: {},
+      [UPLOAD_BUTTON]: {}
+    }
+  },
+  {
+    conditions: ({ task_type, file_uploaded, form_saved, envelope_status }) =>
+      task_type === taskTypes.FORM &&
+      ((file_uploaded === false && form_saved === true) ||
+        (file_uploaded === true && form_saved === false)) &&
+      envelope_status === envelopeStates.COMPLETED,
+    actions: {
+      [EDIT_BUTTON]: {
+        primary: true
+      },
+      [DOCUSIGN_BUTTON]: {},
+      [RESEND_BUTTON]: {},
+      [EMAIL_BUTTON]: {},
+      [NOTIFY_ADMIN_BUTTON]: {},
+      [PRINT_BUTTON]: {},
+      [UPLOAD_BUTTON]: {}
+    }
+  },
+  {
+    conditions: ({ task_type, file_uploaded, form_saved, envelope_status }) =>
+      task_type === taskTypes.FORM &&
+      file_uploaded === true &&
+      form_saved === true &&
+      envelope_status === envelopeStates.NONE,
+    actions: {
+      [DOCUSIGN_BUTTON]: {
+        primary: true
+      },
+      [EDIT_BUTTON]: {},
+      [EMAIL_BUTTON]: {},
+      [NOTIFY_ADMIN_BUTTON]: {},
+      [PRINT_BUTTON]: {},
+      [UPLOAD_BUTTON]: {}
+    }
+  },
+  {
+    conditions: ({ task_type, file_uploaded, form_saved, envelope_status }) =>
+      task_type === taskTypes.FORM &&
+      file_uploaded === true &&
+      form_saved === true &&
+      envelope_status === envelopeStates.DELIVERED,
+
+    actions: {
+      [RESEND_BUTTON]: {
+        primary: true
+      },
+      [EDIT_BUTTON]: {
+        enabled: false
+      },
+      [DOCUSIGN_BUTTON]: {},
+      [VOID_BUTTON]: {},
+      [EMAIL_BUTTON]: {},
+      [NOTIFY_ADMIN_BUTTON]: {},
+      [PRINT_BUTTON]: {},
+      [UPLOAD_BUTTON]: {}
+    }
+  },
+  {
+    conditions: ({ task_type, file_uploaded, form_saved, envelope_status }) =>
+      task_type === taskTypes.FORM &&
+      file_uploaded === true &&
+      form_saved === true &&
+      [envelopeStates.DECLINED, envelopeStates.VOIDED].includes(
+        envelope_status
+      ),
+    actions: {
+      [EDIT_BUTTON]: {
+        primary: true
+      },
+      [DOCUSIGN_BUTTON]: {},
+      [RESEND_BUTTON]: {},
+      [EMAIL_BUTTON]: {},
+      [NOTIFY_ADMIN_BUTTON]: {},
+      [PRINT_BUTTON]: {},
+      [UPLOAD_BUTTON]: {}
+    }
+  },
+  {
+    conditions: ({ task_type, file_uploaded, form_saved, envelope_status }) =>
+      task_type === taskTypes.FORM &&
+      file_uploaded === true &&
+      form_saved === true &&
+      envelope_status === envelopeStates.COMPLETED,
+    actions: {
+      [RESEND_BUTTON]: {
+        primary: true
+      },
+      // [EMAIL_BUTTON]: {
+      //   primary: true
+      // },
+      [EDIT_BUTTON]: {},
+      [DOCUSIGN_BUTTON]: {},
+      // [RESEND_BUTTON]: {},
+      [NOTIFY_ADMIN_BUTTON]: {},
+      [PRINT_BUTTON]: {},
+      [UPLOAD_BUTTON]: {}
+    }
+  }
+])
