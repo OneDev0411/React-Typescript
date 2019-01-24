777d5bedaaca5c0561fdfdf013d22d3f7bc28d49
OneDev0411 <cryptoapple1109@gmail.com>
2019-01-25 00:52:44 +0330

    deals: develop get-file-url helper to return more data

--- app/components/Pages/Dashboard/Deals/components/ActionsButton/helpers/get-file-url/index.js
+++ app/components/Pages/Dashboard/Deals/components/ActionsButton/helpers/get-file-url/index.js
@@ -1,26 +1,52 @@
 function getDocumentsUrl({ deal, task, document, isBackOffice }) {
   if (document.type === 'task' && document.submission) {
     return isBackOffice
-      ? [{ url: `/dashboard/deals/${deal.id}/view/${document.id}` }]
-      : [{ url: document.submission.file.url }]
+      ? [
+          {
+            id: task.submission.file.id,
+            name: task.submission.file.name,
+            url: `/dashboard/deals/${deal.id}/view/${task.id}`
+          }
+        ]
+      : [
+          {
+            id: task.submission.file.id,
+            name: task.submission.file.name,
+            url: task.submission.file.url
+          }
+        ]
   }
 
   return isBackOffice
     ? [
         {
+          id: document.id,
+          name: document.name,
           url: `/dashboard/deals/${deal.id}/view/${task.id}/attachment/${
             document.id
           }`
         }
       ]
-    : [{ url: document.url }]
+    : [{ id: document.id, name: document.name, url: document.url }]
 }
 
 function getTaskDocumentsUrl({ deal, task, isBackOffice }) {
   if (task.submission) {
     return isBackOffice
-      ? [{ url: `/dashboard/deals/${deal.id}/view/${task.id}` }]
-      : [{ url: task.submission.file.url }]
+      ? [
+          {
+            id: task.submission.file.id,
+            name: task.submission.file.name,
+            url: `/dashboard/deals/${deal.id}/view/${task.id}`
+          }
+        ]
+      : [
+          {
+            id: task.submission.file.id,
+            name: task.submission.file.name,
+            url: task.submission.file.url
+          }
+        ]
   }
 
   const attachments = task.room.attachments.sort(
@@ -30,15 +56,29 @@ function getTaskDocumentsUrl({ deal, task, isBackOffice }) {
   return isBackOffice
     ? [
         {
+          id: attachments[0].id,
+          name: attachments[0].name,
           url: `/dashboard/deals/${deal.id}/view/${task.id}/attachment/${
             attachments[0].id
           }`
         }
       ]
-    : [{ url: attachments[0].url }]
+    : [
+        {
+          id: attachments[0].id,
+          name: attachments[0].name,
+          url: attachments[0].url
+        }
+      ]
 }
 
-export function getFileUrl({ type, deal, task, document, isBackOffice }) {
+export function getFileUrl({
+  type,
+  deal,
+  task,
+  document,
+  isBackOffice = false
+}) {
   if (type === 'task') {
     return getTaskDocumentsUrl({ deal, task, document, isBackOffice })
   }
