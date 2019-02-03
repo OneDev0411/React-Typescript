3bb8ae88a2b3294ab0c9349b415b01bfcffaa5da
OneDev0411 <cryptoapple1109@gmail.com>
2019-02-03 18:02:47 +0330

    deals: refactor Notification styles

--- app/components/Pages/Dashboard/Deals/Dashboard/Folders/Checklist/Notification/styled.js
+++ app/components/Pages/Dashboard/Deals/Dashboard/Folders/Checklist/Notification/styled.js
@@ -1,20 +1,25 @@
 import styled from 'styled-components'
 
-import Badge from 'components/Badge'
+import { red } from 'views/utils/colors'
 
 export const Container = styled.div`
   position: relative;
-  margin-right: 1rem;
+  margin-right: 0.25rem;
   cursor: pointer;
 
-  svg path {
-    fill: #17181a;
+  svg {
+    fill: #d1d1d1;
   }
 `
 
-export const BadgeCounter = styled(Badge)`
+export const BadgeCounter = styled.div`
   position: absolute;
-  font-size: 0.75rem;
-  top: -0.2rem;
-  right: -0.6rem;
+  background: ${red.A100};
+  top: -3px;
+  right: -3px;
+  font-size: 10px;
+  color: #fff;
+  border-radius: 4px;
+  padding: 0 3px;
+  border: 2px solid #fff;
 `
