9dd2f559431b707ab830049b03d95fd511c24997
OneDev0411 <cryptoapple1109@gmail.com>
2019-02-08 18:38:02 +0330

    #2233 [CONTACT/PROFILE] updated: section styles

--- app/components/Pages/Dashboard/Contacts/Profile/components/Section/index.js
+++ app/components/Pages/Dashboard/Contacts/Profile/components/Section/index.js
@@ -1,36 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
 
-import { borderColor } from '../../../../../../../views/utils/colors'
 import { Menu } from './Menu'
-import { Header, Title } from './styled'
-
-const Container = styled.div`
-  padding: 1.5rem;
-
-  &:not(:last-of-type) {
-    padding-bottom: 0;
-
-    :after {
-      margin-top: 1.5em;
-      display: block;
-      content: '';
-      height: 1px;
-      width: 3.25rem;
-      background-color: ${borderColor};
-    }
-  }
-
-  &:hover .menu__icon {
-    fill: #000 !important;
-  }
-`
+import { Container, Header, Title } from './styled'
 
 Section.propTypes = {
   onAdd: PropTypes.func,
   onEdit: PropTypes.func,
-  title: PropTypes.string
+  title: PropTypes.string,
+  isNew: PropTypes.bool
+}
+
+Section.defaultProps = {
+  isNew: false
 }
 
 export function Section(props) {
@@ -40,7 +22,9 @@ export function Section(props) {
         <Title>{props.title}</Title>
         <Menu {...props} />
       </Header>
-      <div>{props.children}</div>
+      <div style={{ padding: props.isNew ? 0 : '0 1.5rem' }}>
+        {props.children}
+      </div>
     </Container>
   )
 }
