0b79d1ef830f74a556e841287e75a76cbbb8cfb4
OneDev0411 <cryptoapple1109@gmail.com>
2019-02-14 16:39:42 +0330

    #2234 [VIEWS/INLINE FIELDS] Added: InlineEditableField

---
+++ app/views/components/inline-editable-fields/InlineEditableField/EditMode/index.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { noop } from 'utils/helpers'
+import IconButton from 'components/Button/IconButton'
+import ActionButton from 'components/Button/ActionButton'
+import DeleteIcon from 'components/SvgIcons/DeleteOutline/IconDeleteOutline'
+
+import { ActionBar, Container } from './styled'
+
+export class EditMode extends React.Component {
+  static propTypes = {
+    handleDelete: PropTypes.func,
+    handleSave: PropTypes.func.isRequired,
+    isDisabled: PropTypes.bool,
+    render: PropTypes.func.isRequired,
+    showDelete: PropTypes.bool,
+    toggleMode: PropTypes.func.isRequired
+  }
+
+  static defaultProps = {
+    isDisabled: false,
+    handleDelete: noop,
+    showDelete: false
+  }
+
+  onSave = () => this.props.handleSave({}, this.props.toggleMode)
+
+  onDelete = () => this.props.handleDelete(this.props.toggleMode)
+
+  render() {
+    const { isDisabled, showDelete } = this.props
+
+    return (
+      <Container>
+        {this.props.render(this.props)}
+        <ActionBar showDelete={showDelete}>
+          {showDelete && (
+            <IconButton isFit disabled={isDisabled} onClick={this.onDelete}>
+              <DeleteIcon />
+            </IconButton>
+          )}
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center'
+            }}
+          >
+            <ActionButton
+              size="small"
+              appearance="link"
+              disabled={isDisabled}
+              onClick={this.props.toggleMode}
+            >
+              Cancel
+            </ActionButton>
+            <ActionButton
+              size="small"
+              disabled={isDisabled}
+              onClick={this.onSave}
+            >
+              {isDisabled ? 'Saving...' : 'Save'}
+            </ActionButton>
+          </div>
+        </ActionBar>
+      </Container>
+    )
+  }
+}
