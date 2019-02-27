fa8a3018d43f5925fba2d5ba7f5ef4db4c70e9b8
OneDev0411 <cryptoapple1109@gmail.com>
2019-02-27 15:07:31 +0330

    #2301 feat(components): implement ShowMoreLess component

---
+++ app/views/components/ShowMoreLess/index.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+import Flex from 'styled-flex-component'
+
+import { ShowMoreLessText } from './styled'
+
+export class ShowMoreLess extends Component {
+  static defaultProps = {
+    isOpen: false,
+    row: false,
+    column: true,
+    count: 7,
+    moreText: 'Show More',
+    lessText: 'Show Less',
+    style: {}
+  }
+
+  state = {
+    isOpen: this.props.isOpen
+  }
+
+  toggleState = () => {
+    this.setState(({ isOpen }) => ({
+      isOpen: !isOpen
+    }))
+  }
+
+  renderShowMore = () => (
+    <ShowMoreLessText onClick={this.toggleState}>
+      {this.props.moreText}
+    </ShowMoreLessText>
+  )
+
+  renderShowLess = () => (
+    <ShowMoreLessText onClick={this.toggleState}>
+      {this.props.lessText}
+    </ShowMoreLessText>
+  )
+
+  renderOpenState() {
+    return (
+      <Flex
+        column={this.props.column}
+        row={this.props.row}
+        style={this.props.style}
+      >
+        {this.props.children}
+        {this.renderShowLess()}
+      </Flex>
+    )
+  }
+
+  renderCloseState() {
+    return (
+      <Flex
+        column={this.props.column}
+        row={this.props.row}
+        style={this.props.style}
+      >
+        {React.Children.map(this.props.children, (child, i) =>
+          i < this.props.count ? child : null
+        )}
+        {this.renderShowMore()}
+      </Flex>
+    )
+  }
+
+  render() {
+    if (this.state.isOpen) {
+      return this.renderOpenState()
+    }
+
+    return this.renderCloseState()
+  }
+}
