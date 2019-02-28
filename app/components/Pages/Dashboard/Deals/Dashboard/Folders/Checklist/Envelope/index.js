861e3aaebe5dddd9a6229491a360aea4e1a6b63c
OneDev0411 <cryptoapple1109@gmail.com>
2019-02-28 15:29:38 +0300

    deals: remove deprecated sort

--- app/components/Pages/Dashboard/Deals/Dashboard/Folders/Checklist/Envelope/index.js
+++ app/components/Pages/Dashboard/Deals/Dashboard/Folders/Checklist/Envelope/index.js
@@ -26,15 +26,6 @@ class EnvelopeView extends React.Component {
       .filter(
         envelope => ['Voided', 'Declined'].includes(envelope.status) === false
       )
-      .sort((a, b) => {
-        if (a.status === 'Completed')
-          return -1
-
-        if (b.status === 'Completed')
-          return 1
-
-        return 0
-      })
       .sort((a, b) => b.created_at - a.created_at)
 
     if (envelopes.length === 0) {
