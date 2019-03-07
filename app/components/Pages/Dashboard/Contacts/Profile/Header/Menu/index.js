2dc27254b20108df4036e457fae7c5d072f2c954
OneDev0411 <cryptoapple1109@gmail.com>
2019-03-07 14:47:10 +0300

    #2377 add Send Email component to Contact’s profile

--- app/components/Pages/Dashboard/Contacts/Profile/Header/Menu/index.js
+++ app/components/Pages/Dashboard/Contacts/Profile/Header/Menu/index.js
@@ -3,6 +3,8 @@ import Flex from 'styled-flex-component'
 
 import SendContactCard from 'components/InstantMarketing/adapters/SendContactCard'
 
+import SendEmailButton from 'components/SendEmailButton'
+
 import { CloseButton } from 'components/Button/CloseButton'
 
 import Chat from './ChatButton'
@@ -14,7 +16,7 @@ export function Menu(props) {
   return (
     <Flex alignCenter style={{ padding: '1.5em 0' }}>
       <SendContactCard contact={contact}>Send a Card</SendContactCard>
-
+      <SendEmailButton style={{ marginLeft: '1rem' }} />
       <Chat contact={contact} />
       <Divider />
       <CloseButton isFit iconSize="large" inverse />
