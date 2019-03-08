b601622691f30756d93cd74ebf7e687365516e93
OneDev0411 <cryptoapple1109@gmail.com>
2019-03-08 22:44:50 +0330

    #2384 [Models/Brand] added: get flows

---
+++ app/models/brand/get-brand-flows.js
@@ -0,0 +1,19 @@
+import Fetch from '../../services/fetch'
+
+const defaultQuery = {
+  associations: ['brand_flow.steps', 'brand_flow_step.event']
+}
+
+export async function getBrandFlows(id, query = defaultQuery) {
+  if (!id) {
+    throw new Error(`The brand id must be a UUID. ${id}`)
+  }
+
+  try {
+    const response = await new Fetch().get(`/brands/${id}/flows`).query(query)
+
+    return response.body.data
+  } catch (error) {
+    throw error
+  }
+}
