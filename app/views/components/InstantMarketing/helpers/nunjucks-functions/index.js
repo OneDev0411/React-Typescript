7d2571df6e4f43c16ea4f7370b7c69db0dec8779
OneDev0411 <cryptoapple1109@gmail.com>
2019-03-15 01:42:52 +0300

    marketing center: #2297 fix redirecting listing button

--- app/views/components/InstantMarketing/helpers/nunjucks-functions/index.js
+++ app/views/components/InstantMarketing/helpers/nunjucks-functions/index.js
@@ -1,13 +1,15 @@
 import flattenBrand from 'utils/flatten-brand'
 
+import config from '../../../../../../config/public'
+
 export const getAsset = (brand, name) =>
   flattenBrand(brand).assets.marketing[name] || ''
 
-export function getListingUrl(brand, listing) {
-  const brandData = flattenBrand(brand)
-  const { listing_url } = brandData.message
+export function getListingUrl(activeBrand, listing) {
+  const brand = flattenBrand(activeBrand)
+  const listing_url = brand.messages && brand.messages.listing_url
 
   return listing_url
     ? listing_url.replace('%1', listing.mls_number)
-    : `https://rechat.com/dashboard/mls/${listing.mls_number}`
+    : `${config.app.url}/dashboard/mls/${listing.id}`
 }
