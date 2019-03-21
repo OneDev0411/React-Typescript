5fdd430ac64bd061320f35b38edbd459513daf5d
OneDev0411 <cryptoapple1109@gmail.com>
2019-03-22 02:11:52 +0300

    deals: #1201 refactor helper of get deal status

--- app/models/Deal/helpers/context/get-status/index.js
+++ app/models/Deal/helpers/context/get-status/index.js
@@ -1,5 +1,6 @@
 import { getField } from '../get-field'
+import { getStatusField } from '../../dynamic-context'
 
 export function getStatus(deal) {
-  return deal.deleted_at ? 'Archived' : getField(deal, 'listing_status')
+  return deal.deleted_at ? 'Archived' : getField(deal, getStatusField(deal))
 }
