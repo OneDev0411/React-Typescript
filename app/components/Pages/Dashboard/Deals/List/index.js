dc1673b87a8233c3e49672dd35b9489ea47debc7
OneDev0411 <cryptoapple1109@gmail.com>
2019-03-21 22:10:21 +0300

    deals: lint file

--- app/components/Pages/Dashboard/Deals/List/index.js
+++ app/components/Pages/Dashboard/Deals/List/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+
 import AgentTable from './Agent'
 import BackOfficeTable from './BackOffice'
 import { isBackOffice } from '../../../../../utils/user-teams'
