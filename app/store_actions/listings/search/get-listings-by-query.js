2dbda55c81a4512eacbfa8ca66f3f0bbb43f460a
OneDev0411 <cryptoapple1109@gmail.com>
2019-03-26 00:55:42 +0430

    #2441 [Actions/MLS] updated: getListingsByQuery

--- app/store_actions/listings/search/get-listings-by-query.js
+++ app/store_actions/listings/search/get-listings-by-query.js
@@ -1,25 +1,62 @@
+import { normalize } from 'normalizr'
 import { batchActions } from 'redux-batched-actions'
 
+import { setMapProps, setOnMapAutoMove } from 'actions/listings/map'
+import searchActions from 'actions/listings/search'
+
+import extendedBounds from 'utils/extendedBounds'
+
+import * as schema from 'models/listings/schema'
 import { searchListings } from 'models/listings/search/search-listings'
 
 import * as listingsTypes from '../../../constants/listings'
 
-export const getListingsByQuery = (text, query) => async dispatch => {
+export const getListingsByQuery = (text, query) => async (
+  dispatch,
+  getState
+) => {
   try {
-    batchActions([
-      dispatch({
-        tabName: 'search',
-        type: listingsTypes.FETCH_LISTINGS_REQUEST
-      })
-    ])
+    dispatch({
+      tabName: 'search',
+      type: listingsTypes.FETCH_LISTINGS_REQUEST
+    })
 
     const response = await searchListings(text, query)
 
-    dispatch({
-      response,
+    const { code, info, data } = response
+
+    const normilizedListings = normalize(data, schema.listingsList)
+
+    const successPatch = {
+      response: {
+        ...normilizedListings,
+        info: {
+          code,
+          ...info
+        }
+      },
       tabName: 'search',
       type: listingsTypes.FETCH_LISTINGS_SUCCESS
-    })
+    }
+
+    if (data.length === 0) {
+      return dispatch(successPatch)
+    }
+
+    const newProps = extendedBounds(
+      data.map(({ location: { latitude: lat, longitude: lng } }) => ({
+        lat,
+        lng
+      })),
+      getState().search.map.props
+    )
+
+    batchActions([
+      dispatch(setOnMapAutoMove()),
+      dispatch(setMapProps('search', newProps)),
+      dispatch(successPatch),
+      dispatch(searchActions.setSearchInput(text))
+    ])
   } catch (error) {
     dispatch({
       tabName: 'search',
