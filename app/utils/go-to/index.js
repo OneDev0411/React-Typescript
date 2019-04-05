e83e283933c1b6b880262e48ed8d2d1f6e266eb6
OneDev0411 <cryptoapple1109@gmail.com>
2019-04-05 18:58:31 +0430

    #2345 feat(utils): add query param object in go-to helper and pass it to destination

--- app/utils/go-to/index.js
+++ app/utils/go-to/index.js
@@ -4,16 +4,38 @@ import { browserHistory } from 'react-router'
  * Going to a route with memorizing current page URL and title as a state
  * @param {string} url Next page url.
  * @param {string || null} pageTitle Current page title.
+ * @param {object} query Query params as an object
+ * @param {object} state Additional state params as an object
  */
 
-export function goTo(url, pageTitle) {
+export function goTo(url, pageTitle = null, query = {}, state = {}) {
+  const previousPage = pageTitle ? {
+    previousPage: {
+      url: window && window.location.pathname,
+      title: pageTitle
+    }
+  } : {}
+
+  const search = Object.keys(query).reduce((acc, curr, index) => {
+    const mark = index === 0 ? '?' : '&'
+    return `${acc}${mark}${curr}=${query[curr]}`
+  }, '')
+
+  console.log('GO TO', {
+    pathname: url,
+    search,
+    state: {
+      ...state,
+      previousPage
+    }
+  })
+
   browserHistory.push({
     pathname: url,
-    state: pageTitle ? {
-      previousPage: {
-        url: window && window.location.pathname,
-        title: pageTitle
-      }
-    } : undefined
+    search,
+    state: {
+      ...state,
+      previousPage
+    }
   })
 }
