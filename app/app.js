5f2894a8e2e8bc502dafe2552cee2013f23c26ac
OneDev0411 <cryptoapple1109@gmail.com>
2019-04-05 19:17:52 +0430

    #2345 feat: register smoothscroll polyfill

--- app/app.js
+++ app/app.js
@@ -2,9 +2,12 @@ import React from 'react'
 import NotificationsSystem from 'reapop'
 import notificationTheme from 'reapop-theme-wybo'
 import useScroll from 'react-router-scroll/lib/useScroll'
+
 import ConfirmationModal from './components/Partials/Confirmation'
+
 import { Router, browserHistory, applyRouterMiddleware } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
+import smoothscroll from 'smoothscroll-polyfill'
 
 import { hot } from 'react-hot-loader/root'
 
@@ -17,9 +20,13 @@ import routes from './routes'
 // store
 import store from './stores'
 
-
 // history
 const history = syncHistoryWithStore(browserHistory, store)
+
+// smooth scroll polyfill
+if (typeof window !== 'undefined') {
+  smoothscroll.polyfill()
+}
 
 const A = () => (
   <div>
