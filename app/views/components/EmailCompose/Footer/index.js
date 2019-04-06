abce4209f91ed6e0d79d0859a4f8cc66ca973bd4
OneDev0411 <cryptoapple1109@gmail.com>
2019-04-07 02:02:55 +0430

    adding scheduler to EmailCompose

--- app/views/components/EmailCompose/Footer/index.js
+++ app/views/components/EmailCompose/Footer/index.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Flex from 'styled-flex-component'
-
 import { Field } from 'react-final-form'
 
 import ActionButton from 'components/Button/ActionButton'
+import IconButton from 'components/Button/IconButton'
+import TimeIcon from 'components/SvgIcons/Time/IconTime'
+import Tooltip from 'components/tooltip'
+
+import DatePicker from '../../../../components/Pages/Dashboard/Deals/components/DatePicker'
 
 import { AddDealFile } from '../components/AddDealFile'
 
 export function Footer(props) {
+  const [isSchedulerOpen, setSchedulerOpen] = useState(true)
+
   return (
     <Flex justifyBetween alignCenter style={{ width: '100%' }}>
       <Flex>
@@ -21,13 +27,39 @@ export function Footer(props) {
         )}
       </Flex>
 
-      <ActionButton
-        type="submit"
-        disabled={props.isSubmitting}
-        onClick={props.handleSubmit}
-      >
-        {props.isSubmitting ? 'Sending...' : 'Send'}
-      </ActionButton>
+      <div>
+        <DatePicker
+          show={isSchedulerOpen}
+          saveText="Schedule"
+          onClose={() => setSchedulerOpen(false)}
+          // saveText={contexts[selectedField] ? 'Update Date' : 'Add Date'}
+          // initialDate={contexts[selectedField]}
+          // onSelectDate={date => this.onChangeDateContext(date)}
+          containerStyle={{
+            top: '-8px',
+            right: '0',
+            left: 'unset',
+            transform: 'translateY(-100%)'
+          }}
+        />
+        <ActionButton
+          type="submit"
+          disabled={props.isSubmitting}
+          onClick={props.handleSubmit}
+        >
+          {props.isSubmitting ? 'Sending...' : 'Send'}
+        </ActionButton>
+
+        <Tooltip caption="Schedule Email">
+          <IconButton
+            inverse
+            appearance="primary"
+            onClick={() => setSchedulerOpen(!isSchedulerOpen)}
+          >
+            <TimeIcon />
+          </IconButton>
+        </Tooltip>
+      </div>
     </Flex>
   )
 }
