f78c0ee0f4102826c2b42996465d0bfd113346f4
OneDev0411 <cryptoapple1109@gmail.com>
2019-04-10 20:14:17 +0430

    #2404 feat(mc): add keepDealsWithoutListing param to get mls drawer initial deals to keep deals without listing in the list

--- app/views/components/InstantMarketing/helpers/get-mls-drawer-initial-deals/index.js
+++ app/views/components/InstantMarketing/helpers/get-mls-drawer-initial-deals/index.js
@@ -3,13 +3,18 @@ import _ from 'underscore'
 /**
  * returns a list of deals to show as initial list in listings drawer
  * @param {Object} deals - list of deals
+ * @param {Boolean} keepDealsWithoutListing - keep deals without listing
  * the filtering and sorting logic is a bit strange
  * https://gitlab.com/rechat/templates/issues/13#note_122552988
  */
 /** */
-export function getMlsDrawerInitialDeals(deals) {
-  return _.chain(deals)
-    .filter(deal => deal.listing !== null)
-    .sortBy(deal => (deal.deal_type === 'Selling' ? 1 : 2))
-    .value()
+export function getMlsDrawerInitialDeals(
+  deals,
+  keepDealsWithoutListing = false
+) {
+  const chain = keepDealsWithoutListing
+    ? _.chain(deals)
+    : _.chain(deals).filter(deal => deal.listing !== null)
+
+  return chain.sortBy(deal => (deal.deal_type === 'Selling' ? 1 : 2)).value()
 }
