d2174845a8cc7040c4a52cc977c0a49c9a31323e
OneDev0411 <cryptoapple1109@gmail.com>
2019-04-12 22:47:12 +0430

    code formatting for UserEntry

--- app/views/components/ConfirmationModal/UserEntry.js
+++ app/views/components/ConfirmationModal/UserEntry.js
@@ -1,21 +1,22 @@
 import React from 'react'
 
 function UserEntry(props, ref) {
-  if (props.isShow) {
-    const sharedProps = {
-      defaultValue: props.inputDefaultValue,
-      placeholder: props.inputPlaceholder,
-      ref
-    }
-
-    if (props.multilineEntry === false) {
-      return <input {...sharedProps} className="confirmation-input textinput" />
-    }
-
-    return <textarea {...sharedProps} className="confirmation-input textarea" />
+  if (!props.isShow) {
+    return null
   }
 
-  return null
+  const sharedProps = {
+    defaultValue: props.inputDefaultValue,
+    placeholder: props.inputPlaceholder,
+    ref
+  }
+  const baseClassName = 'confirmation-input'
+
+  if (props.multilineEntry === false) {
+    return <input {...sharedProps} className={`${baseClassName} textinput`} />
+  }
+
+  return <textarea {...sharedProps} className={`${baseClassName} textarea`} />
 }
 
 export default React.forwardRef(UserEntry)
