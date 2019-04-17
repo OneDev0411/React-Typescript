f1c062f71fd338d3dc86454e6d307b58d0bf89d8
OneDev0411 <cryptoapple1109@gmail.com>
2019-04-17 22:27:29 +0300

    deals: change worker to https

--- app/utils/import-pdf-js.js
+++ app/utils/import-pdf-js.js
@@ -3,7 +3,7 @@ import pdfjs from 'pdfjs-dist'
 export default async function importPdfJs() {
   /* eslint-disable max-len */
   // const pdfjs = await import('pdfjs-dist' /* webpackChunkName: "pdfviewjs" */)
-  pdfjs.GlobalWorkerOptions.workerSrc = 'http://pdfjs-dist.surge.sh/pdf.worker.min.js'
+  pdfjs.GlobalWorkerOptions.workerSrc = 'https://pdfjs-dist.surge.sh/pdf.worker.min.js'
 
   return pdfjs
 }
