5e517e0dd46a57aefa39b61826eae5d251f7acf0
OneDev0411 <cryptoapple1109@gmail.com>
2019-04-17 22:10:24 +0300

    deals: lint import-pdf-js

--- app/utils/import-pdf-js.js
+++ app/utils/import-pdf-js.js
@@ -3,7 +3,6 @@ import pdfjs from 'pdfjs-dist'
 export default async function importPdfJs() {
   /* eslint-disable max-len */
   // const pdfjs = await import('pdfjs-dist' /* webpackChunkName: "pdfviewjs" */)
-
   pdfjs.GlobalWorkerOptions.workerSrc = 'http://pdfjs-dist.surge.sh/pdf.worker.min.js'
 
   return pdfjs
