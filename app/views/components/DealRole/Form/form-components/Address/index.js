2ee13641b0c0b7ad73c892ffb7b02c74dbcbe3a3
OneDev0411 <cryptoapple1109@gmail.com>
2019-04-23 02:34:43 +0300

    deals: implement a wrapper for Address fields

---
+++ app/views/components/DealRole/Form/form-components/Address/index.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+import { InlineAddressField } from 'components/inline-editable-fields/InlineAddressField'
+import { Input } from 'components/inline-editable-fields/styled'
+
+import {
+  InputContainer,
+  InputError,
+  InputRequired,
+  InputLabel
+} from 'components/Forms/styled'
+
+import { postLoadFormat } from '../../../../../../components/Pages/Dashboard/Contacts/Profile/Addresses/AddressField/helpers/post-load-format'
+
+export function Address(props) {
+  return (
+    <InputContainer style={props.style}>
+      <InputLabel hasError={props.meta.submitFailed && props.meta.error}>
+        {props.label}
+        &nbsp;
+        <InputRequired>{props.isRequired && '*'}</InputRequired>
+      </InputLabel>
+
+      <InlineAddressField
+        address={props.value}
+        handleSubmit={props.input.onChange}
+        preSaveFormat={x => {
+          console.log(x)
+
+          return x
+        }}
+        postLoadFormat={postLoadFormat}
+        handleInputChange={props.input.onChange}
+        renderSearchField={inputProps => (
+          <Input
+            {...inputProps}
+            type="text"
+            autoFocus
+            style={{
+              padding: 0,
+              border: 'none'
+            }}
+          />
+        )}
+      />
+
+      {props.showError && props.meta.error && props.meta.touched && (
+        <InputError>{props.meta.error}</InputError>
+      )}
+    </InputContainer>
+  )
+}
