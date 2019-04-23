75c42f2169473c91a73ce1e998c1505259b781ce
OneDev0411 <cryptoapple1109@gmail.com>
2019-04-23 21:04:20 +0430

    #2574: fix(Deals/FactSheet): prevent layout regression caused by showing email
    email was added to Roles component as requested in #2279. But it wasn't meant
    to be shown in other places.

--- app/components/Pages/Dashboard/Deals/components/Roles/index.js
+++ app/components/Pages/Dashboard/Deals/components/Roles/index.js
@@ -192,7 +192,7 @@ class Roles extends React.Component {
                     <RoleTitle>{getLegalFullName(role)}</RoleTitle>
                     <RoleType>
                       {roleName(role.role)}
-                      {role.user ? ` . ${role.user.email}` : null}
+                      {this.props.showEmail && role.user ? ` . ${role.user.email}` : null}
                     </RoleType>
                   </RoleInfo>
                 </Flex>
@@ -269,6 +269,8 @@ class Roles extends React.Component {
 Roles.propsTypes = {
   disableAddRole: PropTypes.bool,
   allowDeleteRole: PropTypes.bool,
+  showEmail: PropTypes.bool,
+  showTitle: PropTypes.bool,
   isEmailRequired: PropTypes.bool,
   filter: PropTypes.func,
   addRoleActionRenderer: PropTypes.func,
@@ -277,6 +279,7 @@ Roles.propsTypes = {
 }
 
 Roles.defaultProps = {
+  showEmail: false,
   disableAddRole: false,
   allowDeleteRole: true,
   isEmailRequired: false,
