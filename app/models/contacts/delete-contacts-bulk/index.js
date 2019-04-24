e0c0bcad197960fad0ef4dd494a077c36d512b2f
OneDev0411 <cryptoapple1109@gmail.com>
2019-04-25 01:48:25 +0430

    #2200 feat(models): implement bulk contact delete (to use in entire mode)

---
+++ app/models/contacts/delete-contacts-bulk/index.js
@@ -0,0 +1,42 @@
+import Fetch from '../../../services/fetch'
+
+export async function deleteContactsBulk({
+  users = [],
+  searchText = '',
+  conditionOperator = 'and',
+  filters = [],
+  excludes = []
+}) {
+  try {
+    const request = new Fetch().delete('/contacts/')
+
+    request.query({
+      filter_type: conditionOperator
+    })
+
+    const payload = {
+      filters: filters.map(({ attribute_def, invert, operator, value }) => ({
+        attribute_def,
+        invert,
+        operator,
+        value
+      })),
+      excludes
+    }
+    const cleanedSearchText = searchText
+      .trim()
+      .replace(/[~`!#$%^&*(){}=<>?,:;'"\]\[\/\\]/g, '')
+
+    if (cleanedSearchText.length > 0) {
+      payload.query = cleanedSearchText
+    }
+
+    if (users.length > 0) {
+      request.query(`users[]=${users.join('&users[]=')}`)
+    }
+
+    return request.send(payload)
+  } catch (error) {
+    throw error
+  }
+}
