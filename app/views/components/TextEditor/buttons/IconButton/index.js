ddd3d331f0d5fda24c8bf1008383ab25c4675931
OneDev0411 <cryptoapple1109@gmail.com>
2019-04-26 02:18:39 +0430

    #2493 feat(Contacts/Note): add support for rich text in contact notes
    - A component for safely rendering HTML is added
    - A couple of adjustments are made to TextEditor component

--- app/views/components/TextEditor/buttons/IconButton/index.js
+++ app/views/components/TextEditor/buttons/IconButton/index.js
@@ -6,8 +6,11 @@ export default function Button(props) {
   const onClick = e =>
     props.isBlockButton ? props.toggleBlockType(e) : props.toggleInlineStyle(e)
 
+  // NOTE: type="button" is required because default type is "submit"
+  // and this causes unwanted form submission if the text editor component
+  // is used inside a form
   return (
-    <IconButton onClick={onClick} isFit {...props}>
+    <IconButton type="button" onClick={onClick} isFit {...props}>
       {props.children}
     </IconButton>
   )
