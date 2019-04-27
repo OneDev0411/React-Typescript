91aaafa284f3b197f3bae66f5f5bcab8bc51a220
OneDev0411 <cryptoapple1109@gmail.com>
2019-04-28 00:44:34 +0300

    develop a helper to return visible fields of roles

---
+++ app/views/components/DealRole/helpers/get-visible-fields/index.js
@@ -0,0 +1,56 @@
+import _ from 'underscore'
+
+import { TYPE_PERSON } from '../../constants/role-types'
+
+function getVisibleFields(args) {
+  const list = ['company', 'email', 'phone_number']
+
+  if (args.role_type === TYPE_PERSON) {
+    list.push(
+      'title',
+      'legal_prefix',
+      'legal_first_name',
+      'legal_middle_name',
+      'legal_last_name'
+    )
+  }
+
+  if (args.isFirstNameRequired) {
+    list.push('legal_first_name')
+  }
+
+  if (args.isLastNameRequired) {
+    list.push('legal_last_name')
+  }
+
+  if (
+    [
+      'CoBuyerAgent',
+      'BuyerAgent',
+      'BuyerReferral',
+      'CoSellerAgent',
+      'SellerAgent',
+      'SellerReferral'
+    ].includes(args.role)
+  ) {
+    list.push('commission')
+  }
+
+  if (
+    ['SellerAgent', 'CoSellerAgent', 'BuyerAgent', 'CoBuyerAgent'].includes(
+      args.role
+    )
+  ) {
+    list.push('mls_id')
+  }
+
+  if (['Seller', 'Buyer'].includes(args.role)) {
+    list.push('current_address', 'future_address')
+  }
+
+  return _.uniq(list)
+}
+
+export default _.memoize(getVisibleFields, args =>
+  [args.role, args.role_type].join('')
+)
