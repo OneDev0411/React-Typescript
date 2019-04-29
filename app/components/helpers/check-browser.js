9599bf0e9bc54717e9d30909d7bf31aaa5e51093
OneDev0411 <cryptoapple1109@gmail.com>
2019-04-29 17:42:07 +0430

    [App] refactor: checkBrowser - added outdated

--- app/components/helpers/check-browser.js
+++ app/components/helpers/check-browser.js
@@ -5,74 +5,69 @@ import { confirmation } from '../../store_actions/confirmation'
 
 export function checkBrowser() {
   return async dispatch => {
+    const validBrowsers = {
+      chrome: 68,
+      safari: 10
+    }
+
+    const updateLink = {
+      chrome: 'https://www.google.com/chrome/',
+      safari: 'https://support.apple.com/en-us/HT204416'
+    }
+
     const browser = bowser.getParser(window.navigator.userAgent)
     const browserInfo = browser.getBrowser()
-
-    const content = (() => {
-      switch (browserInfo.name) {
-        case 'Firefox':
-        case 'Microsoft Edge':
-        case 'Internet Explorer':
-          return {
-            title: `Sorry. ${browserInfo.name} is not a supported browser.`,
-            description:
-              'For the best experience on Rechat, we recommend using Google Chrome.',
-            confirmLabel: 'Get Chrome'
-          }
-        default:
-          return {
-            confirmLabel: 'Update Browser',
-            title: `Your web browser (${browserInfo.name} ${
-              browserInfo.version.split('.')[0]
-            }) is out of date.`,
-            description: (
-              <p>
-                We recommend updating your browser to
-                <a
-                  href="https://www.google.com/chrome/"
-                  target="_blank"
-                  style={{ margin: 'auto 4px' }}
-                >
-                  Chrome
-                </a>
-                for more security, speed and the best experience on Rechat.
-              </p>
-            )
-          }
-      }
-    })()
+    const name = browserInfo.name
+    const version = Number(browserInfo.version.split('.')[0])
 
     const isValidBrowser = browser.satisfies({
       macos: {
-        safari: '>=10'
+        safari: `>=${validBrowsers.safari}`
       },
 
-      chrome: '>=68'
+      chrome: `>=${validBrowsers.chrome}`
     })
 
-    if (isValidBrowser !== true) {
-      let downloadLink
-
-      switch (browserInfo.name) {
-        case 'Chrome':
-        case 'Firefox':
-        case 'Internet Explorer':
-          downloadLink = 'https://www.google.com/chrome/'
-          break
-        default:
-          break
-      }
-
-      dispatch(
-        confirmation({
-          message: content.title,
-          description: content.description,
-          confirmLabel: content.confirmLabel,
-          cancelLabel: 'No, thanks',
-          hideConfirmButton: !downloadLink,
-          onConfirm: () => window.open(downloadLink, '_blank')
-        })
-      )
+    if (
+      isValidBrowser === true &&
+      version >= validBrowsers[name.toLowerCase()]
+    ) {
+      return
     }
+
+    console.log(browserInfo)
+
+    let content = {
+      title: `Sorry. ${name} is not a supported browser.`,
+      description:
+        'For the best experience on Rechat, we recommend using Google Chrome.',
+      confirmLabel: 'Get Chrome'
+    }
+
+    if (['Chrome', 'Safari'].includes(name)) {
+      content = {
+        confirmLabel: 'Update Browser',
+        title: `Your web browser (${name} ${version}) is out of date.`,
+        description: (
+          <p>
+            We recommend updating your browser to the latest version for more
+            security, speed and the best experience on Rechat.
+          </p>
+        )
+      }
+    }
+
+    const downloadLink =
+      name === 'Safari' ? updateLink.safari : updateLink.chrome
+
+    dispatch(
+      confirmation({
+        message: content.title,
+        description: content.description,
+        confirmLabel: content.confirmLabel,
+        cancelLabel: 'No, thanks',
+        onConfirm: () => window.open(downloadLink, '_blank')
+      })
+    )
   }
 }
