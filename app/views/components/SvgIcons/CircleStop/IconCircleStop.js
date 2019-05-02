c560bc468dbd75ee824c849868fb8224f65e0bcc
OneDev0411 <cryptoapple1109@gmail.com>
2019-05-02 17:25:49 +0430

    #2555 [Views/Icons] Added: CircleStop

---
+++ app/views/components/SvgIcons/CircleStop/IconCircleStop.js
@@ -0,0 +1,93 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import styled, { css } from 'styled-components'
+
+const width = '24'
+const height = '24'
+const viewBox = '0 0 24 24'
+
+const sizes = {}
+
+// somehow sizes is ending up in markup, even if it is not a valid svg attribute
+// until we have a better solution, just render it empty, instead to '[Object object]'
+Object.defineProperty(sizes, 'toString', { value: () => '', enumerable: false })
+
+const getDimensions = (size, sizes) => {
+  if (
+    size &&
+    typeof size.width === 'number' &&
+    typeof size.height === 'number'
+  ) {
+    return size
+  }
+
+  return size && sizes[size] ? sizes[size] : { width, height }
+}
+
+const getCss = (size, sizes, fillColor, fillColorRule, noStyles) => {
+  if (noStyles) {
+    return ''
+  }
+
+  const dimensions = getDimensions(size, sizes)
+  const fillRule =
+    fillColor && fillColorRule ? `${fillColorRule}{ fill: ${fillColor}; }` : ''
+
+  return css`
+    width: ${dimensions.width}px;
+    height: ${dimensions.height}px;
+    ${fillRule}
+  `
+}
+
+const propsToCss = ({ size, sizes, fillColor, fillColorRule, noStyles }) =>
+  getCss(size, sizes, fillColor, fillColorRule, noStyles)
+
+const Image = styled.svg`
+  ${propsToCss}
+`
+
+const children = (
+  <Fragment>
+    <path
+      d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12C23.993 5.376 18.624.007 12 0zm0 21.55A9.551 9.551 0 1 1 21.551 12 9.562 9.562 0 0 1 12 21.55z"
+      key="key-0"
+    />
+    <rect width="9" height="9" x="7.5" y="7.5" rx=".98" key="key-1" />
+  </Fragment>
+)
+
+const defaultProps = {
+  children,
+  viewBox,
+  fillColor: null,
+  fillColorRule: '&&& path, &&& use, &&& g',
+  sizes,
+  size: null
+}
+
+const propTypes = {
+  fillColor: PropTypes.string,
+  fillColorRule: PropTypes.string,
+  viewBox: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  size: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.shape({
+      height: PropTypes.number.isRequired,
+      width: PropTypes.number.isRequired
+    })
+  ]),
+  sizes: PropTypes.shape({
+    height: PropTypes.number,
+    width: PropTypes.number
+  })
+}
+
+export default Object.assign(Image, {
+  getDimensions,
+  getCss,
+  defaultProps,
+  propTypes,
+  displayName: 'CircleStopIcon'
+})
