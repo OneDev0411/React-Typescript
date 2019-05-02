fa1d6ea6f0f0dbac1c1a08cb010d5e0b92c419f4
OneDev0411 <cryptoapple1109@gmail.com>
2019-05-02 16:06:39 +0430

    #2555 [CRM/Timeline] refactor: AutoEmailItem

--- app/components/Pages/Dashboard/Contacts/Profile/Timeline/AutoEmailItem/index.js
+++ app/components/Pages/Dashboard/Contacts/Profile/Timeline/AutoEmailItem/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import styled from 'styled-components'
 
 import { brandBackground } from 'views/utils/colors'
 import EmailAssociation from 'components/CRMEmailAssociation'
@@ -7,17 +6,13 @@ import EmailAssociation from 'components/CRMEmailAssociation'
 import { MetaInfo } from './MetaInfo'
 import { Container } from '../CRMTaskItem/styled'
 
-const ItemContainer = styled(Container)`
-  background-color: ${brandBackground};
-`
-
 export default function AutoEmailItem({ email }) {
   return (
-    <ItemContainer>
+    <Container style={{ backgroundColor: brandBackground }}>
       <MetaInfo scheduledFor={email.due_date} />
       <EmailAssociation
         association={{ email: { ...email, body: email.html } }}
       />
-    </ItemContainer>
+    </Container>
   )
 }
