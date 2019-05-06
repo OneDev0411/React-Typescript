cbe15c8bdcad65006f7c1855c488fbe44ffc2239
OneDev0411 <cryptoapple1109@gmail.com>
2019-05-06 23:06:57 +0430

    chore(Contacts/Profile): forget about dynamically loading html sanitizer 😔
    Using dynamic impot for html-sanitizer has weird problems in production.
    It only happen on Irish, and in local development it's fine.
    We

--- app/views/components/SanitizedHtml/index.js
+++ app/views/components/SanitizedHtml/index.js
@@ -1,30 +1,20 @@
 import React, { useMemo } from 'react'
-import usePromise from 'react-use-promise'
-
+import sanitizeHtml from 'sanitize-html'
 /**
  * NOTE: we don't use react-sanitized-html because:
  * - It doesn't memoize sanitized html
  * - sanitize-html is not lazy loaded.
+ *
+ * IMPORTANT: we used to load dynamic import for loading sanitize-html
+ * but it had weird problems on production build, so we statically
+ * import it for now. TODO: see if we can find out and solve the production
+ * build problem and switch back to dynamic import.
  */
 
-/*
- sanitize-html is a large library (~217kb), so we lazy load it while nothing
- is blocked.
-*/
-
-const sanitizeHtmlPromise = import('sanitize-html' /* webpackChunkName: "sanitize-html" */)
-
 export default function SanitizedHtml({ html }) {
-  const [sanitizeHtmlModule] = usePromise(sanitizeHtmlPromise, [])
-
-  const { default: sanitizeHtml } = sanitizeHtmlModule || {}
-
   // We can optionally show something like "loading ..." if sanitize-html
   // is not yet loaded. the "loading ..." text can also be a prop.
-  const sanitizedHtml = useMemo(
-    () => (sanitizeHtml && html ? sanitizeHtml(html) : ''),
-    [sanitizeHtml, html]
-  )
+  const sanitizedHtml = useMemo(() => (html ? sanitizeHtml(html) : ''), [html])
 
   return <div dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />
 }
