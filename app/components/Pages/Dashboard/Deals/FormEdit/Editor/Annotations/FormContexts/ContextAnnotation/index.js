8f8c026e4107f579143b4c2e9655a723611adc6e
OneDev0411 <cryptoapple1109@gmail.com>
2019-05-07 11:13:47 +0300

    deals: refactor context annotations and use inline styles

--- app/components/Pages/Dashboard/Deals/FormEdit/Editor/Annotations/FormContexts/ContextAnnotation/index.js
+++ app/components/Pages/Dashboard/Deals/FormEdit/Editor/Annotations/FormContexts/ContextAnnotation/index.js
@@ -2,16 +2,17 @@ import React, { useRef } from 'react'
 
 import ToolTip from 'components/tooltip'
 
-import { calculateWordWrap } from '../../../../utils/word-wrap'
+import IconButton from 'components/Button/IconButton'
+import IconAdd from 'components/SvgIcons/AddCircleOutline/IconAddCircleOutline'
 
-import { Container } from './styled'
+import { calculateWordWrap } from '../../../../utils/word-wrap'
 
 export default function Context(props) {
   const container = useRef()
 
-  const handleClick = () =>
+  const handleClick = index =>
     !props.isReadOnly &&
-    props.onClick(container.current.getBoundingClientRect())
+    props.onClick(container.current.getBoundingClientRect(), index)
 
   const { appearance, rects, values, fontSize } = calculateWordWrap(
     props.annotations,
@@ -23,30 +24,61 @@ export default function Context(props) {
 
   return (
     <React.Fragment>
-      {rects.map((rect, index) => (
-        <ToolTip
-          key={index}
-          captionIsHTML
-          isCustom={false}
-          caption={props.tooltip}
-          placement="bottom"
-          multiline
-        >
-          <Container
+      {rects.map((rect, index) => {
+        const style = {
+          fontSize: `${fontSize}px`,
+          fontFamily: appearance.font,
+          color: appearance.color,
+          fontWeight: appearance.bold ? 'bold' : 'normal',
+          position: 'absolute',
+          left: `${rect.left}px`,
+          top: `${rect.top}px`,
+          width: `${rect.width}px`,
+          height: `${rect.height}px`,
+          backgroundColor: props.readOnly ? 'transparent' : '#d2e5f2',
+          cursor: props.readOnly ? 'auto' : 'pointer',
+          lineHeight: 'normal'
+        }
+
+        return (
+          <div
+            key={index}
             id={props.annotations[index].fieldName}
-            fontName={appearance.font}
-            fontSize={fontSize}
-            bold={appearance.bold}
-            color={appearance.color}
-            rect={rect}
             ref={container}
+            style={style}
             readOnly={props.isReadOnly}
-            onClick={handleClick}
           >
-            {values[index]}
-          </Container>
-        </ToolTip>
-      ))}
+            <div style={{ display: 'inline-block' }}>
+              {props.type === 'role' && values[index] ? (
+                values[index].split(',').map((value, index) => (
+                  <ToolTip
+                    key={index}
+                    captionIsHTML
+                    isCustom={false}
+                    caption={value}
+                    placement="bottom"
+                    multiline
+                  >
+                    <span onClick={() => handleClick(index)}>{value}, </span>
+                  </ToolTip>
+                ))
+              ) : (
+                <span onClick={handleClick}>{values[index]}</span>
+              )}
+            </div>
+
+            {props.type === 'role' && (
+              <span style={{ marginLeft: '0.5rem' }}>
+                <ToolTip caption="Add new role" placement="bottom">
+                  <IconButton isFit iconSize="medium">
+                    <IconAdd style={{ fill: appearance.color }} />
+                  </IconButton>
+                </ToolTip>
+              </span>
+            )}
+          </div>
+        )
+      })}
     </React.Fragment>
   )
 }
