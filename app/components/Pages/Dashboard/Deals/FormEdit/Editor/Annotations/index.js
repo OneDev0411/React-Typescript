0ee3c77592b98238ca73d257c5b8156521ca4063
OneDev0411 <cryptoapple1109@gmail.com>
2019-05-13 02:10:19 +0300

    deals: reimplement Annotations container of form

--- app/components/Pages/Dashboard/Deals/FormEdit/Editor/Annotations/index.js
+++ app/components/Pages/Dashboard/Deals/FormEdit/Editor/Annotations/index.js
@@ -1,166 +1,30 @@
 import React from 'react'
 import _ from 'underscore'
 
-import importPdfJs from 'utils/import-pdf-js'
-import uuid from 'utils/uuid'
+// import { getValue } from '../../utils/types'
 
-import { getValue } from '../../utils/types'
+import { Container } from './styled'
 
-import { FormInputs } from './FormInputs'
-import { FormContexts } from './FormContexts'
+import { FormInputs } from './BasicInputs'
+import { AddressInputs } from './AddressInputs'
+import ContextInputs from './Context'
+import Roles from './Roles'
 
-export default class Annotations extends React.Component {
-  state = {
-    isLoaded: false
+export default function Annotations(props) {
+  const sharedProps = {
+    annotations: props.annotations,
+    pageIndex: props.pageIndex,
+    deal: props.deal,
+    values: props.values,
+    onValueUpdate: props.onValueUpdate
   }
 
-  componentDidMount() {
-    setTimeout(() => this.init(), 10)
-  }
-
-  inputs = []
-
-  contexts = []
-
-  roles = {
-    all: [],
-    single: []
-  }
-
-  init = async () => {
-    await this.indexAnnotations()
-
-    this.setState({ isLoaded: true })
-  }
-
-  async indexAnnotations() {
-    const PDFJS = await importPdfJs()
-
-    this.page = await this.props.document.getPage(this.props.page)
-
-    // get width of actual viewport
-    const { width } = this.page.getViewport(this.props.scale)
-
-    this.viewport = this.page.getViewport(
-      this.props.displayWidth / (width / this.props.scale)
-    )
-
-    const annotations = await this.page.getAnnotations()
-
-    this.annotations = _.sortBy(annotations, item => item.order)
-
-    this.annotations = this.annotations.map(annotation => ({
-      ...annotation,
-      rect: PDFJS.Util.normalizeRect(
-        this.viewport.convertToViewportRectangle(annotation.rect)
-      )
-    }))
-
-    const info = this.annotations.map(this.getInfo)
-
-    // get contexts
-    const contexts = _.filter(info, { type: 'Context' })
-
-    this.inputs = _.filter(info, { type: 'Input' })
-    this.contexts = _.groupBy(contexts, 'context')
-
-    this.roles.all = _.chain(info)
-      .filter({ type: 'Roles' })
-      .groupBy('role')
-      .value()
-
-    this.roles.single = _.chain(info)
-      .filter({ type: 'Role' })
-      .groupBy('role')
-      .value()
-
-    this.setDefaultValues()
-    this.props.onCalculateContextAnnotations(contexts)
-  }
-
-  setDefaultValues = () => {
-    const defaultValues = {}
-
-    this.annotations.forEach(annotation => {
-      defaultValues[annotation.fieldName] = getValue(annotation)
-    })
-
-    this.props.onSetValues(defaultValues, true)
-  }
-
-  getInfo = annotation => {
-    const jsCode =
-      annotation.additional && annotation.additional.calculate
-        ? annotation.additional.calculate.JS
-        : null
-
-    const normalInput = {
-      type: 'Input',
-      group: Symbol(''),
-      order: 0,
-      annotation
-    }
-
-    if (!jsCode) {
-      return normalInput
-    }
-
-    let calculated
-
-    try {
-      calculated = JSON.parse(jsCode)
-    } catch (e) {}
-
-    if (!calculated) {
-      return normalInput
-    }
-
-    const { type } = calculated
-
-    if (!type) {
-      return normalInput
-    }
-
-    return {
-      group: uuid(),
-      order: 0,
-      ...calculated,
-      annotation
-    }
-  }
-
-  render() {
-    if (!this.state.isLoaded) {
-      return false
-    }
-
-    return (
-      <div
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          bottom: 0,
-          right: 0
-        }}
-      >
-        <FormInputs
-          inputs={this.inputs}
-          values={this.props.values}
-          annotations={this.annotations}
-          onValueUpdate={this.props.onValueUpdate}
-          onSetValues={this.props.onSetValues}
-        />
-
-        <FormContexts
-          deal={this.props.deal}
-          contexts={this.contexts}
-          roles={this.roles}
-          values={this.props.values}
-          onSetValues={this.props.onSetValues}
-          onClick={this.props.onClick}
-        />
-      </div>
-    )
-  }
+  return (
+    <Container>
+      <FormInputs {...sharedProps} />
+      <Roles {...sharedProps} />
+      <ContextInputs {...sharedProps} />
+      <AddressInputs {...sharedProps} />
+    </Container>
+  )
 }
