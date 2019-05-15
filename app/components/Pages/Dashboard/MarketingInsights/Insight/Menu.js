4ec185c8c8371980ae06d0f4c9d54e3c7aa4ad24
OneDev0411 <cryptoapple1109@gmail.com>
2019-05-15 21:37:47 +0430

    Refactoring Menu component in Insights

--- app/components/Pages/Dashboard/MarketingInsights/Insight/Menu.js
+++ app/components/Pages/Dashboard/MarketingInsights/Insight/Menu.js
@@ -1,33 +1,21 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import Flex from 'styled-flex-component'
 
 import { CloseButton } from 'components/Button/CloseButton'
 
-import Divider from './Divider'
-
 function Menu(props) {
-  const { closeButtonQuery, backUrl } = props
-
   return (
     <Flex alignCenter style={{ padding: '1.5em 0' }}>
-      <Divider />
       <CloseButton
         isFit
         iconSize="large"
         inverse
         defaultBackUrl="/dashboard/contacts"
-        backUrl={backUrl}
-        query={closeButtonQuery}
+        backUrl={props.backUrl}
+        query={props.closeButtonQuery}
       />
     </Flex>
   )
 }
 
-function mapStateToProps({ contacts }) {
-  return {
-    attributeDefs: contacts.attributeDefs
-  }
-}
-
-export default connect(mapStateToProps)(Menu)
+export default Menu
