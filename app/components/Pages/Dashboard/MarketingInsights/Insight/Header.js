5db7cfa3cb28271e180ee4b512ccf18250c45933
OneDev0411 <cryptoapple1109@gmail.com>
2019-05-15 21:30:54 +0430

    fixing missing proptypes

--- app/components/Pages/Dashboard/MarketingInsights/Insight/Header.js
+++ app/components/Pages/Dashboard/MarketingInsights/Insight/Header.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import Menu from './Menu'
 import { InsightsHeader } from './styled'
 import { H1 } from '../../../../../views/components/Typography/headings'
 import { show_title } from '../List/helpers'
 
+Header.propsType = {
+  title: PropTypes.string,
+  backUrl: PropTypes.string
+}
 Header.defaultProps = {
   title: ''
 }
@@ -15,10 +20,7 @@ function Header(props) {
         <H1 style={{ lineHeight: 1.5 }}>{show_title(props.title)}</H1>
       </div>
       <div>
-        <Menu
-          backUrl={props.backUrl}
-          closeButtonQuery={props.closeButtonQuery}
-        />
+        <Menu backUrl={props.backUrl} />
       </div>
     </InsightsHeader>
   )
