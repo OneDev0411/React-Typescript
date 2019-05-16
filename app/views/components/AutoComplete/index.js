3d09dbb4f55e4a77e33821a5e76c4f17a5f63606
OneDev0411 <cryptoapple1109@gmail.com>
2019-05-16 17:25:31 +0300

    deals: #2672 disable browser auto complete

--- app/views/components/AutoComplete/index.js
+++ app/views/components/AutoComplete/index.js
@@ -154,6 +154,7 @@ export class AutoComplete extends React.Component {
                 isRequired={props.isRequired}
                 placeholder={props.placeholder}
                 label={props.label}
+                autocomplete="off"
                 style={this.props.inputStyle}
                 {...getInputProps({ ...props.input, ...props.inputProps })}
               />
