99822844d5698fb8a107ef970d2c4f32abec89da
OneDev0411 <cryptoapple1109@gmail.com>
2019-05-21 00:38:19 +0300

    deals: #2672 fix padding of address field

--- app/views/components/DealRole/Form/form-components/Address/styled.js
+++ app/views/components/DealRole/Form/form-components/Address/styled.js
@@ -5,6 +5,7 @@ import { grey, borderColor } from 'views/utils/colors'
 
 export const Input = styled(BaseInput)`
   border-bottom: 1px solid ${borderColor};
+  padding: 0.25rem 0;
 
   :focus {
     border-radius: 0;
