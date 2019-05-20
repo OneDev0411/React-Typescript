45028473d2575fb63ca7f9d5f1b5133279859dfb
OneDev0411 <cryptoapple1109@gmail.com>
2019-05-21 01:41:32 +0300

    deals: #2672 fix avatar title of role name

--- app/components/Pages/Dashboard/Deals/utils/get-avatar-title/index.js
+++ app/components/Pages/Dashboard/Deals/utils/get-avatar-title/index.js
@@ -1,7 +1,16 @@
 export function getAvatarTitle(role) {
-  const { user, legal_first_name, legal_last_name, company_title } = role
+  const {
+    user,
+    legal_first_name,
+    legal_last_name,
+    company_title,
+    role_type
+  } = role
+
   const fullName =
-    `${legal_first_name} ${legal_last_name}`.trim() || company_title
+    role_type === 'Person'
+      ? company_title
+      : `${legal_first_name} ${legal_last_name}`.trim()
 
   return fullName || (user && user.display_name)
 }
