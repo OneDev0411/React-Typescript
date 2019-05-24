d752262bf9773ccb9030831e754b0750f9e72f3a
OneDev0411 <cryptoapple1109@gmail.com>
2019-05-24 15:51:16 +0430

    #2590 [Models] added: get-crm-timeline

---
+++ app/models/get-crm-timeline.js
@@ -0,0 +1,55 @@
+import getCalendar from 'models/calendar/get-calendar'
+import { CRM_TASKS_QUERY } from 'models/contacts/helpers/default-query'
+
+async function getCRMTimeline(query = {}, associations = []) {
+  const [low, high] = [15, 25].map(
+    y => new Date(`20${y}-01-01`).getTime() / 1000
+  )
+
+  const calenderAssociations = [
+    'calendar_event.full_campaign',
+    'calendar_event.full_crm_task'
+  ]
+
+  try {
+    const calendarData = await getCalendar(
+      low,
+      high,
+      [],
+      {
+        ...query,
+        object_types: ['crm_association', 'email_campaign_recipient']
+      },
+      [
+        ...CRM_TASKS_QUERY.associations,
+        ...calenderAssociations,
+        ...associations
+      ]
+    )
+
+    return normalizeData(calendarData)
+  } catch (error) {
+    throw error
+  }
+}
+
+function normalizeData(data) {
+  return data
+    .map(item => {
+      let _item
+
+      if (item.object_type === 'crm_association') {
+        _item = item.full_crm_task
+      } else if (item.object_type === 'email_campaign_recipient') {
+        _item = {
+          ...item.full_campaign,
+          due_date: item.full_campaign.due_at
+        }
+      }
+
+      return _item
+    })
+    .filter(Boolean)
+}
+
+export default getCRMTimeline
