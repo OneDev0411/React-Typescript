2cbc51ec66f01f6d924ce38d5e3d97d1329da25c
OneDev0411 <cryptoapple1109@gmail.com>
2019-05-30 12:42:13 +0300

    tests: #2758 refactor unit tests

--- tests/e2e/jest.config.js
+++ tests/e2e/jest.config.js
@@ -2,6 +2,7 @@ const { resolve } = require('path')
 
 module.exports = {
   rootDir: resolve('tests/e2e'),
+  verbose: true,
   setupFilesAfterEnv: ['./jest.setup.js'],
   transform: {
     '^.+\\.js$': 'babel-jest'
