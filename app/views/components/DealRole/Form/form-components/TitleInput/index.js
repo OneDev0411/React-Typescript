a7a5e59332720af8353fafdeab0c9fc6a41afc39
OneDev0411 <cryptoapple1109@gmail.com>
2019-05-30 14:42:40 +0300

    deals: refactor deal roles dropdown based on new changes

--- app/views/components/DealRole/Form/form-components/TitleInput/index.js
+++ app/views/components/DealRole/Form/form-components/TitleInput/index.js
@@ -12,29 +12,19 @@ export function TitleInput(props) {
     return false
   }
 
-  const selectedItem = props.input.value
-    ? items.find(item => item.value === props.input.value)
-    : items[0]
-
   return (
     <SelectInput
       style={{
         width: '20%',
         marginRight: '0.5rem'
       }}
-      dropdownStyle={{
-        borderBottom: '1px solid #dce5eb',
-        height: '2rem',
-        paddingBottom: '0.25rem'
-      }}
+      hasBottomLine
       searchable={false}
       clearable
       input={props.input}
       meta={props.meta}
       label="Title"
       items={items}
-      selectedItem={selectedItem}
-      defaultSelectedItem={selectedItem}
       dropdownOptions={{
         fullHeight: true
       }}
