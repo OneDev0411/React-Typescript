43fe84dbddd120c1843faa12676e123052182677
OneDev0411 <cryptoapple1109@gmail.com>
2019-05-31 04:11:29 +0430

    #2752 fix(mc): make templateId required in upload asset helper
    it's not optional in the server side

--- app/views/components/InstantMarketing/Builder/AssetManager/helpers.ts
+++ app/views/components/InstantMarketing/Builder/AssetManager/helpers.ts
@@ -4,12 +4,9 @@ import Fetch from 'services/fetch'
 
 const ASSET_UPLOAD_ENDPOINT = '/templates/assets'
 
-export function uploadAsset(file: File, templateId?: string): Promise<Response> {
-  const request = new Fetch()
+export function uploadAsset(file: File, templateId: string): Promise<Response> {
+  return new Fetch()
     .upload(ASSET_UPLOAD_ENDPOINT)
     .attach('attachment', file, file.name)
-  if (templateId) {
-    request.field('template', templateId)
-  }
-  return request
+    .field('template', templateId)
 }
