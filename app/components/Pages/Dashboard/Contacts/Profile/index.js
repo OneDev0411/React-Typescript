ebda3714d35df0b94f376703d28685a93c72aa09
OneDev0411 <cryptoapple1109@gmail.com>
2019-06-03 15:02:11 +0430

    Revert "#2384 [Contact/Profile] hide flows" hide
    This reverts commit 2252a382b2cfa3a382e49ef51ea5fad5d6f4991a.

--- app/components/Pages/Dashboard/Contacts/Profile/index.js
+++ app/components/Pages/Dashboard/Contacts/Profile/index.js
@@ -8,7 +8,7 @@ import { Tabs, TabList, Tab, TabPanels, TabPanel } from '@reach/tabs'
 import { viewAs, viewAsEveryoneOnTeam } from 'utils/user-teams'
 import { isFetchingTags, selectTags } from 'reducers/contacts/tags'
 
-import deleteFlow from 'models/flows/delete-flow'
+// import deleteFlow from 'models/flows/delete-flow'
 import { normalizeContact } from 'models/contacts/helpers/normalize-contact'
 import { updateContactQuery } from 'models/contacts/helpers/default-query'
 import { getContact } from 'models/contacts/get-contact'
@@ -32,7 +32,7 @@ import Loading from '../../../../Partials/Loading'
 import NewTask from '../../../../../views/CRM/Tasks/components/NewTask'
 
 import { Container } from '../components/Container'
-import Flows from './Flows'
+// import Flows from './Flows'
 import { Dates } from './Dates'
 import Deals from './Deals'
 import { Details } from './Details'
@@ -129,10 +129,10 @@ class ContactProfile extends React.Component {
       const response = await getContact(this.props.params.id, {
         associations: [
           ...updateContactQuery.associations,
-          'contact.deals',
-          'contact.flows',
-          'flow_step.email',
-          'flow_step.crm_task'
+          'contact.deals'
+          // 'contact.flows',
+          // 'flow_step.email',
+          // 'flow_step.crm_task'
         ]
       })
 
@@ -265,19 +265,19 @@ class ContactProfile extends React.Component {
     }
   }
 
-  stopFlow = async id => {
-    try {
-      await deleteFlow(id)
-      this.fetchContact()
-    } catch (error) {
-      console.log(error)
-    }
-  }
+  // stopFlow = async id => {
+  //   try {
+  //     await deleteFlow(id)
+  //     this.fetchContact()
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
 
-  addToFlowCallback = () => {
-    this.fetchContact()
-    this.fetchTimeline()
-  }
+  // addToFlowCallback = () => {
+  //   this.fetchContact()
+  //   this.fetchTimeline()
+  // }
 
   render() {
     const { contact } = this.state
@@ -298,12 +298,12 @@ class ContactProfile extends React.Component {
 
     const thirdColumnSections = [
       <Dates contact={contact} key="s1" />,
-      <Flows
-        key="s2"
-        contactId={contact.id}
-        flows={contact.flows}
-        user={user}
-      />,
+      // <Flows
+      //   key="s2"
+      //   contactId={contact.id}
+      //   flows={contact.flows}
+      //   user={user}
+      // />,
       <Deals contact={contact} key="s3" />
     ]
 
@@ -326,7 +326,7 @@ class ContactProfile extends React.Component {
                 : null
             }
             closeButtonQuery={this.props.location.state}
-            addToFlowCallback={this.addToFlowCallback}
+            // addToFlowCallback={this.addToFlowCallback}
           />
 
           <ColumnsContainer>
@@ -345,14 +345,14 @@ class ContactProfile extends React.Component {
 
                 <Partner {..._props} />
 
-                {!this.state.isDesktopScreen && (
+                {/* {!this.state.isDesktopScreen && (
                   <Flows
                     contactId={contact.id}
                     flows={contact.flows}
                     user={user}
                     onStop={this.stopFlow}
                   />
-                )}
+                )} */}
 
                 {!this.state.isDesktopScreen && <Deals contact={contact} />}
 
