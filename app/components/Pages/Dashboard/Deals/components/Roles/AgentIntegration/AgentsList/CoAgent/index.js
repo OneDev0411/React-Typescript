59ec6b0303faaa6851b826cafc464385f6410410
OneDev0411 <cryptoapple1109@gmail.com>
2019-06-05 22:31:46 +0300

    deals: refactor co-agents and make the search client side

--- app/components/Pages/Dashboard/Deals/components/Roles/AgentIntegration/AgentsList/CoAgent/index.js
+++ app/components/Pages/Dashboard/Deals/components/Roles/AgentIntegration/AgentsList/CoAgent/index.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import memoize from 'lodash/memoize'
 import PropTypes from 'prop-types'
+import Fuse from 'fuse.js'
+import useDebouncedCallback from 'use-debounce/lib/callback'
 
 import Avatar from 'components/Avatar'
-import { getUserInitials } from 'models/user/helpers/get-user-initials'
 import { getBrandAvailableMembers } from 'utils/user-teams'
 
 import Search from 'components/Grid/Search'
 
+import { EmptyState } from '../styled'
+
 import {
   Card,
   Header,
-  Body,
   Title,
   SubTitle,
   RowItem,
@@ -26,18 +28,26 @@ CoAgent.propTypes = {
 }
 
 export function CoAgent(props) {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [debouncedSetSearchTerm] = useDebouncedCallback(setSearchTerm, 500)
+
+  const teams = normalizeTeams(props.teams, searchTerm)
+
   return (
     <div>
       <Search
-        onChange={props.onSearch}
+        value={props.searchTerm}
+        onChange={debouncedSetSearchTerm}
         style={{ marginBottom: '1rem' }}
         placeholder="Search for teams or agents"
       />
 
-      {props.teams.map((office, officeIndex) => {
-        const agents = getBrandAvailableMembers(office)
+      {!props.isLoading && !teams.length && (
+        <EmptyState>No search result</EmptyState>
+      )}
 
-        if (!agents.length) {
+      {teams.map((office, officeIndex) => {
+        if (!office.users.length) {
           return false
         }
 
@@ -45,41 +55,61 @@ export function CoAgent(props) {
           <Card key={officeIndex}>
             <Header>
               <Title>{office.name}</Title>
-              <SubTitle>{getSubtitle(office)}</SubTitle>
+              <SubTitle>{office.subtitle}</SubTitle>
             </Header>
-            <Body>
-              {agents.map((user, userIndex) => (
-                <RowItem key={`${officeIndex}${userIndex}`}>
-                  <Avatar
-                    size={40}
-                    title={getUserInitials(user)}
-                    image={user.profile_image_url}
-                    style={{
-                      marginRight: '1rem'
-                    }}
-                  />
 
-                  <div>
-                    <AgentTitle
-                      onClick={() =>
-                        props.onSelectAgent({
-                          ...user,
-                          brand_id: office.id
-                        })
-                      }
-                    >
-                      {user.display_name}
-                    </AgentTitle>
-                    <AgentEmail>{user.email}</AgentEmail>
-                  </div>
-                </RowItem>
-              ))}
-            </Body>
+            {office.users.map((user, userIndex) => (
+              <RowItem
+                key={`${officeIndex}${userIndex}`}
+                onClick={() =>
+                  props.onSelectAgent({
+                    ...user,
+                    brand_id: office.id
+                  })
+                }
+              >
+                <Avatar
+                  size={40}
+                  title={user.display_name}
+                  image={user.profile_image_url}
+                  style={{
+                    marginRight: '1rem'
+                  }}
+                />
+
+                <div>
+                  <AgentTitle>{user.display_name}</AgentTitle>
+                  <AgentEmail>{user.email}</AgentEmail>
+                </div>
+              </RowItem>
+            ))}
           </Card>
         )
       })}
     </div>
   )
+}
+
+function normalizeTeams(teams, searchTerm) {
+  const list = teams.map(office => {
+    const agents = getBrandAvailableMembers(office).map(user => ({
+      ...user,
+      office: office.name
+    }))
+
+    return {
+      name: office.name,
+      subtitle: getSubtitle(office),
+      users: searchTerm
+        ? new Fuse(agents, {
+            keys: ['office', 'display_name', 'email'],
+            threshold: 0.1
+          }).search(searchTerm)
+        : agents
+    }
+  })
+
+  return list.every(office => !office.users.length) ? [] : list
 }
 
 const getSubtitle = memoize(
@@ -92,7 +122,7 @@ const getSubtitle = memoize(
       currentOffice = currentOffice.parent
     }
 
-    return names.reverse().join(' > ')
+    return names.length > 1 ? names.reverse().join(' > ') : ''
   },
   office => office.id
 )
