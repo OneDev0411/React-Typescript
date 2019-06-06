409b271197a5e7a13fb2c036aa22734e5e234bd3
OneDev0411 <cryptoapple1109@gmail.com>
2019-06-06 14:48:01 +0430

    chore: update IUser after checking with Abbas

--- types/User.d.ts
+++ types/User.d.ts
@@ -1,13 +1,13 @@
 declare interface IUserBase {
-  first_name: string
-  last_name: string
+  first_name: string | null
+  last_name: string | null
   display_name: string
   email: string
-  phone_number?: string
+  phone_number: string | null
   is_shadow: boolean
   fake_email?: boolean
-  profile_image_url?: string
-  cover_image_url?: string
+  profile_image_url: string | null
+  cover_image_url: string | null
   password: string
 }
 declare interface IUserInput extends IUserBase {
@@ -20,6 +20,8 @@ declare interface IUserInput extends IUserBase {
   actions?: string[]
 }
 declare interface IUser extends IUserBase {
+  last_seen_at: number | null
+  cover_image_thumbnail_url: string | null
   brand: string | null
   id: UUID
 
