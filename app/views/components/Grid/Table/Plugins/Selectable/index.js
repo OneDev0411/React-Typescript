cbb46bcb0cc16528e70b40febec3a29f9ca60656
OneDev0411 <cryptoapple1109@gmail.com>
2019-06-19 00:38:11 +0430

    #2842 refactor(grid/plugins/selectable): address MR discussions

--- app/views/components/Grid/Table/Plugins/Selectable/index.js
+++ app/views/components/Grid/Table/Plugins/Selectable/index.js
@@ -53,36 +53,30 @@ export class SelectablePlugin {
   setData(data = []) {
     // set new data
     this.data = data
-    this.validateSelection()
+    this.removeInvalids()
   }
 
   /**
    * Make sure all selected or excluded rows exist in the data.
    * Removes selected or excluded items which don't exist in the data anymore.
    */
-  validateSelection() {
+  removeInvalids() {
     const { selectedRows = {}, excludedRows = {} } = this.StorageObject
-    const selectedRowIds = Object.keys(selectedRows)
-    const excludedRowIds = Object.keys(excludedRows)
     const ids = this.data.map(item => item.id)
 
-    let changed = false
+    const removeInvalids = mapping =>
+      Object.entries(mapping || {}).reduce((acc, [id, value]) => {
+        return !ids.includes(id)
+          ? acc
+          : {
+              ...acc,
+              [id]: value
+            }
+      }, {})
 
-    excludedRowIds.forEach(id => {
-      if (!ids.includes(id)) {
-        delete excludedRows[id]
-        changed = true
-      }
-    })
-    selectedRowIds.forEach(id => {
-      if (!ids.includes(id)) {
-        delete selectedRows[id]
-        changed = true
-      }
-    })
-
-    if (changed) {
-      this.StorageObject = { selectedRows, excludedRows }
+    this.StorageObject = {
+      selectedRows: removeInvalids(selectedRows),
+      excludedRows: removeInvalids(excludedRows)
     }
   }
 
