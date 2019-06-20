ec7223051f0f9f5f604cb6d16f272be489721deb
OneDev0411 <cryptoapple1109@gmail.com>
2019-06-21 00:40:23 +0430

    #2723 fix(mc): hide LinkedIn share for all other templates and hide them from LI templates

--- app/views/components/InstantMarketing/Builder/SocialActions/index.test.tsx
+++ app/views/components/InstantMarketing/Builder/SocialActions/index.test.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import toJson from 'enzyme-to-json'
 
-import SocialActions from '.'
-import { Button } from './styled'
 import { SOCIAL_NETWORKS } from '../constants'
+import { Button } from './styled'
+
+import SocialActions from '.'
 
 describe('MC SocialActions component', () => {
   it('renders', () => {
     const wrapper = shallow(
-      <SocialActions networks={SOCIAL_NETWORKS} />
+      <SocialActions onClick={() => null} networks={SOCIAL_NETWORKS} />
     )
 
     expect(toJson(wrapper)).toMatchSnapshot()
@@ -17,13 +18,14 @@ describe('MC SocialActions component', () => {
 
   it('renders all needed buttons based on networks prop', () => {
     const wrapper = shallow(
-      <SocialActions networks={SOCIAL_NETWORKS} />
+      <SocialActions onClick={() => null} networks={SOCIAL_NETWORKS} />
     )
 
     expect(wrapper.children()).toHaveLength(SOCIAL_NETWORKS.length)
-  
+
     SOCIAL_NETWORKS.map((network, index) => {
       const socialButton = wrapper.childAt(index)
+
       expect(socialButton.type()).toBe(Button)
       expect(socialButton.childAt(0).hasClass(network.className)).toBeTruthy()
     })
