700822e4e7857b986add2d1a592e53072d35c09f
OneDev0411 <cryptoapple1109@gmail.com>
2019-06-23 00:42:49 +0300

    test(Roles) add unit test for get-roles-text

---
+++ app/components/Pages/Dashboard/Deals/FormEdit/utils/get-roles-text/indext.test.ts
@@ -0,0 +1,46 @@
+import deal from 'fixtures/deal/seller-with-offer'
+
+import { getRoleText } from '.'
+
+describe('Test deal form get role names', () => {
+  it('Should return correct names when annotation type is Roles and attribute is legal_full_name', () => {
+    // @ts-ignore
+    const names = getRoleText(deal.roles, deal, ['SellerAgent', 'Seller'], {
+      type: 'Roles',
+      attribute: 'legal_full_name'
+    })
+
+    expect(names).toBe('Mr Ramin Mousavi, Shayan Hamidi')
+  })
+
+  it('Should return correct names when annotation type is Roles and attribute is not legal_full_name', () => {
+    // @ts-ignore
+    const names = getRoleText(deal.roles, deal, ['SellerAgent', 'Seller'], {
+      type: 'Roles',
+      attribute: 'legal_first_name'
+    })
+
+    expect(names).toBe('Ramin, Shayan')
+  })
+
+  it('Should return correct names when annotation type is Roles and attribute is nested', () => {
+    // @ts-ignore
+    const names = getRoleText(deal.roles, deal, ['SellerAgent', 'Seller'], {
+      type: 'Roles',
+      attribute: 'user.display_name'
+    })
+
+    expect(names).toBe('ramin@rechat.com, Shayan Hamidi')
+  })
+
+  it('Should return correct names when annotation type is Role', () => {
+    // @ts-ignore
+    const names = getRoleText(deal.roles, deal, ['SellerAgent', 'Seller'], {
+      type: 'Role',
+      attribute: 'legal_first_name',
+      number: 1
+    })
+
+    expect(names).toBe('Shayan')
+  })
+})
