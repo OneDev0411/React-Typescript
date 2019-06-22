1fee0369bd80910ccd0e80548f5f7420dad32b7e
OneDev0411 <cryptoapple1109@gmail.com>
2019-06-22 04:02:52 +0000

    Resolve "Use Cypress as e2e test runner"

---
+++ tests/cypress/integration/helpers/authentication.ts
@@ -0,0 +1,21 @@
+const email = Cypress.env('E2E_EMAIL')
+const password = Cypress.env('E2E_PASSWORD')
+
+import { getTestSelector, waitForPage } from './page'
+
+export function signin() {
+  cy.visit('/signin')
+
+  cy.get('#username').type(email)
+  cy.get('#password').type(password)
+  cy.get('form').submit()
+
+  waitForPage('/dashboard')
+}
+
+export function signout() {
+  cy.get(getTestSelector('settings-dropdown-button')).click()
+  cy.get('[href="/signout"]').click()
+
+  waitForPage('/signin')
+}
