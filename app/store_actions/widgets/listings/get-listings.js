c08d97a88438533326e234110824cb5393d542b7
OneDev0411 <cryptoapple1109@gmail.com>
2019-06-28 23:24:41 +0430

    fix(widgets): added brand id to get listings request

--- app/store_actions/widgets/listings/get-listings.js
+++ app/store_actions/widgets/listings/get-listings.js
@@ -1,15 +1,25 @@
 import api from '../../../models/widgets/listings'
+import { getBrandByHostname } from '../../../models/brand/get-brand-by-hostname'
 import * as actionsType from '../../../constants/widgets/listing'
 
-const getListingsByValert = (options, widgetOptions) => (dispatch) => {
+const getListingsByValert = (options, widgetOptions) => async (
+  dispatch,
+  getState
+) => {
   try {
+    let { user, brand } = getState()
+
+    if (!user && !brand) {
+      brand = await getBrandByHostname(window.location.hostname)
+    }
+
     dispatch({
       type: actionsType.FETCH_WIDGET_LISTING_REQUEST,
       options,
       widgetOptions
     })
 
-    return api.byValert(options, widgetOptions).then(
+    return api.byValert(options, widgetOptions, brand).then(
       response => {
         dispatch({
           type: actionsType.FETCH_WIDGET_LISTING_SUCCESS,
