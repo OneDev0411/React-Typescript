c65417078b837815fa498c2526df40d415ba25d3
OneDev0411 <cryptoapple1109@gmail.com>
2019-07-01 14:30:18 +0300

    #2915 test(deal role): make sure escrow always showing names

---
+++ app/views/components/DealRole/index.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { render } from 'enzyme'
+
+import user from 'fixtures/users/agent'
+
+import { DealRole } from '.'
+
+const form = {}
+
+describe('Test Deal Roles component', () => {
+  it('Should show legal_names when role is Title', () => {
+    const wrapper = render(
+      <DealRole
+        isOpen
+        form={form}
+        user={user}
+        dealSide="Buyer"
+        allowedRoles={['Title']}
+        onClose={() => {}}
+        onUpsertRole={() => {}}
+      />
+    )
+
+    expect(wrapper.find('[name="legal_first_name"]')).toHaveLength(1)
+    expect(wrapper.find('[name="legal_last_name"]')).toHaveLength(1)
+  })
+})
