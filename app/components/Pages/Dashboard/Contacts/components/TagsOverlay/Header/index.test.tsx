323311aaa9fa91a615b12dddca35160317479153
OneDev0411 <cryptoapple1109@gmail.com>
2019-07-03 12:56:03 +0000

    #2830 Link to the tag settings from Tag Drawer and tags list

---
+++ app/components/Pages/Dashboard/Contacts/components/TagsOverlay/Header/index.test.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import { Link } from 'react-router'
+
+import IconButton from 'components/Button/IconButton'
+import CloseIcon from 'components/SvgIcons/Close/CloseIcon'
+
+import Header from '.'
+
+describe('TagsOverlay header', () => {
+  it('renders', () => {
+    const wrapper = shallow(
+      <Header title="manage tags here" onClose={() => null} />
+    )
+
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('renders proper title', () => {
+    const wrapper = shallow(
+      <Header title="manage tags here" onClose={() => null} />
+    )
+
+    expect(wrapper.text()).toContain('manage tags here')
+  })
+
+  it('renders link to tag management settings', () => {
+    const wrapper = shallow(
+      <Header title="manage tags here" onClose={() => null} />
+    )
+
+    expect(wrapper.find(Link)).toHaveLength(1)
+  })
+
+  it('renders close button', () => {
+    const wrapper = shallow(
+      <Header title="manage tags here" onClose={() => null} />
+    )
+
+    expect(wrapper.find(IconButton)).toHaveLength(1)
+    expect(wrapper.find(CloseIcon)).toHaveLength(1)
+  })
+})
