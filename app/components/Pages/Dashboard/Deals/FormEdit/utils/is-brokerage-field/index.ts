a97053b0c5ba2e60fd6ef71a3c9f41e80bdda196
OneDev0411 <cryptoapple1109@gmail.com>
2019-07-04 12:39:41 +0000

    #2886 fix(deal office form): consider attributes
    More info on MR 330

---
+++ app/components/Pages/Dashboard/Deals/FormEdit/utils/is-brokerage-field/index.ts
@@ -0,0 +1,37 @@
+export const brokerageFields: string[] = [
+  'office_name',
+  'office_email',
+  'office_phone',
+  'office_fax',
+  'office_license_number',
+  'office_mls_id',
+  'office_address.full',
+  'office_address.city',
+  'office_address.state',
+  'office_address.postal_code',
+  'agent.office.name',
+  'agent.office.email',
+  'agent.office.phone',
+  'agent.office.fax',
+  'agent.office.license_number',
+  'agent.office.mls_id',
+  'agent.office.address',
+  'agent.office.city',
+  'agent.office.state',
+  'agent.office.postal_code'
+]
+
+export function isBrokerageField(annotation): boolean {
+  if (annotation.attribute && brokerageFields.includes(annotation.attribute)) {
+    return true
+  }
+
+  if (
+    Array.isArray(annotation.attributes) &&
+    annotation.attributes.some(attr => brokerageFields.includes(attr))
+  ) {
+    return true
+  }
+
+  return false
+}
