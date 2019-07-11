3bd2e570cd38aa4df7da38b40d4657c0985373aa
OneDev0411 <cryptoapple1109@gmail.com>
2019-07-11 17:27:28 +0430

    #2858 test(contacts): clean helpers and write test for email send from profile

--- tests/cypress/integration/contacts/profile/helpers.ts
+++ tests/cypress/integration/contacts/profile/helpers.ts
@@ -1,15 +1,11 @@
+import { setDateFieldToFuture } from 'helpers/fields'
+
 // Adds a task from contact profile page
-// TODO: Implement add a task with a specific date and and time after UI redesign
-export function addTask(title: string, future: boolean = false) {
+export function addTask(title: string, future = false) {
   cy.getByTestSelector('add-task').type(title)
 
   if (future) {
-    cy.getByTestSelector('date-time-picker-button').click()
-    cy.get('.DayPicker-NavButton--next').click()
-    cy.get('.DayPicker-Day')
-      .last()
-      .click()
-    cy.getByTestSelector('date-picker-done').click()
+    setDateFieldToFuture()
   }
 
   cy.getByTestSelector('save-task').click()
