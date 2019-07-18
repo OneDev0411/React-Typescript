0fa9370f0f3241bbd1c56c02adcebcf07ab74314
OneDev0411 <cryptoapple1109@gmail.com>
2019-07-18 19:01:02 +0430

    #2970 test(contacts): add e2e tests for last touch sort on contact touch change

---
+++ tests/cypress/integration/contacts/list/sort.test.ts
@@ -0,0 +1,34 @@
+import { changeSort } from 'helpers/grid'
+
+import { addTask } from '../profile/helpers'
+
+// https://gitlab.com/rechat/web/issues/2970
+describe('Contacts list sort', () => {
+  const contactId = '41310770-5aee-409a-b47a-97f6d2f16a91'
+
+  beforeEach(() => {
+    cy.viewport(1600, 800)
+    cy.sandbox()
+    cy.signin()
+    cy.visit('/dashboard/contacts')
+  })
+
+  it('Sorting by last touch should work properly after updating a contact last touch', () => {
+    changeSort(1)
+
+    cy.navigate(`/dashboard/contacts/${contactId}`)
+    addTask('Test call')
+    // Let's wait for a few seconds to make sure the last touch will be updated
+    cy.wait(5000)
+
+    cy.navigate('/dashboard/contacts')
+
+    cy.pageShouldContain('Last Touch was just now')
+    cy.getByTestSelector('grid-row')
+      .eq(0)
+      .should('contain', 'Daniel Day-Lewis')
+    cy.getByTestSelector('grid-row')
+      .eq(1)
+      .should('contain', 'Christine Lillian Akin')
+  })
+})
