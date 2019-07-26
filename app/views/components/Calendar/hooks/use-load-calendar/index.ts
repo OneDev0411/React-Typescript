8da137dcad908573562ed121acb465d70015fbcb
OneDev0411 <cryptoapple1109@gmail.com>
2019-07-26 17:56:00 +0300

    #2991 feat(calendar): improve useLoadCalendar hook

--- app/views/components/Calendar/hooks/use-load-calendar/index.ts
+++ app/views/components/Calendar/hooks/use-load-calendar/index.ts
@@ -2,10 +2,12 @@ import { useState } from 'react'
 import useEffect from 'use-deep-compare-effect'
 
 import { getCalendar, FilterQuery } from '../../models/get-calendar'
+
 import { normalize } from './normalize-events'
+import { sortEvents } from './sort-events'
 
 interface ApiOptions {
-  range: [number, number]
+  range: DateRange
   filter?: FilterQuery
   associations?: string[]
   users?: UUID[]
@@ -14,29 +16,50 @@ interface ApiOptions {
 export function useLoadCalendar(apiOptions: ApiOptions) {
   const [events, setEvents] = useState<CalendarEventsList>({})
   const [error, setError] = useState(null)
-  const [isLoading, setLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     async function getCalendarEvents() {
       try {
+        if (isLoading) {
+          return
+        }
+
         // enable loading flag
-        setLoading(true)
+        setIsLoading(true)
+
+        console.log(
+          `[ + ] Fetching Calendar in range of %c${new Date(
+            apiOptions.range[0] * 1000
+          ).toUTCString()} %c${new Date(
+            apiOptions.range[1] * 1000
+          ).toUTCString()}`,
+          'color: green',
+          'color: blue'
+        )
 
         // fetch calendar data from server based on given parameters
-        const events = await getCalendar(apiOptions)
+        const newEvents = await getCalendar(apiOptions)
 
-        // return fetched events
-        setEvents(normalize(apiOptions.range, events))
+        // concat newEvents and current events
+        const list = sortEvents({
+          ...events,
+          ...normalize(apiOptions.range, newEvents)
+        })
+
+        setEvents(list)
       } catch (e) {
         console.log(e)
         setError(e)
         setEvents({})
       } finally {
-        setLoading(false)
+        setIsLoading(false)
       }
     }
 
     getCalendarEvents()
+
+    // eslint-disable-next-line
   }, [apiOptions])
 
   return {
