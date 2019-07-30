53c00b5ec2dba6f6a7d33d805fcc1ebb5c6f76b7
OneDev0411 <cryptoapple1109@gmail.com>
2019-07-30 03:28:13 +0300

    #2991 feat(calendar): complete types of Calendar event

--- types/Calendar/index.d.ts
+++ types/Calendar/index.d.ts
@@ -7,6 +7,30 @@ declare interface CalendarEvent {
   title: string
   timestamp: number
   object_type: string
+  campaign: UUID | null
+  contact: UUID | null
+  created_by: string
+  crm_task: UUID
+  date: string
+  deal: UUID | null
+  event_type: string
+  metadata: { status: string }
+  next_occurence: string
+  object_type: string
+  recurring: boolean
+  status: string
+  timestamp: number
+  timestamp_midday: string
+  timestamp_readable: string
+  title: string
+  type: string
+  type_label: string
+  users: UUID[]
+  full_contact?: IContact
+  full_deal?: IDeal
+  full_crm_task?: {
+    assignees: IUser[]
+  }
 }
 
 interface CalendarEventsList {
