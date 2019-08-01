f6394f2877abf5f712d359b2c35381019c434fb3
OneDev0411 <cryptoapple1109@gmail.com>
2019-08-01 04:26:35 +0300

    #2991 feat(calendar): develop a function to select first day with event

---
+++ app/views/components/Calendar/helpers/get-row-by-date/index.ts
@@ -0,0 +1,44 @@
+import fecha from 'fecha'
+
+export function getRowIdByDate(
+  date: Date,
+  rows: any[],
+  eventKeys: string[],
+  calendarRange: [number, number]
+) {
+  let dayId = format(date)
+
+  while (
+    eventKeys.indexOf(dayId) === -1 &&
+    isDayInRange(dayId, calendarRange)
+  ) {
+    const nextDay = date.setDate(date.getDate() + 1)
+
+    // format next day
+    dayId = format(nextDay)
+  }
+
+  return rows.findIndex(row => row.is_header && row.title === dayId)
+}
+
+/**
+ * checks given day is in range or not
+ * @param day
+ * @param highRange
+ */
+function isDayInRange(day: string, calendarRange: [number, number]): boolean {
+  const date = new Date(day)
+
+  const timestamp =
+    (date.getTime() + date.getTimezoneOffset() * 60 * 1000) / 1000
+
+  return timestamp > calendarRange[0] && timestamp < calendarRange[1]
+}
+
+/**
+ * returns formatted style of date
+ * @param date
+ */
+function format(date: Date | number) {
+  return fecha.format(new Date(date), 'YYYY-M-D')
+}
