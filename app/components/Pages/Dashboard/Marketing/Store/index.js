7a2cf84975ca2b9eb6f562631c4e995888972438
OneDev0411 <cryptoapple1109@gmail.com>
2019-08-07 03:15:58 +0430

    chore(marketing): add temporary banner

--- app/components/Pages/Dashboard/Marketing/Store/index.js
+++ app/components/Pages/Dashboard/Marketing/Store/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { Callout } from 'components/Callout'
+
 import { Header } from '../components/PageHeader'
 import Cards from './Cards'
 
@@ -12,6 +14,14 @@ export default class Store extends React.Component {
           isSideMenuOpen={this.props.isSideMenuOpen}
           toggleSideMenu={this.props.toggleSideMenu}
         />
+
+        <Callout type="error">
+          <span style={{ color: '#fff', fontSize: '1.2rem', fontWeight: 500 }}>
+            We are currently working on this feature, we will notify you shortly
+            when our work completes.
+          </span>
+        </Callout>
+
         <Cards isSideMenuOpen={this.props.isSideMenuOpen} />
       </div>
     )
