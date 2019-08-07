34760dc1daba2776b67ce4c43d0f4f8ff8d7dfaf
OneDev0411 <cryptoapple1109@gmail.com>
2019-08-07 04:40:25 +0430

    Revert "chore(marketing): add temporary banner"
    This reverts commit 8d80be3a3a30da426be9589a392193fb2e8ebe5d.

--- app/components/Pages/Dashboard/Marketing/Store/index.js
+++ app/components/Pages/Dashboard/Marketing/Store/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import { Callout } from 'components/Callout'
-
 import { Header } from '../components/PageHeader'
 import Cards from './Cards'
 
@@ -14,14 +12,6 @@ export default class Store extends React.Component {
           isSideMenuOpen={this.props.isSideMenuOpen}
           toggleSideMenu={this.props.toggleSideMenu}
         />
-
-        <Callout type="error">
-          <span style={{ color: '#fff', fontSize: '1.2rem', fontWeight: 500 }}>
-            We are currently working on this feature, we will notify you shortly
-            when our work completes.
-          </span>
-        </Callout>
-
         <Cards isSideMenuOpen={this.props.isSideMenuOpen} />
       </div>
     )
