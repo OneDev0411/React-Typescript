4229e3e8275bd7f63d54b4b1a575ea143ca91db0
OneDev0411 <cryptoapple1109@gmail.com>
2019-08-20 02:32:50 +0430

    #3002 chore(browser modal): running linter

--- app/views/components/CheckBrowser/index.tsx
+++ app/views/components/CheckBrowser/index.tsx
@@ -1,61 +1,78 @@
 import React from 'react'
-import { SnackbarContent, IconButton, Button, Theme } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import { useTheme } from '@material-ui/styles';
+import { SnackbarContent, IconButton, Button, Theme } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { useTheme } from '@material-ui/styles'
 
-import { browserStatus, generatePropeprMessage, isListingPage } from './CheckBrowser-helpers'
+import {
+  browserStatus,
+  generatePropeprMessage,
+  isListingPage
+} from './CheckBrowser-helpers'
 import CloseIcon from '../SvgIcons/Close/CloseIcon'
 
 const useStyles = makeStyles(theme => {
-    return {
-        snackbar: {
-            backgroundColor: theme.palette.warning.light,
-            minWidth: '40rem',
-            margin: theme.spacing(1),
-            zIndex: 2019,
-            position: 'fixed',
-            left: '50%',
-            transform: 'translateX(-50%)',
-            color: theme.palette.warning.dark
-        },
+  return {
+    snackbar: {
+      backgroundColor: theme.palette.warning.light,
+      minWidth: '40rem',
+      margin: theme.spacing(1),
+      zIndex: 2019,
+      position: 'fixed',
+      left: '50%',
+      transform: 'translateX(-50%)',
+      color: theme.palette.warning.dark
     }
-});
-
+  }
+})
 
 interface CheckBrowserPropsType extends React.FC {
-
-    id: string
-    children: React.ReactNode
+  id: string
+  children: React.ReactNode
 }
 
 function CheckBrowser(props: CheckBrowserPropsType) {
-    const theme = useTheme<Theme>();
-    const classes = useStyles();
-    const status = browserStatus()
-    const message = generatePropeprMessage(status)
-    const [isShowSnackbar, setShowSnackbar] = React.useState(!status.isSupported || status.isOutdated);
+  const theme = useTheme<Theme>()
+  const classes = useStyles()
+  const status = browserStatus()
+  const message = generatePropeprMessage(status)
+  const [isShowSnackbar, setShowSnackbar] = React.useState(
+    !status.isSupported || status.isOutdated
+  )
 
-    if (process.env.NODE_ENV === 'development' || isListingPage(props.id)) return props.children;
+  if (process.env.NODE_ENV === 'development' || isListingPage(props.id)) {
+    return props.children
+  }
 
-    return <>
-        {isShowSnackbar && <SnackbarContent
-            elevation={0}
-            className={classes.snackbar}
-            message={message.text}
-            action={[
-                <Button key={message.actionText} aria-label={message.actionText} color="inherit" onClick={() => window.open(message.actionLink, '_blank')}>
-                    {message.actionText}
-                </Button>,
-                <IconButton key="close" aria-label="close" color="inherit"
-                    onClick={() => setShowSnackbar(false)}
-                >
-                    <CloseIcon fill={theme.palette.warning.dark} />
-                </IconButton>,
-
-            ]}
-        />}
-        {props.children}
+  return (
+    <>
+      {isShowSnackbar && (
+        <SnackbarContent
+          elevation={0}
+          className={classes.snackbar}
+          message={message.text}
+          action={[
+            <Button
+              key={message.actionText}
+              aria-label={message.actionText}
+              color="inherit"
+              onClick={() => window.open(message.actionLink, '_blank')}
+            >
+              {message.actionText}
+            </Button>,
+            <IconButton
+              key="close"
+              aria-label="close"
+              color="inherit"
+              onClick={() => setShowSnackbar(false)}
+            >
+              <CloseIcon fill={theme.palette.warning.dark} />
+            </IconButton>
+          ]}
+        />
+      )}
+      {props.children}
     </>
+  )
 }
 
 export default CheckBrowser
--- types/npm-modules/@material-ui:core.d.ts
+++ types/npm-modules/@material-ui:core.d.ts
@@ -1,13 +1,11 @@
-import { Palette } from '@material-ui/core/styles/createPalette';
+import { Palette } from '@material-ui/core/styles/createPalette'
 
-declare module "@material-ui/core/styles/createPalette"{
-
-    interface Palette{
-
-        warning: {
-            main: string,
-            dark: string,
-            light: string
-        }
+declare module '@material-ui/core/styles/createPalette' {
+  interface Palette {
+    warning: {
+      main: string
+      dark: string
+      light: string
     }
+  }
 }
