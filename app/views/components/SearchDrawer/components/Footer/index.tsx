e322e8f7632738dcac986e6cb9f881591d12c5a8
OneDev0411 <cryptoapple1109@gmail.com>
2019-08-21 00:37:22 +0430

    #3049 chore(multi select): lint

--- app/views/components/SearchDrawer/components/Footer/index.tsx
+++ app/views/components/SearchDrawer/components/Footer/index.tsx
@@ -4,52 +4,51 @@ import Drawer from 'components/OverlayDrawer'
 import ActionButton from 'components/Button/ActionButton'
 
 interface ButtonPropsType {
-    selectedItemsCount: number,
-    buttonProps: {
-        onClick: () => void
-        disabled: boolean
-    }
+  selectedItemsCount: number
+  buttonProps: {
+    onClick: () => void
+    disabled: boolean
+  }
 }
 
 interface FooterPropsType {
-    multipleSelection: boolean
-    selectedItemsCount: number
-    renderAction: (props: ButtonPropsType) => void
-    handleSelectMultipleItems: () => void
+  multipleSelection: boolean
+  selectedItemsCount: number
+  renderAction: (props: ButtonPropsType) => void
+  handleSelectMultipleItems: () => void
 }
 
 function Footer(props: FooterPropsType) {
-    if (!props.multipleSelection) {
-        return null;
-    }
+  if (!props.multipleSelection) {
+    return null
+  }
 
-    let content;
-    const isButtonDisabled = props.selectedItemsCount === 0
-    const buttonProps = {
-        onClick: props.handleSelectMultipleItems,
-        disabled: isButtonDisabled
-    }
+  let content
+  const isButtonDisabled = props.selectedItemsCount === 0
+  const buttonProps = {
+    onClick: props.handleSelectMultipleItems,
+    disabled: isButtonDisabled
+  }
 
-    // Detecting and putting proper action button in `content`
-    if (props.renderAction) {
+  // Detecting and putting proper action button in `content`
+  if (props.renderAction) {
+    content = props.renderAction({
+      selectedItemsCount: props.selectedItemsCount,
+      buttonProps
+    })
+  } else {
+    content = (
+      <ActionButton {...buttonProps}>
+        {`${props.selectedItemsCount} Items Selected`}
+      </ActionButton>
+    )
+  }
 
-        content = props.renderAction({
-            selectedItemsCount: props.selectedItemsCount,
-            buttonProps
-        })
-    }
-    else {
-        content = <ActionButton
-            {...buttonProps}
-        >
-            {`${props.selectedItemsCount} Items Selected`}
-        </ActionButton>
-    }
-
-    // Render the final output
-    return <Drawer.Footer style={{ flexDirection: 'row-reverse' }}>
-        {content}
+  return (
+    <Drawer.Footer style={{ flexDirection: 'row-reverse' }}>
+      {content}
     </Drawer.Footer>
+  )
 }
 
 export default Footer
