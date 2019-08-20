8c53b2a1111a0412f3a805c2aecea114b9bab6c8
OneDev0411 <cryptoapple1109@gmail.com>
2019-08-20 19:24:13 +0300

    #3171 chore(calendar): fix hover color of OH action

--- app/views/components/Calendar/components/List/Event/index.tsx
+++ app/views/components/Calendar/components/List/Event/index.tsx
@@ -33,14 +33,14 @@ const useStyles = makeStyles((theme: Theme) => ({
     height: '100%',
     borderBottom: (props: StyleProps) =>
       props.hasBorderBottom ? '1px solid rgba(219, 230, 253, 0.5)' : 'none',
-    '& button': {
+    '& button, a.MuiButtonBase-root': {
       borderColor: '#eee',
       color: '#eee'
     },
     '&:hover': {
       transition: '0.2s ease-in background-color',
       backgroundColor: theme.palette.action.hover,
-      '& button': {
+      '& button, a.MuiButtonBase-root': {
         borderColor: 'inherit',
         color: 'inherit'
       }
