5264dc8333cf04a26f5e0abfb56be72436b6fb4e
OneDev0411 <cryptoapple1109@gmail.com>
2019-09-02 19:24:04 +0430

    #3138 wip_feat: add quick suggestions to bulk email compose

--- app/views/components/EmailCompose/BulkEmailComposeDrawer/index.tsx
+++ app/views/components/EmailCompose/BulkEmailComposeDrawer/index.tsx
@@ -81,6 +81,7 @@ export function BulkEmailComposeDrawer({
         readOnly={disableAddNewRecipient}
         name="to"
         component={EmailRecipientsChipsInput as any}
+        includeQuickSuggestions
         TextFieldProps={
           {
             inputProps: {
