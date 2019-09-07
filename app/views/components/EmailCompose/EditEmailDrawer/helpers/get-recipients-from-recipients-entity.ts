efb1c43c8bf1131863f0a2146d6001e36bd6b9a5
OneDev0411 <cryptoapple1109@gmail.com>
2019-09-08 00:25:00 +0300

    #3221 chore: apply necessary changes after rebase

--- app/views/components/EmailCompose/EditEmailDrawer/helpers/get-recipients-from-recipients-entity.ts
+++ app/views/components/EmailCompose/EditEmailDrawer/helpers/get-recipients-from-recipients-entity.ts
@@ -1,36 +1,41 @@
-import { notUndefined } from 'utils/ts-utils'
-
 export function getRecipientsFromRecipientsEntity(
   sendType: IEmailRecipient['send_type'],
   recipients: IEmailRecipient<'list' | 'contact'>[]
 ): IDenormalizedEmailRecipientInput[] {
   return recipients
     .filter(recipient => recipient.send_type === sendType)
-    .map<IDenormalizedEmailRecipientInput | undefined>(recipient => {
-      if (recipient.recipient_type === 'Tag') {
-        return {
-          recipient_type: 'Tag',
-          tag: {
-            type: 'crm_tag',
-            text: recipient.tag
-          } as IContactTag
-        }
-      }
-
-      if (recipient.recipient_type === 'List') {
-        return {
-          recipient_type: 'List',
-          list: recipient.list
-        }
-      }
-
-      if (recipient.recipient_type === 'Email' && recipient.email) {
-        return {
-          recipient_type: 'Email',
-          email: recipient.email,
-          contact: recipient.contact
-        }
+    .map<IDenormalizedEmailRecipientInput>(recipient => {
+      // With this switch case, if new type of recipients are added ever,
+      // we get a ts error here and we need to fix it.
+      switch (recipient.recipient_type) {
+        case 'Tag':
+          return {
+            recipient_type: 'Tag',
+            tag: {
+              type: 'crm_tag',
+              text: recipient.tag
+            } as IContactTag
+          }
+        case 'List':
+          return {
+            recipient_type: 'List',
+            list: recipient.list
+          }
+        case 'Email':
+          return {
+            recipient_type: 'Email',
+            email: recipient.email!,
+            contact: recipient.contact
+          }
+        case 'AllContacts':
+          return {
+            recipient_type: 'AllContacts'
+          }
+        case 'Brand':
+          return {
+            recipient_type: 'Brand',
+            brand: recipient.brand
+          }
       }
     })
-    .filter(notUndefined)
 }
