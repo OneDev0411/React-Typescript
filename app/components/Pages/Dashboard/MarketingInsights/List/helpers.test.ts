7fff08f0db4cf16d2b294bfd6afe25cec30c56fd
OneDev0411 <cryptoapple1109@gmail.com>
2019-09-13 09:02:42 +0430

    chore(insights): add some unit tests to make sure sorting is working as expected

---
+++ app/components/Pages/Dashboard/MarketingInsights/List/helpers.test.ts
@@ -0,0 +1,25 @@
+import { item1, item2, item3, item4 } from 'fixtures/insights/insight'
+
+import { doSort, SortValues } from './helpers'
+
+describe('Sort in Marketing Insights (List)', function() {
+  let mockList
+
+  beforeEach(function() {
+    mockList = [item1, item2, item3, item4]
+  })
+
+  it('should sorting the list by newest date', function() {
+    const result = doSort(mockList, SortValues.NEWEST)
+    const expectedResult = [item1, item2, item3, item4]
+
+    expect(result).toEqual(expectedResult)
+  })
+
+  it('should sorting the list by oldest date', function() {
+    const result = doSort(mockList, SortValues.OLDEST)
+    const expectedResult = [item4, item3, item2, item1]
+
+    expect(result).toEqual(expectedResult)
+  })
+})
