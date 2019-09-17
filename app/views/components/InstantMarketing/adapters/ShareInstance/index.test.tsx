a989d416630edc5edd5436ef246c7f7ab5658ddb
OneDev0411 <cryptoapple1109@gmail.com>
2019-09-17 14:28:02 +0430

    #3358 test(mc): add a failing test for #3358
    /spend 4h

---
+++ app/views/components/InstantMarketing/adapters/ShareInstance/index.test.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react'
+
+import { fireEvent, render } from '@testing-library/react'
+// eslint-disable-next-line import/no-unresolved
+import user from 'fixtures/users/agent.json'
+
+// eslint-disable-next-line import/no-unresolved
+import templateInstance from 'fixtures/marketing-center/template-instance.json'
+
+import { createBulkEmailCampaign } from 'models/email/create-bulk-email-campaign'
+
+import { TestBed } from '../../../../../../tests/unit/TestBed'
+
+// we use the connected component because we need to provide redux store
+// for inner components anyways
+import ShareInstance from '.'
+
+jest.mock('models/email/create-bulk-email-campaign')
+
+describe('ShareInstance', () => {
+  test('it renders', () => {
+    render(
+      <TestBed reduxState={{ user }}>
+        {/*
+      //@ts-ignore */}
+        <ShareInstance instance={templateInstance} isTriggered />
+      </TestBed>
+    )
+  })
+
+  // https://gitlab.com/rechat/web/issues/3358
+  test('It passes template id when sending email', () => {
+    const { getByTestId } = render(
+      <TestBed reduxState={{ user }}>
+        <ShareInstance
+          instance={templateInstance}
+          // hasExternalTrigger
+          isTriggered
+        />
+      </TestBed>
+    )
+
+    const recipientsInput = getByTestId('email-recipients-input')
+    const subjectInput = getByTestId('email-subject')
+    const subject = 'Testing ShareInstance action in MC'
+    const email = 'alireza.mirian@gmail.com'
+
+    // Add a recipient
+    fireEvent.change(recipientsInput, {
+      target: { value: email }
+    })
+    fireEvent.keyDown(recipientsInput, {
+      key: 'Enter',
+      code: 13,
+      charCode: 13
+    })
+
+    // Set subject
+    fireEvent.change(subjectInput, {
+      target: { value: subject }
+    })
+
+    // Send email
+    fireEvent.click(getByTestId('compose-send-email'))
+    expect(createBulkEmailCampaign).toHaveBeenCalledWith({
+      from: user.id,
+      html: templateInstance.html,
+      attachments: [],
+      due_at: undefined,
+      subject,
+      template: templateInstance.id,
+      to: [
+        {
+          recipient_type: 'Email',
+          email
+        }
+      ]
+    })
+  })
+})
