db9a87875723167e329aba6da440f45a53fa7f53
OneDev0411 <cryptoapple1109@gmail.com>
2019-09-17 04:41:25 +0430

    #3334 wip_feat(views): updated deals section

--- app/components/Pages/Dashboard/Contacts/Profile/Deals/List/Item/styled.js
+++ app/components/Pages/Dashboard/Contacts/Profile/Deals/List/Item/styled.js
@@ -1,48 +1,18 @@
 import styled from 'styled-components'
 
-import {
-  grey,
-  brandBackground
-} from '../../../../../../../../views/utils/colors'
-
 export const Container = styled.div`
   display: flex;
-  margin: 0 -0.5em 0.5em;
+  margin: 0 -0.5em;
   padding: 0.5em;
-
-  &:last-of-type {
-    margin-bottom: 0;
-  }
 
   &:hover {
     cursor: pointer;
-    border-radius: 3px;
-    background-color: ${brandBackground};
+    border-radius: ${props => props.theme.shape.borderRadius}px;
+    background-color: ${props => props.theme.palette.action.hover};
   }
-`
-
-export const Price = styled.b`
-  margin-right: 1em;
-  font-weight: bold;
-`
-
-export const Status = styled.span`
-  font-size: 0.875rem;
-  line-height: 1;
-  color: #fff;
-  padding: 0.25em 0.5em;
-  border-radius: 3px;
-  background-color: ${props => props.color};
-`
-
-export const Address = styled.div`
-  font-size: 0.875rem;
-  color: ${grey.A900};
 `
 
 export const Role = styled.div`
   font-size: 0.875rem;
-  color: ${grey.A900};
-  font-weight: 600;
-  margin-bottom: 0.5em;
+  color: ${props => props.theme.palette.text.hint};
 `
