8803e5ea4791fc3a5f8ab2e706e6137ff8bb2622
OneDev0411 <cryptoapple1109@gmail.com>
2019-09-19 02:06:38 +0430

    #3334 refactor(crm/profile): updated tags section to TS

--- app/components/Pages/Dashboard/Contacts/Profile/Tags/TagsList.tsx
+++ app/components/Pages/Dashboard/Contacts/Profile/Tags/TagsList.tsx
@@ -1,18 +1,17 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Box } from '@material-ui/core'
-import uniq from 'lodash/uniq'
+import uniqBy from 'lodash/uniqBy'
 
 import { Tag } from './Tag'
 
-TagsList.propTypes = {
-  tags: PropTypes.arrayOf(PropTypes.shape()).isRequired
+interface Props {
+  tags: ICRMTag[]
 }
 
-export function TagsList(props) {
+export default function TagsList(props: Props) {
   return (
     <Box display="flex" alignItems="center" flexWrap="wrap">
-      {uniq(props.tags, tag => tag.text).map(tag => (
+      {uniqBy(props.tags, tag => tag.text).map(tag => (
         <Tag key={tag.id} text={tag.text} />
       ))}
     </Box>
---
+++ app/components/Pages/Dashboard/Contacts/Profile/Tags/TagsSection.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { Button } from '@material-ui/core'
+
+import { IAppState } from 'reducers/index'
+import { selectDefinitionByName } from 'reducers/contacts/attributeDefs'
+import { getContactAttribute } from 'models/contacts/helpers/get-contact-attribute'
+
+import { Section } from '../components/Section'
+import TagsOverlay from '../../components/TagsOverlay'
+
+import TagsList from './TagsList'
+
+interface Props {
+  contact: INormalizedContact
+  tags: ICRMTag[]
+}
+
+function Tags(props: Props) {
+  const hasTags = props.tags.length > 0
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+
+  return (
+    <Section
+      onEdit={hasTags ? () => setIsDrawerOpen(true) : undefined}
+      title="Tags"
+      setting={{
+        tooltip: 'Manage Tags',
+        href: '/dashboard/account/manage-tags'
+      }}
+    >
+      <div style={{ padding: '0 1.5rem' }}>
+        {hasTags ? (
+          <TagsList tags={props.tags} />
+        ) : (
+          <Button
+            color="secondary"
+            variant="outlined"
+            onClick={() => setIsDrawerOpen(true)}
+          >
+            Add Tag
+          </Button>
+        )}
+      </div>
+
+      <TagsOverlay
+        contact={props.contact}
+        closeOverlay={() => setIsDrawerOpen(false)}
+        isOpen={isDrawerOpen}
+        selectedContactsIds={[props.contact.id]}
+      />
+    </Section>
+  )
+}
+
+const mapStateToProps = (
+  state: IAppState,
+  props: { contact: INormalizedContact }
+) => {
+  const attributeDef = selectDefinitionByName(
+    state.contacts.attributeDefs,
+    'tag'
+  )
+
+  const tags = attributeDef
+    ? getContactAttribute(props.contact, attributeDef)
+    : []
+
+  return {
+    tags
+  }
+}
+
+export default connect(mapStateToProps)(Tags)
