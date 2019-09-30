7eb69f25622f239ee9d236cfafb7643f53e1d540
OneDev0411 <cryptoapple1109@gmail.com>
2019-09-30 10:29:00 +0330

    #3368 wip_feat(email-threads): update email thread calendar event UI

--- app/views/components/Calendar/components/List/Event/EmailThread/index.tsx
+++ app/views/components/Calendar/components/List/Event/EmailThread/index.tsx
@@ -1,12 +1,16 @@
 import React, { useContext } from 'react'
 
 import { eventTypesIcons as eventIcons } from 'views/utils/event-types-icons'
+import { getTrimmedArrayAndOthersText } from 'utils/get-trimmed-array-and-others-text'
+import { parseEmailRecipient } from 'components/EmailRecipientsChipsInput/helpers/parse-email-recipient'
+import MiniContactProfile from 'components/MiniContact/MiniContactProfile'
 
 import { ListContext } from '../../context'
-
 import { EventContainer } from '../components/EventContainer'
-
 import styles from '../styles'
+import { EventBadge } from '../components/EventBadge'
+import IconAttachment from '../../../../../SvgIcons/Attachment/IconAttachment'
+import { iconSizes } from '../../../../../SvgIcons/icon-sizes'
 
 interface Props {
   style: React.CSSProperties
@@ -16,6 +20,11 @@ interface Props {
 
 export function EmailThread({ style, event, nextItem }: Props) {
   const { setSelectedEvent } = useContext(ListContext)
+  const thread = event.full_thread
+
+  const { visibleItems: recipients, othersText } = getTrimmedArrayAndOthersText(
+    thread.to
+  )
 
   return (
     <EventContainer
@@ -37,6 +46,40 @@ export function EmailThread({ style, event, nextItem }: Props) {
           >
             Email
           </a>
+          &nbsp;to&nbsp;
+          {recipients.map((recipient, index) => {
+            const { displayName, emailAddress } = parseEmailRecipient(recipient)
+
+            return (
+              <React.Fragment key={index}>
+                {index !== 0 && <>,&nbsp;</>}
+                <MiniContactProfile
+                  as="span"
+                  data={{
+                    email_address: emailAddress,
+                    display_name:
+                      displayName !== emailAddress ? displayName : undefined
+                  }}
+                  type="insight"
+                >
+                  <span>{displayName || emailAddress}</span>
+                </MiniContactProfile>
+              </React.Fragment>
+            )
+          })}
+          {othersText && (
+            <>
+              &nbsp;and <span>{othersText}</span>
+            </>
+          )}
+          {thread.email_count > 1 && (
+            <EventBadge>{thread.email_count}</EventBadge>
+          )}
+          {thread.has_attachments && (
+            <EventBadge padding="dense">
+              <IconAttachment size={iconSizes.small} />
+            </EventBadge>
+          )}
         </div>
       }
       subtitle={<div>{event.title || 'No Subject'}</div>}
