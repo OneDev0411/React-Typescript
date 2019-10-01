ee09c0d59bb3a932212b61a880cdd836f35fc9d6
OneDev0411 <cryptoapple1109@gmail.com>
2019-10-02 02:30:07 +0330

    #1805 refactor(tour/list): empty statt styles

--- app/components/Pages/Dashboard/Tours/List/EmptyState.tsx
+++ app/components/Pages/Dashboard/Tours/List/EmptyState.tsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import { Box } from '@material-ui/core'
+import { makeStyles, createStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    box: {
+      alignItems: 'center',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      margin: '20vh auto 0',
+      maxWidth: '33rem',
+      textAlign: 'center'
+    }
+  })
+)
 
 import CreateNewTour from './CreateNewTour'
 
 export default function EmptyState(props: { onOpenDrawer: () => void }) {
+  const classes = useStyles()
+
   return (
-    <Box
-      alignItems="center"
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      margin="20vh auto 0"
-      maxWidth="33rem"
-      textAlign="center"
-    >
+    <Box className={classes.box}>
       <img src="/static/images/contacts/zero-state.svg" alt="Houston" />
       <h2>No toursheets are scheduled… Yet!</h2>
       <p>
