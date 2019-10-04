dcf4fbe4d198bb44ff13a8f46e153d1c729c5d00
OneDev0411 <cryptoapple1109@gmail.com>
2019-10-04 16:51:33 +0330

    #3437 refactor(constants/listings): options

---
+++ app/constants/listings/defaults.ts
@@ -0,0 +1,16 @@
+interface Location {
+  lat: number
+  lng: number
+}
+
+export const TEXAS_LOCATION: Location = {
+  lat: 31.9685988,
+  lng: -99.9018131
+}
+
+export const DALLAS_LOCATION: Location = {
+  lat: 32.7767,
+  lng: -96.797
+}
+
+export const DEFAULT_ZOOM: number = 14
