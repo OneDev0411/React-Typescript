ab912bfa0b25afce2c2d0ea45070a6046a4b9e7b
OneDev0411 <cryptoapple1109@gmail.com>
2019-10-04 16:32:45 +0330

    #3438 fix(mls/listing): empty mls and mls_name

--- app/components/Pages/Dashboard/Listings/Listing/components/MLSNote.js
+++ app/components/Pages/Dashboard/Listings/Listing/components/MLSNote.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import fecha from 'fecha'
 
-export default function MLSNote({ listing }) {
+export default function MLSNote({ mls, mlsName }) {
+  if (!mlsName && !mls) {
+    return null
+  }
+
   const today = new Date()
 
   return (
     <div style={{ maxWidth: '60em', marginTop: '2em', color: '#aaa' }}>
       <div>
-        {`Listing information provided in part by the ${
-          listing.mls_name
-        }, for personal, non-commercial use by viewers of
+        {`Listing information provided in part by the ${mlsName}, for personal, non-commercial use by viewers of
         this site and may not be reproduced or redistributed. All information is
         deemed reliable but not guaranteed.`}
       </div>
       <div>
-        {`Copyright © ${
-          listing.mls
-        } ${today.getFullYear()}. All rights reserved. Last updated at ${fecha.format(
+        {`Copyright © ${mls} ${today.getFullYear()}. All rights reserved. Last updated at ${fecha.format(
           today,
           'mediumDate'
         )}.`}
