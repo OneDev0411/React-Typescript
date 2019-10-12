65d2019bb4138fbfc3f1d2b9209eb42c78f04373
OneDev0411 <cryptoapple1109@gmail.com>
2019-10-12 13:30:48 +0330

    chore: remove node_modules caching in CI altogether
    It didn't worked as expected and the occasional saves in pipeline execution time didn't worth the introduced
    problems and the time it was taking from the team. So we decided to remove it.

--- githooks/pre-commit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# gsed is actually required but it's exposed as sed in some macs
-# it's recommended to install gnu-sed (gsed) via `brew install gnu-sed`
-cmd=$(which gsed || which sed)
-$cmd -i "/CI_CACHE_KEY:/c\  CI_CACHE_KEY: \"$(md5sum package-lock.json| cut -c 1-32)\"" .gitlab-ci.yml
-git add .gitlab-ci.yml
