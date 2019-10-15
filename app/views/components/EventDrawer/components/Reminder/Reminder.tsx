e6d60feca8b15ed1b101bc51b62b52e9d03b25ef
OneDev0411 <cryptoapple1109@gmail.com>
2019-10-15 13:48:11 +0330

    refactor(views): event drawer reminder field (#1805)

--- app/views/components/EventDrawer/components/Reminder/Reminder.tsx
+++ app/views/components/EventDrawer/components/Reminder/Reminder.tsx
@@ -5,15 +5,12 @@ import { ReminderField } from 'components/final-form-fields'
 import { FieldContainer } from '../../styled'
 
 export default function Reminder(props: { dueDate: Date }) {
-  const dueDateTimestamp = new Date(props.dueDate).getTime()
-  const now = Date.now()
-
-  if (dueDateTimestamp < now - 1) {
+  if (props.dueDate.getTime() < Date.now() - 1) {
     return null
   }
 
   return (
-    <FieldContainer alignCenter justifyBetween style={{ marginBottom: '2em' }}>
+    <FieldContainer>
       <ReminderField dueDate={props.dueDate} />
     </FieldContainer>
   )
