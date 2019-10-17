9d844ccf5a0ec857760ae9a0c825129fa7993312
OneDev0411 <cryptoapple1109@gmail.com>
2019-10-17 17:35:35 +0330

    refactor: address MR discussions (#3521)

--- scripts/update-gl-issue-labels.js
+++ scripts/update-gl-issue-labels.js
@@ -1,54 +1,5 @@
 const https = require('https')
 
-async function main() {
-  const GITLAB_PRIVATE_TOKEN = process.env.GITLAB_PRIVATE_TOKEN
-  const COMMIT_MESSAGE = process.env.CI_COMMIT_TITLE
-  const labelToRemove = process.argv[2] || 'Merge Requested'
-  const labelToAdd = process.argv[3] || 'Deployed to Boer'
-
-  if (!COMMIT_MESSAGE) {
-    console.warn('CI_COMMIT_TITLE should exist')
-
-    return
-  }
-
-  const [iid] = COMMIT_MESSAGE.match(/\d{4}/g) || []
-
-  if (!iid) {
-    return
-  }
-
-  const issue = await sendRequest(
-    `https://gitlab.com/api/v4/projects/rechat%2fweb/issues/${iid}`,
-    {
-      method: 'GET',
-      headers: {
-        'PRIVATE-TOKEN': GITLAB_PRIVATE_TOKEN
-      }
-    }
-  )
-
-  const newLabels = issue.labels
-    .filter(label => label !== labelToRemove)
-    .concat(labelToAdd)
-
-  const queryString = `labels=${newLabels.join(',')}`
-
-  await sendRequest(
-    `https://gitlab.com/api/v4/projects/rechat%2fweb/issues/${iid}?${queryString}`,
-    {
-      method: 'PUT',
-      headers: {
-        'PRIVATE-TOKEN': GITLAB_PRIVATE_TOKEN
-      }
-    }
-  )
-}
-
-main().catch(e => {
-  console.error('error in updating labels', e)
-})
-
 async function sendRequest(url, options) {
   return new Promise((resolve, reject) => {
     const req = https.request(url, options, response => {
@@ -56,7 +7,7 @@ async function sendRequest(url, options) {
       response.on('data', buffer => {
         const data = JSON.parse(buffer.toString())
 
-        if (`${response.statusCode}`[0] === '2') {
+        if (response.statusCode >= 200 && response.statusCode < 300) {
           return resolve(data)
         }
 
@@ -67,3 +18,52 @@ async function sendRequest(url, options) {
     req.end()
   })
 }
+
+async function main() {
+  const GITLAB_PRIVATE_TOKEN = process.env.GITLAB_PRIVATE_TOKEN
+  const COMMIT_TITLE = process.env.CI_COMMIT_TITLE
+  const labelToRemove = process.argv[2]
+  const labelToAdd = process.argv[3]
+
+  if (!COMMIT_TITLE) {
+    console.warn('CI_COMMIT_TITLE should exist')
+
+    return
+  }
+
+  const [issueNumber] = COMMIT_TITLE.match(/\d{4}/g) || []
+
+  if (!issueNumber) {
+    return
+  }
+
+  const issue = await sendRequest(
+    `https://gitlab.com/api/v4/projects/rechat%2fweb/issues/${issueNumber}`,
+    {
+      method: 'GET',
+      headers: {
+        'PRIVATE-TOKEN': GITLAB_PRIVATE_TOKEN
+      }
+    }
+  )
+
+  const newLabels = issue.labels
+    .filter(label => label !== labelToRemove)
+    .concat(labelToAdd || [])
+
+  const queryString = `labels=${newLabels.join(',')}`
+
+  await sendRequest(
+    `https://gitlab.com/api/v4/projects/rechat%2fweb/issues/${issueNumber}?${queryString}`,
+    {
+      method: 'PUT',
+      headers: {
+        'PRIVATE-TOKEN': GITLAB_PRIVATE_TOKEN
+      }
+    }
+  )
+}
+
+main().catch(e => {
+  console.error('Error updating GL issue labels', e)
+})
