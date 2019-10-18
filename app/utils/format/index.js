c8e6090dd0c754831465283d159d102994b68587
OneDev0411 <cryptoapple1109@gmail.com>
2019-10-18 23:03:55 +0330

    chore(mc): removed national code from phone numbers (#3528)

--- app/utils/format/index.js
+++ app/utils/format/index.js
@@ -21,8 +21,7 @@ export function formatPhoneNumber(phoneNumber) {
   const match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/)
 
   if (match) {
-    const intlCode = (match[1] ? '+1 ' : '')
-    return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('')
+    return ['(', match[2], ') ', match[3], '-', match[4]].join('')
   }
   
   return phoneNumber
