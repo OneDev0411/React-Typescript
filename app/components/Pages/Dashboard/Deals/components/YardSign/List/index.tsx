cd0c3e198f756a5d7a901903dcbe89d334b985a5
OneDev0411 <cryptoapple1109@gmail.com>
2019-10-18 21:07:18 +0300

    wip_feat(deals): develop YardSign list (#3496)

---
+++ app/components/Pages/Dashboard/Deals/components/YardSign/List/index.tsx
@@ -0,0 +1,101 @@
+import React, { useMemo } from 'react'
+import { connect } from 'react-redux'
+
+import {
+  Button,
+  Typography,
+  createStyles,
+  makeStyles,
+  Theme
+} from '@material-ui/core'
+import fecha from 'fecha'
+
+import { IAppState } from 'reducers/index'
+
+import { selectDealTasks } from 'reducers/deals/tasks'
+
+import { Divider } from 'components/Divider'
+
+import { Container } from './styled'
+
+interface StateProps {
+  user: IUser
+  tasks: IDealTask[]
+}
+
+interface Props {
+  deal: IDeal
+  onClickNewItem(): void
+  onSelectItem(task: IDealTask): void
+}
+
+const useStyles = makeStyles((theme: Theme) => {
+  return createStyles({
+    buttonLabel: {
+      alignItems: 'flex-start',
+      flexDirection: 'column'
+    },
+    date: {
+      color: theme.palette.grey.A700
+    }
+  })
+})
+
+function List(props: Props & StateProps) {
+  const classes = useStyles()
+
+  const tasks = useMemo(() => {
+    return props.tasks.filter(task => task.task_type === 'YardSign')
+  }, [props.tasks])
+
+  return (
+    <Container>
+      <Button
+        fullWidth
+        color="primary"
+        size="large"
+        classes={{
+          label: classes.buttonLabel
+        }}
+        onClick={props.onClickNewItem}
+      >
+        New Yard Sign Request
+      </Button>
+      <Divider />
+
+      {tasks.map(task => (
+        <div key={task.id}>
+          <Button
+            fullWidth
+            color="secondary"
+            size="large"
+            classes={{
+              label: classes.buttonLabel
+            }}
+            onClick={() => props.onSelectItem(task)}
+          >
+            <Typography variant="body1">{task.title}</Typography>
+            <Typography variant="body1" className={classes.date}>
+              {fecha.format(
+                new Date(task.created_at * 1000),
+                'dddd, MMMM D, YYYY - hh:mmA'
+              )}
+            </Typography>
+          </Button>
+        </div>
+      ))}
+    </Container>
+  )
+}
+
+function mapStateToProps(
+  { user, deals }: IAppState,
+  ownProps: Props
+): StateProps {
+  return {
+    user,
+    tasks: selectDealTasks(ownProps.deal, deals.checklists, deals.tasks)
+  }
+}
+
+export default connect(mapStateToProps)(List)
