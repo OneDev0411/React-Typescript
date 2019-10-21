589450b8710c31979b248048059c95d04e3dfd0a
OneDev0411 <cryptoapple1109@gmail.com>
2019-10-21 18:27:09 +0330

    wip_feat(editor): consider block level links in removeLink helper (#3465)

--- app/views/components/TextEditor/utils/remove-link.ts
+++ app/views/components/TextEditor/utils/remove-link.ts
@@ -1,15 +1,22 @@
 import { EditorState, RichUtils, SelectionState } from 'draft-js'
-
 import {
   getEntityRange,
   getSelectionEntity,
-  getSelectionInlineStyle
+  getSelectionInlineStyle,
+  setBlockData
 } from 'draftjs-utils'
 
-export function removeLink(editorState: EditorState) {
+import { getSelectedBlock } from './get-selected-block'
+
+export function removeLink(editorState: EditorState): EditorState {
   const entityKey = getSelectionEntity(editorState)
 
-  if (entityKey) {
+  const entity =
+    entityKey && editorState.getCurrentContent().getEntity(entityKey)
+
+  const selectedBlock = getSelectedBlock(editorState)
+
+  if (entity && entity.getType() === 'LINK') {
     const entityRange = getEntityRange(editorState, entityKey)
 
     const selection = editorState.getSelection()
@@ -34,4 +41,19 @@ export function removeLink(editorState: EditorState) {
 
     return newEditorState
   }
+
+  if (selectedBlock) {
+    const data = selectedBlock.getData()
+
+    const href = data.get('href')
+
+    if (href) {
+      return setBlockData(editorState, {
+        href: undefined,
+        tooltip: undefined
+      })
+    }
+  }
+
+  return editorState
 }
