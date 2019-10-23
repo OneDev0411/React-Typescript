f1488fae3ffd17d0e3a7f1b81b5cdf2be9d843ff
OneDev0411 <cryptoapple1109@gmail.com>
2019-10-23 16:24:01 +0330

    feat(editor): remove unnecessary file picker dialog when adding image (#3473)

--- app/views/components/TextEditor/buttons/AddImageButton/index.tsx
+++ app/views/components/TextEditor/buttons/AddImageButton/index.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react'
-import { useState } from 'react'
 
 import { Tooltip } from '@material-ui/core'
 
 import ImageFileIcon from 'components/SvgIcons/ImageFile/ImageFileIcon'
 
-import { ImagePickerModal } from './ImagePickerModal'
 import { ToolbarIconButton } from '../ToolbarIconButton'
+import { FilePicker } from '../../../FilePicker'
 
 interface Props {
   onImageSelected: (file: File) => void
@@ -15,39 +14,23 @@ interface Props {
 /**
  * When clicked, opens a dialog which lets user select an image and finally
  * calls `onImageSelected` prop with the dataURl of the selected image.
- * For now, it uses {@link ImageUploader} for selecting image, but it can
- * be replaced with a more advanced component which lets image selection
- * from listings for example or from Dropbox. It's not necessarily good to
- * use the component which is used for selecting Avatar
- * (which requires features like crop) here too.
  * @param props
  * @constructor
  */
 export function AddImageButton(props: Props) {
-  const [open, setOpen] = useState(false)
-  const handleClick = () => {
-    setOpen(true)
-  }
-
-  const close = () => setOpen(false)
-
   const selectImage = async files => {
     props.onImageSelected(files[0])
-    close()
   }
 
   return (
-    <>
-      <Tooltip title="Insert Image">
-        <ToolbarIconButton onClick={handleClick} data-test="add-image-button">
-          <ImageFileIcon />
-        </ToolbarIconButton>
-      </Tooltip>
-      <ImagePickerModal
-        isOpen={open}
-        onFileSelected={selectImage}
-        onClose={close}
-      />
-    </>
+    <FilePicker onFilePicked={selectImage}>
+      {({ pickFiles }) => (
+        <Tooltip title="Insert Image">
+          <ToolbarIconButton onClick={pickFiles} data-test="add-image-button">
+            <ImageFileIcon />
+          </ToolbarIconButton>
+        </Tooltip>
+      )}
+    </FilePicker>
   )
 }
