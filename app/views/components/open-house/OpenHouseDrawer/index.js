1fcdebaf3c3e08ec5231012d250a08c0ed977d3b
OneDev0411 <cryptoapple1109@gmail.com>
2019-11-01 20:41:48 +0300

    fix(open house): fix edit registration page (#3576)

--- app/views/components/open-house/OpenHouseDrawer/index.js
+++ app/views/components/open-house/OpenHouseDrawer/index.js
@@ -349,12 +349,9 @@ class OpenHouseDrawerInternal extends React.Component {
     const { isDisabled, openHouse, error } = this.state
 
     return (
-      <Drawer
-        open={this.props.isOpen && !this.state.isTemplateBuilderOpen}
-        onClose={this.props.onClose}
-      >
+      <Drawer open={this.props.isOpen} onClose={this.props.onClose}>
         <Drawer.Header
-          title={`${this.isNew ? 'New' : 'Edit'} Open House Registration Pages`}
+          title={`${this.isNew ? 'New' : 'Edit'} Open House Registration Page`}
         />
 
         <Drawer.Body>
