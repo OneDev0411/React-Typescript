72bf03dc3babf54fb6d287bbe49007d0f8afb1e5
OneDev0411 <cryptoapple1109@gmail.com>
2019-11-09 22:03:19 +0300

    fix(grid): fix showing sorting plugin (#2221)

--- app/views/components/Grid/Table/index.js
+++ app/views/components/Grid/Table/index.js
@@ -15,8 +15,10 @@ import { ActionablePlugin } from './Plugins/Actionable'
 import { TableSummary } from './TableSummary'
 
 class Grid extends React.Component {
-  componentDidMount() {
-    const { plugins } = this.props
+  constructor(props) {
+    super(props)
+
+    const { plugins } = props
 
     if (plugins.sortable) {
       this.sortablePlugin = new SortablePlugin({
@@ -30,6 +32,10 @@ class Grid extends React.Component {
         options: plugins.loadable
       })
     }
+  }
+
+  componentDidMount() {
+    const { plugins } = this.props
 
     if (plugins.selectable) {
       this.selectablePlugin = new SelectablePlugin({
