8dc6c29c0d82ef1a55bd651dd237f4fd31c6e689
OneDev0411 <cryptoapple1109@gmail.com>
2019-11-10 19:05:34 +0330

    fix(editor): fix image sizing and alignment problem in Outlook Desktop (#3037)

---
+++ app/views/components/TextEditor/utils/render-atomic-block-attributes.ts
@@ -0,0 +1,26 @@
+import { AtomicBlockEntityData } from '../types'
+import { renderAttributes } from './render-attributes'
+
+/**
+ * IMPORTANT: this is mainly because of Outlook desktop which seem to strip
+ * styles
+ * Creates attributes string based on entity data created by plugins used for
+ * atomic blocks like image. Their internal blockRendererFn is not useful
+ * because they don't render to string, and also the signature is different than
+ * blockRendererFn in stateToHTML options.
+ * @param alignment
+ * @param width
+ * @param height
+ */
+export function renderAtomicBlockAttrs({
+  src,
+  alignment,
+  width,
+  height
+}: AtomicBlockEntityData): string {
+  return renderAttributes({
+    src,
+    width,
+    align: alignment
+  })
+}
