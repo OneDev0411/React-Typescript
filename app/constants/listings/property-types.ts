9e50d9898e43eeac3f2062817bba658fafbedda3
OneDev0411 <cryptoapple1109@gmail.com>
2019-11-22 16:45:27 +0300

    wip_feat(constans/listings): added property types (#3451)

---
+++ app/constants/listings/property-types.ts
@@ -0,0 +1,9 @@
+export const LEASE_PROPERTY_TYPES: string[] = ['Residential Lease']
+
+export const PROPERTY_TYPES: string[] = [
+  ...LEASE_PROPERTY_TYPES,
+  'Residential',
+  'Multi-Family,Commercial',
+  'Lots & Acreage',
+  'Unknown'
+]
