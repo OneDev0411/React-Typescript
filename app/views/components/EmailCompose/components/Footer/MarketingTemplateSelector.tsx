4c54cc9aac12db2a520ef815b8712f1658a7112f
OneDev0411 <cryptoapple1109@gmail.com>
2019-11-26 01:56:46 +0330

    wip_feat(email-compose): finish UI for My Design template cards (#3671)

--- app/views/components/EmailCompose/components/Footer/MarketingTemplateSelector.tsx
+++ app/views/components/EmailCompose/components/Footer/MarketingTemplateSelector.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { useState } from 'react'
 
 import {
   Box,
@@ -17,6 +18,7 @@ import MarketingTemplateCard, {
 } from 'components/MarketingTemplateCard'
 import { ScrollableArea } from 'components/ScrollableArea'
 import { ServerError } from 'components/ServerError'
+import MarketingTemplatePreviewModal from 'components/MarketingTemplatePreviewModal'
 
 import useTemplatesHistory from '../../../../../components/Pages/Dashboard/Marketing/History/useTemplatesHistory'
 import { FooterBottomDrawerZeroState } from './FooterBottomDrawerZeroState'
@@ -42,6 +44,12 @@ const useStyles = makeStyles(
 
 export function MarketingTemplateSelector(props: Props) {
   const [templates, loading, error] = useTemplatesHistory()
+  const [isPreviewModalOpen, setPreviewModalOpen] = useState(false)
+  const [
+    selectedTemplate,
+    setSelectedTemplate
+  ] = useState<IMarketingTemplateInstance | null>(null)
+
   const classes = useStyles()
 
   if (error) {
@@ -78,29 +86,62 @@ export function MarketingTemplateSelector(props: Props) {
           key={template.id}
           classes={{ card: classes.card }}
           template={template}
+          handlePreview={() => {
+            setSelectedTemplate(template)
+            setPreviewModalOpen(true)
+          }}
+          actions={
+            <>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => props.onTemplateSelected(template)}
+              >
+                Add
+              </Button>
+            </>
+          }
         />
       ))
 
   return (
-    <ScrollableArea hasThinnerScrollbar shadowHeight={20} shadowColor="white">
-      <ListItem
-        dense
-        button
-        divider
-        component={Link}
-        target="_blank"
-        to="/dashboard/marketing"
-      >
-        <ListItemText
-          primary={<Box color="primary.main">Open My Designs</Box>}
-        />
-      </ListItem>
-      <MarketingTemplateMasonry
-        breakpointCols={COLUMN_COUNT}
-        className={classes.masonry}
-      >
-        {items}
-      </MarketingTemplateMasonry>
-    </ScrollableArea>
+    <>
+      <ScrollableArea hasThinnerScrollbar shadowHeight={20} shadowColor="white">
+        <ListItem
+          dense
+          button
+          divider
+          component={Link}
+          target="_blank"
+          to="/dashboard/marketing"
+        >
+          <ListItemText
+            primary={<Box color="primary.main">Open My Designs</Box>}
+          />
+        </ListItem>
+        <MarketingTemplateMasonry
+          breakpointCols={COLUMN_COUNT}
+          className={classes.masonry}
+        >
+          {items}
+        </MarketingTemplateMasonry>
+      </ScrollableArea>
+      <MarketingTemplatePreviewModal
+        type="history"
+        isOpen={isPreviewModalOpen}
+        onClose={() => setPreviewModalOpen(false)}
+        selectedTemplate={selectedTemplate}
+        setSelectedTemplate={setSelectedTemplate}
+        actions={
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => props.onTemplateSelected(selectedTemplate)}
+          >
+            Add
+          </Button>
+        }
+      />
+    </>
   )
 }
