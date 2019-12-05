73e8de9dd1be42a975b104f0590ef5fcc1dd86c2
OneDev0411 <cryptoapple1109@gmail.com>
2019-12-05 09:34:37 -0800

    fix(mc): clean up. i was sleepy

--- app/views/components/InstantMarketing/Builder/Blocks/adapt.ts
+++ app/views/components/InstantMarketing/Builder/Blocks/adapt.ts
@@ -1,17 +1,15 @@
 function adapt(parent: any, template: string): string {
   const type = parent.get('type')
 
-  let adapted = template
-
   if (type === 'mj-section') {
-    adapted = `<mj-column>${template}</mj-column>`
+    return `<mj-column width="100%">${template}</mj-column>`
   }
 
   if (type === 'mj-wrapper') {
-    adapted = `<mj-section><mj-column>${template}</mj-column></mj-section>`
+    return `<mj-section><mj-column width="100%">${template}</mj-column></mj-section>`
   }
 
-  return adapted
+  return template
 }
 
 export default adapt
