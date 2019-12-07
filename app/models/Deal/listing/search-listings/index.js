911ff6a57ee79f1c68e196e700e01593429c2ed4
OneDev0411 <cryptoapple1109@gmail.com>
2019-12-07 17:29:16 +0300

    fix(search listing drawer): refactor model

--- app/models/Deal/listing/search-listings/index.js
+++ app/models/Deal/listing/search-listings/index.js
@@ -4,29 +4,15 @@ import Fetch from '../../../../services/fetch'
  * search listings
  */
 export async function searchListings(criteria) {
-  let queryName = 'q'
   const isMlsNumber = /^[0-9]{5,8}$/.test(criteria)
-  const normalizeMlsObject = data => ({
-    ...data.property,
-    id: data.id,
-    is_mls_search: true,
-    price: data.price,
-    status: data.status,
-    cover_image_url: data.cover_image_url
-  })
+  const queryName = isMlsNumber ? 'mls_number' : 'q'
 
   try {
-    if (isMlsNumber) {
-      queryName = 'mls_number'
-    }
-
     const response = await new Fetch().get(
       `/listings/search?${queryName}=${encodeURIComponent(criteria)}`
     )
 
-    const { data } = response.body
-
-    return isMlsNumber ? [normalizeMlsObject(data)] : data
+    return response.body.data
   } catch (e) {
     throw e
   }
