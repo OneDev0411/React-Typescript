cde331af44983ed76ddd81242a0dc7ed6c81c0a2
OneDev0411 <cryptoapple1109@gmail.com>
2019-12-13 19:47:59 +0330

    wip_fix(email-compose): fix editing email when sender account is deleted (#3660)

--- app/views/components/TextEditor/features/Emoji/index.tsx
+++ app/views/components/TextEditor/features/Emoji/index.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Tooltip } from '@material-ui/core'
-import { mergeClasses } from '@material-ui/styles'
 import createEmojiPlugin from 'draft-js-emoji-plugin'
+import EmojiSelectPopover from 'draft-js-emoji-plugin/lib/components/EmojiSelect/Popover'
+import defaultEmojiGroups from 'draft-js-emoji-plugin/lib/constants/defaultEmojiGroups'
+import createEmojisFromStrategy from 'draft-js-emoji-plugin/lib/utils/createEmojisFromStrategy'
+import strategy from 'emojione/emoji.json'
+import { mergeWith } from 'lodash'
+
+import { useLatestValueRef } from 'hooks/use-latest-value-ref'
+
+import IconEmoji from 'components/SvgIcons/Emoji/IconEmoji'
 
 import { ToolbarFragment } from '../../components/ToolbarFragment'
 import { useEditorPlugins } from '../../hooks/use-editor-plugins'
@@ -13,18 +21,49 @@ import { useEmojiStyles } from './use-emoji-styles'
  * with the latest version of draft-js, not the version we use right now
  */
 import 'draft-js-emoji-plugin/lib/plugin.css'
+import { ToolbarIconButton } from '../../components/ToolbarIconButton'
 
-export function EmojiFeature() {
+import { useEditor } from '../../hooks/use-editor'
+
+import { BaseDropdown } from '../../../BaseDropdown'
+
+const emojis = createEmojisFromStrategy(strategy)
+const imagePath = '//cdn.jsdelivr.net/emojione/assets/svg/'
+const cacheBustParam = '?v=2.2.7'
+const imageType = 'svg'
+const toneSelectOpenDelay = 500
+
+interface Props {
+  /**
+   * If true, emoji suggestion popover will be closed upon selecting an emoji.
+   * defaults to true
+   */
+  closeOnSelection?: boolean
+}
+
+export function EmojiFeature({ closeOnSelection = true }: Props) {
   const emojiTheme = useEmojiStyles()
 
+  const { setEditorState, editorState } = useEditor()
+  const editorStateRef = useLatestValueRef(editorState)
+  const storeRef = useRef({
+    setEditorState,
+    getEditorState: () => editorStateRef.current
+  })
+
+  const theme = mergeWith(
+    defaultTheme,
+    emojiTheme,
+    {
+      emojiSelectPopoverGroups: 'emoji-group' // for closing on click
+    },
+    (value1, value2, value3) =>
+      [value1, value2, value3].filter(i => i).join(' ')
+  )
   const { emojiPlugin } = useEditorPlugins(
     () => ({
       emojiPlugin: createEmojiPlugin({
-        theme: mergeClasses({
-          baseClasses: defaultTheme,
-          newClasses: emojiTheme,
-          Component: null
-        }),
+        theme,
         positionSuggestions: getEmojiSuggestionsPosition
         // imagePath: 'https://ssl.gstatic.com/mail/emoji/v7/png48/emoji_u',
         // imageType: 'png'
@@ -32,16 +71,49 @@ export function EmojiFeature() {
     }),
     []
   )
-  const { EmojiSuggestions, EmojiSelect } = emojiPlugin
+  const { EmojiSuggestions } = emojiPlugin
 
   return (
     <>
       <ToolbarFragment group="emoji">
-        <Tooltip title="Emoji (:)">
-          <span>
-            <EmojiSelect />
-          </span>
-        </Tooltip>
+        <BaseDropdown
+          renderDropdownButton={({ isActive, onClick, ref }) => (
+            <Tooltip title="Emoji (:)">
+              <ToolbarIconButton
+                color={isActive ? 'primary' : undefined}
+                ref={ref}
+                onClick={onClick}
+              >
+                <IconEmoji />
+              </ToolbarIconButton>
+            </Tooltip>
+          )}
+          PopperProps={{ placement: 'bottom' }}
+          renderMenu={({ close }) => (
+            <div
+              onClick={e => {
+                if (
+                  closeOnSelection &&
+                  (e.target as HTMLElement).closest('.emoji-group')
+                ) {
+                  close()
+                }
+              }}
+            >
+              <EmojiSelectPopover
+                cacheBustParam={cacheBustParam}
+                imagePath={imagePath}
+                imageType={imageType}
+                theme={theme}
+                store={storeRef.current}
+                groups={defaultEmojiGroups}
+                emojis={emojis}
+                toneSelectOpenDelay={toneSelectOpenDelay}
+                isOpen
+              />
+            </div>
+          )}
+        />
       </ToolbarFragment>
       <EmojiSuggestions />
     </>
