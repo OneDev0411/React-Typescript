5587204615cc151a326180ca711cbfbbfbff85a3
OneDev0411 <cryptoapple1109@gmail.com>
2019-12-13 20:30:23 +0330

    wip_fix(mc.rte): fix emoji plugin toolbar suggestion menu issues (#3660)

--- app/views/components/InstantMarketing/Builder/RichTextEditor/McTextEditor/index.tsx
+++ app/views/components/InstantMarketing/Builder/RichTextEditor/McTextEditor/index.tsx
@@ -1,6 +1,6 @@
 import { createStyles, makeStyles, Theme } from '@material-ui/core'
 
-import React, { CSSProperties, forwardRef } from 'react'
+import React, { CSSProperties, forwardRef, useEffect } from 'react'
 
 import { TextEditor } from 'components/TextEditor'
 import { TextEditorProps } from 'components/TextEditor/types'
@@ -9,6 +9,7 @@ import { nativelyStopEventPropagationOfEventViaRef } from 'utils/natively-stop-e
 
 import { TemplateExpressionsFeature } from '../../../../TextEditor/features/TemplateExpressions'
 import { RichTextFeature } from '../../../../TextEditor/features/RichText'
+import { EmojiFeature } from '../../../../TextEditor/features/Emoji'
 
 const useStyles = makeStyles(
   (theme: Theme) =>
@@ -46,11 +47,7 @@ const useStyles = makeStyles(
         marginTop: '-41px !important',
         pointerEvents: 'initial',
         background: '#fff',
-        boxShadow: theme.shadows[5],
-        // This is a super hacky fragile temporary solution until #3660 is fixed
-        '& span[title="Emoji (:)"], & span[title="Emoji (:)"] + span': {
-          display: 'none'
-        }
+        boxShadow: theme.shadows[5]
       }
     }),
   { name: 'McTextEditor' }
@@ -67,6 +64,8 @@ export const McTextEditor = forwardRef(function McTextEditor(
 ) {
   const classes = useStyles({ targetStyle })
 
+  useOverlayCloseBugFix()
+
   return (
     <TextEditor
       ref={ref}
@@ -78,9 +77,45 @@ export const McTextEditor = forwardRef(function McTextEditor(
       {...textEditorProps}
     >
       <RichTextFeature lists={false} textSize={false} />
+      <EmojiFeature />
       <TemplateExpressionsFeature
         templateVariableSuggestionGroups={defaultTemplateVariableSuggestions}
       />
     </TextEditor>
   )
 })
+
+/**
+ * ## Problem:
+ *
+ * In grape.js, a mousedown handler is set on document which closes RTE.
+ * here is the code: https://github.com/artf/grapesjs/blob/84eecaedc5f0693e4e43db7f523913c79443898a/src/dom_components/view/ComponentTextView.js#L178
+ * This causes problems for overlays opened by different features in the editor.
+ * In fact, clicking anything in a portal outside the editor's dom node will
+ * close the editor because of this listener.
+ *
+ * ## Solution:
+ * This hook sets up a listener on document and calls stopImmediatePropagation()
+ * on mousedown events. At first the idea was to call stopImmediatePropagation
+ * only if the event target is within a popover (we can detect mui overlays
+ * easily). But it seems unconditional call to stopImmediatePropagation doesn't
+ * break anything and works fine. We can add that condition in future if we
+ * found any issue with calling stopImmediatePropagation unconditionally.
+ *
+ * Note that we need to call `stopImmediatePropagation` instead of
+ * `stopPropagation` as we want to cancel the other event listener on
+ * document.
+ */
+function useOverlayCloseBugFix() {
+  useEffect(() => {
+    const handler = e => {
+      e.stopImmediatePropagation()
+    }
+
+    document.addEventListener('mousedown', handler)
+
+    return () => {
+      document.removeEventListener('mousedown', handler)
+    }
+  }, [])
+}
