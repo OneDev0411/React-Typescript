5f309c5806d275ca4d71885cbe6237784f74ecd9
OneDev0411 <cryptoapple1109@gmail.com>
2019-12-28 00:44:30 +0330

    wip_feat(contact): init merge contact callout component (#2988)

---
+++ app/components/Pages/Dashboard/Contacts/Profile/MergeDuplicates/index.tsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react'
+import { Button } from '@material-ui/core'
+
+import { getContactDuplicateContacts } from 'models/contacts/get-contact-duplicate-contacts'
+
+import { Callout } from 'components/Callout'
+
+import { CallOutContentContainer } from './styled'
+
+interface Props {
+  contact: IContact
+}
+
+export default function MergeDuplicates({ contact }: Props) {
+  const [isOpen, setIsOpen] = useState(true)
+  const [duplicateContacts, setDuplicateContacts] = useState<IContact[]>([])
+
+  useEffect(() => {
+    async function fetchDuplicates() {
+      const duplicates = await getContactDuplicateContacts(contact.id)
+
+      setDuplicateContacts(duplicates.contacts)
+    }
+
+    fetchDuplicates()
+  })
+
+  if (duplicateContacts.length === 0 || !isOpen) {
+    return null
+  }
+
+  const handleReviewClick = () => {
+    console.log('Review Clicked')
+  }
+
+  return (
+    <Callout
+      dense
+      type="warn"
+      closeButtonTooltip="Dismiss"
+      style={{ padding: '0.5rem 1rem', margin: '1rem' }}
+      onClose={() => setIsOpen(false)}
+    >
+      <CallOutContentContainer>
+        <span>
+          We’ve found {duplicateContacts.length} other contacts similar to{' '}
+          {contact.display_name}, Do you want to merge them?
+        </span>
+        <Button color="primary" variant="text" onClick={handleReviewClick}>
+          Review
+        </Button>
+      </CallOutContentContainer>
+    </Callout>
+  )
+}
---
+++ app/models/contacts/merge-contacts-batch/index.ts
@@ -0,0 +1,22 @@
+import Fetch from '../../../services/fetch'
+
+interface ContactsMergeCluster {
+  parent: UUID
+  sub_contacts: UUID[]
+}
+
+export async function mergeContactsBatch({
+  clusters
+}: {
+  clusters: ContactsMergeCluster[]
+}): Promise<unknown> {
+  try {
+    const response = await new Fetch().post('/contacts/merge').send({
+      clusters
+    })
+
+    return response.body.data
+  } catch (error) {
+    throw error
+  }
+}
