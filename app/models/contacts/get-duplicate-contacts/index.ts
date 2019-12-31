84df9e0a50af1e296dd848087210ccf2c0c6d920
OneDev0411 <cryptoapple1109@gmail.com>
2019-12-31 16:57:38 +0330

    chore(models): remove commented stuff (#2988)

--- app/models/contacts/get-duplicate-contacts/index.ts
+++ app/models/contacts/get-duplicate-contacts/index.ts
@@ -1,11 +1,9 @@
 import Fetch from '../../../services/fetch'
-// import { defaultQuery } from '../helpers/default-query'
 import { DuplicateContacts } from '../get-contact-duplicate-contacts/index'
 
 export async function getDuplicateContacts(): Promise<DuplicateContacts[]> {
   try {
     const response = await new Fetch().get('/contacts/duplicates')
-    // .query(defaultQuery)
 
     return response.body.data
   } catch (error) {
