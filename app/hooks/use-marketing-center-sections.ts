7f2a529d3c806a3ac12504da38058849c343eb55
OneDev0411 <cryptoapple1109@gmail.com>
2020-01-09 01:38:03 +0330

    feat(mc): added blank layouts to menu (#3820)
    (cherry picked from commit 45252531cc871900761c39a02be5f97a30ddb49a)

--- app/hooks/use-marketing-center-sections.ts
+++ app/hooks/use-marketing-center-sections.ts
@@ -35,6 +35,12 @@ const ALL_SECTIONS: Section[] = [
     title: 'Life',
     items: [
       {
+        title: '‌Blank Layouts',
+        value: 'Layout',
+        link: urlGenerator('/Layout'),
+        access: ['BackOffice']
+      },
+      {
         title: 'Newsletters',
         value: 'Newsletter',
         link: urlGenerator('/Newsletter')
@@ -79,6 +85,12 @@ const ALL_SECTIONS: Section[] = [
     type: SectionsEnum.LINK,
     title: 'Properties',
     items: [
+      {
+        title: '‌Blank Layouts',
+        value: 'ListingLayout',
+        link: urlGenerator('/ListingLayout'),
+        access: ['BackOffice']
+      },
       {
         title: 'As Seen In',
         value: 'AsSeenIn',
