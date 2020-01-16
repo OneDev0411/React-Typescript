ffcaa1dea0a90ba74255fd6b4040ef4296bdf2d1
OneDev0411 <cryptoapple1109@gmail.com>
2020-01-16 11:13:35 +0330

    chore(models/user): added user lookup (#3752)

---
+++ app/models/user/lookup-user-by-email/index.ts
@@ -0,0 +1,25 @@
+import Fetch from '../../../services/fetch'
+
+interface UserLookup {
+  password: boolean
+  is_shadow: boolean
+  sso_url?: string
+}
+
+export async function lookupUserByEmail(email: string): Promise<UserLookup> {
+  const response = await new Fetch({ proxy: true })
+    .post('/users/lookup')
+    .set({ 'x-auth-mode': 'client_id' })
+    .send({ email })
+
+  const {
+    data,
+    info: { password, is_shadow }
+  } = response.body
+
+  if (Array.isArray(data) && data.length === 1) {
+    return { sso_url: data[0].url, password, is_shadow }
+  }
+
+  return { password, is_shadow }
+}
