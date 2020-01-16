fc47fadee8b63df7b7efa5dc17cf3905af0996c8
OneDev0411 <cryptoapple1109@gmail.com>
2020-01-16 11:12:49 +0330

    chore(services): updated fetch options type (#3752)

--- app/services/fetch/types.ts
+++ app/services/fetch/types.ts
@@ -1,9 +1,9 @@
 type Progress = () => void
 
 export interface IOptions {
-  proxy: boolean
-  useReferencedFormat: boolean
-  progress: null | Progress
+  proxy?: boolean
+  useReferencedFormat?: boolean
+  progress?: null | Progress
 }
 
 export interface IMiddleware {
