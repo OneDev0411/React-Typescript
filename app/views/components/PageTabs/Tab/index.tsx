5600bb9a386a31b7bfa5e8dc7ed0cd7ce2c77111
OneDev0411 <cryptoapple1109@gmail.com>
2020-01-24 22:37:28 +0300

    chore(page tabs): fix theme spacing (#3787)

--- app/views/components/PageTabs/Tab/index.tsx
+++ app/views/components/PageTabs/Tab/index.tsx
@@ -3,8 +3,8 @@ import { Tab as BaseTab, withStyles } from '@material-ui/core'
 
 export const Tab = withStyles(theme => ({
   root: {
-    minWidth: '4rem',
-    fontSize: theme.spacing(2),
+    minWidth: theme.spacing(8),
+    fontSize: theme.typography.subtitle1.fontSize,
     '&$selected': {
       fontWeight: 'bold'
     }
