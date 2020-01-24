ae3c43eed3284c22bbf718a3499eecdf0955ba9d
OneDev0411 <cryptoapple1109@gmail.com>
2020-01-24 19:32:24 +0330

    feat(components): implement global header component

---
+++ app/views/components/GlobalHeader/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Typography, makeStyles, Theme } from '@material-ui/core'
+
+import GlobalActionsButton from 'components/GlobalActionsButton'
+
+const useStyles = makeStyles((theme: Theme) => ({
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: theme.spacing(3),
+    width: '100%'
+  },
+  content: {
+    flexGrow: 1
+  }
+}))
+
+interface Props {
+  title?: string
+  noGlobalActionsButton?: boolean
+  children: React.ReactNode
+}
+
+export default function GlobalHeader({
+  title,
+  noGlobalActionsButton,
+  children
+}: Props) {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.wrapper}>
+      {title && (
+        <div>
+          <Typography variant="h3">{title}</Typography>
+        </div>
+      )}
+      <div className={classes.content}>{children}</div>
+      {!noGlobalActionsButton && (
+        <div>
+          <GlobalActionsButton />
+        </div>
+      )}
+    </div>
+  )
+}
---
+++ app/views/components/GlobalHeaderWithSearch/index.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { throttle } from 'lodash'
+import { TextField, makeStyles, Theme } from '@material-ui/core'
+
+import GlobalHeader from 'components/GlobalHeader'
+
+const useStyles = makeStyles((theme: Theme) => ({
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+}))
+
+interface Props {
+  placeholder: string
+  onSearch: (query: string) => void
+}
+
+export default function GlobalHeaderWithSearch({
+  placeholder,
+  onSearch,
+  children,
+  ...globalHeaderProps
+}: Props & React.ComponentProps<typeof GlobalHeader>) {
+  const classes = useStyles()
+  const [searchQueryValue, setSearchQueryValue] = useState('')
+  const throttledSearchHandler = useRef(
+    throttle((value: string) => onSearch(value), 1000)
+  )
+
+  useEffect(() => {
+    throttledSearchHandler.current(searchQueryValue)
+  }, [searchQueryValue])
+
+  const handleQueryChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQueryValue(ev.currentTarget.value)
+  }
+
+  return (
+    <GlobalHeader {...globalHeaderProps}>
+      <div className={classes.wrapper}>
+        {children}
+        <TextField
+          value={searchQueryValue}
+          size="small"
+          variant="outlined"
+          placeholder={placeholder}
+          onChange={handleQueryChange}
+        />
+      </div>
+    </GlobalHeader>
+  )
+}
