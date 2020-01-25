079be5613a81e8c5343e0eb496a868da0492d142
OneDev0411 <cryptoapple1109@gmail.com>
2020-01-25 17:45:23 +0330

    chore(dc): make more readable for emtpy state component (#3874)

--- app/components/Pages/Dashboard/Contexts/components/EmptyState/index.tsx
+++ app/components/Pages/Dashboard/Contexts/components/EmptyState/index.tsx
@@ -1,33 +1,32 @@
 import React from 'react'
 import { Box, Button } from '@material-ui/core'
-import { makeStyles, createStyles } from '@material-ui/styles'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     box: {
       alignItems: 'center',
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'center',
-      margin: '20vh auto 0',
       maxWidth: '33rem',
       textAlign: 'center'
     }
   })
 )
 
-export default function EmptyState(props: { onOpenNewContext: () => void }) {
+interface Props {
+  onOpenNewContext: () => void
+}
+
+export default function EmptyState({ onOpenNewContext }: Props) {
   const classes = useStyles()
 
   return (
-    <Box className={classes.box}>
+    <Box mt={16} mx="auto" className={classes.box}>
       <img src="/static/images/contacts/zero-state.svg" alt="Houston" />
       <h2>No deal context… Yet!</h2>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={props.onOpenNewContext}
-      >
+      <Button variant="contained" color="primary" onClick={onOpenNewContext}>
         Create New Context
       </Button>
     </Box>
