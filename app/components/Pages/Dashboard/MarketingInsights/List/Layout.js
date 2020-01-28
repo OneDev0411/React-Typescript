49153396ce52b1a5c10dff4af6ba33d2a7270323
OneDev0411 <cryptoapple1109@gmail.com>
2020-01-28 19:18:51 +0330

    chore(email-insight): update header and action  btn base on new design (#3893)

--- app/components/Pages/Dashboard/MarketingInsights/List/Layout.js
+++ app/components/Pages/Dashboard/MarketingInsights/List/Layout.js
@@ -1,47 +1,42 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import { Link } from 'react-router'
 
-import {
-  Container as PageContainer,
-  Content as PageContent
-} from 'components/SlideMenu'
-import PageSideNav from 'components/PageSideNav'
-import { SectionsEnum } from 'components/PageSideNav/types'
+import Header from 'components/GlobalHeader'
+import { PageTabs, Tab } from 'components/PageTabs'
 
 const urlGenerator = url => `/dashboard/insights${url}`
 
-function InsightsLayout(props) {
-  const { isSideMenuOpen } = props
-  const sections = [
+function InsightsLayout({ sentCount, scheduledCount, children }) {
+  const currentUrl = window.location.pathname
+  const Items = [
     {
-      type: SectionsEnum.LINK,
-      title: 'Email Insight',
-      items: [
-        {
-          title: 'Sent',
-          isIndex: true,
-          link: urlGenerator('/'),
-          badge: props.sentCount
-        },
-        {
-          title: 'Scheduled',
-          link: urlGenerator('/scheduled'),
-          badge: props.scheduledCount
-        }
-      ]
+      label: `Sent (${sentCount})`,
+      to: urlGenerator('/')
+    },
+    {
+      label: `Scheduled (${scheduledCount})`,
+      to: urlGenerator('/scheduled')
     }
   ]
 
   return (
-    <PageContainer isOpen={isSideMenuOpen}>
+    <React.Fragment>
       <Helmet>
         <title>Insights | Rechat</title>
       </Helmet>
-      <PageSideNav isOpen={isSideMenuOpen} sections={sections} />
-      <PageContent isSideMenuOpen={isSideMenuOpen}>
-        {props.children}
-      </PageContent>
-    </PageContainer>
+      <Header title="Email Insight" />
+      <PageTabs
+        defaultValue={currentUrl}
+        tabs={Items.map(({ label, to }, i) => {
+          return (
+            <Tab key={i} component={Link} label={label} to={to} value={to} />
+          )
+        })}
+      />
+
+      <div>{children}</div>
+    </React.Fragment>
   )
 }
 
