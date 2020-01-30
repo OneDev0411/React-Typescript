015dda43554a7548e1f99b1c4cad2c38ba147830
OneDev0411 <cryptoapple1109@gmail.com>
2020-01-30 13:31:17 +0330

    fix(page tab): fix apply tablink style (#3884)

--- app/views/components/PageTabs/Tab/index.tsx
+++ app/views/components/PageTabs/Tab/index.tsx
@@ -1,25 +1,12 @@
 import React from 'react'
-import {
-  Tab as BaseTab,
-  createStyles,
-  makeStyles,
-  Theme
-} from '@material-ui/core'
+import { Tab as BaseTab, withStyles } from '@material-ui/core'
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      minWidth: theme.spacing(8),
-      fontSize: theme.typography.subtitle1.fontSize,
-      '&$selected': {
-        fontWeight: 'bold'
-      }
+export const Tab = withStyles(theme => ({
+  root: {
+    minWidth: theme.spacing(8),
+    fontSize: theme.typography.subtitle1.fontSize,
+    '&$selected': {
+      fontWeight: 'bold'
     }
-  })
-)
-
-export const Tab = props => {
-  const classes = useStyles()
-
-  return <BaseTab {...props} className={classes.root} />
-}
+  }
+}))(props => <BaseTab {...props} />)
