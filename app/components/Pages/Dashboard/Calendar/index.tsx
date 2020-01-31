a9f239a52b81a326322a6a14099584d467c16384
OneDev0411 <cryptoapple1109@gmail.com>
2020-01-31 20:22:39 +0300

    chore(calendar): add basic layout

--- app/components/Pages/Dashboard/Calendar/index.tsx
+++ app/components/Pages/Dashboard/Calendar/index.tsx
@@ -1,7 +1,158 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+// import debounce from 'lodash/debounce'
+// import fecha from 'fecha'
+import isEqual from 'lodash/isEqual'
+
+// import List from 'components/Calendar'
+import { CalendarRef } from 'components/Calendar/types'
+
+import GlobalHeader from 'components/GlobalHeader'
+// import DatePicker from 'components/DatePicker'
+
+// import { TodayButton } from './components/TodayButton'
+// import Filters, { defaultFilter, FiltersRef } from './components/Filters'
+// import { Export } from './components/Export'
+// import CreateEvent from './components/CreateEvent'
+
+// import {
+//   Container,
+//   ListContainer,
+//   Sidebar,
+//   Header,
+//   Main,
+//   SideHeader,
+//   Title
+// } from './styled'
 
 const CalendarPage: React.FC = props => {
-  return null
+  const calendarRef = useRef<CalendarRef>(null)
+  const filtersRef = useRef<FiltersRef>(null)
+
+  /*
+   * current active date of Calendar. the first visible day of day in
+   * list will be active date
+   */
+  const [activeDate, setActiveDate] = useState<Date>(new Date())
+
+  /**
+   * keeps list of fetched days
+   */
+  const [fetchedDays, setFetchedDays] = useState<{ [key: string]: number }>({})
+
+  /**
+   * current filters
+   */
+  const [filter, setFilter] = useState<object>(defaultFilter)
+
+  /**
+   * triggers when user clicks on a date in datepicker of left side
+   * @param date
+   */
+  // const handleDatePickerChange = (date: Date = new Date()) => {
+  //   if (calendarRef.current) {
+  //     setActiveDate(date)
+
+  //     calendarRef.current.jumpToDate(date)
+  //   }
+  // }
+
+  /**
+   * triggers when user creates a new event
+   * @param event
+   * @param type
+   */
+  const handleEventChange = (event: IEvent, type: string): void => {
+    // set filters to All Events when creating a new event
+    if (type === 'created') {
+      filtersRef.current!.changeFilter(0)
+    }
+
+    calendarRef.current!.updateCrmEvents(event, type)
+  }
+
+  /**
+   * triggers when new data is fetched by calendar
+   * @param events
+   */
+  const handleOnLoadEvents = (events: ICalendarEventsList) => {
+    setFetchedDays(getDaysWithEvent(events))
+  }
+
+  /**
+   * triggers when user changes the calendar filter
+   * @param filter
+   */
+  const handleChangeFilter = (selectedFilter: object): void => {
+    if (isEqual(filter, selectedFilter)) {
+      return
+    }
+
+    setFilter(selectedFilter)
+  }
+
+  return (
+    <Container>
+      <GlobalHeader />
+      {/* <Sidebar>
+        <div>
+          <SideHeader>
+            <Title>Calendar</Title>
+
+            <TodayButton onClick={() => handleDatePickerChange()} />
+          </SideHeader>
+
+          <DatePicker
+            modifiers={{ empty: getEmptyDays }}
+            selectedDate={activeDate}
+            onChange={handleDatePickerChange}
+          />
+        </div>
+
+        <Export />
+      </Sidebar> */}
+      {/* <Header>
+        <Filters ref={filtersRef} onChange={handleChangeFilter} />
+        <CreateEvent onEventChange={handleEventChange} />
+      </Header>
+
+      <ListContainer className="u-scrollbar--thinner">
+        <List
+          ref={calendarRef}
+          filter={filter}
+          onChangeActiveDate={debounce(setActiveDate, 100)}
+          onLoadEvents={handleOnLoadEvents}
+        />
+      </ListContainer> */}
+    </Container>
+  )
+}
+
+/**
+ * returns flatted list of days that have at least one event
+ * @param events
+ */
+function getDaysWithEvent(events: ICalendarEventsList) {
+  return Object.values(events).reduce((acc, daysOfMonth) => {
+    // finds list of days with more than one event
+    const filterByDays = Object.entries(daysOfMonth).reduce(
+      (acc, [dayId, dayEvents]) => {
+        if (dayEvents.length === 0) {
+          return acc
+        }
+
+        return {
+          ...acc,
+          [dayId]: dayEvents.length
+        }
+      },
+      {}
+    )
+
+    return {
+      ...acc,
+      ...filterByDays
+    }
+  }, {})
 }
 
 export default CalendarPage
