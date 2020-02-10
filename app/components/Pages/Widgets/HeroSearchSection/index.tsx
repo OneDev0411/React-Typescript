81664d5c2be67ad2652ab6412050558821c82cc2
OneDev0411 <cryptoapple1109@gmail.com>
2020-02-10 21:12:54 +0330

    fix(widget): responsiving hero-widget (#3804)

--- app/components/Pages/Widgets/HeroSearchSection/index.tsx
+++ app/components/Pages/Widgets/HeroSearchSection/index.tsx
@@ -1,21 +1,50 @@
 import React from 'react'
 import { createStyles, makeStyles } from '@material-ui/styles'
 
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+
 import Brand from '../../../../controllers/Brand'
 import SearchIcon from '../../../../views/components/SvgIcons/Search/IconSearch'
 
 import MlsSearchAutocomplete from '../MlsSearchAutocomplete'
 
+interface StylesProps {
+  isTablet: boolean
+  isDesktop: boolean
+  brandSearchBackgroundImage: string
+}
+
 const useStyles = makeStyles(
   () =>
     createStyles({
-      backgroundCover: ({
-        brandSearchBackgroundImage
-      }: {
-        brandSearchBackgroundImage: string
-      }) => ({
+      searchBox__input: (props: StylesProps) => {
+        const initialStyles = {
+          width: 'calc(100% - 2rem)',
+          height: '100%',
+          padding: '0.5rem 0.7rem',
+          fontSize: '0.8rem',
+          border: 'none',
+          borderRadius: '4px',
+
+          '&:focus': {
+            outline: 'none'
+          }
+        }
+
+        if (props.isDesktop) {
+          return {
+            ...initialStyles,
+            width: 'calc(100% - 2.5rem)',
+            padding: '0.5rem 1rem',
+            fontSize: '1.1rem'
+          }
+        }
+
+        return initialStyles
+      },
+      backgroundCover: (props: StylesProps) => ({
         backgroundColor: '#cecece',
-        backgroundImage: `url(${brandSearchBackgroundImage})`,
+        backgroundImage: `url(${props.brandSearchBackgroundImage})`,
         backgroundPosition: 'center center',
         backgroundSize: 'cover',
         position: 'absolute',
@@ -34,18 +63,52 @@ const useStyles = makeStyles(
       title: {
         color: '#fff'
       },
-      wrapper: {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        textAlign: 'center',
-        width: '550px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        zIndex: 2
+      wrapper: (props: StylesProps) => {
+        if (props.isDesktop) {
+          return {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            textAlign: 'center',
+            width: '550px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            zIndex: 2
+          }
+        }
+
+        if (props.isTablet) {
+          return {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            textAlign: 'center',
+            width: '300px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            zIndex: 2
+          }
+        }
+
+        return {
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          textAlign: 'center',
+          width: '230px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          zIndex: 2
+        }
       },
       searchBox: {
         width: '100%',
@@ -60,18 +123,6 @@ const useStyles = makeStyles(
         width: '1.5rem !important',
         height: '1.5rem !important',
         opacity: 0.3
-      },
-      searchBox__input: {
-        width: 'calc(100% - 2.5rem)',
-        height: '100%',
-        padding: '0.5rem 1rem',
-        fontSize: '1.1rem',
-        border: 'none',
-        borderRadius: '4px',
-
-        '&:focus': {
-          outline: 'none'
-        }
       }
     }),
   { name: 'HeroSearchSection' }
@@ -84,7 +135,9 @@ interface Props {
 export default function HeroSearchSection({ brand }: Props) {
   const brandSearchBackgroundImage = Brand.asset('search_bg', '', brand)
   const brandSearchHeadline = Brand.message('search_headline', '', brand)
-  const classes = useStyles({ brandSearchBackgroundImage })
+  const isDesktop = useMediaQuery('(min-width:768px)')
+  const isTablet = useMediaQuery('(min-width:480px)')
+  const classes = useStyles({ brandSearchBackgroundImage, isDesktop, isTablet })
 
   return (
     <>
