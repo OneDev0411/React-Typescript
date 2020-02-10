04b736d5f2f5e12c1d5dc399a9e08a2eb8efc555
OneDev0411 <cryptoapple1109@gmail.com>
2020-02-10 20:37:00 +0330

    fix(inbox): filter out unrelated email thread events (#3811)

--- app/components/Pages/Dashboard/Inbox/helpers/use-email-thread-events.ts
+++ app/components/Pages/Dashboard/Inbox/helpers/use-email-thread-events.ts
@@ -1,18 +1,58 @@
 import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+
+import { IAppState } from 'reducers'
+import { selectAllConnectedAccounts } from 'reducers/contacts/oAuthAccounts'
+
+interface EventBase {
+  threads: UUID[]
+}
+interface GoogleEvent extends EventBase {
+  google_credential: UUID
+}
+interface MicrosoftEvent extends EventBase {
+  microsoft_credential: UUID
+}
+type Event = GoogleEvent | MicrosoftEvent
 
 export default function useEmailThreadEvents(
   handleUpdateEmailThreads: (updatedEmailThreadIds: UUID[]) => void,
   handleDeleteEmailThreads: (deletedEmailThreadIds: UUID[]) => void
 ) {
+  const oAuthAccounts = useSelector(
+    (state: IAppState) => state.contacts.oAuthAccounts
+  )
+
+  const allAccounts = selectAllConnectedAccounts(oAuthAccounts)
+
   useEffect(() => {
     const socket: SocketIOClient.Socket = (window as any).socket
 
-    socket.on('email_thread:update', handleUpdateEmailThreads)
-    socket.on('email_thread:delete', handleDeleteEmailThreads)
+    function filterEvent(event: Event): boolean {
+      if ('google_credential' in event) {
+        return allAccounts.some(({ id }) => id === event.google_credential)
+      }
+
+      if ('microsoft_credential' in event) {
+        return allAccounts.some(({ id }) => id === event.microsoft_credential)
+      }
+
+      return false
+    }
+
+    function handleFilteredUpdateEmailThreads(event: Event) {
+      filterEvent(event) && handleUpdateEmailThreads(event.threads)
+    }
+    function handleFilteredDeleteEmailThreads(event: Event) {
+      filterEvent(event) && handleDeleteEmailThreads(event.threads)
+    }
+
+    socket.on('email_thread:update', handleFilteredUpdateEmailThreads)
+    socket.on('email_thread:delete', handleFilteredDeleteEmailThreads)
 
     return () => {
-      socket.off('email_thread:update', handleUpdateEmailThreads)
-      socket.off('email_thread:delete', handleDeleteEmailThreads)
+      socket.off('email_thread:update', handleFilteredUpdateEmailThreads)
+      socket.off('email_thread:delete', handleFilteredDeleteEmailThreads)
     }
-  }, [handleDeleteEmailThreads, handleUpdateEmailThreads])
+  }, [allAccounts, handleDeleteEmailThreads, handleUpdateEmailThreads])
 }
