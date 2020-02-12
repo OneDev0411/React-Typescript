677d637d42a170c0ccad70d8c6b69ce1ca765bf5
OneDev0411 <cryptoapple1109@gmail.com>
2020-02-12 09:30:51 +0330

    style(inbox): fix inbox icon color (#3811)

--- app/components/Pages/Dashboard/Inbox/components/InboxEmailThread/InboxEmailThread.tsx
+++ app/components/Pages/Dashboard/Inbox/components/InboxEmailThread/InboxEmailThread.tsx
@@ -39,7 +39,7 @@ export default function InboxEmailThread({ emailThreadId, onClose }: Props) {
     [user, emailThread]
   )
 
-  console.log(contactInfo) // TODO: Remove this line.
+  contactInfo && console.log('contactInfo', contactInfo) // TODO: Remove this line.
 
   const fetchEmailThread = useCallback(async () => {
     if (emailThreadId) {
