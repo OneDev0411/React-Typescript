6546208af775fdb66b650c55910e0fc36748b649
OneDev0411 <cryptoapple1109@gmail.com>
2020-02-25 23:30:45 +0330

    fix(blocks): wip fix on agents, articles and texts (#4011)

--- app/views/components/InstantMarketing/Builder/Blocks/adapt.ts
+++ app/views/components/InstantMarketing/Builder/Blocks/adapt.ts
@@ -2,11 +2,11 @@ function adapt(parent: any, template: string): string {
   const type = parent.get('type')
 
   if (type === 'mj-wrapper') {
-    return `<mj-section><mj-column width="100%">${template}</mj-column></mj-section>`
+    return `<mj-section padding="5px 20px"><mj-column width="100%">${template}</mj-column></mj-section>`
   }
 
   if (type === 'mj-body') {
-    return `<mj-section background-color="#fff"><mj-column width="100%">${template}</mj-column></mj-section>`
+    return `<mj-section padding="5px 20px" background-color="#fff"><mj-column width="100%">${template}</mj-column></mj-section>`
   }
 
   return template
