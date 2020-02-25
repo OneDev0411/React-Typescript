630d5bbdc7e9b62fb60afaa1677126de336665df
OneDev0411 <cryptoapple1109@gmail.com>
2020-02-26 01:05:08 +0330

    fix(blocks): do not add redundant section for sectioned blocks (#4011)

--- app/views/components/InstantMarketing/Builder/Blocks/adapt.ts
+++ app/views/components/InstantMarketing/Builder/Blocks/adapt.ts
@@ -1,6 +1,10 @@
 function adapt(parent: any, template: string): string {
   const type = parent.get('type')
 
+  if (template.startsWith('<mj-section')) {
+    return template
+  }
+
   if (type === 'mj-wrapper') {
     return `<mj-section padding="5px 20px"><mj-column width="100%">${template}</mj-column></mj-section>`
   }
