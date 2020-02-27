71b00daa836e5fcba2a1d34c1d1431c6ebcb89b2
OneDev0411 <cryptoapple1109@gmail.com>
2020-02-27 16:04:41 +0330

    fix(blocks): audit all blocks round 2 (#4011)
    left align blocks, add CTAs for listing blocks, use brand colors, fix spacings

--- app/views/components/InstantMarketing/Builder/Blocks/adapt.ts
+++ app/views/components/InstantMarketing/Builder/Blocks/adapt.ts
@@ -6,11 +6,11 @@ function adapt(parent: any, template: string): string {
   }
 
   if (type === 'mj-wrapper') {
-    return `<mj-section padding="5px 20px"><mj-column width="100%">${template}</mj-column></mj-section>`
+    return `<mj-section padding="20px 20px"><mj-column width="100%">${template}</mj-column></mj-section>`
   }
 
   if (type === 'mj-body') {
-    return `<mj-section padding="5px 20px" background-color="#fff"><mj-column width="100%">${template}</mj-column></mj-section>`
+    return `<mj-section padding="20px 20px" background-color="#fff"><mj-column width="100%">${template}</mj-column></mj-section>`
   }
 
   return template
