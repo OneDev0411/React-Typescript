83ea28392489396e3e9a3e2e1511f2724f8937f6
OneDev0411 <cryptoapple1109@gmail.com>
2020-02-28 21:53:46 +0330

    chore(mls): Improved tabs components to make use of "WithRouter" for location paths.

--- app/components/Pages/Dashboard/Listings/components/Tabs/index.tsx
+++ app/components/Pages/Dashboard/Listings/components/Tabs/index.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from 'react'
+import { withRouter, WithRouterProps } from 'react-router'
 import { Box } from '@material-ui/core'
 import { Theme, ButtonBase, makeStyles, createStyles } from '@material-ui/core'
 
@@ -71,9 +72,11 @@ export const Tabs = ({
   activeSort,
   isFetching,
   saveSearchHandler,
-  showSavedSearchButton
-}: Props) => {
-  const currentUrl = window.location.pathname
+  showSavedSearchButton,
+  location
+}: Props & WithRouterProps) => {
+  // const currentUrl = location.pathname
+  const currentUrl = '/dashboard/mls'
   const classes = useStyle()
   let availableTabs: React.ReactNode[]
 
@@ -133,4 +136,4 @@ export const Tabs = ({
   )
 }
 
-export default Tabs
+export default withRouter(Tabs)
