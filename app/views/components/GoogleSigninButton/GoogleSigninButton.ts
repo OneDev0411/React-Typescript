cb49f58fbc4ef8d19057f747d015f49c8679586e
OneDev0411 <cryptoapple1109@gmail.com>
2020-03-11 20:16:41 +0330

    fix(crm): google btn background color (#4053)

--- app/views/components/GoogleSigninButton/GoogleSigninButton.ts
+++ app/views/components/GoogleSigninButton/GoogleSigninButton.ts
@@ -10,6 +10,7 @@ export const GoogleSigninButton = styled.button<Props>`
   padding: 0;
   border: none;
   text-indent: -10000px;
+  background-color: transparent;
   background-image: ${props =>
     `url(${bgBaseUrl}${props.disabled ? 'disabled' : 'normal'}@2x.png)`};
   background-size: contain;
