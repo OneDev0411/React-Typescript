3695db117f6bb76a6442f0cabdc819775dc408bf
OneDev0411 <cryptoapple1109@gmail.com>
2020-03-12 12:01:46 +0330

    fix(oh): Fixed grid to check if there's listing object in data before extracting it's image. If not, showing a default image.

--- app/components/Pages/Dashboard/OpenHouse/List/columns/Avatar.tsx
+++ app/components/Pages/Dashboard/OpenHouse/List/columns/Avatar.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Avatar } from '@material-ui/core'
 
+import IconHome from 'components/SvgIcons/Home/HomeIcon'
+
 interface Props {
   listings: ICRMTaskAssociation<CRMTaskAssociationType>[]
 }
 
 export default function Photo({ listings }: Props) {
+  if (!listings[0]) {
+    return (
+      <Avatar>
+        <IconHome />
+      </Avatar>
+    )
+  }
+
   const avatar = listings[0].listing.gallery_image_urls[0]
 
   return <Avatar alt="" src={avatar} />
