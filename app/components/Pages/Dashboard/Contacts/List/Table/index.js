fc221daac63927edb180bf386da14409c5622410
OneDev0411 <cryptoapple1109@gmail.com>
2020-03-13 14:30:14 +0330

    chore(contact): remove redundant async (#3912)

--- app/components/Pages/Dashboard/Contacts/List/Table/index.js
+++ app/components/Pages/Dashboard/Contacts/List/Table/index.js
@@ -81,9 +81,7 @@ const ContactsList = props => {
       render: ({ row: contact }) => (
         <FlowCell
           contactId={contact.id}
-          callback={async () => {
-            await props.reloadContacts()
-          }}
+          callback={() => props.reloadContacts()}
           flowsCount={Array.isArray(contact.flows) ? contact.flows.length : 0}
         />
       )
