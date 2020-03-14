6e723a21f174b00a241cec41293233ddd6baa0cb
OneDev0411 <cryptoapple1109@gmail.com>
2020-03-14 21:56:36 +0100

    test(email-threads): add some failing test for EmailThreadItem (#3634)
    More specifically, tests are for collapsing replied content

---
+++ app/views/components/EmailThread/components/EmailThreadItem.test.tsx
@@ -0,0 +1,230 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+
+import gmailThreadJson from 'fixtures/email-thread/gmail-thread-1.json'
+import outlookThreadJson from 'fixtures/email-thread/outlook-thread-1.json'
+import forwardedEmailThreadJson from 'fixtures/email-thread/forwarded-email-thread.json'
+
+import { EmailThreadItem } from './EmailThreadItem'
+import { normalizeThreadMessageToThreadEmail } from '../helpers/normalize-to-email-thread-email'
+
+const gmailThread: IEmailThread<'messages'> = {
+  ...gmailThreadJson,
+  type: 'email_thread',
+  messages: gmailThreadJson.messages as IEmailThreadMessage[]
+}
+
+const outlookThread: IEmailThread<'messages'> = {
+  ...outlookThreadJson,
+  type: 'email_thread',
+  messages: outlookThreadJson.messages as IEmailThreadMessage[]
+}
+
+const forwardedEmailThread: IEmailThread<'messages'> = {
+  ...forwardedEmailThreadJson,
+  type: 'email_thread',
+  messages: forwardedEmailThreadJson.messages as IEmailThreadMessage[]
+}
+
+// normally, email content is rendered in iframe, which makes it hard
+// (if possible) to query. We change this behaviour to simply render
+// the content instead of rendering the iframe.
+jest.mock('../../Iframe', () => ({
+  Iframe: ({ srcDoc }) => (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: srcDoc
+      }}
+    />
+  )
+}))
+
+describe('EmailThreadItem', () => {
+  it('renders', () => {
+    render(
+      <EmailThreadItem
+        email={normalizeThreadMessageToThreadEmail(gmailThread.messages[0])}
+        collapsed={false}
+        onToggleCollapsed={() => {}}
+      />
+    )
+  })
+
+  it('it does not render replied content (replied from gmail)', () => {
+    const replyFromGmail = normalizeThreadMessageToThreadEmail(
+      gmailThread.messages[1]
+    )
+    const somethingInOriginalEmail = 'This is the original content'
+    const somethingInReplyFromGmail =
+      'This reply is sent via gmail client, not rechat'
+
+    const $ = render(
+      <EmailThreadItem
+        email={replyFromGmail}
+        collapsed={false}
+        onToggleCollapsed={() => {}}
+      />
+    )
+
+    expect($.queryByText(somethingInOriginalEmail, { exact: false })).toBeNull()
+    expect(
+      $.queryByText(somethingInReplyFromGmail, { exact: false })
+    ).not.toBeNull()
+  })
+
+  it('it does not render replied content (replied from outlook)', () => {
+    const replyFromOutlook = normalizeThreadMessageToThreadEmail(
+      outlookThread.messages[2]
+    )
+    const somethingInOriginalEmail = 'Reply To Saeed From Outlook'
+    const somethingInReplyFromOutlook = 'Reply body 2'
+
+    const $ = render(
+      <EmailThreadItem
+        email={replyFromOutlook}
+        collapsed={false}
+        onToggleCollapsed={() => {}}
+      />
+    )
+
+    expect($.queryByText(somethingInOriginalEmail, { exact: false })).toBeNull()
+    expect(
+      $.queryByText(somethingInReplyFromOutlook, { exact: false })
+    ).not.toBeNull()
+  })
+
+  it('it does not render replied content (replied from rechat)', () => {
+    const replyFromRechat = normalizeThreadMessageToThreadEmail(
+      gmailThread.messages[3]
+    )
+    const somethingInOriginalEmail = 'This is the original content'
+    const somethingInReplyFromRechat = 'And this reply is sent from rechat!'
+
+    const $ = render(
+      <EmailThreadItem
+        email={replyFromRechat}
+        collapsed={false}
+        onToggleCollapsed={() => {}}
+      />
+    )
+
+    expect($.queryByText(somethingInOriginalEmail, { exact: false })).toBeNull()
+    expect(
+      $.queryByText(somethingInReplyFromRechat, { exact: false })
+    ).not.toBeNull()
+  })
+
+  it('it does not collapse the forwarded content', () => {
+    // IMPORTANT: note that this behaviour is the current behaviour but
+    // gmail for example has more complicated and smarted behaviour.
+    // It doesn't collapse forwarded content if it's inbound, but it
+    // does collapse it when it's in sent. We skip these complexities
+    // and don't collapse forwarded content at all for now.
+    const originalEmail = normalizeThreadMessageToThreadEmail(
+      forwardedEmailThread.messages[0]
+    )
+    const secondForwardEmail = normalizeThreadMessageToThreadEmail(
+      forwardedEmailThread.messages[1]
+    )
+    const replyEmail = normalizeThreadMessageToThreadEmail(
+      forwardedEmailThread.messages[2]
+    )
+    const somethingInOriginalEmail = 'A request at Lejebolig.dk regarding the'
+    const somethingInSecondForward = 'Forwarded with an extra message'
+    const somethingInReplyFromGmail = 'reply to forwarded email'
+
+    const $ = render(
+      <EmailThreadItem
+        email={originalEmail}
+        collapsed={false}
+        onToggleCollapsed={() => {}}
+      />
+    )
+
+    expect(
+      $.queryByText(somethingInOriginalEmail, { exact: false })
+    ).not.toBeNull()
+
+    $.rerender(
+      <EmailThreadItem
+        email={secondForwardEmail}
+        collapsed={false}
+        onToggleCollapsed={() => {}}
+      />
+    )
+
+    expect(
+      $.queryByText(somethingInOriginalEmail, { exact: false })
+    ).not.toBeNull()
+    expect(
+      $.queryByText(somethingInSecondForward, { exact: false })
+    ).not.toBeNull()
+
+    $.rerender(
+      <EmailThreadItem
+        email={replyEmail}
+        collapsed={false}
+        onToggleCollapsed={() => {}}
+      />
+    )
+
+    expect($.queryByText(somethingInOriginalEmail, { exact: false })).toBeNull()
+    expect($.queryByText(somethingInSecondForward, { exact: false })).toBeNull()
+    expect(
+      $.queryByText(somethingInReplyFromGmail, { exact: false })
+    ).not.toBeNull()
+  })
+
+  it('renders "•••" when there is replied content', () => {
+    const email = normalizeThreadMessageToThreadEmail(gmailThread.messages[1])
+
+    const $ = render(
+      <EmailThreadItem
+        email={email}
+        collapsed={false}
+        onToggleCollapsed={() => {}}
+      />
+    )
+
+    expect($.queryByTitle('Show trimmed content')).not.toBeNull()
+  })
+
+  it('does not render "•••" when there is no replied content', () => {
+    const email = normalizeThreadMessageToThreadEmail(gmailThread.messages[0])
+
+    const $ = render(
+      <EmailThreadItem
+        email={email}
+        collapsed={false}
+        onToggleCollapsed={() => {}}
+      />
+    )
+
+    expect($.queryByTitle('Show trimmed content')).toBeNull()
+  })
+
+  it('Toggles quoted content when "•••" is clicked', () => {
+    const email = normalizeThreadMessageToThreadEmail(gmailThread.messages[1])
+    const originalContent = 'This is the original content'
+
+    const $ = render(
+      <EmailThreadItem
+        email={email}
+        collapsed={false}
+        onToggleCollapsed={() => {}}
+      />
+    )
+
+    const toggleButton = $.getByTitle('Show trimmed content')
+
+    expect($.queryByText(originalContent)).toBeNull()
+
+    fireEvent.click(toggleButton)
+
+    expect($.queryByText(originalContent)).not.toBeNull()
+
+    fireEvent.click(toggleButton)
+
+    expect($.queryByText(originalContent)).toBeNull()
+  })
+})
