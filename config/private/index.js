70fcc86a6417087e1fa863067d4921bec1595d45
OneDev0411 <cryptoapple1109@gmail.com>
2020-03-24 19:04:45 +0430

    chore(config): handled test env (#4087)

--- config/private/index.js
+++ config/private/index.js
@@ -1,4 +1,6 @@
 // config/index.js
 const env = process.env.NODE_ENV
-const filename = env === 'stage' ? 'development' : env
-module.exports = require('./' + filename)
+const filename = ['stage', 'test'].includes(env) ? 'development' : env
+
+// eslint-disable-next-line import/no-dynamic-require
+module.exports = require(`./${filename}`)
--- config/public/index.js
+++ config/public/index.js
@@ -1,6 +1,6 @@
 // config/public/index.js
 const env = process.env.NODE_ENV
-const filename = env === 'stage' ? 'development' : env
+const filename = ['stage', 'test'].includes(env) ? 'development' : env
 
 // eslint-disable-next-line import/no-dynamic-require
 module.exports = require(`./${filename}`)
