451547da7e4b9a1acd53fd72c50f5d24fbe700cd
OneDev0411 <cryptoapple1109@gmail.com>
2020-03-26 22:01:00 +0430

    chore(marketing): active tab item (#3913)

--- app/components/Pages/Dashboard/Marketing/Tabs/index.tsx
+++ app/components/Pages/Dashboard/Marketing/Tabs/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import _findIndex from 'lodash/findIndex'
 
 import { PageTabs, Tab, TabLink } from 'components/PageTabs'
+import { SectionItem } from 'components/PageSideNav/types'
 import { SectionCollection } from 'hooks/use-marketing-center-sections'
 
 import MegaMenu from './SectionMegaMenu'
@@ -8,12 +10,34 @@ import MegaMenu from './SectionMegaMenu'
 interface Props {
   sections: SectionCollection
   mediums: string[]
+  templateTypes: string
 }
 
-const ContactsTabs = ({ sections, mediums }: Props) => {
+const ContactsTabs = ({ sections, mediums, templateTypes }: Props) => {
   const { marketingCenter, life, properties } = sections
   const keys: string[] = [life.key, properties.key]
 
+  const checkValidTemplateTypes = (items: SectionItem[]) => {
+    return _findIndex(items, (i: SectionItem) => {
+      const val = Array.isArray(i.value) ? i.value.join(',') : i.value
+
+      return val === templateTypes
+    })
+  }
+
+  const getActiveTab = () => {
+    if (templateTypes) {
+      if (checkValidTemplateTypes(life.items) >= 0) {
+        return life.key
+      }
+
+      if (checkValidTemplateTypes(properties.items) >= 0) {
+        return properties.key
+      }
+    }
+
+    return marketingCenter.items[0].link
+  }
   const renderMegaMenu = ({ selectedTab, close }) => {
     switch (selectedTab) {
       case life.key:
@@ -27,6 +51,7 @@ const ContactsTabs = ({ sections, mediums }: Props) => {
 
   return (
     <PageTabs
+      defaultValue={getActiveTab()}
       tabs={[
         ...marketingCenter.items.map(i => (
           <TabLink key={i.link} to={i.link} value={i.link} label={i.title} />
