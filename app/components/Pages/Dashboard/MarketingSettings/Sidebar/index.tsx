7d6e01b6175a89f9f3454ed15988dff16050879b
OneDev0411 <cryptoapple1109@gmail.com>
2020-04-01 19:25:21 +0430

    wip_feat(admin): mc admin color picker and scrollable sidebar (#3967)

--- app/components/Pages/Dashboard/MarketingSettings/Sidebar/index.tsx
+++ app/components/Pages/Dashboard/MarketingSettings/Sidebar/index.tsx
@@ -4,6 +4,7 @@ import {
   Divider,
   TextField,
   FormControl,
+  InputAdornment,
   Typography,
   ExpansionPanel,
   ExpansionPanelSummary,
@@ -22,8 +23,15 @@ const useStyles = makeStyles(
   (theme: Theme) =>
     createStyles({
       wrapper: {
-        borderLeft: `1px solid ${theme.palette.divider}`,
-        height: '100%'
+        borderLeft: `1px solid ${theme.palette.divider}`
+      },
+      scrollableArea: {
+        maxHeight: '100vh',
+        overflow: 'overlay'
+      },
+      colorPickerSquare: {
+        width: theme.spacing(3),
+        height: theme.spacing(3)
       }
     }),
   { name: 'MarketingSettingsSidebar' }
@@ -55,53 +63,68 @@ export default function Sidebar({ sections, settings, onUpdate }: Props) {
 
   return (
     <div className={classes.wrapper}>
-      {sections.map(section => (
-        <React.Fragment key={section.name}>
-          <ExpansionPanel elevation={0}>
-            <ExpansionPanelSummary expandIcon={<IconKeyboardArrowDown />}>
-              <Typography variant="subtitle2">{section.name}</Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <Grid container spacing={2}>
-                {section.fields.map(field => (
-                  <Grid container item key={field.name}>
-                    <FormControl fullWidth>
-                      {field.type === 'color' && (
-                        <ColorPicker
-                          size="small"
-                          variant="outlined"
-                          label={field.label}
-                          value={settings[field.name]}
-                          TextFieldProps={{ value: settings[field.name] }}
-                          onChange={(color?: string) => {
-                            if (!color) {
-                              return
-                            }
+      <div className={classes.scrollableArea}>
+        {sections.map((section, sectionIndex) => (
+          <React.Fragment key={section.name}>
+            <ExpansionPanel elevation={0}>
+              <ExpansionPanelSummary expandIcon={<IconKeyboardArrowDown />}>
+                <Typography variant="subtitle2">{section.name}</Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Grid container spacing={2}>
+                  {section.fields.map(field => (
+                    <Grid container item key={field.name}>
+                      <FormControl fullWidth>
+                        {field.type === 'color' && (
+                          <ColorPicker
+                            size="small"
+                            variant="outlined"
+                            label={field.label}
+                            value={settings[field.name]}
+                            TextFieldProps={{ value: settings[field.name] }}
+                            InputProps={{
+                              endAdornment: (
+                                <InputAdornment position="end">
+                                  <span
+                                    className={classes.colorPickerSquare}
+                                    style={{
+                                      backgroundColor: settings[field.name]
+                                    }}
+                                  />
+                                </InputAdornment>
+                              )
+                            }}
+                            onChange={(color?: string) => {
+                              if (!color) {
+                                return
+                              }
 
-                            handleUpdateSettings(field.name, color)
-                          }}
-                        />
-                      )}
-                      {field.type !== 'color' && (
-                        <TextField
-                          size="small"
-                          variant="outlined"
-                          label={field.label}
-                          value={settings[field.name]}
-                          onChange={e =>
-                            handleUpdateSettings(field.name, e.target.value)
-                          }
-                        />
-                      )}
-                    </FormControl>
-                  </Grid>
-                ))}
-              </Grid>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-          <Divider />
-        </React.Fragment>
-      ))}
+                              handleUpdateSettings(field.name, color)
+                            }}
+                          />
+                        )}
+                        {field.type !== 'color' && (
+                          <TextField
+                            size="small"
+                            variant="outlined"
+                            label={field.label}
+                            value={settings[field.name]}
+                            onChange={e =>
+                              handleUpdateSettings(field.name, e.target.value)
+                            }
+                          />
+                        )}
+                      </FormControl>
+                    </Grid>
+                  ))}
+                </Grid>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+            {sectionIndex < sections.length - 1 && <Divider />}
+          </React.Fragment>
+        ))}
+      </div>
+      <Divider />
     </div>
   )
 }
