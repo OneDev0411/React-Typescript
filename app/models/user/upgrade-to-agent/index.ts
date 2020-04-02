729677affad10cc5aa464e322b659d3fd0a7cd93
OneDev0411 <cryptoapple1109@gmail.com>
2020-04-02 19:06:13 +0430

    refactor(models/user): upgrade to agent (#4048)

--- app/models/user/upgrade-to-agent/index.ts
+++ app/models/user/upgrade-to-agent/index.ts
@@ -1,16 +1,23 @@
 import Fetch from '../../../services/fetch'
 
+export interface UpgradeAgentParams {
+  agent: UUID
+  secret: string
+}
+
 /**
  * Upgrade user to agent wirh its agent_id and secret question answer
- * @param {object} // contain agent and secret key
  */
-const upgrade = async params => {
+export async function upgradeAgent(
+  params: UpgradeAgentParams
+): Promise<IUser | undefined> {
   try {
     const response = await new Fetch().patch('/users/self/upgrade').send(params)
+
     return response.body.data
   } catch (error) {
     throw error
   }
 }
 
-export default upgrade
+export default upgradeAgent
