780e039f707bf68c5c01cb067ba4d3a45be519ca
OneDev0411 <cryptoapple1109@gmail.com>
2020-04-03 19:34:54 +0430

    chore(view): updated lastTouched to span (#4106)

--- app/views/components/LastTouched/index.tsx
+++ app/views/components/LastTouched/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import timeago from 'timeago.js'
-import { Typography } from '@material-ui/core'
+import { Typography, Box, Theme } from '@material-ui/core'
+import { makeStyles, createStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    wrapper: {
+      fontSize: theme.typography.body2.fontSize,
+      fontWeight: theme.typography.body2.fontWeight
+    }
+  })
+)
 
 interface Props {
   contact: IContact
 }
 
 export function LastTouched(props: Props) {
+  const classes = useStyles()
   const { last_touch: lastTouch, next_touch: nextTouch } = props.contact
 
   if (!lastTouch) {
@@ -18,17 +29,17 @@ export function LastTouched(props: Props) {
   }
 
   return (
-    <div>
-      <Typography variant="body2" component="span">
+    <Box className={classes.wrapper}>
+      <span>
         Last Touch was <b>{timeago().format(lastTouch * 1000)}</b>
         {!nextTouch && '.'}
-      </Typography>
+      </span>
       {nextTouch && (
-        <Typography variant="body2" component="span">
+        <span>
           , you wanted to be in touch every{' '}
           <b>{Math.round((nextTouch - lastTouch) / 86400)}</b> days.
-        </Typography>
+        </span>
       )}
-    </div>
+    </Box>
   )
 }
