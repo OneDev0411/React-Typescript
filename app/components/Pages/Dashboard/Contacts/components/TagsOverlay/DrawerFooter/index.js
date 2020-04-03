762a45f04566c4ebc511153572d56fad41ce9aa3
OneDev0411 <cryptoapple1109@gmail.com>
2020-04-03 16:32:43 +0430

    chore(drawer): added className to button in tag drawer (#4083)

--- app/components/Pages/Dashboard/Contacts/components/TagsOverlay/DrawerFooter/index.js
+++ app/components/Pages/Dashboard/Contacts/components/TagsOverlay/DrawerFooter/index.js
@@ -2,35 +2,47 @@ import styled from 'styled-components'
 import React from 'react'
 import Flex from 'styled-flex-component'
 
-import { Button } from '@material-ui/core'
+import { Button, createStyles } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    button: {
+      marginLeft: '0.5em'
+    }
+  })
+)
 
 const FooterContainer = styled(Flex)`
   width: 100%;
 `
-
 const TagsOverlayFooter = ({
   tagsLength,
   onSubmit,
   closeOverlay,
   isSubmitting
-}) => (
-  <FooterContainer alignCenter justifyBetween>
-    {`${tagsLength} tags selected`}
-    <Flex center>
-      <Button variant="outlined" onClick={closeOverlay}>
-        Cancel
-      </Button>
-      <Button
-        color="secondary"
-        variant="contained"
-        data-test="save-tags-button"
-        style={{ marginLeft: '0.5em' }}
-        onClick={onSubmit}
-      >
-        {isSubmitting ? 'Saving...' : 'Save'}
-      </Button>
-    </Flex>
-  </FooterContainer>
-)
+}) => {
+  const classes = useStyles()
+
+  return (
+    <FooterContainer alignCenter justifyBetween>
+      {`${tagsLength} tags selected`}
+      <Flex center>
+        <Button variant="outlined" onClick={closeOverlay}>
+          Cancel
+        </Button>
+        <Button
+          color="secondary"
+          variant="contained"
+          data-test="save-tags-button"
+          className={classes.button}
+          onClick={onSubmit}
+        >
+          {isSubmitting ? 'Saving...' : 'Save'}
+        </Button>
+      </Flex>
+    </FooterContainer>
+  )
+}
 
 export default TagsOverlayFooter
