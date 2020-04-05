e26c83e2a90d7169776d3b71cd8484718afaf72d
OneDev0411 <cryptoapple1109@gmail.com>
2020-04-05 16:18:16 +0430

    chore(icons): added circle check outline (#4048)

---
+++ app/views/components/SvgIcons/CircleCheckOutline/IconCircleCheckOutline.js
@@ -0,0 +1,96 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import styled, { css } from 'styled-components'
+
+const width = '24'
+const height = '24'
+const viewBox = '0 0 24 24'
+
+const sizes = {
+  small: { width: 16, height: 16 },
+  medium: { width: 24, height: 24 },
+  large: { width: 32, height: 32 }
+}
+
+// somehow sizes is ending up in markup, even if it is not a valid svg attribute
+// until we have a better solution, just render it empty, instead to '[Object object]'
+Object.defineProperty(sizes, 'toString', { value: () => '', enumerable: false })
+
+const getDimensions = (size, sizes) => {
+  if (size && typeof size.width === 'number' && typeof size.height === 'number') {
+    return size
+  }
+  return size && sizes[size]
+    ? sizes[size]
+    : { width, height }
+}
+
+const getCss = (size, sizes, fillColor, fillColorRule, noStyles) => {
+  if (noStyles) { return '' }
+  const dimensions = getDimensions(size, sizes)
+  const fillRule = fillColor && fillColorRule ? `${fillColorRule}{ fill: ${fillColor}; }` : ''
+  return css`
+    width: ${dimensions.width}px;
+    height: ${dimensions.height}px;
+    ${fillRule}
+  `
+}
+
+const propsToCss = ({
+  size,
+  sizes,
+  fillColor,
+  fillColorRule,
+  noStyles
+}) => getCss(size, sizes, fillColor, fillColorRule, noStyles)
+
+const Image = styled.svg`${propsToCss}`
+
+const children = (
+  <Fragment>
+    <path
+      fill='none'
+      d='M0 0h24v24H0V0z'
+      key='key-0'
+    />
+    <path
+      d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.88-11.71L10 14.17l-1.88-1.88a.996.996 0 1 0-1.41 1.41l2.59 2.59c.39.39 1.02.39 1.41 0L17.3 9.7a.996.996 0 0 0 0-1.41c-.39-.39-1.03-.39-1.42 0z'
+      key='key-1'
+    />
+  </Fragment>
+)
+
+const defaultProps = {
+  children,
+  viewBox,
+  fillColor: null,
+  fillColorRule: '&&& path, &&& use, &&& g',
+  sizes,
+  size: null
+}
+
+const propTypes = {
+  fillColor: PropTypes.string,
+  fillColorRule: PropTypes.string,
+  viewBox: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  size: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.shape({
+      height: PropTypes.number.isRequired,
+      width: PropTypes.number.isRequired
+    })
+  ]),
+  sizes: PropTypes.shape({
+    height: PropTypes.number,
+    width: PropTypes.number
+  })
+}
+
+export default Object.assign(Image, {
+  getDimensions,
+  getCss,
+  defaultProps,
+  propTypes,
+  displayName: 'IconCircleCheckOutline'
+})
