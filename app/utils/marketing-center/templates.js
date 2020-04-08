493aef9cd061fe8f292bf3897396b46227865142
OneDev0411 <cryptoapple1109@gmail.com>
2020-04-08 18:20:15 +0430

    chore(mc): update brand styles markup (#3967)

--- app/utils/marketing-center/templates.js
+++ app/utils/marketing-center/templates.js
@@ -28,8 +28,7 @@ export const BRAND_STYLES_TEMPLATE = `{% if get('body-font-family') %}
 <mj-attributes>
   <mj-body
     background-color="{{get('body-bg-color')}}"
-  ></mj-body>
-
+  />
   <mj-class
     name="body"
     background-color="{{get('body-bg-color')}}"
@@ -37,8 +36,7 @@ export const BRAND_STYLES_TEMPLATE = `{% if get('body-font-family') %}
     font-family="{{get('body-font-family')}}"
     font-weight="{{get('body-font-weight')}}"
     font-size="{{get('body-font-size')}}"
-  ></mj-class>
-
+  />
   <mj-class
     name="container"
     background-color="{{get('container-bg-color')}}"
@@ -46,40 +44,56 @@ export const BRAND_STYLES_TEMPLATE = `{% if get('body-font-family') %}
     font-family="{{get('container-font-family')}}"
     font-weight="{{get('container-font-weight')}}"
     font-size="{{get('container-font-size')}}"
-  ></mj-class>
-
+  />
+  <mj-class
+    name="inverted-container"
+    background-color="{{get('inverted-container-bg-color')}}"
+    color="{{get('inverted-container-text-color')}}"
+  />
   <mj-class
     name="h1"
     color="{{get('h1-text-color')}}"
     font-family="{{get('h1-font-family')}}"
     font-weight="{{get('h1-font-weight')}}"
     font-size="{{get('h1-font-size')}}"
-  ></mj-class>
-
+  />
+  <mj-class
+    name="inverted-h1"
+    color="{{get('inverted-h1-text-color')}}"
+  />
   <mj-class
     name="h2"
     color="{{get('h2-text-color')}}"
     font-family="{{get('h2-font-family')}}"
     font-weight="{{get('h2-font-weight')}}"
     font-size="{{get('h2-font-size')}}"
-  ></mj-class>
-
+  />
+  <mj-class
+    name="inverted-h2"
+    color="{{get('inverted-h2-text-color')}}"
+  />
   <mj-class
     name="h3"
     color="{{get('h3-text-color')}}"
     font-family="{{get('h3-font-family')}}"
     font-weight="{{get('h3-font-weight')}}"
     font-size="{{get('h3-font-size')}}"
-  ></mj-class>
-
+  />
+  <mj-class
+    name="inverted-h3"
+    color="{{get('inverted-h3-text-color')}}"
+  />
   <mj-class
     name="light"
     color="{{get('light-text-color')}}"
     font-family="{{get('light-font-family')}}"
     font-weight="{{get('light-font-weight')}}"
     font-size="{{get('light-font-size')}}"
-  ></mj-class>
-
+  />
+  <mj-class
+    name="inverted-light"
+    color="{{get('inverted-light-text-color')}}"
+  />
   <mj-button
     background-color="{{get('button-bg-color')}}"
     color="{{get('button-text-color')}}"
@@ -87,7 +101,12 @@ export const BRAND_STYLES_TEMPLATE = `{% if get('body-font-family') %}
     border="{{get('button-border')}}"
     font-weight="{{get('button-font-weight')}}"
     font-size="{{get('button-font-size')}}"
-  ></mj-button>
+  />
+  <mj-button
+    name="inverted-button"
+    background-color="{{get('inverted-button-bg-color')}}"
+    color="{{get('inverted-button-text-color')}}"
+  />
 </mj-attribues>
 `
 
