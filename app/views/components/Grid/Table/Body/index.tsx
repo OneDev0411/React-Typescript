03fc06b8b44d8407111bf308615c128ab78a8177
OneDev0411 <cryptoapple1109@gmail.com>
2020-04-22 15:48:17 +0300

    chore(grid): remove intersection

--- app/views/components/Grid/Table/Body/index.tsx
+++ app/views/components/Grid/Table/Body/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
-import { TableBody, createStyles, makeStyles, Theme } from '@material-ui/core'
+import {
+  TableBody,
+  TableCell,
+  TableRow,
+  makeStyles,
+  Theme
+} from '@material-ui/core'
+
+import cn from 'classnames'
 
 import {
   TableColumn,
@@ -9,8 +17,9 @@ import {
   TdProps
 } from '../types'
 
+import { resolveAccessor } from '../helpers/resolve-accessor'
+
 import { useGridContext } from '../hooks/use-grid-context'
-import { Row } from './Row'
 
 interface Props<Row> {
   columns: TableColumn<Row>[]
@@ -22,15 +31,62 @@ interface Props<Row> {
   getTdProps?: (data: TdProps<Row>) => object
 }
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    table: {
-      '& tr:nth-child(odd)': {
-        backgroundColor: theme.palette.grey[50]
+interface Props<Row> {
+  row: Row
+  rowIndex: number
+  rowsCount: number
+  columns: TableColumn<Row>[]
+  hoverable: boolean
+  selection: GridSelectionOptions<Row> | null
+  isSelected: boolean
+  classes: GridClasses
+  getTrProps?: (data: TrProps<Row>) => object
+  getTdProps?: (data: TdProps<Row>) => object
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  table: {
+    '& tr:nth-child(odd)': {
+      backgroundColor: theme.palette.grey[50]
+    }
+  },
+  row: ({ selection }: { selection: GridSelectionOptions<any> | null }) => {
+    let styles = {
+      fontSize: theme.typography.body1.fontSize,
+      fontWeight: theme.typography.body1.fontWeight,
+      '& td:first-child': {
+        paddingLeft: theme.spacing(1)
+      },
+      '&:hover .primary': {
+        cursor: 'pointer'
       }
     }
-  })
-)
+
+    if (selection) {
+      styles = {
+        ...styles,
+        ...{
+          '&:hover .selection--default-value': {
+            display: 'none !important'
+          },
+          '&:hover .selection--checkbox': {
+            display: 'block !important '
+          },
+          '& .selected': {
+            backgroundColor: theme.palette.action.selected
+          }
+        }
+      }
+    }
+
+    return styles
+  },
+  column: {
+    padding: 0,
+    borderBottom: 'none',
+    height: theme.spacing(8)
+  }
+}))
 
 export function Body<Row>({
   columns,
@@ -43,7 +99,7 @@ export function Body<Row>({
 }: Props<Row & { id?: string }>) {
   const [state] = useGridContext()
 
-  const bodyClasses = useStyles()
+  const bodyClasses = useStyles({ selection })
 
   const isRowSelected = (row: Row & { id?: string }, rowIndex: number) => {
     return (
@@ -54,26 +110,73 @@ export function Body<Row>({
   }
 
   return (
-    <>
-      <TableBody className={bodyClasses.table}>
-        {rows.map((row, rowIndex: number) => {
-          return (
-            <Row
-              key={row.id || rowIndex}
-              row={row}
-              rowIndex={rowIndex}
-              rowsCount={rows.length}
-              columns={columns}
-              selection={selection}
-              isSelected={isRowSelected(row, rowIndex)}
-              classes={classes}
-              hoverable={hoverable}
-              getTdProps={getTdProps}
-              getTrProps={getTrProps}
-            />
-          )
-        })}
-      </TableBody>
-    </>
+    <TableBody className={bodyClasses.table}>
+      {rows.map((row, rowIndex: number) => (
+        <TableRow
+          key={row.id || rowIndex}
+          className={cn(bodyClasses.row, classes.row)}
+          hover={hoverable}
+          {...getTrProps({
+            rowIndex,
+            row,
+            selected: isRowSelected(row, rowIndex)
+          })}
+        >
+          {columns
+            .filter(
+              (column: TableColumn<Row>) => column.render || column.accessor
+            )
+            .map((column: TableColumn<Row>, columnIndex: number) => (
+              <TableCell
+                key={columnIndex}
+                align={column.align || 'inherit'}
+                classes={{
+                  root: cn(bodyClasses.column, column.class)
+                }}
+                className={cn({
+                  primary: column.primary === true,
+                  selected: isRowSelected
+                })}
+                style={{
+                  width: column.width || 'inherit',
+                  ...(column.rowStyle || {}),
+                  ...(column.style || {})
+                }}
+                {...getTdProps({
+                  columnIndex,
+                  column,
+                  rowIndex,
+                  row
+                })}
+              >
+                {getCell(column, row, rowIndex, columnIndex, rows.length)}
+              </TableCell>
+            ))}
+        </TableRow>
+      ))}
+    </TableBody>
   )
+}
+
+function getCell<Row>(
+  column: TableColumn<Row>,
+  row: Row,
+  rowIndex: number,
+  columnIndex: number,
+  totalRows: number
+) {
+  if (column.render) {
+    return column.render({
+      row,
+      totalRows,
+      rowIndex,
+      columnIndex
+    })
+  }
+
+  if (column.accessor) {
+    return resolveAccessor(column.accessor, row, rowIndex)
+  }
+
+  return ''
 }
--- app/views/components/Grid/Table/features/Selection/use-row-selection.tsx
+++ app/views/components/Grid/Table/features/Selection/use-row-selection.tsx
@@ -56,6 +56,7 @@ export function useRowsSelection<Row>(
             <div
               className="selection--checkbox"
               style={{ display: showDefaultValue ? 'none' : 'block' }}
+              onClick={e => e.stopPropagation()}
             >
               <Checkbox
                 checked={isRowSelected<Row>(
