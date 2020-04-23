a15671eeb5cf520722ad80ce1e1c130f42608f4d
OneDev0411 <cryptoapple1109@gmail.com>
2020-04-23 20:40:57 +0430

    chore(grid): use a pure js way for checking property exists (#4218)

--- app/views/components/Grid/Table/Table/index.tsx
+++ app/views/components/Grid/Table/Table/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import _get from 'lodash/get'
 import { Table, makeStyles, createStyles, Theme } from '@material-ui/core'
 
 import useEffectOnce from 'react-use/lib/useEffectOnce'
@@ -156,7 +155,11 @@ export function GridTable<Row>({
       <Actions<Row>
         rows={newRows}
         totalRows={totalRows}
-        showSelectAll={_get(selection, 'showSelectAll', true)}
+        showSelectAll={
+          selection && typeof selection.showSelectAll !== 'undefined'
+            ? selection.showSelectAll
+            : true
+        }
         TableActions={TableActions}
       />
     </>
