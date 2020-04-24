385584fdb1ec7da68a345d0c4a7618dbbdd8f5f1
OneDev0411 <cryptoapple1109@gmail.com>
2020-04-24 14:14:47 +0430

    refactor(onboarding/header): updated logo size (#4048)

--- app/components/Pages/AgentOnboarding/Header.tsx
+++ app/components/Pages/AgentOnboarding/Header.tsx
@@ -16,8 +16,8 @@ const useStyles = makeStyles((theme: Theme) =>
       marginBottom: theme.spacing(6)
     },
     logo: {
-      width: 100,
-      height: 100,
+      width: 96,
+      height: 96,
       marginBottom: theme.spacing(2)
     },
     title: {
