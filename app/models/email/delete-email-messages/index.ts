f751bd4f6878d1dbd2f3088c5beedc93111d278e
OneDev0411 <cryptoapple1109@gmail.com>
2020-04-26 14:19:32 +0430

    feat(inbox): add delete & mark as read/unread models (#4204,#4227,#4037)

---
+++ app/models/email/delete-email-messages/index.ts
@@ -0,0 +1,7 @@
+import Fetch from '../../../services/fetch'
+
+export async function deleteEmailMessages(messageIds: UUID[]): Promise<void> {
+  await new Fetch().delete('/emails/messages/trash').send({
+    'message_ids[]': messageIds
+  })
+}
---
+++ app/models/email/set-email-messages-read-status/index.ts
@@ -0,0 +1,11 @@
+import Fetch from '../../../services/fetch'
+
+export async function setEmailMessagesReadStatus(
+  messageIds: UUID[],
+  status: boolean
+): Promise<void> {
+  await new Fetch().put('/emails/messages/status').send({
+    'message_ids[]': messageIds,
+    is_read: status
+  })
+}
