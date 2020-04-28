ae511e32f2595cdb72adf9bea78cc88325e40b65
OneDev0411 <cryptoapple1109@gmail.com>
2020-04-29 01:58:04 +0300

    chore(insight): split bounced and unsubscribed columns (#4200)

--- app/components/Pages/Dashboard/MarketingInsights/Insight/ContactsTable.tsx
+++ app/components/Pages/Dashboard/MarketingInsights/Insight/ContactsTable.tsx
@@ -8,8 +8,7 @@ import { RenderProps } from 'components/Grid/Table/types'
 import { useGridStyles } from 'components/Grid/Table/styles'
 
 import { SortableColumnsType as SortFieldType } from './SortField'
-import RowBadges from './RowBadges'
-import { ContactColumn } from './styled'
+import { ContactColumn, StyledBadge } from './styled'
 import { contactsList } from './helpers'
 import { ContactsListType } from './types'
 
@@ -21,10 +20,9 @@ interface ContactsPropsType {
 
 const columns = [
   {
-    header: 'Contact',
     id: 'contact',
     primary: true,
-    width: '40%',
+    width: '30%',
     accessor: (row: ContactsListType) => row.display_name,
     render: ({ row }: RenderProps<ContactsListType>) => (
       <ContactColumn>
@@ -37,30 +35,34 @@ const columns = [
     )
   },
   {
-    header: 'Badge',
-    id: 'badge',
-    width: '20%',
-    render: ({ row }: RenderProps<ContactsListType>) => (
-      <div className="labels-container">
-        <RowBadges data={row} />
-      </div>
-    )
+    id: 'bounce',
+    accessor: (row: ContactsListType) => row.failed,
+    render: ({ row }: RenderProps<ContactsListType>) =>
+      row.failed > 0 && (
+        <StyledBadge>Bounced {row.failed >= 2 && row.failed}</StyledBadge>
+      )
   },
   {
-    header: 'Opened',
+    id: 'unsubscribe',
+    accessor: (row: ContactsListType) => row.unsubscribed,
+    render: ({ row }: RenderProps<ContactsListType>) =>
+      row.unsubscribed > 0 && (
+        <StyledBadge>
+          Unsubscribed {row.unsubscribed >= 2 && row.unsubscribed}
+        </StyledBadge>
+      )
+  },
+  {
     id: 'opened',
     class: 'opaque',
-    width: '20%',
     accessor: (row: ContactsListType) => row.opened,
     render: ({ row }: RenderProps<ContactsListType>) => (
       <span>Opened: {row.opened}</span>
     )
   },
   {
-    header: 'Clicked',
     id: 'clicked',
     class: 'opaque',
-    width: '20%',
     accessor: (row: ContactsListType) => row.clicked,
     render: ({ row }: RenderProps<ContactsListType>) => (
       <span>Clicked: {row.clicked}</span>
@@ -71,8 +73,6 @@ const columns = [
 function ContactsTable({ item, sortBy, onChangeSort }: ContactsPropsType) {
   const gridClasses = useGridStyles()
   const rows = contactsList(item)
-
-  console.log('>>', sortBy)
 
   return (
     <Table<ContactsListType>
