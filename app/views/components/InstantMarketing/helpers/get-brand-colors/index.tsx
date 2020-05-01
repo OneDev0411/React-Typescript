e2d7562a043bcf48ddd5f351c88255749189d469
OneDev0411 <cryptoapple1109@gmail.com>
2020-05-01 22:35:16 +0430

    fix(mc): implement get brand colors based on new palette (#3967)

--- app/views/components/InstantMarketing/helpers/get-brand-colors/index.tsx
+++ app/views/components/InstantMarketing/helpers/get-brand-colors/index.tsx
@@ -1,8 +1,26 @@
+import flattenBrand from 'utils/flatten-brand'
+
 const DEFAULT_COLORS = ['#fff', '#ccc', '#999', '#666', '#333', '#000']
 
 export function getBrandColors(brand: IBrand): string[] {
-  console.log('BRAND', brand)
-  // TODO: use new color palette
+  const flattedBrand = flattenBrand(brand)
 
-  return DEFAULT_COLORS
+  if (
+    !flattedBrand ||
+    !flattedBrand.settings ||
+    !flattedBrand.settings.palette ||
+    !flattedBrand.settings.palette.palette
+  ) {
+    return DEFAULT_COLORS
+  }
+
+  const brandPalette = flattedBrand.settings.palette
+    .palette as BrandSettingsPalette
+
+  const colorKeys = Object.keys(brandPalette).filter(key =>
+    key.includes('color')
+  )
+  const brandColors = colorKeys.map(key => brandPalette[key])
+
+  return [...new Set(brandColors)]
 }
