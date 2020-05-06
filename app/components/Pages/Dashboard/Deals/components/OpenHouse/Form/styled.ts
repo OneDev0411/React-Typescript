2e166737589a92b5f63e6172138ad0f9c5ef3b71
OneDev0411 <cryptoapple1109@gmail.com>
2020-05-06 19:28:04 +0300

    chore(deal open house): use theme.spacing (#4055)

--- app/components/Pages/Dashboard/Deals/components/OpenHouse/Form/styled.ts
+++ app/components/Pages/Dashboard/Deals/components/OpenHouse/Form/styled.ts
@@ -1,13 +1,15 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import { PickerContainer } from 'components/DateTimePicker/styled'
 
 export const DatePickerContainer = styled(PickerContainer)`
-  padding: 0;
-  box-shadow: none;
-  margin-bottom: 1em;
+  ${({ theme }) => css`
+    padding: 0;
+    box-shadow: none;
+    margin-bottom: ${theme.spacing(1)};
 
-  .DayPicker-Caption {
-    margin: 1% auto;
-  }
+    .DayPicker-Caption {
+      margin: 1% auto;
+    }
+  `}
 `
