bcd14b8551459163f673552caf64f193396ad7b0
OneDev0411 <cryptoapple1109@gmail.com>
2020-05-27 22:11:10 +0430

    chore(view): added MeetingRoom icon to SvgIcons

--- app/views/components/SvgIcons/icons.ts
+++ app/views/components/SvgIcons/icons.ts
@@ -5,3 +5,5 @@ export const imageAlignLeft: string = 'M21,15 L15,15 L15,17 L21,17 L21,15 Z M21,
 export const imageAlignRight: string = 'M9,15 L3,15 L3,17 L9,17 L9,15 Z M9,7 L3,7 L3,9 L9,9 L9,7 Z M3,13 L9,13 L9,11 L3,11 L3,13 Z M3,21 L21,21 L21,19 L3,19 L3,21 Z M3,3 L3,5 L21,5 L21,3 L3,3 Z M11,7 L11,17 L21,17 L21,7 L11,7 Z'
 
 export const imageAlignCenter: string = 'M3,21 L21,21 L21,19 L3,19 L3,21 Z M3,3 L3,5 L21,5 L21,3 L3,3 Z M5,7 L5,17 L19,17 L19,7 L5,7 Z'
+
+export const MeetingRoomOutlined: string = 'M19 19V4h-4V3H5v16H3v2h12V6h2v15h4v-2h-2zm-6 0H7V5h6v14zm-3-8h2v2h-2z'
