dea6e007ebfd09b5fc88de9473c759fba1c34f45
OneDev0411 <cryptoapple1109@gmail.com>
2020-06-01 06:42:41 -0700

    fix(deals): fix the merge issues

--- app/components/Pages/Dashboard/Deals/Dashboard/Folders/Checklist/EnvelopeStatus/index.tsx
+++ app/components/Pages/Dashboard/Deals/Dashboard/Folders/Checklist/EnvelopeStatus/index.tsx
@@ -19,18 +19,6 @@ export function EnvelopeStatus({ deal, task, envelope }: Props) {
       return 'Declined'
     }
 
-    const extra = envelope.status === 'Created' ? ' (Draft)' : ''
-
-    if (envelope.status === 'Voided') {
-      return 'Void'
-    }
-
-    if (envelope.status === 'Declined') {
-      return 'Declined'
-    }
-
-    const extra = envelope.status === 'Created' ? ' (Draft)' : ''
-
     const recipients = envelope.recipients.filter(
       recipient => recipient.envelope_recipient_type === 'Signer'
     )
@@ -41,6 +29,7 @@ export function EnvelopeStatus({ deal, task, envelope }: Props) {
 
     const signedCount = recipients.filter(r => r.status === 'Completed').length
 
+    const extra = envelope.status === 'Created' ? ' (Draft)' : ''
     return `${signedCount} of ${recipients.length} signed${extra}`
   }
 
