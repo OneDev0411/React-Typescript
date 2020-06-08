1f26d4954c5867e64b706e3a42527e3407205417
OneDev0411 <cryptoapple1109@gmail.com>
2020-06-08 22:43:40 +0300

    fix(sentry): add more logs

--- app/index.js
+++ app/index.js
@@ -7,7 +7,12 @@ import Provider from './provider'
 if (process.env.NODE_ENV === 'production') {
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
-    release: process.env.SOURCE_VERSION
+    release: process.env.SOURCE_VERSION,
+    beforeSend: (event, hint) => {
+      console.log('[ Sentry ] ', hint.originalException)
+
+      return event
+    }
   })
 }
 
