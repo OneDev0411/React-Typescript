f2ea3789fb6b78624499154c31d46a1dacfe87d8
OneDev0411 <cryptoapple1109@gmail.com>
2020-06-08 19:40:12 -0700

    fix(ci): prevent source mapping vendors

--- webpack/production.js
+++ webpack/production.js
@@ -24,7 +24,7 @@ function postcss() {
   return [require('autoprefixer')()]
 }
 
-webpackConfig.devtool = 'source-map'
+webpackConfig.devtool = false
 
 webpackConfig.output.pathinfo = false
 webpackConfig.output.publicPath = process.env.ASSETS_BASEURL
@@ -39,6 +39,12 @@ webpackConfig.entry = {
   app: [appConfig.compile.entry],
   vendor: appConfig.compile.vendors
 }
+
+webpackConfig.plugins.push(
+  new webpack.SourceMapDevToolPlugin({
+    exclude: [/vendor/gi]
+  })
+)
 
 webpackConfig.plugins.push(
   new webpack.optimize.AggressiveMergingPlugin(),
