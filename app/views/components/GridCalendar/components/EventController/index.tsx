277cdf88cf1898a5d90de394095e9aeab0e7af3b
OneDev0411 <cryptoapple1109@gmail.com>
2020-06-17 19:42:31 +0430

    wip_feat(calendar): click action for some events (#4353)

--- app/views/components/GridCalendar/components/EventController/index.tsx
+++ app/views/components/GridCalendar/components/EventController/index.tsx
@@ -26,5 +26,35 @@ export const EventController = ({
       />
     ) : null
 
+  if (event?.object_type === 'contact' && event?.event_type === 'next_touch') {
+    window.open(`/dashboard/contacts/${event.contact}`, '_blank')
+
+    return null
+  }
+
+  if (
+    event?.object_type === 'deal_context' &&
+    [
+      'list_date',
+      'expiration_date',
+      'contract_date',
+      'inspection_date',
+      'option_period',
+      'financing_due',
+      'title_due',
+      't47_due',
+      'closing_date',
+      'possession_date',
+      'lease_executed',
+      'lease_application_date',
+      'lease_begin',
+      'lease_end'
+    ].includes(event?.event_type)
+  ) {
+    window.open(`/dashboard/deals/${event.deal}`)
+
+    return null
+  }
+
   return <>{eventDrawer}</>
 }
