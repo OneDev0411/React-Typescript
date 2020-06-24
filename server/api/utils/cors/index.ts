c7b1157c13bff45e964f3ad275a7cc3ed1fa5cc7
OneDev0411 <cryptoapple1109@gmail.com>
2020-06-24 19:58:23 +0430

    fix(routes): limit cors api access to authorized rechat users

--- server/api/utils/cors/index.ts
+++ server/api/utils/cors/index.ts
@@ -6,14 +6,23 @@ const router = new Router()
 const app = new Koa()
 
 router.get('/utils/cors/:url(.+)', async ctx => {
+  const { user } = ctx.session
+
+  if (!user) {
+    ctx.status = 401
+    ctx.body = 'Unauthorized'
+
+    return
+  }
+
   const url: string = ctx.params.url
   const urlWithQueryString = ctx.querystring ? `${url}?${ctx.querystring}` : url
 
   const response = await superagent.get(urlWithQueryString).send()
 
-  ctx.body = response.body
-  ctx.status = response.status
   ctx.set(response.header)
+  ctx.status = response.status
+  ctx.body = response.body
 })
 
 module.exports = app.use(router.routes())
