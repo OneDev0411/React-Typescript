540a16d15d21db14647a5c99ce1ed0edb32df35a
OneDev0411 <cryptoapple1109@gmail.com>
2020-06-24 23:08:27 +0430

    refactor(view): added margin prop to SvgIcon

--- app/views/components/SvgIcons/SvgIcon.tsx
+++ app/views/components/SvgIcons/SvgIcon.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import cn from 'classnames'
 import { Icon as BaseIcon } from '@mdi/react'
 import { IconProps } from '@mdi/react/dist/IconProps'
 
+import { useIconStyles } from '../../../styles/use-icon-styles'
+
 import { muiIconSizes } from './icon-sizes'
 
-export function SvgIcon(props: IconProps) {
+interface Props {
+    rightMargined?: boolean
+    leftMargined?: boolean
+}
+
+export function SvgIcon(props: IconProps & Props) {
+    const classes = useIconStyles()
+    const className = cn({
+        [classes.rightMargin]: props.rightMargined,
+        [classes.leftMargin]: props.leftMargined,
+        className: props.className
+    })
     return (
-        <BaseIcon size={muiIconSizes.medium} {...props} />
+        <BaseIcon size={muiIconSizes.medium} {...props} className={className} />
     )
 }
