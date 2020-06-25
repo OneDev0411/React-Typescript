80fa60c648d59bc5b98a26ea84164b65cf783a81
OneDev0411 <cryptoapple1109@gmail.com>
2020-06-25 22:06:22 +0430

    chore(calendar): no need refresh after create event (#4406)

--- app/components/Pages/Dashboard/Calendar/index.tsx
+++ app/components/Pages/Dashboard/Calendar/index.tsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { WithRouterProps } from 'react-router'
 
 import GlobalHeader from 'components/GlobalHeader'
 
 import { GridCalendar } from 'components/GridCalendar'
+import { ActionRef } from 'components/GridCalendar/types'
 
 import { useStyles as useCommonStyles } from './use-styles'
 
 export default function CalendarPage(props: WithRouterProps) {
   const classes = useCommonStyles()
+  const actionRef = useRef<ActionRef>(null)
+
+  const handleCreateEvent = (event: IEvent) => {
+    actionRef.current!.updateCrmEvents(event, 'created')
+  }
 
   return (
     <div className={classes.container}>
-      <GlobalHeader title="Calendar" noPadding />
+      <GlobalHeader
+        title="Calendar"
+        onCreateEvent={handleCreateEvent}
+        noPadding
+      />
       <div className={classes.listContainer}>
-        <GridCalendar />
+        <GridCalendar actionRef={actionRef} />
       </div>
     </div>
   )
