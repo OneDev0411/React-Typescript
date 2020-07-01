bd559f16a379c14d47c00ef72b70625992b8af7b
OneDev0411 <cryptoapple1109@gmail.com>
2020-06-30 19:57:10 -0700

    build(webpack): apparently only Terser can tree shake. enabling to test

--- webpack/production.js
+++ webpack/production.js
@@ -4,6 +4,7 @@ import MomentLocalesPlugin from 'moment-locales-webpack-plugin'
 import CompressionPlugin from 'compression-webpack-plugin'
 import S3Plugin from 'webpack-s3-plugin'
 import SentryCliPlugin from '@sentry/webpack-plugin'
+import TerserPlugin from 'terser-webpack-plugin'
 
 import moment from 'moment'
 
@@ -16,7 +17,9 @@ webpackConfig.optimization = {
   splitChunks: {
     chunks: 'all'
   },
-  usedExports: true
+  usedExports: true,
+  minimize: true,
+  minimizer: [new TerserPlugin()]
 }
 
 const Expires = moment().utc().add('1', 'month').toDate()
