0e47fc1d399ba7909f8eed2f2279bd2b93832125
OneDev0411 <cryptoapple1109@gmail.com>
2020-07-02 12:26:47 +0430

    refactor(contacts): use mui theme for sync with google spacings (#4374)

--- app/components/Pages/Dashboard/Contacts/List/ImportContactsButton/ConnectedAccount/styled.ts
+++ app/components/Pages/Dashboard/Contacts/List/ImportContactsButton/ConnectedAccount/styled.ts
@@ -1,18 +1,19 @@
-import styled from 'styled-components'
+import styled, { ThemeProps } from 'styled-components'
+import { Theme } from '@material-ui/core'
 
 export const AccountInfoWrapper = styled.div`
   line-height: 1.5;
-  padding-top: 10px;
-  min-width: 240px;
+  padding-top: ${({ theme }: ThemeProps<Theme>) => theme.spacing(1.25)}px;
+  min-width: ${({ theme }: ThemeProps<Theme>) => theme.spacing(30)}px;
   .header {
     display: flex;
   }
   .secondary {
     display: flex;
-    padding-left: 28px;
+    padding-left: ${({ theme }: ThemeProps<Theme>) => theme.spacing(3.5)}px;
   }
   .secondary .settings {
     position: relative;
-    right: -10px;
+    right: ${({ theme }: ThemeProps<Theme>) => theme.spacing(-1.25)}px;
   }
 `
