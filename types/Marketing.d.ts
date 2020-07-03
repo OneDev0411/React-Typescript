c9036377005bda45c0f683769dbf995f7e57d033
OneDev0411 <cryptoapple1109@gmail.com>
2020-07-03 19:15:45 +0430

    refactor(mc): refactor mc parts and types related to mediums (#4420)

--- types/Marketing.d.ts
+++ types/Marketing.d.ts
@@ -11,30 +11,24 @@ declare interface IMarketingTemplate extends IModel<'template'> {
   name: string
   brand: null
   template_type: string
-  deleted_at: null
-  medium: string
+  medium: MarketingTemplateMedium
   video: boolean
   url: string
   variant: string
   inputs: string[]
 }
 
-declare type IMarketingTemplateInstance<
-  Associations extends 'contacts' | 'deals' | 'listings' | 'template' = ''
-> = {
-  id: UUID
-  created_at: number
+declare interface IMarketingTemplateInstance
+  extends IModel<'template_instance'> {
   html: string
   file: File
   created_by: string
   branch: string
-  updated_at: number
-  deleted_at: null
-  type: 'template_instance'
-} & Association<'contacts', IContact[], Associations> &
-  Association<'deals', IDeal[], Associations> &
-  AssociationL<'listings', any, Associations> &
-  Association<'template', IMarketingTemplate, Associations>
+  contacts?: IContact[]
+  deals?: IDeal[]
+  listings?: IListings[]
+  template?: ITemplate[]
+}
 
 interface File {
   id: UUID
@@ -49,4 +43,19 @@ interface File {
   url: string
   preview_url: string
   mime: string
+}
+
+declare interface IMarketingTemplateInstanceData {
+  listings?: UUID[]
+  deals?: UUID[]
+  contacts?: UUID[]
+}
+
+declare enum MarketingTemplateMedium {
+  Email = 'Email',
+  Social = 'Social',
+  Letter = 'Letter',
+  LinkedInCover = 'LinkedInCover',
+  FacebookCover = 'FacebookCover',
+  InstagramStor = 'InstagramStory'
 }
