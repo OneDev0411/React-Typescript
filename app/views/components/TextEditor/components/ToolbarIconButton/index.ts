c4cd7a6a5b67e9b498af65639e71ca120ae753dd
OneDev0411 <cryptoapple1109@gmail.com>
2020-07-07 12:54:49 +0430

    fix(account): rich text editor icon colors (#4414)

--- app/views/components/TextEditor/components/ToolbarIconButton/index.ts
+++ app/views/components/TextEditor/components/ToolbarIconButton/index.ts
@@ -5,8 +5,21 @@ import { IconButtonProps } from '@material-ui/core/IconButton'
 export const ToolbarIconButton = styled(IconButton).attrs({
   type: 'button' as string
 })`
-  color: ${({ theme, color }: ThemeProps<Theme> & IconButtonProps) =>
-    !color && theme.palette.grey['800']};
+  ${({ theme, color }: ThemeProps<Theme> & IconButtonProps) => {
+    if (!color || color === 'default') {
+      return ''
+    }
+
+    return `
+        color: ${
+          color === 'primary'
+            ? theme.palette.primary.main
+            : color === 'secondary'
+            ? theme.palette.secondary.main
+            : color
+        } !important;
+      `
+  }}
   padding: ${({ theme }: ThemeProps<Theme>) => `${theme.spacing(1)}px`};
   margin: 0;
   svg {
