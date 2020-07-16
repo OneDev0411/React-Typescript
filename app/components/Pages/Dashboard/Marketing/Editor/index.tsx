4c7b3b7054eba7e488489746c726117a25dfd3bd
OneDev0411 <cryptoapple1109@gmail.com>
2020-07-16 19:23:27 +0430

    refactor(mc): use useTitle hook instead of helmet in editor

--- app/components/Pages/Dashboard/Marketing/Editor/index.tsx
+++ app/components/Pages/Dashboard/Marketing/Editor/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
+import { useTitle } from 'react-use'
 
 import Acl from 'components/Acl'
 import StatefulUrlFlow from 'components/InstantMarketing/adapters/StatefulUrl'
 
 export default function MarketingEditor() {
+  useTitle('Marketing | Rechat')
+
   return (
     <Acl.Marketing fallbackUrl="/dashboard/mls">
-      <Helmet>
-        <title>Marketing | Rechat</title>
-      </Helmet>
       <StatefulUrlFlow />
     </Acl.Marketing>
   )
