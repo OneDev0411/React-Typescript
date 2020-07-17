74952793db6a15de102d5a80d7b134ebf92039a9
OneDev0411 <cryptoapple1109@gmail.com>
2020-07-18 00:33:38 +0430

    refactor(svgicon): fix icon props invalid import and indents! (#4453)

--- app/views/components/SvgIcons/SvgIcon.tsx
+++ app/views/components/SvgIcons/SvgIcon.tsx
@@ -1,24 +1,29 @@
-import React from 'react'
+import React, { ComponentProps } from 'react'
 import cn from 'classnames'
 import { Icon as BaseIcon } from '@mdi/react'
-import { IconProps } from '@mdi/react/dist/IconProps'
 
 import { useIconStyles } from '../../../styles/use-icon-styles'
 
 import { muiIconSizes } from './icon-sizes'
 
+type IconProps = ComponentProps<typeof BaseIcon>
+
 interface Props {
-    rightMargined?: boolean
-    leftMargined?: boolean
+  rightMargined?: boolean
+  leftMargined?: boolean
 }
 
 export function SvgIcon(props: IconProps & Props) {
-    const classes = useIconStyles()
-    const className = cn({
-        [classes.rightMargin]: props.rightMargined,
-        [classes.leftMargin]: props.leftMargined
-    }, props.className)
-    return (
-        <BaseIcon size={muiIconSizes.medium} {...props} className={className} />
-    )
+  const classes = useIconStyles()
+  const className = cn(
+    {
+      [classes.rightMargin]: props.rightMargined,
+      [classes.leftMargin]: props.leftMargined
+    },
+    props.className
+  )
+
+  return (
+    <BaseIcon size={muiIconSizes.medium} {...props} className={className} />
+  )
 }
