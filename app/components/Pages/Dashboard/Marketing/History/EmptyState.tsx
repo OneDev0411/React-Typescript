c48a402e326c4ea46ef2089ad3db80520e177839
OneDev0411 <cryptoapple1109@gmail.com>
2020-07-18 01:00:22 +0430

    feat(mc): add brand settings alert message and link to mc empty state (#4453)

--- app/components/Pages/Dashboard/Marketing/History/EmptyState.tsx
+++ app/components/Pages/Dashboard/Marketing/History/EmptyState.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
-import { Box, Button, useMediaQuery } from '@material-ui/core'
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+import { useSelector } from 'react-redux'
+import {
+  Box,
+  Button,
+  useMediaQuery,
+  createStyles,
+  makeStyles,
+  Theme,
+  Link
+} from '@material-ui/core'
+import { Alert } from '@material-ui/lab'
 
+import { hasUserAccessToBrandSettings } from 'utils/user-teams'
+import { IAppState } from 'reducers'
 import IconVideo from 'components/SvgIcons/VideoFilled/IconVideoFilled'
 
 interface StylesProps {
@@ -33,6 +44,12 @@ const useStyles = makeStyles((theme: Theme) =>
     videoIcon: {
       marginRight: '0.5em',
       fill: `${theme.palette.primary.main}`
+    },
+    brandSettingsAlertMessage: {
+      width: '100%'
+    },
+    brandSettingsAlertBox: {
+      width: '100%'
     }
   })
 )
@@ -44,32 +61,60 @@ export default function EmptyState() {
     `/static/images/marketing/my-designs/empty-state${
       rate > 0 ? `@${rate}x` : ''
     }.png`
+  const user = useSelector(({ user }: IAppState) => user)
+  const hasAccessToBrandSettings = hasUserAccessToBrandSettings(user)
 
   return (
-    <Box display="flex" flexDirection="column" p={7} position="relative">
-      <h1 className={classes.jumbo}>
-        Impress your clients with stunning designs using ready-made social &
-        email templates
-      </h1>
-      <div>
-        <Button
-          color="primary"
-          target="blank"
-          href="https://help.rechat.com/en/collections/1969137-marketing"
-        >
-          <IconVideo className={classes.videoIcon} />
-          Learn More
-        </Button>
-      </div>
-      <img
-        alt="empty-state"
-        className={classes.coverImage}
-        srcSet={`
+    <>
+      {hasAccessToBrandSettings && (
+        <Box mt={1.5}>
+          <Alert
+            severity="info"
+            classes={{ message: classes.brandSettingsAlertMessage }}
+          >
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              className={classes.brandSettingsAlertBox}
+            >
+              <div>
+                You can customize your marketing materials. Use your brand's
+                custom font, logo and color.
+              </div>
+              <div>
+                <Link href="/dashboard/brand-settings" color="secondary">
+                  Setup your brand here
+                </Link>
+              </div>
+            </Box>
+          </Alert>
+        </Box>
+      )}
+      <Box display="flex" flexDirection="column" p={7} position="relative">
+        <h1 className={classes.jumbo}>
+          Impress your clients with stunning designs using ready-made social &
+          email templates
+        </h1>
+        <div>
+          <Button
+            color="primary"
+            target="blank"
+            href="https://help.rechat.com/en/collections/1969137-marketing"
+          >
+            <IconVideo className={classes.videoIcon} />
+            Learn More
+          </Button>
+        </div>
+        <img
+          alt="empty-state"
+          className={classes.coverImage}
+          srcSet={`
           ${getCoverSrc(0)} 1x,
           ${getCoverSrc(2)} 2x,
           ${getCoverSrc(3)} 3x
         `}
-      />
-    </Box>
+        />
+      </Box>
+    </>
   )
 }
