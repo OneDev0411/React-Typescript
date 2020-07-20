0425fd8a5aec5ac3a523259586b25821a575e969
OneDev0411 <cryptoapple1109@gmail.com>
2020-07-20 21:39:42 +0300

    refactor(deals): refactor deals folder menu (#4488)

--- app/components/Pages/Dashboard/Deals/Dashboard/Folders/Checklist/Menu/index.tsx
+++ app/components/Pages/Dashboard/Deals/Dashboard/Folders/Checklist/Menu/index.tsx
@@ -1,22 +1,13 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 import { mdiDotsVertical } from '@mdi/js'
-
 import { MenuItem, IconButton, makeStyles, Theme } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
+import { addNotification } from 'reapop'
+
+import { updateChecklist } from 'actions/deals'
 
 import { BaseDropdown } from 'components/BaseDropdown'
-// import { connect } from 'react-redux'
-// import { addNotification as notify } from 'reapop'
-
 import { SvgIcon } from 'components/SvgIcons/SvgIcon'
-
-// import { updateChecklist } from 'actions/deals'
-// import { confirmation } from 'actions/confirmation'
-
-// import Spinner from 'components/Spinner'
-// import { BasicDropdown } from 'components/BasicDropdown'
-
-// import { Loading } from './styled'
 
 const useStyles = makeStyles(
   (theme: Theme) => ({
@@ -25,7 +16,7 @@ const useStyles = makeStyles(
     }
   }),
   {
-    name: 'ChecklistMenu'
+    name: 'ChecklistOptionsMenu'
   }
 )
 
@@ -37,13 +28,39 @@ interface Props {
 
 export function FolderOptionsMenu({ deal, checklist, isBackOffice }: Props) {
   const classes = useStyles()
+  const dispatch = useDispatch()
+  const [isSaving, setIsSaving] = useState(false)
 
-  const handleToggleTerminate = () => {}
+  const handleUpdateChecklist = async (data: object, close: () => void) => {
+    setIsSaving(true)
 
-  const handleToggleDeactivate = () => {}
+    try {
+      await dispatch(
+        updateChecklist(deal.id, checklist.id, {
+          ...checklist,
+          ...data
+        })
+      )
+    } catch (e) {
+      addNotification({
+        message: 'Something bad happened. please try again',
+        status: 'error'
+      })
+    } finally {
+      dispatch(
+        addNotification({
+          message: 'The checklist is updated',
+          status: 'success'
+        })
+      )
+
+      setIsSaving(false)
+      close()
+    }
+  }
 
   if (
-    deal.is_draft ||
+    !isBackOffice ||
     (!checklist.is_terminatable && !checklist.is_deactivatable)
   ) {
     return null
@@ -58,133 +75,41 @@ export function FolderOptionsMenu({ deal, checklist, isBackOffice }: Props) {
       )}
       renderMenu={({ close }) => (
         <div>
-          <MenuItem
-            onClick={() => {
-              close()
-              handleToggleTerminate()
-            }}
-          >
-            {checklist.is_terminated ? 'Active' : 'Terminate'}
-          </MenuItem>
+          {checklist.is_terminatable && (
+            <MenuItem
+              disabled={isSaving}
+              onClick={() => {
+                handleUpdateChecklist(
+                  {
+                    is_terminated: !checklist.is_terminated
+                  },
+                  close
+                )
+              }}
+            >
+              {checklist.is_terminated ? 'Active' : 'Terminate'}
+            </MenuItem>
+          )}
 
-          <MenuItem
-            onClick={() => {
-              close()
-              handleToggleDeactivate()
-            }}
-          >
-            {checklist.is_deactivated
-              ? 'Make this a primary offer'
-              : 'Make this a back up offer'}
-          </MenuItem>
+          {checklist.is_deactivatable && (
+            <MenuItem
+              disabled={isSaving}
+              onClick={() => {
+                handleUpdateChecklist(
+                  {
+                    is_deactivated: !checklist.is_deactivated
+                  },
+                  close
+                )
+              }}
+            >
+              {checklist.is_deactivated
+                ? 'Make this a primary offer'
+                : 'Make this a back up offer'}
+            </MenuItem>
+          )}
         </div>
       )}
     />
   )
 }
-// class ChecklistMenu extends React.Component {
-//   state = {
-//     isWorking: false
-//   }
-
-//   get MenuItems() {
-//     const { checklist, isBackOffice } = this.props
-//     const list = []
-
-//     if (isBackOffice && checklist.is_terminatable) {
-//       list.push({
-//         label: checklist.is_terminated ? 'Active' : 'Terminate',
-//         onClick: this.terminateChecklist
-//       })
-//     }
-
-//     if (isBackOffice && checklist.is_deactivatable) {
-//       list.push({
-//         label: checklist.is_deactivated
-//           ? 'Make this a primary offer'
-//           : 'Make this a back up offer',
-//         onClick: this.deactivateChecklist
-//       })
-//     }
-
-//     return list
-//   }
-
-//   terminateChecklist = async () => {
-//     const { checklist } = this.props
-
-//     this.setState({ isWorking: true })
-
-//     try {
-//       await this.props.updateChecklist(this.props.deal.id, checklist.id, {
-//         ...checklist,
-//         is_terminated: !checklist.is_terminated
-//       })
-
-//       this.props.notify({
-//         message: `The checklist has been ${
-//           checklist.is_terminated ? 'activated' : 'terminated'
-//         }`,
-//         status: 'success'
-//       })
-//     } catch (e) {
-//       console.log(e)
-//     }
-
-//     this.setState({ isWorking: false })
-//   }
-
-//   deactivateChecklist = async () => {
-//     const { checklist } = this.props
-
-//     this.setState({ isWorking: true })
-
-//     try {
-//       await this.props.updateChecklist(this.props.deal.id, checklist.id, {
-//         ...checklist,
-//         is_deactivated: !checklist.is_deactivated
-//       })
-
-//       this.props.notify({
-//         message: `The checklist has been changed to ${
-//           checklist.is_deactivated ? 'primary' : 'backup'
-//         } offer`,
-//         status: 'success'
-//       })
-//     } catch (e) {
-//       console.log(e)
-//     }
-
-//     this.setState({ isWorking: false })
-//   }
-
-//   render() {
-//     if (this.state.isWorking) {
-//       return (
-//         <Loading>
-//           <Spinner />
-//         </Loading>
-//       )
-//     }
-
-//     const menuItems = this.MenuItems
-
-//     if (menuItems.length === 0) {
-//       return false
-//     }
-
-//     return (
-//       <BasicDropdown
-//         pullTo="right"
-//         style={{ margin: '0.25rem -0.75rem 0 0' }}
-//         buttonRenderer={props => <SvgIcon path={mdiDotsVertical} {...props} />}
-//         items={menuItems}
-//         onChange={item => item.onClick()}
-//       />
-//     )
-//   }
-// }
-
-// export default connect(null, { notify, confirmation, updateChecklist })(
-//   ChecklistMenu
-// )
