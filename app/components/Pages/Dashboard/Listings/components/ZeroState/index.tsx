390ca7201660f53895874a5bc96f6d9fe59337b1
OneDev0411 <cryptoapple1109@gmail.com>
2020-08-05 17:34:25 +0430

    fix(mls): polish MLS zero state (#1841)

--- app/components/Pages/Dashboard/Listings/components/ZeroState/index.tsx
+++ app/components/Pages/Dashboard/Listings/components/ZeroState/index.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
-import { Box } from '@material-ui/core'
+import { Box, Typography } from '@material-ui/core'
+
+import { mdiHomeSearchOutline } from '@mdi/js'
+
+import { SvgIcon } from 'components/SvgIcons/SvgIcon'
+import { muiIconSizes } from 'components/SvgIcons/icon-sizes'
 
 export default function ZeroState() {
-  return <Box p={2}>No listings found.</Box>
+  return (
+    <Box p={3} textAlign="center" width={1}>
+      <SvgIcon path={mdiHomeSearchOutline} size={muiIconSizes.xlarge} />
+      <Typography variant="h6">No results found.</Typography>
+      <Typography>Try clearing your search or some of your filters.</Typography>
+    </Box>
+  )
 }
