70d3126886f26852aa35513a689d350ccf76deda
OneDev0411 <cryptoapple1109@gmail.com>
2020-08-26 19:12:16 +0430

    wip_feat(mc): implement image uploader for social wizard

--- app/components/Pages/Dashboard/Marketing/Wizard/hooks.ts
+++ app/components/Pages/Dashboard/Marketing/Wizard/hooks.ts
@@ -3,18 +3,18 @@ import { useDeepCompareEffect } from 'react-use'
 import _set from 'lodash/set'
 import _clone from 'lodash/cloneDeep'
 
-import { TemplateVariable } from './types'
+import { TemplateVariable, TemplateVariableType } from './types'
 
 export function useEntityWithSetter<T extends object>(
   entity: T
-): [T, (data: TemplateVariable[]) => void] {
+): [T, (data: TemplateVariable<TemplateVariableType>[]) => void] {
   const [innerEntity, setInnerEntity] = useState<T>(entity)
 
   useDeepCompareEffect(() => {
     setInnerEntity(entity)
   }, [entity])
 
-  function setValue(newData: TemplateVariable[]) {
+  function setValue(newData: TemplateVariable<TemplateVariableType>[]) {
     const newEntity = _clone<T>(innerEntity)
 
     newData.forEach(item => {
