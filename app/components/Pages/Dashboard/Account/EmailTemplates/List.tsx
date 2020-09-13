00ad5db2a997275eb1b513f1927729c414854305
OneDev0411 <cryptoapple1109@gmail.com>
2020-09-13 20:26:23 +0430

    fix(email templates): validate empty space template body (#4225, #3604)

--- app/components/Pages/Dashboard/Account/EmailTemplates/List.tsx
+++ app/components/Pages/Dashboard/Account/EmailTemplates/List.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import { useEffect, useState, useContext } from 'react'
 import { connect } from 'react-redux'
 import { ThunkDispatch } from 'redux-thunk'
