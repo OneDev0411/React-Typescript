daf6a53c997a95216307906a327a736aa6a7ace7
OneDev0411 <cryptoapple1109@gmail.com>
2020-09-15 23:20:16 -0700

    fix(build): another attempt

--- webpack/base.js
+++ webpack/base.js
@@ -41,7 +41,12 @@ module.exports = {
       deals: resolvePath('../app/components/Pages/Dashboard/Deals'),
       crm: resolvePath('../app/components/Pages/Dashboard/Contacts'),
       animations: resolvePath('../app/animations'),
-      fixtures: resolvePath('../tests/unit/fixtures')
+      fixtures: resolvePath('../tests/unit/fixtures'),
+      resolve: {
+        alias: {
+          fabric: 'fabric-pure-browser'
+        }
+      }
     }
   },
   plugins: [new webpack.DefinePlugin(config.globals)],
