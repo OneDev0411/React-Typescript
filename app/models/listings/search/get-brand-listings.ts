8429e238d49452c75cf1648025a9950eab3e49b8
OneDev0411 <cryptoapple1109@gmail.com>
2020-10-01 19:06:29 +0330

    refactor(models): simplify get brand listings model (#4720)

--- app/models/listings/search/get-brand-listings.ts
+++ app/models/listings/search/get-brand-listings.ts
@@ -3,21 +3,23 @@ import { byValert } from './get-listings'
 export async function getBrandListings(
   brand: UUID
 ): Promise<ICompactListing[]> {
-  const response = await byValert({
-    brand,
-    limit: 200,
-    sort_order: ['status'],
-    property_types: ['Residential'],
-    property_subtypes: [
-      'RES-Single Family',
-      'RES-Condo',
-      'RES-Townhouse',
-      'RES-Half Duplex',
-      'RES-Farm/Ranch'
-    ]
-  })
-
-  return Object.keys(response.entities.listings).map(
-    id => response.entities.listings[id]
+  const response = await byValert(
+    {
+      brand,
+      limit: 9,
+      sort_order: ['status'],
+      property_types: ['Residential'],
+      property_subtypes: [
+        'RES-Single Family',
+        'RES-Condo',
+        'RES-Townhouse',
+        'RES-Half Duplex',
+        'RES-Farm/Ranch'
+      ]
+    },
+    {},
+    false
   )
+
+  return response.data
 }
