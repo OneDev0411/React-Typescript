8ab50df461983b2472d1227e33dff51dc5ee30c8
OneDev0411 <cryptoapple1109@gmail.com>
2020-10-01 19:09:28 +0330

    fix(an): implement loading/zero states of an (#4720)

--- app/components/Pages/Dashboard/AgentNetwork/index.tsx
+++ app/components/Pages/Dashboard/AgentNetwork/index.tsx
@@ -2,16 +2,18 @@ import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { withRouter } from 'react-router'
 import { useTitle } from 'react-use'
-import { Grid } from '@material-ui/core'
+import { Box, Grid, Typography } from '@material-ui/core'
 
 import { IAppState } from 'reducers'
 import { ACL } from 'constants/acl'
 import { getActiveTeamId } from 'utils/user-teams'
 import { getBrandListings } from 'models/listings/search/get-brand-listings'
+import { useLoadingEntities } from 'hooks/use-loading'
 
 import Acl from 'components/Acl'
 import PageLayout from 'components/GlobalPageLayout'
 import CompactListingCard from 'components/ListingCards/CompactListingCard'
+import LoadingContainer from 'components/LoadingContainer'
 
 import Listings from './Sections/Listings'
 
@@ -19,7 +21,8 @@ export function AgentNetwork() {
   useTitle('Agent Network | Rechat')
 
   const user = useSelector<IAppState, IUser>(state => state.user)
-  const [listings, setListings] = useState<ICompactListing[]>([])
+  const [listings, setListings] = useState<Nullable<ICompactListing[]>>(null)
+  const [isLoading, setIsLoading] = useLoadingEntities(listings)
   const brand = getActiveTeamId(user)
 
   useEffect(() => {
@@ -28,12 +31,17 @@ export function AgentNetwork() {
         return
       }
 
-      const brandListings = await getBrandListings(brand)
+      try {
+        const brandListings = await getBrandListings(brand)
 
-      setListings(brandListings)
+        setListings(brandListings)
+      } catch (error) {
+        setIsLoading(false)
+        console.error(error)
+      }
     }
     fetchBrandListings()
-  }, [brand])
+  }, [brand, setIsLoading])
 
   return (
     <Acl access={[ACL.MARKETING]}>
@@ -41,14 +49,27 @@ export function AgentNetwork() {
         <PageLayout.Header title="Agent Network" />
         <PageLayout.Main>
           <Listings onSeeAllClick={console.log} />
-          <br />
-          <Grid container spacing={3}>
-            {listings.map(listing => (
-              <Grid key={listing.id} container item xs={12} sm={6} md={4}>
-                <CompactListingCard listing={listing} onClick={console.log} />
-              </Grid>
-            ))}
-          </Grid>
+          <Box py={2}>
+            <Grid container spacing={3}>
+              {isLoading && (
+                <Grid container item xs alignItems="center" justify="center">
+                  <LoadingContainer noPaddings />
+                </Grid>
+              )}
+              {listings?.map(listing => (
+                <Grid key={listing.id} container item xs={12} sm={6} md={4}>
+                  <CompactListingCard listing={listing} onClick={console.log} />
+                </Grid>
+              ))}
+              {listings?.length === 0 && (
+                <Grid container item xs alignItems="center" justify="center">
+                  <Typography variant="h2">
+                    You don't have any listing to use.
+                  </Typography>
+                </Grid>
+              )}
+            </Grid>
+          </Box>
         </PageLayout.Main>
       </PageLayout>
     </Acl>
