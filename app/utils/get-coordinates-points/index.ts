02006e5caea83d3b17c626be14c9c1ac68755e99
OneDev0411 <cryptoapple1109@gmail.com>
2020-10-07 20:37:16 +0330

    refactor(utils): refactor get coordinates points helpers with TS (#4720)

---
+++ app/utils/get-coordinates-points/index.ts
@@ -0,0 +1,58 @@
+interface Point {
+  latitude: number
+  longitude: number
+}
+
+export function calculatePointsFromBounds(bounds: google.maps.LatLngBounds): Point[] {
+  const sw = bounds.getSouthWest()
+  const ne = bounds.getNorthEast()
+
+  const southWest = new window.google.maps.LatLng(sw.lat(), sw.lng())
+  const northEast = new window.google.maps.LatLng(ne.lat(), ne.lng())
+  const southEast = new window.google.maps.LatLng(sw.lat(), ne.lng())
+  const northWest = new window.google.maps.LatLng(ne.lat(), sw.lng())
+
+  return [northEast, northWest, southWest, southEast, northEast].map(point => ({
+    latitude: point.lat(),
+    longitude: point.lng()
+  }))
+}
+
+export function getMapBoundsInCircle(
+  center: google.maps.LatLng | google.maps.LatLngLiteral,
+  radius?: number,
+  returnBounds?: false
+): Point[]
+
+export function getMapBoundsInCircle(
+  center: google.maps.LatLng | google.maps.LatLngLiteral,
+  radius?: number,
+  returnBounds?: true
+): { points: Point[], bounds: google.maps.LatLngBounds }
+
+export function getMapBoundsInCircle(
+  center: google.maps.LatLng | google.maps.LatLngLiteral,
+  radius: number = 3,
+  returnBounds: boolean = false
+) {
+  if (!window.google) {
+    throw new Error('Google api not found!')
+  }
+
+  const circle = new window.google.maps.Circle({
+    center,
+    radius: radius * 1609.34
+  })
+
+  if (returnBounds) {
+    const bounds = circle.getBounds()
+
+    return {
+      bounds,
+      points: calculatePointsFromBounds(bounds)
+    }
+  }
+
+  return calculatePointsFromBounds(circle.getBounds())
+}
+
