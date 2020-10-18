78146e1c4e0e7b9d030e946bee677a845d5313c4
OneDev0411 <cryptoapple1109@gmail.com>
2020-10-19 01:35:57 +0300

    fix(create offer): don’t show optional contexts

--- app/components/Pages/Dashboard/Deals/create/create-offer.js
+++ app/components/Pages/Dashboard/Deals/create/create-offer.js
@@ -557,15 +557,17 @@ class CreateOffer extends React.Component {
                 onChangeDealStatus={status => this.changeDealStatus(status)}
               />
 
-              <Contexts
-                areContextsRequired={requiredFields.includes('contexts')}
-                hasError={this.hasError('contexts')}
-                contexts={contexts}
-                onChangeContext={(field, value) =>
-                  this.changeContext(field, value)
-                }
-                fields={dealContexts}
-              />
+              {contexts.length > 0 && requiredFields.includes('contexts') && (
+                <Contexts
+                  areContextsRequired={requiredFields.includes('contexts')}
+                  hasError={this.hasError('contexts')}
+                  contexts={contexts}
+                  onChangeContext={(field, value) =>
+                    this.changeContext(field, value)
+                  }
+                  fields={dealContexts}
+                />
+              )}
             </div>
           )}
         </div>
