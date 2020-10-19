03d0e7e5a542eb5685fcdd2153463c7ecf5fc7d3
OneDev0411 <cryptoapple1109@gmail.com>
2020-10-19 09:04:22 +0200

    chore(overview): updated chart

--- app/components/Pages/Dashboard/Overview/Chart.tsx
+++ app/components/Pages/Dashboard/Overview/Chart.tsx
@@ -7,63 +7,17 @@ import { scaleTime, scaleLinear } from '@visx/scale'
 import { AxisLeft, AxisBottom } from '@visx/axis'
 import { DateValue } from '@visx/mock-data/lib/generators/genDateValue'
 import { MarkerCircle } from '@visx/marker'
+import { timeFormat } from 'd3-time-format'
 
-const range = (start, stop, step) =>
-  Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step)
+const closedDeals = [1, 2, 0, 1, 1, 2, 1, 2, 1, 1]
 
-const years = [
-  { year: 2019, monthsRange: [1, 12] },
-  { year: 2020, monthsRange: [1, new Date().getMonth() + 1] }
-]
-
-const datesRange = years.map(item => {
-  const months = range(item.monthsRange[0], item.monthsRange[1], 1)
-
-  return {
-    year: item.year,
-    months
-  }
-})
-
-const dates: Date[] = []
-
-datesRange.forEach(dateRange => {
-  dateRange.months.forEach(month => {
-    const date = new Date(`${month}-${new Date().getDate()}-${dateRange.year}`)
-
-    dates.push(date)
-  })
-})
-
-const incomes = [
-  0,
-  20,
-  240,
-  700,
-  850,
-  1900,
-  2300,
-  3300,
-  3500,
-  4000,
-  4252,
-  4700,
-  5000,
-  5000,
-  5400,
-  5400,
-  5450,
-  6100,
-  6580,
-  6800,
-  6960,
-  7000.25
-]
-
-const data: DateValue[] = incomes.map((value, index) => ({
-  date: dates[index],
-  value: value * 1000
+const data: DateValue[] = closedDeals.map((value, index) => ({
+  date: new Date(`${index + 1}-01-2020`),
+  value
 }))
+
+const format = timeFormat('%b')
+const formatDate = (date: Date) => format(date)
 
 // data accessors
 const getX = (d: DateValue) => d.date
@@ -74,7 +28,7 @@ const xScale = scaleTime<number>({
   domain: extent(data, getX) as [Date, Date]
 })
 const yScale = scaleLinear<number>({
-  domain: [0, max(data, getY) as number]
+  domain: [0, (max(data, getY) + 1) as number]
 })
 
 type CurveProps = {
@@ -83,7 +37,7 @@ type CurveProps = {
 }
 
 export default function Chart({ width, height }: CurveProps) {
-  const margin = { top: 8, right: 16, bottom: 24, left: 56 }
+  const margin = { top: 8, right: 12, bottom: 24, left: 24 }
   const xMax = width - margin.left - margin.right
   const yMax = height - margin.top - margin.bottom
 
@@ -92,11 +46,21 @@ export default function Chart({ width, height }: CurveProps) {
 
   return (
     <svg width={width} height={height}>
-      <MarkerCircle id="marker-circle" fill="#0d5f75" size={1} refX={2} />
+      <MarkerCircle id="marker-circle" fill="#0d5f75" size={2} refX={2} />
       <Group left={margin.left} top={margin.top}>
         <rect width={width} height={height} fill="#fff" rx={14} ry={14} />
-        <AxisBottom top={yMax} scale={xScale} numTicks={width > 520 ? 10 : 5} />
-        <AxisLeft scale={yScale} numTicks={5} />
+        <AxisLeft
+          scale={yScale}
+          numTicks={4}
+          tickFormat={(d: number) => d.toString()}
+        />
+        <AxisBottom top={yMax} scale={xScale} tickFormat={formatDate} />
+        <text x="-60" y="15" transform="rotate(-90)" fontSize={10}>
+          Closed Deals
+        </text>
+        <text x={xMax - 28} y={yMax - 8} fontSize={12}>
+          2020
+        </text>
         <LinePath<DateValue>
           curve={curveNatural}
           data={data}
@@ -104,9 +68,11 @@ export default function Chart({ width, height }: CurveProps) {
           y={d => yScale(getY(d)) ?? 0}
           stroke="#0d9dbd"
           strokeWidth={2}
-          strokeOpacity={0.6}
+          strokeOpacity={1}
           shapeRendering="geometricPrecision"
+          markerEnd="url(#marker-circle)"
           markerMid="url(#marker-circle)"
+          markerStart="url(#marker-circle)"
         />
       </Group>
     </svg>
