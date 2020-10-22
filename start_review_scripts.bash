31f4570fa18b86534ae4525932eda24c337fa05f
OneDev0411 <cryptoapple1109@gmail.com>
2020-10-22 12:41:35 -0700

    fix(ci): fix the param again

--- start_review_scripts.bash
+++ start_review_scripts.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-curl -X POST https://api.heroku.com/apps -H "Accept: application/vnd.heroku+json; version=3" -H "Authorization: Bearer $HEROKU_API_KEY" -H "Content-Type: application/json" -d "{\"name\":\"rechat-$CI_PROJECT_NAME-$CI_COMMIT_REF_SLUG\",\"region\":\"us\", \"organization:name\":\"rechat\"}"
+curl -X POST https://api.heroku.com/apps -H "Accept: application/vnd.heroku+json; version=3" -H "Authorization: Bearer $HEROKU_API_KEY" -H "Content-Type: application/json" -d "{\"name\":\"rechat-$CI_PROJECT_NAME-$CI_COMMIT_REF_SLUG\",\"region\":\"us\", \"organization\":\"{\"name\":\"rechat\"}\"}"
 git checkout -B $CI_COMMIT_REF_SLUG
 git config user.email $GITLAB_USER_EMAIL
 git config user.name $GITLAB_USER_NAME
