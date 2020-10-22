e187b13a3da44de68ceb9bf0177027f490770ee7
OneDev0411 <cryptoapple1109@gmail.com>
2020-10-22 12:32:51 -0700

    ci(build): set organization

--- start_review_scripts.bash
+++ start_review_scripts.bash
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 
-curl -X POST https://api.heroku.com/apps -H "Accept: application/vnd.heroku+json; version=3" -H "Authorization: Bearer $HEROKU_API_KEY" -H "Content-Type: application/json" -d "{\"name\":\"$CI_PROJECT_NAME-$CI_COMMIT_REF_SLUG\",\"region\":\"eu\"}"
+curl -X POST https://api.heroku.com/apps -H "Accept: application/vnd.heroku+json; version=3" -H "Authorization: Bearer $HEROKU_API_KEY" -H "Content-Type: application/json" -d "{\"name\":\"rechat-$CI_PROJECT_NAME-$CI_COMMIT_REF_SLUG\",\"region\":\"us\", \"organization\":\"rechat\"}"
 git checkout -B $CI_COMMIT_REF_SLUG
 git config user.email $GITLAB_USER_EMAIL
 git config user.name $GITLAB_USER_NAME
 git add .
 git commit -m "Ready to push latest changes to new heroku review app"
-git push https://heroku:$HEROKU_API_KEY@git.heroku.com/$CI_PROJECT_NAME-$CI_COMMIT_REF_SLUG.git $CI_COMMIT_REF_SLUG:master
+git push https://heroku:$HEROKU_API_KEY@git.heroku.com/rechat-$CI_PROJECT_NAME-$CI_COMMIT_REF_SLUG.git $CI_COMMIT_REF_SLUG:master
