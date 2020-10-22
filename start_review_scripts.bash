f876b23415661038a1364221580659797016f5a1
OneDev0411 <cryptoapple1109@gmail.com>
2020-10-22 16:14:00 -0700

    fix(ci): fix the envs

--- start_review_scripts.bash
+++ start_review_scripts.bash
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 curl -X POST https://api.heroku.com/teams/apps -H "Accept: application/vnd.heroku+json; version=3" -H "Authorization: Bearer $HEROKU_API_KEY" -H "Content-Type: application/json" -d "{\"name\":\"rechat-$CI_PROJECT_NAME-$CI_COMMIT_REF_SLUG\",\"region\":\"us\", \"team\":\"rechat\"}"
-curl https://api.heroku.com/apps/rechat-irish/config-vars -H "Accept: application/vnd.heroku+json; version=3" -H "Authorization: Bearer $HEROKU_API_KEY" > .configs
-curl -X PATCH https://api.heroku.com/apps/$CI_PROJECT_NAME-$CI_COMMIT_REF_SLUG/config-vars -H "Accept: application/vnd.heroku+json; version=3" -H "Authorization: Bearer $HEROKU_API_KEY" -H 'Content-Type: application/json' -d '@./configs' > /dev/null
+curl https://api.heroku.com/apps/rechat-irish/config-vars -H "Accept: application/vnd.heroku+json; version=3" -H "Authorization: Bearer $HEROKU_API_KEY" > /tmp/.configs
+curl -X PATCH https://api.heroku.com/apps/$CI_PROJECT_NAME-$CI_COMMIT_REF_SLUG/config-vars -H "Accept: application/vnd.heroku+json; version=3" -H "Authorization: Bearer $HEROKU_API_KEY" -H 'Content-Type: application/json' -d '@/tmp/configs' > /dev/null
 git checkout -B $CI_COMMIT_REF_SLUG
 git push https://heroku:$HEROKU_API_KEY@git.heroku.com/rechat-$CI_PROJECT_NAME-$CI_COMMIT_REF_SLUG.git $CI_COMMIT_REF_SLUG:master
