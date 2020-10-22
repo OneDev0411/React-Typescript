2ebaee1e9328f396b9309becdc0604da2b413b86
OneDev0411 <cryptoapple1109@gmail.com>
2020-10-22 15:07:29 -0700

    fix(build): no need to commit yet

--- start_review_scripts.bash
+++ start_review_scripts.bash
@@ -2,8 +2,4 @@
 
 curl -X POST https://api.heroku.com/teams/apps -H "Accept: application/vnd.heroku+json; version=3" -H "Authorization: Bearer $HEROKU_API_KEY" -H "Content-Type: application/json" -d "{\"name\":\"rechat-$CI_PROJECT_NAME-$CI_COMMIT_REF_SLUG\",\"region\":\"us\", \"team\":\"rechat\"}"
 git checkout -B $CI_COMMIT_REF_SLUG
-git config user.email $GITLAB_USER_EMAIL
-git config user.name $GITLAB_USER_NAME
-git add .
-git commit -m "Ready to push latest changes to new heroku review app"
 git push https://heroku:$HEROKU_API_KEY@git.heroku.com/rechat-$CI_PROJECT_NAME-$CI_COMMIT_REF_SLUG.git $CI_COMMIT_REF_SLUG:master
