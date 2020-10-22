274691ee0e7b754cad6b04c08c493393645f6786
OneDev0411 <cryptoapple1109@gmail.com>
2020-10-22 16:12:52 -0700

    ci(cleanup): duplicated by mistake

--- start_review_scripts.bash
+++ start_review_scripts.bash
@@ -1,7 +1,6 @@
 #!/bin/bash -ex
 
 curl -X POST https://api.heroku.com/teams/apps -H "Accept: application/vnd.heroku+json; version=3" -H "Authorization: Bearer $HEROKU_API_KEY" -H "Content-Type: application/json" -d "{\"name\":\"rechat-$CI_PROJECT_NAME-$CI_COMMIT_REF_SLUG\",\"region\":\"us\", \"team\":\"rechat\"}"
-curl -X POST https://api.heroku.com/teams/apps -H "Accept: application/vnd.heroku+json; version=3" -H "Authorization: Bearer $HEROKU_API_KEY" -H "Content-Type: application/json" -d "{\"name\":\"rechat-$CI_PROJECT_NAME-$CI_COMMIT_REF_SLUG\",\"region\":\"us\", \"team\":\"rechat\"}"
 curl https://api.heroku.com/apps/rechat-irish/config-vars -H "Accept: application/vnd.heroku+json; version=3" -H "Authorization: Bearer $HEROKU_API_KEY" > .configs
 curl -X PATCH https://api.heroku.com/apps/$CI_PROJECT_NAME-$CI_COMMIT_REF_SLUG/config-vars -H "Accept: application/vnd.heroku+json; version=3" -H "Authorization: Bearer $HEROKU_API_KEY" -H 'Content-Type: application/json' -d '@./configs' > /dev/null
 git checkout -B $CI_COMMIT_REF_SLUG
