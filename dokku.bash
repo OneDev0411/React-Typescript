2ed230a26cc28c7cab8834af0416d0eb2b10061d
OneDev0411 <cryptoapple1109@gmail.com>
2020-10-23 22:16:48 -0700

    feat(ci): test comment

--- dokku.bash
+++ dokku.bash
@@ -28,9 +28,4 @@ ssh dokku@rechat.co letsencrypt $APP
 ssh dokku@rechat.co apps:unlock $APP
 # git push dokku@rechat.co:$APP $CI_COMMIT_REF_SLUG:master
 
-# CI_MERGE_REQUEST_IID=$(curl --request GET --header "Private-Token: $GITLAB_PERSONAL_API_PRIVATE_TOKEN" "https://gitlab.your-server.com/api/v4/projects/$CI_PROJECT_ID/repository/commits/$CI_COMMIT_SHA/merge_requests" --insecure | jq --raw-output ".[0].iid")
-# $ curl --request POST --header "Private-Token: aPs-aU-kL_YUtWiyD_QG" https://gitlab.com/api/v4/projects/$CI_MERGE_REQUEST_PROJECT_ID/merge_requests/11/notes --data "Comment test"
-
-echo "CI_MERGE_REQUEST_ID:  $CI_MERGE_REQUEST_ID"
-echo "CI_MERGE_REQUEST_IID: $CI_MERGE_REQUEST_IID"
-echo "CI_MERGE_REQUEST_PROJECT_ID: $CI_MERGE_REQUEST_PROJECT_ID"
+curl --request POST --header "Private-Token: $PRIVATE_TOKEN" https://gitlab.com/api/v4/projects/$CI_MERGE_REQUEST_PROJECT_ID/merge_requests/$CI_MERGE_REQUEST_IID/notes --data "Test Comment"
