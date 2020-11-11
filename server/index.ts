f222eae010424618bab8274c6ca3936eb9a25ecd
OneDev0411 <cryptoapple1109@gmail.com>
2020-11-11 18:37:56 +0300

    refactor(proxy): separate routes

--- server/index.ts
+++ server/index.ts
@@ -2,54 +2,26 @@ import path from 'path'
 import os from 'os'
 
 import throng from 'throng'
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import compress from 'compression'
 import cookieSession from 'cookie-session'
 import enforce from 'express-sslify'
-import bodyParser from 'body-parser'
+import timeout from 'connect-timeout'
 
 import webpack from 'webpack'
 import history from 'connect-history-api-fallback'
 import webpackDevMiddleware from 'webpack-dev-middleware'
 
-import proxifierRoute from './app/routes/proxifier'
-import userProfileRoute from './app/routes/user/profile'
-import usersLookupRoute from './app/routes/user/user-lookup'
-import usersOAuthTokenRoute from './app/routes/user/oauth-token'
-import signupRoute from './app/routes/user/signup'
-import dealDocusignLoginRoute from './app/routes/deal/docusign-login'
-import dealEnvelopeEditRoute from './app/routes/deal/envelope-edit'
-import dealEnvelopeSignRoute from './app/routes/deal/envelope-sign'
-import dealExportRoute from './app/routes/deal/export'
-import dealReportRoute from './app/routes/deal/report'
-import contactsExportOutlookRoute from './app/routes/contacts/export-outlook'
-import livebyReportRoute from './app/routes/liveby/report'
-import livebyNeighborhoodsRoute from './app/routes/liveby/neighborhoods'
-import myMarketingMattersPunchoutRoute from './app/routes/my-marketing-matters/punchout'
-import openHouseRoute from './app/routes/openhouse/registration'
-import getPdfSizeRoute from './app/routes/get-pdf-size'
-import refreshUserTokenRoute from './app/routes/refresh-token'
-import corsRoute from './app/routes/cors'
-import renderMjmlRoute from './app/routes/render-mjml'
-import urlMetadataRoute from './app/routes/url-metadata'
+import routes from './routes'
 
+const port = process.env.PORT || 8080
 const isProduction = process.env.NODE_ENV === 'production'
 const isDevelopment = !isProduction
 
 const app = express()
-const port = process.env.PORT || 8080
 
-const requestLimit = bodyParser.json({
-  limit: '10mb'
-})
-
+app.use(timeout('30s'))
 app.use(compress())
-
-if (isProduction) {
-  app.set('trust proxy', 1)
-  app.disable('x-powered-by')
-  app.use(enforce.HTTPS())
-}
 
 app.use(
   cookieSession({
@@ -60,71 +32,16 @@ app.use(
   })
 )
 
-/**
- * deals routes.
- */
-app.post('/api/proxifier', bodyParser.json(), proxifierRoute)
+// setup routes
+app.use('/', routes)
+app.use(haltOnTimedout)
 
-/**
- * user routes.
- */
-app.get('/api/users/profile', userProfileRoute)
-app.post('/api/users/lookup', bodyParser.json(), usersLookupRoute)
-app.post('/api/oauth2/token', bodyParser.json(), usersOAuthTokenRoute)
-app.post('/api/users', bodyParser.json(), signupRoute)
-
-/**
- * deals routes.
- */
-app.get('/api/deals/docusign/login', dealDocusignLoginRoute)
-app.get('/api/deals/envelope/:id/edit', dealEnvelopeEditRoute)
-app.get('/api/deals/envelope/:id/sign/:recipient', dealEnvelopeSignRoute)
-app.get('/api/deals/export/:brand', dealExportRoute)
-app.get('/api/deals/report/:data', dealReportRoute)
-
-/**
- * contacts routes.
- */
-app.post(
-  '/api/contacts/export/outlook/:brand',
-  bodyParser.json(),
-  contactsExportOutlookRoute
-)
-
-/**
- * liveby routes.
- */
-app.post('/api/liveby/report', requestLimit, livebyReportRoute)
-app.post('/api/liveby/neighborhoods', requestLimit, livebyNeighborhoodsRoute)
-
-/**
- * my marketing matters routes.
- */
-app.post(
-  '/api/my-marketing-matters/punchout',
-  requestLimit,
-  myMarketingMattersPunchoutRoute
-)
-
-/**
- * open house routes.
- */
-app.post('/openhouse/:id/:brand/register', openHouseRoute)
-
-/**
- * utility routes
- */
-app.get('/api/utils/cors/:url(.+)', corsRoute)
-app.post('/api/pdf/get-size', getPdfSizeRoute)
-app.post('/api/user/refresh-token', refreshUserTokenRoute)
-app.post('/api/utils/render-mjml', requestLimit, renderMjmlRoute)
-app.post('/api/utils/get-url-metadata', requestLimit, urlMetadataRoute)
+app.use(history())
 
 if (isDevelopment) {
   const config = require('../webpack/development').default
   const compiler = webpack(config)
 
-  app.use(history())
   app.use(
     webpackDevMiddleware(compiler, {
       publicPath: config.output.publicPath
@@ -135,7 +52,9 @@ if (isDevelopment) {
 }
 
 if (isProduction) {
-  app.use(history())
+  app.set('trust proxy', 1)
+  app.disable('x-powered-by')
+  app.use(enforce.HTTPS())
 
   app.use(
     '/',
@@ -143,6 +62,20 @@ if (isProduction) {
       maxAge: '7d'
     })
   )
+}
+
+function haltOnTimedout(
+  req: Request & {
+    timedout: boolean
+  },
+  res: Response,
+  next: NextFunction
+) {
+  if (req.timedout) {
+    console.error(`[ Timeout ] ${req.method}\t ${req.url}`)
+  }
+
+  next()
 }
 
 throng({
