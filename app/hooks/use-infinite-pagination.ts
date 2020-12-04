3a1b4a0bdad4dabbc8bbf848d6577fe6d8c7fb4f
OneDev0411 <cryptoapple1109@gmail.com>
2020-12-04 22:48:00 +0330

    feat(components): implement use infinite pagination (#4720)

---
+++ app/hooks/use-infinite-pagination.ts
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+import {
+  useInfiniteScroll,
+  InfiniteScrollingOptions
+} from 'hooks/use-infinite-scroll'
+
+interface UseInfinitePaginationOptions<T> {
+  items: T[]
+  pageSize?: number
+  infiniteScrollProps?: Omit<
+    InfiniteScrollingOptions,
+    'onScrollBottom' | 'onScrollTop'
+  >
+}
+
+export function useInfinitePagination<T>({
+  items,
+  pageSize = 12,
+  infiniteScrollProps = {
+    accuracy: 300,
+    debounceTime: 100
+  }
+}: UseInfinitePaginationOptions<T>): T[] {
+  const [loadedItemsCount, setLoadedItemsCount] = useState<number>(pageSize)
+
+  const loadNextPage = () =>
+    setLoadedItemsCount(loadedItemsCount => loadedItemsCount + pageSize)
+
+  useInfiniteScroll({
+    ...infiniteScrollProps,
+    onScrollBottom: loadNextPage
+  })
+
+  return items.slice(0, loadedItemsCount)
+}
