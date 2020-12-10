85f7fd85c47230bea1f5832e71a5e7dc73a6ea96
OneDev0411 <cryptoapple1109@gmail.com>
2020-12-10 17:21:35 +0300

    feat(forms): develop roles stamp (#4875)

--- app/components/Pages/Dashboard/Deals/FormEdit/utils/get-roles-text/index.ts
+++ app/components/Pages/Dashboard/Deals/FormEdit/utils/get-roles-text/index.ts
@@ -5,10 +5,16 @@ export function getRoleText(
   roles: IDealRole[],
   deal: IDeal,
   roleNames: string[],
-  annotation: any
+  annotation: IFormAnnotation
 ): string {
   const validRoles: string[] = normalizeRoleNames(deal, roleNames)
   const list: IDealRole[] = roles.filter(role => validRoles.includes(role.role))
+
+  if (annotation.attributes.includes('stamp')) {
+    const role = list[0]
+
+    return `${role.agent?.office?.name}\t\t${role.agent?.office?.address}\t\tPhone: ${role.agent?.office?.phone}\n${role.legal_full_name}`
+  }
 
   if (annotation.type === 'Roles') {
     return list
