a2881d733d166dcb226d393dfe7295e68f70a543
OneDev0411 <cryptoapple1109@gmail.com>
2020-12-10 20:25:41 -0800

    chore(sentry): enable tracing

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import * as Sentry from '@sentry/browser'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
 
 import Provider from './provider'
 
@@ -8,6 +9,7 @@ if (process.env.NODE_ENV === 'production') {
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
     release: process.env.SOURCE_VERSION,
+    integrations: [new Integrations.BrowserTracing()],
     beforeSend: (event, hint) => {
       console.log('[ Sentry ] ', hint.originalException)
 
