0c9f4ba3c9464274bec1aae5637b112e9c738e95
OneDev0411 <cryptoapple1109@gmail.com>
2020-12-10 20:31:47 -0800

    chore(sentry): disable tracing. we dont have it on our plan

--- app/index.js
+++ app/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import * as Sentry from '@sentry/react'
-import { Integrations } from '@sentry/tracing'
 
 import Provider from './provider'
 
@@ -9,7 +8,6 @@ if (process.env.NODE_ENV === 'production') {
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
     release: process.env.SOURCE_VERSION,
-    integrations: [new Integrations.BrowserTracing()],
     beforeSend: (event, hint) => {
       console.log('[ Sentry ] ', hint.originalException)
 
