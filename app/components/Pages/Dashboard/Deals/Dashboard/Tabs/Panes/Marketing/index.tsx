620f643b18668a455a8bcd81d27fa38058566867
OneDev0411 <cryptoapple1109@gmail.com>
2020-12-22 00:03:15 +0330

    fix(deals): restric deal marketing AN pane access to users with access (#4914)

--- app/components/Pages/Dashboard/Deals/Dashboard/Tabs/Panes/Marketing/index.tsx
+++ app/components/Pages/Dashboard/Deals/Dashboard/Tabs/Panes/Marketing/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { isSelling } from 'models/Deal/helpers/context'
 
+import Acl from 'components/Acl'
+
 import AgentNetwork from './AgentNetwork'
 import MyMarketingMatters from './MyMarketingMatters'
 import OpenHouse from './OpenHouse'
@@ -19,7 +21,9 @@ export default function MarketingPane({ deal, user }: Props) {
   return (
     <MarketingContainer>
       {deal.listing && <AgentNetwork deal={deal} />}
-      <MyMarketingMatters user={user} deal={deal} />
+      <Acl.AgentNetwork>
+        <MyMarketingMatters user={user} deal={deal} />
+      </Acl.AgentNetwork>
       {isSelling(deal) && deal.listing && <OpenHouse deal={deal} user={user} />}
       <EmailMarketing deal={deal} user={user} />
       <SocialMarketing deal={deal} user={user} />
