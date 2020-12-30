5bc5808a65a4153093b1dca26348278cf916202f
OneDev0411 <cryptoapple1109@gmail.com>
2020-12-30 19:53:44 +0330

    refactor: remove DropdownButton and MenuItem (#4434)

--- app/components/Pages/Dashboard/Chatroom/Rooms/settings.js
+++ app/components/Pages/Dashboard/Chatroom/Rooms/settings.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
-import { DropdownButton, MenuItem } from 'react-bootstrap'
 import { mdiLogout } from '@mdi/js'
-import { Box, Tooltip } from '@material-ui/core'
+import {
+  Box,
+  Tooltip,
+  MenuItem,
+  MenuList,
+  Popover,
+  makeStyles
+} from '@material-ui/core'
 
 import { SvgIcon } from 'components/SvgIcons/SvgIcon'
 import { muiIconSizes } from 'components/SvgIcons/icon-sizes'
@@ -10,28 +16,57 @@ import { muiIconSizes } from 'components/SvgIcons/icon-sizes'
 import Chatroom from '../Util/chatroom'
 import MoreIcon from '../../Partials/Svgs/MoreIcon'
 
-const RoomSettings = ({ user, room, iconSize = 17 }) => (
-  <Tooltip placement="bottom" title="More Items">
-    <DropdownButton
-      id="drp-room-settings"
-      className="room-settings"
-      noCaret
-      pullRight
-      title={
-        <span className="icon settings">
+const useStyles = makeStyles(theme => ({
+  paper: { marginTop: theme.spacing(1) }
+}))
+
+const RoomSettings = ({ user, room, iconSize = 17 }) => {
+  const classes = useStyles()
+  const [anchorEl, setAnchorEl] = useState(null)
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  return (
+    <>
+      <Tooltip placement="bottom" title="More Items">
+        <span className="icon settings" onClick={handleClick}>
           <MoreIcon height={iconSize} />
         </span>
-      }
-    >
-      <MenuItem eventKey="1" onClick={() => Chatroom.leaveRoom(user.id, room)}>
-        <Box display="flex" alignItems="center">
-          <SvgIcon path={mdiLogout} size={muiIconSizes.small} />
-          <Box ml={1}>Leave this chat</Box>
-        </Box>
-      </MenuItem>
-    </DropdownButton>
-  </Tooltip>
-)
+      </Tooltip>
+      <Popover
+        id="drp-room-settings"
+        className="room-settings"
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right'
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right'
+        }}
+        classes={{ paper: classes.paper }}
+      >
+        <MenuList>
+          <MenuItem onClick={() => Chatroom.leaveRoom(user.id, room)}>
+            <Box display="flex" alignItems="center">
+              <SvgIcon path={mdiLogout} size={muiIconSizes.small} />
+              <Box ml={1}>Leave this chat</Box>
+            </Box>
+          </MenuItem>
+        </MenuList>
+      </Popover>
+    </>
+  )
+}
 
 export default connect(s => ({
   user: s.data.user
