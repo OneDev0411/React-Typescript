b3baeb7268bf9891d080c77b4124105fe33cdbd9
OneDev0411 <cryptoapple1109@gmail.com>
2020-12-30 13:28:41 +0330

    fix(mls): fix type errors due to our new user selector

--- app/hooks/use-log-user-activity.ts
+++ app/hooks/use-log-user-activity.ts
@@ -1,11 +1,11 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-import { IAppState } from 'reducers'
+import { selectUserUnsafe } from 'selectors/user'
 import logUserActivity from 'models/user/post-new-activity'
 
 export function useLogUserActivity(activityObjectId?: UUID) {
-  const user = useSelector<IAppState, IUser>((state: IAppState) => state.user)
+  const user = useSelector(selectUserUnsafe)
 
   React.useEffect(() => {
     if (activityObjectId && user) {
