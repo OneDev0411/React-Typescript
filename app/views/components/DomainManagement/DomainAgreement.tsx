76a5de6f68f1f1d240a63e12edd716806e31910b
OneDev0411 <cryptoapple1109@gmail.com>
2021-01-15 17:19:31 +0330

    refactor(website): upgrade the agreement styles (#4933)

--- app/views/components/DomainManagement/DomainAgreement.tsx
+++ app/views/components/DomainManagement/DomainAgreement.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { Box, Button } from '@material-ui/core'
+import { Box, Button, makeStyles } from '@material-ui/core'
 
 import { useWizardForm } from 'components/QuestionWizard/use-context'
 import {
@@ -10,9 +10,26 @@ import {
 import useAsync from 'hooks/use-async'
 import getDomainAgreements from 'models/domains/get-domain-agreements'
 
+const useStyles = makeStyles(
+  theme => ({
+    agreement: {
+      maxHeight: 10 * 19,
+      overflowX: 'hidden',
+      overflowY: 'auto',
+      marginBottom: theme.spacing(2),
+      backgroundColor: 'rgba(0, 0, 0, 0.09)',
+      padding: '12px 10px',
+      borderTopLeftRadius: theme.shape.borderRadius,
+      borderTopRightRadius: theme.shape.borderRadius
+    }
+  }),
+  { name: 'DomainAgreement' }
+)
+
 interface DomainAgreementProps {
   domainName: string
   onNextClick: (agreementKeys: string[]) => void
+  disabled: boolean
   step?: number // TODO: Remove this
 }
 
@@ -21,8 +38,10 @@ const defaultAgreementList: IDomainAgreement[] = []
 function DomainAgreement({
   step,
   domainName,
-  onNextClick
+  onNextClick,
+  disabled
 }: DomainAgreementProps) {
+  const classes = useStyles()
   const wizard = useWizardForm()
   const { run, data: agreements, isLoading } = useAsync({
     data: defaultAgreementList
@@ -45,25 +64,31 @@ function DomainAgreement({
     <QuestionSection step={step}>
       <QuestionTitle>Domain Registration Agreement</QuestionTitle>
       <QuestionForm>
-        <Box>
-          {isLoading ? (
-            'loading...'
-          ) : (
-            <>
+        {isLoading ? (
+          'loading...'
+        ) : (
+          <>
+            <Box>
               {agreements.map(agreement => (
                 <div
                   key={agreement.agreementKey}
+                  className={classes.agreement}
                   dangerouslySetInnerHTML={{ __html: agreement.content }}
                 />
               ))}
-            </>
-          )}
-        </Box>
-        <Box marginTop={3}>
-          <Button onClick={handleNext} variant="contained" color="secondary">
-            Next
-          </Button>
-        </Box>
+            </Box>
+            <Box marginTop={3}>
+              <Button
+                onClick={handleNext}
+                variant="contained"
+                color="secondary"
+                disabled={disabled}
+              >
+                Next
+              </Button>
+            </Box>
+          </>
+        )}
       </QuestionForm>
     </QuestionSection>
   )
