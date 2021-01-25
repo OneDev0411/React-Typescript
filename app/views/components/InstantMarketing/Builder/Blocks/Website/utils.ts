0a057ea0770ec1203913b028183f60aad794ba5a
OneDev0411 <cryptoapple1109@gmail.com>
2021-01-25 20:25:39 +0330

    feat(website): implement handleBlockDragStopEvent logic (#4933)

--- app/views/components/InstantMarketing/Builder/Blocks/Website/utils.ts
+++ app/views/components/InstantMarketing/Builder/Blocks/Website/utils.ts
@@ -1,3 +1,11 @@
+import { Model } from 'backbone'
+
+import { Editor } from 'grapesjs'
+
+import nunjucks from 'components/InstantMarketing/helpers/nunjucks'
+
+import { TemplateRenderData } from '../../utils/get-template-render-data'
+
 export const isComponent = (type: string) => (el: HTMLElement) =>
   el.dataset?.type === type
 
@@ -8,3 +16,67 @@ export const baseView = (blockClassNames: string | undefined) => ({
     }
   }
 })
+
+export function handleBlockDragStopEvent(
+  editor: Editor,
+  templates: Record<string, string>,
+  renderData: TemplateRenderData
+): void
+export function handleBlockDragStopEvent<T>(
+  editor: Editor,
+  templates: Record<string, string>,
+  renderData: TemplateRenderData | ((selectedItem: T) => TemplateRenderData),
+  onDrop: (model: Model) => void
+): { selectHandler: (selectedItem?: T) => void }
+export function handleBlockDragStopEvent<T>(
+  editor: Editor,
+  templates: Record<string, string>,
+  renderData: TemplateRenderData | ((selectedItem: T) => TemplateRenderData),
+  onDrop?: (model: Model) => void
+) {
+  function appendBlock(model: any, renderData: TemplateRenderData) {
+    const template = templates[model.attributes.attributes['data-block']]
+
+    if (template) {
+      const mjml = nunjucks.renderString(template, renderData)
+
+      model.parent().append(mjml, { at: model.opt.at })
+    }
+
+    model.remove()
+  }
+
+  let modelHandle: any
+
+  const selectHandler = (selectedItem?: T) => {
+    if (!modelHandle) {
+      return
+    }
+
+    if (selectedItem) {
+      appendBlock(
+        modelHandle,
+        typeof renderData === 'function' ? renderData(selectedItem) : renderData
+      )
+    }
+
+    modelHandle = null
+  }
+
+  editor.on('block:drag:stop', (model: Model, block: any) => {
+    if (!model || !templates[block.id]) {
+      return
+    }
+
+    if (onDrop) {
+      modelHandle = model
+      onDrop(model)
+    } else if (typeof renderData !== 'function') {
+      appendBlock(model, renderData)
+    }
+  })
+
+  return {
+    selectHandler
+  }
+}
