7fce0cce898908bdbdb4f8fc5c5c291f2676cd0d
OneDev0411 <cryptoapple1109@gmail.com>
2021-01-26 10:22:27 +0330

    feat(dashboard): add gmailOrOutlookSynced property to appcues user data

--- app/services/appcues/use-appcues.ts
+++ app/services/appcues/use-appcues.ts
@@ -3,7 +3,10 @@ import { useSelector } from 'react-redux'
 
 import { selectUserUnsafe } from 'selectors/user'
 
+import { getOAuthAccounts } from 'models/o-auth-accounts/get-o-auth-accounts'
+
 import { IAppState } from 'reducers'
+import { OAuthProvider } from 'constants/contacts'
 
 interface Location {
   pathname: string
@@ -22,14 +25,23 @@ export function useAppcues() {
       return
     }
 
-    if (user && user.id) {
-      // Behind the scenes, this call also invokes Appcues.page()
-      // More: https://docs.appcues.com/article/161-javascript-api
-      window.Appcues.identify(user.id, {
-        firstName: user.first_name,
-        lastName: user.last_name,
-        userType: user.user_type
-      })
-    }
+    ;(async function prepareAndSendUserData() {
+      const google = await getOAuthAccounts(OAuthProvider.Google)
+      const outlook = await getOAuthAccounts(OAuthProvider.Outlook)
+
+      if (user && user.id) {
+        const userData = {
+          firstName: user.first_name,
+          lastName: user.last_name,
+          userType: user.user_type,
+          gmailOrOutlookSynced: Boolean(google.length || outlook.length)
+        }
+
+        console.log(userData)
+        // Behind the scenes, this call also invokes Appcues.page()
+        // More: https://docs.appcues.com/article/161-javascript-api
+        window.Appcues.identify(user.id, userData)
+      }
+    })()
   }, [pathname, user])
 }
