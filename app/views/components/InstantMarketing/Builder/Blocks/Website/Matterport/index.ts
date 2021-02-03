7495a4ded15891cba9b54d9c106dbb387141c512
OneDev0411 <cryptoapple1109@gmail.com>
2021-02-03 15:56:53 +0330

    feat(website): implement the fallback icon for matterport (#4933)

--- app/views/components/InstantMarketing/Builder/Blocks/Website/Matterport/index.ts
+++ app/views/components/InstantMarketing/Builder/Blocks/Website/Matterport/index.ts
@@ -37,6 +37,9 @@ export interface MatterportBlockOptions {
 
 const isMatterportComponent = isComponent(typeEmbedMatterport)
 
+const svgAttrs =
+  'xmlns="http://www.w3.org/2000/svg" width="100" viewBox="0 0 24 24" style="fill: rgba(0,0,0,0.15); transform: scale(0.75); margin: 0 auto"'
+
 export default function registerMatterportBlock(
   editor: Editor,
   renderData: TemplateRenderData,
@@ -55,12 +58,11 @@ export default function registerMatterportBlock(
           tagName: 'iframe',
           void: 0,
           modelId: '',
+          fallback: `<svg ${svgAttrs}>
+            <path d="M2.28 3L1 4.27l2 2V19c0 1.1.9 2 2 2h12.73l2 2L21 21.72 2.28 3m2.55 0L21 19.17V5a2 2 0 0 0-2-2H4.83M8.5 13.5l2.5 3 1-1.25L14.73 18H5l3.5-4.5z"></path>
+          </svg>`,
           attributes: {
-            allowfullscreen: 'allowfullscreen',
-            style: {
-              width: '100%',
-              height: '480px'
-            }
+            allowfullscreen: 'allowfullscreen'
           }
         },
         initialize(...args: any[]) {
@@ -96,7 +98,11 @@ export default function registerMatterportBlock(
          * @private
          */
         getSrc() {
-          // TODO: See all the available URL parameters here:
+          if (!this.get('modelId')) {
+            return this.getSrcResult({ fallback: 1 })
+          }
+
+          // You can see all the available URL parameters here:
           // https://support.matterport.com/hc/en-us/articles/209980967-URL-Parameters
           return [
             'http://my.matterport.com/show/?',
@@ -138,17 +144,24 @@ export default function registerMatterportBlock(
         el.style.height = '100%'
         el.style.width = '100%'
 
+        this.iframeEl = el
+
         return el
       },
-      updateMatterport() {
-        this.model.trigger('change:modelId')
+      updateSrc() {
+        const { model, iframeEl } = this
+
+        if (!iframeEl) {
+          return
+        }
+
+        iframeEl.src = model.get('src')
       },
       render(...args) {
         ImageView.prototype.render.apply(this, args)
         this.updateClasses()
 
         this.el.appendChild(this.renderIframe())
-        this.updateMatterport()
 
         return this
       },
