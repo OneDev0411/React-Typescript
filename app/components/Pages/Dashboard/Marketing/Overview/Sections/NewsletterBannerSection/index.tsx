f256348284ad77e09f83af99b3bf5f803d3db67c
OneDev0411 <cryptoapple1109@gmail.com>
2021-02-19 17:09:25 +0330

    refactor(mc): replace newsletter banner image with a component (#4969)

--- app/components/Pages/Dashboard/Marketing/Overview/Sections/NewsletterBannerSection/index.tsx
+++ app/components/Pages/Dashboard/Marketing/Overview/Sections/NewsletterBannerSection/index.tsx
@@ -1,17 +1,32 @@
 import React from 'react'
-import { Grid, Theme, makeStyles } from '@material-ui/core'
+import {
+  Grid,
+  Box,
+  Typography,
+  Theme,
+  makeStyles,
+  Button
+} from '@material-ui/core'
 
 import Link from 'components/ALink'
 
 const useStyles = makeStyles(
   (theme: Theme) => ({
     container: {
-      width: '100%',
-      marginBottom: theme.spacing(4)
+      backgroundColor: theme.palette.tertiary.main,
+      borderRadius: theme.shape.borderRadius,
+      margin: theme.spacing(4, 1),
+      padding: theme.spacing(0, 4)
     },
     image: {
       width: '100%',
       borderRadius: theme.shape.borderRadius
+    },
+    title: {
+      color: theme.palette.common.white
+    },
+    subtitle: {
+      color: theme.palette.warning.ultralight
     }
   }),
   {
@@ -23,14 +38,35 @@ export default function NewsletterBannerSection() {
   const classes = useStyles()
 
   return (
-    <Grid item className={classes.container}>
-      <Link noStyle to="/dashboard/marketing/Newsletter">
+    <Grid
+      container
+      alignItems="center"
+      direction="row"
+      className={classes.container}
+    >
+      <Grid item xs={8}>
         <img
           alt="newsletter"
-          src="/static/images/marketing/overview/newsletter-banner.png"
-          className={classes.image}
+          src="/static/images/marketing/overview/newsletter.png"
         />
-      </Link>
+      </Grid>
+      <Grid item xs={4}>
+        <Box pl={6}>
+          <Typography variant="h4" className={classes.title}>
+            Make A Newsletter
+          </Typography>
+          <Typography variant="h5" className={classes.subtitle}>
+            Using our powerful yet easy to use block editor.
+          </Typography>
+          <Box pt={2}>
+            <Link noStyle to="/dashboard/marketing/Newsletter">
+              <Button color="primary" variant="outlined">
+                Start Here
+              </Button>
+            </Link>
+          </Box>
+        </Box>
+      </Grid>
     </Grid>
   )
 }
