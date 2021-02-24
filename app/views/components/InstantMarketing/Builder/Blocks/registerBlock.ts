323cc6c5a32e04ff757bb34edbdfac97b5249265
OneDev0411 <cryptoapple1109@gmail.com>
2021-02-24 21:07:41 +0330

    feat(website): fix the drag and drop limitation issues (#4933)

--- app/views/components/InstantMarketing/Builder/Blocks/registerBlock.ts
+++ app/views/components/InstantMarketing/Builder/Blocks/registerBlock.ts
@@ -9,7 +9,9 @@ function registerBlock(
   { label, category, blockName, template, icon, adaptive = false }: BlockOptions
 ): void {
   const document = domParser.parseFromString(template, 'text/html')
-  const { tagName } = document.body.children[0]
+  const firstChild = document.body.children[0]
+  const { tagName } = firstChild
+  const dataType = firstChild.getAttribute('data-type')
 
   const elementName = adaptive
     ? 'mj-adaptive'
@@ -22,7 +24,9 @@ function registerBlock(
     label: icon
       ? `<div style="background-image:url(${icon});">${label}</div>`
       : label,
-    content: `<${elementName} data-block="${blockName}"></${elementName}>`
+    content: `<${elementName} data-block="${blockName}" ${
+      dataType ? `data-type="${dataType}"` : ''
+    }></${elementName}>`
   })
 }
 
