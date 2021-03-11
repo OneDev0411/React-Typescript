c7d9ab93f3b06432f0301e738ae857912732bbf7
OneDev0411 <cryptoapple1109@gmail.com>
2021-03-11 19:22:43 +0330

    feat(showings): implement the contained version of chip (#5006)

--- app/views/components/Chip/index.tsx
+++ app/views/components/Chip/index.tsx
@@ -5,12 +5,14 @@ import type { ChipProps as MChipProps } from '@material-ui/core'
 import useStyles from './styles'
 
 type ChipColor = 'green' | 'orange' | 'red' | 'black'
+type ChipVariant = 'text' | 'contained'
 
 export interface ChipProps extends Pick<MChipProps, 'label'> {
   color?: ChipColor
+  variant?: ChipVariant
 }
 
-function Chip({ label, color = 'black' }: ChipProps) {
+function Chip({ label, color = 'black', variant = 'text' }: ChipProps) {
   const classes = useStyles()
 
   const colorClassNames: Record<ChipColor, string> = {
@@ -21,7 +23,13 @@ function Chip({ label, color = 'black' }: ChipProps) {
   }
 
   return (
-    <span className={classNames(classes.root, colorClassNames[color])}>
+    <span
+      className={classNames(
+        classes.root,
+        colorClassNames[color],
+        variant === 'text' ? classes.text : classes.contained
+      )}
+    >
       {label}
     </span>
   )
--- app/views/components/Chip/styles.ts
+++ app/views/components/Chip/styles.ts
@@ -3,10 +3,27 @@ import { makeStyles } from '@material-ui/core'
 export default makeStyles(
   theme => ({
     root: { ...theme.typography.body3 },
-    green: { color: theme.palette.success.main },
-    orange: { color: theme.palette.warning.main },
-    red: { color: theme.palette.error.main },
-    black: { color: theme.palette.text.primary }
+    green: {},
+    orange: {},
+    red: {},
+    black: {},
+    text: {
+      '&$green': { color: theme.palette.success.main },
+      '&$orange': { color: theme.palette.warning.main },
+      '&$red': { color: theme.palette.error.main },
+      '&$black': { color: theme.palette.text.primary }
+    },
+    contained: {
+      color: theme.palette.common.white,
+      lineHeight: `${theme.spacing(3)}px`,
+      display: 'inline-block',
+      borderRadius: theme.spacing(3),
+      padding: theme.spacing(0, 0.5),
+      '&$green': { backgroundColor: theme.palette.success.main },
+      '&$orange': { backgroundColor: theme.palette.warning.main },
+      '&$red': { backgroundColor: theme.palette.error.main },
+      '&$black': { backgroundColor: theme.palette.text.primary }
+    }
   }),
   { name: 'Chip' }
 )
