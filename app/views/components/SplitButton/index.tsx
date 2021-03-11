e5fb6070cc99227f302b23b48a816ecd69a6a19d
OneDev0411 <cryptoapple1109@gmail.com>
2021-03-11 14:12:11 +0330

    fix(showings): fix style of arrow button on small size (#5006)

--- app/views/components/SplitButton/index.tsx
+++ app/views/components/SplitButton/index.tsx
@@ -1,14 +1,11 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, ComponentType } from 'react'
 import {
   Button,
   ButtonGroup,
   ClickAwayListener,
-  createStyles,
   Grow,
-  makeStyles,
   Paper,
   Popper,
-  Theme,
   useTheme
 } from '@material-ui/core'
 import { PopperPlacementType } from '@material-ui/core/Popper'
@@ -19,16 +16,18 @@ import { SvgIcon } from 'components/SvgIcons/SvgIcon'
 
 import { ClassesProps } from 'utils/ts-utils'
 
-interface RenderMenuProps {
+import useStyles from './styles'
+
+export interface RenderMenuProps {
   closeMenu: (event?: React.MouseEvent<any>) => void
 }
 
-interface Props {
+interface Props extends ClassesProps<typeof useStyles> {
   color?: 'inherit' | 'primary' | 'secondary' | 'default'
   children: ReactNode
   disabled?: boolean
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
-  renderMenu: (props: RenderMenuProps) => ReactNode
+  RenderMenu: ComponentType<RenderMenuProps>
   popperPlacement?: PopperPlacementType
   className?: string
   size?: 'small' | 'medium' | 'large'
@@ -36,17 +35,7 @@ interface Props {
   variant?: 'contained' | 'outlined' | undefined
 }
 
-const styles = (theme: Theme) =>
-  createStyles({
-    mainButton: {
-      flex: 1
-    }
-  })
-const useStyles = makeStyles(styles, { name: 'SplitButton' })
-
-export default function SplitButton(
-  props: Props & ClassesProps<typeof styles>
-) {
+export default function SplitButton(props: Props) {
   const [isOpen, setIsOpen] = React.useState(false)
   const anchorRef = React.useRef<HTMLDivElement>(null)
   const theme = useTheme()
@@ -84,6 +73,7 @@ export default function SplitButton(
           aria-haspopup="true"
           onClick={handleToggle}
           size="small"
+          className={props.size === 'small' ? classes.smallArrow : undefined}
         >
           <SvgIcon path={mdiChevronDown} />
         </Button>
@@ -103,11 +93,13 @@ export default function SplitButton(
                 placement === 'bottom' ? 'center top' : 'center bottom'
             }}
           >
-            <ClickAwayListener onClickAway={handleClose}>
-              <Paper id="menu-list-grow">
-                {props.renderMenu({ closeMenu: handleClose })}
-              </Paper>
-            </ClickAwayListener>
+            <div>
+              <ClickAwayListener onClickAway={handleClose}>
+                <Paper id="menu-list-grow">
+                  <props.RenderMenu closeMenu={handleClose} />
+                </Paper>
+              </ClickAwayListener>
+            </div>
           </Grow>
         )}
       </Popper>
