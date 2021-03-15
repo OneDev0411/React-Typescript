b04fd749b0ae9e2799c1ef543dd048eee7f48212
OneDev0411 <cryptoapple1109@gmail.com>
2021-03-15 12:18:00 +0330

    feat(showings): define the LinkButton component (#5006)

---
+++ app/components/Pages/Dashboard/Showings/components/LinkButton/index.tsx
@@ -0,0 +1,5 @@
+import { Button } from '@material-ui/core'
+
+import withLinkButton from './with-link-button'
+
+export default withLinkButton(Button)
---
+++ app/components/Pages/Dashboard/Showings/components/LinkButton/with-link-button.tsx
@@ -0,0 +1,27 @@
+import React, { forwardRef, ComponentType, Ref } from 'react'
+
+import { Link, LinkProps } from 'react-router'
+
+export type LinkButtonProps = Partial<
+  Pick<LinkProps, 'to' | 'target' | 'rel' | 'href'>
+>
+
+function withLinkButton<T extends LinkButtonProps>(
+  Component: ComponentType<T>
+) {
+  return forwardRef(function LinkButton(
+    { to, ...other }: T & LinkButtonProps,
+    ref: Ref<unknown>
+  ) {
+    const moreProps = to
+      ? {
+          component: Link,
+          to
+        }
+      : {}
+
+    return <Component ref={ref} {...(other as T)} {...moreProps} />
+  })
+}
+
+export default withLinkButton
