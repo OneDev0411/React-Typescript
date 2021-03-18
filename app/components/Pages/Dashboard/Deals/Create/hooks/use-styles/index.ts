089054890914f9227e34e81654ca74e167dd6c31
OneDev0411 <cryptoapple1109@gmail.com>
2021-03-18 18:42:48 +0300

    fix(deals): remove deprecated style

--- app/components/Pages/Dashboard/Deals/Create/hooks/use-styles/index.ts
+++ app/components/Pages/Dashboard/Deals/Create/hooks/use-styles/index.ts
@@ -2,16 +2,6 @@ import { makeStyles } from '@material-ui/core'
 
 export const useStyles = makeStyles(
   () => ({
-    container: {
-      position: 'fixed',
-      left: 0,
-      right: 0,
-      top: 0,
-      bottom: 0,
-      zIndex: 100,
-      overflowY: 'scroll',
-      backgroundColor: '#fff'
-    },
     root: {
       width: '80%',
       maxWidth: '800px',
