20a60ba14cc70b501f5ec15effa180740feec430
OneDev0411 <cryptoapple1109@gmail.com>
2021-03-22 13:51:07 +0430

    feat(showings): create some basic files for property step (#5006)

---
+++ app/components/Pages/Dashboard/Showings/components/ShowingStepProperty/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { QuestionSection, QuestionTitle } from 'components/QuestionWizard'
+
+function ShowingStepProperty() {
+  return (
+    <QuestionSection>
+      <QuestionTitle>What is the address for the property?</QuestionTitle>
+    </QuestionSection>
+  )
+}
+
+export default ShowingStepProperty
