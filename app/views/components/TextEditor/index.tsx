4e08fe286bb26e3b1f02d011abaf276cafd0dba8
OneDev0411 <cryptoapple1109@gmail.com>
2021-03-22 18:11:22 +0300

    fix: react-avatar damaged package

--- app/views/components/TextEditor/index.tsx
+++ app/views/components/TextEditor/index.tsx
@@ -6,7 +6,7 @@ import React, {
   useMemo,
   useRef
 } from 'react'
-import Dropzone from 'react-dropzone'
+import Dropzone from 'react-dropzone-rechat'
 import { EditorState } from 'draft-js'
 import PluginsEditor from 'draft-js-plugins-editor'
 import cn from 'classnames'
@@ -139,9 +139,9 @@ export const TextEditor = forwardRef(
 
     useEffect(() => {}, [])
 
-    const dropzoneProps: Partial<ComponentType<
-      typeof Dropzone
-    >> = getDropzoneProps({
+    const dropzoneProps: Partial<
+      ComponentType<typeof Dropzone>
+    > = getDropzoneProps({
       disabled: !onAttachmentDropped,
       accept: onAttachmentDropped ? '*/*' : undefined,
       onDrop: (files: File[]) => {
