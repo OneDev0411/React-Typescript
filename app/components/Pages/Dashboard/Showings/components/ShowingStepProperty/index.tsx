ea70886c53a1c2e06d7c481470fd2ce068832807
OneDev0411 <cryptoapple1109@gmail.com>
2021-03-24 11:34:25 +0430

    feat(showings): temp commit (#5006)

--- app/components/Pages/Dashboard/Showings/components/ShowingStepProperty/index.tsx
+++ app/components/Pages/Dashboard/Showings/components/ShowingStepProperty/index.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { QuestionSection, QuestionTitle } from 'components/QuestionWizard'
+import {
+  QuestionSection,
+  QuestionTitle,
+  QuestionForm
+} from 'components/QuestionWizard'
+import ListingsAndPlacesSearchInput from 'components/ListingsAndPlacesSearchInput'
 
 function ShowingStepProperty() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [deal, setDeal] = useState()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [listing, setListing] = useState()
+
   return (
     <QuestionSection>
       <QuestionTitle>What is the address for the property?</QuestionTitle>
+      <QuestionForm>
+        <ListingsAndPlacesSearchInput
+          onSelect={result => console.log('onSelect', result)}
+        />
+      </QuestionForm>
     </QuestionSection>
   )
 }
