faf5b0fb1249b79b3ca63428410bf33131442296
OneDev0411 <cryptoapple1109@gmail.com>
2021-03-29 21:05:27 +0430

    feat(showings): fix the container typing issue (#5006)

--- app/views/components/TabContentSwitch/Container.tsx
+++ app/views/components/TabContentSwitch/Container.tsx
@@ -1,20 +1,25 @@
-import { Children, ReactElement } from 'react'
+import React, { Children, ReactElement } from 'react'
 
 import type { ItemProps } from './Item'
 
-type Child<T> = ReactElement<ItemProps<T>> | undefined
+type Item<T> = ReactElement<ItemProps<T>>
+type ItemOrBoolean<T> = Item<T> | boolean
 
 export interface ContainerProps<T = unknown> {
   value: T
-  children: Child<T> | (Child<T> | false)[]
+  children: ItemOrBoolean<T> | ItemOrBoolean<T>[]
 }
 
 function Container<T = unknown>({ value, children }: ContainerProps<T>) {
-  return (
-    Children.toArray(children).find(
-      child => !!child && value === child!.props.value
-    ) || null
-  )
+  const child = Children.toArray(children).find(child => {
+    // I know the children type but `.toArray()` is not a generic method, so
+    // I have to use the below type cast to fix the type issues
+    const item = child as ItemOrBoolean<T>
+
+    return !!item && typeof item !== 'boolean' && value === item.props.value
+  })
+
+  return <>{child}</>
 }
 
 export default Container
