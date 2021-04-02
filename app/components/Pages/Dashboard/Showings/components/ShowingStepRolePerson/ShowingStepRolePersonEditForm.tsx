be66af35e239bfb5aaba2201e740972f2058cc93
OneDev0411 <cryptoapple1109@gmail.com>
2021-04-02 18:50:17 +0430

    feat(showings): implement the base structure of edit form (#5006)

--- app/components/Pages/Dashboard/Showings/components/ShowingStepRolePerson/ShowingStepRolePersonEditForm.tsx
+++ app/components/Pages/Dashboard/Showings/components/ShowingStepRolePerson/ShowingStepRolePersonEditForm.tsx
@@ -1,23 +1,59 @@
 import React from 'react'
+import { Box, Button, Typography } from '@material-ui/core'
+
+import { Form } from 'react-final-form'
+
+import { FormTextField, FormPhoneField } from 'components/final-form-fields'
 
 import { ShowingRolePerson } from '../../types'
 
 interface ShowingStepRolePersonEditFormProps {
+  roleType: IShowingRoleType
   initialData: ShowingRolePerson
   onSubmit: (data: ShowingRolePerson) => void
+  submitLabel?: string
 }
 
 function ShowingStepRolePersonEditForm({
+  roleType,
   initialData,
-  onSubmit
+  onSubmit,
+  submitLabel = 'Next'
 }: ShowingStepRolePersonEditFormProps) {
+  const handleSubmit = e => {
+    console.log('handleSubmit', e.values)
+  }
+
   return (
-    <div>
-      ShowingStepRolePersonEditForm
-      <button type="button" onClick={onSubmit as any}>
-        next
-      </button>
-    </div>
+    <Form
+      onSubmit={handleSubmit}
+      initialValues={initialData}
+      render={({ handleSubmit, submitting }) => {
+        return (
+          <form onSubmit={handleSubmit}>
+            <Box mb={2}>
+              <Typography variant="h6">Listing {roleType} Info</Typography>
+            </Box>
+
+            <FormTextField label="First Name" name="first_name" required />
+            <FormTextField label="Last Name" name="last_name" required />
+            <FormTextField label="Email" name="email" required />
+            <FormPhoneField label="Phone" name="phone_number" required />
+
+            <Box mt={4} display="flex" justifyContent="flex-end">
+              <Button
+                type="submit"
+                disabled={submitting}
+                color="primary"
+                variant="contained"
+              >
+                {submitLabel}
+              </Button>
+            </Box>
+          </form>
+        )
+      }}
+    />
   )
 }
 
--- app/components/Pages/Dashboard/Showings/components/ShowingStepRolePerson/index.tsx
+++ app/components/Pages/Dashboard/Showings/components/ShowingStepRolePerson/index.tsx
@@ -51,6 +51,7 @@ function ShowingStepRolePerson({
         )}
         {person && isEditable && (
           <ShowingStepRolePersonEditForm
+            roleType={roleType}
             initialData={person}
             onSubmit={handleSubmit}
           />
