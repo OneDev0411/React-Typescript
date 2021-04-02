1491ba4d3ac2372e3a2d66811de66925f43c99ef
OneDev0411 <cryptoapple1109@gmail.com>
2021-04-02 18:28:07 +0430

    feat(showings): implement FormPhoneField component (#5006)

---
+++ app/views/components/final-form-fields/FormPhoneField/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { formatPhoneNumber } from 'utils/format'
+
+import { isValidPhoneNumber } from 'utils/helpers'
+
+import FormTextField, { FormTextFieldProps } from '../FormTextField'
+
+export interface FormPhoneFieldProps
+  extends Omit<FormTextFieldProps, 'validate' | 'format'> {
+  name: string
+  format?: boolean
+}
+
+const validate = async value => {
+  if (!(await isValidPhoneNumber(value))) {
+    return 'Invalid Phone Number'
+  }
+}
+
+function FormPhoneField({ format = true, ...otherProps }: FormPhoneFieldProps) {
+  return (
+    <FormTextField
+      {...otherProps}
+      format={format ? formatPhoneNumber : undefined}
+      validate={validate}
+    />
+  )
+}
+
+export default FormPhoneField
