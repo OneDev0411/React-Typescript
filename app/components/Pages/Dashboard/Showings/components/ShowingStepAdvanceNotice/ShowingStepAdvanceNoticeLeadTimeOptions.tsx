127652260d0fbb70e78c91643b1655d085e3ebf3
OneDev0411 <cryptoapple1109@gmail.com>
2021-04-07 17:45:20 +0430

    refactor(showings): refactor the adnavce time options

--- app/components/Pages/Dashboard/Showings/components/ShowingStepAdvanceNotice/ShowingStepAdvanceNoticeLeadTimeOptions.tsx
+++ app/components/Pages/Dashboard/Showings/components/ShowingStepAdvanceNotice/ShowingStepAdvanceNoticeLeadTimeOptions.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 
-import {
-  Box,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select
-} from '@material-ui/core'
+import { Box } from '@material-ui/core'
 
-import { hourToSeconds } from './helpers'
+import SelectField, { SelectItem } from 'components/SelectField'
 
-const hours = new Array(12).fill(null)
+import { hourToSeconds } from '../../helpers'
+
+const hourOptions: SelectItem<number>[] = new Array(12)
+  .fill(null)
+  .map((a, index) => ({
+    label: `${index + 1} Hour${index > 0 ? 's' : ''}`,
+    value: hourToSeconds(index + 1)
+  }))
 
 interface ShowingStepAdvanceNoticeLeadTimeOptionsProps {
   label?: string
@@ -23,28 +24,15 @@ function ShowingStepAdvanceNoticeLeadTimeOptions({
   value,
   onChange
 }: ShowingStepAdvanceNoticeLeadTimeOptionsProps) {
-  const handleChange = (event: React.ChangeEvent<{ value: number }>) => {
-    onChange(event.target.value)
-  }
-
   return (
     <Box mb={1.5} mt={2} mx={1}>
-      <FormControl variant="outlined" size="small" fullWidth>
-        <InputLabel>{label}</InputLabel>
-        <Select
-          value={value || ''}
-          onChange={handleChange}
-          displayEmpty
-          label={label}
-        >
-          <MenuItem value="" hidden />
-          {hours.map((a, index) => (
-            <MenuItem key={index} value={hourToSeconds(index + 1)}>
-              {index + 1} Hour{index > 0 && 's'}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+      <SelectField<number>
+        options={hourOptions}
+        onChange={onChange}
+        value={value ?? undefined}
+        displayEmpty
+        label={label}
+      />
     </Box>
   )
 }
