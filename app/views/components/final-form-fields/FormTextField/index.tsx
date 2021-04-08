a8608312a8deaf0a7e5359804a085a62d7771174
OneDev0411 <cryptoapple1109@gmail.com>
2021-04-08 15:30:07 +0430

    refactor(showings): simplify the FormTextField component (#5006)

--- app/views/components/final-form-fields/FormTextField/index.tsx
+++ app/views/components/final-form-fields/FormTextField/index.tsx
@@ -1,45 +1,27 @@
 import React from 'react'
-import { TextField, TextFieldProps } from '@material-ui/core'
-import { Field, FieldProps } from 'react-final-form'
+import { Field } from 'react-final-form'
+import { TextField } from 'final-form-material-ui'
+import type { TextFieldProps } from '@material-ui/core'
 
-export interface FormTextFieldProps
-  extends Omit<TextFieldProps, 'name' | 'defaultValue'>,
-    Pick<FieldProps<string, any>, 'validate' | 'format'> {
-  name: string
-}
+import { FinalFieldProps } from '../types'
+
+export type FormTextFieldProps = FinalFieldProps<TextFieldProps>
 
 function FormTextField({
-  name,
-  helperText,
   variant = 'outlined',
   size = 'small',
   fullWidth = true,
   margin = 'normal',
-  validate,
-  format,
   ...otherProps
 }: FormTextFieldProps) {
   return (
     <Field
-      name={name}
-      validate={validate}
-      format={format}
-      render={({ input, meta }) => {
-        const hasError = meta.error && meta.touched
-
-        return (
-          <TextField
-            {...otherProps}
-            {...input}
-            variant={variant}
-            size={size}
-            fullWidth={fullWidth}
-            margin={margin}
-            error={hasError}
-            helperText={hasError ? meta.error : helperText}
-          />
-        )
-      }}
+      {...otherProps}
+      variant={variant}
+      size={size}
+      fullWidth={fullWidth}
+      margin={margin}
+      component={TextField}
     />
   )
 }
