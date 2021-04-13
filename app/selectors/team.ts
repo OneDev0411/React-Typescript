6d55d649d49b56fc62d634e8a703db5c096e23a5
OneDev0411 <cryptoapple1109@gmail.com>
2021-04-13 16:21:49 +0430

    feat(showings): add types to the selectors (#5006)

--- app/selectors/team.ts
+++ app/selectors/team.ts
@@ -7,15 +7,16 @@ import { selectUser } from './user'
  * Returns the active team id for the current user if exists
  * @param state The app state
  */
-export const selectActiveTeamIdUnsafe = (state: IAppState) =>
-  getActiveTeamId(selectUser(state))
+export function selectActiveTeamIdUnsafe(state: IAppState): Nullable<UUID> {
+  return getActiveTeamId(selectUser(state))
+}
 
 /**
  * Returns the active team id for the current user or throw an error
  * if there is no active team
  * @param state The app state
  */
-export const selectActiveTeamId = (state: IAppState) => {
+export function selectActiveTeamId(state: IAppState): UUID {
   const activeTeamId = selectActiveTeamIdUnsafe(state)
 
   if (!activeTeamId) {
