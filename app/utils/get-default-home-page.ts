8ddf14651a6c400b3689789865ddb841cb30611a
OneDev0411 <cryptoapple1109@gmail.com>
2021-04-16 18:31:38 +0430

    feat(general): change default routes for different ACLs  (#4934)

--- app/utils/get-default-home-page.ts
+++ app/utils/get-default-home-page.ts
@@ -1,25 +1,11 @@
 import { IUserState } from 'reducers/user'
-import { hasUserAccess } from './user-teams'
 
 export function getUserDefaultHomepage(user?: IUserState): string {
-  const dashboardBaseUrl = '/dashboard/'
-  let defaultHomepage = `${dashboardBaseUrl}mls`
+  const dashboardBaseUrl = '/dashboard'
 
   if (!user) {
-    return defaultHomepage
+    return `${dashboardBaseUrl}/mls`
   }
 
-  if (hasUserAccess(user, 'Marketing')) {
-    return `${dashboardBaseUrl}marketing`
-  }
-
-  if (hasUserAccess(user, 'CRM')) {
-    return `${dashboardBaseUrl}contacts`
-  }
-
-  if (hasUserAccess(user, 'Deals') || hasUserAccess(user, 'BackOffice')) {
-    return `${dashboardBaseUrl}deals`
-  }
-
-  return defaultHomepage
+  return `${dashboardBaseUrl}/overview`
 }
