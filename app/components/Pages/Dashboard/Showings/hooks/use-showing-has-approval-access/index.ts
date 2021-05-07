4174d49c4c8d3549ec5038e71679243fff3f5db2
OneDev0411 <cryptoapple1109@gmail.com>
2021-05-07 13:30:20 +0430

    fix(showing): add the can approve logic to the approval check hook (#5006)

--- app/components/Pages/Dashboard/Showings/hooks/use-showing-has-approval-access/index.ts
+++ app/components/Pages/Dashboard/Showings/hooks/use-showing-has-approval-access/index.ts
@@ -5,7 +5,7 @@ import { selectUserId } from 'selectors/user'
 function useShowingHasApprovalAccess(roles?: IShowingRole[]): boolean {
   const userId = useSelector(selectUserId)
 
-  return !!roles?.some(role => role.user_id === userId)
+  return !!roles?.some(role => role.user_id === userId && role.can_approve)
 }
 
 export default useShowingHasApprovalAccess
