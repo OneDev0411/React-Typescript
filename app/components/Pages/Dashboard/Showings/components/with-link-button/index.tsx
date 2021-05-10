b57158a1468b985baf087c9817c6ca755d22eddb
OneDev0411 <cryptoapple1109@gmail.com>
2021-05-10 15:39:23 +0430

    fix(showing): fix the hover issue on LinkButton (#5006)

--- app/components/Pages/Dashboard/Showings/components/with-link-button/index.tsx
+++ app/components/Pages/Dashboard/Showings/components/with-link-button/index.tsx
@@ -1,16 +1,31 @@
 import { forwardRef, ComponentType, Ref } from 'react'
+import classNames from 'classnames'
 
 import { Link, LinkProps } from 'react-router'
 
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles(
+  {
+    root: {
+      // TODO: remove this when the bootstrap styles got removed
+      '&:hover, &:focus': { color: 'white' }
+    }
+  },
+  { name: 'LinkButton' }
+)
+
 export type LinkButtonProps = Partial<
-  Pick<LinkProps, 'to' | 'target' | 'rel' | 'href'>
+  Pick<LinkProps, 'to' | 'target' | 'rel' | 'href' | 'className'>
 >
 
 function withLinkButton<T>(Component: ComponentType<T>) {
   return forwardRef(function LinkButton(
-    { to, ...other }: T & LinkButtonProps,
+    { to, className, ...other }: T & LinkButtonProps,
     ref: Ref<unknown>
   ) {
+    const classes = useStyles()
+
     const moreProps = to
       ? {
           component: Link,
@@ -18,7 +33,14 @@ function withLinkButton<T>(Component: ComponentType<T>) {
         }
       : {}
 
-    return <Component ref={ref} {...(other as T)} {...moreProps} />
+    return (
+      <Component
+        ref={ref}
+        {...(other as T)}
+        {...moreProps}
+        className={classNames(classes.root, className)}
+      />
+    )
   })
 }
 
