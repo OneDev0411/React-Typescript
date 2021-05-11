13c240e380646da29bf62ddedfaa50fa804f6a6b
OneDev0411 <cryptoapple1109@gmail.com>
2021-05-11 19:49:55 +0430

    fix(contacts): left out from last commit

---
+++ app/components/Pages/Dashboard/Deals/List/BackOffice/Grid/ZeroState/index.tsx
@@ -0,0 +1,12 @@
+import { ZeroState } from 'partials/ZeroState'
+
+export function ContactsZeroState() {
+  return (
+    <ZeroState
+      imageUrl="/static/images/zero-state/contacts.png"
+      title="You’ve done it. Inbox zero."
+      subTitle="Sit back and relax. Get a coffee. We’ll notify you when a deal needs your
+      attention."
+    />
+  )
+}
