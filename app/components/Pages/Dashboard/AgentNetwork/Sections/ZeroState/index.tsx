d035ffcf04a67135058a13b392e3e20798bb80d8
OneDev0411 <cryptoapple1109@gmail.com>
2021-05-12 10:47:41 +0430

    fix(zerostate): fix marketing insights and agents network images

--- app/components/Pages/Dashboard/AgentNetwork/Sections/ZeroState/index.tsx
+++ app/components/Pages/Dashboard/AgentNetwork/Sections/ZeroState/index.tsx
@@ -3,7 +3,7 @@ import { ZeroState } from 'partials/ZeroState'
 export function AgentNetworksZeroState() {
   return (
     <ZeroState
-      imageUrl="/static/images/zero-state/contacts.png"
+      imageUrl="/static/images/zero-state/agents-network.png"
       title="You don't have any listings."
       subTitle="Try searching for an address or listing at the top."
     />
