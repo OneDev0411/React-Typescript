a9f4d0ea6d2a8722c1983835cbb7607cfdffc6b8
OneDev0411 <cryptoapple1109@gmail.com>
2021-05-12 22:40:10 +0300

    feat(agents drawer): reimplement with virtual lists (#5076)

--- app/views/components/TeamAgentsDrawer/List/index.tsx
+++ app/views/components/TeamAgentsDrawer/List/index.tsx
@@ -1,25 +1,17 @@
-import React from 'react'
-import { Checkbox } from '@material-ui/core'
+import { ComponentProps, useMemo } from 'react'
+import AutoSizer from 'react-virtualized-auto-sizer'
 
-import { TextWithHighlights } from 'components/TextWithHighlights'
-import { Avatar } from 'components/Avatar'
+import { Box } from '@material-ui/core'
+
+import VirtualList from 'components/VirtualList'
 
 import Search from 'components/Grid/Search'
 
-// import { IUserWithBrand } from '../types'
-
-// import { EmptyState } from '../styled'
 import { NormalizedBrand, BrandedUser } from 'components/TeamAgents/types'
 
-import {
-  AgentEmail,
-  AgentTitle,
-  Card,
-  Header,
-  RowItem,
-  SubTitle,
-  Title
-} from './styled'
+import { ListRow } from '../ListRow'
+
+import { RowType } from '../types'
 
 interface Props {
   teams: NormalizedBrand[]
@@ -38,85 +30,91 @@ export function AgentsList({
   onSelectAgent,
   onChangeCriteria
 }: Props) {
+  const rows = useMemo(() => {
+    return teams
+      .filter(office => office.users.length > 0)
+      .flatMap(office => {
+        const agents = office.users.map((user: IUser) => {
+          const isSelected =
+            multiSelection && selectedAgents.some(agent => agent.id === user.id)
+
+          return {
+            type: RowType.Agent,
+            user,
+            office,
+            isSelected,
+            multiSelection
+          }
+        })
+
+        const header = office.name
+          ? [
+              {
+                type: RowType.Header,
+                name: office.name,
+                subtitle: office.subtitle
+              }
+            ]
+          : []
+
+        return [
+          ...header,
+          ...agents,
+          {
+            type: RowType.Spacer
+          }
+        ]
+      })
+  }, [teams, multiSelection, selectedAgents])
+
+  const getRowLength = (index: number): number => {
+    switch (rows[index].type) {
+      case RowType.Agent:
+        return 70
+
+      case RowType.Header:
+        return 60
+
+      case RowType.Spacer:
+        return 15
+
+      default:
+        return 0
+    }
+  }
+
   return (
-    <div>
+    <Box display="flex" flexDirection="column" height="100%">
       <Search
         style={{ margin: '1rem 0' }}
         placeholder="Search for teams or agents"
         onChange={onChangeCriteria}
       />
 
-      {teams.map((office, officeIndex) => {
-        if (office.users.length === 0) {
-          return null
-        }
-
-        return (
-          <Card key={officeIndex}>
-            {office.name && (
-              <Header>
-                <Title>
-                  <TextWithHighlights search={searchCriteria}>
-                    {office.name}
-                  </TextWithHighlights>
-                </Title>
-                <SubTitle>{office.subtitle}</SubTitle>
-              </Header>
-            )}
-
-            {office.users.map((user: IUser, userIndex: number) => {
-              const isSelected =
-                multiSelection &&
-                selectedAgents.some(agent => agent.id === user.id)
-
-              return (
-                <RowItem
-                  key={`${officeIndex}${userIndex}`}
-                  onClick={() =>
-                    onSelectAgent({
-                      ...user,
-                      brand_id: office.id
-                    })
-                  }
-                >
-                  {multiSelection && (
-                    <Checkbox
-                      color="primary"
-                      style={{
-                        padding: 0,
-                        marginRight: '1rem'
-                      }}
-                      checked={isSelected}
-                    />
-                  )}
-
-                  <Avatar
-                    alt={user.display_name}
-                    user={user}
-                    style={{
-                      marginRight: '1rem'
-                    }}
-                  />
-
-                  <div>
-                    <AgentTitle to="">
-                      <TextWithHighlights search={searchCriteria}>
-                        {user.display_name}
-                      </TextWithHighlights>
-                    </AgentTitle>
-
-                    <AgentEmail>
-                      <TextWithHighlights search={searchCriteria}>
-                        {user.email}
-                      </TextWithHighlights>
-                    </AgentEmail>
-                  </div>
-                </RowItem>
-              )
-            })}
-          </Card>
-        )
-      })}
-    </div>
+      <Box flexGrow={1}>
+        <AutoSizer>
+          {({ width, height }) => (
+            <VirtualList
+              width={width}
+              height={height}
+              itemCount={rows.length}
+              itemData={
+                {
+                  rows,
+                  searchCriteria,
+                  onSelectAgent
+                } as ComponentProps<typeof ListRow>['data']
+              }
+              threshold={5}
+              isLoading={false}
+              itemSize={getRowLength}
+              overscanCount={15}
+            >
+              {ListRow}
+            </VirtualList>
+          )}
+        </AutoSizer>
+      </Box>
+    </Box>
   )
 }
