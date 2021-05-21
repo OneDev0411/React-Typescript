20d93e5a7f0075f64bcdd52fc0297c508bd1e553
OneDev0411 <cryptoapple1109@gmail.com>
2021-05-21 18:30:38 +0430

    refactor(branch): port to function component and fix inf loop  (#5161)

---
+++ app/components/Pages/Branch/helpers/redirectHandler.js
@@ -0,0 +1,207 @@
+import { browserHistory } from 'react-router'
+
+import { createUrlSearch } from 'utils/helpers'
+
+import submitSigninForm from '../../../../store_actions/auth/signin'
+
+import generateVerificationActionRedirectUrl from './generateVerificationActionRedirectUrl'
+import getActionRedirectURL from './getActionRedirectURL'
+import getConflictMessageText from './getConflictMessageText'
+
+const redirectHandler = async (
+  actionType,
+  branchData,
+  loggedInUser,
+  setActiveModal,
+  dispatch
+) => {
+  let redirect
+
+  Object.keys(branchData).forEach(key => {
+    if (key === 'email' || key === 'phone_number') {
+      branchData[key] = encodeURIComponent(branchData[key])
+    }
+  })
+
+  let {
+    token,
+    email,
+    action,
+    listing,
+    phone_number,
+    receiving_user,
+    userInfo
+  } = branchData
+
+  email = email || (userInfo && userInfo.email)
+
+  const branchUrl = branchData['~referring_link']
+
+  const params = {
+    action,
+    loggedInUser,
+    userInfo,
+    branchUrl,
+    email
+  }
+  const hasConflict = () =>
+    loggedInUser &&
+    ((receiving_user && receiving_user !== loggedInUser.id) ||
+      (email && decodeURIComponent(email) !== loggedInUser.email))
+
+  if (actionType === 'VERIFY') {
+    redirect = generateVerificationActionRedirectUrl(branchData)
+    console.log('verify', branchData, redirect)
+
+    const setParams = () => ({
+      ...params,
+      redirectTo: branchUrl,
+      verificationType: phone_number ? 'phone number' : 'email address'
+    })
+
+    if (!loggedInUser) {
+      setActiveModal({
+        name: 'NEEDS_TO_LOGIN',
+        params: setParams()
+      })
+
+      return
+    }
+
+    if (hasConflict()) {
+      // console.log('different user logged in with receiving user')
+      setActiveModal({
+        name: 'VERIFYING_CONFLICT',
+        params: setParams()
+      })
+
+      return
+    }
+  } else if (actionType === 'OpenMarketingWizard') {
+    const marketingWizardRedirectUrl = getActionRedirectURL(branchData)
+
+    // If the user is wrong, ask him to relogin with the right user
+    if (hasConflict()) {
+      params.redirectTo = marketingWizardRedirectUrl
+      params.messageText = getConflictMessageText(email)
+      setActiveModal({ name: 'CONFLICT', params })
+
+      return
+    }
+
+    // If it's logged in, just redirect as it's the right user
+    if (loggedInUser) {
+      browserHistory.push(marketingWizardRedirectUrl)
+
+      return
+    }
+
+    // It it's not logged in, login and redirect
+    await dispatch(
+      submitSigninForm(
+        {
+          refresh_token: branchData.refresh_token.token,
+          grant_type: 'refresh_token',
+          client_id: branchData.refresh_token.client
+        },
+        marketingWizardRedirectUrl,
+        branchData.refresh_token.user
+      )
+    )
+
+    return
+  } else if (userInfo && userInfo.is_shadow) {
+    console.log('isShadow:', branchData)
+    redirect = '/register'
+
+    if (listing) {
+      redirect = `/dashboard/mls/${listing}`
+    }
+
+    let queryParams = {
+      token,
+      email,
+      phone_number,
+      redirectTo: getActionRedirectURL(branchData)
+    }
+
+    if (hasConflict()) {
+      console.log('you logged with different user')
+      queryParams.redirectFromSignout = redirect
+      params.to = `/signout${createUrlSearch(queryParams)}`
+      params.messageText =
+        'You are currently logged in a different user. Please sign out and sign up your new account.'
+      setActiveModal({ name: 'SHADOW_CONFLICT', params })
+
+      return
+    }
+
+    redirect += createUrlSearch(queryParams)
+  } else if (actionType === 'UserLogin') {
+    console.log('UserLogin', branchData)
+
+    const loginHandler = () =>
+      dispatch(
+        submitSigninForm(
+          {
+            refresh_token: branchData.refresh_token.token,
+            grant_type: 'refresh_token',
+            client_id: branchData.refresh_token.client
+          },
+          branchData.uri || '/dashboard/',
+          branchData.refresh_token.user
+        )
+      )
+
+    if (hasConflict()) {
+      console.log('UserLogin - CONFLICT')
+
+      setActiveModal({
+        name: 'CONFLICT',
+        params: {
+          ...params,
+          messageText: getConflictMessageText(email),
+          actionButtonProps: {
+            onClick: loginHandler,
+            text: 'Sign in'
+          }
+        }
+      })
+
+      return
+    }
+
+    loginHandler()
+
+    return
+  } else if (loggedInUser) {
+    console.log('loggedIn', branchData)
+    redirect = getActionRedirectURL(branchData)
+
+    if (hasConflict()) {
+      console.log('you logged with deferent user')
+      params.redirectTo = redirect
+      params.messageText = getConflictMessageText(email)
+      setActiveModal({ name: 'CONFLICT', params })
+
+      return
+    }
+  } else {
+    console.log('you registered before with this email', branchData)
+
+    let username = ''
+
+    if (email) {
+      username = `username=${email}`
+    }
+
+    redirect = !listing
+      ? `/signin?${username}&redirectTo=`
+      : `/dashboard/mls/${listing}?${username}&redirectTo=`
+    redirect += getActionRedirectURL(branchData)
+  }
+
+  browserHistory.push(redirect)
+}
+
+export default redirectHandler
---
+++ app/components/Pages/Branch/index.jsx
@@ -0,0 +1,160 @@
+// Branch.js
+import React, { useState } from 'react'
+import { useDeepCompareEffect } from 'react-use'
+import * as BranchSDK from 'branch-sdk'
+import { browserHistory, withRouter } from 'react-router'
+import idx from 'idx'
+
+import { useDispatch, useSelector } from 'react-redux'
+
+import { selectUser } from 'selectors/user'
+import { getActiveBrand } from 'utils/user-teams'
+
+import publicConfig from '../../../../config/public'
+import Loading from '../../Partials/Loading'
+import { getBrandInfo } from '../Auth/SignIn/get-brand-info'
+import { lookUpUserByEmail } from '../../../models/user/lookup-user-by-email'
+import ConflictModal from './components/ConflictModal'
+import NeedsToLoginModal from './components/NeedsToLoginModal'
+import VerifyRedirectModal from './components/VerifyRedirectModal'
+
+import redirectHandler from './helpers/redirectHandler'
+
+const branchKey = publicConfig.branch.key
+
+const Branch = ({ location }) => {
+  const [activeModal, setActiveModal] = useState(null)
+  const [branchData, setBranchData] = useState(null)
+  const waitingForRedirect = Object.keys(location.query).includes(
+    'waitingForRedirect'
+  )
+
+  const dispatch = useDispatch()
+  const loggedInUser = useSelector(selectUser)
+
+  const brand = getActiveBrand(loggedInUser)
+
+  let content = <Loading />
+
+  useDeepCompareEffect(() => {
+    if (activeModal) {
+      const brandInfo = getBrandInfo(brand)
+      const { name, params } = activeModal
+
+      switch (name) {
+        case 'CONFLICT':
+        case 'SHADOW_CONFLICT':
+        case 'PROTECTED_RESOURCE':
+          content = <ConflictModal params={params} brandInfo={brandInfo} />
+          break
+        case 'VERIFIED':
+        case 'VERIFYING_CONFLICT':
+          content = (
+            <VerifyRedirectModal
+              type={name}
+              params={params}
+              brandInfo={brandInfo}
+            />
+          )
+          break
+        case 'NEEDS_TO_LOGIN':
+          content = <NeedsToLoginModal params={params} brandInfo={brandInfo} />
+          break
+        default:
+          break
+      }
+    } else if (!branchData) {
+      BranchSDK.init(
+        branchKey,
+        {
+          retries: 30,
+          retry_delay: 3000
+        },
+        async (err, data) => {
+          if (err) {
+            console.log('Init - error', err, data)
+            browserHistory.push('/oops')
+          }
+
+          if (idx(data, d => d.data_parsed.action)) {
+            let userInfo = null
+
+            if (data.data_parsed.email) {
+              try {
+                userInfo = await lookUpUserByEmail(data.data_parsed.email)
+              } catch (error) {
+                console.log(error)
+              }
+            }
+
+            setBranchData({
+              ...data.data_parsed,
+              userInfo
+            })
+          } else {
+            console.log('Init - success but with corrupted data', data)
+          }
+        }
+      )
+    } else if (!waitingForRedirect) {
+      const { userInfo, action } = branchData
+
+      if (action) {
+        if (action === 'ShareTemplateInstance') {
+          browserHistory.push({ pathname: '/share', state: branchData })
+        } else if (action.includes('Verification')) {
+          let email_confirmed
+          let phone_confirmed
+
+          if (userInfo) {
+            email_confirmed = userInfo.email_confirmed
+            phone_confirmed = userInfo.phone_confirmed
+          }
+
+          const { email_code, phone_code } = branchData
+
+          if (
+            (email_code && !email_confirmed) ||
+            (phone_code && !phone_confirmed)
+          ) {
+            redirectHandler('VERIFY', branchData, loggedInUser, setActiveModal)
+          } else {
+            setActiveModal({
+              name: 'VERIFIED',
+              params: {
+                userInfo,
+                verificationType: phone_code ? 'phone number' : 'email address'
+              }
+            })
+          }
+        } else {
+          redirectHandler(
+            branchData.action || 'OTHER', // userLogin
+            branchData,
+            loggedInUser, // false
+            setActiveModal,
+            dispatch
+          )
+        }
+      } else {
+        console.log('last oops in last else', branchData)
+        browserHistory.push('/oops')
+      }
+    }
+  }, [branchData])
+
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      {content}
+    </div>
+  )
+}
+
+export default withRouter(Branch)
