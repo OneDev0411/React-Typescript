f5492734b6689875b0566530a545e917285d00dc
OneDev0411 <cryptoapple1109@gmail.com>
2021-06-12 01:05:25 +0430

    feat(showing): define FormCheckbox component (#5006)

--- app/views/components/final-form-fields/types.ts
+++ app/views/components/final-form-fields/types.ts
@@ -1,4 +1,7 @@
 import type { FieldProps } from 'react-final-form'
 
-export type FinalFieldProps<T> = Omit<T, 'name' | 'defaultValue'> &
+export type FinalFieldProps<T> = Omit<
+  T,
+  'name' | 'defaultValue' | 'defaultChecked'
+> &
   Pick<FieldProps<string, any>, 'name' | 'format' | 'validate'>
