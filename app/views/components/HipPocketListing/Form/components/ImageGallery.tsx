92e700a664c0fe855719cd75e7b0b250186acb68
OneDev0411 <cryptoapple1109@gmail.com>
2021-06-13 22:44:06 +0430

    feat(components): implement iamge gallery pagination on hip pocket (#5203)

--- app/views/components/HipPocketListing/Form/components/ImageGallery.tsx
+++ app/views/components/HipPocketListing/Form/components/ImageGallery.tsx
@@ -1,6 +1,16 @@
-import { Grid, Box, makeStyles } from '@material-ui/core'
+import { useState } from 'react'
+import {
+  Grid,
+  Box,
+  Typography,
+  Fade,
+  fade,
+  makeStyles
+} from '@material-ui/core'
 
 import { ImageGalleryProps } from '../../types'
+
+const PAGE_SIZE = 3
 
 const useStyles = makeStyles(
   theme => ({
@@ -8,7 +18,30 @@ const useStyles = makeStyles(
       position: 'relative',
       width: '100%',
       paddingTop: '100%',
-      border: `1px solid ${theme.palette.divider}`
+      overflow: 'hidden',
+      border: `1px solid ${theme.palette.divider}`,
+      borderRadius: theme.shape.borderRadius
+    },
+    loadMoreContainer: {
+      position: 'absolute',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      textAlign: 'center',
+      color: theme.palette.common.white,
+      backgroundColor: fade(theme.palette.common.black, 0.75),
+      cursor: 'pointer',
+      zIndex: 1,
+      transition: theme.transitions.create('background-color'),
+
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.black, 0.5)
+      }
     },
     image: {
       position: 'absolute',
@@ -16,7 +49,6 @@ const useStyles = makeStyles(
       left: 0,
       bottom: 0,
       right: 0,
-      borderRadius: theme.shape.borderRadius,
       width: '100%',
       height: '100%',
       objectFit: 'cover'
@@ -31,21 +63,46 @@ export default function HipPocketListingFormImageGallery({
   images
 }: ImageGalleryProps) {
   const classes = useStyles()
+  const [loadedItemsCount, setLoadedItemsCount] = useState<number>(PAGE_SIZE)
 
   if (images.length === 0) {
     return null
   }
 
+  const handleClickLoadMore = () => {
+    setLoadedItemsCount(Infinity)
+  }
+
+  const loadedImages = images.slice(0, loadedItemsCount)
+
+  const moreImagesToShow: number = images.length - loadedImages.length
+
   return (
     <Grid container item>
       <Grid container item direction="row" spacing={1}>
-        {images.map((image, index) => (
-          <Grid item xs={4} key={index}>
-            <Box className={classes.imageContainer}>
-              <img src={image} alt={image} className={classes.image} />
-            </Box>
-          </Grid>
-        ))}
+        {loadedImages.map((image, index) => {
+          const shouldShowLoadMore =
+            moreImagesToShow > 0 && index === loadedImages.length - 1
+
+          return (
+            <Grid item xs={4} key={index}>
+              <Fade in>
+                <Box className={classes.imageContainer}>
+                  {shouldShowLoadMore && (
+                    <Box
+                      className={classes.loadMoreContainer}
+                      onClick={handleClickLoadMore}
+                    >
+                      <Typography variant="h6">{moreImagesToShow}</Typography>
+                      <Typography variant="body2">More Photos</Typography>
+                    </Box>
+                  )}
+                  <img src={image} alt={image} className={classes.image} />
+                </Box>
+              </Fade>
+            </Grid>
+          )
+        })}
       </Grid>
     </Grid>
   )
