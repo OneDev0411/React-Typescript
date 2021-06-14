8003bfc6a485ada6c2ba649006a03491cd69cbd0
OneDev0411 <cryptoapple1109@gmail.com>
2021-06-14 14:36:30 +0430

    docs(components): add docs to hip pocket form and drawer props (#5203)

--- app/views/components/HipPocketListing/types.ts
+++ app/views/components/HipPocketListing/types.ts
@@ -15,14 +15,42 @@ export interface HipPocketListingFields extends HipPocketListing {
 }
 
 export interface HipPocketListingFormProps {
+  /**
+   * Save button copy/text
+   *
+   * The default value is `'Save Listing'`
+   */
   saveButtonText?: string
+  /**
+   * The image upload handler for upload image button
+   *
+   * This function receives a file array and
+   * it should return a promise of passed file links
+   * as an string array
+   */
   onImageUpload?: (files: File[]) => Promise<string[]>
+  /**
+   * The save button click handler
+   */
   onSave: (data: HipPocketListing) => void
 }
 
 export interface HipPocketListingDrawerProps extends HipPocketListingFormProps {
+  /**
+   * The drawer title
+   *
+   * The default value is `'Listing Details'`
+   */
   title?: string
+  /**
+   * The drawer open state
+   */
   isOpen: boolean
+  /**
+   * The drawer close handler
+   *
+   * It can/shoud be used to change `isOpen` prop and close the drawer
+   */
   onClose: () => void
 }
 
