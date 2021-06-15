29bd48781a2880b3950f41122061299198646962
OneDev0411 <cryptoapple1109@gmail.com>
2021-06-15 19:13:31 +0300

    fix(deals): show both deal_type and active offer contexts

--- app/components/Pages/Dashboard/Deals/Dashboard/Factsheet/hooks/use-factsheet-contexts.ts
+++ app/components/Pages/Dashboard/Deals/Dashboard/Factsheet/hooks/use-factsheet-contexts.ts
@@ -11,17 +11,33 @@ export function useFactsheetContexts(deal: IDeal, section: string) {
   const list = useSelector<
     IAppState,
     IBrandChecklist['required_contexts'] & IBrandChecklist['optional_contexts']
-  >(({ deals }) =>
-    uniqBy(
+  >(({ deals }) => {
+    const contexts: IDealContext[] = []
+
+    if (deal.has_active_offer) {
+      contexts.concat(
+        getBrandChecklistContexts(
+          deals.brandChecklists,
+          deal.brand.id,
+          deal.property_type?.id,
+          'Offer'
+        )
+      )
+    }
+
+    contexts.concat(
       getBrandChecklistContexts(
         deals.brandChecklists,
         deal.brand.id,
         deal.property_type?.id,
-        deal.has_active_offer ? 'Offer' : deal.deal_type
-      ),
-      context => context.key
-    ).filter(context => context.section === section)
-  )
+        deal.deal_type
+      )
+    )
+
+    return uniqBy(contexts, context => context.key).filter(
+      context => context.section === section
+    )
+  })
 
   if (section === 'Dates') {
     const fieldsWithValue = list
