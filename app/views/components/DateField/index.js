b06a3e1e29e99d54df4f14814de3536b2a3f4d9b
OneDev0411 <cryptoapple1109@gmail.com>
2021-06-28 23:39:03 +0430

    chore(contact-profile): not showing 31 days for feb (#5016)

--- app/views/components/DateField/index.js
+++ app/views/components/DateField/index.js
@@ -1,84 +1,113 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
 import PropTypes from 'prop-types'
 import Flex from 'styled-flex-component'
 
-import { days, months } from 'utils/date-times'
+import { months } from 'utils/date-times'
 import Button from 'components/Button/ActionButton'
 import { BasicDropdown } from 'components/BasicDropdown'
 
 import { Input } from './styled'
 
-const daysItems = days.map(day => ({
-  label: day < 10 ? `0${day}` : day.toString(),
-  value: day
-}))
-const monthsItems = months.map((month, index) => ({
-  label: month,
-  value: index
-}))
+export function DateField(props) {
+  const [day, setDay] = useState(props.day || { label: 'Day', value: null })
+  const [month, setMonth] = useState(
+    props.month || { label: 'Month', value: null }
+  )
+  const [year, setYear] = useState(props.year || '')
 
-export class DateField extends React.Component {
-  static propTypes = {
-    day: PropTypes.shape().isRequired,
-    month: PropTypes.shape().isRequired,
-    year: PropTypes.any,
-    onChangeDay: PropTypes.func.isRequired,
-    onChangeMonth: PropTypes.func.isRequired,
-    onChangeYear: PropTypes.func,
-    showYear: PropTypes.bool,
-    toggleYearOption: PropTypes.func
+  const daysItems = useMemo(() => {
+    const selectedMonth = month.value ? parseInt(month.value, 10) + 1 : 0
+    const selectedYear = year ? parseInt(year, 10) : 1800
+    const daysInMonth = new Date(selectedYear, selectedMonth, 0).getDate()
+
+    const days = [...Array(daysInMonth).keys()].map(day => day + 1)
+
+    return days.map(day => ({
+      label: day < 10 ? `0${day}` : day.toString(),
+      value: day
+    }))
+  }, [month.value, year])
+  const monthsItems = useMemo(
+    () =>
+      months.map((month, index) => ({
+        label: month,
+        value: index
+      })),
+    []
+  )
+
+  console.log({ props })
+
+  const display = 'flex'
+
+  const onChangeDay = value => {
+    setDay(value)
+    props.onChangeDay(value)
   }
 
-  static defaultProps = {
-    showYear: false,
-    onChangeYear() {},
-    toggleYearOption() {},
-    year: ''
+  const onChangeMonth = value => {
+    setMonth(value)
+    props.onChangeMonth(value)
   }
 
-  onChangeYear = event => {
+  const onChangeYear = event => {
     const { value } = event.target
 
     if (!value || /^\d+$/.test(value)) {
-      this.props.onChangeYear(value)
+      setYear(value)
+      props.onChangeYear(value)
     }
   }
 
-  render() {
-    const { props } = this
-    const display = 'flex'
+  return (
+    <Flex data-test="date-field">
+      <BasicDropdown
+        items={monthsItems}
+        onChange={onChangeMonth}
+        selectedItem={month}
+        style={{ display }}
+        buttonRenderer={props.dropdownButtonRenderer}
+      />
+      <BasicDropdown
+        disabled={!month.value}
+        items={daysItems}
+        selectedItem={day}
+        onChange={onChangeDay}
+        style={{ display, margin: '0 0.5rem 0' }}
+        buttonRenderer={props.dropdownButtonRenderer}
+      />
+      {props.showYear ? (
+        <Input
+          type="text"
+          autoComplete="disabled"
+          placeholder="Year"
+          maxLength="4"
+          onChange={onChangeYear}
+          value={year}
+        />
+      ) : (
+        <Button appearance="link" onClick={props.toggleYearOption}>
+          + Add Year
+        </Button>
+      )}
+    </Flex>
+  )
+}
 
-    return (
-      <Flex data-test="date-field">
-        <BasicDropdown
-          items={monthsItems}
-          onChange={props.onChangeMonth}
-          selectedItem={props.month}
-          style={{ display }}
-          buttonRenderer={props.dropdownButtonRenderer}
-        />
-        <BasicDropdown
-          items={daysItems}
-          selectedItem={props.day}
-          onChange={props.onChangeDay}
-          style={{ display, margin: '0 0.5rem 0' }}
-          buttonRenderer={props.dropdownButtonRenderer}
-        />
-        {props.showYear ? (
-          <Input
-            type="text"
-            autoComplete="disabled"
-            placeholder="Year"
-            maxLength="4"
-            onChange={this.onChangeYear}
-            value={props.year || ''}
-          />
-        ) : (
-          <Button appearance="link" onClick={props.toggleYearOption}>
-            + Add Year
-          </Button>
-        )}
-      </Flex>
-    )
-  }
+DateField.propTypes = {
+  day: PropTypes.shape().isRequired,
+  month: PropTypes.shape().isRequired,
+  year: PropTypes.any,
+  onChangeDay: PropTypes.func.isRequired,
+  onChangeMonth: PropTypes.func.isRequired,
+  onChangeYear: PropTypes.func,
+  showYear: PropTypes.bool,
+  toggleYearOption: PropTypes.func
+}
+
+DateField.defaultProps = {
+  showYear: false,
+  onChangeYear() {},
+  toggleYearOption() {},
+  year: ''
 }
