675feef916a70b169871b89ab84590840a5b39d7
OneDev0411 <cryptoapple1109@gmail.com>
2021-07-01 19:10:53 +0430

    fix(showings): merge all the CTA buttons into one item (#5295)

--- app/components/Pages/Dashboard/Listings/pages/Listings/ListingsListColumnActions.tsx
+++ app/components/Pages/Dashboard/Listings/pages/Listings/ListingsListColumnActions.tsx
@@ -1,74 +1,19 @@
-import { Button, makeStyles } from '@material-ui/core'
-
-import { ACL } from '@app/constants/acl'
-import { useAcl } from '@app/views/components/Acl/use-acl'
-
-import ListingsListColumnActionsDealButton from './ListingsListColumnActionsDealButton'
-import ListingsListColumnActionsOpenHouseButton, {
-  ListingsListColumnActionsOpenHouseButtonProps
-} from './ListingsListColumnActionsOpenHouseButton'
-import ListingsListColumnActionsViewListingButton from './ListingsListColumnActionsViewListingButton'
-import useListingsOpenHouseHasAccess from './use-listings-open-house-has-access'
-
-const useStyles = makeStyles(
-  theme => ({
-    button: { marginLeft: theme.spacing(1) }
-  }),
-  { name: 'ListingsListColumnActions' }
-)
+import ListingsListColumnActionsMegaButton, {
+  ListingsListColumnActionsMegaButtonProps
+} from './ListingsListColumnActionsMegaButton'
 
 export interface ListingsListColumnActionsProps
-  extends Pick<ListingsListColumnActionsOpenHouseButtonProps, 'row'> {
+  extends Pick<ListingsListColumnActionsMegaButtonProps, 'row' | 'hasActions'> {
   className: string
-  hasActions: boolean
 }
-
-const dealsAccess = { oneOf: [ACL.DEALS, ACL.BACK_OFFICE] }
 
 function ListingsListColumnActions({
   className,
-  row,
-  hasActions
+  ...otherProps
 }: ListingsListColumnActionsProps) {
-  const classes = useStyles()
-  const hasOpenHouseAccess = useListingsOpenHouseHasAccess()
-  const hasDealsAccess = useAcl(dealsAccess)
-
   return (
     <div className={className}>
-      {hasOpenHouseAccess && (
-        <ListingsListColumnActionsOpenHouseButton
-          className={classes.button}
-          variant="outlined"
-          size="small"
-          row={row}
-        />
-      )}
-      <ListingsListColumnActionsViewListingButton
-        className={classes.button}
-        variant="outlined"
-        size="small"
-        listingId={row.id}
-      />
-      <Button
-        className={classes.button}
-        variant="outlined"
-        size="small"
-        onClick={() => alert('The action is not implemented')}
-      >
-        {/* TODO: Connect this to Mamal's page */}
-        Market Listing
-      </Button>
-      {hasDealsAccess && hasActions && (
-        <ListingsListColumnActionsDealButton
-          className={classes.button}
-          variant="contained"
-          size="small"
-          color="secondary"
-          listingId={row.id}
-          target="_blank"
-        />
-      )}
+      <ListingsListColumnActionsMegaButton {...otherProps} />
     </div>
   )
 }
