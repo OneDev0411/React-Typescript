7054a138ee9415c397efafdfc796581e6f7683a3
OneDev0411 <cryptoapple1109@gmail.com>
2021-07-08 17:10:14 +0300

    feat(app): develop use query param hook

---
+++ app/hooks/use-query-param.ts
@@ -0,0 +1,44 @@
+import { useMemo } from 'react'
+import { useLocation } from 'react-use'
+
+type UseQueryParam = [string | null, (value: string) => void]
+
+function useQueryParamValue(name: string): string | null {
+  const location = useLocation()
+
+  const value = useMemo(() => new URLSearchParams(location.search).get(name), [
+    location.search,
+    name
+  ])
+
+  return value
+}
+
+export function useQueryParam(name: string): UseQueryParam {
+  const value = useQueryParamValue(name)
+
+  const setValue = (value: string) => {
+    const url = new URL(window.location.href)
+
+    url.searchParams.set(name, encodeURIComponent(value))
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/History/pushState
+    window.history.pushState({}, '', url.toString())
+  }
+
+  return [value, setValue]
+}
+
+export function useReplaceQueryParam(name: string): UseQueryParam {
+  const value = useQueryParamValue(name)
+
+  const setValue = (value: string) => {
+    const url = new URL(window.location.href)
+
+    url.searchParams.set(name, encodeURIComponent(value))
+
+    window.history.replaceState({}, '', url.toString())
+  }
+
+  return [value, setValue]
+}
