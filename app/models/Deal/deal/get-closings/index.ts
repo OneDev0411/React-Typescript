766fc7ee1443b81425a24f694e101636cd15d26f
OneDev0411 <cryptoapple1109@gmail.com>
2021-07-08 23:53:26 +0430

    fix(deals): add the days limitation logic to the closings model (#5292)

--- app/models/Deal/deal/get-closings/index.ts
+++ app/models/Deal/deal/get-closings/index.ts
@@ -1,11 +1,24 @@
+import { addDays } from 'date-fns'
+
 import Fetch from '@app/services/fetch'
 
-export async function getClosings(): Promise<IDeal[]> {
-  // TODO: add the 14 days limitation to the filter
+export async function getClosings(days: number = 14): Promise<IDeal[]> {
   return (
-    await new Fetch().post('/deals/filter').query({
-      // TODO: Ask Ramin or Emil about the required associations
-      associations: ['deal.brand']
-    })
+    await new Fetch({ proxy: false })
+      .post('/deals/filter')
+      .query({
+        // TODO: Ask Ramin or Emil about the required associations
+        associations: ['deal.brand']
+      })
+      .send({
+        contexts: {
+          closing_date: {
+            date: {
+              from: new Date().toISOString(),
+              to: addDays(new Date(), days).toISOString()
+            }
+          }
+        }
+      })
   ).body.data
 }
