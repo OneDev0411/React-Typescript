dd577c3a847dd61cfb559be21eea69ca4227c44e
OneDev0411 <cryptoapple1109@gmail.com>
2021-07-12 19:40:55 +0430

    fix(marketing): fix listing marketing page title

--- app/components/Pages/Dashboard/Listings/Marketing/index.tsx
+++ app/components/Pages/Dashboard/Listings/Marketing/index.tsx
@@ -1,26 +1,52 @@
+import { useState, useEffect } from 'react'
 import { WithRouterProps } from 'react-router'
+import { useTitle } from 'react-use'
 
-import ListingMarketing from 'components/ListingMarketing'
-import PageLayout from 'components/GlobalPageLayout'
+import ListingMarketing from '@app/views/components/ListingMarketing'
+import PageLayout from '@app/views/components/GlobalPageLayout'
 
-interface Props {}
+import { getListingAddress } from '@app/utils/listing'
+import getListing from '@app/models/listings/listing/get-listing'
 
 export default function ListingMarketingPage({
   params,
   location,
   router
-}: Props & WithRouterProps) {
+}: WithRouterProps) {
+  useTitle('Marketing | Rechat')
+
   const listingId: UUID = params.id
   const templateType: Optional<IMarketingTemplateType> = location.query.type
-  const title: string = location.query.title ?? 'Listing Marketing'
   const medium: Optional<string> = location.hash.split('#').pop() || undefined
+
+  const [listing, setListing] = useState<Nullable<IListing>>(null)
+
+  useEffect(() => {
+    async function fetchListing() {
+      try {
+        const listing = await getListing(listingId)
+
+        setListing(listing)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    fetchListing()
+  }, [listingId])
+
+  if (!listing) {
+    return null
+  }
+
+  const title = getListingAddress(listing)
 
   return (
     <PageLayout>
       <PageLayout.Header title={title} />
       <PageLayout.Main>
         <ListingMarketing
-          listingId={listingId}
+          listing={listing}
           templateType={templateType}
           medium={medium as IMarketingTemplateMedium}
           onChangeTemplateType={type => {
