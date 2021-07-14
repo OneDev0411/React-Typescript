fbfaa1ca1e529830f636eae99de29848ce3f47a3
OneDev0411 <cryptoapple1109@gmail.com>
2021-07-14 20:10:52 +0430

    fix(deals): improve the hook structure and add more comments (#5291)

--- app/hooks/use-query-param.ts
+++ app/hooks/use-query-param.ts
@@ -3,22 +3,23 @@ import { useLocation } from 'react-use'
 import { browserHistory } from 'react-router'
 import { LocationDescriptor } from 'history'
 
-type UseQueryParam = [string, (value: string) => void]
+type UseQueryParam = [string, (value: string) => void, () => void]
 
 export function useQueryParamValue(name: string): string {
   const location = useLocation()
 
-  const value = useMemo(
-    () => new URLSearchParams(location.search).get(name),
-    [location.search, name]
-  )
+  const value = useMemo(() => new URLSearchParams(location.search).get(name), [
+    location.search,
+    name
+  ])
 
   return decodeURIComponent(value || '')
 }
 
 function useQueryParamBase(
   historyAction: (path: LocationDescriptor) => void,
-  name: string
+  name: string,
+  deleteIfEmpty: boolean
 ): UseQueryParam {
   const value = useQueryParamValue(name)
 
@@ -26,24 +27,80 @@ function useQueryParamBase(
     (value: string) => {
       const url = new URL(window.location.href)
 
-      if (value) {
-        url.searchParams.set(name, encodeURIComponent(value))
-      } else {
+      if (deleteIfEmpty && !value) {
         url.searchParams.delete(name)
+      } else {
+        url.searchParams.set(name, encodeURIComponent(value))
       }
 
-      historyAction.call(browserHistory, url.toString())
+      historyAction(url.toString())
     },
-    [historyAction, name]
+    [historyAction, name, deleteIfEmpty]
   )
 
-  return [value, setValue]
+  const deleteValue = useCallback(() => {
+    const url = new URL(window.location.href)
+
+    url.searchParams.delete(name)
+
+    historyAction(url.toString())
+  }, [historyAction, name])
+
+  return [value, setValue, deleteValue]
 }
 
+/**
+ * This hook provides the param value, a setter and a delete function for
+ * managing the param.
+ *
+ * Please consider that it calls the `pushState` method under the hood to
+ * insert a new state into the browser history.
+ * @param name The param name
+ * @returns [the param value, the setter function, the delete function]
+ */
 export function useQueryParam(name: string): UseQueryParam {
-  return useQueryParamBase(browserHistory.push, name)
+  return useQueryParamBase(browserHistory.push, name, false)
 }
 
+/**
+ * This hook provides the param value, a setter and a delete function for
+ * managing the param.
+ *
+ * Please consider that it calls the `replaceState` method under the hood to
+ * replace the state on the browser history.
+ * @param name The param name
+ * @returns [the param value, the setter function, the delete function]
+ */
 export function useReplaceQueryParam(name: string): UseQueryParam {
-  return useQueryParamBase(browserHistory.replace, name)
+  return useQueryParamBase(browserHistory.replace, name, false)
+}
+
+/**
+ * This hook provides the param value, a setter and a delete function for
+ * managing the param.
+ *
+ * Please consider that it calls the `pushState` method under the hood to insert
+ * a new state into the browser history.
+ *
+ * Also, this hook deletes the param automatically if you pass an empty string.
+ * @param name The param name
+ * @returns [the param value, the setter function, the delete function]
+ */
+export function useAutoQueryParam(name: string): UseQueryParam {
+  return useQueryParamBase(browserHistory.push, name, true)
+}
+
+/**
+ * This hook provides the param value, a setter and a delete function for
+ * managing the param.
+ *
+ * Please consider that it calls the `replaceState` method under the hood to
+ * replace the state on the browser history.
+ *
+ * Also, this hook deletes the param automatically if you pass an empty string.
+ * @param name The param name
+ * @returns [the param value, the setter function, the delete function]
+ */
+export function useReplaceAutoQueryParam(name: string): UseQueryParam {
+  return useQueryParamBase(browserHistory.replace, name, true)
 }
