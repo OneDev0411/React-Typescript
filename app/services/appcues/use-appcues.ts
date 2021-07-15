8c94d62288393a13198c19d118c169808605524c
OneDev0411 <cryptoapple1109@gmail.com>
2021-07-15 10:58:02 +0430

    refactor(appcues): cleaning the code by using helper functions (#5355)

--- app/services/appcues/use-appcues.ts
+++ app/services/appcues/use-appcues.ts
@@ -1,84 +1,47 @@
 import { useEffect, useMemo } from 'react'
 import { useSelector } from 'react-redux'
-import moment from 'moment'
 
-import { selectUserHasAccess } from 'selectors/acl'
-import { selectUserUnsafe } from 'selectors/user'
-import { getOAuthAccounts } from 'models/o-auth-accounts/get-o-auth-accounts'
+import { selectUserAccessList, selectUserUnsafe } from 'selectors/user'
 import { IAppState } from 'reducers'
-import { OAuthProvider } from 'constants/contacts'
-import { ACL } from 'constants/acl'
 
-interface Location {
-  pathname: string
-}
+import { AppcuesUserInfo, Location } from './types'
+import { prepareAndSendUserData } from './helpers'
 
 export function useAppcues() {
-  const accessList = useSelector((state: IAppState) =>
-    Object.keys(ACL).reduce((acc, access) => {
-      const hasAccess = selectUserHasAccess(state, ACL[access])
-
-      return {
-        ...acc,
-        [`has${ACL[access]}Access`]: hasAccess
-      }
-    }, {})
-  )
+  const accessList = useSelector(selectUserAccessList)
 
   const location = useSelector<IAppState, Location | null>(
     state => state.data.location
   )
   const user = useSelector(selectUserUnsafe)
 
-  const userInfoToWatch = useMemo(
-    () =>
-      user && user.id
-        ? {
-            id: user.id,
-            firstName: user.first_name,
-            fullName: user.display_name,
-            email: user.email,
-            userType: user.user_type,
-            createdAt: user.created_at
-          }
-        : null,
-    [user]
-  )
+  const userInfoToWatch = useMemo<Nullable<AppcuesUserInfo>>(() => {
+    return user?.id
+      ? {
+          id: user.id,
+          firstName: user.first_name,
+          fullName: user.display_name,
+          email: user.email,
+          userType: user.user_type,
+          createdAt: user.created_at
+        }
+      : null
+  }, [
+    user?.id,
+    user?.first_name,
+    user?.display_name,
+    user?.email,
+    user?.user_type,
+    user?.created_at
+  ])
 
   const pathname = location ? location.pathname : null
 
   useEffect(() => {
-    if (!pathname) {
+    if (!pathname || !userInfoToWatch) {
       return
     }
 
-    if (userInfoToWatch) {
-      ;(async function prepareAndSendUserData() {
-        const { id, createdAt, ...userInfo } = userInfoToWatch
-        const google = await getOAuthAccounts(OAuthProvider.Google)
-        const outlook = await getOAuthAccounts(OAuthProvider.Outlook)
-
-        const starts = moment(createdAt * 1000)
-        const ends = moment()
-        const accountAge = moment.duration(ends.diff(starts))
-
-        const userData = {
-          ...userInfo,
-          accountAgeInDays:
-            accountAge.days() +
-            accountAge.months() * 30 +
-            accountAge.years() * 365,
-          gmailOrOutlookSynced: Boolean(google.length || outlook.length),
-          ...accessList
-        }
-
-        // Normally what we should be doing here is to call Appcues.Page(), but
-        // behind the scenes, Appcues.identify() also invokes that function.
-        // Reac more: https://docs.appcues.com/article/161-javascript-api
-        window.AppcuesReady(() => {
-          window.Appcues.identify(id, userData)
-        })
-      })()
-    }
+    prepareAndSendUserData(accessList, userInfoToWatch)
   }, [pathname, userInfoToWatch, accessList])
 }
