a4b332775b8860506b8c225ac3ed2d046a612249
OneDev0411 <cryptoapple1109@gmail.com>
2021-07-15 13:52:39 +0430

    refactor(appcues): replace useEffect with useDeepCompareEffect (#5355)

--- app/services/appcues/use-appcues.ts
+++ app/services/appcues/use-appcues.ts
@@ -1,5 +1,4 @@
-import { useMemo } from 'react'
-import { useDeepCompareEffect } from 'react-use'
+import { useMemo, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
 import { selectUserAccessList, selectUserUnsafe } from 'selectors/user'
@@ -38,10 +37,7 @@ export function useAppcues() {
 
   const pathname = location ? location.pathname : null
 
-  // TODO: Use `useEffect` instead of `useDeepCompareEffect`
-  // when store.teams unwanted update is fixed
-  // https://gitlab.com/rechat/web/-/issues/5355#note_626397173
-  useDeepCompareEffect(() => {
+  useEffect(() => {
     if (!pathname || !userInfoToWatch) {
       return
     }
