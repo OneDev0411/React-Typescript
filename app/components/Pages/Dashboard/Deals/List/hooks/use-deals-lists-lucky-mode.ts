72fbc167a4049e1242df4001b9f696a1f3bb5616
OneDev0411 <cryptoapple1109@gmail.com>
2021-07-16 15:40:37 +0430

    fix(deals): change the comment format (#5291)

--- app/components/Pages/Dashboard/Deals/List/hooks/use-deals-lists-lucky-mode.ts
+++ app/components/Pages/Dashboard/Deals/List/hooks/use-deals-lists-lucky-mode.ts
@@ -14,14 +14,16 @@ function useDealsListsLuckyMode(deals: IDeal[], isLoading: boolean) {
 
   useEffect(() => {
     if (isLuckyMode && !isLoading) {
-      // The deal close button uses the history.goBack() method to redirect the user
-      // to the previous page. In this case, this method does not work, and it will
-      // redirect the user to the deal page again because of the lucky param.
-      // So we need to delete the lucky param and then go to the deal page to prevent
-      // the redirect issue.
-      // Besides, we need to delete the param if there is multiple items on the
-      // results. Because we don't want to redirect the user to the first deal if he
-      // switch to another tab or change the search term.
+      /**
+       * The deal close button uses the history.goBack() method to redirect the user
+       * to the previous page. In this case, this method does not work, and it will
+       * redirect the user to the deal page again because of the lucky param.
+       * So we need to delete the lucky param and then go to the deal page to prevent
+       * the redirect issue.
+       * Besides, we need to delete the param if there is multiple items on the
+       * results. Because we don't want to redirect the user to the first deal if he
+       * switch to another tab or change the search term.
+       */
       deleteLucky()
 
       if (deals.length === 1) {
