8c0feaa99a67a73fcce496eddf3ff6f1fe81449c
OneDev0411 <cryptoapple1109@gmail.com>
2021-07-18 11:00:20 +0430

    refactor(appcues): add DEFAULT_APPCUES_USER_ACL (#5355)

--- app/services/appcues/helpers.ts
+++ app/services/appcues/helpers.ts
@@ -1,32 +1,34 @@
 import { differenceInDays } from 'date-fns'
 
-import { AccessRecord } from '@app/constants/acl'
+import { ACL } from '@app/constants/acl'
 import { getOAuthAccounts } from 'models/o-auth-accounts/get-o-auth-accounts'
 import { OAuthProvider } from 'constants/contacts'
-import { ACL } from 'constants/acl'
 
 import { AppcuesUserInfo } from './types'
 
-export function createAppcuesAccessList(
-  userACL: IPermission[],
-  allACL: AccessRecord
-) {
-  const AllACLs = Object.keys(allACL).reduce((acc, cur) => {
+export const DEFAULT_APPCUES_USER_ACL = Object.values(ACL).reduce(
+  (acc, cur) => {
     return {
       ...acc,
-      [`has${allACL[cur]}Access`]: false
+      [`has${ACL[cur]}Access`]: false
     }
-  }, {})
+  },
+  {} as Record<`has${keyof typeof ACL}Access`, boolean>
+)
 
-  const mappedAccessList: Record<string, boolean> = userACL.reduce(
-    (acc, cur) => ({
-      ...acc,
-      [`has${cur}Access`]: true
-    }),
-    AllACLs
-  )
+export function createAppcuesAccessList(
+  userACL: IPermission[],
+  defaultUserAcl = DEFAULT_APPCUES_USER_ACL
+) {
+  const appcuesAccessList: typeof DEFAULT_APPCUES_USER_ACL = {
+    ...defaultUserAcl
+  }
 
-  return mappedAccessList
+  userACL.forEach(acl => {
+    appcuesAccessList[acl] = true
+  })
+
+  return appcuesAccessList
 }
 
 export async function prepareAndSendUserData(
@@ -37,7 +39,7 @@ export async function prepareAndSendUserData(
   const google = await getOAuthAccounts(OAuthProvider.Google)
   const outlook = await getOAuthAccounts(OAuthProvider.Outlook)
 
-  const mappedAccessList = createAppcuesAccessList(accessList, ACL)
+  const mappedAccessList = createAppcuesAccessList(accessList)
 
   const userData = {
     ...restInfo,
