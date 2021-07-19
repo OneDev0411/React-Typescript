acde30f944208ce9717c286b0ac86e20d0d98620
OneDev0411 <cryptoapple1109@gmail.com>
2021-07-19 20:21:25 +0430

    refactor(appcues): change AppcuesUserAccessList type (#5355)

--- app/services/appcues/types.ts
+++ app/services/appcues/types.ts
@@ -1,5 +1,3 @@
-import { ACL } from '@app/constants/acl'
-
 export interface AppcuesUserInfo {
   firstName: string | null
   fullName: string
@@ -8,7 +6,4 @@ export interface AppcuesUserInfo {
   createdAt: number
 }
 
-export type AppcuesUserAccessList = Record<
-  `has${typeof ACL[keyof typeof ACL]}Access`,
-  boolean
->
+export type AppcuesUserAccessList = Record<`has${IPermission}Access`, boolean>
