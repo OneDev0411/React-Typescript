77c7f3bba8654d9260b4f9faa5e290dd46634276
OneDev0411 <cryptoapple1109@gmail.com>
2021-07-19 09:32:33 +0430

    fix(deals): move the logic to outside the if (#5291)

--- app/components/Pages/Dashboard/Deals/List/hooks/use-deals-lists-lucky-mode.ts
+++ app/components/Pages/Dashboard/Deals/List/hooks/use-deals-lists-lucky-mode.ts
@@ -13,22 +13,24 @@ function useDealsListsLuckyMode(deals: IDeal[], isLoading: boolean) {
   const isLuckyMode = !!lucky
 
   useEffect(() => {
-    if (isLuckyMode && !isLoading) {
-      /**
-       * The deal close button uses the history.goBack() method to redirect the user
-       * to the previous page. In this case, this method does not work, and it will
-       * redirect the user to the deal page again because of the lucky param.
-       * So we need to delete the lucky param and then go to the deal page to prevent
-       * the redirect issue.
-       * Besides, we need to delete the param if there is multiple items on the
-       * results. Because we don't want to redirect the user to the first deal if he
-       * switch to another tab or change the search term.
-       */
-      deleteLucky()
+    if (!isLuckyMode || isLoading) {
+      return
+    }
 
-      if (deals.length === 1) {
-        goTo(`/dashboard/deals/${deals[0].id}`)
-      }
+    /**
+     * The deal close button uses the history.goBack() method to redirect the user
+     * to the previous page. In this case, this method does not work, and it will
+     * redirect the user to the deal page again because of the lucky param.
+     * So we need to delete the lucky param and then go to the deal page to prevent
+     * the redirect issue.
+     * Besides, we need to delete the param if there is multiple items on the
+     * results. Because we don't want to redirect the user to the first deal if he
+     * switch to another tab or change the search term.
+     */
+    deleteLucky()
+
+    if (deals.length === 1) {
+      goTo(`/dashboard/deals/${deals[0].id}`)
     }
   }, [deals, deleteLucky, isLoading, isLuckyMode])
 }
