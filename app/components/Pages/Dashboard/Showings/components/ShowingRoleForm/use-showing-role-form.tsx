f77b8087097af4648c170693f8b5f0e1bc739f97
OneDev0411 <cryptoapple1109@gmail.com>
2021-07-23 12:04:53 +0430

    fix(showing): change the checkbox label (#5246)

--- app/components/Pages/Dashboard/Showings/components/ShowingRoleForm/use-showing-role-form.tsx
+++ app/components/Pages/Dashboard/Showings/components/ShowingRoleForm/use-showing-role-form.tsx
@@ -48,7 +48,7 @@ function useShowingRoleForm({
           (!hideAddToContactCheckbox || !!values.contact)
         }
         saveToContactCheckboxLabel={
-          values.contact ? 'Update the contact' : 'Add to contact'
+          values.contact ? 'Update the contact' : 'Add to my contact'
         }
       />
     )
