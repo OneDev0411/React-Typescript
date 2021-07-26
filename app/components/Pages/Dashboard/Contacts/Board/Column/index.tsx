50da6d0105a8a10674704c30286ec6b1f849843c
OneDev0411 <cryptoapple1109@gmail.com>
2021-07-27 01:16:09 +0300

    feat(boards): virtualize

--- app/components/Pages/Dashboard/Contacts/Board/Column/index.tsx
+++ app/components/Pages/Dashboard/Contacts/Board/Column/index.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import { useMemo, memo } from 'react'
 
 import { Box, Chip, makeStyles, Theme, Typography } from '@material-ui/core'
 import Skeleton from '@material-ui/lab/Skeleton'
@@ -8,10 +8,13 @@ import {
   DroppableProvided,
   DroppableStateSnapshot
 } from 'react-beautiful-dnd'
+import AutoSizer from 'react-virtualized-auto-sizer'
+import { areEqual } from 'react-window'
 
+import VirtualList from '@app/views/components/VirtualList'
 import { SvgIcon } from 'components/SvgIcons/SvgIcon'
 
-import { ColumnCard } from './Card'
+import { ColumnCard, ColumnDraggableCard } from './Card'
 
 const useStyles = makeStyles(
   (theme: Theme) => ({
@@ -38,10 +41,6 @@ const useStyles = makeStyles(
     body: {
       // https://github.com/atlassian/react-beautiful-dnd/issues/1640
       height: `calc(100% - ${theme.spacing(6)}px)`
-    },
-    innerBody: {
-      height: '100%',
-      overflowY: 'auto'
     },
     placeholder: {
       margin: theme.spacing(0.5),
@@ -114,49 +113,155 @@ export function BoardColumn({
         </Box>
       </div>
 
-      <Droppable
-        droppableId={id}
-        type="column"
-        direction="vertical"
-        isDropDisabled={!droppable}
-        ignoreContainerClipping
-        isCombineEnabled={false}
-      >
-        {(provided: DroppableProvided, snapshot: DroppableStateSnapshot) => (
-          <div
-            className={classes.body}
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-          >
-            <div className={classes.innerBody}>
-              {snapshot.isDraggingOver && (
-                <div className={classes.placeholder} />
+      {isLoading && (
+        <div className={classes.body}>
+          {new Array(randomNumber).fill(null).map((_, index) => (
+            <Skeleton
+              key={index}
+              animation="wave"
+              className={classes.skeleton}
+            />
+          ))}
+        </div>
+      )}
+
+      <div className={classes.body}>
+        <AutoSizer>
+          {({ width, height }) => (
+            <Droppable
+              droppableId={id}
+              type="column"
+              mode="virtual"
+              direction="vertical"
+              isDropDisabled={!droppable}
+              ignoreContainerClipping
+              isCombineEnabled={false}
+              renderClone={(provided, snapshot, rubric) => (
+                <ColumnCard
+                  provided={provided}
+                  isDragging={snapshot.isDragging}
+                  contact={list[rubric.source.index]}
+                />
               )}
-
-              {isLoading
-                ? new Array(randomNumber)
-                    .fill(null)
-                    .map((_, index) => (
-                      <Skeleton
-                        key={index}
-                        animation="wave"
-                        className={classes.skeleton}
-                      />
-                    ))
-                : list.map((contact, index) => (
-                    <ColumnCard
-                      key={contact.id}
-                      columnId={id}
-                      rowId={index}
-                      contact={contact}
-                    />
-                  ))}
-            </div>
-
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
+            >
+              {(
+                provided: DroppableProvided,
+                snapshot: DroppableStateSnapshot
+              ) => (
+                <div ref={provided.innerRef} {...provided.droppableProps}>
+                  <VirtualList
+                    width={width}
+                    height={height}
+                    itemCount={list.length}
+                    itemData={
+                      {
+                        rows: list,
+                        columnId: id
+                      } as React.ComponentProps<typeof ColumnCard>['data']
+                    }
+                    threshold={2}
+                    itemSize={() => 100}
+                    overscanCount={3}
+                  >
+                    {ColumnDraggableCard}
+                  </VirtualList>
+                </div>
+              )}
+            </Droppable>
+          )}
+        </AutoSizer>
+      </div>
     </div>
   )
 }
+
+// export function Column({
+//   id,
+//   title,
+//   list,
+//   isLoading,
+//   droppable = true
+// }: Props) {
+//   const classes = useStyles()
+
+//   const randomNumber = useMemo(() => Math.floor(Math.random() * 6) + 1, [])
+
+//   return (
+//     <div className={classes.root}>
+//       <div className={classes.head}>
+//         {isLoading ? (
+//           <Skeleton animation="wave" className={classes.titleSkeleton} />
+//         ) : (
+//           <Chip label={title} size="small" />
+//         )}
+
+//         <Box
+//           display="flex"
+//           justifyContent="flex-end"
+//           alignItems="center"
+//           flexGrow={1}
+//         >
+//           <Box display="flex" alignItems="center" mr={1}>
+//             <SvgIcon path={mdiCardsOutline} />
+//             <Box ml={0.5}>
+//               {isLoading ? (
+//                 <Skeleton animation="wave" width="16px" />
+//               ) : (
+//                 <Typography variant="subtitle1">{list.length}</Typography>
+//               )}
+//             </Box>
+//           </Box>
+//         </Box>
+//       </div>
+
+//       <Droppable
+//         droppableId={id}
+//         type="column"
+//         direction="vertical"
+//         isDropDisabled={!droppable}
+//         ignoreContainerClipping
+//         isCombineEnabled={false}
+//       >
+//         {(provided: DroppableProvided, snapshot: DroppableStateSnapshot) => (
+//           <div
+//             className={classes.body}
+//             ref={provided.innerRef}
+//             {...provided.droppableProps}
+//           >
+//             <>
+//               {snapshot.isDraggingOver && (
+//                 <div className={classes.placeholder} />
+//               )}
+//
+
+//               <AutoSizer>
+//                 {({ width, height }) => (
+//                   <VirtualList
+//                     width={width}
+//                     height={height}
+//                     itemCount={list.length}
+//                     itemData={
+//                       {
+//                         rows: list,
+//                         columnId: id
+//                       } as React.ComponentProps<typeof ColumnCard>['data']
+//                     }
+//                     threshold={2}
+//                     itemSize={() => 120}
+//                     overscanCount={3}
+//                   >
+//                     {ColumnCard}
+//                   </VirtualList>
+//                 )}
+//               </AutoSizer>
+//             </>
+
+//             {/* {provided.placeholder} */}
+//           </div>
+//         )}
+//       </Droppable>
+//     </div>
+//   )
+// }
+
+// export const BoardColumn = memo(Column, areEqual)
