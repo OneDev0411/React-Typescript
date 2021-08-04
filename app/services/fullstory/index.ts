533af32595add8fff1039bc4315c3a4e8caf708f
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-04 14:33:08 +0300

    fix(fullstory): better handling for production environments condition  (#5449)

--- app/services/fullstory/index.ts
+++ app/services/fullstory/index.ts
@@ -1,14 +1,16 @@
-import { isLocalhost } from '@app/utils/location-hostname'
-
 export function setupFullStory(user: IUser) {
-  if (window && window.FS && user) {
+  // We only record sessions on FullStory if it's not on development environment
+  // That includes beta.rechat.com
+
+  if (
+    process.env.NODE_ENV === 'production' &&
+    process.env.APP_URL === 'https://app.rechat.com' &&
+    window?.FS &&
+    user
+  ) {
     window.FS.identify(user.id, {
       name: user.display_name,
       email: user.email
     })
-
-    if (isLocalhost()) {
-      window.FS.shutdown()
-    }
   }
 }
--- app/utils/location-hostname.ts
+++
@@ -1,6 +0,0 @@
-export function isLocalhost() {
-  return (
-    window.location.hostname === 'localhost' ||
-    window.location.hostname === '127.0.0.1'
-  )
-}
