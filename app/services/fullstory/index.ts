15eddc0387cf3f2d78f5eb469bf1030ce336e357
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-04 14:00:49 +0300

    chore(fullstory): stop recording session if localhost (#5449)

--- app/services/fullstory/index.ts
+++ app/services/fullstory/index.ts
@@ -1,8 +1,14 @@
+import { isLocalhost } from '@app/utils/location-hostname'
+
 export function setupFullStory(user: IUser) {
   if (window && window.FS && user) {
     window.FS.identify(user.id, {
       name: user.display_name,
       email: user.email
     })
+
+    if (isLocalhost()) {
+      window.FS.shutdown()
+    }
   }
 }
---
+++ app/utils/location-hostname.ts
@@ -0,0 +1,6 @@
+export function isLocalhost() {
+  return (
+    window.location.hostname === 'localhost' ||
+    window.location.hostname === '127.0.0.1'
+  )
+}
