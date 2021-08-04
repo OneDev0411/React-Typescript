40868a1b2195727d355d40a0fe65ac93f7aaddff
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-04 14:58:27 +0300

    fix(fullstory): going back to previous approach, shutting down  (#5449)

--- app/services/fullstory/index.ts
+++ app/services/fullstory/index.ts
@@ -2,15 +2,17 @@ export function setupFullStory(user: IUser) {
   // We only record sessions on FullStory if it's not on development environment
   // That includes beta.rechat.com
 
-  if (
-    process.env.NODE_ENV === 'production' &&
-    process.env.APP_URL === 'https://app.rechat.com' &&
-    window?.FS &&
-    user
-  ) {
+  if (window?.FS && user) {
     window.FS.identify(user.id, {
       name: user.display_name,
       email: user.email
     })
+
+    if (
+      process.env.NODE_ENV === 'production' &&
+      process.env.APP_URL === 'https://app.rechat.com'
+    ) {
+      window.FS.shutdown()
+    }
   }
 }
