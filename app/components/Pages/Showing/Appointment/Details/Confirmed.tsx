8d9e8a96bdd607ae2791753202f99a717bfadb3e
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-05 18:54:41 +0430

    feat(showings): add google and outlook calendar buttons to showing buyer

--- app/components/Pages/Showing/Appointment/Details/Confirmed.tsx
+++ app/components/Pages/Showing/Appointment/Details/Confirmed.tsx
@@ -1,11 +1,21 @@
+import { useState } from 'react'
+
 import {
   Stepper,
   Step,
   StepLabel,
   StepContent,
+  Grid,
+  Button,
+  Fade,
   Typography,
   useTheme
 } from '@material-ui/core'
+import { mdiCalendar } from '@mdi/js'
+import { CalendarEvent } from 'calendar-link'
+
+import AddToCalendarButton from '@app/views/components/AddToCalendarButton'
+import { SvgIcon } from '@app/views/components/SvgIcons/SvgIcon'
 
 import { getFormattedAppointmentDateTime } from '../utils'
 
@@ -15,6 +25,15 @@ interface Props {
 
 export default function ShowingAppointmentConfirmed({ appointment }: Props) {
   const theme = useTheme()
+  const [isCalendarButtonsVisible, setIsCalendarButtonsVisible] =
+    useState<boolean>(false)
+
+  const addTocalendarButtonProps: CalendarEvent = {
+    title: appointment.showing.title,
+    location: appointment.showing.listing?.property.address.full_address,
+    start: appointment.time,
+    duration: [appointment.showing.duration, 'seconds']
+  }
 
   return (
     <Stepper orientation="vertical">
@@ -23,12 +42,57 @@ export default function ShowingAppointmentConfirmed({ appointment }: Props) {
           <Typography variant="h6">You're All Set!</Typography>
         </StepLabel>
         <StepContent>
-          <Typography variant="body1" color="textSecondary">
-            You set an appointment for{' '}
-            <span style={{ color: theme.palette.text.primary }}>
-              {getFormattedAppointmentDateTime(appointment)}
-            </span>
-          </Typography>
+          <Grid container direction="column" spacing={2}>
+            <Grid item>
+              <Typography variant="body1" color="textSecondary">
+                You set an appointment for{' '}
+                <span style={{ color: theme.palette.text.primary }}>
+                  {getFormattedAppointmentDateTime(appointment)}
+                </span>
+              </Typography>
+            </Grid>
+            <Grid
+              container
+              item
+              direction="row"
+              alignItems="center"
+              spacing={2}
+            >
+              <Grid item>
+                <Button
+                  variant="text"
+                  onClick={() => setIsCalendarButtonsVisible(true)}
+                >
+                  <SvgIcon path={mdiCalendar} />
+                  &nbsp;Add To Calendar
+                </Button>
+              </Grid>
+              <Grid item>
+                <Fade in={isCalendarButtonsVisible} mountOnEnter>
+                  <div>
+                    <AddToCalendarButton
+                      calendar="Google"
+                      {...addTocalendarButtonProps}
+                    >
+                      Google
+                    </AddToCalendarButton>
+                  </div>
+                </Fade>
+              </Grid>
+              <Grid item>
+                <Fade in={isCalendarButtonsVisible} mountOnEnter>
+                  <div>
+                    <AddToCalendarButton
+                      calendar="Outlook"
+                      {...addTocalendarButtonProps}
+                    >
+                      Outlook
+                    </AddToCalendarButton>
+                  </div>
+                </Fade>
+              </Grid>
+            </Grid>
+          </Grid>
         </StepContent>
       </Step>
     </Stepper>
