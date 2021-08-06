8a1524ebfe8a927d24868f3beec0289b50219e25
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-06 12:32:59 +0300

    fix(pdf.js): fix worker

--- app/utils/import-pdf-js.js
+++ app/utils/import-pdf-js.js
@@ -1,9 +1,8 @@
 export default async function importPdfJs() {
   const PDFJS = await import('pdfjs-dist')
-  // const pdfjsWorker = await import('pdfjs-dist/build/pdf.worker.entry')
+  const pdfjsWorker = await import('pdfjs-dist/build/pdf.worker.entry')
 
-  PDFJS.GlobalWorkerOptions.workerSrc =
-    'https://pdfjs-dist.surge.sh/pdf.worker.min.98bbbce.js'
+  PDFJS.GlobalWorkerOptions.workerSrc = pdfjsWorker
 
   return PDFJS
 }
