69f11e86375d4d07ed60be34c29945309d95f1b3
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-06 18:22:12 +0300

    fix(date picker): fix margin of toolbar

--- app/views/components/DatePicker/YearMonthList/styled.js
+++ app/views/components/DatePicker/YearMonthList/styled.js
@@ -3,7 +3,8 @@ import styled from 'styled-components'
 export const Container = styled.div`
   position: relative;
   border-bottom: none !important;
-  height: 20px !important;
   text-align: left !important;
   padding-left: 0 !important;
+  height: unset !important;
+  margin: ${({ theme }) => theme.spacing(0.5, 2)};
 `
