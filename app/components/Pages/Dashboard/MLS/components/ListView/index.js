7dea8746f845c26872988b010474654d0b21fcfc
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-18 18:18:48 +0430

    feat(mls): add pagination to the list view (#5473)

--- app/components/Pages/Dashboard/MLS/components/ListView/index.js
+++ app/components/Pages/Dashboard/MLS/components/ListView/index.js
@@ -1,8 +1,19 @@
-import React, { useCallback, memo, useState } from 'react'
+import React, { useCallback, useState, memo, useEffect } from 'react'
 
-import { Box, Checkbox, useTheme } from '@material-ui/core'
+import {
+  Grid,
+  Checkbox,
+  useTheme,
+  makeStyles,
+  Typography
+} from '@material-ui/core'
+import Pagination from '@material-ui/lab/Pagination'
 import pluralize from 'pluralize'
 
+import {
+  getListingsPage,
+  getResultsCountText
+} from '@app/components/Pages/Dashboard/MLS/helpers/pagination-utils'
 import Table from 'components/Grid/Table'
 import { useGridStyles } from 'components/Grid/Table/styles'
 import { ListingDetailsModal } from 'components/ListingDetailsModal'
@@ -10,15 +21,39 @@ import { useListSelection } from 'components/ListSelection/use-list-selection'
 import LoadingComponent from 'components/Spinner'
 
 const BASE_URL = '/dashboard/mls'
+const PAGE_SIZE = 30
 
 import { ShareListings } from '../ShareListings'
 import ZeroState from '../ZeroState'
 
 import { Address } from './columns/Address'
 
-const ListView = ({ sortedListings, listings, isFetching }) => {
+const useStyles = makeStyles(
+  theme => ({
+    container: {
+      marginBottom: theme.spacing(1)
+    },
+    resultsCountContainer: {
+      paddingLeft: theme.spacing(1),
+      marginBottom: theme.spacing(2),
+      color: theme.palette.text.secondary
+    },
+    paginationContainer: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignContent: 'center',
+      margin: theme.spacing(5, 0),
+      paddingBottom: theme.spacing(8)
+    }
+  }),
+  { name: 'ListView' }
+)
+const ListView = ({ sortedListings, isFetching }) => {
   const theme = useTheme()
+  const classes = useStyles()
+
   const gridClasses = useGridStyles()
+  const [currentPage, setCurrentPage] = useState(1)
   const { selections, toggleItem } = useListSelection()
   const [selectedListingId, setSelectedListingId] = useState(null)
   const [isListingDetailsModalOpen, setIsListingDetailsModalOpen] =
@@ -113,41 +148,91 @@ const ListView = ({ sortedListings, listings, isFetching }) => {
     }
   ]
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+
+  useEffect(() => {
+    setCurrentPage(1)
+    scrollToTop()
+  }, [sortedListings])
+
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value)
+    scrollToTop()
+  }
+
+  const renderTable = () => {
+    if (isFetching) {
+      return <LoadingComponent />
+    }
+
+    if (!sortedListings.length) {
+      return <ZeroState />
+    }
+
+    return (
+      <Table
+        columns={columns}
+        rows={getListingsPage(sortedListings, currentPage, PAGE_SIZE)}
+        selection={{
+          render: ({ row: listing }) => (
+            <Checkbox
+              checked={selections.some(item => item.id === listing.id)}
+              onChange={() => toggleItem(listing)}
+            />
+          ),
+          columnProps: {
+            width: theme.spacing(4)
+          }
+        }}
+        classes={{
+          row: gridClasses.row
+        }}
+      />
+    )
+  }
+
+  const isListingsDisplayed = !isFetching && sortedListings.length
+
   return (
     <>
-      {!sortedListings.length ? (
-        <ZeroState />
-      ) : (
-        <Box pb={1}>
-          <Table
-            columns={columns}
-            rows={sortedListings}
-            totalRows={listings.info.total}
-            loading={isFetching ? 'middle' : null}
-            LoadingStateComponent={LoadingComponent}
-            selection={{
-              render: ({ row: listing }) => (
-                <Checkbox
-                  checked={selections.some(item => item.id === listing.id)}
-                  onChange={() => toggleItem(listing)}
-                />
-              ),
-              columnProps: {
-                width: theme.spacing(4)
-              }
-            }}
-            classes={{
-              row: gridClasses.row
-            }}
-          />
-          <ListingDetailsModal
-            isOpen={isListingDetailsModalOpen}
-            listingId={selectedListingId}
-            closeHandler={closeListingDetailsModal}
-          />
-          <ShareListings />
-        </Box>
-      )}
+      <Grid className={classes.container}>
+        <Grid container className={classes.resultsCountContainer}>
+          {isListingsDisplayed ? (
+            <Typography variant="body3">
+              {getResultsCountText(
+                sortedListings.length,
+                currentPage,
+                PAGE_SIZE
+              )}
+            </Typography>
+          ) : null}
+        </Grid>
+        {renderTable()}
+        {isListingsDisplayed ? (
+          <Grid container className={classes.paginationContainer}>
+            <Pagination
+              page={currentPage}
+              onChange={handlePageChange}
+              count={Math.ceil(sortedListings.length / PAGE_SIZE)}
+              variant="outlined"
+              color="primary"
+              size="large"
+              shape="rounded"
+            />
+          </Grid>
+        ) : null}
+        <ListingDetailsModal
+          isOpen={isListingDetailsModalOpen}
+          listingId={selectedListingId}
+          closeHandler={closeListingDetailsModal}
+        />
+        <ShareListings />
+      </Grid>
     </>
   )
 }
