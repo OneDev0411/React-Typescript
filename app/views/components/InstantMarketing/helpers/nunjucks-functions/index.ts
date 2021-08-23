0c18412517e5d2aa8121ba3dfc254ff625911dd5
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-23 18:59:04 +0430

    fix(mc): fix listing url merge conflict issues

--- app/views/components/InstantMarketing/helpers/nunjucks-functions/index.ts
+++ app/views/components/InstantMarketing/helpers/nunjucks-functions/index.ts
@@ -1,6 +1,9 @@
 import _get from 'lodash/get'
 
 import { DEFAULT_BRAND_PALETTE } from 'utils/constants'
+import flattenBrand from 'utils/flatten-brand'
+
+import config from '../../../../../../config/public'
 
 export function getListingUrl(
   activeBrand: IBrand,
@@ -18,7 +21,12 @@ export function getListingUrl(
     return listing.url
   }
 
-  return ''
+  const brand = flattenBrand(activeBrand)
+  const listing_url = brand && brand.messages ? brand.messages.listing_url : ''
+
+  return listing_url
+    ? listing_url.replace('%1', listing.mls_number)
+    : `${config.app.url}/dashboard/mls/${listing.id}`
 }
 
 export function get(brand: IBrand, key: BrandMarketingPaletteKey): string {
