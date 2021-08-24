958a34be4b88c0baa2df1ff599875edeb689dd78
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-24 17:55:02 +0430

    refactor(mls2): fetchResults on state.search changes

--- app/components/Pages/Dashboard/Properties/hooks/useFetchListings.ts
+++ app/components/Pages/Dashboard/Properties/hooks/useFetchListings.ts
@@ -1,50 +1,88 @@
+import { useEffect, useRef } from 'react'
+
 import useThunkReducer from 'react-hook-thunk-reducer'
-import { useDebounce } from 'react-use'
+import { useSelector } from 'react-redux'
+
+import { createValertQueryString } from '@app/components/Pages/Dashboard/Properties/helpers/get-listings-helpers'
+import { pointFromBounds } from '@app/components/Pages/Dashboard/Properties/helpers/map-helpers'
+import {
+  PROPOSED_AGENT_ZOOM_LEVEL,
+  QUERY_LIMIT
+} from '@app/components/Pages/Dashboard/Properties/mapOptions'
+import api from '@app/models/listings/search'
+import { IAppState } from 'reducers'
 
 import { setListings, setIsLoading } from '../context/actions'
-import { reducer, initialState } from '../context/reducers'
+import { reducer, initialState, ListingsState } from '../context/reducers'
 
-const DEBOUNCE_MS = 500
+export default function useFetchListings(
+  userInitialState: Partial<ListingsState>
+) {
+  const [state, dispatch] = useThunkReducer(reducer, {
+    ...initialState,
+    ...userInitialState
+  })
 
-export default function useFetchListings() {
-  // Instead of using `useReducer` which doesn't support thunks
-  const [state, dispatch] = useThunkReducer(reducer, initialState)
+  const brand = useSelector<IAppState, IBrand>(({ brand }) => brand)
+
+  // TO fix calling GoogleMap onChange at initialization
+  // https://github.com/google-map-react/google-map-react/blob/master/DOC.md
+  const isInitCall = useRef(true)
 
   // Upon each change in search we fetch new results
-  useDebounce(
-    () => {
-      async function fetchResults() {
-        // 1. if state.search.drawing is changed
-        //    it means user has drawn a polygon, so we search in that area only
-        //    having a drawing always takes precedence over `bounds` if they're both set.
-        //    Consider the case where user has set a drawing on map and search results
-        //    are shown for that area, then user pans/zooms map, we should not search
-        //    for the newly set `bounds` and instead should wait until user clicks on
-        //    `remove drawing` then we can use latest `bounds` that are set.
-        //
-        // 2. if state.search.bounds is changed
-        //    it means map is moved (by zooming or panning), so we do search again
-        //    NOTE: consider case 1
-        //
-        // 3. if state.search.filter changed
-        //    ...
+  useEffect(() => {
+    async function fetchResults() {
+      console.log("Some params in search changed. Let's fetch new results!")
 
-        console.log("Some params in search changed. Let's fetch new results!")
+      /* if state.search.drawing is present
+       *  it means user has drawn a polygon, so we search in that area only
+       *  having a drawing always takes precedence over `bounds` if they're both set.
+       *  Consider the case where user has set a drawing on map and search results
+       *  are shown for that area, then user pans/zooms map, we should not search
+       *  for the newly set `bounds` and instead should wait until user clicks on
+       *  `remove drawing` then we can use latest `bounds` that are set.
+       */
+      dispatch(setIsLoading(true))
 
-        dispatch(setIsLoading(true))
+      const valertQueryString = createValertQueryString(
+        state.map.zoom,
+        PROPOSED_AGENT_ZOOM_LEVEL,
+        brand
+      )
 
-        // TODO: Load listings from model
-        // const results = await listings // await fetch
-        const results = []
+      const points =
+        state.search.drawing.length > 0
+          ? state.search.drawing
+          : pointFromBounds(state.search.bounds)
 
-        dispatch(setListings(results))
-        dispatch(setIsLoading(false))
+      const valertOptions = {
+        ...state.search.filters,
+        points,
+        offices: state.search.office ?? [state.search.office],
+        postal_codes: null,
+        limit: QUERY_LIMIT
       }
+
+      const response = await api.getListings.byValert(
+        valertOptions,
+        valertQueryString
+      )
+
+      dispatch(setListings(response.data, response.info))
+      dispatch(setIsLoading(false))
+    }
+
+    // Prevent fetchResults on initialCall to fix calling GoogleMap onChange at initialization.
+    if (!isInitCall.current) {
       fetchResults()
-    },
-    DEBOUNCE_MS,
-    [state.search, dispatch]
-  )
+    } else {
+      isInitCall.current = false
+    }
+
+    // Because we already has state.search.bounds on the deps list,
+    // We don't need to call fetchResults() on every state.map.zoom change
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state.search, dispatch, brand])
 
   return [state, dispatch]
 }
