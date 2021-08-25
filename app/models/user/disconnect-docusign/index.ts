c457b9722d01e96f705cce2662095f2fa2bf6216
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-25 20:49:00 +0300

    fix(account settings): left out from last commit  (#5517)

--- app/models/user/disconnect-docusign/index.ts
+++ app/models/user/disconnect-docusign/index.ts
@@ -1,6 +1,6 @@
-import Fetch from '../../../services/fetch'
+import Fetch from '@app/services/fetch'
 
-export async function disconnectDocuSign(): Promise<any> {
+export async function disconnectDocuSign(): Promise<void> {
   try {
     await new Fetch().delete('/users/self/docusign').send()
   } catch (err) {
