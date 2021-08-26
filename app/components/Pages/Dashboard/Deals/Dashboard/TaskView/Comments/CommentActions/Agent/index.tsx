4513f6d2c48b3d967569bf01effc0a3d8e5ac4d3
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-26 22:32:23 +0300

    refactor(deals): refactor notify agent (#5315)

---
+++ app/components/Pages/Dashboard/Deals/Dashboard/TaskView/Comments/CommentActions/Agent/index.tsx
@@ -0,0 +1,91 @@
+import { Button, Tooltip } from '@material-ui/core'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { IAppState } from '@app/reducers'
+import { confirmation } from 'actions/confirmation'
+
+interface Props {
+  task: IDealTask
+  hasComment: boolean
+  isSaving: boolean
+  onSendComment: (attentionRequested: boolean, taskStatus?: string) => void
+}
+
+export default function Agent({
+  task,
+  hasComment,
+  isSaving,
+  onSendComment
+}: Props) {
+  const dispatch = useDispatch()
+  const checklist = useSelector<IAppState, IDealChecklist | null>(
+    ({ deals }) => {
+      return deals.checklists ? deals.checklists[task.checklist] : null
+    }
+  )
+
+  const isSendDisabled =
+    task.task_type === 'GeneralComments' && hasComment === false
+
+  const sendComment = () => {
+    const isBackupChecklist = checklist?.is_deactivated
+
+    if (isBackupChecklist) {
+      dispatch(
+        confirmation({
+          message: 'Sorry, can not send message',
+          description: 'You can not Notify Office for Backup Offers.',
+          confirmLabel: 'Okay, got it!',
+          hideCancelButton: true
+        })
+      )
+
+      return
+    }
+
+    onSendComment(true)
+  }
+
+  const cancelNotify = () => {
+    dispatch(
+      confirmation({
+        message: 'Cancel Notify Office?',
+        description:
+          'Your pending Notify Office request will be canceled for this task',
+        confirmLabel: 'Yes, cancel',
+        cancelLabel: 'No',
+        onConfirm: () => onSendComment(false, 'Incomplete')
+      })
+    )
+  }
+
+  return (
+    <>
+      <Tooltip title={hasComment ? '' : 'Notify office to Review'}>
+        <Button
+          color="secondary"
+          variant="contained"
+          disabled={isSaving || isSendDisabled}
+          onClick={sendComment}
+        >
+          {isSaving ? 'Saving...' : 'Notify Office'}
+        </Button>
+      </Tooltip>
+
+      {task.attention_requested && (
+        <Button
+          color="secondary"
+          variant="outlined"
+          style={{
+            borderColor: '#f6a623',
+            color: '#f6a623'
+          }}
+          disabled={isSaving}
+          onClick={cancelNotify}
+        >
+          {isSaving ? 'Processing...' : 'Cancel Notify'}
+        </Button>
+      )}
+    </>
+  )
+}
