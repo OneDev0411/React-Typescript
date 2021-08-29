7ddd6f1a7daa418227437a885a114506013423dd
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-29 17:16:43 +0430

    wip_feat(mls2): change Marker color based on status

--- app/components/Pages/Dashboard/Properties/ExploreTab/components/Marker.tsx
+++ app/components/Pages/Dashboard/Properties/ExploreTab/components/Marker.tsx
@@ -2,16 +2,19 @@ import { makeStyles } from '@material-ui/core'
 import cn from 'classnames'
 import numeral from 'numeral'
 
+import listing_util from 'utils/listing'
+
+import { MINIMAL_MARKER_ZOOM_LEVEL } from '../../mapOptions'
+
 import MarkerPopup from './MarkerPopup'
 
 const useStyles = makeStyles(
-  () => ({
+  theme => ({
     bubble: {
       position: 'relative',
       transform: 'translate(-26px, -25px)', // Marker Anchor
       height: 20,
       width: 50,
-      background: '#00B286',
       cursor: 'pointer',
       borderRadius: 3,
       overflow: 'visible',
@@ -36,7 +39,7 @@ const useStyles = makeStyles(
       '&.hover, &.selected': {
         height: 25,
         width: 55,
-        backgroundColor: '#0F3057',
+        backgroundColor: theme.palette.grey[800],
         transform: 'translate(-28px, -28px)', // Marker Anchor
         padding: 3,
         zIndex: 3
@@ -46,14 +49,13 @@ const useStyles = makeStyles(
       height: 10,
       width: 10,
       transform: 'translate(-5px, -5px)', // Marker Anchor
-      backgroundColor: '#00B286',
       border: '1px solid #fff',
       borderRadius: '50%',
       display: 'inline-block',
       boxShadow: '0px 10px 18px -6px rgba(0,0,0,0.42)',
       cursor: 'pointer',
       '&.hover, &.selected': {
-        backgroundColor: '#0F3057',
+        backgroundColor: theme.palette.grey[800],
         height: 15,
         width: 15,
         transform: 'translate(-8px, -8px)', // Marker Anchor
@@ -64,26 +66,34 @@ const useStyles = makeStyles(
   { name: 'Maker' }
 )
 
-const Marker = ({ lat, lng, listing, zoom }) => {
+interface Props {
+  lat?: number
+  lng?: number
+  listing: ICompactListingWithUIState
+  zoom: number
+}
+
+const Marker = ({ lat, lng, listing, zoom }: Props) => {
   const classes = useStyles()
 
   if (!lat || !lng) {
     return null
   }
 
-  const price_small = numeral(listing.price).format('0.[00]a')
+  const price_small: string = numeral(listing.price).format('0.[00]a')
+
+  const status_color = listing_util.getStatusColorClass(listing.status)
 
   // We render markers differently based on the current zoom level...
   return (
     <>
-      {zoom >= 8 && (
+      {zoom >= MINIMAL_MARKER_ZOOM_LEVEL && (
         <div
-          data-lat={lat}
-          data-lng={lng}
           className={cn(classes.bubble, {
             hover: listing.hover,
             selected: listing.clicked
           })}
+          style={{ backgroundColor: status_color }}
         >
           {`${price_small}`}
           {(listing.hover || listing.clicked) && (
@@ -91,14 +101,13 @@ const Marker = ({ lat, lng, listing, zoom }) => {
           )}
         </div>
       )}
-      {zoom < 8 && (
+      {zoom < MINIMAL_MARKER_ZOOM_LEVEL && (
         <div
-          data-lat={lat}
-          data-lng={lng}
           className={cn(classes.dot, {
             hover: listing.hover,
             selected: listing.clicked
           })}
+          style={{ backgroundColor: status_color }}
         >
           {(listing.hover || listing.clicked) && (
             <MarkerPopup listing={listing} />
