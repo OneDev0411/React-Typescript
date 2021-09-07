88f267653219472f66b26c8079f26d9ab483aa4c
OneDev0411 <cryptoapple1109@gmail.com>
2021-09-07 20:23:32 +0430

    wip_feat(global-triggers): add create model (#5564)

---
+++ app/models/instant-marketing/global-triggers/create-trigger.ts
@@ -0,0 +1,29 @@
+import Fetch from '../../../services/fetch'
+
+export async function createTrigger(
+  payload: IGlobalTriggerInput
+): Promise<ApiResponseBody<IGlobalTrigger>> {
+  try {
+    if (!payload.brand) {
+      throw new Error('Brand not provided')
+    }
+
+    if (!payload.event_type) {
+      throw new Error('EventType not provided')
+    }
+
+    /* because the end point accept a negative value that
+     shows the time before the main date */
+    if (!('wait_for' in payload) || payload.wait_for > 0) {
+      throw new Error('invalid wait_for value')
+    }
+
+    const response = await new Fetch()
+      .put(`/brands/${payload.brand}}/triggers/${payload.event_type}`)
+      .send(payload)
+
+    return response.body
+  } catch (e) {
+    throw e
+  }
+}
