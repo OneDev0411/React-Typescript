a85835abc90e587153b9d875d5fb8179fad11f7b
OneDev0411 <cryptoapple1109@gmail.com>
2021-09-09 13:15:48 +0300

    refactor(mls2): refactor zero state component

--- app/components/Pages/Dashboard/Properties/components/ZeroState/index.tsx
+++ app/components/Pages/Dashboard/Properties/components/ZeroState/index.tsx
@@ -1,17 +1,51 @@
-import React from 'react'
+import { Box, Theme, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
 
-import { Box, Typography } from '@material-ui/core'
-import { mdiHomeSearchOutline } from '@mdi/js'
-
-import { muiIconSizes } from 'components/SvgIcons/icon-sizes'
-import { SvgIcon } from 'components/SvgIcons/SvgIcon'
+const useStyles = makeStyles(
+  (theme: Theme) => ({
+    main: {
+      display: 'flex',
+      height: '100%',
+      width: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column'
+    },
+    image: {
+      maxWidth: '80%',
+      width: '325px',
+      margin: theme.spacing(2, 0)
+    },
+    message: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'column'
+    },
+    title: { margin: theme.spacing(0.5) },
+    subtitle: { margin: theme.spacing(0.5) }
+  }),
+  { name: 'PropertiesZeroState' }
+)
 
 export default function ZeroState() {
+  const classes = useStyles()
+
   return (
-    <Box p={3} textAlign="center" width={1}>
-      <SvgIcon path={mdiHomeSearchOutline} size={muiIconSizes.xlarge} />
-      <Typography variant="h6">No results found.</Typography>
-      <Typography>Try clearing your search or some of your filters.</Typography>
+    <Box className={classes.main}>
+      <img
+        className={classes.image}
+        src="/static/images/zero-state/agents-network.png"
+        alt="zero results"
+      />
+      <Box className={classes.message}>
+        <Typography className={classes.title} variant="h6">
+          You don't have any listings.
+        </Typography>
+        <Typography className={classes.subtitle} variant="body1">
+          Try searching for an address or listing at the top.
+        </Typography>
+      </Box>
     </Box>
   )
 }
