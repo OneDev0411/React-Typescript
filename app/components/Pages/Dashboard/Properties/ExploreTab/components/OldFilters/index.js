32a0030acfb7e230dfc038dd756ce3c0ec419425
OneDev0411 <cryptoapple1109@gmail.com>
2021-09-10 13:40:25 +0300

    refactor(mls2): rename old filters folders (#5475)

---
+++ app/components/Pages/Dashboard/Properties/ExploreTab/components/OldFilters/index.js
@@ -0,0 +1,200 @@
+import { connect } from 'react-redux'
+import compose from 'recompose/compose'
+import withHandlers from 'recompose/withHandlers'
+import { reduxForm } from 'redux-form'
+
+import actions from 'actions/listings/search/filters'
+import ActionButton from 'components/Button/ActionButton'
+import { getStatusColor } from 'utils/listing'
+
+import {
+  FILTERS_INITIAL_VALUES,
+  property_subtypes,
+  architectural_styles
+} from '../../../constants/constants'
+
+import GroupRadios from './components/GroupRadios'
+import MinMaxInputs from './components/MinMaxInputs'
+import SubStatuses from './components/SubStatuses'
+import Tags from './components/Tags'
+import Counties from './Counties'
+import FiltersListingsStatusRow from './FiltersListingsStatusRow'
+import { yesNoEitherFieldItems } from './helpers/yes-no-either-field-items'
+import MasterBedroom from './MasterBedroom'
+import MlsAreaSelects from './MlsAreaSelects'
+import Price from './Price'
+import Schools from './Schools'
+import SoldStatusChildrens from './SoldStatusChildrens'
+import { pendingStatuses, otherStatuses } from './statuses'
+import Subdivision from './Subdivision'
+import YearBuilt from './YearBuilt'
+
+const Filters = ({
+  resetHandler,
+  pristine,
+  activeSold,
+  handleSubmit,
+  isSubmitting,
+  onSubmitHandler,
+  activeOpenHouses,
+  activeOtherListings,
+  activePendingListings
+}) => (
+  <div className="c-filters">
+    <div className="c-filters__inner-wrapper u-scrollbar--thinner">
+      <form
+        onSubmit={handleSubmit(onSubmitHandler)}
+        className="c-filters__content"
+      >
+        <div>
+          <FiltersListingsStatusRow
+            name="listing_statuses.sold"
+            title="Sold"
+            hasAccordion
+            isField
+            hasSwitchToggle
+            color={`#${getStatusColor('Sold')}`}
+            onChangeSwitchToggle={activeSold}
+          >
+            <SoldStatusChildrens name="minimum_sold_date" />
+          </FiltersListingsStatusRow>
+
+          <FiltersListingsStatusRow
+            name="listing_statuses.active"
+            title="Active"
+            isField
+            hasSwitchToggle
+            color={`#${getStatusColor('Active')}`}
+          />
+
+          <FiltersListingsStatusRow
+            title="Pending"
+            hasAccordion
+            hasSwitchToggle
+            name="pending-statuses"
+            fields={pendingStatuses}
+            color={`#${getStatusColor('Pending')}`}
+            onChangeSwitchToggle={activePendingListings}
+          >
+            <SubStatuses fields={pendingStatuses} />
+          </FiltersListingsStatusRow>
+
+          <FiltersListingsStatusRow
+            name="open_house"
+            title="Open House Only"
+            icon="OH"
+            isField
+            hasSwitchToggle
+            color={`#${getStatusColor('Active')}`}
+            onChangeSwitchToggle={activeOpenHouses}
+          />
+
+          <FiltersListingsStatusRow
+            title="Other Listing Statuses"
+            hasAccordion
+            hasSwitchToggle
+            name="other-statuses"
+            fields={otherStatuses}
+            color={`#${getStatusColor('Sold')}`}
+            onChangeSwitchToggle={activeOtherListings}
+          >
+            <SubStatuses fields={otherStatuses} />
+          </FiltersListingsStatusRow>
+        </div>
+        <div style={{ padding: '2rem 1rem 5rem', backgroundColor: '#fff' }}>
+          <MlsAreaSelects />
+          <Counties />
+          <Price />
+          <Tags
+            name="property_subtypes"
+            label="Property Subtypes"
+            fields={property_subtypes}
+          />
+          <Tags
+            label="Style of Home"
+            name="architectural_styles"
+            fields={architectural_styles}
+          />
+          <GroupRadios name="minimum_bedrooms" label="Bedrooms" />
+          <MasterBedroom />
+          <GroupRadios name="minimum_bathrooms" label="Bathrooms" />
+          <GroupRadios name="minimum_parking_spaces" label="Parking Spaces" />
+          <Subdivision />
+          <Schools />
+          <MinMaxInputs name="square_meters" label="Square Footage" />
+          <MinMaxInputs name="lot_square_meters" label="Lot Size Area (Acre)" />
+          <GroupRadios
+            label="Pool"
+            name="pool"
+            fields={yesNoEitherFieldItems}
+          />
+          <YearBuilt />
+        </div>
+      </form>
+      <div className="c-filters__form-cta-buttons">
+        <ActionButton
+          size="large"
+          style={{ marginRight: '1rem' }}
+          appearance="outline"
+          onClick={resetHandler}
+          disabled={isSubmitting || pristine}
+        >
+          Reset Filters
+        </ActionButton>
+        <ActionButton
+          appearance="secondary"
+          size="large"
+          disabled={isSubmitting}
+          onClick={handleSubmit(onSubmitHandler)}
+        >
+          {isSubmitting ? 'Updating...' : 'Update Filters'}
+        </ActionButton>
+      </div>
+    </div>
+  </div>
+)
+
+export default compose(
+  connect(
+    ({ user }) => {
+      const priceZeroCleaner = user && user.user_type === 'Agent'
+
+      return {
+        initialValues: {
+          ...FILTERS_INITIAL_VALUES,
+          priceZeroCleaner
+        }
+      }
+    },
+    { ...actions }
+  ),
+  reduxForm({
+    form: 'filters',
+    destroyOnUnmount: false
+  }),
+  withHandlers({
+    onSubmitHandler:
+      ({ submitFiltersForm }) =>
+      values => {
+        submitFiltersForm(values)
+      }
+  }),
+  withHandlers({
+    resetHandler:
+      ({
+        reset,
+        initialValues,
+        submitFiltersForm,
+        submitSucceeded,
+        submitFailed
+      }) =>
+      () => {
+        if (submitSucceeded || submitFailed) {
+          reset()
+          submitFiltersForm(initialValues)
+        } else {
+          reset()
+        }
+      }
+  })
+)(Filters)
