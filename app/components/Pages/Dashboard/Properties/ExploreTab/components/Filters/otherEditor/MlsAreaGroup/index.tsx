059711de65ba4271d528c0db98737552d0ad977d
OneDev0411 <cryptoapple1109@gmail.com>
2021-09-15 17:14:48 +0300

    wip_feat(mls2): add some filters layout (other filters) (#5475)

--- app/components/Pages/Dashboard/Properties/ExploreTab/components/Filters/otherEditor/MlsAreaGroup/index.tsx
+++ app/components/Pages/Dashboard/Properties/ExploreTab/components/Filters/otherEditor/MlsAreaGroup/index.tsx
@@ -1,5 +1,10 @@
-import { TextField } from '@material-ui/core'
+import { useState } from 'react'
+
+import { CircularProgress, TextField } from '@material-ui/core'
 import Autocomplete from '@material-ui/lab/Autocomplete'
+import { useEffectOnce } from 'react-use'
+
+import api from 'models/listings/search'
 
 import { useStyles } from '../../styles'
 import { EditorGroup } from '../EditorGroup'
@@ -7,44 +12,179 @@ import { EditorGroup } from '../EditorGroup'
 export const MlsAreaGroup = () => {
   const classes = useStyles()
 
+  const [mlsAreas, setMlsAreas] = useState<IMLSArea[]>([])
+  const [selectedMlsAreas, setSelectedMlsAreas] = useState<IMLSArea[]>([])
+  const [loadingMlsAreas, setLoadingMlsAreas] = useState<boolean>(false)
+  const [mlsSubAreas, setSubMlsAreas] = useState<IMLSArea[]>([])
+  const [selectedMlsSubAreas, setSelectedMlsSubAreas] = useState<IMLSArea[]>([])
+  const [loadingMlsSubAreas, setLoadingMlsSubAreas] = useState<boolean>(false)
+  const [counties, setCounties] = useState<ICounty[]>([])
+  const [selectedCounties, setSelectedCounties] = useState<ICounty[]>([])
+  const [loadingCounties, setLoadingCounties] = useState<boolean>(false)
+
+  const getMLSAreaList = () => {
+    setLoadingMlsAreas(true)
+    api
+      .getMlsAreas()
+      .then(areas => {
+        setMlsAreas(areas)
+      })
+      .finally(() => setLoadingMlsAreas(false))
+  }
+
+  const getSubMLSAreaList = (areas: IMLSArea[]) => {
+    setLoadingMlsSubAreas(true)
+    api
+      .getMlsSubAreas(areas.map(({ number }) => number))
+      .then(subareas => {
+        setSubMlsAreas(subareas)
+      })
+      .finally(() => setLoadingMlsSubAreas(false))
+  }
+
+  const onCountyInputChange = (event: any, newInputValue: string) => {
+    setLoadingCounties(true)
+    api
+      .getCounties(newInputValue)
+      .then(counties => {
+        setCounties(counties.options)
+      })
+      .finally(() => setLoadingCounties(false))
+  }
+
+  const onMLSAreaChange = (event: any, values: IMLSArea[]) => {
+    const selectedValues = values || []
+
+    setSelectedMlsAreas(selectedValues)
+
+    if (values && values.length > 0) {
+      setSelectedMlsSubAreas([])
+      getSubMLSAreaList(selectedValues)
+    }
+  }
+
+  const onMLSAreaSubChange = (event: any, values: IMLSArea[]) => {
+    const selectedValues = values || []
+
+    setSelectedMlsSubAreas(selectedValues)
+  }
+
+  const onCountyChange = (event: any, values: ICounty[]) => {
+    const selectedValues = values || []
+
+    setSelectedCounties(selectedValues)
+  }
+
+  useEffectOnce(() => {
+    getMLSAreaList()
+  })
+
   return (
     <EditorGroup title="MLS Areas">
       <Autocomplete
         className={classes.select}
         id="area-select"
-        options={areas}
+        options={mlsAreas}
         size="small"
         autoHighlight
-        getOptionLabel={option => option.label}
-        renderOption={option => option.label}
+        multiple
+        limitTags={1}
+        value={selectedMlsAreas}
+        onChange={onMLSAreaChange}
+        loading={loadingMlsAreas}
+        getOptionSelected={(option, value) => option.number === value.number}
+        getOptionLabel={option => option.title}
+        renderOption={option => option.title}
         renderInput={params => (
           <TextField
             {...params}
             variant="outlined"
             label="Area"
-            inputProps={{
-              ...params.inputProps,
-              autoComplete: 'new-password' // disable autocomplete and autofill
+            placeholder="Type in your MLS area..."
+            InputProps={{
+              ...params.InputProps,
+              autoComplete: 'new-password', // disable autocomplete and autofill
+              endAdornment: (
+                <>
+                  {loadingMlsAreas ? (
+                    <CircularProgress color="inherit" size={20} />
+                  ) : null}
+                  {params.InputProps.endAdornment}
+                </>
+              )
             }}
           />
         )}
       />
+      {selectedMlsAreas.length > 0 && (
+        <Autocomplete
+          className={classes.select}
+          id="sub-area-select"
+          options={mlsSubAreas}
+          size="small"
+          autoHighlight
+          multiple
+          limitTags={1}
+          value={selectedMlsSubAreas}
+          onChange={onMLSAreaSubChange}
+          loading={loadingMlsSubAreas}
+          getOptionSelected={(option, value) => option.number === value.number}
+          getOptionLabel={option => option.title}
+          renderOption={option => option.title}
+          renderInput={params => (
+            <TextField
+              {...params}
+              variant="outlined"
+              label="Sub Area"
+              placeholder="Type in your MLS sub area..."
+              InputProps={{
+                ...params.InputProps,
+                autoComplete: 'new-password', // disable autocomplete and autofill
+                endAdornment: (
+                  <>
+                    {loadingMlsSubAreas ? (
+                      <CircularProgress color="inherit" size={20} />
+                    ) : null}
+                    {params.InputProps.endAdornment}
+                  </>
+                )
+              }}
+            />
+          )}
+        />
+      )}
       <Autocomplete
         className={classes.select}
-        id="country-select"
-        options={Counties}
+        id="counties-select"
+        options={counties}
         size="small"
         autoHighlight
+        multiple
+        limitTags={1}
+        value={selectedCounties}
+        onInputChange={onCountyInputChange}
+        onChange={onCountyChange}
+        loading={loadingCounties}
+        getOptionSelected={(option, value) => option.value === value.value}
         getOptionLabel={option => option.label}
-        renderOption={option => option.label}
+        renderOption={option => option.value}
         renderInput={params => (
           <TextField
             {...params}
             variant="outlined"
             label="Counties"
-            inputProps={{
-              ...params.inputProps,
-              autoComplete: 'new-password' // disable autocomplete and autofill
+            placeholder="Type in county name..."
+            InputProps={{
+              ...params.InputProps,
+              autoComplete: 'new-password', // disable autocomplete and autofill
+              endAdornment: (
+                <>
+                  {loadingCounties ? (
+                    <CircularProgress color="inherit" size={20} />
+                  ) : null}
+                  {params.InputProps.endAdornment}
+                </>
+              )
             }}
           />
         )}
@@ -52,22 +192,3 @@ export const MlsAreaGroup = () => {
     </EditorGroup>
   )
 }
-
-// TODO: Get MLS area and Contry data from API
-interface Area {
-  code: string
-  label: string
-}
-
-const areas: Area[] = [
-  { code: '1', label: 'Cedar Hill Area' },
-  { code: '2', label: 'Desoto Area' },
-  { code: '3', label: 'Lancaster Area' }
-]
-
-interface County {
-  code: string
-  label: string
-}
-
-const Counties: County[] = [{ code: 'WA', label: 'Whashington' }]
