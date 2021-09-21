8eaebef0b8709a8a75674e759d6e9527d5e1cff3
OneDev0411 <cryptoapple1109@gmail.com>
2021-09-21 22:36:44 +0430

    chore(brand-selector): make it global
    (cherry picked from commit 9b034421af92d311fee8d5a76a52b4cba7dd6a5b)

---
+++ app/views/components/BrandSelectorDrawer/index.tsx
@@ -0,0 +1,138 @@
+import { useState, ReactNode } from 'react'
+
+import {
+  Button,
+  ButtonProps,
+  Typography,
+  CircularProgress,
+  Theme,
+  makeStyles
+} from '@material-ui/core'
+import { debounce } from 'lodash'
+import { useSelector } from 'react-redux'
+
+import Search from '@app/views/components/Grid/Search'
+import Drawer, { OverlayDrawerProps } from '@app/views/components/OverlayDrawer'
+import TreeView from '@app/views/components/TreeView'
+import { selectUser } from 'selectors/user'
+
+import { useTeam } from './hooks/use-team'
+
+const getNodeId = (team: IBrand) => team.id
+
+const useStyles = makeStyles(
+  (theme: Theme) => ({
+    searchContainer: {
+      margin: theme.spacing(2, 0)
+    },
+    team: {
+      padding: theme.spacing(1, 0),
+      cursor: 'pointer'
+    },
+    loading: {
+      textAlign: 'center',
+      marginTop: theme.spacing(3)
+    },
+    nodeName: {
+      padding: theme.spacing(1, 0)
+    }
+  }),
+  { name: 'BrandSelectorDrawer' }
+)
+
+interface Props {
+  nodeRenderer?: (brand: IBrand, onClose: () => void) => ReactNode
+  defaultButtonProps?: Omit<ButtonProps, 'onClick'>
+  drawerProps?: Omit<OverlayDrawerProps, 'open' | 'onClose'>
+  buttonRenderer?: (onClick: (brand: IBrand) => void) => ReactNode
+}
+
+export function BrandSelectorDrawer({
+  nodeRenderer,
+  defaultButtonProps = {},
+  drawerProps = {},
+  buttonRenderer
+}: Props) {
+  const user = useSelector(selectUser)
+  const classes = useStyles()
+
+  const [query, setQuery] = useState<string>('')
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  const { error, loading, initialExpandedNodes, getChildNodes } = useTeam(
+    user,
+    query
+  )
+
+  const hanldeOpenDrawer = () => setIsOpen(true)
+  const hanldeCloseDrawer = () => {
+    if (query) {
+      setQuery('')
+    }
+
+    setIsOpen(false)
+  }
+
+  const debouncedSetQuery = debounce(setQuery, 400)
+
+  const renderNode = (brand: IBrand) => {
+    if (!nodeRenderer) {
+      return (
+        <div className={classes.nodeName}>
+          <Typography variant="body2">{brand.name}</Typography>
+        </div>
+      )
+    }
+
+    return nodeRenderer(brand, hanldeCloseDrawer)
+  }
+
+  const renderTreeView = () => {
+    if (loading) {
+      return (
+        <div className={classes.loading}>
+          <CircularProgress />
+        </div>
+      )
+    }
+
+    if (error) {
+      return (
+        <Typography variant="body1" color="textSecondary">
+          Somthing Went Wrong!
+        </Typography>
+      )
+    }
+
+    return (
+      <TreeView
+        selectable
+        getChildNodes={getChildNodes}
+        initialExpandedNodes={initialExpandedNodes}
+        getNodeId={getNodeId}
+        renderNode={renderNode}
+      />
+    )
+  }
+
+  return (
+    <>
+      <Button {...defaultButtonProps} onClick={hanldeOpenDrawer}>
+        Our Agents
+      </Button>
+      <Drawer {...drawerProps} open={isOpen} onClose={hanldeCloseDrawer}>
+        <Drawer.Header title="Select Agents" />
+        <Drawer.Body>
+          <div className={classes.searchContainer}>
+            <Search
+              placeholder="Search for teams and agents"
+              onChange={value => debouncedSetQuery(value)}
+            />
+          </div>
+
+          {renderTreeView()}
+        </Drawer.Body>
+      </Drawer>
+    </>
+  )
+}
--- app/views/components/EmailRecipientQuickSuggestions/components/BrandSelector/index.tsx
+++ app/views/components/EmailRecipientQuickSuggestions/components/BrandSelector/index.tsx
@@ -1,41 +1,6 @@
-import { useState } from 'react'
-
-import {
-  Button,
-  Typography,
-  CircularProgress,
-  Theme,
-  makeStyles
-} from '@material-ui/core'
-import { debounce } from 'lodash'
-import { useSelector } from 'react-redux'
-
-import Search from '@app/views/components/Grid/Search'
-import Drawer from '@app/views/components/OverlayDrawer'
-import TreeView from '@app/views/components/TreeView'
-import { selectUser } from 'selectors/user'
+import { BrandSelectorDrawer } from '@app/views/components/BrandSelectorDrawer'
 
 import { Brand } from './components/Brand'
-import { useTeam } from './hooks/use-team'
-
-const getNodeId = (team: IBrand) => team.id
-
-const useStyles = makeStyles(
-  (theme: Theme) => ({
-    searchContainer: {
-      margin: theme.spacing(2, 0)
-    },
-    team: {
-      padding: theme.spacing(1, 0),
-      cursor: 'pointer'
-    },
-    loading: {
-      textAlign: 'center',
-      marginTop: theme.spacing(3)
-    }
-  }),
-  { name: 'BrandSelector' }
-)
 
 interface Props {
   currentRecipients?: IDenormalizedEmailRecipientInput[]
@@ -46,98 +11,35 @@ interface Props {
 }
 
 export function BrandSelector({ onSelect, currentRecipients = [] }: Props) {
-  const user = useSelector(selectUser)
-  const classes = useStyles()
-
-  const [query, setQuery] = useState<string>('')
-  const [isOpen, setIsOpen] = useState<boolean>(false)
-
-  const { error, loading, initialExpandedNodes, getChildNodes } = useTeam(
-    user,
-    query
-  )
-
-  const hanldeOpenDrawer = () => setIsOpen(true)
-  const hanldeCloseDrawer = () => {
-    if (query) {
-      setQuery('')
-    }
-
-    setIsOpen(false)
-  }
-
-  const debouncedSetQuery = debounce(setQuery, 400)
-
-  const handleOnClickBrand = (brand: IBrand) => {
+  const handleOnClickBrand = (brand: IBrand, onClose: () => void) => {
     const recipient: IDenormalizedEmailRecipientBrandInput = {
       recipient_type: 'Brand',
       brand
     }
 
     onSelect(recipient, 'BCC')
-    hanldeCloseDrawer()
+    onClose()
   }
 
-  const renderNode = (brand: IBrand) => {
+  const renderBrandNode = (brand, onClose) => {
     return (
       <Brand
         brand={brand}
         currentRecipients={currentRecipients}
-        onClick={handleOnClickBrand}
-      />
-    )
-  }
-
-  const renderTreeView = () => {
-    if (loading) {
-      return (
-        <div className={classes.loading}>
-          <CircularProgress />
-        </div>
-      )
-    }
-
-    if (error) {
-      return (
-        <Typography variant="body1" color="textSecondary">
-          Somthing Went Wrong!
-        </Typography>
-      )
-    }
-
-    return (
-      <TreeView
-        selectable
-        getChildNodes={getChildNodes}
-        initialExpandedNodes={initialExpandedNodes}
-        getNodeId={getNodeId}
-        renderNode={renderNode}
+        onClick={() => handleOnClickBrand(brand, onClose)}
       />
     )
   }
 
   return (
-    <>
-      <Button size="small" onClick={hanldeOpenDrawer}>
-        Our Agents
-      </Button>
-      {/*
-        I set the drawer width to the 43rem manually bacause in our email drawer we set this
+    <BrandSelectorDrawer
+      defaultButtonProps={{ size: 'small' }}
+      /*
+        we set the drawer width to the 43rem manually bacause in our email drawer we set this
         value and base on shayan request we want the brand selector drawer cover the email drawer
-      */}
-      <Drawer open={isOpen} onClose={hanldeCloseDrawer} width="43rem">
-        <Drawer.Header title="Select Agents" />
-        <Drawer.Body>
-          <div className={classes.searchContainer}>
-            <Search
-              placeholder="Search for teams and agents"
-              onChange={value => debouncedSetQuery(value)}
-            />
-          </div>
-
-          {renderTreeView()}
-        </Drawer.Body>
-      </Drawer>
-    </>
+      */
+      drawerProps={{ width: '43rem' }}
+      nodeRenderer={renderBrandNode}
+    />
   )
 }
