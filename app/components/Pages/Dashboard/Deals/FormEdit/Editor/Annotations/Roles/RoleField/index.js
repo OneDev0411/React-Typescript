15736b342418aba00a48980a1d26ea007bcf9813
OneDev0411 <cryptoapple1109@gmail.com>
2021-09-22 22:57:29 +0300

    fix(forms): fix auto unlinking (#5584)

--- app/components/Pages/Dashboard/Deals/FormEdit/Editor/Annotations/Roles/RoleField/index.js
+++ app/components/Pages/Dashboard/Deals/FormEdit/Editor/Annotations/Roles/RoleField/index.js
@@ -1,7 +1,7 @@
 import { useState, useMemo, Fragment } from 'react'
 
 import { Tooltip } from '@material-ui/core'
-import { mdiCloseCircleOutline, mdiPencilOutline } from '@mdi/js'
+import { mdiPencilOutline } from '@mdi/js'
 
 import DealRole from 'components/DealRole'
 import { SvgIcon } from 'components/SvgIcons/SvgIcon'
@@ -94,15 +94,6 @@ export function RoleField(props) {
           onClick={() => setRole(null)}
           onUpsertRole={props.onUpsertRole}
         />
-
-        <Tooltip title="Unlink Role" placement="top">
-          <SvgIcon
-            path={mdiCloseCircleOutline}
-            onClick={props.onToggleUnlink}
-            size={props.style.height}
-            color={props.style.color}
-          />
-        </Tooltip>
       </div>
 
       {activeRole !== undefined && (
