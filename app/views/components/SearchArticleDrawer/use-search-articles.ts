4a2c3eda46a08b57190dae1f26af735dc11e2a54
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-01 00:26:04 +0330

    feat(marketing): define a hook for having article search functionality (#5598)

---
+++ app/views/components/SearchArticleDrawer/use-search-articles.ts
@@ -0,0 +1,42 @@
+import { useCallback, useMemo } from 'react'
+
+import Fuse from 'fuse.js'
+
+import { convertFeedItemToArticleMetadata } from './helpers'
+import { RSSArticleMetadata, RSSSource } from './types'
+import { useRSSFeedItems } from './use-rss-feed-items'
+
+interface UseSearchArticles {
+  isArticlesLoading: boolean
+  allArticles: RSSArticleMetadata[]
+  searchArticles: (term: string) => RSSArticleMetadata[]
+}
+
+export function useSearchArticles(rssSources: RSSSource[]): UseSearchArticles {
+  const { rssFeedItems, isLoading } = useRSSFeedItems(rssSources)
+
+  const allArticles = useMemo(
+    () => rssFeedItems.map(convertFeedItemToArticleMetadata),
+    [rssFeedItems]
+  )
+
+  const searchArticles = useCallback(
+    (term: string) => {
+      if (!term) {
+        return allArticles
+      }
+
+      return new Fuse(allArticles, {
+        keys: ['title'],
+        threshold: 0.2
+      }).search(term)
+    },
+    [allArticles]
+  )
+
+  return {
+    isArticlesLoading: isLoading,
+    allArticles,
+    searchArticles
+  }
+}
