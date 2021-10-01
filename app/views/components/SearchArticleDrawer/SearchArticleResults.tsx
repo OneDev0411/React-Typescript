0b01a4e79c6bf88b19360a8ced8b5fce249fe758
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-01 15:56:48 +0330

    fix(marketing): fix the selected mode on new search results (#5598)

--- app/views/components/SearchArticleDrawer/SearchArticleResults.tsx
+++ app/views/components/SearchArticleDrawer/SearchArticleResults.tsx
@@ -14,13 +14,19 @@ function SearchArticleResults({
   selected,
   onSelect
 }: SearchArticleResultsProps) {
+  // Create a map using the selected urls so it can check the selected items easily
+  const selectedUrls = selected.reduce<Record<string, true>>(
+    (acc, article) => ({ ...acc, [article.url]: true }),
+    {}
+  )
+
   return (
     <Grid container>
       {results.map(article => (
         <Grid item key={article.url} xs={12} sm={4}>
           <SearchArticleItem
             article={article}
-            isSelected={selected.includes(article)}
+            isSelected={selectedUrls[article.url]}
             onSelect={() => onSelect(article)}
           />
         </Grid>
