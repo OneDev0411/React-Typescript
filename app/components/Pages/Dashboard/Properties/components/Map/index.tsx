281cea8120511945434d23d082b63a0394262013
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-06 16:29:33 +0300

    fix(mls2): loading markers after map init (#5475)

--- app/components/Pages/Dashboard/Properties/components/Map/index.tsx
+++ app/components/Pages/Dashboard/Properties/components/Map/index.tsx
@@ -238,6 +238,7 @@ export const Map = ({
       >
         {!drawingMode &&
           mapIsShown &&
+          mapRef.current &&
           listings.map(
             listing =>
               checkIsInBound(
