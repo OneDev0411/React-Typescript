2cd84653e487dea82c345f8d75f2c49e4fdd7d90
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-07 02:55:13 +0300

    feat(deals): develop deleting task splitter (#5207)

--- app/components/Pages/Dashboard/Deals/Dashboard/Folders/Checklist/TaskRow/TaskSplitter/index.tsx
+++ app/components/Pages/Dashboard/Deals/Dashboard/Folders/Checklist/TaskRow/TaskSplitter/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, KeyboardEvent } from 'react'
+import { useEffect, useState, KeyboardEvent, ChangeEvent } from 'react'
 
 import {
   Box,
@@ -10,12 +10,13 @@ import {
   Button,
   Theme
 } from '@material-ui/core'
-import { mdiPencil } from '@mdi/js'
+import { mdiDelete, mdiPencil } from '@mdi/js'
 import { useDispatch } from 'react-redux'
 
-import { updateTask } from '@app/store_actions/deals'
 import { muiIconSizes } from '@app/views/components/SvgIcons/icon-sizes'
 import { SvgIcon } from '@app/views/components/SvgIcons/SvgIcon'
+import { confirmation } from 'actions/confirmation'
+import { updateTask, deleteTask } from 'actions/deals'
 
 const useStyles = makeStyles(
   (theme: Theme) => ({
@@ -32,6 +33,13 @@ const useStyles = makeStyles(
     },
     textField: {
       width: '300px'
+    },
+    iconEdit: {
+      color: `${theme.palette.grey['500']} !important`
+    },
+    iconDelete: {
+      color: `${theme.palette.grey['500']} !important`,
+      marginLeft: theme.spacing(1)
     }
   }),
   {
@@ -58,9 +66,13 @@ export function TaskSplitter({ task }: Props) {
   const handleSaveTitle = () => {
     setIsEditing(false)
 
+    if (title.trim() === task.title) {
+      return
+    }
+
     dispatch(
       updateTask(task.id, {
-        title
+        title: title.trim()
       })
     )
   }
@@ -74,6 +86,17 @@ export function TaskSplitter({ task }: Props) {
   const handleChangeTitle = (e: ChangeEvent<HTMLInputElement>) =>
     setTitle(e.target.value)
 
+  const handleDelete = () => {
+    dispatch(
+      confirmation({
+        message: 'Delete this section?',
+        description: `You are deleting the ${title} section`,
+        confirmLabel: 'Yes, Delete',
+        onConfirm: () => dispatch(deleteTask(task.checklist, task.id))
+      })
+    )
+  }
+
   return (
     <Box className={classes.root} onDoubleClick={handleStartEditing}>
       {isEditing ? (
@@ -86,9 +109,20 @@ export function TaskSplitter({ task }: Props) {
           InputProps={{
             endAdornment: (
               <InputAdornment position="end">
-                <Button size="small" onClick={handleSaveTitle}>
-                  Save
-                </Button>
+                <Box mb={0.5}>
+                  <Button
+                    size="small"
+                    color="primary"
+                    disabled={title.trim().length === 0}
+                    onClick={handleSaveTitle}
+                  >
+                    Save
+                  </Button>
+
+                  <Button size="small" onClick={() => setIsEditing(false)}>
+                    Cancel
+                  </Button>
+                </Box>
               </InputAdornment>
             )
           }}
@@ -96,9 +130,26 @@ export function TaskSplitter({ task }: Props) {
       ) : (
         <Box display="flex" alignItems="center">
           <Typography variant="subtitle1">{title}</Typography>
-          <Box ml={2} className="visible-on-hover">
-            <IconButton size="small" onClick={handleStartEditing}>
+          <Box
+            display="flex"
+            alignItems="center"
+            ml={4}
+            className="visible-on-hover"
+          >
+            <IconButton
+              size="small"
+              className={classes.iconEdit}
+              onClick={handleStartEditing}
+            >
               <SvgIcon path={mdiPencil} size={muiIconSizes.small} />
+            </IconButton>
+
+            <IconButton
+              size="small"
+              className={classes.iconDelete}
+              onClick={handleDelete}
+            >
+              <SvgIcon path={mdiDelete} size={muiIconSizes.small} />
             </IconButton>
           </Box>
         </Box>
