d32bb3fae0c263578491e7573468f452c980cb00
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-07 03:23:42 +0300

    feat(forms): polish unlink and default value components (#5584)

--- app/components/Pages/Dashboard/Deals/FormEdit/components/EditTemplateButton/index.tsx
+++ app/components/Pages/Dashboard/Deals/FormEdit/components/EditTemplateButton/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-import { ButtonBase, Tooltip } from '@material-ui/core'
-import { mdiDotsHorizontal } from '@mdi/js'
+import { ButtonBase, MenuItem, MenuList } from '@material-ui/core'
+import { mdiCogOutline, mdiDotsHorizontal } from '@mdi/js'
 
+import { BaseDropdown } from '@app/views/components/BaseDropdown'
 import { muiIconSizes } from '@app/views/components/SvgIcons/icon-sizes'
 import Acl from 'components/Acl'
 import { SvgIcon } from 'components/SvgIcons/SvgIcon'
@@ -19,23 +20,44 @@ export function EditTemplateButton({ style, annotation, type }: Props) {
 
   return (
     <Acl.BackOffice accessControlPolicy="ActiveTeamAndParents">
-      <Tooltip title="Set Default Value">
-        <ButtonBase
-          className="button-visible-on-hover"
-          style={{
-            position: 'absolute',
-            cursor: 'pointer',
-            width: '16px',
-            color: '#fff',
-            backgroundColor: 'rgb(33, 118, 203)',
-            borderRadius: '0 4px 4px 0',
-            ...style
-          }}
-          onClick={() => defaultValueContext.setAnnotation(annotation, type)}
-        >
-          <SvgIcon path={mdiDotsHorizontal} size={muiIconSizes.xsmall} />
-        </ButtonBase>
-      </Tooltip>
+      <BaseDropdown
+        placement="bottom-end"
+        renderDropdownButton={renderProps => (
+          <ButtonBase
+            className="button-visible-on-hover"
+            style={{
+              position: 'absolute',
+              cursor: 'pointer',
+              width: '16px',
+              color: '#fff',
+              backgroundColor: 'rgb(33, 118, 203)',
+              borderRadius: '0 4px 4px 0',
+              ...style
+            }}
+            {...renderProps}
+          >
+            <SvgIcon path={mdiDotsHorizontal} size={muiIconSizes.xsmall} />
+          </ButtonBase>
+        )}
+        renderMenu={({ close }) => (
+          <MenuList>
+            <MenuItem
+              onClick={() => {
+                defaultValueContext.setAnnotation(annotation, type)
+                close()
+              }}
+            >
+              <SvgIcon
+                path={mdiCogOutline}
+                color="rgb(161,161,161)"
+                size={muiIconSizes.medium}
+                rightMargined
+              />
+              Set Default Value
+            </MenuItem>
+          </MenuList>
+        )}
+      />
     </Acl.BackOffice>
   )
 }
--- app/components/Pages/Dashboard/Deals/FormEdit/components/UnlinkFieldButton/index.tsx
+++ app/components/Pages/Dashboard/Deals/FormEdit/components/UnlinkFieldButton/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-import { ButtonBase, Tooltip } from '@material-ui/core'
-import { mdiDotsHorizontal } from '@mdi/js'
+import { ButtonBase, MenuItem, MenuList } from '@material-ui/core'
+import { mdiDotsHorizontal, mdiPencilOutline } from '@mdi/js'
 
+import { BaseDropdown } from '@app/views/components/BaseDropdown'
 import { muiIconSizes } from '@app/views/components/SvgIcons/icon-sizes'
 import { SvgIcon } from 'components/SvgIcons/SvgIcon'
 
@@ -13,22 +14,43 @@ interface Props {
 
 export function UnlinkFieldButton({ style, onClick }: Props) {
   return (
-    <Tooltip title="Convert field to plain text">
-      <ButtonBase
-        className="button-visible-on-hover"
-        style={{
-          position: 'absolute',
-          cursor: 'pointer',
-          width: '16px',
-          color: '#fff',
-          backgroundColor: 'rgb(33, 118, 203)',
-          borderRadius: '0 4px 4px 0',
-          ...style
-        }}
-        onClick={onClick}
-      >
-        <SvgIcon path={mdiDotsHorizontal} size={muiIconSizes.xsmall} />
-      </ButtonBase>
-    </Tooltip>
+    <BaseDropdown
+      placement="bottom-end"
+      renderDropdownButton={renderProps => (
+        <ButtonBase
+          className="button-visible-on-hover"
+          style={{
+            position: 'absolute',
+            cursor: 'pointer',
+            width: '16px',
+            color: '#fff',
+            backgroundColor: 'rgb(33, 118, 203)',
+            borderRadius: '0 4px 4px 0',
+            ...style
+          }}
+          {...renderProps}
+        >
+          <SvgIcon path={mdiDotsHorizontal} size={muiIconSizes.xsmall} />
+        </ButtonBase>
+      )}
+      renderMenu={({ close }) => (
+        <MenuList>
+          <MenuItem
+            onClick={() => {
+              onClick()
+              close()
+            }}
+          >
+            <SvgIcon
+              path={mdiPencilOutline}
+              color="rgb(161,161,161)"
+              size={muiIconSizes.medium}
+              rightMargined
+            />
+            Make Editable
+          </MenuItem>
+        </MenuList>
+      )}
+    />
   )
 }
