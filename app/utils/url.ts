2e7dcf16cb5df46a54a306784dea9a5f2644f718
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-11 23:08:54 +0330

    fix(marketing): use regex to check the url and protocols (#5597)

--- app/utils/url.ts
+++ app/utils/url.ts
@@ -2,14 +2,14 @@ export function isValidUrl(url: string): boolean {
   try {
     const result = new URL(url)
 
-    return ['http:', 'https:'].includes(result.protocol)
+    return /^https?:$/.test(result.protocol)
   } catch (_) {
     return false
   }
 }
 
 export function prependHTTPSIfNeeded(url: string): string {
-  if (url.indexOf('http://') === 0 || url.indexOf('https://') === 0) {
+  if (/^https?:\/\//.test(url)) {
     return url
   }
 
