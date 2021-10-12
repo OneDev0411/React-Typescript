cdfe261a899f1b502fa00ae5b13c912a4702e669
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-12 19:59:50 +0330

    feat(super campaign): create a new super campaign if needed (#5681)

--- app/views/components/InstantMarketing/Builder/AdminContinueButton/AdminContinueButton.tsx
+++ app/views/components/InstantMarketing/Builder/AdminContinueButton/AdminContinueButton.tsx
@@ -1,18 +1,27 @@
 import { useState } from 'react'
 
-import { Button, ButtonProps } from '@material-ui/core'
+import { Button, ButtonProps, CircularProgress } from '@material-ui/core'
+
+import { goTo } from '@app/utils/go-to'
 
 import AdminContinueDrawer from './AdminContinueDrawer'
+import { useCreateSuperCampaign } from './use-create-super-campaign'
 
 interface AdminContinueButtonProps
-  extends Omit<ButtonProps, 'variant' | 'color' | 'onClick' | 'children'> {
+  extends Omit<
+    ButtonProps,
+    'variant' | 'color' | 'onClick' | 'children' | 'startIcon'
+  > {
   onClick: () => void
   hasAddToMarketingCenter: boolean
+  templateId: UUID
 }
 
 function AdminContinueButton({
   onClick,
   hasAddToMarketingCenter,
+  disabled,
+  templateId,
   ...otherProps
 }: AdminContinueButtonProps) {
   const [isOpen, setIsOpen] = useState(false)
@@ -21,7 +30,20 @@ function AdminContinueButton({
 
   const closeDrawer = () => setIsOpen(false)
 
-  const redirectToSuperCampaign = () => console.log('redirectToSuperCampaign') // TODO: implement this
+  const { isCreatingSuperCampaign, createSuperCampaign } =
+    useCreateSuperCampaign()
+
+  const createSuperCampaignAndRedirect = async () => {
+    const superCampaign = await createSuperCampaign({
+      subject: '',
+      description: '',
+      due_at: new Date(),
+      recipients: [],
+      template: templateId
+    })
+
+    goTo(`/dashboard/super-campaign/${superCampaign.id}`)
+  }
 
   const addToMarketingCenter = () => console.log('addToMarketingCenter') // TODO: implement this
 
@@ -32,6 +54,12 @@ function AdminContinueButton({
         variant="contained"
         color="primary"
         onClick={openDrawer}
+        disabled={disabled || isCreatingSuperCampaign}
+        startIcon={
+          isCreatingSuperCampaign && (
+            <CircularProgress color="inherit" size={20} />
+          )
+        }
       >
         Continue
       </Button>
@@ -39,7 +67,7 @@ function AdminContinueButton({
         open={isOpen}
         onClose={closeDrawer}
         onContinueClick={onClick}
-        onSuperCampaignClick={redirectToSuperCampaign}
+        onSuperCampaignClick={createSuperCampaignAndRedirect}
         onAddToMarketingCenterClick={addToMarketingCenter}
         hasAddToMarketingCenter={hasAddToMarketingCenter}
       />
--- types/SuperCampaign/SuperCampaign.d.ts
+++ types/SuperCampaign/SuperCampaign.d.ts
@@ -1,3 +1,9 @@
-declare interface ISuperCampaign {
+declare interface ISuperCampaign extends IModel<'super_campaign'> {
+  brand: UUID
+  created_by: UUID
+  executed_at: null
+  recipients: null
+  subject: string
+  template_instance: null
   // TODO: complete this type
 }
