4ebb4fd0c9beacbea58cb70fb6f95d759d01199d
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-13 14:24:52 +0330

    fix(super campaign): fix API model and use dash instead of underscore (#5681)

--- app/models/super-campaign/create-super-campaign.ts
+++ app/models/super-campaign/create-super-campaign.ts
@@ -3,7 +3,7 @@ import Fetch from 'services/fetch'
 async function createSuperCampaign(
   data: ISuperCampaignInput
 ): Promise<ISuperCampaign> {
-  return (await new Fetch().post('/email/super_campaigns').send(data)).body.data
+  return (await new Fetch().post('/email/super-campaigns').send(data)).body.data
 }
 
 export default createSuperCampaign
