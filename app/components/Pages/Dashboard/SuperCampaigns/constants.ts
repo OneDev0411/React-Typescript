33fe931d064225270a722f3feb1b321e283dba8f
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-13 16:57:03 +0330

    feat(super campaign): define the tabs component for the detail page (#5689)

---
+++ app/components/Pages/Dashboard/SuperCampaigns/constants.ts
@@ -0,0 +1,9 @@
+import type { SuperCampaignDetailTabType } from './types'
+
+export const superCampaignDetailTabs: Record<
+  SuperCampaignDetailTabType,
+  SuperCampaignDetailTabType
+> = {
+  Overview: 'Overview',
+  Results: 'Results'
+}
