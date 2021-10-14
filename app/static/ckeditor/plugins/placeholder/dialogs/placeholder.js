5b78f8474a14afea286a3406f8c2c732b6a1ffe6
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-15 00:29:06 +0330

    fix(mc): fix rte placeholder fallback value setter (#4428)

--- app/static/ckeditor/plugins/placeholder/dialogs/placeholder.js
+++ app/static/ckeditor/plugins/placeholder/dialogs/placeholder.js
@@ -1,16 +1,5 @@
-/**
- * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
- */
-
-/**
- * @fileOverview Definition for placeholder plugin dialog.
- *
- */
-
-'use strict'
-
-CKEDITOR.dialog.add('placeholder', function (editor) {
+// eslint-disable-next-line no-undef
+CKEDITOR.dialog.add('placeholder', editor => {
   let lang = editor.lang.placeholder
   let generalLabel = editor.lang.common.generalTab
   let validFallbackRegex = /^[^\[\]<>]+$/
@@ -54,12 +43,15 @@ CKEDITOR.dialog.add('placeholder', function (editor) {
             label: lang.fallback,
             default: '',
             required: true,
+            // eslint-disable-next-line no-undef
             validate: CKEDITOR.dialog.validate.regex(
               validFallbackRegex,
               lang.invalidFallback
             ),
             setup(widget) {
-              this.setValue(widget.data.fallback)
+              if (widget.data.fallback) {
+                this.setValue(widget.data.fallback)
+              }
             },
             commit(widget) {
               widget.setData('fallback', this.getValue())
