32fb6269db2c67db98cfe92dd4ba5e46475f1544
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-27 10:17:00 +0330

    feat(super campaign): add the itemSize prop to Table (#5689)

--- app/views/components/Grid/Table/index.tsx
+++ app/views/components/Grid/Table/index.tsx
@@ -19,6 +19,7 @@ export interface Props<Row> {
   rows: Row[]
   totalRows: number
   virtualize?: boolean
+  rowSize?: number
   selection?: GridSelectionOptions<Row> | null
   sorting?: GridSortingOption | null
   infiniteScrolling?: InfiniteScrollingOptions | null
