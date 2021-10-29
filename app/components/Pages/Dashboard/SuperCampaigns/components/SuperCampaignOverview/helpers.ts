19a46b2f1fd69c8a1bba79838ddecc6b6d2e4114
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-29 10:26:29 +0330

    feat(super campaign): implement a new helper to calc result percentage (#5716)

--- app/components/Pages/Dashboard/SuperCampaigns/components/SuperCampaignOverview/helpers.ts
+++ app/components/Pages/Dashboard/SuperCampaigns/components/SuperCampaignOverview/helpers.ts
@@ -4,3 +4,14 @@ export function isSuperCampaignEnrollmentOptedOut(
   // TODO: Implement the logic to detect opted-out options
   return false
 }
+
+export function getSuperCampaignResultPercentage(
+  value: number,
+  total: number
+): string {
+  if (!total) {
+    return ''
+  }
+
+  return `(${(value / total) * 100}%)`
+}
