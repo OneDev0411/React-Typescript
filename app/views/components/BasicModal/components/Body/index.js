fc92bcd4eecef4e051ca22d4bbffc368e3c85d4a
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-30 19:46:20 +0300

    fix(crm): investigates and fixes styling issues with create list modal (#4721)

--- app/views/components/BasicModal/components/Body/index.js
+++ app/views/components/BasicModal/components/Body/index.js
@@ -4,13 +4,9 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   position: relative;
-  padding: 1em;
+  padding: 1.25rem 1rem;
   overflow-x: hidden;
   overflow-y: scroll;
-
-  @media screen and (min-width: 48em) {
-    height: ${({ noFooter }) => (noFooter ? 410 : 314)}px;
-  }
 `
 
 export default props => <Container {...props}>{props.children}</Container>
