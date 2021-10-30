34eb5e530c5864ab6f8f9de65b4cfd06cac7ea13
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-30 19:46:20 +0300

    fix(crm): investigates and fixes styling issues with create list modal (#4721)

--- app/views/components/Grid/SavedSegments/Create/styled.js
+++ app/views/components/Grid/SavedSegments/Create/styled.js
@@ -2,31 +2,39 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: inline-flex;
-  margin-left: ${props => props.theme.spacing(1)}px;
   vertical-align: middle;
+  padding: 1rem 0;
 `
 
 export const ItemRow = styled.div`
   display: flex;
   align-items: center;
-  width: 100%;
-  margin-bottom: 1.5em;
+  gap: 1rem;
   cursor: pointer;
+  padding: 1rem 0 2rem;
+
+  :not(:last-child) {
+    border-bottom: 1px solid ${props => props.theme.palette.grey['100']};
+  }
 `
 
-export const ItemTitle = styled.span`
+export const ItemContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-flow: column;
+`
+
+export const ItemTitle = styled.div`
   font-weight: 400;
   color: #000;
 `
 
 export const TextInput = styled.input`
-  width: 300px;
-  height: 2.5rem;
-  margin-left: 1em;
   border-radius: 3px;
   background-color: #ffffff;
   border: solid 1px ${props => props.theme.palette.grey['300']};
-  padding: 0 1em;
+  padding: 0.5rem 1rem;
+  margin-top: 0.5rem;
 
   :focus {
     outline: none;
