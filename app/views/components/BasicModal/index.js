84b595d947f48ef69906ba32e67717d060ea9cef
OneDev0411 <cryptoapple1109@gmail.com>
2021-11-02 00:46:43 +0300

    fix(crm): adds explicit autoHeight

--- app/views/components/BasicModal/index.js
+++ app/views/components/BasicModal/index.js
@@ -30,7 +30,7 @@ const BasicModal = ({
   ...rest
 }) => (
   <BareModal
-    autoHeight
+    autoHeight={true}
     isOpen={isOpen}
     shouldCloseOnOverlayClick={shouldCloseOnOverlayClick}
     contentLabel={title}
