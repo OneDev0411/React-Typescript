b4e87cf0fd7786feea3ab5abb0ac331443104096
OneDev0411 <cryptoapple1109@gmail.com>
2021-11-02 00:42:05 +0300

    fix(crm): removes overflow from basemodal's body (#4721)

--- app/views/components/BasicModal/components/Body/index.js
+++ app/views/components/BasicModal/components/Body/index.js
@@ -5,8 +5,6 @@ import styled from 'styled-components'
 const Container = styled.div`
   position: relative;
   padding: 1.25rem 1rem;
-  overflow-x: hidden;
-  overflow-y: scroll;
 `
 
 export default props => <Container {...props}>{props.children}</Container>
