62fe32277ea6fc64bd898cd61c453606d7f080d0
OneDev0411 <cryptoapple1109@gmail.com>
2021-11-02 01:40:26 +0300

    fix(crm): adds padding for basic modal components (#4721)

--- app/views/components/BasicModal/components/Body/index.js
+++ app/views/components/BasicModal/components/Body/index.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   position: relative;
-  padding: 1.25rem 1rem;
+  padding: 1.25rem 1.5rem;
 `
 
 export default props => <Container {...props}>{props.children}</Container>
