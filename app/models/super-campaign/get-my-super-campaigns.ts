6147a7adcf0f5451346bf23a2c37a97b270b6c90
OneDev0411 <cryptoapple1109@gmail.com>
2021-11-04 17:04:22 +0330

    feat(super campaign): add todo items to get super campaign model (#5741)

--- app/models/super-campaign/get-my-super-campaigns.ts
+++ app/models/super-campaign/get-my-super-campaigns.ts
@@ -6,8 +6,11 @@ async function getMySuperCampaigns(): Promise<
   return (
     await new Fetch().get('/email/super-campaigns/self').query({
       associations: ['super_campaign.template_instance']
+      // limit: '2' // TODO: Ask the API guys about this
     })
   ).body.data
+    .filter(item => !!item.template_instance)
+    .slice(0, 2) // TODO: Remove this when the API supports limit
 }
 
 export default getMySuperCampaigns
