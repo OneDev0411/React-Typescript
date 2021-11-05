0b3bbef3b5495625de93d756ad8147040438a80c
OneDev0411 <cryptoapple1109@gmail.com>
2021-11-05 20:07:43 +0300

    feat(deals): make visible to admin is back

---
+++ app/components/Pages/Dashboard/Deals/Dashboard/Header/Menu/MakeVisibleToOffice/index.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+import { Button } from '@material-ui/core'
+
+import useNotify from '@app/hooks/use-notify'
+
+import MakeVisibleToAdmin from '../../../../Create/MakeVisibleToAdmin'
+
+interface Props {
+  deal: IDeal
+}
+
+export function MakeVisible({ deal }: Props) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+  const notify = useNotify()
+
+  const onComplete = () => {
+    notify({
+      status: 'success',
+      message:
+        'Your deal is now live, please make sure you notify your office to review any checklist item you have completed.'
+    })
+  }
+
+  return (
+    <>
+      <Button
+        size="small"
+        variant="outlined"
+        onClick={() => setIsDialogOpen(true)}
+      >
+        Make Visible To Office
+      </Button>
+
+      {isDialogOpen && (
+        <MakeVisibleToAdmin
+          dealId={deal.id}
+          onClose={() => setIsDialogOpen(false)}
+          onComplete={onComplete}
+        />
+      )}
+    </>
+  )
+}
