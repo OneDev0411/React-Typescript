53aaacb3a3515cc7a31e5812399d290b6fc5eba6
OneDev0411 <cryptoapple1109@gmail.com>
2021-11-05 15:19:16 +0330

    feat(super campaign): move the tags selector logic to global scope (#5741)

---
+++ app/views/components/SuperCampaignTagsPopover/index.tsx
@@ -0,0 +1,48 @@
+import useSafeState from '@app/hooks/use-safe-state'
+import {
+  PopoverContactTagSelector,
+  PopoverContactTagSelectorProps
+} from '@app/views/components/TagSelector'
+import { SelectorOption } from '@app/views/components/TagSelector/type'
+
+export interface SuperCampaignTagsPopoverProps
+  extends Pick<
+    PopoverContactTagSelectorProps,
+    'anchorRenderer' | 'defaultIsDirty' | 'minTagCount'
+  > {
+  tags: string[]
+  onTagsChange: (tags: string[]) => Promise<void>
+}
+
+function SuperCampaignTagsPopover({
+  tags,
+  onTagsChange,
+  ...otherProps
+}: SuperCampaignTagsPopoverProps) {
+  const [isSaving, setIsSaving] = useSafeState(false)
+
+  const handleTagsChange = async (tags: SelectorOption[]) => {
+    setIsSaving(true)
+    await onTagsChange(tags.map(tag => tag.title))
+    setIsSaving(false)
+  }
+
+  return (
+    <PopoverContactTagSelector
+      {...otherProps}
+      value={tags.map(tag => ({
+        title: tag,
+        value: tag
+      }))}
+      filter={
+        {
+          // selectedIds: [contact.id] // TODO: Hamed jan, please do not forget to fix this
+        }
+      }
+      callback={handleTagsChange}
+      disabled={isSaving}
+    />
+  )
+}
+
+export default SuperCampaignTagsPopover
