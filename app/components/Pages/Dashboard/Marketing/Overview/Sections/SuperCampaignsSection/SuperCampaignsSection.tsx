e54216de254dcdbf9c075fa8655f1c55d08346ad
OneDev0411 <cryptoapple1109@gmail.com>
2021-11-05 17:02:26 +0330

    feat(super campaign): move the render cards logic to a new component (#5741)

--- app/components/Pages/Dashboard/Marketing/Overview/Sections/SuperCampaignsSection/SuperCampaignsSection.tsx
+++ app/components/Pages/Dashboard/Marketing/Overview/Sections/SuperCampaignsSection/SuperCampaignsSection.tsx
@@ -1,38 +1,20 @@
-import { Grid } from '@material-ui/core'
-
-import LinkSectionAction from '../LinkSectionAction'
 import SectionLayout from '../SectionLayout'
 
-import SuperCampaignWithEnrollmentCard from './SuperCampaignWithEnrollmentCard'
+import SuperCampaignCards from './SuperCampaignCards'
 import { useGetSuperCampaignsWithEnrollment } from './use-get-super-campaigns-with-enrollment'
 
 function SuperCampaignsSection() {
-  const { superCampaignsWithEnrollment } = useGetSuperCampaignsWithEnrollment()
+  const { superCampaignsWithEnrollment, setSuperCampaignsWithEnrollment } =
+    useGetSuperCampaignsWithEnrollment()
 
   // TODO: Handle loading and empty state
 
   return (
-    <SectionLayout
-      title="Campaigns"
-      actionNode={
-        superCampaignsWithEnrollment?.length ? (
-          <LinkSectionAction
-            title="View all"
-            url="/dashboard/insights/super-campaign"
-          />
-        ) : null
-      }
-      gridProps={{ sm: 12 }}
-    >
-      <Grid container spacing={2}>
-        {superCampaignsWithEnrollment.map(superCampaignWithEnrollment => (
-          <Grid key={superCampaignWithEnrollment.id} item sm={6}>
-            <SuperCampaignWithEnrollmentCard
-              superCampaignWithEnrollment={superCampaignWithEnrollment}
-            />
-          </Grid>
-        ))}
-      </Grid>
+    <SectionLayout title="Campaigns" gridProps={{ sm: 12 }}>
+      <SuperCampaignCards
+        superCampaignsWithEnrollment={superCampaignsWithEnrollment}
+        setSuperCampaignsWithEnrollment={setSuperCampaignsWithEnrollment}
+      />
     </SectionLayout>
   )
 }
