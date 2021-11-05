1bad7d535c6803d3003f444cff0283aea3bac708
OneDev0411 <cryptoapple1109@gmail.com>
2021-11-05 19:44:31 +0330

    feat(super campaign): move the styles to a seperate file (#5741)

---
+++ app/components/Pages/Dashboard/Marketing/Overview/Sections/SuperCampaignsSection/use-super-campaign-with-enrollment-card-styles.ts
@@ -0,0 +1,8 @@
+import { makeStyles } from '@material-ui/core'
+
+export default makeStyles(
+  theme => ({
+    root: { minHeight: theme.spacing(23) }
+  }),
+  { name: 'useSuperCampaignWithEnrollmentCardStyles' }
+)
