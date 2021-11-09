670da5ca0ce1a94f09a4d1d2c497fe69cb08ad7f
OneDev0411 <cryptoapple1109@gmail.com>
2021-11-09 15:14:49 +0330

    fix(super campaign): put a comment to describe the magic value (#5741)

--- app/components/Pages/Dashboard/Marketing/Overview/Sections/SuperCampaignsSection/use-super-campaign-with-enrollment-card-styles.ts
+++ app/components/Pages/Dashboard/Marketing/Overview/Sections/SuperCampaignsSection/use-super-campaign-with-enrollment-card-styles.ts
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core'
 
 export default makeStyles(
   theme => ({
-    root: { minHeight: theme.spacing(23) }
+    root: { minHeight: theme.spacing(23) } // This is the min card height according to the designs
   }),
   { name: 'useSuperCampaignWithEnrollmentCardStyles' }
 )
