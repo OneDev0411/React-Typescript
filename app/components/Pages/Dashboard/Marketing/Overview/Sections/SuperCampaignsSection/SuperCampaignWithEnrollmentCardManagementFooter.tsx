fa73ba7f291fd9188adc271f09dfd347abc074cf
OneDev0411 <cryptoapple1109@gmail.com>
2021-12-01 12:27:15 +0330

    fix(super campaign): remove the static text from management footer (#5795)

--- app/components/Pages/Dashboard/Marketing/Overview/Sections/SuperCampaignsSection/SuperCampaignWithEnrollmentCardManagementFooter.tsx
+++ app/components/Pages/Dashboard/Marketing/Overview/Sections/SuperCampaignsSection/SuperCampaignWithEnrollmentCardManagementFooter.tsx
@@ -1,28 +1,6 @@
-import { Typography, makeStyles } from '@material-ui/core'
-
-const useStyles = makeStyles(
-  theme => ({
-    root: {
-      padding: theme.spacing(1, 2),
-      backgroundColor: theme.palette.info.ultralight,
-      color: theme.palette.info.dark,
-      minHeight: theme.spacing(9),
-      position: 'relative'
-    }
-  }),
-  { name: 'SuperCampaignWithEnrollmentCardManagementFooter' }
-)
-
 function SuperCampaignWithEnrollmentCardManagementFooter() {
-  const classes = useStyles()
-
-  return (
-    <div className={classes.root}>
-      <Typography variant="caption">
-        24 users from 18 teams and offices participate in this campaign.
-      </Typography>
-    </div>
-  )
+  // TODO: The design team decided to skip this footer and display nothing for now
+  return <div />
 }
 
 export default SuperCampaignWithEnrollmentCardManagementFooter
