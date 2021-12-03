ca1a60ff97c85a6bb8e865f1e2ea1fff23761081
OneDev0411 <cryptoapple1109@gmail.com>
2021-12-03 13:41:59 +0330

    feat(super campaign): add a new association to super campaign type (#5797)

--- types/SuperCampaign/SuperCampaign.d.ts
+++ types/SuperCampaign/SuperCampaign.d.ts
@@ -1,14 +1,21 @@
-declare type ISuperCampaignAssociations = 'template_instance'
+declare type ISuperCampaignAssociations =
+  | 'template_instance'
+  | 'created_by'
+  | 'template_instance_and_created_by'
 
 declare interface ISuperCampaign<A extends ISuperCampaignAssociations = ''>
   extends IModel<'super_campaign'> {
-  created_by: UUID
+  created_by: A extends 'created_by' | 'template_instance_and_created_by'
+    ? IUser
+    : UUID
   brand: UUID
   due_at?: number
   executed_at?: number
   subject?: string
   description?: string
-  template_instance?: A extends 'template_instance'
+  template_instance?: A extends
+    | 'template_instance'
+    | 'template_instance_and_created_by'
     ? IMarketingTemplateInstance
     : UUID
   tags: Nullable<string[]>
