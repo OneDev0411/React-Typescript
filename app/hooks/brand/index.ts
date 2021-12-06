dc1604bcff844dce98b3db251ea73e2401c510e6
OneDev0411 <cryptoapple1109@gmail.com>
2021-12-06 13:11:54 +0330

    wip_feat(team-switcher): export all hooks (#5596)

---
+++ app/hooks/brand/index.ts
@@ -0,0 +1,4 @@
+export * from './use-active-brand'
+export * from './use-active-brand-id'
+export * from './use-unsafe-active-brand'
+export * from './use-unsafe-active-brand-id'
---
+++ app/hooks/team/index.ts
@@ -0,0 +1,4 @@
+export * from './use-active-team'
+export * from './use-active-team-id'
+export * from './use-unsafe-active-team'
+export * from './use-unsafe-active-team-id'
