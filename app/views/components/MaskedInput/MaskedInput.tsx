bc420dc7b19bac9c9401eeb08d3fcfa48c33f40a
OneDev0411 <cryptoapple1109@gmail.com>
2021-12-09 21:14:27 +0300

    fix(masked input): fix typings

--- app/views/components/MaskedInput/MaskedInput.tsx
+++ app/views/components/MaskedInput/MaskedInput.tsx
@@ -1,9 +1,9 @@
 import Masked from 'react-text-mask'
-import type { maskArray } from 'react-text-mask'
+import type { Mask } from 'react-text-mask'
 
 interface Props {
   inputRef: (ref: HTMLInputElement | null) => void
-  mask: maskArray
+  mask: Mask
 }
 
 export default function MaskedInput({ inputRef, ...rest }: Props) {
