ac53af2575302b1922ab36d76e0b014402b4fd9a
OneDev0411 <cryptoapple1109@gmail.com>
2021-12-14 20:13:36 +0300

    fix(statuses): fix loading statuses

--- app/models/brand/get-property-types.ts
+++ app/models/brand/get-property-types.ts
@@ -4,9 +4,15 @@ export async function getBrandPropertyTypes(
   brandId: UUID
 ): Promise<IDealPropertyType[]> {
   try {
-    const response = await new Fetch().get(
-      `/brands/${brandId}/deals/property_types`
-    )
+    const response = await new Fetch()
+      .get(`/brands/${brandId}/deals/property_types`)
+      .query({
+        associations: [
+          'brand_checklist.required_contexts',
+          'brand_checklist.optional_contexts',
+          'brand_checklist.statuses'
+        ]
+      })
 
     return response.body.data
   } catch (e) {
