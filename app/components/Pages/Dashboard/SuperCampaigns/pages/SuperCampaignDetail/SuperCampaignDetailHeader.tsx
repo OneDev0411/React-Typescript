aa38bd6c39365d328892953f1363815c8472ec3f
OneDev0411 <cryptoapple1109@gmail.com>
2021-12-19 12:00:59 +0330

    feat(super campaign): use the admin actions on management page (#5797)

--- app/components/Pages/Dashboard/SuperCampaigns/pages/SuperCampaignDetail/SuperCampaignDetailHeader.tsx
+++ app/components/Pages/Dashboard/SuperCampaigns/pages/SuperCampaignDetail/SuperCampaignDetailHeader.tsx
@@ -1,10 +1,14 @@
 import { makeStyles } from '@material-ui/core'
 
+import { goTo } from '@app/utils/go-to'
+
+import SuperCampaignAdminMoreActions from '../../components/SuperCampaignAdminMoreActions'
+
 import SuperCampaignCloseButton from './SuperCampaignCloseButton'
-import SuperCampaignDetailHeaderMoreActions from './SuperCampaignDetailHeaderMoreActions'
 import SuperCampaignDetailHeaderSchedule, {
   SuperCampaignDetailHeaderScheduleProps
 } from './SuperCampaignDetailHeaderSchedule'
+import { useGetSuperCampaignBackUrl } from './use-get-super-campaign-back-url'
 
 const useStyles = makeStyles(
   theme => ({
@@ -27,6 +31,19 @@ function SuperCampaignDetailHeader({
   setSuperCampaign
 }: SuperCampaignDetailHeaderProps) {
   const classes = useStyles()
+  const backUrl = useGetSuperCampaignBackUrl()
+
+  const handleSendNow = (newSuperCampaign: ISuperCampaign) =>
+    setSuperCampaign({
+      ...newSuperCampaign,
+      template_instance: superCampaign?.template_instance
+    })
+
+  const handleDelete = () => goTo(backUrl)
+
+  const handleDuplicate = (newSuperCampaign: ISuperCampaign) => {
+    goTo(`/dashboard/insights/super-campaign/${newSuperCampaign.id}/detail`)
+  }
 
   return (
     <div className={classes.root}>
@@ -37,10 +54,15 @@ function SuperCampaignDetailHeader({
             superCampaign={superCampaign}
             setSuperCampaign={setSuperCampaign}
           />
-          <SuperCampaignDetailHeaderMoreActions
+          <SuperCampaignAdminMoreActions
             className={classes.margin}
-            superCampaign={superCampaign}
-            setSuperCampaign={setSuperCampaign}
+            superCampaign={{
+              ...superCampaign,
+              template_instance: superCampaign.template_instance?.id
+            }}
+            onSendNow={handleSendNow}
+            onDelete={handleDelete}
+            onDuplicate={handleDuplicate}
           />
         </>
       )}
