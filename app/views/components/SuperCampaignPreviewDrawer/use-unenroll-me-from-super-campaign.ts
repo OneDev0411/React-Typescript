4b8a4c200766454c6f5ee4865922c381b277c112
OneDev0411 <cryptoapple1109@gmail.com>
2021-12-21 00:28:01 +0330

    feat(super campaign): move the hooks to the campaign preview folder (#5797)

---
+++ app/views/components/SuperCampaignPreviewDrawer/use-unenroll-me-from-super-campaign.ts
@@ -0,0 +1,27 @@
+import { useRunActionThenNotify } from '@app/hooks/use-run-action-then-notify'
+import unenrollMeFromSuperCampaignModel from '@app/models/super-campaign/unenroll-me-from-super-campaign'
+
+type UseUnenrollMeFromSuperCampaign = {
+  isDeleting: boolean
+  unenrollMeFromSuperCampaign: () => Promise<void>
+}
+
+export function useUnenrollMeFromSuperCampaign(
+  superCampaignId: UUID,
+  onUnenroll: () => void
+): UseUnenrollMeFromSuperCampaign {
+  const { isRunning, runActionThenNotify } = useRunActionThenNotify()
+
+  const unenrollMeFromSuperCampaign = () =>
+    runActionThenNotify(
+      async () => {
+        await unenrollMeFromSuperCampaignModel(superCampaignId)
+
+        onUnenroll()
+      },
+      'You have been opted-out from the campaign',
+      'Something went wrong while opting-out the campaign'
+    )
+
+  return { isDeleting: isRunning, unenrollMeFromSuperCampaign }
+}
