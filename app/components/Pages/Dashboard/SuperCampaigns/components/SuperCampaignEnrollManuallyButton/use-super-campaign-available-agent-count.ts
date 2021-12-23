3e19330ca8da95708899633b735b20022cb03502
OneDev0411 <cryptoapple1109@gmail.com>
2021-12-23 11:54:03 +0330

    fix(super campaign): complete available agent logic in manual button (#5865)

---
+++ app/components/Pages/Dashboard/SuperCampaigns/components/SuperCampaignEnrollManuallyButton/use-super-campaign-available-agent-count.ts
@@ -0,0 +1,46 @@
+import { useEffect } from 'react'
+
+import useAsync from '@app/hooks/use-async'
+
+export function useSuperCampaignAvailableAgentCount(
+  teamAgentsModelFn: () => Promise<IBrand[]>,
+  enrolledAgentCount: number,
+  eligibleBrands: Nullable<string[]>
+): number {
+  const {
+    run,
+    data: totalAgentCount,
+    isLoading
+  } = useAsync({ data: 0, status: 'pending' })
+
+  useEffect(() => {
+    run(async () => {
+      const brands = await teamAgentsModelFn()
+
+      // Count all users in all brands
+      return brands.reduce((totalAgentCount, brand) => {
+        if (!brand || !brand.roles) {
+          return totalAgentCount
+        }
+
+        return (
+          totalAgentCount +
+          // Count all users in the brand
+          brand.roles.reduce((brandUsersCount, role) => {
+            if (!role.users) {
+              return brandUsersCount
+            }
+
+            return brandUsersCount + role.users.length
+          }, 0)
+        )
+      }, 0)
+    })
+  }, [run, teamAgentsModelFn, eligibleBrands])
+
+  if (isLoading) {
+    return 0
+  }
+
+  return Math.max(totalAgentCount - enrolledAgentCount, 0) // Do not return a negative value
+}
