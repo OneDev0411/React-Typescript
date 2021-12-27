68b3c3b80362d0e49404dc8e53b1b5050ad0875e
OneDev0411 <cryptoapple1109@gmail.com>
2021-12-27 12:11:06 +0300

    refactor(deals): add missing style name (#5755)

--- app/components/Pages/Dashboard/Deals/List/BackOffice/styles.ts
+++ app/components/Pages/Dashboard/Deals/List/BackOffice/styles.ts
@@ -1,18 +1,20 @@
 import { makeStyles } from '@material-ui/core'
 import { createStyles } from '@material-ui/styles'
 
-export const useStyles = makeStyles(theme =>
-  createStyles({
-    container: {
-      margin: theme.spacing(5)
-    },
-    headerContainer: {
-      width: '100%',
-      display: 'flex',
-      justifyContent: 'flex-end'
-    },
-    filtersContainer: {
-      margin: theme.spacing(5, 0)
-    }
-  })
+export const useStyles = makeStyles(
+  theme =>
+    createStyles({
+      container: {
+        margin: theme.spacing(5)
+      },
+      headerContainer: {
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'flex-end'
+      },
+      filtersContainer: {
+        margin: theme.spacing(5, 0)
+      }
+    }),
+  { name: 'DealsListBackOffice' }
 )
