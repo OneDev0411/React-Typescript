0e8ab6991d754e0845a2a05c5b27f7a2060cda77
OneDev0411 <cryptoapple1109@gmail.com>
2022-01-12 15:13:14 +0300

    fix(compile): remove esbuild from compile proccess (#5914)

--- webpack/development.js
+++ webpack/development.js
@@ -1,45 +1,68 @@
 // @ts-nocheck
+const path = require('path')
+
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const webpack = require('webpack')
 const { merge } = require('webpack-merge')
 const Webpackbar = require('webpackbar')
 
 const common = require('./base')
 
-const postcssOptions = {
-  plugins: [
-    require('postcss-preset-env')(),
-    require('postcss-browser-reporter')(),
-    require('postcss-reporter')()
-  ]
-}
-
 const config = {
   mode: 'development',
+  devtool: 'eval',
   output: {
+    path: path.resolve(__dirname, '../dist'),
     filename: '[name].bundle.js',
-    publicPath: '/'
+    publicPath: '/',
+    chunkFilename: '[name].[chunkhash].js',
+    globalObject: 'self'
   },
-  plugins: [new Webpackbar()],
-  module: {
-    rules: [
-      {
-        test: /\.css/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions
-            }
-          }
-        ]
-      },
-      {
-        test: /\.scss/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+  cache: { type: 'filesystem', allowCollectingMemory: true },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 0,
+      maxAsyncRequests: Infinity,
+      maxInitialRequests: Infinity,
+      cacheGroups: {
+        default: {
+          chunks: 'async',
+          minSize: 30000,
+          minChunks: 2,
+          maxAsyncRequests: 5,
+          maxInitialRequests: 3,
+          priority: -20,
+          reuseExistingChunk: true
+        },
+        vendors: {
+          name: 'vendors',
+          enforce: true,
+          test(module) {
+            return module.resource && module.resource.includes('node_modules')
+          },
+          priority: -10,
+          reuseExistingChunk: true
+        }
       }
-    ]
-  }
+    },
+    removeAvailableModules: false,
+    removeEmptyChunks: false,
+    runtimeChunk: {
+      name: entrypoint => `runtime-${entrypoint.name}`
+    },
+    minimize: false
+  },
+  plugins: [
+    new Webpackbar(),
+    new webpack.HotModuleReplacementPlugin(),
+
+    new ReactRefreshWebpackPlugin({
+      overlay: {
+        sockIntegration: 'whm'
+      }
+    })
+  ].filter(Boolean)
 }
 
 module.exports = merge(common, config)
