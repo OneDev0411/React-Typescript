dcbd3da09ee0e2f4ee6f40afd3327d89ff982f20
OneDev0411 <cryptoapple1109@gmail.com>
2022-01-14 01:52:15 +0330

    feat(super campaign): reimplement the update campaign tags hook (#5912)

--- app/models/super-campaign/use-update-super-campaign-tags.ts
+++ app/models/super-campaign/use-update-super-campaign-tags.ts
@@ -1,33 +1,77 @@
-import { useRunActionThenNotify } from '@app/hooks/use-run-action-then-notify'
-import { updateSuperCampaignTags as updateSuperCampaignTagsModel } from '@app/models/super-campaign'
+import { UseMutationResult, useQueryClient } from 'react-query'
+import { ResponseError } from 'superagent'
 
-interface UseUpdateSuperCampaignTags {
-  isSaving: boolean
-  updateSuperCampaignTags: (tags: string[]) => Promise<void>
-}
+import { useMutation, UseMutationOptions } from '@app/hooks/query'
+
+import { getAll, getOne } from './query-keys/campaign'
+import { updateSuperCampaignTags } from './update-super-campaign-tags'
+
+export type UseUpdateSuperCampaignTags = UseMutationResult<
+  ISuperCampaign,
+  ResponseError,
+  string[],
+  { previousSuperCampaign?: ISuperCampaign<'template_instance'> }
+>
+
+export type UseUpdateSuperCampaignTagsOptions = Omit<
+  UseMutationOptions<
+    ISuperCampaign,
+    ResponseError,
+    string[],
+    { previousSuperCampaign?: ISuperCampaign<'template_instance'> }
+  >,
+  'notify' | 'invalidates' | 'onMutate'
+>
 
 export function useUpdateSuperCampaignTags(
-  superCampaign: ISuperCampaign<'template_instance'>,
-  setSuperCampaign: (superCampaign: ISuperCampaign<'template_instance'>) => void
+  superCampaign: Pick<ISuperCampaign, 'id'>,
+  options?: UseUpdateSuperCampaignTagsOptions
 ): UseUpdateSuperCampaignTags {
-  const { isRunning, runActionThenNotify } = useRunActionThenNotify()
+  const queryClient = useQueryClient()
 
-  const updateSuperCampaignTags = async (tags: string[]) =>
-    runActionThenNotify(
-      async () => {
-        const updatedSuperCampaign = await updateSuperCampaignTagsModel(
-          superCampaign.id,
-          tags
+  return useMutation(
+    async tags => updateSuperCampaignTags(superCampaign.id, tags),
+    {
+      ...options,
+      notify: {
+        onSuccess: 'The tags were updated',
+        onError: 'Something went wrong while saving the tags. Please try again.'
+      },
+      invalidates: [getAll()],
+      onMutate: async tags => {
+        await queryClient.cancelQueries(getOne(superCampaign.id))
+
+        const previousSuperCampaign = queryClient.getQueryData<
+          ISuperCampaign<'template_instance'>
+        >(getOne(superCampaign.id))
+
+        if (!previousSuperCampaign) {
+          return {}
+        }
+
+        queryClient.setQueryData<ISuperCampaign<'template_instance'>>(
+          getOne(superCampaign.id),
+          { ...previousSuperCampaign, tags }
         )
 
-        setSuperCampaign({
-          ...updatedSuperCampaign,
-          template_instance: superCampaign.template_instance
-        })
+        return { previousSuperCampaign }
       },
-      'The tags were updated',
-      'Something went wrong while saving the tags. Please try again.'
-    )
+      onError: (error, tags, context) => {
+        if (!context?.previousSuperCampaign) {
+          return
+        }
 
-  return { isSaving: isRunning, updateSuperCampaignTags }
+        queryClient.setQueryData(
+          getOne(superCampaign.id),
+          context.previousSuperCampaign
+        )
+
+        options?.onError?.(error, tags, context)
+      },
+      onSettled: (...args) => {
+        queryClient.invalidateQueries(getOne(superCampaign.id))
+        options?.onSettled?.(...args)
+      }
+    }
+  )
 }
