3fc1ca557bbad2148834f577b5391386b0b2ddf7
OneDev0411 <cryptoapple1109@gmail.com>
2022-01-14 00:13:47 +0330

    feat(super campaign): reimplement send hook using update hook (#5912)

--- app/models/super-campaign/use-send-super-campaign.ts
+++ app/models/super-campaign/use-send-super-campaign.ts
@@ -1,48 +1,49 @@
 import { useContext } from 'react'
 
-import { UseMutationResult } from 'react-query'
-
-import { useMutation } from '@app/hooks/query'
 import { convertDateToTimestamp } from '@app/utils/date-utils'
 import ConfirmationModalContext from '@app/views/components/ConfirmationModal/context'
 
-import { getAll } from './query-keys/campaign'
-import { updateSuperCampaign } from './update-super-campaign'
+import {
+  UseUpdateSuperCampaign,
+  UseUpdateSuperCampaignOptions,
+  useUpdateSuperCampaign
+} from './use-update-super-campaign'
 
-export type UseSendSuperCampaign = UseMutationResult<ISuperCampaign>
+export type UseSendSuperCampaign = UseUpdateSuperCampaign
+
+export type UseSendSuperCampaignOptions = Omit<
+  UseUpdateSuperCampaignOptions,
+  'notify'
+>
 
 export function useSendSuperCampaign(
-  superCampaign: ISuperCampaign
+  superCampaign: ISuperCampaign<'template_instance'>,
+  options?: UseSendSuperCampaignOptions
 ): UseSendSuperCampaign {
   const confirmation = useContext(ConfirmationModalContext)
-  const { mutate, ...other } = useMutation(
-    async () =>
-      updateSuperCampaign(superCampaign.id, {
-        subject: superCampaign.subject,
-        description: superCampaign.description,
-        template_instance: superCampaign.template_instance,
-        due_at: convertDateToTimestamp(new Date())
-      }),
-    {
-      notify: {
-        onSuccess: 'The campaign was sent',
-        onError:
-          'Something went wrong while sending the campaign. Please try again.'
-      },
-      invalidates: [getAll()] // TODO: use optimistic update if possible
+
+  const mutation = useUpdateSuperCampaign(superCampaign, {
+    ...options,
+    notify: {
+      onSuccess: 'The campaign was sent',
+      onError:
+        'Something went wrong while sending the campaign. Please try again.'
     }
-  )
+  })
 
   return {
+    ...mutation,
     mutate: () => {
       confirmation.setConfirmationModal({
         message: `Are you sure you want to send "${
           superCampaign.subject || 'Untitled Campaign'
         }" now?`,
         confirmLabel: 'Yes, I am',
-        onConfirm: () => mutate()
+        onConfirm: () =>
+          mutation.mutate({
+            due_at: convertDateToTimestamp(new Date())
+          })
       })
-    },
-    ...other
+    }
   }
 }
