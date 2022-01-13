bab24c787779e2f90ab16af00bf483f0c62a44fc
OneDev0411 <cryptoapple1109@gmail.com>
2022-01-14 00:44:22 +0330

    feat(super campaign): use the create hook to remove duplicated logic (#5912)

--- app/models/super-campaign/use-duplicate-super-campaign.ts
+++ app/models/super-campaign/use-duplicate-super-campaign.ts
@@ -1,16 +1,20 @@
 import { UseMutationResult } from 'react-query'
+import { ResponseError } from 'superagent'
 
-import { useMutation, UseMutationOptions } from '@app/hooks/query'
+import { UseMutationOptions } from '@app/hooks/query'
 
-import { createSuperCampaign } from './create-super-campaign'
-import { getAll } from './query-keys/campaign'
+import { useCreateSuperCampaign } from './use-create-super-campaign'
 
 export type UseDuplicateSuperCampaign = UseMutationResult<
   ISuperCampaign<'template_instance'>
 >
 
 export type UseDuplicateSuperCampaignOptions = Omit<
-  UseMutationOptions<ISuperCampaign<'template_instance'>>,
+  UseMutationOptions<
+    ISuperCampaign<'template_instance'>,
+    ResponseError,
+    unknown
+  >,
   'notify' | 'invalidates'
 >
 
@@ -18,24 +22,25 @@ export function useDuplicateSuperCampaign(
   superCampaign: ISuperCampaign<'template_instance'>,
   options?: UseDuplicateSuperCampaignOptions
 ): UseDuplicateSuperCampaign {
-  return useMutation(
-    async () =>
-      createSuperCampaign({
+  const mutation = useCreateSuperCampaign({
+    ...options,
+    notify: {
+      onSuccess: 'The campaign was duplicated',
+      onError:
+        'Something went wrong while duplicating the campaign. Please try again.'
+    }
+  })
+
+  return {
+    ...mutation,
+    mutate: () =>
+      mutation.mutate({
         subject: superCampaign.subject,
         description: superCampaign.description,
         template_instance: superCampaign.template_instance?.id,
         tags: superCampaign.tags ?? undefined,
         eligible_brands: superCampaign.eligible_brands ?? undefined,
         due_at: null
-      }),
-    {
-      notify: {
-        onSuccess: 'The campaign was duplicated',
-        onError:
-          'Something went wrong while duplicating the campaign. Please try again.'
-      },
-      invalidates: [getAll()], // TODO: use optimistic update if possible
-      ...options
-    }
-  )
+      })
+  }
 }
