b3f3d11a87e6811365ce649e2bf9bfc687597dde
OneDev0411 <cryptoapple1109@gmail.com>
2022-01-14 12:39:40 +0330

    refactor(super campaign): improve types of create campaign hook (#5912)

--- app/models/super-campaign/use-create-super-campaign.ts
+++ app/models/super-campaign/use-create-super-campaign.ts
@@ -6,24 +6,60 @@ import { useMutation, UseMutationOptions } from '@app/hooks/query'
 import { createSuperCampaign } from './create-super-campaign'
 import { getAll } from './query-keys/campaign'
 
-export type UseCreateSuperCampaign = UseMutationResult<
-  ISuperCampaign<'template_instance'>
+export type UseCreateSuperCampaign<
+  TVariables extends ISuperCampaignInput | ISuperCampaign = ISuperCampaignInput
+> = UseMutationResult<
+  ISuperCampaign<'template_instance'>,
+  ResponseError,
+  TVariables
 >
 
-export type UseCreateSuperCampaignOptions = Omit<
+export type UseCreateSuperCampaignOptions<
+  TVariables extends ISuperCampaignInput | ISuperCampaign = ISuperCampaignInput
+> = Omit<
   UseMutationOptions<
     ISuperCampaign<'template_instance'>,
     ResponseError,
-    ISuperCampaignInput
+    TVariables
   >,
   'invalidates'
 >
 
-export function useCreateSuperCampaign(
-  options?: UseCreateSuperCampaignOptions
-): UseCreateSuperCampaign {
-  return useMutation(createSuperCampaign, {
-    invalidates: [getAll()], // TODO: use optimistic update if possible
-    ...options
-  })
+function isSuperCampaign(
+  data: ISuperCampaignInput | ISuperCampaign
+): data is ISuperCampaign {
+  return 'type' in data && data.type === 'super_campaign'
+}
+
+function getSuperCampaignInput(
+  data: ISuperCampaignInput | ISuperCampaign
+): ISuperCampaignInput {
+  if (isSuperCampaign(data)) {
+    const superCampaign = data
+
+    return {
+      subject: superCampaign.subject,
+      description: superCampaign.description,
+      template_instance: superCampaign.template_instance,
+      tags: superCampaign.tags ?? undefined,
+      eligible_brands: superCampaign.eligible_brands ?? undefined,
+      due_at: superCampaign.due_at
+    }
+  }
+
+  return data
+}
+
+export function useCreateSuperCampaign<
+  TVariables extends ISuperCampaignInput | ISuperCampaign = ISuperCampaignInput
+>(
+  options?: UseCreateSuperCampaignOptions<TVariables>
+): UseCreateSuperCampaign<TVariables> {
+  return useMutation(
+    variables => createSuperCampaign(getSuperCampaignInput(variables)),
+    {
+      ...options,
+      invalidates: [getAll()] // TODO: use optimistic update if possible
+    }
+  )
 }
