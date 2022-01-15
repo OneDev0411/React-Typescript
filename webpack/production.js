62e9519eb007d447a5cf257eb25f30514301a7ae
OneDev0411 <cryptoapple1109@gmail.com>
2022-01-14 22:54:22 -0800

    fix(ci): attempt

--- webpack/production.js
+++ webpack/production.js
@@ -25,7 +25,7 @@ const config = {
   output: {
     pathinfo: false,
     publicPath: process.env.ASSETS_BASEURL,
-    globalObject: 'this'
+    globalObject: 'self'
   },
   optimization: {
     minimize: true,
