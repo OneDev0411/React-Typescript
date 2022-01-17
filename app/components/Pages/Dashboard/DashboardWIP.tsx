9199f89e111e49a07639daa02582ef99c776bd80
OneDev0411 <cryptoapple1109@gmail.com>
2022-01-17 16:50:21 +0330

    wip_feat(team-switcher): wip create a better base dashboard

--- app/components/Pages/Dashboard/DashboardWIP.tsx
+++ app/components/Pages/Dashboard/DashboardWIP.tsx
@@ -1,7 +1,17 @@
-import { Component, cloneElement, useMemo } from 'react'
+/*
+  TODO: due to time limit I just convert it to typescript and small
+  improvmennt to fix the bug but it would be good if make it better in typescript
+*/
+import {
+  useMemo,
+  useEffect,
+  useCallback,
+  ReactElement,
+  cloneElement
+} from 'react'
 
 import { useSelector, useDispatch } from 'react-redux'
-import { withRouter } from 'react-router'
+import { RouteComponentProps } from 'react-router'
 import { useEffectOnce, useTitle } from 'react-use'
 
 import {
@@ -9,9 +19,15 @@ import {
   AccessListType
 } from '@app/components/helpers/get-dashboard-access-list'
 import syncOpenHouseData from '@app/components/helpers/sync-open-house-offline-registers'
+import { useLoadUserAndActiveTeam } from '@app/hooks/use-load-user-and-active-team'
 import asyncComponentLoader from '@app/loader'
-import { isLoadedContactAttrDefs } from '@app/reducers/contacts/attributeDefs'
-import { selectListings } from '@app/reducers/listings'
+import { IAppState } from '@app/reducers'
+import {
+  isLoadedContactAttrDefs,
+  IAttributeDefsState
+} from '@app/reducers/contacts/attributeDefs'
+import { selectContactAttributeDefs } from '@app/selectors/contacts'
+import { selectDeals, selectDealsList } from '@app/selectors/deals'
 import ChatSocket from '@app/services/socket/chat'
 import ContactSocket from '@app/services/socket/contacts'
 import DealSocket from '@app/services/socket/deals'
@@ -32,9 +48,41 @@ import Intercom from '@app/views/components/Intercom'
 
 import { DashboardLayout } from './DashboardLayout'
 
-export function Dashboard(props) {
+const InstantChat = asyncComponentLoader({
+  loader: () => import('./Chatroom/InstantChat')
+})
+
+// TODO: fix type which is set to unknown
+type DashboardState = {
+  rooms: unknown
+  isFetchingDeals: boolean
+  deals: Record<UUID, IDeal>
+  contactsAttributeDefs: IAttributeDefsState
+}
+
+interface DashboardProps
+  extends RouteComponentProps<Record<any, any>, Record<any, any>> {
+  children: ReactElement
+}
+
+export function Dashboard({ params, children, location }: DashboardProps) {
   useTitle('Rechat | Dashboard')
 
+  const { user, activeTeam, ...ff } = useLoadUserAndActiveTeam()
+
+  console.log({ user, activeTeam, ff })
+
+  const {
+    rooms,
+    deals,
+    isFetchingDeals,
+    contactsAttributeDefs
+  }: DashboardState = useSelector((state: IAppState) => ({
+    rooms: state.chatroom.rooms,
+    deals: selectDealsList(state),
+    contactsAttributeDefs: selectContactAttributeDefs(state),
+    isFetchingDeals: selectDeals(state).properties.isFetchingDeals
+  }))
   const dispatch = useDispatch()
 
   const {
@@ -47,11 +95,31 @@ export function Dashboard(props) {
     [activeTeam]
   )
 
+  const handleOnlineEvent = useCallback(() => {
+    // update the number of unread emails in Inbox nav link notification badge
+    dispatch(fetchUnreadEmailThreadsCount())
+  }, [dispatch])
+
+  const initializeSockets = (user: IUser) => {
+    new NotificationSocket(user)
+    new ChatSocket(user)
+
+    if (hasCrmAccess) {
+      new ContactSocket(user)
+    }
+
+    if (hasDealsAccess) {
+      new DealSocket(user)
+    }
+
+    if (hasShowingsAccess) {
+      new ShowingSocket(user)
+    }
+  }
+
   useEffectOnce(() => {
     const init = async () => {
-      const { user, activeTeam, deals } = this.props
-
-      console.log('init of dashboard', { user, activeTeam, deals })
+      // console.log('init of new dashboard', { user, activeTeam, deals })
 
       if (!activeTeam || !user) {
         return
@@ -63,11 +131,11 @@ export function Dashboard(props) {
       if (
         hasDealsAccess &&
         Object.keys(deals).length === 0 &&
-        !this.props.isFetchingDeals
+        !isFetchingDeals
       ) {
         const searchParamValue =
-          this.props.location.pathname.startsWith('/dashboard/deals') &&
-          new URLSearchParams(this.props.location.search).get('q')
+          location.pathname.startsWith('/dashboard/deals') &&
+          new URLSearchParams(location.search).get('q')
 
         if (
           (hasBackOfficeAccess || viewAsEveryoneOnTeam(activeTeam)) &&
@@ -89,12 +157,12 @@ export function Dashboard(props) {
       // load CRM attributes definition
       if (
         (hasCrmAccess || hasDealsAccess) &&
-        !isLoadedContactAttrDefs(this.props.contactsAttributeDefs)
+        !isLoadedContactAttrDefs(contactsAttributeDefs)
       ) {
         dispatch(getAttributeDefs())
       }
 
-      this.initializeSockets(user)
+      initializeSockets(user)
 
       dispatch(getAllNotifications())
 
@@ -107,7 +175,7 @@ export function Dashboard(props) {
         // fetch the number of unread email threads
         dispatch(fetchUnreadEmailThreadsCount())
 
-        window.addEventListener('online', this.handleOnlineEvent)
+        window.addEventListener('online', handleOnlineEvent)
       }
 
       // fetch the number of showing notifications count
@@ -118,4 +186,38 @@ export function Dashboard(props) {
 
     init()
   })
+
+  useEffect(() => {
+    return () => {
+      dispatch(deactivateIntercom(true))
+
+      if (activeTeam && hasCrmAccess) {
+        window.removeEventListener('online', handleOnlineEvent)
+      }
+    }
+  }, [activeTeam, dispatch, handleOnlineEvent, hasCrmAccess])
+
+  return (
+    <CheckBrowser id={params.id}>
+      <div className="u-scrollbar">
+        {user && !user.email_confirmed && !user.fake_email && (
+          <EmailVerificationBanner show email={user.email} />
+        )}
+
+        {user && <InstantChat user={user} rooms={rooms} />}
+
+        <DashboardLayout>
+          {cloneElement(children, {
+            data: {
+              path: location.pathname,
+              location
+            },
+            user
+          })}
+        </DashboardLayout>
+
+        <Intercom />
+      </div>
+    </CheckBrowser>
+  )
 }
--- app/hooks/use-load-user-and-active-team.ts
+++ app/hooks/use-load-user-and-active-team.ts
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffectOnce } from 'react-use'
 
 import { getActiveTeam } from '@app/models/user/get-active-team'
+import { IAppState } from '@app/reducers'
 import { IUserState } from '@app/reducers/user'
 import { setActiveTeam } from '@app/store_actions/active-team'
 import { SIGNIN_SUCCESS } from 'constants/auth/signin'
@@ -11,37 +12,53 @@ import { selectActiveTeamUnsafe } from 'selectors/team'
 import { selectUserUnsafe } from 'selectors/user'
 import Fetch from 'services/fetch'
 
-type UseLoadUserAndActiveTeamReturn = {
+type UseLoadUserAndActiveTeamState = {
   user: IUserState
+  activeTeam: Nullable<IUserTeam>
+}
+
+type UseLoadUserAndActiveTeamReturn = {
+  user: IUser
+  userff: any
+  activeTeam: IUserTeam
+  activeTeamff: any
   error: Nullable<string>
   isLoading: boolean
   isGuest: boolean
+  state: UseLoadUserAndActiveTeamState
 }
 
 export function useLoadUserAndActiveTeam(): UseLoadUserAndActiveTeamReturn {
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState<Nullable<string>>(null)
   const [isGuest, setIsGuest] = useState(false)
+  let state = useSelector<IAppState, UseLoadUserAndActiveTeamState>(state => ({
+    user: selectUserUnsafe(state),
+    activeTeam: selectActiveTeamUnsafe(state)
+  }))
+  const user = useRef<IUserState>(state.user)
+  const activeTeam = useRef<Nullable<IUserTeam>>(state.activeTeam)
 
   const dispatch = useDispatch()
-  const activeTeam = useSelector(selectActiveTeamUnsafe)
-  const user = useSelector(selectUserUnsafe)
+
+  console.log('useLoadUserAndActiveTeam', { state })
 
   useEffectOnce(() => {
     const loadActiveTeam = async (user: IUser) => {
       try {
-        const activeTeam = await getActiveTeam(user)
+        const res = await getActiveTeam(user)
 
-        dispatch(setActiveTeam(activeTeam))
+        activeTeam.current = res
+        dispatch(setActiveTeam(res))
       } catch (e) {
         setError(e.response.data)
       }
     }
 
     const loadUser = async () => {
-      if (user) {
-        if (!activeTeam) {
-          await loadActiveTeam(user)
+      if (user.current) {
+        if (!activeTeam.current) {
+          await loadActiveTeam(user.current)
         }
 
         return
@@ -51,13 +68,13 @@ export function useLoadUserAndActiveTeam(): UseLoadUserAndActiveTeamReturn {
         setIsLoading(true)
         setIsGuest(false)
 
-        const {
-          body: { data: user }
-        } = await new Fetch().get('/api/users/profile')
+        const res = await new Fetch().get('/api/users/profile')
+        const fetchedUser = res.body.data
 
-        await loadActiveTeam(user)
+        user.current = fetchedUser
+        await loadActiveTeam(fetchedUser)
         dispatch({
-          user,
+          user: fetchedUser,
           type: SIGNIN_SUCCESS
         })
       } catch (e) {
@@ -70,11 +87,19 @@ export function useLoadUserAndActiveTeam(): UseLoadUserAndActiveTeamReturn {
 
     loadUser()
   })
+  // console.log('useLoadUserAndActiveTeam', {
+  //   user,
+  //   activeTeam
+  // })
 
   return {
-    user,
     error,
     isGuest,
-    isLoading
+    isLoading,
+    user: user.current!,
+    userff: user,
+    activeTeam: activeTeam.current!,
+    activeTeamff: activeTeam,
+    state
   }
 }
