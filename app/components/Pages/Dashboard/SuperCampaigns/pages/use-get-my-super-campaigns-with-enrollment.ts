2c25037e3f19736f747cd0ae3c3931e24fa665ca
OneDev0411 <cryptoapple1109@gmail.com>
2022-01-19 16:06:41 +0330

    refactor(super campaign): reimplement the hook (#5912)

--- app/components/Pages/Dashboard/SuperCampaigns/pages/use-get-my-super-campaigns-with-enrollment.ts
+++ app/components/Pages/Dashboard/SuperCampaigns/pages/use-get-my-super-campaigns-with-enrollment.ts
@@ -1,108 +1,48 @@
-import { Dispatch, SetStateAction, useEffect } from 'react'
+import { useMemo } from 'react'
 
-import useAsync from '@app/hooks/use-async'
 import {
-  getMySuperCampaignEnrollments,
-  getMySuperCampaigns
+  useGetMySuperCampaigns,
+  useGetMySuperCampaignEnrollments
 } from '@app/models/super-campaign'
 
 interface UseGetMySuperCampaignsWithEnrollment {
   isLoading: boolean
   superCampaignsWithEnrollment: ISuperCampaignWithEnrollment[]
-  setSuperCampaignsWithEnrollment: Dispatch<
-    SetStateAction<ISuperCampaignWithEnrollment[]>
-  >
-  enrollToSuperCampaign: (
-    superCampaignId: UUID,
-    enrollment: ISuperCampaignEnrollment
-  ) => void
-  unenrollFromSuperCampaign: (superCampaignId: UUID) => void
-  setEnrollmentNotificationsEnabled: (
-    superCampaignId: UUID,
-    enabled: boolean
-  ) => void
 }
 
 export function useGetMySuperCampaignsWithEnrollment(
   limit?: number
 ): UseGetMySuperCampaignsWithEnrollment {
+  const { data: mySuperCampaigns, isLoading: isSuperCampaignsLoading } =
+    useGetMySuperCampaigns(limit)
   const {
-    isLoading,
-    data: superCampaignsWithEnrollment,
-    setData: setSuperCampaignsWithEnrollment,
-    run
-  } = useAsync<ISuperCampaignWithEnrollment[]>({
-    data: [],
-    status: 'pending'
-  })
+    data: mySuperCampaignEnrollments,
+    isLoading: isSuperCampaignEnrollmentsLoading
+  } = useGetMySuperCampaignEnrollments()
 
-  useEffect(() => {
-    run(async () => {
-      const [mySuperCampaigns, mySuperCampaignEnrollments] = await Promise.all([
-        getMySuperCampaigns(limit),
-        getMySuperCampaignEnrollments()
-      ])
+  const superCampaignsWithEnrollment = useMemo<
+    ISuperCampaignWithEnrollment[]
+  >(() => {
+    const superCampaignEnrollmentMap = (
+      mySuperCampaignEnrollments ?? []
+    ).reduce<Record<UUID, ISuperCampaignEnrollment>>(
+      (acc, enrollment) => ({
+        ...acc,
+        [enrollment.super_campaign]: enrollment
+      }),
+      {}
+    )
 
-      const superCampaignEnrollmentMap = mySuperCampaignEnrollments.reduce<
-        Record<UUID, ISuperCampaignEnrollment>
-      >(
-        (acc, enrollment) => ({
-          ...acc,
-          [enrollment.super_campaign]: enrollment
-        }),
-        {}
-      )
-
-      return mySuperCampaigns.map<ISuperCampaignWithEnrollment>(
-        mySuperCampaign => ({
-          ...mySuperCampaign,
-          enrollment: superCampaignEnrollmentMap[mySuperCampaign.id]
-        })
-      )
-    })
-  }, [run, limit])
-
-  const updateSuperCampaignEnrollment = (
-    superCampaignId: UUID,
-    modifier: (
-      enrollment: Optional<ISuperCampaignEnrollment>
-    ) => Optional<ISuperCampaignEnrollment>
-  ) =>
-    setSuperCampaignsWithEnrollment(superCampaignsWithEnrollment =>
-      superCampaignsWithEnrollment.map(superCampaignWithEnrollment => {
-        if (superCampaignWithEnrollment.id !== superCampaignId) {
-          return superCampaignWithEnrollment
-        }
-
-        return {
-          ...superCampaignWithEnrollment,
-          enrollment: modifier(superCampaignWithEnrollment.enrollment)
-        }
+    return (mySuperCampaigns ?? []).map<ISuperCampaignWithEnrollment>(
+      mySuperCampaign => ({
+        ...mySuperCampaign,
+        enrollment: superCampaignEnrollmentMap[mySuperCampaign.id]
       })
     )
-
-  const enrollToSuperCampaign = (
-    superCampaignId: UUID,
-    enrollment: ISuperCampaignEnrollment
-  ) => updateSuperCampaignEnrollment(superCampaignId, () => enrollment)
-
-  const unenrollFromSuperCampaign = (superCampaignId: UUID) =>
-    updateSuperCampaignEnrollment(superCampaignId, () => undefined)
-
-  const setEnrollmentNotificationsEnabled = (
-    superCampaignId: UUID,
-    enabled: boolean
-  ) =>
-    updateSuperCampaignEnrollment(superCampaignId, enrollment =>
-      enrollment ? { ...enrollment, notifications_enabled: enabled } : undefined
-    )
+  }, [mySuperCampaignEnrollments, mySuperCampaigns])
 
   return {
-    isLoading,
-    superCampaignsWithEnrollment,
-    setSuperCampaignsWithEnrollment,
-    enrollToSuperCampaign,
-    unenrollFromSuperCampaign,
-    setEnrollmentNotificationsEnabled
+    isLoading: isSuperCampaignsLoading && isSuperCampaignEnrollmentsLoading,
+    superCampaignsWithEnrollment
   }
 }
