f5bf954fbafdb1e969705627f49e776e7efa304c
OneDev0411 <cryptoapple1109@gmail.com>
2022-01-25 13:19:08 +0330

    wip_feat(team-switcher): add impersonate user reducer (#5596)

---
+++ app/reducers/user/impersonate-user.ts
@@ -0,0 +1,22 @@
+import * as actionTypes from '@app/constants/user'
+import type { SetImpersonateUserAction } from '@app/store_actions/user/impersonate-user'
+import { BrandedUser } from '@app/views/components/TeamAgents/types'
+
+export type IImpersonateUserState = Nullable<IUser | BrandedUser>
+
+const defaultImpersonateUserState: IImpersonateUserState = null
+
+function impersonateUserReducer(
+  state: IImpersonateUserState = defaultImpersonateUserState,
+  action: SetImpersonateUserAction
+) {
+  switch (action.type) {
+    case actionTypes.SET_IMPERSONATE_USER:
+      return action.payload
+
+    default:
+      return state
+  }
+}
+
+export default impersonateUserReducer
