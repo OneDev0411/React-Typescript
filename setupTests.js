ac5007c0dcd443e0cfdc81079c096bcf9cc46e98
OneDev0411 <cryptoapple1109@gmail.com>
2022-02-01 19:36:23 +0300

    wip_fix(ci): increase async tests timeout

--- setupTests.js
+++ setupTests.js
@@ -82,5 +82,8 @@ global.getSelection = () => {
 // Using data-test instead of data-testId
 configure({ testIdAttribute: 'data-test' })
 
+// Increase async tests timeout
+configure({ asyncUtilTimeout: 20000 })
+
 // Fix mapbox-gl issue
 window.URL.createObjectURL = function () {}
