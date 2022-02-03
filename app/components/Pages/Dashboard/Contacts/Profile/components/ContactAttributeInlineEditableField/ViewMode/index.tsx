6ca35a6544513227ec40ad725eff1facba8df91a
OneDev0411 <cryptoapple1109@gmail.com>
2022-02-03 17:19:41 +0300

    fix(crm): fixes styling issue in contact profile sidebar (#6026)

--- app/components/Pages/Dashboard/Contacts/Profile/components/ContactAttributeInlineEditableField/ViewMode/index.tsx
+++ app/components/Pages/Dashboard/Contacts/Profile/components/ContactAttributeInlineEditableField/ViewMode/index.tsx
@@ -25,15 +25,16 @@ const useStyles = makeStyles(
       }
     },
     titleContainer: {
-      width: '100%'
+      display: 'flex',
+      alignItems: 'center',
+      gap: theme.spacing(1)
     },
     title: {
-      width: '100%',
       color: theme.palette.grey[600]
     },
     value: {
+      flex: '1 0 auto',
       display: 'inline-block',
-      width: '100%',
       whiteSpace: 'nowrap',
       overflow: 'hidden',
       textAlign: 'right',
@@ -71,26 +72,26 @@ export function ViewMode(props: Props) {
           className={classes.title}
           data-test={`contact-attribute${title ? `-${title}` : ''}`}
         >
-          <span>{title}</span>
-          {props.is_primary && (
-            <Tooltip title="Primary">
-              <SvgIcon
-                path={mdiStarOutline}
-                leftMargined
-                className={classes.starIcon}
-                size={muiIconSizes.small}
-              />
-            </Tooltip>
-          )}
-          {props.isTriggerable && (
+          {title}
+        </Typography>
+
+        {props.is_primary && (
+          <Tooltip title="Primary">
             <SvgIcon
-              path={mdiLightningBoltOutline}
-              leftMargined
-              className={classes.triggerIcon}
+              path={mdiStarOutline}
+              className={classes.starIcon}
               size={muiIconSizes.small}
             />
-          )}
-        </Typography>
+          </Tooltip>
+        )}
+
+        {props.isTriggerable && (
+          <SvgIcon
+            path={mdiLightningBoltOutline}
+            className={classes.triggerIcon}
+            size={muiIconSizes.small}
+          />
+        )}
       </Box>
       <Box className={classes.value}>
         <Typography variant="body2">{props.value || '-'}</Typography>
