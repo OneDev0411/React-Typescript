c619767e369c992f27d652ed8c92a38c0cc1bc06
OneDev0411 <cryptoapple1109@gmail.com>
2022-02-10 22:01:55 +0330

    fix: mock the package and fix the tests

--- app/views/components/EmailCompose/SingleEmailComposeForm.test.tsx
+++ app/views/components/EmailCompose/SingleEmailComposeForm.test.tsx
@@ -25,11 +25,7 @@ jest.mock('models/contacts/search-contacts')
 jest.mock('models/contacts/get-contacts-tags')
 jest.mock('models/filter-segments/get-segments')
 jest.mock('models/o-auth-accounts/get-o-auth-accounts')
-jest.mock('use-dropbox-chooser', () => ({
-  useDropboxChooser: () => ({
-    isOpen: false
-  })
-}))
+jest.mock('use-dropbox-chooser')
 
 describe('BulkEmailComposeForm', () => {
   /**
