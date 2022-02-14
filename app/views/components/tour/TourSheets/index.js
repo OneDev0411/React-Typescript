7ca60ae9fcd307e76955f2544c5a027f93bc46c4
OneDev0411 <cryptoapple1109@gmail.com>
2022-02-14 12:55:23 +0300

    fix(tours): force user to print tours in portrait mode (#5940)

--- app/views/components/tour/TourSheets/index.js
+++ app/views/components/tour/TourSheets/index.js
@@ -1,12 +1,13 @@
-import React from 'react'
-
 import PropTypes from 'prop-types'
+
+import usePrintLayout from '@app/hooks/use-print-layout'
 
 import BareModal from '../../BareModal'
 
 import { CoverPage } from './CoverPage'
 import { Header } from './Header'
 import { LocationPage } from './LocationPage'
+
 import './styles/main.scss'
 
 TourSheets.propTypes = {
@@ -18,6 +19,8 @@ TourSheets.propTypes = {
 }
 
 export function TourSheets(props) {
+  usePrintLayout('portrait')
+
   const { tour, listings, handleClose } = props
   const pageTitle = 'Tour Sheets Preview'
   let agent = tour && tour.created_by
@@ -33,28 +36,30 @@ export function TourSheets(props) {
   }
 
   return (
-    <BareModal
-      isOpen
-      className="c-tour-sheets"
-      overlayClassName="c-tour-sheets-modal"
-      contentLabel={pageTitle}
-      onRequestClose={handleClose}
-    >
-      <Header
-        className="c-tour-sheets-modal__header"
-        handleClose={handleClose}
-        title={pageTitle}
-      />
-      <CoverPage tour={tour} listings={listings} agent={agent} />
-      {listings.map((listing, index) => (
-        <LocationPage
-          key={index}
-          index={index}
-          listing={listing}
-          tour={tour}
-          agent={agent}
+    <>
+      <BareModal
+        isOpen
+        className="c-tour-sheets"
+        overlayClassName="c-tour-sheets-modal"
+        contentLabel={pageTitle}
+        onRequestClose={handleClose}
+      >
+        <Header
+          className="c-tour-sheets-modal__header"
+          handleClose={handleClose}
+          title={pageTitle}
         />
-      ))}
-    </BareModal>
+        <CoverPage tour={tour} listings={listings} agent={agent} />
+        {listings.map((listing, index) => (
+          <LocationPage
+            key={index}
+            index={index}
+            listing={listing}
+            tour={tour}
+            agent={agent}
+          />
+        ))}
+      </BareModal>
+    </>
   )
 }
