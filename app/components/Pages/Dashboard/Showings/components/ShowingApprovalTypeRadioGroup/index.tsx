ac8167957786510d4bc30768e187e4f955e0299a
OneDev0411 <cryptoapple1109@gmail.com>
2022-02-16 17:38:14 +0330

    feat(showing): description was added to Radio

--- app/components/Pages/Dashboard/Showings/components/ShowingApprovalTypeRadioGroup/index.tsx
+++ app/components/Pages/Dashboard/Showings/components/ShowingApprovalTypeRadioGroup/index.tsx
@@ -3,15 +3,21 @@ import { RadioGroup, RadioItem, RadioGroupProps } from 'components/RadioGroup'
 const approvalTypeOptions: RadioItem<IShowingApprovalType>[] = [
   {
     label: 'Appointments required, confirm with any',
-    value: 'Any'
+    value: 'Any',
+    description:
+      'Permission must be obtained from ANY of the designated listing contacts (Owner(s)/Occupant(s)/ Listing Agent(s)) before the appointment request can be confirmed. Typically used for occupied homes.'
   },
   {
     label: 'Appointments required, confirm with all',
-    value: 'All'
+    value: 'All',
+    description:
+      'Permission must be obtained from ALL designated listing contacts (Owner(s)/Occupant(s)/Listing Agent(s)) before the appointment request can be confirmed.'
   },
   {
     label: 'Go and Show',
-    value: 'None'
+    value: 'None',
+    description:
+      'Appointment requests are documented and immediately confirmed. No additional calls will be made. Typically used for vacant homes on lockbox.'
   }
 ]
 
