232c04597bfa23727538a40e410784b6a19f1d8b
OneDev0411 <cryptoapple1109@gmail.com>
2022-02-25 01:32:35 +0330

    feat(account settings): use the new connect button (#6093)

--- app/components/Pages/Dashboard/Account/ConnectedAccounts/ConnectedInstagram.tsx
+++ app/components/Pages/Dashboard/Account/ConnectedAccounts/ConnectedInstagram.tsx
@@ -1,6 +1,9 @@
-import { Button } from '@material-ui/core'
+import useNotify from '@app/hooks/use-notify'
 
 import ConnectedInstagramAccounts from '../components/ConnectedInstagramAccounts'
+import ConnectInstagramAccountButton, {
+  FacebookAuthErrorCode
+} from '../components/ConnectInstagramAccountButton'
 
 import ConnectedAccountsLayout from './ConnectedAccountsLayout'
 
@@ -9,15 +12,33 @@ interface Props {
 }
 
 function ConnectedInstagram({ className }: Props) {
+  const notify = useNotify()
+
+  const handleAuthSuccess = () =>
+    notify({
+      status: 'success',
+      message: 'Your Instagram account has been connected'
+    })
+
+  const handleAuthError = (errorCode: FacebookAuthErrorCode) =>
+    notify({
+      status: 'error',
+      message: `Something went wrong. errorCode: ${errorCode}`
+    })
+
   return (
     <ConnectedAccountsLayout
       className={className}
       title="Instagram"
       description="Share marketing stuff directly to Instagram"
       action={
-        <Button variant="outlined" size="small">
+        <ConnectInstagramAccountButton
+          variant="outlined"
+          onAuthSuccess={handleAuthSuccess}
+          onAuthError={handleAuthError}
+        >
           Connect Instagram Account
-        </Button>
+        </ConnectInstagramAccountButton>
       }
     >
       <ConnectedInstagramAccounts />
--- app/components/Pages/Dashboard/Account/components/ConnectInstagramAccountButton/index.ts
+++ app/components/Pages/Dashboard/Account/components/ConnectInstagramAccountButton/index.ts
@@ -1,1 +1,2 @@
 export { default } from './ConnectInstagramAccountButton'
+export * from './types'
