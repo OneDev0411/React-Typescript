35c1d9c545109ca2998d29476585019d641e76fb
OneDev0411 <cryptoapple1109@gmail.com>
2022-02-25 18:01:17 +0300

    refactor(proxy): refactor signout endpoint (#6111)

--- server/app/routes/user/signout/index.ts
+++ server/app/routes/user/signout/index.ts
@@ -4,31 +4,13 @@ import { RequestWithSession } from '../../../../types'
 
 export default async (req: RequestWithSession, res: Response) => {
   if (!req.session?.user) {
-    res.redirect('/')
+    res.status(500).send()
 
     return
   }
 
-  const { redirectFromSignout, redirect_to, ...queryParams } = req.query
-  let redirect = redirectFromSignout || redirect_to || '/signin'
-
-  if (queryParams) {
-    redirect += createParams(queryParams as Record<string, string>)
-  }
-
   req.session.user = null
 
-  res.redirect(redirect as string)
-}
-
-function createParams(params: Record<string, string>) {
-  const queryString = Object.keys(params)
-    .map(key => `${key}=${encodeURIComponent(params[key])}`)
-    .join('&')
-
-  if (queryString.length > 0) {
-    return `?${queryString}`
-  }
-
-  return ''
+  res.status(200)
+  res.json({})
 }
