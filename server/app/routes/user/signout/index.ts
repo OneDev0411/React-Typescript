175fa41b368dc3db46b0804f0e157190ee561be8
OneDev0411 <cryptoapple1109@gmail.com>
2022-03-03 19:11:37 +0300

    refactor(auth): remove 500 error on signout (#6111)

--- server/app/routes/user/signout/index.ts
+++ server/app/routes/user/signout/index.ts
@@ -3,12 +3,6 @@ import { Response } from 'express'
 import { RequestWithSession } from '../../../../types'
 
 export default async (req: RequestWithSession, res: Response) => {
-  if (!req.session?.user) {
-    res.status(500).send()
-
-    return
-  }
-
   req.session.user = null
 
   res.status(200)
