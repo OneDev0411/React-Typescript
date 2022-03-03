2c486317302a0c02d0c58b44067cd0a97ffe841e
OneDev0411 <cryptoapple1109@gmail.com>
2022-03-03 19:01:29 +0300

    chore(auth): remove extra codes (#6111)

--- app/models/auth/signout/index.ts
+++ app/models/auth/signout/index.ts
@@ -1,13 +1,7 @@
 import Fetch from 'services/fetch'
 
 const signout = async (): Promise<unknown> => {
-  try {
-    const response = await new Fetch().post('/api/users/signout').send({})
-
-    return response
-  } catch ({ status }) {
-    throw status
-  }
+  return new Fetch().post('/api/users/signout').send({})
 }
 
 export default signout
