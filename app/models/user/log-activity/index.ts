c4ad8cb702acddf8e6ad4cc9b63c9bc0ba7441bd
OneDev0411 <cryptoapple1109@gmail.com>
2022-03-04 16:33:17 +0300

    refactor(log): replace js-cookie with 'universal-cookie' (#6111)

--- app/models/user/log-activity/index.ts
+++ app/models/user/log-activity/index.ts
@@ -1,6 +1,8 @@
-import Cookies from 'js-cookie'
+import Cookies from 'universal-cookie'
 
 import Fetch from '../../../services/fetch'
+
+const cookies = new Cookies()
 
 /**
  * Log new activity.
@@ -15,16 +17,15 @@ export async function logUserActivity(
 
     // Get user ip address from cookie which is set by the proxy server
     if (isIpNeeded) {
-      const userCookie = Cookies.get('user')
-      const userIpAddress = userCookie
-        ? (JSON.parse(userCookie)?.ip as string)
-        : ''
+      const userCookie = cookies.get('user') as unknown as { ip: string }
+
+      const userIpAddress = userCookie ? userCookie.ip : ''
 
       data = {
         ...activity,
         // add user ip address to object_sa if needed
         object: {
-          ...activity.object,
+          ...(activity.object as object),
           ip_address: userIpAddress
         }
       }
