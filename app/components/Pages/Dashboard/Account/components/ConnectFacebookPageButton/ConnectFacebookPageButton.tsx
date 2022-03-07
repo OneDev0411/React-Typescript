466d7e1a671e81df088bb982051f28013ac2c2e4
OneDev0411 <cryptoapple1109@gmail.com>
2022-03-07 14:40:52 +0330

    feat(mc): handle the auth error state with cofirmation dialog (#6094)

--- app/components/Pages/Dashboard/Account/components/ConnectFacebookPageButton/ConnectFacebookPageButton.tsx
+++ app/components/Pages/Dashboard/Account/components/ConnectFacebookPageButton/ConnectFacebookPageButton.tsx
@@ -1,36 +1,73 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 
 import { Button, ButtonProps, CircularProgress } from '@material-ui/core'
 
+import ConfirmationModalContext from '@app/views/components/ConfirmationModal/context'
+
+import { FacebookAuthErrorCode } from './types'
 import { useFacebookAuth, UseFacebookAuthOptions } from './use-facebook-auth'
 
-interface ConnectFacebookPageButtonProps
+export interface ConnectFacebookPageButtonProps
   extends Omit<ButtonProps, 'size' | 'onClick' | 'startIcon' | 'disabled'>,
-    Pick<UseFacebookAuthOptions, 'onAuthSuccess' | 'onAuthError'> {}
+    Pick<
+      UseFacebookAuthOptions,
+      'onAuthSuccess' | 'onAuthError' | 'onAuthWindowOpen' | 'onAuthWindowClose'
+    > {
+  onErrorDialogOpen?: () => void
+  onErrorDialogClose?: () => void
+}
 
 function ConnectFacebookPageButton({
   onAuthSuccess,
   onAuthError,
+  onAuthWindowOpen,
+  onAuthWindowClose,
+  onErrorDialogOpen,
+  onErrorDialogClose,
   ...buttonProps
 }: ConnectFacebookPageButtonProps) {
   const [isWorking, setIsWorking] = useState(false)
 
+  const confirmation = useContext(ConfirmationModalContext)
+
+  const handleAuthError = (errorCode: FacebookAuthErrorCode) => {
+    onErrorDialogOpen?.()
+
+    confirmation.setConfirmationModal({
+      message: 'Something went wrong!',
+      description: `We’re unable to connect to instagram at this moment. Please try again later.\nerror code: ${errorCode}`,
+      confirmLabel: 'Ok',
+      needsCancel: false,
+      onConfirm: () => {
+        onErrorDialogClose?.()
+      }
+    })
+
+    onAuthError?.(errorCode)
+  }
+
   const { openAuthWindow } = useFacebookAuth({
-    onAuthWindowOpen: () => setIsWorking(true),
-    onAuthWindowClose: () => setIsWorking(false),
+    onAuthWindowOpen: () => {
+      setIsWorking(true)
+      onAuthWindowOpen?.()
+    },
+    onAuthWindowClose: () => {
+      setIsWorking(false)
+      onAuthWindowClose?.()
+    },
     onAuthSuccess,
-    onAuthError
+    onAuthError: handleAuthError
   })
 
   return (
     <Button
+      {...buttonProps}
       size="small"
       onClick={openAuthWindow}
       startIcon={
         isWorking ? <CircularProgress size={20} color="inherit" /> : null
       }
       disabled={isWorking}
-      {...buttonProps}
     />
   )
 }
