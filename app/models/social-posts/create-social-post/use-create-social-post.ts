1af3241ceac270230ac74412f97a16927d21c076
OneDev0411 <cryptoapple1109@gmail.com>
2022-03-15 20:10:43 +0330

    fix(mc): define DataInput interface and add brand prop (#6094)

--- app/models/social-posts/create-social-post/use-create-social-post.ts
+++ app/models/social-posts/create-social-post/use-create-social-post.ts
@@ -7,21 +7,25 @@ import { list } from '../query-keys'
 
 import { createSocialPost } from './create-social-post'
 
+interface DataInput extends ISocialPostInput {
+  brand: UUID
+}
+
 export type UseCreateSocialPost = UseMutationResult<
   ISocialPost,
   ResponseError,
-  ISocialPostInput
+  DataInput
 >
 
 export type UseCreateSocialPostOptions = Omit<
-  UseMutationOptions<ISocialPost, ResponseError, ISocialPostInput>,
+  UseMutationOptions<ISocialPost, ResponseError, DataInput>,
   'invalidates'
 >
 
 export function useCreateSocialPost(
   options?: UseCreateSocialPostOptions
 ): UseCreateSocialPost {
-  return useMutation(createSocialPost, {
+  return useMutation(async values => createSocialPost(values), {
     ...options,
     // Just invalidate the lists of campaigns because we dont know the list sort logic
     // at this stage so having an optimistic update is not easy
