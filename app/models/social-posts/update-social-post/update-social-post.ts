5477f8aef09f238733500618b737a86d530993f3
OneDev0411 <cryptoapple1109@gmail.com>
2022-03-30 17:04:35 +0430

    fix(mc): rename the data key to due_at (#6302)

--- app/models/social-posts/update-social-post/update-social-post.ts
+++ app/models/social-posts/update-social-post/update-social-post.ts
@@ -11,10 +11,8 @@ export async function updateSocialPost(
     (
       await new Fetch()
         .put(`/brands/${brandId}/social-post/${socialPostId}`)
-        // TODO: Ask API guys to:
-        // 1. Use the same patterns as create social record -> Needs to change dueAt to due_at
-        // 2. Have consistant types on get and post methods. -> Seems like the get is in seconds and the put is in milliseconds
-        .send({ dueAt: data.due_at * 1000 })
+        // TODO: Ask API guys to have consistant types on get and post methods. -> Seems like the get is in seconds and the put is in milliseconds
+        .send({ due_at: data.due_at * 1000 })
     ).body.data
   )
 }
