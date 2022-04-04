468f98f43aa71998283464d7fc9e092c45949b1f
OneDev0411 <cryptoapple1109@gmail.com>
2022-04-04 16:16:28 +0430

    fix(mc): convert due_at to milliseconds (#6302)

--- app/components/Pages/Dashboard/MarketingInsights/pages/SocialPostList/helpers.ts
+++ app/components/Pages/Dashboard/MarketingInsights/pages/SocialPostList/helpers.ts
@@ -3,5 +3,8 @@ import { isBefore } from 'date-fns'
 export function isSocialPostExecuted(
   socialPost: Pick<ISocialPost, 'executed_at' | 'due_at'>
 ): boolean {
-  return !!socialPost.executed_at || isBefore(socialPost.due_at, new Date())
+  return (
+    // The socialPost.due_at is in seconds but isBefore needs a milliseconds value
+    !!socialPost.executed_at || isBefore(socialPost.due_at * 1000, new Date())
+  )
 }
