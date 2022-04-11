381f75a6c196da5fb8e89b0a39b17e493ac69b2b
OneDev0411 <cryptoapple1109@gmail.com>
2022-04-11 22:38:32 +0430

    refactor(navigation): remove bowser and react-smart-banner library (#6071)

--- app/views/components/CheckBrowser/index.tsx
+++ app/views/components/CheckBrowser/index.tsx
@@ -3,9 +3,6 @@ import { useState, ReactElement } from 'react'
 import { Snackbar, Link, Theme } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import { Alert } from '@material-ui/lab'
-import bowser from 'bowser'
-import SmartBanner from 'react-smartbanner'
-import 'react-smartbanner/dist/main.css'
 
 import {
   browserStatus,
@@ -30,13 +27,8 @@ interface CheckBrowserPropsType {
 
 function CheckBrowser(props: CheckBrowserPropsType) {
   const classes = useStyles()
-
   const status = browserStatus()
-
-  const { name: operatingSystem } = bowser
-    .getParser(window.navigator.userAgent)
-    .getOS()
-
+  const message = getMessage(status)
   const [isShowSnackbar, setShowSnackbar] = useState(
     !status.isSupported || status.isOutdated
   )
@@ -45,26 +37,8 @@ function CheckBrowser(props: CheckBrowserPropsType) {
     return props.children
   }
 
-  let message = getMessage(status)
-
-  if (operatingSystem === 'Android') {
-    message = {
-      text: 'Android',
-      actionText: 'Action Text',
-      actionLink:
-        'https://play.google.com/store/apps/details?id=com.rechat.mobile'
-    }
-  } else if (operatingSystem === 'iOS') {
-    message = {
-      text: 'iOS',
-      actionText: 'Action Text',
-      actionLink: 'https://apps.apple.com/us/app/rechat/id974093560'
-    }
-  }
-
   return (
     <>
-      <SmartBanner title="Facebook" />
       {isShowSnackbar && (
         <Snackbar
           open
