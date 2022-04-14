0a510c33c8385f47d95550fa004d924da9704ab5
OneDev0411 <cryptoapple1109@gmail.com>
2022-04-14 20:12:54 +0430

    fix(server): stop using the web typings in server for now (#6367)

--- server/app/routes/videobolt/helpers.ts
+++ server/app/routes/videobolt/helpers.ts
@@ -1,24 +1,21 @@
 import axios from 'axios'
 
-import { VideoboltVideo } from '../../../../app/views/components/SearchVideoDrawer/types'
+// TODO: fix this import.
+// Looks like the server can not use these types from the web here.
+// import { VideoboltVideo } from '../../../../app/views/components/SearchVideoDrawer/types'
 
 import { VIDEOBOLT_API_URL, VIDEOBOLT_KEY } from './constants'
 
-export async function getVideoboltVideos(
-  email: string
-): Promise<VideoboltVideo[]> {
+export async function getVideoboltVideos(email: string): Promise<unknown[]> {
   try {
-    const response = await axios.get<{ videos: VideoboltVideo[] }>(
-      VIDEOBOLT_API_URL,
-      {
-        headers: {
-          Key: VIDEOBOLT_KEY
-        },
-        params: {
-          email
-        }
+    const response = await axios.get<{ videos: unknown[] }>(VIDEOBOLT_API_URL, {
+      headers: {
+        Key: VIDEOBOLT_KEY
+      },
+      params: {
+        email
       }
-    )
+    })
 
     return response.data.videos
   } catch (error) {
