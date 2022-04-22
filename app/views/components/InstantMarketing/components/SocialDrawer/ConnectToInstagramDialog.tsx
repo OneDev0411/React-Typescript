6930ac7246185aaede3c8b10f53ef96298038b7a
OneDev0411 <cryptoapple1109@gmail.com>
2022-04-22 15:42:13 +0430

    feat(social post): update the help dialog logic on account settings (#6410)

--- app/views/components/InstantMarketing/components/SocialDrawer/ConnectToInstagramDialog.tsx
+++ app/views/components/InstantMarketing/components/SocialDrawer/ConnectToInstagramDialog.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 import { DialogContent, Typography, makeStyles } from '@material-ui/core'
 import { mdiArrowLeftRight } from '@mdi/js'
@@ -6,6 +6,8 @@ import { mdiArrowLeftRight } from '@mdi/js'
 import ConnectFacebookPageButton, {
   ConnectFacebookPageButtonProps
 } from '@app/components/Pages/Dashboard/Account/components/ConnectFacebookPageButton'
+import HowToConnectToInstagramButton from '@app/components/Pages/Dashboard/Account/components/HowToConnectToInstagramButton'
+import HowToConnectToInstagramDialog from '@app/components/Pages/Dashboard/Account/components/HowToConnectToInstagramDialog'
 import Logo from '@app/views/components/Logo'
 import ModernDialog, {
   ModernDialogProps
@@ -44,9 +46,11 @@ function ConnectToInstagramDialog({
   ...dialogProps
 }: ConnectToInstagramDialogProps) {
   const classes = useStyles()
+  const connectButtonRef = useRef<HTMLButtonElement>(null)
 
   const [isAuthWindowOpen, setIsAuthWindowOpen] = useState(false)
   const [isErrorDialogOpen, setIsErrorDialogOpen] = useState(false)
+  const [isHelpDialogOpen, setIsHelpDialogOpen] = useState(false)
 
   const handleAuthWindowOpen = () => setIsAuthWindowOpen(true)
 
@@ -56,55 +60,78 @@ function ConnectToInstagramDialog({
 
   const handleErrorDialogClose = () => setIsErrorDialogOpen(false)
 
+  const openHelpDialog = () => setIsHelpDialogOpen(true)
+
+  const closeHelpDialog = () => setIsHelpDialogOpen(false)
+
+  const handleConnectButton = () => {
+    // Wait a bit for the connect dialog to be available and then click on the connect button
+    setTimeout(() => connectButtonRef.current?.click(), 500)
+  }
+
   return (
-    <ModernDialog
-      {...dialogProps}
-      open={!isErrorDialogOpen && open}
-      onClose={!isAuthWindowOpen ? onClose : undefined}
-    >
-      <ModernDialog.Header title="Do you want to connect Instagram to Rechat?" />
-      <ModernDialog.Body wrapInDialogContent={false}>
-        <div className={classes.graphic}>
-          <Logo
-            className={classes.icon}
-            fallbackUrl="/static/images/logo--mini.svg"
-          />
-          <SvgIcon
-            className={classes.leftRightArrow}
-            path={mdiArrowLeftRight}
-            size={muiIconSizes.large}
-          />
-          <img
-            className={classes.icon}
-            src="/static/images/instagram-logo.svg"
-            alt="Instagram Logo"
-          />
-        </div>
-        <DialogContent>
-          <Typography variant="body2">
-            We will not send anything without your permission. Easily manage
-            your connected account later in Account Settings.
-          </Typography>
-        </DialogContent>
-      </ModernDialog.Body>
-      <ModernDialog.Footer
-        cancelLabel="Cancel"
-        cancelProps={{ disabled: isAuthWindowOpen }}
-        confirmLabel="Continue"
-        keepDialogOpenOnConfirm
-        renderConfirm={props => (
-          <ConnectFacebookPageButton
-            {...props}
-            onAuthError={onAuthError}
-            onAuthSuccess={onAuthSuccess}
-            onAuthWindowOpen={handleAuthWindowOpen}
-            onAuthWindowClose={handleAuthWindowClose}
-            onErrorDialogOpen={handleErrorDialogOpen}
-            onErrorDialogClose={handleErrorDialogClose}
-          />
-        )}
+    <>
+      <ModernDialog
+        {...dialogProps}
+        open={!isErrorDialogOpen && !isHelpDialogOpen && open}
+        onClose={onClose}
+      >
+        <ModernDialog.Header
+          title="Do you want to connect Instagram to Rechat?"
+          closeProps={{ disabled: isAuthWindowOpen }}
+        />
+        <ModernDialog.Body wrapInDialogContent={false}>
+          <div className={classes.graphic}>
+            <Logo
+              className={classes.icon}
+              fallbackUrl="/static/images/logo--mini.svg"
+            />
+            <SvgIcon
+              className={classes.leftRightArrow}
+              path={mdiArrowLeftRight}
+              size={muiIconSizes.large}
+            />
+            <img
+              className={classes.icon}
+              src="/static/images/instagram-logo.svg"
+              alt="Instagram Logo"
+            />
+          </div>
+          <DialogContent>
+            <Typography variant="body2">
+              We will not send anything without your permission. Easily manage
+              your connected account later in Account Settings.
+            </Typography>
+          </DialogContent>
+        </ModernDialog.Body>
+        <ModernDialog.Footer
+          cancelLabel="How to connect to Instagram"
+          cancelProps={{ disabled: isAuthWindowOpen }}
+          onCancel={openHelpDialog}
+          keepDialogOpenOnCancel
+          renderCancel={props => <HowToConnectToInstagramButton {...props} />}
+          confirmLabel="Continue"
+          keepDialogOpenOnConfirm
+          renderConfirm={props => (
+            <ConnectFacebookPageButton
+              {...props}
+              ref={connectButtonRef}
+              onAuthError={onAuthError}
+              onAuthSuccess={onAuthSuccess}
+              onAuthWindowOpen={handleAuthWindowOpen}
+              onAuthWindowClose={handleAuthWindowClose}
+              onErrorDialogOpen={handleErrorDialogOpen}
+              onErrorDialogClose={handleErrorDialogClose}
+            />
+          )}
+        />
+      </ModernDialog>
+      <HowToConnectToInstagramDialog
+        open={isHelpDialogOpen}
+        onConnectClick={handleConnectButton}
+        onClose={closeHelpDialog}
       />
-    </ModernDialog>
+    </>
   )
 }
 
