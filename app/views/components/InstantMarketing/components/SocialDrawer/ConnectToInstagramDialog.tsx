ef6b9950880c75ae2be5f939af302af869dc4e0f
OneDev0411 <cryptoapple1109@gmail.com>
2022-04-22 12:45:27 +0430

    refactor(social post): reimplement connect dialog using modern dialog (#6410)

--- app/views/components/InstantMarketing/components/SocialDrawer/ConnectToInstagramDialog.tsx
+++ app/views/components/InstantMarketing/components/SocialDrawer/ConnectToInstagramDialog.tsx
@@ -1,39 +1,20 @@
 import { useState } from 'react'
 
-import {
-  Dialog,
-  DialogProps,
-  DialogContent,
-  DialogActions,
-  Button,
-  DialogTitle,
-  Box,
-  Typography,
-  IconButton,
-  makeStyles
-} from '@material-ui/core'
-import { mdiClose, mdiArrowLeftRight } from '@mdi/js'
+import { DialogContent, Typography, makeStyles } from '@material-ui/core'
+import { mdiArrowLeftRight } from '@mdi/js'
 
 import ConnectFacebookPageButton, {
   ConnectFacebookPageButtonProps
 } from '@app/components/Pages/Dashboard/Account/components/ConnectFacebookPageButton'
 import Logo from '@app/views/components/Logo'
+import ModernDialog, {
+  ModernDialogProps
+} from '@app/views/components/ModernDialog'
 import { muiIconSizes } from '@app/views/components/SvgIcons/icon-sizes'
 import { SvgIcon } from '@app/views/components/SvgIcons/SvgIcon'
 
 const useStyles = makeStyles(
   theme => ({
-    paper: { maxWidth: 456 },
-    header: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      minHeight: theme.spacing(7),
-      borderBottom: `1px solid ${theme.palette.grey[200]}`,
-      paddingRight: theme.spacing(1)
-    },
-    title: { flex: 1 },
-    footer: { padding: theme.spacing(2, 3, 3, 3) },
     graphic: {
       padding: theme.spacing(3, 0, 2, 0),
       display: 'flex',
@@ -52,7 +33,7 @@ const useStyles = makeStyles(
   { name: 'ConnectToInstagramDialog' }
 )
 
-type ConnectToInstagramDialogProps = DialogProps &
+type ConnectToInstagramDialogProps = ModernDialogProps &
   Pick<ConnectFacebookPageButtonProps, 'onAuthError' | 'onAuthSuccess'>
 
 function ConnectToInstagramDialog({
@@ -67,8 +48,6 @@ function ConnectToInstagramDialog({
   const [isAuthWindowOpen, setIsAuthWindowOpen] = useState(false)
   const [isErrorDialogOpen, setIsErrorDialogOpen] = useState(false)
 
-  const handleCancel = () => onClose?.({}, 'escapeKeyDown')
-
   const handleAuthWindowOpen = () => setIsAuthWindowOpen(true)
 
   const handleAuthWindowClose = () => setIsAuthWindowOpen(false)
@@ -78,68 +57,54 @@ function ConnectToInstagramDialog({
   const handleErrorDialogClose = () => setIsErrorDialogOpen(false)
 
   return (
-    <Dialog
+    <ModernDialog
       {...dialogProps}
       open={!isErrorDialogOpen && open}
       onClose={!isAuthWindowOpen ? onClose : undefined}
-      fullWidth
-      classes={{ paper: classes.paper }}
     >
-      <Box className={classes.header}>
-        <DialogTitle className={classes.title} disableTypography>
-          <Typography variant="subtitle1">
-            Do you want to connect Instagram to Rechat
+      <ModernDialog.Header title="Do you want to connect Instagram to Rechat?" />
+      <ModernDialog.Body wrapInDialogContent={false}>
+        <div className={classes.graphic}>
+          <Logo
+            className={classes.icon}
+            fallbackUrl="/static/images/logo--mini.svg"
+          />
+          <SvgIcon
+            className={classes.leftRightArrow}
+            path={mdiArrowLeftRight}
+            size={muiIconSizes.large}
+          />
+          <img
+            className={classes.icon}
+            src="/static/images/instagram-logo.svg"
+            alt="Instagram Logo"
+          />
+        </div>
+        <DialogContent>
+          <Typography variant="body2">
+            We’re not gonna send anything without your permission. you can
+            manage your connected account later in account settings
           </Typography>
-        </DialogTitle>
-        <IconButton onClick={handleCancel} disabled={isAuthWindowOpen}>
-          <SvgIcon path={mdiClose} />
-        </IconButton>
-      </Box>
-      <div className={classes.graphic}>
-        <Logo
-          className={classes.icon}
-          fallbackUrl="/static/images/logo--mini.svg"
-        />
-        <SvgIcon
-          className={classes.leftRightArrow}
-          path={mdiArrowLeftRight}
-          size={muiIconSizes.large}
-        />
-        <img
-          className={classes.icon}
-          src="/static/images/instagram-logo.svg"
-          alt="Instagram Logo"
-        />
-      </div>
-      <DialogContent>
-        <Typography variant="body2">
-          We’re not gonna send anything without your permission. you can manage
-          your connected account later in account settings
-        </Typography>
-      </DialogContent>
-      <DialogActions className={classes.footer}>
-        <Button
-          onClick={handleCancel}
-          variant="outlined"
-          size="small"
-          disabled={isAuthWindowOpen}
-        >
-          Cancel
-        </Button>
-        <ConnectFacebookPageButton
-          variant="contained"
-          color="primary"
-          onAuthError={onAuthError}
-          onAuthSuccess={onAuthSuccess}
-          onAuthWindowOpen={handleAuthWindowOpen}
-          onAuthWindowClose={handleAuthWindowClose}
-          onErrorDialogOpen={handleErrorDialogOpen}
-          onErrorDialogClose={handleErrorDialogClose}
-        >
-          Continue
-        </ConnectFacebookPageButton>
-      </DialogActions>
-    </Dialog>
+        </DialogContent>
+      </ModernDialog.Body>
+      <ModernDialog.Footer
+        cancelLabel="Cancel"
+        cancelProps={{ disabled: isAuthWindowOpen }}
+        confirmLabel="Continue"
+        keepDialogOpenOnConfirm
+        renderConfirm={props => (
+          <ConnectFacebookPageButton
+            {...props}
+            onAuthError={onAuthError}
+            onAuthSuccess={onAuthSuccess}
+            onAuthWindowOpen={handleAuthWindowOpen}
+            onAuthWindowClose={handleAuthWindowClose}
+            onErrorDialogOpen={handleErrorDialogOpen}
+            onErrorDialogClose={handleErrorDialogClose}
+          />
+        )}
+      />
+    </ModernDialog>
   )
 }
 
