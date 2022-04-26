071e4deab2374063a6ce470aeb10505b49ce6d18
OneDev0411 <cryptoapple1109@gmail.com>
2022-04-26 17:23:08 +0430

    feat(social post): add the preview button to the social post list (#6411)

--- app/components/Pages/Dashboard/MarketingInsights/pages/SocialPostList/SocialPostTableColumnActions.tsx
+++ app/components/Pages/Dashboard/MarketingInsights/pages/SocialPostList/SocialPostTableColumnActions.tsx
@@ -1,21 +1,15 @@
-import { Button, IconButton, makeStyles } from '@material-ui/core'
+import { IconButton, makeStyles } from '@material-ui/core'
 import { mdiTrashCanOutline, mdiOpenInNew } from '@mdi/js'
+import { useSelector } from 'react-redux'
 
-import {
-  useDeleteSocialPost,
-  useUpdateSocialPost
-} from '@app/models/social-posts'
-import {
-  convertDateToTimestamp,
-  convertTimestampToDate
-} from '@app/utils/date-utils'
-import { futureTimeValidator } from '@app/utils/validations/future-time'
-import { DateTimePicker } from '@app/views/components/DateTimePicker'
+import { useDeleteSocialPost } from '@app/models/social-posts'
+import { selectUserId } from '@app/selectors/user'
 import LinkButton from '@app/views/components/LinkButton'
 import { muiIconSizes } from '@app/views/components/SvgIcons/icon-sizes'
 import { SvgIcon } from '@app/views/components/SvgIcons/SvgIcon'
 
-import { isSocialPostExecuted } from './helpers'
+import { isSocialPostExecuted, isSocialPostFailed } from './helpers'
+import SocialPostPreviewButton from './SocialPostPreviewButton'
 
 const useStyles = makeStyles(
   theme => ({
@@ -45,18 +39,17 @@ function SocialPostTableColumnActions({
 
   const { mutate: deleteSocialPost, isLoading: isDeleting } =
     useDeleteSocialPost()
-  const { mutate: updateSocialPost, isLoading: isUpdating } =
-    useUpdateSocialPost()
+
+  const { mutate: silentDeleteSocialPost, isLoading: isSilentDeleting } =
+    useDeleteSocialPost({ notify: { onSuccess: '', onError: '' } })
 
   const handleDelete = () => deleteSocialPost(socialPost)
 
-  const handleDueAtChange = (date: Date) =>
-    updateSocialPost({
-      socialPost,
-      inputData: { due_at: convertDateToTimestamp(date) }
-    })
+  const handleSilentDelete = () => silentDeleteSocialPost(socialPost)
 
-  const isWorking = isDeleting || isUpdating
+  const isSocialPostMine = useSelector(selectUserId) === socialPost.owner.id
+
+  const isWorking = isDeleting || isSilentDeleting
 
   // If it is already posted, it can display the post link
   if (socialPost.post_link) {
@@ -74,39 +67,26 @@ function SocialPostTableColumnActions({
     )
   }
 
-  // If it is executed, display nothing
-  if (isSocialPostExecuted(socialPost)) {
+  // Do not show the actions if the social post is not mine
+  if (!isSocialPostMine) {
     return null
   }
 
-  const selectedDate = convertTimestampToDate(socialPost.due_at)
+  // If it is executed successfully, display nothing
+  if (isSocialPostExecuted(socialPost) && !isSocialPostFailed(socialPost)) {
+    return null
+  }
 
-  // Otherwise, it is not executed yet and we need to have the reschedule and delete buttons
+  // Otherwise, it is not executed yet and we need to have the reschedule/retry and delete buttons
   return (
     <div className={classes.root}>
-      <DateTimePicker
-        onClose={handleDueAtChange}
-        showTimePicker
-        selectedDate={selectedDate}
-        defaultSelectedDate={selectedDate}
-        datePickerModifiers={{
-          disabled: {
-            before: new Date()
-          }
-        }}
-        validate={futureTimeValidator}
-      >
-        {({ handleOpen }) => (
-          <Button
-            color="primary"
-            size="small"
-            onClick={handleOpen}
-            disabled={isWorking}
-          >
-            Reschedule
-          </Button>
-        )}
-      </DateTimePicker>
+      <SocialPostPreviewButton
+        socialPost={socialPost}
+        color="primary"
+        size="small"
+        disabled={isWorking}
+        onSocialPostScheduledOrSent={handleSilentDelete}
+      />
       <IconButton
         className={classes.deleteButton}
         size="small"
