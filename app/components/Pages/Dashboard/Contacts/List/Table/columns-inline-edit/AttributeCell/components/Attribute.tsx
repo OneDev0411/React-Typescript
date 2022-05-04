42bffb28131471064375b84b609ceeb007c9c30d
OneDev0411 <cryptoapple1109@gmail.com>
2022-05-04 12:27:35 +0430

    wip_feat(contact): ui audit attr cell

--- app/components/Pages/Dashboard/Contacts/List/Table/columns-inline-edit/AttributeCell/components/Attribute.tsx
+++ app/components/Pages/Dashboard/Contacts/List/Table/columns-inline-edit/AttributeCell/components/Attribute.tsx
@@ -1,13 +1,20 @@
-import { makeStyles, Theme } from '@material-ui/core'
+import { ReactNode } from 'react'
+
+import { InputBase, NativeSelect, makeStyles, Theme } from '@material-ui/core'
 import { mdiContentCopy, mdiTrashCanOutline } from '@mdi/js'
 
 import { SvgIcon, muiIconSizes } from '@app/views/components/SvgIcons'
 
-// interface Props {
-// value: string
-// label: string
-// onDelete: () => void
-// }
+interface Props {
+  labels: string[]
+  values: {
+    value: string
+    label: string
+  }
+  actions?: ReactNode
+  onAdd: () => void
+  onDelete: () => void
+}
 
 const useStyles = makeStyles(
   (theme: Theme) => ({
@@ -17,14 +24,32 @@ const useStyles = makeStyles(
       padding: theme.spacing(1, 2),
       borderBottom: `1px solid ${theme.palette.divider}`
     },
-    valueContainer: {},
+    valuesContainer: {
+      display: 'flex',
+      paddingRight: theme.spacing(1),
+      alignItems: 'center',
+      flexGrow: 1
+    },
+    value: {
+      // background: 'red',
+      flexGrow: 1
+    },
+    label: {
+      // background: 'blue'
+    },
     actionContainer: {
       display: 'flex',
       alignItems: 'center',
       color: theme.palette.action.disabled
     },
+    customActionContainer: {
+      marginRight: theme.spacing(0.5)
+    },
     actionButton: {
-      cursor: 'pointer'
+      cursor: 'pointer',
+      '&:not(:last-child)': {
+        marginRight: theme.spacing(0.5)
+      }
     }
   }),
   {
@@ -32,23 +57,35 @@ const useStyles = makeStyles(
   }
 )
 
-export function Attribute() {
+export function Attribute({ actions }: Partial<Props>) {
   const classes = useStyles()
+
+  const handleCopyAttribute = () => {
+    console.log('handleCopyAttribute')
+  }
+  const handleRemoveAttribute = () => {
+    console.log('handleRemoveAttribute')
+  }
 
   return (
     <div className={classes.container}>
-      <div className={classes.valueContainer}>valueContainer</div>
+      <div className={classes.valuesContainer}>
+        <InputBase name="value" margin="none" className={classes.value} />
+        <NativeSelect id="select" className={classes.label}>
+          <option value="22">test</option>
+          <option value="33">test 2</option>
+        </NativeSelect>
+      </div>
       <div className={classes.actionContainer}>
-        <SvgIcon
-          path={mdiContentCopy}
-          size={muiIconSizes.small}
-          className={classes.actionButton}
-        />
-        <SvgIcon
-          path={mdiTrashCanOutline}
-          size={muiIconSizes.small}
-          className={classes.actionButton}
-        />
+        {actions && (
+          <div className={classes.customActionContainer}>{actions}</div>
+        )}
+        <div className={classes.actionButton} onClick={handleCopyAttribute}>
+          <SvgIcon path={mdiContentCopy} size={muiIconSizes.small} />
+        </div>
+        <div className={classes.actionButton} onClick={handleRemoveAttribute}>
+          <SvgIcon path={mdiTrashCanOutline} size={muiIconSizes.small} />
+        </div>
       </div>
     </div>
   )
