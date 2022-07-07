8d719f8c42135c6de557512464e4ff7e2a0d71f5
OneDev0411 <cryptoapple1109@gmail.com>
2022-07-07 18:25:21 +0300

    wip_fix(server): add log to check checkOFACEmbargo issue on beta

--- server/app/middlewares/check-ofac-embargo/index.ts
+++ server/app/middlewares/check-ofac-embargo/index.ts
@@ -11,6 +11,16 @@ export function checkOFACEmbargo(
     (req.headers['x-forwarded-for'] as string) ??
     req.ip
 
+  // TODO: this is just for the sake of test on server,
+  // Warn: please remove this line after testing
+  console.log({
+    userIP,
+    reqIP: req.ip,
+    cfIP: req.headers['cf-connecting-ip'],
+    xForward: req.headers['x-forwarded-for'],
+    check: check(userIP)
+  })
+
   if (check(userIP)) {
     next()
 
