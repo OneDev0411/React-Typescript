41151af1bd14233c6caa870ea8c412af14e1b3e0
OneDev0411 <cryptoapple1109@gmail.com>
2022-07-26 15:28:03 +0300

    refactor(mc): change useMarketingCenterMediums sort order (#5610)

--- app/hooks/use-marketing-center-mediums.ts
+++ app/hooks/use-marketing-center-mediums.ts
@@ -4,7 +4,23 @@ import _groupBy from 'lodash/groupBy'
 import { useDeepCompareEffect } from 'react-use'
 
 import { notNull } from '@app/utils/ts-utils'
-import { onlyUnique, sortAlphabetically } from 'utils/helpers'
+import { compare, onlyUnique } from 'utils/helpers'
+
+const MARKETING_TEMPLATE_MEDIUM_PRIORITY: Record<
+  IMarketingTemplateMedium,
+  number
+> = {
+  Social: 1,
+  InstagramStory: 2,
+  FacebookCover: 3,
+  LinkedInCover: 3,
+  RealtorCover: 3,
+  TwitterCover: 3,
+  YouTubeCover: 3,
+  Letter: 4,
+  Email: 5,
+  Website: 6
+}
 
 function getFormattedUniqueMediums(
   templatesOrAssets: (IBrandMarketingTemplate | IBrandAsset)[]
@@ -17,7 +33,15 @@ function getFormattedUniqueMediums(
     })
     .filter(notNull)
 
-  return mediums.filter(onlyUnique).sort(sortAlphabetically).reverse()
+  return mediums
+    .filter(onlyUnique)
+    .sort((a, b) =>
+      compare(
+        MARKETING_TEMPLATE_MEDIUM_PRIORITY[b],
+        MARKETING_TEMPLATE_MEDIUM_PRIORITY[a]
+      )
+    )
+    .reverse()
 }
 
 export type TemplateTypeToMediumsMap = {
