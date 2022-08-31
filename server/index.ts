c021d15f3a4928a8ed62d592ac30cf6e9c7be154
OneDev0411 <cryptoapple1109@gmail.com>
2022-08-31 19:44:09 +0530

    fix: removing server code (#6557)

--- server/index.ts
+++ server/index.ts
@@ -94,16 +94,15 @@ if (isProduction) {
   app.use(enforce.HTTPS())
 
   const setHeaders = (res: Response, path: string) => {
-    // prevent caching of index.html
-    // if (serveStatic.mime.lookup(path) === 'text/html') {
-    //   res.setHeader('Surrogate-Control', 'no-store')
-    //   res.setHeader(
-    //     'Cache-Control',
-    //     'no-store, no-cache, must-revalidate, proxy-revalidate'
-    //   )
-    //   res.setHeader('Pragma', 'no-cache')
-    //   res.setHeader('Expires', '0')
-    // }
+    if (serveStatic.mime.lookup(path) === 'text/html') {
+      res.setHeader('Surrogate-Control', 'no-store')
+      res.setHeader(
+        'Cache-Control',
+        'no-store, no-cache, must-revalidate, proxy-revalidate'
+      )
+      res.setHeader('Pragma', 'no-cache')
+      res.setHeader('Expires', '0')
+    }
   }
 
   app.use(
