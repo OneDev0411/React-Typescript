ec84b10f08700c5c9408308ca55854ac0e64fafb
OneDev0411 <cryptoapple1109@gmail.com>
2022-09-13 18:18:38 +0300

    refactor(mc): sort template picker categories alphabetically (#6789)

--- app/views/components/MarketingTemplatePickers/MarketingTemplateAndTemplateInstancePickerModal/index.tsx
+++ app/views/components/MarketingTemplatePickers/MarketingTemplateAndTemplateInstancePickerModal/index.tsx
@@ -13,6 +13,7 @@ import {
 import { useActiveBrandId } from '@app/hooks/brand'
 import { getBrandMarketingCategories } from '@app/models/brand/get-brand-marketing-categories'
 import { sortAlphabetically } from '@app/utils/helpers'
+import { getTemplateTypeLabel } from '@app/utils/marketing-center/get-template-type-label'
 import MarketingSearchInput from '@app/views/components/MarketingSearchInput'
 import MarketingTemplateAndTemplateInstancePicker from '@app/views/components/MarketingTemplatePickers/MarketingTemplateAndTemplateInstancePicker'
 import {
@@ -69,14 +70,19 @@ export default function MarketingTemplateAndTemplateInstancePickerModal({
         )
 
         const loadedCategories = Object.keys(brandMarketingCategories).sort(
-          sortAlphabetically
+          (a, b) =>
+            sortAlphabetically(getTemplateTypeLabel(a), getTemplateTypeLabel(b))
         ) as IMarketingTemplateType[]
 
         setCategories(loadedCategories)
         setSelectedTab(loadedCategories[0])
       } catch (e) {
         // use template types as a plan B when we can load accurate categories list
-        setCategories(pickerProps.templateTypes.sort(sortAlphabetically))
+        setCategories(
+          pickerProps.templateTypes.sort((a, b) =>
+            sortAlphabetically(getTemplateTypeLabel(a), getTemplateTypeLabel(b))
+          )
+        )
         console.log(e)
       }
     }
