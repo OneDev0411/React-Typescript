ab87a2230deef089382a4f55a0b023efe268e9a3
OneDev0411 <cryptoapple1109@gmail.com>
2022-09-28 15:41:45 +0530

    fix(assigneeemail): handling email template (#6557)

--- app/components/Pages/Dashboard/Contacts/Profile/Assignee/AssigneeEmail.tsx
+++ app/components/Pages/Dashboard/Contacts/Profile/Assignee/AssigneeEmail.tsx
@@ -27,19 +27,16 @@ const AssigneeEmail = ({
             recipient_type: 'Email'
           }
         ],
-        body: `Hi ${currentAgentName},
-                Can you help me out?
-                As I so often turn to you for advice when it comes to my business, I'd love if you could help in finding me some new Clients.
-
-                If you could refer anyone in your network that you think would benefit from my help or services, please send them my way.
-
-                I know your good word goes a long way with these things, and because of your insight on my business, you know what a qualified client looks like for me!
-
-                I appreciate it!
-
-                Cheers,
-                ${contactName}
-                `
+        body: [
+          `Hi ${currentAgentName} <br/> <br/>`,
+          'Can you help me out? <br/><br/>',
+          'As I so often turn to you for advice when it comes to my business, I&apos;d love if you could help in finding me some new Clients.<br/><br/>',
+          'If you could refer anyone in your network that you think would benefit from my help or services, please send them my way. <br/><br/>',
+          'I know your good word goes a long way with these things, and because of your insight on my business, you know what a qualified client looks like for me! <br/><br/>',
+          'I appreciate it!<br/><br/>',
+          'Cheers, <br/>',
+          `${contactName}`
+        ].join('')
       }}
     />
   )
