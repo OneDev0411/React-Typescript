ef71186e1309e2bb64a0117a1300bc9b1c801b99
OneDev0411 <cryptoapple1109@gmail.com>
2022-10-06 17:29:04 -0400

    fix(deals): fixed issue
    skip
    
    skip
     (#6508)

--- app/components/Pages/Dashboard/Deals/components/ActionsButton/styled.ts
+++ app/components/Pages/Dashboard/Deals/components/ActionsButton/styled.ts
@@ -19,7 +19,7 @@ export const PrimaryAction = styled.div<{
     justify-content: center;
     height: 100%;
     ${theme.typography.subtitle2};
-
+    width: max-content;
     width: ${(props: PrimaryActionProps) =>
       props.hasSecondaryActions ? '8.5rem' : 'auto'};
     padding: ${(props: PrimaryActionProps) =>
@@ -39,7 +39,6 @@ export const PrimaryAction = styled.div<{
       border-radius: ${theme.shape.borderRadius}px;
 
       &.Add {
-        width: max-content;
         &:before {
           content '+';
           padding-right: 0.5rem;
@@ -51,7 +50,6 @@ export const PrimaryAction = styled.div<{
       }
 
       &.Remove {
-        width: max-content;
         &:before {
           content '× ';
           padding-right: 0.5rem;
