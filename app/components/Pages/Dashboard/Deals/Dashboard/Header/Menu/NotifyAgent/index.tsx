66daf772780e4e13cd4ab688ac34a6c278ecec7a
OneDev0411 <cryptoapple1109@gmail.com>
2022-10-26 20:24:34 +0300

    fix(deal): add emoji to notify emails (#6907)

--- app/components/Pages/Dashboard/Deals/Dashboard/Header/Menu/NotifyAgent/index.tsx
+++ app/components/Pages/Dashboard/Deals/Dashboard/Header/Menu/NotifyAgent/index.tsx
@@ -130,7 +130,7 @@ export function NotifyAgents({ deal }: Props) {
             to: getEmailRecipients(),
             body: [
               'Hello,<br /><br />',
-              'Just a friendly reminder that the following documents are still outstanding for your Deal.<br />',
+              'Just a friendly reminder that the following documents are still outstanding for your Deal.⏳<br />',
               'You can visit the below links to upload the missing documents.',
               '<ul>',
               ...selectedTasks.map(
