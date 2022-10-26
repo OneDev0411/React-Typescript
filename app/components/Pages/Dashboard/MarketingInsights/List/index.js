43521c4c0789697f5d74a7e12beb1187bdb64d24
OneDev0411 <cryptoapple1109@gmail.com>
2022-10-26 18:38:14 +0300

    fix(mc): insights table horizontal scroll (#6682)

--- app/components/Pages/Dashboard/MarketingInsights/List/index.js
+++ app/components/Pages/Dashboard/MarketingInsights/List/index.js
@@ -78,7 +78,6 @@ export default function List(props) {
         header: 'Title',
         id: 'title-date',
         primary: true,
-        width: isScheduled ? '100%' : '38%',
         accessor: row => row.due_at,
         render: ({ row }) => <TitleColumn data={row} reloadList={reloadList} />
       },
