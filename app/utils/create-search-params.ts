96c55824b6b99273241bbb79cad566c14930264e
OneDev0411 <cryptoapple1109@gmail.com>
2022-11-09 19:22:50 +0300

    refactor(router): extend withRouter hook (#6942)

--- app/utils/create-search-params.ts
+++ app/utils/create-search-params.ts
@@ -1,14 +1,17 @@
 /**
- * A proxy hook to react-router-dom@6 useNavigate
+ * Create search params string from a query object
  * @example <caption>Without url</caption>
- * getLocationSearch({query: 'something', id: 1})
+ * createSearchParams({query: 'something', id: 1})
  * // Returns: '?query=something&id=1
  * @example <caption>With url</caption>
- * getLocationSearch({query: 'something', id: 1},'/path?agent=12')
+ * createSearchParams({query: 'something', id: 1},'/path?agent=12')
  * // Returns: '?agent=12&query=something&id=1
- * @returns {String} Returns location search string
+ * @returns {string} Returns location search string
  */
-export const getLocationSearch = (query: object, url: string = ''): String => {
+export const createSearchParams = (
+  query: Record<string, string>,
+  url: string = ''
+): string => {
   const splittedUrl = url.split('?')
   const embeddedSearch = splittedUrl[1]
 
