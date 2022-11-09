910930f20b4bc1940218bcf7df0c2fa2dc9b105f
OneDev0411 <cryptoapple1109@gmail.com>
2022-11-09 12:37:38 +0530

    fix(contact-profile): handling scrollbar timeline (#6792)

--- app/views/components/ContactProfileTimeline/components/List/EventLoader/index.tsx
+++ app/views/components/ContactProfileTimeline/components/List/EventLoader/index.tsx
@@ -59,7 +59,10 @@ const useStyles = makeStyles(
       top: 0,
       left: 0,
       right: 0,
-      bottom: 0
+      bottom: 0,
+      '&::-webkit-scrollbar': {
+        display: 'none'
+      }
     },
     progressLoaderContainer: {
       position: 'absolute',
