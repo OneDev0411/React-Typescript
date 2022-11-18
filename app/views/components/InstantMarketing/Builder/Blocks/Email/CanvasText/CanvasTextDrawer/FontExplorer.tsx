6e9d9fd6d242e94ebf399f96fe67fb65dd4e1d69
OneDev0411 <cryptoapple1109@gmail.com>
2022-11-18 14:37:44 +0300

    feat(mc): develop Font Explorer for fancy font editor (#6943)

---
+++ app/views/components/InstantMarketing/Builder/Blocks/Email/CanvasText/CanvasTextDrawer/FontExplorer.tsx
@@ -0,0 +1,184 @@
+import { useMemo, useState } from 'react'
+
+import {
+  Button,
+  InputAdornment,
+  makeStyles,
+  TextField,
+  Theme
+} from '@material-ui/core'
+import { mdiMagnify } from '@mdi/js'
+import { Font } from '@samuelmeuli/font-manager'
+import cn from 'classnames'
+import FontFaceObserver from 'fontfaceobserver'
+import matchSorter from 'match-sorter'
+
+import { useFonts } from '@app/hooks/use-fonts'
+import { SvgIcon } from '@app/views/components/SvgIcons'
+
+const LIMIT = 30
+
+const useStyles = makeStyles(
+  (theme: Theme) => ({
+    root: {
+      margin: theme.spacing(2, 0)
+    },
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+      gap: theme.spacing(2)
+    },
+    showMore: {
+      margin: theme.spacing(2, 0)
+    },
+    block: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: theme.shape.borderRadius,
+      textAlign: 'center',
+      fontSize: 20,
+      padding: theme.spacing(2),
+      '&.active': {
+        color: theme.palette.secondary.main
+      },
+      '&:hover': {
+        backgroundColor: theme.palette.grey['100'],
+        cursor: 'pointer'
+      }
+    },
+    searchBar: {
+      position: 'sticky',
+      top: 0,
+      backgroundColor: '#fff',
+      padding: theme.spacing(2, 0)
+    },
+    searchBarTextFieldIcon: {
+      color: theme.palette.grey['400']
+    }
+  }),
+  {
+    name: 'MarketingCenterBlocksCanvasTextFontExplorer'
+  }
+)
+
+interface Props {
+  onSelect: (font: Font) => void
+}
+
+export function FontExplorer({ onSelect }: Props) {
+  const classes = useStyles()
+  const [fonts] = useFonts({
+    limit: 300,
+    categories: ['handwriting', 'display', 'serif', 'monospace'],
+    sort: 'popularity',
+    scripts: ['latin', 'latin-ext'],
+    variants: ['500', '700']
+  })
+  const [activeFont, setActiveFont] = useState<Nullable<Font>>(null)
+  const [displayCount, setDisplayCount] = useState(LIMIT)
+  const [searchCriteria, setSearchCriteria] = useState('')
+
+  const list = useMemo(() => {
+    const list = Object.values(Object.fromEntries(fonts))
+
+    const filteredList =
+      searchCriteria.length > 1
+        ? matchSorter(list, searchCriteria, {
+            keys: ['family', 'kind']
+          })
+        : list
+
+    return filteredList.slice(0, displayCount)
+  }, [fonts, displayCount, searchCriteria])
+
+  const handleShowMore = () => {
+    setDisplayCount(count =>
+      count + LIMIT < fonts.size ? count + LIMIT : fonts.size
+    )
+  }
+
+  const handleSelectFont = (font: Font) => {
+    setActiveFont(font)
+
+    new FontFaceObserver(font.family)
+      .load()
+      .then(() => {
+        onSelect(font)
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+
+  const isShowMoreVisible = () => {
+    if (searchCriteria && list.length < displayCount) {
+      return false
+    }
+
+    if (displayCount >= fonts.size) {
+      return false
+    }
+
+    return true
+  }
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.searchBar}>
+        <TextField
+          placeholder="Search font name"
+          color="secondary"
+          size="small"
+          fullWidth
+          value={searchCriteria}
+          onChange={e => {
+            setSearchCriteria(e.target.value)
+            setDisplayCount(LIMIT)
+          }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SvgIcon
+                  path={mdiMagnify}
+                  className={classes.searchBarTextFieldIcon}
+                />
+              </InputAdornment>
+            )
+          }}
+        />
+      </div>
+
+      <div className={classes.container}>
+        {list.map(font => (
+          <div
+            key={font.id}
+            className={cn(classes.block, {
+              active: activeFont?.id === font.id
+            })}
+            style={{
+              fontFamily: font.family
+            }}
+            onClick={() => handleSelectFont(font)}
+          >
+            {font.family}
+          </div>
+        ))}
+      </div>
+
+      {isShowMoreVisible() && (
+        <Button
+          variant="text"
+          color="secondary"
+          size="large"
+          className={classes.showMore}
+          fullWidth
+          onClick={handleShowMore}
+        >
+          Show More Fonts...
+        </Button>
+      )}
+    </div>
+  )
+}
