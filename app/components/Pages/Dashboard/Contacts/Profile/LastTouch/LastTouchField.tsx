d9b4b1b15489817564cf4db83d0f5cd556917d97
OneDev0411 <cryptoapple1109@gmail.com>
2022-11-29 22:35:25 +0530

    fix(contact-profile): last touch design update (#6792)

--- app/components/Pages/Dashboard/Contacts/Profile/LastTouch/LastTouchField.tsx
+++ app/components/Pages/Dashboard/Contacts/Profile/LastTouch/LastTouchField.tsx
@@ -12,60 +12,16 @@ const useStyles = makeStyles(
       justifyContent: 'space-between',
       color: theme.palette.grey[600],
       borderRadius: theme.shape.borderRadius,
-      cursor: 'pointer',
+      cursor: 'default',
       '&:hover': {
         textDecoration: 'none',
         color: theme.palette.grey[600],
-        background: theme.palette.action.hover
+        background: 'none'
       }
     },
     value: {
       color: theme.palette.grey[900],
       textAlign: 'right'
-    },
-    videoModeActionBar: {
-      position: 'absolute',
-      top: '90%',
-      right: '0',
-      display: 'flex',
-      zIndex: 1,
-      visibility: 'visible'
-    },
-    videoModeActionBarActive: {
-      visibility: 'visible'
-    },
-    action: {
-      padding: theme.spacing(0, 2),
-      background: theme.palette.background.paper,
-      borderRadius: theme.shape.borderRadius,
-      textAlign: 'center',
-      color: theme.palette.grey[800],
-      '& svg': {
-        color: theme.palette.grey[800],
-        margin: 'auto'
-      },
-      '&:hover': {
-        background: theme.palette.action.hover,
-        textDecoration: 'none'
-      }
-    },
-    actionLabel: {
-      display: 'block',
-      color: theme.palette.grey[800],
-      ...theme.typography.caption
-    },
-    actionContainer: {
-      display: 'flex',
-      alignItems: 'center',
-      background: theme.palette.background.paper,
-      padding: theme.spacing(0.5),
-      borderRadius: theme.shape.borderRadius,
-      boxShadow: theme.shadows[4]
-    },
-    actionsWrapper: {
-      width: '200px',
-      display: 'flex',
-      flexDirection: 'column'
     }
   }),
   { name: 'LatTouchField' }
