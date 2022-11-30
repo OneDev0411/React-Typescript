20463782db53d3054feca1f0c3b93443a7520632
OneDev0411 <cryptoapple1109@gmail.com>
2022-11-30 14:18:35 +0300

    feat(canvas text): implement text rotate property (#6943)

---
+++ app/views/components/InstantMarketing/Builder/Blocks/Email/CanvasText/CanvasTextDrawer/properties/TextRotateProperty.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react'
+
+import { Box, Slider, Typography } from '@material-ui/core'
+import { useDebounce } from 'react-use'
+
+import { useCanvasTextContext } from '../hooks/get-canvas-text-context'
+
+export function TextRotateProperty() {
+  const { label, preview } = useCanvasTextContext()
+
+  const [rotation, setRotation] = useState<number>(0)
+  const [debouncedRotation, setDebouncedRotation] = useState(rotation)
+
+  useDebounce(
+    () => {
+      setDebouncedRotation(rotation)
+    },
+    200,
+    [rotation]
+  )
+
+  useEffect(() => {
+    preview()
+  }, [debouncedRotation, preview])
+
+  const handleChangeRotation = (value: number) => {
+    setRotation(value)
+    setDebouncedRotation(value)
+    label?.rotate(value)
+  }
+
+  return (
+    <Box my={2}>
+      <Typography variant="body1" color="textSecondary">
+        Text Rotation
+      </Typography>
+
+      <Box mt={2}>
+        <Slider
+          value={rotation}
+          valueLabelDisplay="auto"
+          defaultValue={rotation}
+          min={-10}
+          max={10}
+          onChange={(_, value) => handleChangeRotation(value as number)}
+        />
+      </Box>
+    </Box>
+  )
+}
