10f9956ef2da56bc045db8adc27cf63e6a841d6d
OneDev0411 <cryptoapple1109@gmail.com>
2022-11-30 14:19:01 +0300

    feat(canvas text): implement text opacity property (#6943)

---
+++ app/views/components/InstantMarketing/Builder/Blocks/Email/CanvasText/CanvasTextDrawer/properties/TextOpacityProperty.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react'
+
+import { Box, Slider, Typography } from '@material-ui/core'
+import { useDebounce } from 'react-use'
+
+import { useCanvasTextContext } from '../hooks/get-canvas-text-context'
+
+export function TextOpacityProperty() {
+  const { setTextProperty, preview } = useCanvasTextContext()
+
+  const [opacity, setOpacity] = useState<number>(1)
+  const [debouncedOpacity, setDebouncedOpacity] = useState(opacity)
+
+  useDebounce(
+    () => {
+      setDebouncedOpacity(opacity)
+    },
+    200,
+    [opacity]
+  )
+
+  useEffect(() => {
+    preview()
+  }, [debouncedOpacity, preview])
+
+  const handleChangeOpacity = (value: number) => {
+    setOpacity(value)
+    setTextProperty('opacity', value)
+  }
+
+  return (
+    <Box my={2}>
+      <Typography variant="body1" color="textSecondary">
+        Text Opacity
+      </Typography>
+
+      <Box mt={2}>
+        <Slider
+          value={opacity}
+          valueLabelDisplay="auto"
+          defaultValue={opacity}
+          min={0.1}
+          max={1}
+          step={0.1}
+          onChange={(_, value) => handleChangeOpacity(value as number)}
+        />
+      </Box>
+    </Box>
+  )
+}
