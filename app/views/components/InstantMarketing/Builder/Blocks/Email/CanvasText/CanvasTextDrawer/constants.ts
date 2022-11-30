18c00f66e291db05918f8661a43e6606b60a2766
OneDev0411 <cryptoapple1109@gmail.com>
2022-11-30 14:20:16 +0300

    refactor(canvas text): implement constants (#6943)

---
+++ app/views/components/InstantMarketing/Builder/Blocks/Email/CanvasText/CanvasTextDrawer/constants.ts
@@ -0,0 +1,23 @@
+import { Konva, LabelConfig } from 'pikaso'
+
+export interface CanvasTextProperties {
+  container: Konva.LabelConfig
+  text: Konva.TextConfig
+  tag: Konva.TagConfig
+  config?: LabelConfig
+}
+
+export const DefaultCanvasTextProperties: CanvasTextProperties = {
+  container: {
+    x: 0,
+    y: 0
+  },
+  text: {
+    text: 'Your Text',
+    fontSize: 40,
+    lineHeight: 1.1,
+    padding: 10,
+    fill: '#000'
+  },
+  tag: {}
+}
