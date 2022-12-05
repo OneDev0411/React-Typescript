bd8376e33ce95b22ed2109788eafa5d845f0881f
OneDev0411 <cryptoapple1109@gmail.com>
2022-12-05 19:25:36 +0300

    refactor(url to blob): improve function (#6941)

--- app/utils/url-to-blob.ts
+++ app/utils/url-to-blob.ts
@@ -2,9 +2,7 @@ export function urlToBlob(url: string): Promise<Blob> {
   return new Promise((resolve, reject) => {
     fetch(url)
       .then(res => res.blob())
-      .then(blob => {
-        resolve(blob)
-      })
+      .then(resolve)
       .catch(reject)
   })
 }
