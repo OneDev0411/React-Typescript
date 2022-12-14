4c038871f9ec74c8f57b837a65a346a87e3fee75
OneDev0411 <cryptoapple1109@gmail.com>
2022-12-14 20:34:25 +0300

    feat(insight): separate sort options of executed and scheduled (#6946)

--- app/components/Pages/Dashboard/MarketingInsights/List/ScheduledList.tsx
+++ app/components/Pages/Dashboard/MarketingInsights/List/ScheduledList.tsx
@@ -7,17 +7,15 @@ import type { SortableColumnsType } from '../types'
 
 import { InsightsTable } from './InsightsTable'
 
-const SORT_FIELD_INSIGHT_KEY = 'insight_layout_sort_field'
+const SORT_FIELD_INSIGHT_KEY = 'insight_scheduled_sort_field'
 
 const SortOptions: SortableColumnsType[] = [
-  { label: 'Newest Executed', value: '-executed_at', ascending: false },
-  { label: 'Oldest Executed', value: 'executed_at', ascending: true },
   { label: 'Newest Created', value: '-created_at', ascending: false },
   { label: 'Oldest Created', value: 'created_at', ascending: true }
 ]
 
-function InsightsList() {
-  useTitle('Insight | Rechat')
+function InsightScheduledList() {
+  useTitle('Scheduled Insight | Rechat')
 
   return (
     <InsightsPageLayout
@@ -31,4 +29,4 @@ function InsightsList() {
   )
 }
 
-export default withRouter(InsightsList)
+export default withRouter(InsightScheduledList)
