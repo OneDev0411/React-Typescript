671f13b05be43bc4e312a0cf0d3c562a98d5af45
OneDev0411 <cryptoapple1109@gmail.com>
2023-01-04 02:53:45 +0300

    feat(document repo): refactor document folder selection (#6918)

--- app/views/components/DocumentsRepository/components/DocumentFolder.tsx
+++ app/views/components/DocumentsRepository/components/DocumentFolder.tsx
@@ -1,5 +1,7 @@
-import { Box, Checkbox, makeStyles, Theme, Typography } from '@material-ui/core'
+import { Box, makeStyles, Theme, Typography } from '@material-ui/core'
 import pluralize from 'pluralize'
+
+import { DocumentFolderSelection } from './DocumenFolderSelection'
 
 const useStyles = makeStyles(
   (theme: Theme) => ({
@@ -20,17 +22,29 @@ const useStyles = makeStyles(
 interface Props {
   title: string
   totalCount: number
+  selectionList: Record<string, boolean>
+  onToggleFolderSelection: (checked: boolean) => void
   children: React.ReactNode
 }
 
-export function DocumentFolder({ title, totalCount, children }: Props) {
+export function DocumentFolder({
+  title,
+  totalCount,
+  selectionList,
+  children,
+  onToggleFolderSelection
+}: Props) {
   const classes = useStyles()
 
   return (
     <Box className={classes.root} mb={2}>
       <Box display="flex" alignItems="center" className={classes.header}>
         <Box mr={1}>
-          <Checkbox color="primary" indeterminate={false} />
+          <DocumentFolderSelection
+            totalCount={totalCount}
+            selectionList={selectionList}
+            onChange={onToggleFolderSelection}
+          />
         </Box>
         <Typography variant="subtitle1">{title}</Typography>
         <Box ml={1}>
