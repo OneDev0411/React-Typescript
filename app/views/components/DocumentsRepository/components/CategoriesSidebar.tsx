c8e9cd1687794b0aa7b96d09fddd915793d25a57
OneDev0411 <cryptoapple1109@gmail.com>
2023-01-03 20:12:37 +0300

    feat(document repo): implement categories sidebar (#6918)

--- app/views/components/DocumentsRepository/components/CategoriesSidebar.tsx
+++ app/views/components/DocumentsRepository/components/CategoriesSidebar.tsx
@@ -1,5 +1,99 @@
-import { Box } from '@material-ui/core'
+import { Box, makeStyles, Theme, Typography } from '@material-ui/core'
+import { Skeleton } from '@material-ui/lab'
+import { mdiChevronRight, mdiFolderOutline } from '@mdi/js'
+import cn from 'classnames'
 
-export function CategoriesSidebar() {
-  return <Box>++++</Box>
+import { muiIconSizes, SvgIcon } from '../../SvgIcons'
+import { useDocumentRepositoryContext } from '../context/use-document-repository-context'
+
+const useStyles = makeStyles(
+  (theme: Theme) => ({
+    root: {
+      padding: theme.spacing(1, 1, 1, 3),
+      color: theme.palette.tertiary.main,
+      '&.active': {
+        color: theme.palette.secondary.main,
+        backgroundColor: theme.palette.info.ultralight
+      }
+    },
+    folderIcon: {
+      color: theme.palette.tertiary.main,
+      '&.active': {
+        color: theme.palette.secondary.main
+      }
+    },
+    chevronIcon: {
+      visibility: 'hidden',
+      '&.active': {
+        color: theme.palette.secondary.main,
+        visibility: 'visible'
+      }
+    }
+  }),
+  {
+    name: 'DocumentRepositoryCategoriesSidebar'
+  }
+)
+
+interface Props {
+  isFetching: boolean
+  categories: string[]
+}
+
+export function CategoriesSidebar({ isFetching, categories }: Props) {
+  const classes = useStyles()
+  const { activeCategoryIndex } = useDocumentRepositoryContext()
+
+  if (isFetching) {
+    return (
+      <Box px={3}>
+        {[...Array(5).keys()].map((_, index) => (
+          <Box key={index} mb={1}>
+            <Skeleton variant="rect" width="100%" height="20px" />
+          </Box>
+        ))}
+      </Box>
+    )
+  }
+
+  return (
+    <Box pt={2}>
+      {categories.map((categoryName, index) => (
+        <Box
+          key={index}
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          className={cn(classes.root, {
+            active: activeCategoryIndex === index
+          })}
+        >
+          <Box display="flex" alignItems="center">
+            <SvgIcon
+              className={cn(classes.folderIcon, {
+                active: activeCategoryIndex === index
+              })}
+              path={mdiFolderOutline}
+              size={muiIconSizes.medium}
+              rightMargined
+            />
+            <Typography variant="subtitle1">
+              {categoryName || 'Untitled'}
+            </Typography>
+          </Box>
+
+          <Box display="flex" alignItems="center">
+            <SvgIcon
+              className={cn(classes.chevronIcon, {
+                active: activeCategoryIndex === index
+              })}
+              path={mdiChevronRight}
+              size={muiIconSizes.medium}
+              rightMargined
+            />
+          </Box>
+        </Box>
+      ))}
+    </Box>
+  )
 }
