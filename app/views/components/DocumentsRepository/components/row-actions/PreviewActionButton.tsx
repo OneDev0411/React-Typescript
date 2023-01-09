9ebb1bdeeb901a9ab944cfea6a85bb48fe5badf7
OneDev0411 <cryptoapple1109@gmail.com>
2023-01-10 01:29:45 +0330

    feat(documents repo): improve preview action button (#6918)

--- app/views/components/DocumentsRepository/components/row-actions/PreviewActionButton.tsx
+++ app/views/components/DocumentsRepository/components/row-actions/PreviewActionButton.tsx
@@ -1,11 +1,37 @@
-import { Button } from '@material-ui/core'
+import { Button, CircularProgress } from '@material-ui/core'
 
-export function PreviewActionButton() {
-  const handleClick = () => {}
+import { useFetchForms } from '../../queries/use-fetch-forms'
+
+interface Props {
+  form: IBrandForm
+}
+
+export function PreviewActionButton({ form }: Props) {
+  const { fetchForms, isFetching } = useFetchForms()
+
+  const handleClick = async () => {
+    const result = await fetchForms([form.id])
+
+    if (result) {
+      window.open(result[0].url)
+    }
+  }
 
   return (
-    <Button size="small" variant="outlined" onClick={handleClick}>
-      Preview
+    <Button
+      size="small"
+      variant="outlined"
+      disabled={isFetching}
+      onClick={handleClick}
+    >
+      {isFetching ? (
+        <>
+          <CircularProgress size={20} />
+          &nbsp;Opening
+        </>
+      ) : (
+        <>Preview</>
+      )}
     </Button>
   )
 }
