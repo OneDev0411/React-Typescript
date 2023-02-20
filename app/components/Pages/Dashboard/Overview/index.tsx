f47070c92356c83bfc86d3d5790b41746d7adf15
OneDev0411 <cryptoapple1109@gmail.com>
2023-02-20 15:11:39 -0800

    feat(theme): new background color for #7177

--- app/components/Pages/Dashboard/Overview/index.tsx
+++ app/components/Pages/Dashboard/Overview/index.tsx
@@ -1,4 +1,3 @@
-import { makeStyles, Theme } from '@material-ui/core/styles'
 import { useTitle } from 'react-use'
 
 import { withRouter } from '@app/routes/with-router'
@@ -6,20 +5,11 @@ import PageLayout from 'components/GlobalPageLayout'
 
 import Welcome from './Welcome'
 
-const useStyles = makeStyles(
-  (theme: Theme) => ({
-    layout: { backgroundColor: theme.palette.background.default }
-  }),
-  { name: 'ThisWeeksSchedule' }
-)
-
 function OverviewDashboard() {
   useTitle('Rechat | Today')
 
-  const classes = useStyles()
-
   return (
-    <PageLayout className={classes.layout}>
+    <PageLayout>
       <PageLayout.Header title="Today" />
       <PageLayout.Main mt={0}>
         <Welcome />
